f8fe5e8b9c30844f73447d28a00e34b969ca3f1d
Xiang Fu <xiafu@linkedin.com>
2014-06-12 16:21:39 -0700

    PINOT- 826 : Adding some simple aggregation functions: Count, Max, Min, Sum, SumDouble.

---
+++ query/src/main/java/com/linkedin/pinot/query/aggregation/function/MaxAggregationFunction.java
@@ -0,0 +1,80 @@
+package com.linkedin.pinot.query.aggregation.function;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.linkedin.pinot.index.segment.ColumnarReader;
+import com.linkedin.pinot.index.segment.IndexSegment;
+import com.linkedin.pinot.query.aggregation.AggregationFunction;
+import com.linkedin.pinot.query.aggregation.AggregationResult;
+import com.linkedin.pinot.query.aggregation.CombineLevel;
+import com.linkedin.pinot.query.aggregation.data.DoubleContainer;
+import com.linkedin.pinot.query.utils.IntArray;
+
+
+public class MaxAggregationFunction implements AggregationFunction {
+
+  private String _maxColumnName;
+
+  public MaxAggregationFunction() {
+
+  }
+
+  public MaxAggregationFunction(String maxColumnName) {
+    this._maxColumnName = maxColumnName;
+  }
+
+  @Override
+  public void init(JSONObject params) {
+    _maxColumnName = params.getString("column");
+  }
+
+  @Override
+  public DoubleContainer aggregate(IntArray docIds, int docIdCount, IndexSegment indexSegment) {
+    double maxValue = Double.NEGATIVE_INFINITY;
+    double tempValue;
+    ColumnarReader columnarReader = indexSegment.getColumnarReader(_maxColumnName);
+    for (int i = 0; i < docIdCount; ++i) {
+      tempValue = columnarReader.getDoubleValue(i);
+      if (tempValue > maxValue) {
+        maxValue = tempValue;
+      }
+    }
+    return new DoubleContainer(maxValue);
+  }
+
+  @Override
+  public List<AggregationResult> combine(List<AggregationResult> aggregationResultList, CombineLevel combineLevel) {
+    AggregationResult result = reduce(aggregationResultList);
+    aggregationResultList.clear();
+    aggregationResultList.add(result);
+    return null;
+  }
+
+  @Override
+  public AggregationResult reduce(List<AggregationResult> aggregationResultList) {
+    DoubleContainer maxValue = (DoubleContainer) aggregationResultList.get(0);
+    for (int i = 1; i < aggregationResultList.size(); ++i) {
+      if (((DoubleContainer) aggregationResultList.get(i)).getValue() > maxValue.getValue()) {
+        maxValue = (DoubleContainer) aggregationResultList.get(i);
+      }
+    }
+    return maxValue;
+  }
+
+  @Override
+  public JSONObject render(AggregationResult finalAggregationResult) {
+    try {
+      if (finalAggregationResult == null) {
+        throw new NoSuchElementException("Final result is null!");
+      }
+      return new JSONObject().put("max", String.format("%1.5f", finalAggregationResult));
+    } catch (JSONException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
