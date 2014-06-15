6afc14bbce73669763361c24072d870b92dbeeb6
Balaji Varadarajan <bvaradar+github@linkedin.com>
2014-06-14 17:20:13 -0700

    PINOT-793 Connection Pooling Implementation, Unit Testing and Metrics Integration

---
+++ transport/src/test/java/com/linkedin/pinot/transport/pool/TestAsyncPoolResourceManagerAdapter.java
@@ -0,0 +1,227 @@
+package com.linkedin.pinot.transport.pool;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.google.common.util.concurrent.MoreExecutors;
+import com.linkedin.pinot.transport.common.Callback;
+
+public class TestAsyncPoolResourceManagerAdapter {
+
+  @Test
+  public void testCreate() {
+
+    // Success
+    {
+      String key = "localhost:8080";
+      String value = "dummy";
+      MyPooledResourceManager rm = new MyPooledResourceManager(true, value);
+      AsyncPoolResourceManagerAdapter< String, String> adapter =
+          new AsyncPoolResourceManagerAdapter<String, String>(key, rm, MoreExecutors.sameThreadExecutor(), null);
+      MyCallback callback = new MyCallback();
+
+      adapter.create(callback);
+      Assert.assertEquals("Callback Resource", value, callback.getResource());
+      Assert.assertEquals("Callback onSuccess", true, callback.isOnSuccessCalled());
+      Assert.assertEquals("Callback onError", false, callback.isOnErrorCalled());
+      Assert.assertNull("Callback Error Null", callback.getThrowable());
+      Assert.assertEquals("Resource Manager create Key", key, rm.getCreateKey());
+    }
+
+    //Error
+    {
+      String key = "localhost:8080";
+      MyPooledResourceManager rm = new MyPooledResourceManager(true, null);
+      AsyncPoolResourceManagerAdapter< String, String> adapter =
+          new AsyncPoolResourceManagerAdapter<String, String>(key, rm, MoreExecutors.sameThreadExecutor(), null);
+      MyCallback callback = new MyCallback();
+
+      adapter.create(callback);
+      Assert.assertNull("Callback Resource", callback.getResource());
+      Assert.assertEquals("Callback onSuccess", false, callback.isOnSuccessCalled());
+      Assert.assertEquals("Callback onError", true, callback.isOnErrorCalled());
+      Assert.assertNotNull("Callback Error Null", callback.getThrowable());
+      Assert.assertEquals("Resource Manager create Key", key, rm.getCreateKey());
+    }
+  }
+
+  @Test
+  public void testValidate() {
+    //Success
+    {
+      String key = "localhost:8080";
+      String value = "dummy";
+      MyPooledResourceManager rm = new MyPooledResourceManager(true, null);
+      AsyncPoolResourceManagerAdapter< String, String> adapter =
+          new AsyncPoolResourceManagerAdapter<String, String>(key, rm, MoreExecutors.sameThreadExecutor(), null);
+
+      boolean ret = adapter.validateGet(value);
+      Assert.assertTrue("Validate Return", ret);
+      Assert.assertEquals("Resource Manager validate Key", key, rm.getKeyForValidate());
+      Assert.assertEquals("Resource Manager validate Resource", value, rm.getResourceForValidate());
+
+      String value2 = "dummy2";
+      ret = adapter.validatePut(value2);
+      Assert.assertTrue("Validate Return", ret);
+
+      Assert.assertEquals("Resource Manager validate Key", key, rm.getKeyForValidate());
+      Assert.assertEquals("Resource Manager validate Resource", value2, rm.getResourceForValidate());
+    }
+
+    //Error
+    {
+      String key = "localhost:8080";
+      String value = "dummy";
+      MyPooledResourceManager rm = new MyPooledResourceManager(false, null);
+      AsyncPoolResourceManagerAdapter< String, String> adapter =
+          new AsyncPoolResourceManagerAdapter<String, String>(key, rm, MoreExecutors.sameThreadExecutor(), null);
+
+      boolean ret = adapter.validateGet(value);
+      Assert.assertFalse("Validate Return", ret);
+
+      Assert.assertEquals("Resource Manager validate Key", key, rm.getKeyForValidate());
+      Assert.assertEquals("Resource Manager validate Resource", value, rm.getResourceForValidate());
+
+      String value2 = "dummy2";
+      ret = adapter.validatePut(value2);
+      Assert.assertFalse("Validate Return", ret);
+
+      Assert.assertEquals("Resource Manager validate Key", key, rm.getKeyForValidate());
+      Assert.assertEquals("Resource Manager validate Resource", value2, rm.getResourceForValidate());
+    }
+  }
+
+  @Test
+  public void testDestroy() {
+
+    // Success
+    {
+      String key = "localhost:8080";
+      String value = "dummy";
+      MyPooledResourceManager rm = new MyPooledResourceManager(true, null);
+      AsyncPoolResourceManagerAdapter< String, String> adapter =
+          new AsyncPoolResourceManagerAdapter<String, String>(key, rm, MoreExecutors.sameThreadExecutor(), null);
+      MyCallback callback = new MyCallback();
+
+      adapter.destroy(value, true, callback);
+      Assert.assertEquals("Callback Resource", value, callback.getResource());
+      Assert.assertEquals("Callback onSuccess", true, callback.isOnSuccessCalled());
+      Assert.assertEquals("Callback onError", false, callback.isOnErrorCalled());
+      Assert.assertNull("Callback Error Null", callback.getThrowable());
+      Assert.assertEquals("Resource Manager create Key", key, rm.getKeyForDestroy());
+      Assert.assertEquals("Resource Manager create Resource", value, rm.getResourceForDestroy());
+    }
+
+    // Error
+    {
+      String key = "localhost:8080";
+      String value = "dummy";
+      MyPooledResourceManager rm = new MyPooledResourceManager(false, null);
+      AsyncPoolResourceManagerAdapter< String, String> adapter =
+          new AsyncPoolResourceManagerAdapter<String, String>(key, rm, MoreExecutors.sameThreadExecutor(), null);
+      MyCallback callback = new MyCallback();
+
+      adapter.destroy(value, true, callback);
+      Assert.assertNull("Callback Resource", callback.getResource());
+      Assert.assertEquals("Callback onSuccess", false, callback.isOnSuccessCalled());
+      Assert.assertEquals("Callback onError", true, callback.isOnErrorCalled());
+      Assert.assertNotNull("Callback Error Null", callback.getThrowable());
+      Assert.assertEquals("Resource Manager create Key", key, rm.getKeyForDestroy());
+      Assert.assertEquals("Resource Manager create Resource", value, rm.getResourceForDestroy());
+    }
+  }
+
+  public static class MyPooledResourceManager implements PooledResourceManager<String, String>
+  {
+    private String _createKey;
+    private final String _createReturnValue;
+    private String _resourceForDestroy;
+    private String _resourceForValidate;
+    private String _keyForDestroy;
+    private String _keyForValidate;
+    private final boolean _boolReturnVal;
+
+    public MyPooledResourceManager(boolean returnVal, String createReturnValue)
+    {
+      _boolReturnVal = returnVal;
+      _createReturnValue = createReturnValue;
+    }
+
+    @Override
+    public String create(String key) {
+      _createKey = key;
+      return _createReturnValue;
+    }
+
+    @Override
+    public boolean destroy(String key, boolean isBad, String resource) {
+      _keyForDestroy = key;
+      _resourceForDestroy = resource;
+      return _boolReturnVal;
+    }
+
+    @Override
+    public boolean validate(String key, String resource) {
+      _keyForValidate = key;
+      _resourceForValidate = resource;
+      return _boolReturnVal;
+    }
+
+    public String getCreateKey() {
+      return _createKey;
+    }
+
+    public String getResourceForDestroy() {
+      return _resourceForDestroy;
+    }
+
+    public String getResourceForValidate() {
+      return _resourceForValidate;
+    }
+
+    public String getKeyForDestroy() {
+      return _keyForDestroy;
+    }
+
+    public String getKeyForValidate() {
+      return _keyForValidate;
+    }
+  }
+
+  public static class MyCallback implements Callback<String>
+  {
+    private String _resource;
+    private Throwable _throwable;
+    private boolean _onSuccessCalled;
+    private boolean _onErrorCalled;
+
+    @Override
+    public void onSuccess(String arg0) {
+      _onSuccessCalled = true;
+      _resource = arg0;
+    }
+
+    @Override
+    public void onError(Throwable arg0) {
+      _onErrorCalled = true;
+      _throwable = arg0;
+    }
+
+    public String getResource() {
+      return _resource;
+    }
+
+    public Throwable getThrowable() {
+      return _throwable;
+    }
+
+    public boolean isOnSuccessCalled() {
+      return _onSuccessCalled;
+    }
+
+    public boolean isOnErrorCalled() {
+      return _onErrorCalled;
+    }
+  }
+}
