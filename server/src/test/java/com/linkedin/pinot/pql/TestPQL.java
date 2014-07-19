45cb31958593df74752f8f1c470671ef9fc30b32
Dhaval Patel <dpatel@linkedin.com>
2014-07-18 19:25:18 -0700

    adding antlr3 grammar and helper java compiler class

---
+++ server/src/test/java/com/linkedin/pinot/pql/TestPQL.java
@@ -0,0 +1,29 @@
+package com.linkedin.pinot.pql;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.antlr.runtime.RecognitionException;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.linkedin.pinot.pql.parsers.PQLCompiler;
+
+
+public class TestPQL {
+
+  private PQLCompiler _compiler;
+
+  @Before
+  public void before() {
+    _compiler = new PQLCompiler(new HashMap<>());
+  }
+
+  @Test
+  public void simpleTest() throws RecognitionException {
+    String statement =
+        "select sum(column1) where (column2 = 'value' and column3='value3') or (column3='value3' and column4='value4') "
+            + "group by column6 top 100";
+    System.out.println(_compiler.compile(statement).toString());
+  }
+}
