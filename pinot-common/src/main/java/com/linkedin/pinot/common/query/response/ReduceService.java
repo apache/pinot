6258fb1bfd580c1ed5bb30dc29feeb0dd87cb358
Xiang Fu <xiafu@linkedin.com>
2014-08-05 15:46:43 -0700

    Adding comments to redueceService and remove dependency of pinot-core from pinot-broker

--- pinot-common/src/main/java/com/linkedin/pinot/common/query/response/ReduceService.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/query/response/ReduceService.java
@@ -6,5 +6,13 @@ import com.linkedin.pinot.common.request.BrokerRequest;
 
 
 public interface ReduceService {
+  /**
+   * Reduce method instanceResponses gathered from server instances to one brokerResponse.
+   * ServerInstance would be helpful in debug mode
+   * 
+   * @param brokerRequest
+   * @param instanceResponseMap
+   * @return BrokerResponse
+   */
   public BrokerResponse reduce(BrokerRequest brokerRequest, Map<ServerInstance, InstanceResponse> instanceResponseMap);
 }
