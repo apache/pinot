50e89f31bad2dee5f62368b9be31a1180da4ae50
kishoreg <kishoreg@apache.org>
2014-09-25 08:12:38 -0700

    PINOT-1063: Removing App*.java files that were automatically generated by maven

--- pinot-core/src/main/java/com/linkedin/pinot/core/indexsegment/utils/Helpers.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/indexsegment/utils/Helpers.java
@@ -8,6 +8,7 @@ import org.apache.commons.lang.StringUtils;
  * @author Dhaval Patel<dpatel@linkedin.com
  * Aug 19, 2014
  */
+@Deprecated //why do we need another wrapper, we should use Gauva libraries
 public class Helpers {
 
   public static class STRING {
