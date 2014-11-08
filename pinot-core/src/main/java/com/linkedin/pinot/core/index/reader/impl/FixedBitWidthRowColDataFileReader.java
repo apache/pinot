ba1936bb342cfb77ba03f5216abd4531fe0748e9
Kishore Gopalakrishna <g.kishore@gmail.com>
2014-11-08 15:27:05 -0800

    Pinot-1052 Fixing issues with fixed bit encoding for single and multi value columns

--- pinot-core/src/main/java/com/linkedin/pinot/core/index/reader/impl/FixedBitWidthRowColDataFileReader.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/index/reader/impl/FixedBitWidthRowColDataFileReader.java
@@ -5,15 +5,11 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.util.BitSet;
-import java.util.Random;
-
-import javax.management.RuntimeErrorException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.linkedin.pinot.core.indexsegment.utils.GenericRowColumnDataFileReader;
+import com.linkedin.pinot.core.util.CustomBitSet;
 
 /**
  * 
@@ -41,6 +37,8 @@ public class FixedBitWidthRowColDataFileReader {
 	private int rowSizeInBits;
 	private ByteBuffer byteBuffer;
 	private final int[] columnSizesInBits;
+
+	private CustomBitSet customBitSet;
 
 	/**
 	 * 
@@ -93,15 +91,18 @@ public class FixedBitWidthRowColDataFileReader {
 			rowSizeInBits += columnSizesInBits[i];
 		}
 		file = new RandomAccessFile(dataFile, "rw");
-		long totalSize = rowSizeInBits * rows;
+		int totalSize = rowSizeInBits * rows;
 		if (isMmap) {
 			byteBuffer = file.getChannel().map(FileChannel.MapMode.READ_ONLY,
 					0, totalSize);
 		} else {
-			byteBuffer = ByteBuffer.allocate((int) totalSize);
+			byteBuffer = ByteBuffer.allocate(totalSize);
 			file.getChannel().read(byteBuffer);
 		}
+		customBitSet = CustomBitSet
+				.withByteBuffer(totalSize, byteBuffer);
 	}
+
 	/**
 	 * 
 	 * @param fileName
@@ -111,8 +112,8 @@ public class FixedBitWidthRowColDataFileReader {
 	 *            in bytes
 	 * @throws IOException
 	 */
-	public FixedBitWidthRowColDataFileReader(ByteBuffer buffer, int rows, int cols,
-			int[] columnSizesInBits) throws IOException {
+	public FixedBitWidthRowColDataFileReader(ByteBuffer buffer, int rows,
+			int cols, int[] columnSizesInBits) throws IOException {
 		this.rows = rows;
 		this.cols = cols;
 		this.columnSizesInBits = columnSizesInBits;
@@ -123,7 +124,11 @@ public class FixedBitWidthRowColDataFileReader {
 			rowSizeInBits += columnSizesInBits[i];
 		}
 		this.byteBuffer = buffer;
+		int totalSize = rowSizeInBits * rows;
+		customBitSet = CustomBitSet
+				.withByteBuffer(totalSize, byteBuffer);
 	}
+
 	public FixedBitWidthRowColDataFileReader(String fileName, int rows,
 			int cols, int[] columnSizes) throws IOException {
 		this(new File(fileName), rows, cols, columnSizes, true);
@@ -154,45 +159,10 @@ public class FixedBitWidthRowColDataFileReader {
 	 * @return
 	 */
 	public int getInt(int row, int col) {
-
 		int startBitOffset = computeBitOffset(row, col);
-		int endBitOffset = startBitOffset + columnSizesInBits[col] - 1;
-		int startByteOffset = startBitOffset / 8;
-		int startBitPos = startBitOffset % 8;
-		int endByteOffset = (endBitOffset) / 8;
-		int endBitPos = (endBitOffset) % 8;
-		return readInt(byteBuffer, startByteOffset, startBitPos, endByteOffset,
-				endBitPos);
-	}
+		int endBitOffset = startBitOffset + columnSizesInBits[col];
+		return customBitSet.readInt(startBitOffset, endBitOffset);
 
-	/*
-	 * byte[] bytes = s.toByteArray(); then bytes.length == (s.length()+7)/8 and
-	 * s.get(n) == ((bytes[n/8] & (1<<(n%8))) != 0) for all n < 8 *
-	 * bytes.length.
-	 */
-	public static int readInt(ByteBuffer byteBuffer, int startByteOffset,
-			int startBitPos, int endByteOffset, int endBitPos) {
-		byte[] dest = new byte[endByteOffset - startByteOffset + 1];
-		byteBuffer.position(startByteOffset);
-		try {
-			byteBuffer.get(dest);
-		} catch (Exception e) {
-			System.err.println("startByteOffset" +startByteOffset );
-			System.err.println("startBitPos" + startBitPos);
-			System.err.println("endByteOffset" + endByteOffset);
-			System.err.println("endBitPos" + endBitPos);
-			throw new RuntimeException(e);
-		}
-		final BitSet set = BitSet.valueOf(dest);
-		int numBits = 8 * dest.length - (startBitPos) - (7 - endBitPos);
-		BitSet project = set.get(startBitPos, startBitPos + numBits);
-		int ret = 0;
-		for (int i = 0; i < numBits; i++) {
-			if (project.get(i)) {
-				ret |= (1 << i);
-			}
-		}
-		return ret;
 	}
 
 	public int getNumberOfRows() {
--- pinot-core/src/test/java/com/linkedin/pinot/index/writer/TestFixedBitWidthRowColDataFileWriter.java
+++ pinot-core/src/test/java/com/linkedin/pinot/index/writer/TestFixedBitWidthRowColDataFileWriter.java
@@ -2,22 +2,22 @@ package com.linkedin.pinot.index.writer;
 
 import java.io.File;
 import java.io.RandomAccessFile;
-import java.util.BitSet;
 import java.util.Random;
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import com.linkedin.pinot.core.index.writer.impl.FixedBitWidthRowColDataFileWriter;
+import com.linkedin.pinot.core.util.CustomBitSet;
 
 public class TestFixedBitWidthRowColDataFileWriter {
 
 	@Test
 	public void testSingleCol() throws Exception {
-		int[] maxBitsArray = new int[] { 5, 10, 15, 25, 31 };
-		for (int maxBits : maxBitsArray) {
-
-			File file = new File("single_col_fixed_bit_" + maxBits + ".dat");
+		int maxBits = 1;
+		while (maxBits < 32) {
+			System.out.println("START test maxBits:" + maxBits);
+			String fileName = getClass().getName()+ "_single_col_fixed_bit_" + maxBits + ".dat";
+			File file = new File(fileName);
+			file.delete();
 			int rows = 100;
 			int cols = 1;
 			int[] columnSizesInBits = new int[] { maxBits };
@@ -27,24 +27,39 @@ public class TestFixedBitWidthRowColDataFileWriter {
 			Random r = new Random();
 			writer.open();
 			int maxValue = (int) Math.pow(2, maxBits);
-			BitSet set = new BitSet(rows * cols * maxBits);
+			CustomBitSet set = CustomBitSet.withBitLength(rows * cols * maxBits);
 			for (int i = 0; i < rows; i++) {
 				data[i] = r.nextInt(maxValue);
 				writer.setInt(i, 0, data[i]);
-				for (int j = 0; j < maxBits; j++) {
-					if (((data[i] >> j) & 1) == 1) {
-						set.set(i * maxBits + j);
-					}
+				int value = data[i];
+				for (int bitPos = maxBits - 1; bitPos >= 0; bitPos--) {
+					if (( value & (1 << bitPos)) != 0) {
+						set.setBit(i * maxBits + (maxBits - bitPos - 1));
+					} 
 				}
 			}
+			System.out.println("bits expected:" + rows * cols * maxBits
+					+ " bytes expected:" + (rows * cols * maxBits + 7) / 8);
+
 			writer.saveAndClose();
 			RandomAccessFile raf = new RandomAccessFile(file, "r");
+			System.out.println("file size:" + raf.length());
 			byte[] b = new byte[(int) raf.length()];
 			raf.read(b);
 			byte[] byteArray = set.toByteArray();
+			if (byteArray.length != raf.length()) {
+				byte[] temp = set.toByteArray();
+				System.err.println("byteArray length:" + temp.length
+						+ "   from file:" + b.length);
+			}
+			System.out.println("byteArray length:" + byteArray.length
+					+ "   from file:" + b.length);
 			Assert.assertEquals(byteArray.length, b.length);
 			Assert.assertEquals(byteArray, b);
 			raf.close();
+			System.out.println("END test maxBits:" + maxBits);
+			maxBits = maxBits + 1;
+			file.delete();
 		}
 	}
 }
