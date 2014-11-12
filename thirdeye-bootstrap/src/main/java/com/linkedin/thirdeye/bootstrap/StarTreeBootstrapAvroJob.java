fe7c851fe40bd1613572eca8cb9915ad8607344b
Greg Brandt <brandt.greg@gmail.com>
2014-11-12 15:25:45 -0800

    fixed bug w/ AvroKey vs. AvroValue

--- thirdeye-bootstrap/src/main/java/com/linkedin/thirdeye/bootstrap/StarTreeBootstrapAvroJob.java
+++ thirdeye-bootstrap/src/main/java/com/linkedin/thirdeye/bootstrap/StarTreeBootstrapAvroJob.java
@@ -164,7 +164,7 @@ public class StarTreeBootstrapAvroJob extends Configured
     }
   }
 
-  public static class StarTreeBootstrapAvroReducer extends Reducer<Text, AvroKey<GenericRecord>, NullWritable, NullWritable>
+  public static class StarTreeBootstrapAvroReducer extends Reducer<Text, AvroValue<GenericRecord>, NullWritable, NullWritable>
   {
     private StarTreeConfig config;
     private int numTimeBuckets;
@@ -193,12 +193,12 @@ public class StarTreeBootstrapAvroJob extends Configured
     }
 
     @Override
-    public void reduce(Text nodeId, Iterable<AvroKey<GenericRecord>> avroRecords, Context context) throws IOException, InterruptedException
+    public void reduce(Text nodeId, Iterable<AvroValue<GenericRecord>> avroRecords, Context context) throws IOException, InterruptedException
     {
       Map<String, Map<Long, StarTreeRecord>> records = new HashMap<String, Map<Long, StarTreeRecord>>();
 
       // Aggregate records
-      for (AvroKey<GenericRecord> avroRecord : avroRecords)
+      for (AvroValue<GenericRecord> avroRecord : avroRecords)
       {
         StarTreeRecord record = toStarTreeRecord(config, avroRecord.datum());
 
