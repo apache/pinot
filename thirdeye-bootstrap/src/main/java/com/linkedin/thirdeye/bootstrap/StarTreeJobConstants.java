765b65c4f0427f17d53f4c4884c878b5fc7bedd6
Greg Brandt <brandt.greg@gmail.com>
2014-11-20 11:11:09 -0800

    Hadoop job to partition incremental batch into Avro files
    BUG=THIRDEYE-13

--- thirdeye-bootstrap/src/main/java/com/linkedin/thirdeye/bootstrap/StarTreeJobConstants.java
+++ thirdeye-bootstrap/src/main/java/com/linkedin/thirdeye/bootstrap/StarTreeJobConstants.java
@@ -7,4 +7,6 @@ public class StarTreeJobConstants
   public static final String PROP_STARTREE_ROOT = "startree.root";
   public static final String PROP_INPUT_PATHS = "input.paths";
   public static final String PROP_OUTPUT_PATH = "output.path";
+
+  public static final String AVRO_FILE_SUFFIX = ".avro";
 }
