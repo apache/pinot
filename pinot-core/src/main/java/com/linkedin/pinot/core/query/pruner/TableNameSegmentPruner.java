8e55d5c8e4ad36c3ee8f93419802aca6a065632f
Xiang Fu <xiafu@linkedin.com>
2014-12-01 15:49:39 -0800

    Fix TableNameSegmentPruner logic

--- pinot-core/src/main/java/com/linkedin/pinot/core/query/pruner/TableNameSegmentPruner.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/query/pruner/TableNameSegmentPruner.java
@@ -19,11 +19,31 @@ public class TableNameSegmentPruner implements SegmentPruner {
 
   @Override
   public boolean prune(IndexSegment segment, BrokerRequest brokerRequest) {
-    if (brokerRequest.getQuerySource() == null || brokerRequest.getQuerySource().getTableName() == null
-        || brokerRequest.getQuerySource().getTableName().equals("")
-        || brokerRequest.getQuerySource().getTableName().equals("default")) {
+    // Prune all the query without QuerySource.
+    if (brokerRequest.getQuerySource() == null) {
+      return true;
+    }
+    // Prune all the mismatched resourceName.
+    if (brokerRequest.getQuerySource().getResourceName() == null
+        || !brokerRequest.getQuerySource().getResourceName().equals(segment.getSegmentMetadata().getResourceName())) {
+      return true;
+    }
+    // For matched resourceName queries, if tableName is null or empty string, will default it as resourceName.
+    if (brokerRequest.getQuerySource().getTableName() == null
+        || brokerRequest.getQuerySource().getTableName().equals("")) {
+      if (brokerRequest.getQuerySource().getResourceName().equals(segment.getSegmentMetadata().getTableName())) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+
+    // If tableName is *, select all the segments
+    if (brokerRequest.getQuerySource().getTableName().equals("*")) {
       return false;
     }
+
+    // Get list of tableNames, and select segments within those table names.
     String[] tableNames = brokerRequest.getQuerySource().getTableName().split(",");
     for (String tableName : tableNames) {
       if (tableName.equals(segment.getSegmentMetadata().getTableName())) {
