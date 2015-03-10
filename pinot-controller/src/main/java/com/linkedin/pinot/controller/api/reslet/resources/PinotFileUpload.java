6363aca724c90ea3d002e18de15daea174f1e501
Jean-Francois Im <jfim@linkedin.com>
2015-03-09 18:16:55 -0700

    Improve upload error logging by adding a message, so that the full stack trace goes into the logs
    RB=442720
    R=kgopalak,xiafu,jfim,dpatel
    A=xiafu

--- pinot-controller/src/main/java/com/linkedin/pinot/controller/api/reslet/resources/PinotFileUpload.java
+++ pinot-controller/src/main/java/com/linkedin/pinot/controller/api/reslet/resources/PinotFileUpload.java
@@ -119,7 +119,7 @@ public class PinotFileUpload extends ServerResource {
       }
       presentation = new StringRepresentation("this is a string");
     } catch (final Exception e) {
-      logger.error(e);
+      logger.error("Caught exception while processing get request", e);
     }
     return presentation;
   }
@@ -192,14 +192,14 @@ public class PinotFileUpload extends ServerResource {
         setStatus(Status.SERVER_ERROR_INTERNAL);
       }
     } catch (final Exception e) {
-      logger.error(e);
+      logger.error("Caught exception in file upload", e);
       setStatus(Status.CLIENT_ERROR_UNPROCESSABLE_ENTITY);
     } finally {
       if ((tmpSegmentDir != null) && tmpSegmentDir.exists()) {
         try {
           FileUtils.deleteDirectory(tmpSegmentDir);
         } catch (final IOException e) {
-          logger.error(e);
+          logger.error("Caught exception in file upload", e);
         }
       }
       if ((dataFile != null) && dataFile.exists()) {
