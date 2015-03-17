003dbf2d4103ddbbdd35f0559b715b0f57be0a3a
Amit Jaspal <ajaspal@ajaspal-ld1.linkedin.biz>
2015-03-16 17:39:44 -0700

    - THIRDEYE-243 Fixing MetricTimeSeries test
    RB=446847
    R=kgopalak,gbrandt
    A=kgopalak

--- thirdeye/thirdeye-bootstrap/src/test/java/com/linkedin/thirdeye/bootstrap/MetricTimeSeriesTest.java
+++ thirdeye/thirdeye-bootstrap/src/test/java/com/linkedin/thirdeye/bootstrap/MetricTimeSeriesTest.java
@@ -3,14 +3,19 @@ package com.linkedin.thirdeye.bootstrap;
 import com.linkedin.thirdeye.api.MetricSchema;
 import com.linkedin.thirdeye.api.MetricTimeSeries;
 import com.linkedin.thirdeye.api.MetricType;
+import com.linkedin.thirdeye.impl.NumberUtils;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import com.google.common.collect.Lists;
+
 
 /**
  *
@@ -20,14 +25,12 @@ import com.google.common.collect.Lists;
 public class MetricTimeSeriesTest {
   @Test
   public void testSimple() throws Exception {
-    List<String> names = Lists.newArrayList("metric1", "metric2", "metric3",
-        "metric4", "metric5");
-    List<MetricType> types = Lists.newArrayList(MetricType.INT, MetricType.INT,
-        MetricType.INT, MetricType.INT, MetricType.INT);
+    List<String> names = Lists.newArrayList("metric1", "metric2", "metric3", "metric4", "metric5");
+    List<MetricType> types =
+        Lists.newArrayList(MetricType.INT, MetricType.INT, MetricType.INT, MetricType.INT, MetricType.INT);
     MetricSchema schema = new MetricSchema(names, types);
     MetricTimeSeries series = new MetricTimeSeries(schema);
-    long startHourSinceEpoch = TimeUnit.HOURS.convert(
-        System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+    long startHourSinceEpoch = TimeUnit.HOURS.convert(System.currentTimeMillis(), TimeUnit.MILLISECONDS);
     Random rand = new Random();
     int NUM_TIME_WINDOWS = 100;
     int[][] data = new int[NUM_TIME_WINDOWS][];
@@ -40,12 +43,19 @@ public class MetricTimeSeriesTest {
         series.set(startHourSinceEpoch + i, name, value);
       }
     }
-    System.out.println(series);
     // serialize to bytes
     byte[] bytes = series.toBytes();
     MetricTimeSeries newSeries = MetricTimeSeries.fromBytes(bytes, schema);
-    System.out.println(newSeries);
-    Assert.assertEquals(newSeries.toBytes(), bytes);
+    Set<Long> timeWindowSet = newSeries.getTimeWindowSet();
+    byte[] newBytes = newSeries.toBytes();
+    Assert.assertEquals(bytes.length, newBytes.length);
+    for (long timeWindow : timeWindowSet) {
+      for (String name : names) {
+        Number m1 = series.get(timeWindow, name);
+        Number m2 = newSeries.get(timeWindow, name);
+        Assert.assertEquals(m1, m2);
+      }
+    }
 
     MetricTimeSeries aggSeries = new MetricTimeSeries(schema);
     aggSeries.aggregate(series);
@@ -59,7 +69,7 @@ public class MetricTimeSeriesTest {
         int v2 = (Integer) newSeries.get(timeWindow, name);
         Assert.assertEquals(v1, v2);
         int v3 = (Integer) aggSeries.get(timeWindow, name);
-        Assert.assertEquals(v3, v1+v2);
+        Assert.assertEquals(v3, v1 + v2);
       }
 
     }
