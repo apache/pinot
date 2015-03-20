16cfad8c2065e3a09ff0e135ec6166f43c792f36
Joe Gillotti <jgillotti@linkedin.com>
2015-03-20 14:26:01 -0700

    Make pinot-controller /dataresources/ endpoint return application/json header mimetype
    Currently, read only endpoints return text/plain, which breaks pretty print
    tools which go based on mime type to format JSON docs nicely.
    
    RB=449758
    R=xiafu,jfim,dpatel
    A=jfim

--- pinot-controller/src/main/java/com/linkedin/pinot/controller/api/reslet/resources/PinotDataResource.java
+++ pinot-controller/src/main/java/com/linkedin/pinot/controller/api/reslet/resources/PinotDataResource.java
@@ -167,7 +167,7 @@ public class PinotDataResource extends ServerResource {
         }
         ret.put("resources", retArray);
 
-        presentation = new StringRepresentation(ret.toString());
+        presentation = new StringRepresentation(ret.toString(), MediaType.APPLICATION_JSON);
       } else {
         final JSONObject ret = new JSONObject();
         for (final String resource : manager.getAllResourceNames()) {
@@ -184,7 +184,7 @@ public class PinotDataResource extends ServerResource {
         JSONObject resourceGetRet = new JSONObject();
         resourceGetRet.put(CommonConstants.Helix.DataSource.RESOURCE_NAME, resourceName);
         resourceGetRet.put("config", resourceName);
-        presentation = new StringRepresentation(resourceGetRet.toString());
+        presentation = new StringRepresentation(resourceGetRet.toString(), MediaType.APPLICATION_JSON);
       }
     } catch (final Exception e) {
       presentation = exceptionToStringRepresentation(e);
