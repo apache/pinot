9afb776a984bba5889074cc371962814ceb77494
Jean-Francois Im <jfim@linkedin.com>
2015-03-25 11:49:58 -0700

    Fixed unit test after the FieldType case changes
    RB=451784
    R=kgopalak,xiafu,jfim,dpatel
    A=xiafu

--- pinot-core/src/test/java/com/linkedin/pinot/index/persist/TestFieldSpec.java
+++ pinot-core/src/test/java/com/linkedin/pinot/index/persist/TestFieldSpec.java
@@ -38,21 +38,21 @@ public class TestFieldSpec {
     fieldSpec.setFieldType(FieldType.DIMENSION);
     fieldSpec.setSingleValueField(true);
     fieldSpec.setDelimeter(",");
-    AssertJUnit.assertEquals("< data type : INT , field type : dimension, single value column, delimeter : , >",
+    AssertJUnit.assertEquals("< data type : INT , field type : DIMENSION, single value column, delimeter : , >",
         fieldSpec.toString());
 
     fieldSpec.setDataType(DataType.DOUBLE);
     fieldSpec.setFieldType(FieldType.METRIC);
     fieldSpec.setSingleValueField(true);
     fieldSpec.setDelimeter(":");
-    AssertJUnit.assertEquals("< data type : DOUBLE , field type : metric, single value column, delimeter : : >",
+    AssertJUnit.assertEquals("< data type : DOUBLE , field type : METRIC, single value column, delimeter : : >",
         fieldSpec.toString());
 
     fieldSpec.setDataType(DataType.STRING);
     fieldSpec.setFieldType(FieldType.DIMENSION);
     fieldSpec.setSingleValueField(false);
     fieldSpec.setDelimeter(";");
-    AssertJUnit.assertEquals("< data type : STRING , field type : dimension, multi value column, delimeter : ; >",
+    AssertJUnit.assertEquals("< data type : STRING , field type : DIMENSION, multi value column, delimeter : ; >",
         fieldSpec.toString());
   }
 
