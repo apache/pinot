f3b75c14a15c343413ad84c47135b6063bad4eac
Jean-Francois Im <jfim@linkedin.com>
2015-04-07 16:53:05 -0700

    Run queries against H2 instead of using hardcoded result values

--- pinot-integration-tests/src/test/java/com/linkedin/pinot/integration/tests/OfflineClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/com/linkedin/pinot/integration/tests/OfflineClusterIntegrationTest.java
@@ -17,6 +17,7 @@ package com.linkedin.pinot.integration.tests;
 
 import com.linkedin.pinot.common.ZkTestUtils;
 import com.linkedin.pinot.common.utils.FileUploadUtils;
+import com.linkedin.pinot.common.utils.StringUtil;
 import com.linkedin.pinot.common.utils.TarGzCompressionUtils;
 import com.linkedin.pinot.core.indexsegment.generator.SegmentGeneratorConfig;
 import com.linkedin.pinot.core.segment.creator.SegmentIndexCreationDriver;
@@ -25,6 +26,13 @@ import com.linkedin.pinot.server.util.SegmentTestUtils;
 import com.linkedin.pinot.util.TestUtils;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,6 +42,12 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.util.Utf8;
 import org.apache.commons.io.FileUtils;
 import org.apache.helix.ExternalViewChangeListener;
 import org.apache.helix.HelixManager;
@@ -58,6 +72,7 @@ public class OfflineClusterIntegrationTest extends ClusterTest {
   private final File _tmpDir = new File("/tmp/OfflineClusterIntegrationTest");
 
   private static final int SEGMENT_COUNT = 12;
+  private Connection _connection;
 
   @BeforeClass
   public void setUp() throws Exception {
@@ -79,8 +94,74 @@ public class OfflineClusterIntegrationTest extends ClusterTest {
     // Convert the Avro data to segments
     _tmpDir.mkdirs();
 
-    System.out.println("Building " + SEGMENT_COUNT + " segments in parallel");
+    // Load data into H2
     ExecutorService executor = Executors.newCachedThreadPool();
+    Class.forName("org.h2.Driver");
+    _connection = DriverManager.getConnection("jdbc:h2:mem:");
+    executor.execute(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          File avroFile = new File(_tmpDir.getPath() + "/On_Time_On_Time_Performance_2014_1.avro");
+          DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
+          DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(avroFile, datumReader);
+
+          Schema schema = dataFileReader.getSchema();
+          List<Schema.Field> fields = schema.getFields();
+          String[] columnNamesAndTypes = new String[fields.size()];
+          int index = 0;
+          for (Schema.Field field : fields) {
+            List<Schema> types = field.schema().getTypes();
+            if (types.size() == 1) {
+              columnNamesAndTypes[index] = field.name() + " " + types.get(0).getName() + " not null";
+            } else {
+              columnNamesAndTypes[index] = field.name() + " " + types.get(0).getName();
+            }
+
+            columnNamesAndTypes[index] = columnNamesAndTypes[index].replace("string", "varchar(128)");
+            ++index;
+          }
+
+          _connection.prepareCall("create table mytable(" + StringUtil.join(",", columnNamesAndTypes) + ")").execute();
+          long start = System.currentTimeMillis();
+          StringBuilder params = new StringBuilder("?");
+          for (int i = 0; i < columnNamesAndTypes.length - 1; i++) {
+            params.append(",?");
+          }
+          PreparedStatement statement = _connection
+              .prepareStatement("INSERT INTO mytable VALUES (" + params.toString() + ")");
+
+          dataFileReader.close();
+
+          for(int currentSegmentIndex = 1; currentSegmentIndex <= SEGMENT_COUNT; ++currentSegmentIndex) {
+            avroFile = new File(_tmpDir.getPath() + "/On_Time_On_Time_Performance_2014_" + currentSegmentIndex + ".avro");
+            datumReader = new GenericDatumReader<GenericRecord>();
+            dataFileReader = new DataFileReader<GenericRecord>(avroFile, datumReader);
+            GenericRecord record = null;
+            while (dataFileReader.hasNext()) {
+              record = dataFileReader.next(record);
+              for (int i = 0; i < columnNamesAndTypes.length; i++) {
+                Object value = record.get(i);
+                if (value instanceof Utf8) {
+                  value = value.toString();
+                }
+                statement.setObject(i + 1, value);
+              }
+              statement.execute();
+            }
+            dataFileReader.close();
+          }
+          System.out.println("Insertion took " + (System.currentTimeMillis() - start));
+        } catch (SQLException e) {
+          e.printStackTrace();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    });
+
+    // Create segments from Avro data
+    System.out.println("Building " + SEGMENT_COUNT + " segments in parallel");
     for(int i = 1; i <= SEGMENT_COUNT; ++i) {
       final int segmentNumber = i;
 
@@ -94,8 +175,7 @@ public class OfflineClusterIntegrationTest extends ClusterTest {
             final SegmentGeneratorConfig genConfig =
                 SegmentTestUtils
                     .getSegmentGenSpecWithSchemAndProjectedColumns(
-                        new File("/tmp/OfflineClusterIntegrationTest/On_Time_On_Time_Performance_2014_"
-                            + segmentNumber + ".avro"),
+                        new File(_tmpDir.getPath() + "/On_Time_On_Time_Performance_2014_" + segmentNumber + ".avro"),
                         outputDir,
                         "daysSinceEpoch", TimeUnit.DAYS, "myresource", "mytable");
 
@@ -165,14 +245,48 @@ public class OfflineClusterIntegrationTest extends ClusterTest {
     latch.await();
   }
 
+  private void runQuery(String pqlQuery, String sqlQuery) throws Exception {
+    Statement statement = _connection.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+    statement.execute(sqlQuery);
+    ResultSet rs = statement.getResultSet();
+
+    // Run the query
+    Map<String, String> actualValues = new HashMap<String, String>();
+    JSONObject response = postQuery(pqlQuery);
+    JSONObject aggregationResults = response.getJSONArray("aggregationResults").getJSONObject(0);
+    if (aggregationResults.has("value")) {
+      // Single value result for the aggregation, compare with the actual value
+      String value = aggregationResults.getString("value");
+
+      rs.first();
+      String sqlValue = rs.getString(1);
+      Assert.assertEquals(value, sqlValue);
+    } else if (aggregationResults.has("groupByResult")) {
+      // Grouped result, build correct values and iterate through to compare both
+      Map<String, String> correctValues = new HashMap<String, String>();
+      rs.beforeFirst();
+      while(rs.next()) {
+        correctValues.put(rs.getString(1), rs.getString(2));
+      }
+
+      // Load values from the query result
+      Assert.assertEquals(actualValues, correctValues);
+      JSONArray groupByResults = aggregationResults.getJSONArray("groupByResult");
+      for(int i = 0; i < groupByResults.length(); ++i) {
+        actualValues.put(
+            groupByResults.getJSONObject(i).getJSONArray("group").getString(0),
+            Integer.toString((int) Double.parseDouble(groupByResults.getJSONObject(i).getString("value")))
+        );
+      }
+
+      Assert.assertEquals(actualValues, correctValues);
+    }
+    System.out.println();
+  }
+
   @Test
   public void testMultipleQueries()
       throws Exception {
-    // Check that we have the right number of rows (100348)
-    JSONObject response = postQuery("select count(*) from 'myresource.mytable'");
-    System.out.println("response = " + response);
-    Assert.assertEquals("100348", response.getJSONArray("aggregationResults").getJSONObject(0).getString("value"));
-
     // Run queries from the properties file
     Properties properties = new Properties();
     properties.load(OfflineClusterIntegrationTest.class.getClassLoader().getResourceAsStream("OfflineClusterIntegrationTest.properties"));
@@ -181,33 +295,10 @@ public class OfflineClusterIntegrationTest extends ClusterTest {
 
     for (String queryName : queryNames) {
       String pql = properties.getProperty(queryName + ".pql");
-      String result = properties.getProperty(queryName + ".result");
 
       System.out.println(pql);
 
-      // Load correct values
-      Map<String, String> correctValues = new HashMap<String, String>();
-      String[] resultTuples = result.split(", ");
-      for (String resultTuple : resultTuples) {
-        int commaIndex = resultTuple.indexOf(',');
-        String key = resultTuple.substring(1, commaIndex);
-        String value = resultTuple.substring(commaIndex + 1, resultTuple.length() - 1);
-        correctValues.put(key, value);
-      }
-
-      // Run the query
-      Map<String, String> actualValues = new HashMap<String, String>();
-      response = postQuery(pql);
-      JSONArray aggregationResults = response.getJSONArray("aggregationResults").getJSONObject(0).getJSONArray("groupByResult");
-      for(int i = 0; i < aggregationResults.length(); ++i) {
-        actualValues.put(
-            aggregationResults.getJSONObject(i).getJSONArray("group").getString(0),
-            Integer.toString((int) Double.parseDouble(aggregationResults.getJSONObject(i).getString("value")))
-        );
-      }
-      System.out.println();
-
-      Assert.assertEquals(actualValues, correctValues);
+      runQuery(pql, pql.replace("'myresource.mytable'", "mytable"));
     }
   }
 
