2a09a0596686a53dd8d1da07546314f3835c0322
Dhaval Patel <dpatel@linkedin.com>
2015-04-27 13:31:45 -0700

    license headers and constructor for *ReaderWriter

--- pinot-core/src/main/java/com/linkedin/pinot/core/index/readerwriter/impl/FixedByteSingleColumnMultiValueReaderWriter.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/index/readerwriter/impl/FixedByteSingleColumnMultiValueReaderWriter.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2014-2015 LinkedIn Corp. (pinot-core@linkedin.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.linkedin.pinot.core.index.readerwriter.impl;
 
 import java.io.IOException;
@@ -5,7 +20,16 @@ import java.io.IOException;
 import com.linkedin.pinot.core.index.reader.DataFileMetadata;
 import com.linkedin.pinot.core.index.readerwriter.SingleColumnMultiValueReaderWriter;
 
-public class FixedByteSingleColumnMultiValueReaderWriter implements  SingleColumnMultiValueReaderWriter{
+
+public class FixedByteSingleColumnMultiValueReaderWriter implements SingleColumnMultiValueReaderWriter {
+
+  /**
+   * number of columns is 1, column size is variable, data type is always int
+   * @param rows
+   */
+
+  public FixedByteSingleColumnMultiValueReaderWriter(int rows) {
+  }
 
   @Override
   public int getCharArray(int row, char[] charArray) {
@@ -64,55 +88,55 @@ public class FixedByteSingleColumnMultiValueReaderWriter implements  SingleColum
   @Override
   public void close() throws IOException {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setCharArray(int row, char[] charArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setShortArray(int row, short[] shortsArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setIntArray(int row, int[] intArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setLongArray(int row, long[] longArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setFloatArray(int row, float[] floatArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setDoubleArray(int row, double[] doubleArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setStringArray(int row, String[] stringArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setBytesArray(int row, byte[][] bytesArray) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
--- pinot-core/src/main/java/com/linkedin/pinot/core/index/readerwriter/impl/FixedByteSingleColumnSingleValueReaderWriter.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/index/readerwriter/impl/FixedByteSingleColumnSingleValueReaderWriter.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2014-2015 LinkedIn Corp. (pinot-core@linkedin.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.linkedin.pinot.core.index.readerwriter.impl;
 
 import java.io.IOException;
@@ -6,9 +21,19 @@ import com.linkedin.pinot.core.index.reader.DataFileMetadata;
 import com.linkedin.pinot.core.index.reader.SingleColumnSingleValueReader;
 import com.linkedin.pinot.core.index.writer.SingleColumnSingleValueWriter;
 
-public class FixedByteSingleColumnSingleValueReaderWriter implements SingleColumnSingleValueReader, SingleColumnSingleValueWriter{
 
-  
+public class FixedByteSingleColumnSingleValueReaderWriter implements SingleColumnSingleValueReader,
+    SingleColumnSingleValueWriter {
+
+  /**
+   *
+   * @param rows
+   * @param column
+   * @param columnSize
+   */
+  public FixedByteSingleColumnSingleValueReaderWriter(int rows, int column, int[] columnSize) {
+  }
+
   @Override
   public DataFileMetadata getMetadata() {
     // TODO Auto-generated method stub
@@ -18,7 +43,7 @@ public class FixedByteSingleColumnSingleValueReaderWriter implements SingleColum
   @Override
   public void close() throws IOException {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
@@ -30,49 +55,49 @@ public class FixedByteSingleColumnSingleValueReaderWriter implements SingleColum
   @Override
   public void setChar(int row, char ch) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setInt(int row, int i) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setShort(int row, short s) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setLong(int row, int l) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setFloat(int row, float f) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setDouble(int row, double d) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setString(int row, String string) throws Exception {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void setBytes(int row, byte[] bytes) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
