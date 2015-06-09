f662b9acb5fbd15c167334d26ef2dffd8ae4cab6
Xiang Fu <xiafu@linkedin.com>
2015-06-09 12:22:28 -0700

    Fixing the wrong method call for get offline table config
    RB=497850
    R=xiafu
    A=jfim

--- pinot-server/src/main/java/com/linkedin/pinot/server/starter/helix/SegmentOnlineOfflineStateModelFactory.java
+++ pinot-server/src/main/java/com/linkedin/pinot/server/starter/helix/SegmentOnlineOfflineStateModelFactory.java
@@ -186,7 +186,7 @@ public class SegmentOnlineOfflineStateModelFactory extends StateModelFactory<Sta
             try {
               if (!isNewSegmentMetadata(segmentMetadataFromServer, segmentMetadataForCheck)) {
                 LOGGER.info("Trying to bootstrap segment from local!");
-                AbstractTableConfig tableConfig = ZKMetadataProvider.getRealtimeTableConfig(propertyStore, tableName);
+                AbstractTableConfig tableConfig = ZKMetadataProvider.getOfflineTableConfig(propertyStore, tableName);
                 INSTANCE_DATA_MANAGER.addSegment(segmentMetadataFromServer, tableConfig);
                 return;
               }
@@ -207,7 +207,7 @@ public class SegmentOnlineOfflineStateModelFactory extends StateModelFactory<Sta
           for (retryCount = 0; retryCount < SEGMENT_LOAD_MAX_RETRY_COUNT; ++retryCount) {
             long attemptStartTime = System.currentTimeMillis();
             try {
-              AbstractTableConfig tableConfig = ZKMetadataProvider.getRealtimeTableConfig(propertyStore, tableName);
+              AbstractTableConfig tableConfig = ZKMetadataProvider.getOfflineTableConfig(propertyStore, tableName);
               final String uri = offlineSegmentZKMetadata.getDownloadUrl();
               final String localSegmentDir = downloadSegmentToLocal(uri, tableName, segmentId);
               final SegmentMetadata segmentMetadata =
