2df6aa5774c5290772a89b7d37b5d27e60afc63c
Jean-Francois Im <jfim@linkedin.com>
2015-09-30 16:21:48 -0700

    Fix ClassCastException in IntermediateResultsBlock
    RB=579213
    G=pinot-dev-reviewers
    R=kgopalak,jfim,ssubrama,dpatel,mshrivas
    A=gbrandt,ssubrama

--- pinot-core/src/main/java/com/linkedin/pinot/core/query/aggregation/function/CountAggregationFunction.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/query/aggregation/function/CountAggregationFunction.java
@@ -37,7 +37,7 @@ import org.slf4j.LoggerFactory;
  * This function will take a column and do sum on that.
  *
  */
-public class CountAggregationFunction implements AggregationFunction<MutableLongValue, MutableLongValue> {
+public class CountAggregationFunction implements AggregationFunction<Number, Number> {
   private static final Logger LOGGER = LoggerFactory.getLogger(CountAggregationFunction.class);
 
   public CountAggregationFunction() {
@@ -55,19 +55,19 @@ public class CountAggregationFunction implements AggregationFunction<MutableLong
   }
 
   @Override
-  public MutableLongValue aggregate(MutableLongValue mergedResult, int docId, Block[] block) {
+  public Number aggregate(Number mergedResult, int docId, Block[] block) {
     if (mergedResult == null) {
       return new MutableLongValue(1L);
     } else {
-      mergedResult.addToValue(1L);
+      ((MutableLongValue) mergedResult).addToValue(1L);
       return mergedResult;
     }
   }
 
   @Override
-  public List<MutableLongValue> combine(List<MutableLongValue> aggregationResultList, CombineLevel combineLevel) {
+  public List<Number> combine(List<Number> aggregationResultList, CombineLevel combineLevel) {
     long combinedValue = 0;
-    for (MutableLongValue value : aggregationResultList) {
+    for (Number value : aggregationResultList) {
       combinedValue += value.longValue();
     }
     aggregationResultList.clear();
@@ -76,7 +76,7 @@ public class CountAggregationFunction implements AggregationFunction<MutableLong
   }
 
   @Override
-  public MutableLongValue combineTwoValues(MutableLongValue aggregationResult0, MutableLongValue aggregationResult1) {
+  public Number combineTwoValues(Number aggregationResult0, Number aggregationResult1) {
     if (aggregationResult0 == null) {
       return aggregationResult1;
     }
@@ -87,16 +87,16 @@ public class CountAggregationFunction implements AggregationFunction<MutableLong
   }
 
   @Override
-  public MutableLongValue reduce(List<MutableLongValue> combinedResultList) {
+  public Number reduce(List<Number> combinedResultList) {
     long reducedValue = 0;
-    for (MutableLongValue value : combinedResultList) {
+    for (Number value : combinedResultList) {
       reducedValue += value.longValue();
     }
     return new MutableLongValue(reducedValue);
   }
 
   @Override
-  public JSONObject render(MutableLongValue reduceResult) {
+  public JSONObject render(Number reduceResult) {
     try {
       if (reduceResult == null) {
         reduceResult = new MutableLongValue(0L);
