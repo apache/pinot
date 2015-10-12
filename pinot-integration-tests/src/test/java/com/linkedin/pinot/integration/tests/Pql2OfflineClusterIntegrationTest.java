09837f1ab4d25ed7ac73fe723daabf1b052b8e74
Jean-Francois Im <jfim@linkedin.com>
2015-10-12 12:53:15 -0700

    Disable PQL2 integration test, as it fails in a limited set of conditions where the old query parser would incorrectly merge query predicates due to using lexicographic comparison instead of integer comparison (eg. WHERE TotalAddGTime BETWEEN '4' AND '103' AND TotalAddGTime BETWEEN '24' AND '30' or WHERE SecurityDelay BETWEEN '10' AND '15' AND SecurityDelay < '3')
    RB=586550
    G=pinot-dev-reviewers
    R=kgopalak,jfim,ssubrama,dpatel,mshrivas
    A=ssubrama

--- pinot-integration-tests/src/test/java/com/linkedin/pinot/integration/tests/Pql2OfflineClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/com/linkedin/pinot/integration/tests/Pql2OfflineClusterIntegrationTest.java
@@ -17,18 +17,29 @@ package com.linkedin.pinot.integration.tests;
 
 import java.util.List;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 /**
  * Integration test that converts avro data for 12 segments and runs queries against it.
  */
+@Test(enabled = false) // jfim: This is disabled because the new parser exposes some bugs in the old one
 public class Pql2OfflineClusterIntegrationTest extends OfflineClusterIntegrationTest {
+  private static final Logger LOGGER = LoggerFactory.getLogger(Pql2OfflineClusterIntegrationTest.class);
+
   @Override
   protected void runQuery(String pqlQuery, List<String> sqlQueries) throws Exception {
-    JSONObject pql1Response = postQuery(pqlQuery, BROKER_BASE_API_URL, false);
-    JSONObject pql2Response = postQuery(pqlQuery, BROKER_BASE_API_URL, true);
-    Assert.assertEquals(pql2Response.getInt("numDocsScanned"), pql1Response.getInt("numDocsScanned"));
+    try {
+      JSONObject pql1Response = postQuery(pqlQuery, BROKER_BASE_API_URL, false);
+      JSONObject pql2Response = postQuery(pqlQuery, BROKER_BASE_API_URL, true);
+      Assert.assertEquals(pql2Response.getInt("numDocsScanned"), pql1Response.getInt("numDocsScanned"));
+    } catch (AssertionError assertionError) {
+      LOGGER.error("Num docs scanned did not match for query {}", pqlQuery);
+      throw assertionError;
+    }
   }
 
   @Override
