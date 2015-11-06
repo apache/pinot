b5b2d37c5236da50d1498040f1d713f41cfc4c5b
Gil Michlin <gmichlin@gmichlin-3158.local>
2015-11-05 19:01:18 -0800

    java 7 support for pinot-hadoop

--- pinot-common/src/main/java/com/linkedin/pinot/common/utils/MmapUtils.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/utils/MmapUtils.java
@@ -40,7 +40,7 @@ public class MmapUtils {
   private static final AtomicLong MMAP_BUFFER_USAGE = new AtomicLong(0L);
   private static final long BYTES_IN_MEGABYTE = 1024L * 1024L;
   private static final Map<ByteBuffer, AllocationContext> BUFFER_TO_CONTEXT_MAP =
-      Collections.synchronizedMap(new WeakIdentityHashMap<>());
+      Collections.synchronizedMap(new WeakIdentityHashMap<ByteBuffer, AllocationContext>());
 
   private enum AllocationType {
     MMAP,
