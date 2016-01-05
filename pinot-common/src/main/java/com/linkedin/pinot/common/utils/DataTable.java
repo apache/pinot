da9b31e60640502da09a3888440be330495fe301
Mayank Shrivastava <mshrivastava@linkedin.com>
2016-01-04 17:40:35 -0800

    [PINOT-2515]: Correctly handle UTF-8 encoding, fix for query comparison

--- pinot-common/src/main/java/com/linkedin/pinot/common/utils/DataTable.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/utils/DataTable.java
@@ -364,7 +364,7 @@ public class DataTable {
         int valueLength = in.readInt();
         byte[] valueBytes = new byte[valueLength];
         in.read(valueBytes);
-        map.put(new String(keyBytes), new String(valueBytes));
+        map.put(new String(keyBytes, UTF8), new String(valueBytes, UTF8));
       }
     } catch (Exception e) {
       LOGGER.error("Exception while deserializing dictionary", e);
@@ -407,14 +407,14 @@ public class DataTable {
         temp = new byte[readLength];
         in.read(temp);
         Map<Integer, String> childMap = new HashMap<Integer, String>();
-        map.put(new String(temp), childMap);
+        map.put(new String(temp, UTF8), childMap);
         int childMapSize = in.readInt();
         for (int j = 0; j < childMapSize; j++) {
           Integer key = in.readInt();
           int valueLength = in.readInt();
           temp = new byte[valueLength];
           in.read(temp);
-          childMap.put(key, new String(temp));
+          childMap.put(key, new String(temp, UTF8));
         }
       }
     } catch (Exception e) {
--- pinot-core/src/main/java/com/linkedin/pinot/core/segment/index/readers/StringDictionary.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/segment/index/readers/StringDictionary.java
@@ -59,7 +59,7 @@ public class StringDictionary extends ImmutableDictionaryReader {
     byte[] bytes = val.getBytes(UTF_8);
     for (int i = 0; i < lengthofMaxEntry; i++) {
       if (bytes[i] == STRING_PAD_CHAR) {
-        return new String(bytes, 0, i);
+        return new String(bytes, 0, i, UTF_8);
       }
     }
     return val;
