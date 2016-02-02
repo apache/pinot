ab7e0a3771b864540b7bb8b5572bb5ae3bea58d6
Mayank Shrivastava <mshrivastava@linkedin.com>
2016-02-01 20:43:44 -0800

    [PINOT-2635]: Regression fix caused by original checkin

--- pinot-controller/src/main/java/com/linkedin/pinot/controller/helix/core/retention/strategy/TimeRetentionStrategy.java
+++ pinot-controller/src/main/java/com/linkedin/pinot/controller/helix/core/retention/strategy/TimeRetentionStrategy.java
@@ -66,16 +66,16 @@ public class TimeRetentionStrategy implements RetentionStrategy {
         return false;
       }
 
-      long endsMills = segmentTimeUnit.toMillis(segmentZKMetadata.getEndTime());
+      long endsMillis = segmentTimeUnit.toMillis(segmentZKMetadata.getEndTime());
 
       // Check that the date in the segment is between 1971 and 2071, as a sanity check for misconfigured time units.
-      if (!TimeUtils.timeValueInValidRange(segmentZKMetadata.getEndTime())) {
+      if (!TimeUtils.timeValueInValidRange(endsMillis)) {
         LOGGER.warn("Skipping purge check for segment {}, timestamp {} {} fails sanity check.",
             segmentZKMetadata.getSegmentName(), segmentZKMetadata.getEndTime(), segmentZKMetadata.getTimeUnit());
         return false;
       }
 
-      Duration segmentTimeUntilNow = new Duration(endsMills, System.currentTimeMillis());
+      Duration segmentTimeUntilNow = new Duration(endsMillis, System.currentTimeMillis());
       if (_retentionDuration.isShorterThan(segmentTimeUntilNow)) {
         return true;
       }
