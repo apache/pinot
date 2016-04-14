a883084a809a397922d8a3d661e32c3b0abdcf5e
Neha Pawar <npawar@linkedin.com>
2016-04-14 15:26:14 -0700

    RAP-8150 : Create topk job in thirdeye-hadoop library

--- thirdeye/thirdeye-hadoop/src/main/java/com/linkedin/thirdeye/hadoop/topk/TopKPhaseMapOutputValue.java
+++ thirdeye/thirdeye-hadoop/src/main/java/com/linkedin/thirdeye/hadoop/topk/TopKPhaseMapOutputValue.java
@@ -13,15 +13,10 @@ import com.linkedin.thirdeye.api.MetricType;
 
 public class TopKPhaseMapOutputValue {
 
-  //String dimensionName;
-  //String dimensionValue;
   Number[] metricValues;
   List<MetricType> metricTypes;
 
-  public TopKPhaseMapOutputValue(//String dimensionName, String dimensionValue,
-      Number[] metricValues, List<MetricType> metricTypes) {
-    //this.dimensionName = dimensionName;
-    //this.dimensionValue = dimensionValue;
+  public TopKPhaseMapOutputValue(Number[] metricValues, List<MetricType> metricTypes) {
     this.metricValues = metricValues;
     this.metricTypes = metricTypes;
   }
@@ -30,30 +25,10 @@ public class TopKPhaseMapOutputValue {
     return metricValues;
   }
 
-  /*public String getDimensionName() {
-    return dimensionName;
-  }
-
-  public String getDimensionValue() {
-    return dimensionValue;
-  }*/
-
   public byte[] toBytes() throws IOException {
 
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
     DataOutputStream dos = new DataOutputStream(baos);
-
-    byte[] bytes;
-
-    /*// dimension name
-    bytes = dimensionName.getBytes();
-    dos.writeInt(bytes.length);
-    dos.write(bytes);
-
-    // dimension value
-    bytes = dimensionValue.getBytes();
-    dos.writeInt(bytes.length);
-    dos.write(bytes);*/
 
     // metric values
     dos.writeInt(metricValues.length);
@@ -88,19 +63,6 @@ public class TopKPhaseMapOutputValue {
     DataInputStream dis = new DataInputStream(new ByteArrayInputStream(buffer));
     int length;
 
-    /*byte[] bytes;
-    // dimension name
-    length = dis.readInt();
-    bytes = new byte[length];
-    dis.read(bytes);
-    String dimensionName = new String(bytes);
-
-    // dimension value
-    length = dis.readInt();
-    bytes = new byte[length];
-    dis.read(bytes);
-    String dimensionValue = new String(bytes);*/
-
     // metric values
     length = dis.readInt();
     Number[] metricValues = new Number[length];
@@ -127,32 +89,8 @@ public class TopKPhaseMapOutputValue {
     }
 
     TopKPhaseMapOutputValue wrapper;
-    wrapper = new TopKPhaseMapOutputValue(
-        //dimensionName, dimensionValue,
-        metricValues, metricTypes);
+    wrapper = new TopKPhaseMapOutputValue(metricValues, metricTypes);
     return wrapper;
-  }
-
-  public static void main(String[] args) throws IOException {
-    Number[] metricValues = new Number[4];
-    List<MetricType> metricTypes = new ArrayList<>();
-    int a = 10;
-    double b = 4.5;
-    long c = 6721234567543L;
-    float d = 22.7F;
-    metricValues[0] = a; metricTypes.add(MetricType.INT);
-    metricValues[1] = b; metricTypes.add(MetricType.DOUBLE);
-    metricValues[2] = c; metricTypes.add(MetricType.LONG);
-    metricValues[3] = d; metricTypes.add(MetricType.FLOAT);
-
-    TopKPhaseMapOutputValue valueWrapper = new TopKPhaseMapOutputValue
-        (//"d1", "v1",
-            metricValues, metricTypes);
-    byte[] bytes = valueWrapper.toBytes();
-    TopKPhaseMapOutputValue output = TopKPhaseMapOutputValue.fromBytes(bytes, metricTypes);
-    for (Number n : metricValues) {
-      System.out.println(n);
-    }
   }
 
 }
