34b2a16ceedbb50adbe12876ee2be1df1d0e74fb
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2016-06-07 16:17:55 -0700

    Fix Travis script so that if the commit range is invalid, exit with error state.

--- .travis_test.sh
+++ .travis_test.sh
@@ -16,15 +16,11 @@
 #
 
 # Ignore changes not related to pinot code
-echo 'Changed files:'
 git diff --name-only $TRAVIS_COMMIT_RANGE | egrep '^(pinot-|pom.xml|.travis)'
 if [ $? -ne 0 ]; then
   echo 'No changes related to the pinot code, skip the test.'
   exit 0
 fi
-
-# Abort on error
-set -e
 
 cd $PINOT_MODULE
 mvn test
