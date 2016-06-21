0d5ef42c0931ea391520ff050c042d2ff1f89055
Jia (Jason) Teoh <jteoh@linkedin.com>
2016-06-21 10:25:51 -0700

    TE: fix test case to reflect [start,end) logic for PqlUtils

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/client/pinot/PqlUtilsTest.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/client/pinot/PqlUtilsTest.java
@@ -28,23 +28,21 @@ public class PqlUtilsTest {
             "timeColumn", 2, TimeUnit.HOURS, null, " timeColumn = 201612"),
         // "" with date range
         getBetweenClauseTestArgs("2016-01-01T00:00:00.000+00:00", "2016-01-02T00:00:00.000+00:00",
-            "timeColumn", 2, TimeUnit.HOURS, null,
-            " timeColumn >= 201612 AND timeColumn <= 201624"),
+            "timeColumn", 2, TimeUnit.HOURS, null, " timeColumn >= 201612 AND timeColumn < 201624"),
         // equal start+end, with format
         getBetweenClauseTestArgs("2016-01-01T00:00:00.000+00:00", "2016-01-01T00:00:00.000+00:00",
             "timeColumn", 1, TimeUnit.DAYS, "yyyyMMdd", " timeColumn = 20160101"),
         // "" with date range
         getBetweenClauseTestArgs("2016-01-01T00:00:00.000+00:00", "2016-01-02T00:00:00.000+00:00",
             "timeColumn", 1, TimeUnit.DAYS, "yyyyMMdd",
-            " timeColumn >= 20160101 AND timeColumn <= 20160102"),
+            " timeColumn >= 20160101 AND timeColumn < 20160102"),
         // Incorrectly aligned date ranges, no format
         getBetweenClauseTestArgs("2016-01-01T01:00:00.000+00:00", "2016-01-01T23:00:00.000+00:00",
-            "timeColumn", 2, TimeUnit.HOURS, null,
-            " timeColumn >= 201613 AND timeColumn <= 201623"),
+            "timeColumn", 2, TimeUnit.HOURS, null, " timeColumn >= 201613 AND timeColumn < 201623"),
         // Incorrectly aligned date ranges, with format
         getBetweenClauseTestArgs("2016-01-01T01:00:00.000+00:00", "2016-01-01T23:00:00.000+00:00",
             "timeColumn", 2, TimeUnit.HOURS, "yyyyMMddHH",
-            " timeColumn >= 2016010102 AND timeColumn <= 2016010122")
+            " timeColumn >= 2016010102 AND timeColumn < 2016010122")
     };
   }
 
