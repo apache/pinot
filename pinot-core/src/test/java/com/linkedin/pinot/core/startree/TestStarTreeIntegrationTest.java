43d25527e0412ba8164c6e4c53fb69a79f91843a
Mayank Shrivastava <mshrivastava@linkedin.com>
2016-06-28 13:46:01 -0700

    Enhanced StarTreeIntegrationTest to test group-by results. Added more query of different types to increase test coverage.

--- pinot-core/src/test/java/com/linkedin/pinot/core/startree/TestStarTreeIntegrationTest.java
+++ pinot-core/src/test/java/com/linkedin/pinot/core/startree/TestStarTreeIntegrationTest.java
@@ -22,6 +22,9 @@ import com.linkedin.pinot.common.data.Schema;
 import com.linkedin.pinot.common.data.TimeFieldSpec;
 import com.linkedin.pinot.common.request.BrokerRequest;
 import com.linkedin.pinot.common.segment.ReadMode;
+import com.linkedin.pinot.common.segment.SegmentMetadata;
+import com.linkedin.pinot.common.utils.request.FilterQueryTree;
+import com.linkedin.pinot.common.utils.request.RequestUtils;
 import com.linkedin.pinot.core.common.BlockDocIdIterator;
 import com.linkedin.pinot.core.common.BlockSingleValIterator;
 import com.linkedin.pinot.core.common.Constants;
@@ -41,9 +44,11 @@ import com.linkedin.pinot.pql.parsers.Pql2Compiler;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.mutable.MutableLong;
@@ -68,6 +73,8 @@ public class TestStarTreeIntegrationTest {
   private static final String TIME_COLUMN_NAME = "daysSinceEpoch";
   private static final int NUM_DIMENSIONS = 4;
   private static final int NUM_METRICS = 2;
+  private static final int METRIC_MAX_VALUE = 10000;
+  private final long _randomSeed = System.nanoTime();
 
   private IndexSegment _segment;
   private Schema _schema;
@@ -93,41 +100,70 @@ public class TestStarTreeIntegrationTest {
    */
   @Test
   public void testHardCodedQueries() throws Exception {
-    String[] queries = new String[] {
+    String[] queries = new String[]{
         "select sum(m1) from T",
-        "select sum(m1) from T where d1 = 'd1-v2' group by d2"
+        "select sum(m1) from T where d1 = 'd1-v1'",
+        "select sum(m1) from T where d1 <> 'd1-v1'",
+        "select sum(m1) from T where d1 between 'd1-v1' and 'd1-v3'",
+        "select sum(m1) from T where d1 in ('d1-v1', 'd1-v2')",
+        "select sum(m1) from T where d1 in ('d1-v1', 'd1-v2') and d2 not in ('d2-v1')",
+        "select sum(m1) from T group by d1",
+        "select sum(m1) from T group by d1, d2",
+        "select sum(m1) from T where d1 = 'd1-v2' group by d1",
+        "select sum(m1) from T where d1 between 'd1-v1' and 'd1-v3' group by d2",
+        "select sum(m1) from T where d1 = 'd1-v2' group by d2, d3",
+        "select sum(m1) from T where d1 <> 'd1-v1' group by d2",
+        "select sum(m1) from T where d1 in ('d1-v1', 'd1-v2') group by d2",
+        "select sum(m1) from T where d1 in ('d1-v1', 'd1-v2') and d2 not in ('d2-v1') group by d3",
+        "select sum(m1) from T where d1 in ('d1-v1', 'd1-v2') and d2 not in ('d2-v1') group by d3, d4"
     };
 
     // Test against all metric columns, instead of just the aggregation column in the query.
     List<String> metricNames = _schema.getMetricNames();
+    SegmentMetadata segmentMetadata = _segment.getSegmentMetadata();
 
     for (int i = 0; i < queries.length; i++) {
       Pql2Compiler compiler = new Pql2Compiler();
       BrokerRequest brokerRequest = compiler.compileToBrokerRequest(queries[i]);
 
-      double[] expectedSums = computeSumUsingRawDocs(metricNames, brokerRequest);
-      double[] actualSums = computeSumUsingAggregatedDocs(metricNames, brokerRequest);
+      FilterQueryTree filterQueryTree = RequestUtils.generateFilterQueryTree(brokerRequest);
+      Assert.assertTrue(RequestUtils.isFitForStarTreeIndex(segmentMetadata, filterQueryTree, brokerRequest));
 
-      Assert.assertEquals(expectedSums.length, actualSums.length, "Mis-match in number of metrics");
-      for (int j = 0; j < expectedSums.length; j++) {
-        Assert.assertEquals(expectedSums[j], actualSums[j],
-            "Mis-match result for metric: " + metricNames.get(j) + " Acutal: " + actualSums[j] + " Expected: "
-                + expectedSums[j]);
+      Map<String, double[]> expectedResult = computeSumUsingRawDocs(metricNames, brokerRequest);
+      Map<String, double[]> actualResult = computeSumUsingAggregatedDocs(metricNames, brokerRequest);
+
+      Assert.assertEquals(expectedResult.size(), actualResult.size(), "Mis-match in number of groups");
+      for (Map.Entry<String, double[]> entry : expectedResult.entrySet()) {
+        String expectedKey = entry.getKey();
+        Assert.assertTrue(actualResult.containsKey(expectedKey));
+
+        double[] expectedSums = entry.getValue();
+        double[] actualSums = actualResult.get(expectedKey);
+
+        for (int j = 0; j < expectedSums.length; j++) {
+          Assert.assertEquals(actualSums[j], expectedSums[j],
+              "Mis-match sum for key '" + expectedKey + "', Metric: " + metricNames.get(j) + ", Random Seed: "
+                  + _randomSeed);
+        }
       }
     }
   }
 
   /**
    * Helper method to compute the sums using raw index.
-   *
-   * @param metricNames
+   *  @param metricNames
    * @param brokerRequest
    */
-  private double[] computeSumUsingRawDocs(List<String> metricNames, BrokerRequest brokerRequest) {
+  private Map<String, double[]> computeSumUsingRawDocs(List<String> metricNames, BrokerRequest brokerRequest) {
     FilterPlanNode planNode = new FilterPlanNode(_segment, brokerRequest);
     Operator rawOperator = planNode.run();
     BlockDocIdIterator rawDocIdIterator = rawOperator.nextBlock().getBlockDocIdSet().iterator();
-    return computeSum(_segment, rawDocIdIterator, metricNames);
+
+    List<String> groupByColumns = Collections.EMPTY_LIST;
+    if (brokerRequest.isSetAggregationsInfo() && brokerRequest.isSetGroupBy()) {
+      groupByColumns = brokerRequest.getGroupBy().getColumns();
+    }
+    return computeSum(_segment, rawDocIdIterator, metricNames, groupByColumns);
   }
 
   /**
@@ -136,11 +172,17 @@ public class TestStarTreeIntegrationTest {
    * @param brokerRequest
    * @return
    */
-  private double[] computeSumUsingAggregatedDocs(List<String> metricNames, BrokerRequest brokerRequest) {
+  private Map<String, double[]> computeSumUsingAggregatedDocs(List<String> metricNames, BrokerRequest brokerRequest) {
     StarTreeIndexOperator starTreeOperator = new StarTreeIndexOperator(_segment, brokerRequest);
     starTreeOperator.open();
     BlockDocIdIterator starTreeDocIdIterator = starTreeOperator.nextBlock().getBlockDocIdSet().iterator();
-    return computeSum(_segment, starTreeDocIdIterator, metricNames);
+
+    List<String> groupByColumns = Collections.EMPTY_LIST;
+    if (brokerRequest.isSetAggregationsInfo() && brokerRequest.isSetGroupBy()) {
+      groupByColumns = brokerRequest.getGroupBy().getColumns();
+    }
+
+    return computeSum(_segment, starTreeDocIdIterator, metricNames, groupByColumns);
   }
 
   /**
@@ -182,12 +224,14 @@ public class TestStarTreeIntegrationTest {
         map.put(dimName, dimName + "-v" + row % (NUM_DIMENSIONS - i));
       }
 
-      // Time column.
-      map.put("daysSinceEpoch", row % 7);
+      Random random = new Random(_randomSeed);
       for (int i = 0; i < NUM_METRICS; i++) {
         String metName = _schema.getMetricFieldSpecs().get(i).getName();
-        map.put(metName, 1);
+        map.put(metName, random.nextInt(METRIC_MAX_VALUE));
       }
+
+      // Time column.
+      map.put("daysSinceEpoch", row % 7);
 
       GenericRow genericRow = new GenericRow();
       genericRow.init(map);
@@ -222,29 +266,56 @@ public class TestStarTreeIntegrationTest {
    * @param metricNames
    * @return
    */
-  private double[] computeSum(IndexSegment segment, BlockDocIdIterator docIdIterator, List<String> metricNames) {
+  private Map<String, double[]> computeSum(IndexSegment segment, BlockDocIdIterator docIdIterator,
+      List<String> metricNames, List<String> groupByColumns) {
     int docId;
     int numMetrics = metricNames.size();
-    Dictionary[] dictionaries = new Dictionary[numMetrics];
-    BlockSingleValIterator[] valIterators = new BlockSingleValIterator[numMetrics];
+    Dictionary[] metricDictionaries = new Dictionary[numMetrics];
+    BlockSingleValIterator[] metricValIterators = new BlockSingleValIterator[numMetrics];
+
+    int numGroupByColumns = groupByColumns.size();
+    Dictionary[] groupByDictionaries = new Dictionary[numGroupByColumns];
+    BlockSingleValIterator[] groupByValIterators = new BlockSingleValIterator[numGroupByColumns];
 
     for (int i = 0; i < numMetrics; i++) {
       String metricName = metricNames.get(i);
       DataSource dataSource = segment.getDataSource(metricName);
-      dictionaries[i] = dataSource.getDictionary();
-      valIterators[i] = (BlockSingleValIterator) dataSource.getNextBlock().getBlockValueSet().iterator();
+      metricDictionaries[i] = dataSource.getDictionary();
+      metricValIterators[i] = (BlockSingleValIterator) dataSource.getNextBlock().getBlockValueSet().iterator();
     }
 
-    double[] sums = new double[numMetrics];
+    for (int i = 0; i < numGroupByColumns; i++) {
+      String groupByColumn = groupByColumns.get(i);
+      DataSource dataSource = segment.getDataSource(groupByColumn);
+      groupByDictionaries[i] = dataSource.getDictionary();
+      groupByValIterators[i] = (BlockSingleValIterator) dataSource.getNextBlock().getBlockValueSet().iterator();
+    }
+
+    Map<String, double[]> result = new HashMap<String, double[]>();
     while ((docId = docIdIterator.next()) != Constants.EOF) {
+
+      StringBuilder stringBuilder = new StringBuilder();
+      for (int i = 0; i < numGroupByColumns; i++) {
+        groupByValIterators[i].skipTo(docId);
+        int dictId = groupByValIterators[i].nextIntVal();
+        stringBuilder.append(groupByDictionaries[i].getStringValue(dictId));
+        stringBuilder.append("_");
+      }
+
+      String key = stringBuilder.toString();
+      if (!result.containsKey(key)) {
+        result.put(key, new double[numMetrics]);
+      }
+
+      double[] sumsSoFar = result.get(key);
       for (int i = 0; i < numMetrics; i++) {
-        valIterators[i].skipTo(docId);
-        int dictId = valIterators[i].nextIntVal();
-        sums[i] += dictionaries[i].getDoubleValue(dictId);
+        metricValIterators[i].skipTo(docId);
+        int dictId = metricValIterators[i].nextIntVal();
+        sumsSoFar[i] += metricDictionaries[i].getDoubleValue(dictId);
       }
     }
 
-    return sums;
+    return result;
   }
 
   private RecordReader createReader(final Schema schema, final List<GenericRow> data) {
