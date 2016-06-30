43ceb49deac71a8956184a43d179fa269b89c48d
kishoreg <kishoreg@apache.org>
2016-06-30 11:33:50 -0700

    Fixing issues in task driver

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/TaskDriver.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/TaskDriver.java
@@ -46,7 +46,7 @@ public class TaskDriver {
         @Override
         public Void call() throws Exception {
           while (!shutdown) {
-            LOG.info("Finding next task to execute");
+            LOG.info("Finding next task to execute for threadId:", Thread.currentThread().getId());
             AnomalyTaskSpec anomalyTaskSpec = selectAndUpdate();
             LOG.info("Executing task: {}", anomalyTaskSpec.getTaskId());
             TaskRunner taskRunner = new TaskRunner();
@@ -58,7 +58,9 @@ public class TaskDriver {
       };
       callables.add(callable);
     }
-    taskExecutorService.invokeAll(callables);
+    for (Callable<Void> callable : callables) {
+      taskExecutorService.submit(callable);
+    }
     System.out.println("Started task driver");
   }
 
@@ -78,8 +80,11 @@ public class TaskDriver {
         do {
           List<AnomalyTaskSpec> anomalyTasks =
               anomalyTaskSpecDAO.findByStatusOrderByCreateTimeAscending(JobStatus.WAITING);
+          LOG.info("Found {} tasks in waiting state", anomalyTasks.size());
           for (AnomalyTaskSpec anomalyTaskSpec : anomalyTasks) {
+            LOG.info("Trying to acquire task : {}", anomalyTaskSpec.getTaskId());
             boolean success = anomalyTaskSpecDAO.updateStatus(anomalyTaskSpec.getTaskId());
+            LOG.info("Task acquired success: {}", success);
             if (success) {
               acquiredTask = anomalyTaskSpec;
               if (!transaction.wasCommitted()) {
