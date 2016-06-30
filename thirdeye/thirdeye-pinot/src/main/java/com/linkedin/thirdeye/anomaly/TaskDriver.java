323eb291c859dc539336180afc4414a02f4aede2
kishoreg <kishoreg@apache.org>
2016-06-30 12:55:39 -0700

    Fixes for task driver

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/TaskDriver.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/TaskDriver.java
@@ -15,11 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.linkedin.thirdeye.anomaly.JobRunner.JobStatus;
-import com.linkedin.thirdeye.detector.api.AnomalyResult;
 import com.linkedin.thirdeye.detector.api.AnomalyTaskSpec;
 import com.linkedin.thirdeye.detector.db.AnomalyTaskSpecDAO;
-
-import sun.nio.ch.sctp.Shutdown;
 
 public class TaskDriver {
 
@@ -46,7 +43,8 @@ public class TaskDriver {
         @Override
         public Void call() throws Exception {
           while (!shutdown) {
-            LOG.info("Finding next task to execute for threadId:", Thread.currentThread().getId());
+            LOG.info("Finding next task to execute for threadId:{}",
+                Thread.currentThread().getId());
             AnomalyTaskSpec anomalyTaskSpec = selectAndUpdate();
             LOG.info("Executing task: {}", anomalyTaskSpec.getTaskId());
             TaskRunner taskRunner = new TaskRunner();
@@ -68,42 +66,44 @@ public class TaskDriver {
     taskExecutorService.shutdown();
   }
 
-  private AnomalyTaskSpec selectAndUpdate() {
-    LOG.info("Starting selectAndUpdate");
-    Session session = sessionFactory.openSession();
+  private AnomalyTaskSpec selectAndUpdate() throws Exception {
+    LOG.info("Starting selectAndUpdate {}", Thread.currentThread().getId());
     AnomalyTaskSpec acquiredTask = null;
-    try {
+    LOG.info("Trying to find a task to execute");
+    do {
+      Session session = sessionFactory.openSession();
       ManagedSessionContext.bind(session);
-      Transaction transaction = session.beginTransaction();
+      Transaction transaction = null;
       try {
-        LOG.info("Trying to find a task to execute");
-        do {
-          List<AnomalyTaskSpec> anomalyTasks =
-              anomalyTaskSpecDAO.findByStatusOrderByCreateTimeAscending(JobStatus.WAITING);
-          LOG.info("Found {} tasks in waiting state", anomalyTasks.size());
-          for (AnomalyTaskSpec anomalyTaskSpec : anomalyTasks) {
-            LOG.info("Trying to acquire task : {}", anomalyTaskSpec.getTaskId());
-            boolean success = anomalyTaskSpecDAO.updateStatus(anomalyTaskSpec.getTaskId());
-            LOG.info("Task acquired success: {}", success);
-            if (success) {
-              acquiredTask = anomalyTaskSpec;
-              if (!transaction.wasCommitted()) {
-                transaction.commit();
-              }
-              break;
+
+        List<AnomalyTaskSpec> anomalyTasks =
+            anomalyTaskSpecDAO.findByStatusOrderByCreateTimeAscending(JobStatus.WAITING);
+        LOG.info("Found {} tasks in waiting state", anomalyTasks.size());
+        for (AnomalyTaskSpec anomalyTaskSpec : anomalyTasks) {
+          transaction = session.beginTransaction();
+          LOG.info("Trying to acquire task : {}", anomalyTaskSpec.getTaskId());
+          boolean success = anomalyTaskSpecDAO.updateStatus(anomalyTaskSpec.getTaskId());
+          LOG.info("Task acquired success: {}", success);
+          if (success) {
+            acquiredTask = anomalyTaskSpec;
+            if (!transaction.wasCommitted()) {
+              transaction.commit();
             }
+            break;
           }
-          Thread.sleep(1000);
-        } while (acquiredTask == null);
-        LOG.info("Acquired task ======" + acquiredTask);
+        }
+        Thread.sleep(1000);
       } catch (Exception e) {
-        transaction.rollback();
-        throw new RuntimeException(e);
+        if (transaction != null) {
+          transaction.rollback();
+        }
+      } finally {
+        session.close();
+        ManagedSessionContext.unbind(sessionFactory);
       }
-    } finally {
-      session.close();
-      ManagedSessionContext.unbind(sessionFactory);
-    }
+    } while (acquiredTask == null);
+    LOG.info("Acquired task ======" + acquiredTask);
+
     return acquiredTask;
   }
 
