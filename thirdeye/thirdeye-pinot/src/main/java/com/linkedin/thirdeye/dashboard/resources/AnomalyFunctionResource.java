adc46065bc4546c5ad26434f932f64804bbf9ed1
Puneet Jaiswal <punit.kj@gmail.com>
2016-07-19 21:32:45 -0700

    thirdeye build fix

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/AnomalyFunctionResource.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/AnomalyFunctionResource.java
@@ -19,6 +19,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Path("dashboard")
 @Produces(MediaType.APPLICATION_JSON)
 public class AnomalyFunctionResource {
 
