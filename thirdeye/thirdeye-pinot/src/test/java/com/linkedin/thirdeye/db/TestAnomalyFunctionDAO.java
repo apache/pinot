03e716b00d3042695a03ab3f9d6e742acc2cc427
Puneet Jaiswal <punit.kj@gmail.com>
2016-07-26 13:46:46 -0700

    thirdeye hql fix (#318)
    * thirdeye hql fix
    
    * adding more tests

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/db/TestAnomalyFunctionDAO.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/db/TestAnomalyFunctionDAO.java
@@ -17,6 +17,9 @@ public class TestAnomalyFunctionDAO extends AbstractDbTestBase {
   public void testCreate() {
     anomalyFunctionId = anomalyFunctionDAO.save(getTestFunctionSpec());
     Assert.assertNotNull(anomalyFunctionId);
+    // test fetch all
+    List<AnomalyFunctionSpec> functions = anomalyFunctionDAO.findAll();
+    Assert.assertEquals(functions.size(), 1);
   }
 
   @Test(dependsOnMethods = {"testCreate"})
