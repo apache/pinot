d34e0ae0c886387d73e7a5c6d2b722d3d84b0d32
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2016-08-02 19:01:15 -0700

    Fix EqualityUtils to correctly compare Collections.
    Changes:
    1. For Collections, should not compare the result of toString(), this will fail all collections without order
    2. For all Objects (non primitive type), just use equals()

--- pinot-common/src/main/java/com/linkedin/pinot/common/utils/EqualityUtils.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/utils/EqualityUtils.java
@@ -17,15 +17,17 @@ package com.linkedin.pinot.common.utils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 
 /**
- * Various utilities to be used in implementing equals and hashCode.
+ * Various utilities in implementing {@link Object#equals(Object)} and {@link Object#hashCode()}.
  *
+ * For primitive float and double, {@code isEqual()} is not the same as Java == operator, {@code isEqual(NaN, NaN)}
+ * returns true instead of false.
  */
 public class EqualityUtils {
   private EqualityUtils() {
@@ -39,12 +41,20 @@ public class EqualityUtils {
     return left == right;
   }
 
+  /**
+   * Compare both arguments for equality, and consider {@code Float.NaN} to be equal to {@code Float.NaN} (unlike the
+   * Java == operator).
+   */
   public static boolean isEqual(float left, float right) {
-    return Float.compare(left, right) == 0;
+    return Float.floatToIntBits(left) == Float.floatToIntBits(right);
   }
 
+  /**
+   * Compare both arguments for equality, and consider {@code Double.NaN} to be equal to {@code Double.NaN} (unlike the
+   * Java == operator).
+   */
   public static boolean isEqual(double left, double right) {
-    return Double.compare(left, right) == 0;
+    return Double.doubleToLongBits(left) == Double.doubleToLongBits(right);
   }
 
   public static boolean isEqual(short left, short right) {
@@ -59,83 +69,59 @@ public class EqualityUtils {
     return left == right;
   }
 
-  public static boolean isEqual(Object[] left, Object[] right) {
+  public static boolean isEqual(@Nullable Object left, @Nullable Object right) {
+    if (left != null && right != null) {
+      return left.equals(right);
+    } else {
+      return left == right;
+    }
+  }
+
+  public static boolean isEqual(@Nullable Object[] left, @Nullable Object[] right) {
     return Arrays.deepEquals(left, right);
   }
 
-  public static boolean isEqual(Collection left, Collection right) {
+  @SuppressWarnings("unchecked")
+  public static boolean isEqualIgnoreOrder(@Nullable List left, @Nullable List right) {
     if (left != null && right != null) {
-      return left.toString().equals(right.toString());
-    } else {
-      return left == right;
-    }
-  }
-
-  public static boolean isEqualIgnoringOrder(List left, List right) {
-    if (left != null && right != null) {
-      ArrayList sortedLeft = new ArrayList(left);
-      ArrayList sortedRight = new ArrayList(right);
+      List sortedLeft = new ArrayList(left);
+      List sortedRight = new ArrayList(right);
       Collections.sort(sortedLeft);
       Collections.sort(sortedRight);
-      return sortedLeft.toString().equals(sortedRight.toString());
+      return sortedLeft.equals(sortedRight);
     } else {
       return left == right;
     }
   }
 
-  public static boolean isEqual(Map left, Map right) {
-    if (left != null && right != null) {
-      if (left.size() != right.size()) {
-        return false;
-      }
-      for (Object key : left.keySet()) {
-        if (right.containsKey(key)) {
-          Object leftValue = left.get(key);
-          Object rightValue = right.get(key);
-          if (!isEqual(leftValue, rightValue)) {
-            return false;
-          }
-        } else {
-          return false;
-        }
-      }
-      return true;
-    } else {
-      return left == right;
-    }
-  }
-
-  public static boolean isEqual(Object left, Object right) {
-    if (left != null)
-      return left.equals(right);
-    else
-      return null == right;
-  }
-
-  public static boolean isNullOrNotSameClass(Object left, Object right) {
+  public static boolean isNullOrNotSameClass(@Nonnull Object left, @Nullable Object right) {
     return right == null || left.getClass() != right.getClass();
   }
 
-  public static boolean isSameReference(Object left, Object right) {
+  public static boolean isSameReference(@Nullable Object left, @Nullable Object right) {
     return left == right;
   }
 
-  public static int hashCodeOf(Object o) {
-    if (o != null)
+  /**
+   * Given an object, return the hashcode of it. For {@code null}, return 0 instead.
+   */
+  public static int hashCodeOf(@Nullable Object o) {
+    if (o != null) {
       return o.hashCode();
-    else
+    } else {
       return 0;
+    }
   }
 
-  public static int hashCodeOf(int previousHashCode, Object o) {
-    return 31 * previousHashCode + hashCodeOf(o);
+  public static int hashCodeOf(int previousHashCode, @Nullable Object o) {
+    return 37 * previousHashCode + hashCodeOf(o);
   }
 
   public static int hashCodeOf(int previousHashCode, int value) {
-    return 31 * previousHashCode + value;
+    return 37 * previousHashCode + value;
   }
 
   public static int hashCodeOf(int previousHashCode, long value) {
-    return 31 * previousHashCode + (int) (value ^ (value >>> 32));
+    return 37 * previousHashCode + (int) (value ^ (value >>> 32));
   }
 }
