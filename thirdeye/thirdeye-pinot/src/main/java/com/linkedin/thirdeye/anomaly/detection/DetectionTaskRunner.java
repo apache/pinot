7bf5b13c8dfd87725eec154ea1c15f4290c86a6a
Neha Pawar <npawar@linkedin.com>
2016-09-29 15:01:33 -0700

    Adding fix for anomaly generation getting skipped in case of 1 granularity bucket

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/detection/DetectionTaskRunner.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/detection/DetectionTaskRunner.java
@@ -86,10 +86,7 @@ public class DetectionTaskRunner implements TaskRunner {
     Map<DimensionKey, MetricTimeSeries> res =
         timeSeriesResponseConverter.toMap(finalResponse, collectionDimensions);
     for (Map.Entry<DimensionKey, MetricTimeSeries> entry : res.entrySet()) {
-      if (entry.getValue().getTimeWindowSet().size() < 2) {
-        LOG.warn("Insufficient data for {} to run anomaly detection function", entry.getKey());
-        continue;
-      }
+
       try {
         // Run algorithm
         DimensionKey dimensionKey = entry.getKey();
