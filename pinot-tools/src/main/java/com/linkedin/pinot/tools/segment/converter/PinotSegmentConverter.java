30111e399024c152b892f24157cfa21be3edc556
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2016-10-31 18:41:11 -0700

    Add tools to convert Pinot segment into another format.
    1. Fix bug in PinotSegmentRecordReader when reading multi-value columns.
    2. Add multi-value support to PinotSegmentToAvroConverter.
    3. Add CSV/JSON converter.

--- pinot-tools/src/main/java/com/linkedin/pinot/tools/segment/converter/PinotSegmentConverter.java
+++ pinot-tools/src/main/java/com/linkedin/pinot/tools/segment/converter/PinotSegmentConverter.java
@@ -15,75 +15,13 @@
  */
 package com.linkedin.pinot.tools.segment.converter;
 
-import org.kohsuke.args4j.CmdLineException;
-import org.kohsuke.args4j.CmdLineParser;
-import org.kohsuke.args4j.Option;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public interface PinotSegmentConverter {
 
-import com.linkedin.pinot.core.data.readers.FileFormat;
-import com.linkedin.pinot.tools.Command;
-import com.linkedin.pinot.tools.admin.command.AbstractBaseAdminCommand;
-
-/**
- * Tool to convert pinot segment into other formats such as AVRO, JSON, CSV
- */
-public class PinotSegmentConverter extends AbstractBaseAdminCommand implements Command {
-
-  private static final Logger LOGGER = LoggerFactory.getLogger(PinotSegmentConverter.class);
-
-  @Option(name = "-segmentIndexDir", required = true, metaVar = "<String>", usage = "Path to segment index directroy")
-  String segmentIndexDir;
-
-  @Option(name = "-outputDir", required = true, metaVar = "<String>", usage = "Path for generation of output")
-  String outputDir;
-
-  @Option(name = "-fileFormat", required = true, metaVar = "<String>", usage = "AVRO/CSV/JSON")
-  String fileFormat;
-
-  @Option(name = "-help", required = false, help = true, aliases = {"-h", "--h", "--help"},
-      usage = "Print this message.")
-  private boolean help = false;
-
-  @Override
-  public boolean execute() throws Exception {
-    if (fileFormat.equals(FileFormat.AVRO.toString())) {
-      PinotSegmentToAvroConverter pinotSegmentToAvroConverter = new PinotSegmentToAvroConverter(segmentIndexDir, outputDir);
-      pinotSegmentToAvroConverter.convert();
-    } else if (fileFormat.equals(FileFormat.CSV.toString())) {
-      LOGGER.error("FileFormat CSV not currently supported");
-    } else if (fileFormat.equals(FileFormat.JSON.toString())) {
-      LOGGER.error("FileFormat JSON not currently supported");
-    } else if (fileFormat.equals(FileFormat.GZIPPED_AVRO)){
-      LOGGER.error("FileFormat GZIPPED_AVRO not currently supported");
-    } else {
-      LOGGER.error("Unknown FileFormat {} must be one of {}", fileFormat, FileFormat.values());
-    }
-    return false;
-  }
-
-  @Override
-  public String description() {
-    return "Converting pinot segment to another format such as AVRO/CSV/JSON";
-  }
-
-  @Override
-  public boolean getHelp() {
-    return help;
-  }
-
-  public static void main(String[] args)
-      throws Exception {
-    PinotSegmentConverter converter = new PinotSegmentConverter();
-    CmdLineParser cmdLineParser = new CmdLineParser(converter);
-    try {
-      cmdLineParser.parseArgument(args);
-    } catch (CmdLineException e) {
-      LOGGER.error("Failed to read command line arguments: ", e);
-      cmdLineParser.printUsage(System.err);
-      System.exit(1);
-    }
-    converter.execute();
-  }
-
+  /**
+   * Convert Pinot Segment into another format.
+   *
+   * @throws Exception
+   */
+  void convert()
+      throws Exception;
 }
