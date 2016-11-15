70a172de8fe4017188888ff8421b4f7e77dff450
Jackie-Jiang <xajiang@linkedin.com>
2016-11-15 10:05:48 -0800

    Remove pinot files from cache even tests failed (#815)

--- .travis_test.sh
+++ .travis_test.sh
@@ -23,30 +23,34 @@ if [ $? -ne 0 ]; then
 fi
 
 # Only run tests for JDK 7
-if [ $TRAVIS_JDK_VERSION != 'oraclejdk7' ]; then
-  echo 'Skip tests for version other than oraclejdk7.'
-
-  # Remove Pinot files from local Maven respository to avoid a useless cache rebuild
+if [ "$TRAVIS_JDK_VERSION" != 'oraclejdk7' ]; then
+  echo 'Skip tests for version other than oracle jdk7.'
+  # Remove Pinot files from local Maven repository to avoid a useless cache rebuild
   rm -rf ~/.m2/repository/com/linkedin/pinot
   exit 0
 fi
 
+passed=0
 # Only run integration tests if needed
-if [ $RUN_INTEGRATION_TESTS != 'false' ]; then
+if [ "$RUN_INTEGRATION_TESTS" != 'false' ]; then
   mvn test -B -P travis,travis-integration-tests-only
-  if [ $? -ne 0 ]; then
-    exit 1
+  if [ $? -eq 0 ]; then
+    passed=1
   fi
 else
   mvn test -B -P travis,travis-no-integration-tests
-  if [ $? -ne 0 ]; then
-    exit 1
+  if [ $? -eq 0 ]; then
+    passed=1
   fi
 fi
 
-bash <(cat .codecov_bash)
-
-# Remove Pinot files from local Maven respository to avoid a useless cache rebuild
+# Remove Pinot files from local Maven repository to avoid a useless cache rebuild
 rm -rf ~/.m2/repository/com/linkedin/pinot
 
-exit 0
+if [ $passed -eq 1 ]; then
+  # Only send code coverage data if passed
+  bash <(cat .codecov_bash)
+  exit 0
+else
+  exit 1
+fi
