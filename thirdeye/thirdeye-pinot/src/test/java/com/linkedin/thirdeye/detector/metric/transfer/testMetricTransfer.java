dd4a13cde6517e0ff7c5fb1ba3205b352c5bed6f
Baolei Li <beli@linkedin.com>
2016-11-21 11:55:23 -0800

    following nameing conventions on third eye

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/detector/metric/transfer/testMetricTransfer.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/detector/metric/transfer/testMetricTransfer.java
@@ -13,7 +13,6 @@ import org.testng.Assert;
 
 public class testMetricTransfer {
 
-  // apply the
   @Test
   public void transfer(){
 
@@ -22,11 +21,11 @@ public class testMetricTransfer {
     String mName = "metric0";
     names.add(0, mName);
     List<MetricType> types = Collections.nCopies(names.size(), MetricType.DOUBLE);
-    MetricSchema _metricSchema = new MetricSchema(names, types);
-    MetricTimeSeries _metrics = new MetricTimeSeries(_metricSchema);
+    MetricSchema metricSchema = new MetricSchema(names, types);
+    MetricTimeSeries metrics = new MetricTimeSeries(metricSchema);
     double [] m0 = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
     for (long i=0l; i<=5l; i++) {
-      _metrics.set(i, mName, 1.0);
+      metrics.set(i, mName, 1.0);
     }
 
     // create a list of mock scaling factors
@@ -34,11 +33,11 @@ public class testMetricTransfer {
     List<ScalingFactor> sfList0 = new ArrayList<>();
     sfList0.add(sf0);
 
-    MetricTransfer.rescaleMetric(_metrics, mName, sfList0);
+    MetricTransfer.rescaleMetric(metrics, mName, sfList0);
     double [] m1_expected = {1.0, 1.0, 0.8, 0.8, 0.8, 1.0};
     double [] m_actual = new double[6];
     for (int i=0; i<=5; i++) {
-      m_actual[i]= _metrics.get(i, mName).doubleValue();
+      m_actual[i]= metrics.get(i, mName).doubleValue();
     }
     Assert.assertEquals(m_actual, m1_expected);
 
@@ -48,9 +47,9 @@ public class testMetricTransfer {
     sfList0.remove(0);
     Assert.assertEquals(sfList0.size(), 0);
     sfList0.add(_sf0);
-    MetricTransfer.rescaleMetric(_metrics, mName, sfList0);
+    MetricTransfer.rescaleMetric(metrics, mName, sfList0);
     for (int i=0; i<=5; i++) {
-      m_actual[i]= _metrics.get(i, mName).doubleValue();
+      m_actual[i]= metrics.get(i, mName).doubleValue();
     }
     Assert.assertEquals(m_actual, m0);
 
@@ -59,7 +58,7 @@ public class testMetricTransfer {
     ScalingFactor sf1 = new ScalingFactor(new TimeRange(12l, 14l), 0.8);
     sfList0.add(sf1);
     for (int i=0; i<=5; i++) {
-      m_actual[i]= _metrics.get(i, mName).doubleValue();
+      m_actual[i]= metrics.get(i, mName).doubleValue();
     }
     Assert.assertEquals(m_actual, m0);
 
