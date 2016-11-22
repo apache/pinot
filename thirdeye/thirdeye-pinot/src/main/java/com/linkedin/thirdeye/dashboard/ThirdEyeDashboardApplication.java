f46e4941db1bed30bdb1f702dc1919142ba15a42
Yen-Jung Chang <ychang@linkedin.com>
2016-11-22 15:45:24 -0800

    Add endpoint for CRUD of override configs (#855)

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/ThirdEyeDashboardApplication.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/ThirdEyeDashboardApplication.java
@@ -15,6 +15,7 @@ import com.linkedin.thirdeye.dashboard.resources.IngraphDashboardConfigResource;
 import com.linkedin.thirdeye.dashboard.resources.IngraphMetricConfigResource;
 import com.linkedin.thirdeye.dashboard.resources.JobResource;
 import com.linkedin.thirdeye.dashboard.resources.MetricConfigResource;
+import com.linkedin.thirdeye.dashboard.resources.OverrideConfigResource;
 import com.linkedin.thirdeye.dashboard.resources.SummaryResource;
 import com.linkedin.thirdeye.dashboard.resources.ThirdEyeResource;
 
@@ -67,7 +68,7 @@ public class ThirdEyeDashboardApplication
     env.jersey().register(new AdminResource());
     env.jersey().register(new SummaryResource());
     env.jersey().register(new ThirdEyeResource());
-
+    env.jersey().register(new OverrideConfigResource());
   }
 
   public static void main(String[] args) throws Exception {
