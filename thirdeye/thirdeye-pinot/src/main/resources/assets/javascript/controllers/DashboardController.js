bdef38298f6ad538207aa75921e3c34a54eb1142
Ravi Aringunram <raringun@raringun-mn1.linkedin.biz>
2016-11-28 10:27:30 -0800

    Add a few JS Dashboard controller methods

--- thirdeye/thirdeye-pinot/src/main/resources/assets/javascript/controllers/DashboardController.js
+++ thirdeye/thirdeye-pinot/src/main/resources/assets/javascript/controllers/DashboardController.js
@@ -3,5 +3,37 @@ function DashboardController(dashboardModel, dashboardView){
 }
 
 DashboardController.prototype ={
+    OnHomeButtonClickEvent : function() {
+
+    },
+
+    OnHeaderTabClickEvent : function() {
+
+    },
+
+    OnUserSettingsClickEvent : function() {
+
+    },
+
+    OnManageAnomaliesClickEvent : function() {
+
+    },
+
+    OnDashboardDisplaySearchEvent : function() {
+
+    },
+
+    OnDashboardDisplayTimeRangeEvent : function() {
+
+    },
+
+    OnDashboardDisplayViewChangeEvent : function() {
+
+    },
+
+    OnCreateAnomalyFunctionClickEvent : function() {
+
+    }
+
     
 }
