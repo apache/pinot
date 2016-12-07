a652f42348fd3b0bcc368edb7a834aa898640583
Puneet Jaiswal <punit.kj@gmail.com>
2016-12-07 11:51:03 -0800

    moving static object to model from view for Timeseries on RCA

--- thirdeye/thirdeye-pinot/src/main/resources/assets/javascript/models/TimeSeriesCompareModel.js
+++ thirdeye/thirdeye-pinot/src/main/resources/assets/javascript/models/TimeSeriesCompareModel.js
@@ -1,5 +1,12 @@
 function TimeSeriesCompareModel(params) {
   this.hashParams = params;
+  this.timeSeriesObject = {
+    'start': '2016-01-3',
+    'end': '2016-01-5',
+    'columns': [['date', '2016-01-01', '2016-01-2', '2016-01-3', '2016-01-4', '2016-01-05',
+      '2016-01-06', '2016-01-07'], ['current', 30, 200, 100, 400, 150, 250, 60],
+      ['baseline', 35, 225, 200, 600, 170, 220, 70]]
+  };
 }
 
 TimeSeriesCompareModel.prototype = {
