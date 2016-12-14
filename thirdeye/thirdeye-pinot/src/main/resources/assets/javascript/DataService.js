b19c90a253e6d7cce1bdb2ea65bf8639a29f9322
Puneet Jaiswal <punit.kj@gmail.com>
2016-12-13 18:36:45 -0800

    adding timeseries compare fetch method in dataservice

--- thirdeye/thirdeye-pinot/src/main/resources/assets/javascript/DataService.js
+++ thirdeye/thirdeye-pinot/src/main/resources/assets/javascript/DataService.js
@@ -1,65 +1,91 @@
 function DataService() {
-
+  this.URL_SEPARATOR = '/';
 }
 
 DataService.prototype = {
 
-    getSynchronousData: function(url, params)  {
-      console.log("request url:", url)
+  getSynchronousData: function(url, data)  {
+    console.log("request url:", url)
 
-      var results = undefined;
-       $.ajax({
-        url: url,
-        data: params,
-        type: 'get',
-        dataType: 'json',
-        async: false,
-        success: function(data) {
-          results = data;
-        },
-        error: function(e) {
-          console.log(e);
-        }
-      });
-       return results;
-    },
-    getAsynchronousData: function(url)  {
-      console.log("request url:", url)
+    var results = undefined;
+    $.ajax({
+      url: url,
+      data: data,
+      type: 'get',
+      dataType: 'json',
+      async: false,
+      success: function(data) {
+        results = data;
+      },
+      error: function(e) {
+        console.log(e);
+      }
+    });
+    return results;
+  },
+  getAsynchronousData: function(url)  {
+    console.log("request url:", url)
 
-      return $.ajax({
-        url: url,
-        type: 'get',
-        dataType: 'json',
+    return $.ajax({
+      url: url,
+      type: 'get',
+      dataType: 'json',
 
-      });
-    },
-    fetchAnomaliesForMetricIds : function(startTime, endTime, metricIds, functionName) {
-      var url = constants.SEARCH_ANOMALIES_METRICIDS + startTime + '/' + endTime;
-      var params = {
-          metricIds : metricIds.join(),
-          functionName : functionName
-      };
-      var anomalies = this.getSynchronousData(url, params);
-      return anomalies;
-    },
-    fetchAnomaliesForDashboardId : function(startTime, endTime, dashboardId, functionName) {
-      var url = constants.SEARCH_ANOMALIES_DASHBOARDID + startTime + '/' + endTime;
-      var params = {
-          dashboardId : dashboardId,
-          functionName : functionName
-      };
-      var anomalies = this.getSynchronousData(url, params);
-      return anomalies;
-    },
-    fetchAnomaliesForAnomalyIds: function(startTime, endTime, anomalyIds, functionName) {
-      var url = constants.SEARCH_ANOMALIES_ANOMALYIDS + startTime + '/' + endTime;
-      var params = {
-          anomalyIds : anomalyIds.join(),
-          functionName : functionName
-      };
-      var anomalies = this.getSynchronousData(url, params);
-      return anomalies;
-    },
+    });
+  },
+  postData: function(url, data) {
+    console.log("request url:", url)
+    return $.ajax({
+      url: url,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      type: 'post',
+      dataType: 'json',
+      data: data
+    });
+  },
+  fetchAnomaliesForMetricIds : function(startTime, endTime, metricIds, functionName) {
+    var url = constants.SEARCH_ANOMALIES_METRICIDS + startTime + this.URL_SEPARATOR + endTime;
+    var data = {
+      metricIds : metricIds.join(),
+      functionName : functionName
+    };
+    var anomalies = this.getSynchronousData(url, data);
+    return anomalies;
+  },
+  fetchAnomaliesForDashboardId : function(startTime, endTime, dashboardId, functionName) {
+    var url = constants.SEARCH_ANOMALIES_DASHBOARDID + startTime + this.URL_SEPARATOR + endTime;
+    var data = {
+      dashboardId : dashboardId,
+      functionName : functionName
+    };
+    var anomalies = this.getSynchronousData(url, data);
+    return anomalies;
+  },
+  fetchAnomaliesForAnomalyIds : function(startTime, endTime, anomalyIds, functionName) {
+    var url = constants.SEARCH_ANOMALIES_ANOMALYIDS + startTime + this.URL_SEPARATOR + endTime;
+    var data = {
+      anomalyIds : anomalyIds.join(),
+      functionName : functionName
+    };
+    var anomalies = this.getSynchronousData(url, data);
+    return anomalies;
+  },
+  updateFeedback : function(anomalyId, feedbackType) {
+    var url = constants.UPDATE_ANOMALY_FEEDBACK + anomalyId;
+    var data = '{ "feedbackType": "' + feedbackType + '","comment": ""}';
+    var response = this.postData(url, data);
+    console.log("Updated backend feedback " + feedbackType);
+  },
 
+  fetchTimeseriesCompare: function (metricId, currentStart, currentEnd, baselineStart, baselineEnd,
+      dimensions, filters) {
+    // TODO : set dimensions and filters
+    var url = "/timeseries/compare/" + metricId + "/" + currentStart + "/" + currentEnd + "/"
+        + baselineStart + "/" + baselineEnd;
+    return this.getSynchronousData(url);
+  }
 
 };
