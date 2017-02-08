7bdf38556c75a5665a330b21a32b81e552ebfc0d
Subbu Subramaniam <mcvsubbu@users.noreply.github.com>
2017-02-08 14:44:32 -0800

    Fix bug handling negative hash code (#1041)
    * Fix bug handling negative hash code
    
    * address review nit

--- pinot-controller/src/main/java/com/linkedin/pinot/controller/helix/core/realtime/PinotLLCRealtimeSegmentManager.java
+++ pinot-controller/src/main/java/com/linkedin/pinot/controller/helix/core/realtime/PinotLLCRealtimeSegmentManager.java
@@ -543,7 +543,9 @@ public class PinotLLCRealtimeSegmentManager {
     // the idealstate udpate fails due to contention. We serialize the updates to the idealstate
     // to reduce this contention. We may still contend with RetentionManager, or other updates
     // to idealstate from other controllers, but then we have the retry mechanism to get around that.
-    Lock lock = _idealstateUpdateLocks[realtimeTableName.hashCode()%NUM_LOCKS];
+    // hash code can be negative, so make sure we are getting a positive lock index
+    int lockIndex = (realtimeTableName.hashCode() & Integer.MAX_VALUE) % NUM_LOCKS;
+    Lock lock = _idealstateUpdateLocks[lockIndex];
     try {
       lock.lock();
       updateHelixIdealState(realtimeTableName, newInstances, committingSegmentNameStr, newSegmentNameStr);
