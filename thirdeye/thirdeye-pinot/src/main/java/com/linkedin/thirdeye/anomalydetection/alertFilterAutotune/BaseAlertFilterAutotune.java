374e6ae0b5f6b80d8f6f436927d6f46eaf92448d
Yungyu Chung <ychung@ychung-ld1.linkedin.biz>
2017-02-09 14:39:05 -0800

    Implement factory and base class for alertFilterAutotune

---
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomalydetection/alertFilterAutotune/BaseAlertFilterAutotune.java
@@ -0,0 +1,12 @@
+package com.linkedin.thirdeye.anomalydetection.alertFilterAutotune;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Created by ychung on 2/8/17.
+ */
+public abstract class BaseAlertFilterAutotune {
+  protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
+}
