a8b9e35135885faa5ad212e2001c2481067bd0c6
Yen-Jung Chang <ychang@linkedin.com>
2017-02-17 17:37:58 -0800

    Revert "Bump up jackson version number to fix method notfound during runtime"

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/ThirdEyeDashboardConfiguration.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/ThirdEyeDashboardConfiguration.java
@@ -1,5 +1,6 @@
 package com.linkedin.thirdeye.dashboard;
 
+import com.fasterxml.jackson.dataformat.yaml.snakeyaml.Yaml;
 import com.linkedin.thirdeye.common.ThirdEyeConfiguration;
 
 public class ThirdEyeDashboardConfiguration extends ThirdEyeConfiguration {
@@ -7,4 +8,9 @@ public class ThirdEyeDashboardConfiguration extends ThirdEyeConfiguration {
   public ThirdEyeDashboardConfiguration() {
     super();
   }
+
+  public static void main(String[] args) {
+    System.out.println(new Yaml().dump(new ThirdEyeDashboardConfiguration()));
+  }
+
 }
