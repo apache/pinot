fc474de6091a989e94adc48eb5be19247258f695
Alexander Pucher <apucher@linkedin.com>
2017-02-23 14:55:12 -0800

    event data source

---
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detector/functionex/impl/ThirdEyeEventDataSource.java
@@ -0,0 +1,78 @@
+package com.linkedin.thirdeye.detector.functionex.impl;
+
+import com.linkedin.thirdeye.datalayer.bao.EventManager;
+import com.linkedin.thirdeye.datalayer.dto.EventDTO;
+import com.linkedin.thirdeye.detector.functionex.AnomalyFunctionExContext;
+import com.linkedin.thirdeye.detector.functionex.AnomalyFunctionExDataSource;
+import com.linkedin.thirdeye.detector.functionex.dataframe.DataFrame;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class ThirdEyeEventDataSource implements AnomalyFunctionExDataSource<String, DataFrame> {
+
+  private final static String PATTERN = "type=(\\w+),start=(\\d+),end=(\\d+)";
+  private final static Pattern pattern = Pattern.compile(PATTERN);
+
+  private final EventManager manager;
+
+  public ThirdEyeEventDataSource(EventManager manager) {
+    this.manager = manager;
+  }
+
+  @Override
+  public DataFrame query(String query, AnomalyFunctionExContext context) throws Exception {
+    List<EventDTO> events = parseQuery(query);
+    DataFrame df = eventsToDataFrame(events);
+    return df;
+  }
+
+  public static class Query {
+    final long start;
+    final long end;
+    final String type;
+
+    public Query(long start, long end, String type) {
+      this.start = start;
+      this.end = end;
+      this.type = type;
+    }
+  }
+
+  private DataFrame eventsToDataFrame(List<EventDTO> events) {
+    String[] type = new String[events.size()];
+    String[] name = new String[events.size()];
+    long[] start = new long[events.size()];
+    long[] end = new long[events.size()];
+
+    for(int i=0; i<events.size(); i++) {
+      EventDTO e = events.get(i);
+      type[i] = e.getEventType();
+      name[i] = e.getName();
+      start[i] = e.getStartTime();
+      end[i] = e.getEndTime();
+    }
+
+    DataFrame df = new DataFrame(events.size());
+    df.addSeries("type", type);
+    df.addSeries("name", name);
+    df.addSeries("start", start);
+    df.addSeries("end", end);
+
+    return df;
+  }
+
+  private List<EventDTO> parseQuery(String query) {
+    Matcher m = pattern.matcher(query);
+
+    if(!m.find())
+      throw new IllegalArgumentException(String.format("query must match pattern '%s'", PATTERN));
+
+    String type = m.group(1);
+    long start = Long.parseLong(m.group(2));
+    long end = Long.parseLong(m.group(3));
+
+    return manager.findEventsBetweenTimeRange(type, start, end);
+  }
+}
---
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/detector/functionex/impl/ThirdEyeEventDataSourceTest.java
@@ -0,0 +1,96 @@
+package com.linkedin.thirdeye.detector.functionex.impl;
+
+import com.linkedin.thirdeye.datalayer.bao.EventManager;
+import com.linkedin.thirdeye.datalayer.bao.MockManager;
+import com.linkedin.thirdeye.datalayer.dto.EventDTO;
+import com.linkedin.thirdeye.detector.functionex.AnomalyFunctionExContext;
+import com.linkedin.thirdeye.detector.functionex.dataframe.DataFrame;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class ThirdEyeEventDataSourceTest {
+  static class MockEventManager extends MockManager<EventDTO> implements EventManager {
+    String type;
+    long start;
+    long end;
+
+    @Override
+    public List<EventDTO> findByEventType(String eventType) {
+      Assert.fail();
+      return null;
+    }
+
+    @Override
+    public List<EventDTO> findEventsBetweenTimeRange(String eventType, long startTime, long endTime) {
+      this.type = eventType;
+      this.start = startTime;
+      this.end = endTime;
+
+      return Arrays.asList(
+          makeEvent("HOLIDAY", "New Years 2016", 1451606400, 1451606400 + 86400),
+          makeEvent("HOLIDAY", "New Years 2017", 1483228800, 1483228800 + 86400),
+          makeEvent("holiday", "New Years 2018", 1514764800, 1514764800 + 86400)
+          );
+    }
+
+    @Override
+    public List<EventDTO> findEventsBetweenTimeRangeByName(String eventType, String name, long startTime,
+        long endTime) {
+      Assert.fail();
+      return null;
+    }
+  }
+
+  ThirdEyeEventDataSource ds;
+  MockEventManager manager;
+  AnomalyFunctionExContext context;
+
+  @BeforeMethod
+  public void before() {
+    manager = new MockEventManager();
+    ds = new ThirdEyeEventDataSource(manager);
+    context = new AnomalyFunctionExContext();
+  }
+
+  @Test
+  public void testParseQuery() throws Exception {
+    ds.query("type=abc,start=0123,end=444", context);
+
+    Assert.assertEquals(manager.type, "abc");
+    Assert.assertEquals(manager.start, 123);
+    Assert.assertEquals(manager.end, 444);
+  }
+
+  @Test(expectedExceptions = IllegalArgumentException.class)
+  public void testParseQueryFail() throws Exception {
+    ds.query("type=abc,start=0123,end=", context);
+  }
+
+  @Test
+  public void testQueryDataFrame() throws Exception {
+    DataFrame df = ds.query("type=IGNORED,start=000,end=000", context);
+
+    Assert.assertEquals(df.getIndex().size(), 3);
+    Assert.assertEquals(df.getSeriesNames(), new HashSet<>(Arrays.asList("type", "name", "start", "end")));
+    Assert.assertEquals(df.toStrings("type").values(), new String[] { "HOLIDAY", "HOLIDAY", "holiday" });
+    Assert.assertEquals(df.toStrings("name").values(), new String[] { "New Years 2016", "New Years 2017", "New Years 2018" });
+    Assert.assertEquals(df.toLongs("start").values(), new long[] { 1451606400, 1483228800, 1514764800 });
+    Assert.assertEquals(df.toLongs("end").values(), new long[] { 1451606400 + 86400, 1483228800 + 86400, 1514764800 + 86400 });
+  }
+
+  private static EventDTO makeEvent(String type, String name, long start, long end) {
+    EventDTO e = new EventDTO();
+    e.setEventType(type);
+    e.setName(name);
+    e.setStartTime(start);
+    e.setEndTime(end);
+    return e;
+  }
+
+}
