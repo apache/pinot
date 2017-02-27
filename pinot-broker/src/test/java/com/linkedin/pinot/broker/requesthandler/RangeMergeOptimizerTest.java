405cb4c64de40ed2a88464bd8d9bfd3e9cae30a0
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2017-02-27 11:18:42 -0800

    Fix a typo inside RangeMergeOptimizer
    Added unit test for the typo.

--- pinot-broker/src/test/java/com/linkedin/pinot/broker/requesthandler/RangeMergeOptimizerTest.java
+++ pinot-broker/src/test/java/com/linkedin/pinot/broker/requesthandler/RangeMergeOptimizerTest.java
@@ -174,6 +174,11 @@ public class RangeMergeOptimizerTest {
             false);
     compareTrees(actualTree, expectedTree);
 
+    // Query with same lower and upper range
+    actualTree = buildFilterQueryTree("select * from table where (time >= 10 and time <= 20) and (time between 10 and 20)", true);
+    expectedTree = buildFilterQueryTree("select * from table where time between 10 and 20", false);
+    compareTrees(actualTree, expectedTree);
+
     // Query without time column
     actualTree = buildFilterQueryTree("select * from table where foo = 'bar'", true);
     expectedTree = buildFilterQueryTree("select * from table where foo = 'bar'", false);
