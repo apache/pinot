089750f1dff76a73a65cc60876a0435174aba80f
Yves Yuen <yyuen@linkedin.com>
2017-03-01 13:12:56 -0800

    transpiling

--- thirdeye/thirdeye-pinot/GruntFile.js
+++ thirdeye/thirdeye-pinot/GruntFile.js
@@ -1,3 +1,35 @@
+// "use strict";
+
+// module.exports = (grunt) => {
+//   require('load-grunt-tasks')(grunt);
+
+//   // Project configuration
+//   grunt.initConfig({
+//     babel: {
+//       options: {
+//         sourceMap: true,
+//         presets: ['es2015']
+//       },
+//       dist: {
+//         // files: {
+//         //   'dist/app.js': './src/main/resources/assets/javascript/HashService.js'
+//         // }
+//         files: [
+//           {
+//             expand: true,
+//             cwd: 'src/main/resources/assets/javascript/',
+//             src: ['**/*.js'],
+//             dest: 'target/classes/assets/javascript/'
+//           }
+//         ]
+//       }
+//     }
+//   });
+
+//   grunt.registerTask('default', ['babel']);
+// };
+
+
 "use strict";
 
 module.exports = (grunt) => {
@@ -11,15 +43,16 @@ module.exports = (grunt) => {
         presets: ['es2015']
       },
       dist: {
-        // files: {
-        //   'dist/app.js': './src/main/resources/assets/javascript/HashService.js'
+        // files : {
+        //   "target/classes/assets/javascript/app.js": "src/main/resources/assets/javascript/*.js"
         // }
         files: [
           {
             expand: true,
             cwd: 'src/main/resources/assets/javascript/',
             src: ['**/*.js'],
-            dest: 'target/classes/assets/javascript/'
+            dest: 'src/main/resources/assets/javascript-compiled/',
+            ext: "-compiled.js"
           }
         ]
       }
