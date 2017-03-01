df5e108b33d7b6d2075486d6b7b395f735f13661
Yves Yuen <yyuen@linkedin.com>
2017-02-28 16:14:14 -0800

    set up grunt task

--- thirdeye/thirdeye-pinot/GruntFile.js
+++ thirdeye/thirdeye-pinot/GruntFile.js
@@ -1,43 +1,30 @@
 "use strict";
+
 module.exports = (grunt) => {
   require('load-grunt-tasks')(grunt);
 
   // Project configuration
   grunt.initConfig({
     babel: {
-        options: {
-            sourceMap: true,
-            presets: ['es2015']
-        },
-        dist: {
-            files: {
-                'dist/app.js': 'src/app.js'
-            }
-        }
+      options: {
+        sourceMap: true,
+        presets: ['es2015']
+      },
+      dist: {
+        // files: {
+        //   'dist/app.js': './src/main/resources/assets/javascript/HashService.js'
+        // }
+        files: [
+          {
+            expand: true,
+            cwd: 'src/main/resources/assets/javascript/',
+            src: ['**/*.js'],
+            dest: 'dist/src'
+          }
+        ]
+      }
     }
   });
 
   grunt.registerTask('default', ['babel']);
-
-
-  // // Project configuration.
-  // grunt.initConfig({
-  //   pkg: grunt.file.readJSON('package.json'),
-  //   uglify: {
-  //     options: {
-  //       banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-  //     },
-  //     build: {
-  //       src: 'src/<%= pkg.name %>.js',
-  //       dest: 'build/<%= pkg.name %>.min.js'
-  //     }
-  //   }
-  // });
-
-  // // Load the plugin that provides the "uglify" task.
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
-
-  // // Default task(s).
-  // grunt.registerTask('default', ['uglify']);
-
 };
