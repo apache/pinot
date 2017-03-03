0b09f517e2b96664b16837f192e58452b27e8942
Alexander Pucher <apucher@linkedin.com>
2017-03-02 18:36:17 -0800

    metric datasource

---
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detector/functionex/impl/ThirdEyeMetricDataSource.java
@@ -0,0 +1,155 @@
+package com.linkedin.thirdeye.detector.functionex.impl;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
+import com.linkedin.thirdeye.api.TimeGranularity;
+import com.linkedin.thirdeye.datalayer.bao.DatasetConfigManager;
+import com.linkedin.thirdeye.datalayer.dto.DatasetConfigDTO;
+import com.linkedin.thirdeye.detector.functionex.AnomalyFunctionExContext;
+import com.linkedin.thirdeye.detector.functionex.AnomalyFunctionExDataSource;
+import com.linkedin.thirdeye.detector.functionex.dataframe.DataFrame;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class ThirdEyeMetricDataSource implements AnomalyFunctionExDataSource<String, DataFrame> {
+
+  private static final int TOP_K = 10000;
+
+  AnomalyFunctionExDataSource<String, DataFrame> pinot;
+  DatasetConfigManager manager;
+
+  // dataset/metric/sum/dimension1=v1,dimension2=v2
+
+  public ThirdEyeMetricDataSource(AnomalyFunctionExDataSource<String, DataFrame> pinot, DatasetConfigManager manager) {
+    this.pinot = pinot;
+    this.manager = manager;
+  }
+
+  @Override
+  public DataFrame query(String query, AnomalyFunctionExContext context) throws Exception {
+    String[] fragments = query.split("/");
+    if(fragments.length < 3)
+      throw new IllegalArgumentException("requires at least dataset, metric, and function name");
+
+    String dataset = fragments[0];
+    String metric = fragments[1];
+    String function = fragments[2];
+    String filterString = null;
+    if(fragments.length >= 4)
+      filterString =  fragments[3];
+
+    Collection<Filter> filters = parseFilters(filterString);
+
+    // fetch dataset config
+    DatasetConfigDTO dto = this.manager.findByDataset(dataset);
+    if(dto == null)
+      throw new IllegalArgumentException(String.format("Could not find dataset '%s'", dataset));
+
+    // Time filter
+    TimeGranularity tg = new TimeGranularity(dto.getTimeDuration(), dto.getTimeUnit());
+    augmentFiltersWithTime(filters, dto.getTimeColumn(), tg, context.getMonitoringWindowStart(), context.getMonitoringWindowEnd());
+
+    // build query
+    String pinotQuery = makeQuery(dataset, metric, function, filters);
+
+    return this.pinot.query(pinotQuery, context);
+  }
+
+  static Collection<Filter> augmentFiltersWithTime(Collection<Filter> filters, String column, TimeGranularity tg, long start, long stop) {
+    String minTime = String.valueOf(tg.convertToUnit(start));
+    String maxTime = String.valueOf(tg.convertToUnit(stop));
+
+    filters.add(new Filter(column, ">=", minTime));
+    filters.add(new Filter(column, "<", maxTime));
+    return filters;
+  }
+
+  static List<Filter> parseFilters(String filterString) {
+    if(filterString == null || filterString.isEmpty())
+      return new ArrayList<>();
+
+    List<Filter> filters = new ArrayList<>();
+    String[] fragments = filterString.split(",");
+    for(String s : fragments) {
+      String[] sfrag = s.split("<=|>=|=|<|>|!=", 2);
+      if(sfrag.length != 2)
+        throw new IllegalArgumentException(String.format("Could not parse filter expression '%s'", s));
+      String key = sfrag[0];
+      String value = sfrag[1];
+      String op = s.substring(key.length(), s.length() - value.length());
+      filters.add(new Filter(key, op, value));
+    }
+    return filters;
+  }
+
+  static String makeDimensions(Collection<Filter> filters) {
+    Set<String> keySet = new HashSet<>();
+    for(Filter f : filters) {
+      keySet.add(f.key);
+    }
+    return String.join(", ", keySet);
+  }
+
+  static String makeFilters(Collection<Filter> filters) {
+    Multimap<String, Filter> mmap = ArrayListMultimap.create();
+    for(Filter f : filters) {
+      mmap.put(f.key, f);
+    }
+
+    List<String> exprAnd = new ArrayList<>();
+    for(Map.Entry<String, Collection<Filter>> e : mmap.asMap().entrySet()) {
+      boolean isAlternativesClause = true;
+      List<String> exprOr = new ArrayList<>();
+      for(Filter f : e.getValue()) {
+        if(!"=".equals(f.operator))
+          isAlternativesClause = false;
+        exprOr.add(filter2string(f));
+      }
+
+      String operator = "AND";
+      if(isAlternativesClause)
+        operator = "OR";
+
+      exprAnd.add(joinExpressions(operator, exprOr));
+    }
+    return joinExpressions("AND", exprAnd);
+  }
+
+  static String filter2string(Filter f) {
+    return String.format("(%s%s'%s')", f.key, f.operator, f.value);
+  }
+
+  static String joinExpressions(String operator, List<String> expressions) {
+    if(expressions.isEmpty())
+      return "";
+    return String.format("(%s)", String.join(String.format(" %s ", operator), expressions));
+  }
+
+  static String makeQuery(String dataset, String metric, String function, Collection<Filter> filters) {
+    String filterString = "";
+    String groupbyString = "";
+    if(!filters.isEmpty()) {
+      filterString = String.format("WHERE %s", makeFilters(filters));
+      groupbyString = String.format("GROUP BY %s", makeDimensions(filters));
+    }
+    return String.format("SELECT %s(%s) FROM %s %s %s TOP %d", function, metric, dataset, filterString, groupbyString, TOP_K);
+  }
+
+  static class Filter {
+    final String key;
+    final String operator;
+    final String value;
+
+    public Filter(String key, String operator, String value) {
+      this.key = key;
+      this.operator = operator;
+      this.value = value;
+    }
+  }
+}
---
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/detector/functionex/impl/ThirdEyeMetricDataSourceTest.java
@@ -0,0 +1,125 @@
+package com.linkedin.thirdeye.detector.functionex.impl;
+
+import com.linkedin.thirdeye.datalayer.bao.DatasetConfigManager;
+import com.linkedin.thirdeye.datalayer.bao.MockManager;
+import com.linkedin.thirdeye.datalayer.dto.DatasetConfigDTO;
+import com.linkedin.thirdeye.detector.functionex.AnomalyFunctionExContext;
+import com.linkedin.thirdeye.detector.functionex.AnomalyFunctionExDataSource;
+import com.linkedin.thirdeye.detector.functionex.dataframe.DataFrame;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+
+public class ThirdEyeMetricDataSourceTest {
+
+  static class MockDataSource implements AnomalyFunctionExDataSource<String, DataFrame> {
+    String query;
+    @Override
+    public DataFrame query(String query, AnomalyFunctionExContext context) throws Exception {
+      this.query = query;
+      return new DataFrame(0);
+    }
+  }
+
+  static class MockDatasetManager extends MockManager<DatasetConfigDTO> implements DatasetConfigManager {
+    @Override
+    public DatasetConfigDTO findByDataset(String dataset) {
+      DatasetConfigDTO dto = new DatasetConfigDTO();
+      dto.setTimeColumn("time");
+      dto.setTimeDuration(10);
+      dto.setTimeUnit(TimeUnit.SECONDS);
+      return dto;
+    }
+
+    @Override
+    public List<DatasetConfigDTO> findActive() {
+      Assert.fail();
+      return null;
+    }
+
+    @Override
+    public List<DatasetConfigDTO> findActiveRequiresCompletenessCheck() {
+      Assert.fail();
+      return null;
+    }
+  }
+
+  MockDataSource pinot;
+  MockDatasetManager manager;
+  ThirdEyeMetricDataSource ds;
+  AnomalyFunctionExContext context;
+
+  @BeforeMethod
+  public void before() {
+    this.pinot = new MockDataSource();
+    this.manager = new MockDatasetManager();
+    this.ds = new ThirdEyeMetricDataSource(pinot, manager);
+
+    this.context = new AnomalyFunctionExContext();
+    this.context.setMonitoringWindowStart(1000000);
+    this.context.setMonitoringWindowEnd(2000000);
+  }
+
+  @Test
+  public void testParseFilters() {
+    String filterString = "a=bb,bb>=ccc,c<=aa,asdf<4,d>asdf,pew!=pew!";
+    List<ThirdEyeMetricDataSource.Filter> filters = ThirdEyeMetricDataSource.parseFilters(filterString);
+    Assert.assertEquals(filters.size(), 6);
+    assertFilter(filters.get(0), "a", "=", "bb");
+    assertFilter(filters.get(1), "bb", ">=", "ccc");
+    assertFilter(filters.get(2), "c", "<=", "aa");
+    assertFilter(filters.get(3), "asdf", "<", "4");
+    assertFilter(filters.get(4), "d", ">", "asdf");
+    assertFilter(filters.get(5), "pew", "!=", "pew!");
+  }
+
+  @Test
+  public void testAlternativesClause() {
+    String s = ThirdEyeMetricDataSource.makeFilters(Arrays.asList(
+        new ThirdEyeMetricDataSource.Filter("a", "=", "b"),
+        new ThirdEyeMetricDataSource.Filter("a", "=", "c")));
+    Assert.assertEquals(s, "(((a='b') OR (a='c')))");
+  }
+
+  @Test
+  public void testAlternativesClauseOneEquals() {
+    String s = ThirdEyeMetricDataSource.makeFilters(Arrays.asList(
+        new ThirdEyeMetricDataSource.Filter("a", "=", "b"),
+        new ThirdEyeMetricDataSource.Filter("a", "<", "c")));
+    Assert.assertEquals(s, "(((a='b') AND (a<'c')))");
+  }
+
+  @Test
+  public void testDefaultClause() {
+    String s = ThirdEyeMetricDataSource.makeFilters(Arrays.asList(
+        new ThirdEyeMetricDataSource.Filter("a", ">=", "b"),
+        new ThirdEyeMetricDataSource.Filter("a", "<", "c")));
+    Assert.assertEquals(s, "(((a>='b') AND (a<'c')))");
+  }
+
+  @Test
+  public void testQueryWithoutFilter() throws Exception {
+    String query = "dataset/metric/function";
+    this.ds.query(query, context);
+    Assert.assertEquals(this.pinot.query, "SELECT function(metric) FROM dataset WHERE (((time>='100') AND (time<'200'))) GROUP BY time TOP 10000");
+  }
+
+  @Test
+  public void testQueryWithFilter() throws Exception {
+    String query = "dataset/metric/function/k1=v1,k1=v2,k2=v2";
+    this.ds.query(query, context);
+    Assert.assertEquals(this.pinot.query, "SELECT function(metric) FROM dataset WHERE (((k1='v1') OR (k1='v2')) AND ((k2='v2')) AND ((time>='100') AND (time<'200'))) GROUP BY k1, k2, time TOP 10000");
+  }
+
+  static void assertFilter(ThirdEyeMetricDataSource.Filter f, String key, String operator, String value) {
+    Assert.assertEquals(f.key, key);
+    Assert.assertEquals(f.operator, operator);
+    Assert.assertEquals(f.value, value);
+  }
+
+}
