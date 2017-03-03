1a3b974bcb8394adf59a43797650446115e6adcb
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2017-03-03 15:17:17 -0800

    Add Travis Support to ThirdEye

--- .travis_install.sh
+++ .travis_install.sh
@@ -22,10 +22,27 @@ if [ $? -ne 0 ]; then
   echo 'Commit range is invalid.'
   exit 1
 fi
-git diff --name-only $TRAVIS_COMMIT_RANGE | egrep '^(pinot-|pom.xml|.travis|.codecov)'
-if [ $? -ne 0 ]; then
-  echo 'No changes related to the pinot code, skip the install.'
-  exit 0
+
+# ThirdEye related changes
+git diff --name-only $TRAVIS_COMMIT_RANGE | egrep '^(thirdeye)'
+noThirdEyeChange=$?
+if [ $noThirdEyeChange -eq 0 ]; then
+  echo 'ThirdEye changes.'
+
+  if [ "$TRAVIS_JDK_VERSION" != 'oraclejdk8' ]; then
+    echo 'Skip ThirdEye install for version other than oracle jdk8.'
+    exit 0
+  fi
 fi
 
 mvn clean install -B -DskipTests=true -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
+# Build ThirdEye for ThirdEye related changes
+if [ $noThirdEyeChange -eq 0 ]; then
+  cd thirdeye
+  mvn clean install -DskipTests
+  exit $?
+fi
