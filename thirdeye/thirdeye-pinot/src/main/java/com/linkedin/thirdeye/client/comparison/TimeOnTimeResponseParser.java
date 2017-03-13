68d7257704d1b44b8ce3f42015dcc57511b4033f
Yen-Jung Chang <ychang@linkedin.com>
2017-03-13 00:05:41 -0700

    Add log whenever DST issue occurs

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/client/comparison/TimeOnTimeResponseParser.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/client/comparison/TimeOnTimeResponseParser.java
@@ -73,6 +73,9 @@ public class TimeOnTimeResponseParser {
     metricFunctions = baselineResponse.getMetricFunctions();
     numMetrics = metricFunctions.size();
     numTimeBuckets = Math.min(currentRanges.size(), baselineRanges.size());
+    if (currentRanges.size() != baselineRanges.size()) {
+      LOGGER.info("Current and baseline time series have different length, which could be induced by DST.");
+    }
     rows = new ArrayList<>();
 
     if (hasGroupByTime) {
