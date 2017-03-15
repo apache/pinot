4837ed6bd4e593c944d59a9bb538910c6d01cc30
Kexin Nie <knie@knie-ld1.linkedin.biz>
2017-03-15 16:09:58 -0700

    reformat dummy alert filter auto tune

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomalydetection/alertFilterAutotune/DummyAlertFilterAutoTune.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomalydetection/alertFilterAutotune/DummyAlertFilterAutoTune.java
@@ -1,6 +1,7 @@
 package com.linkedin.thirdeye.anomalydetection.alertFilterAutotune;
 
 import com.linkedin.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,16 +12,14 @@ public class DummyAlertFilterAutoTune implements AlertFilterAutoTune {
   @Override
   public Map<String, String> initiateAutoTune(List<MergedAnomalyResultDTO> anomalyResults, int nExpectedAnomalies)
       throws Exception {
-    Map<String, String> emptyMap = new HashMap<>();
-    return emptyMap;
+    return Collections.emptyMap();
   }
 
   @Override
   public Map<String, String> tuneAlertFilter(List<MergedAnomalyResultDTO> anomalyResults, double currentPrecision,
       double currentRecall) throws Exception {
     // do nothing and return empty map
-    Map<String, String> emptyMap = new HashMap<>();
-    return emptyMap;
+    return Collections.emptyMap();
   }
 
   @Override
