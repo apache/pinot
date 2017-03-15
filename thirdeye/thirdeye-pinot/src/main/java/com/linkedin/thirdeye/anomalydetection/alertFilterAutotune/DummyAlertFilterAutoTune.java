b249c355231b0b6bf4a167bf3e5bae8d04e47a61
Kexin Nie <knie@linkedin.com>
2017-03-15 16:34:05 -0700

    Initiate AlertFilterAutoTune Interface (#1152)
    * Modify AlertFilterAutoTune Interface to support initiateAlertFilterAutoTune
    
    * reformat dummy alert filter auto tune

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomalydetection/alertFilterAutotune/DummyAlertFilterAutoTune.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomalydetection/alertFilterAutotune/DummyAlertFilterAutoTune.java
@@ -1,7 +1,7 @@
 package com.linkedin.thirdeye.anomalydetection.alertFilterAutotune;
 
 import com.linkedin.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -9,11 +9,16 @@ import java.util.Map;
 public class DummyAlertFilterAutoTune implements AlertFilterAutoTune {
 
   @Override
+  public Map<String, String> initiateAutoTune(List<MergedAnomalyResultDTO> anomalyResults, int nExpectedAnomalies)
+      throws Exception {
+    return Collections.emptyMap();
+  }
+
+  @Override
   public Map<String, String> tuneAlertFilter(List<MergedAnomalyResultDTO> anomalyResults, double currentPrecision,
       double currentRecall) throws Exception {
     // do nothing and return empty map
-    Map<String, String> emptyMap = new HashMap<>();
-    return emptyMap;
+    return Collections.emptyMap();
   }
 
   @Override
