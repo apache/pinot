543325be19742b4437754db76c52f561c252ce9c
Baolei Li <beli@linkedin.com>
2017-03-29 17:21:29 -0700

    upate TimeSeries in anomalydetection context to align with anomlay-detection lib (#1221)

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomalydetection/context/TimeSeries.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomalydetection/context/TimeSeries.java
@@ -1,5 +1,7 @@
 package com.linkedin.thirdeye.anomalydetection.context;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.NavigableMap;
 import java.util.SortedSet;
 import java.util.TreeMap;
@@ -11,7 +13,7 @@ import org.joda.time.Interval;
  * Time series for anomaly detection, which contains the pairs of timestamp and data values from
  * one time series.
  */
-public class TimeSeries {
+public class TimeSeries implements MetricTimeSeries {
 
   /**
    * Timestamps to values. Timestamps are sorted.
@@ -23,6 +25,25 @@ public class TimeSeries {
    */
   private Interval timeSeriesInterval = new Interval(0L, 0L);
 
+  public TimeSeries() {
+    timeSeries = new TreeMap<>();
+  }
+
+  /**
+   * Construct time series with list of timestamps and its corresponding values
+   */
+  public TimeSeries(List<Long> timeStamps, List<Double> values)
+      throws Exception {
+
+    timeSeries = new TreeMap<>();
+    if (timeStamps.size() != values.size()) {
+      throw new IOException("time stamps list and value list need to match in size!!");
+    }
+
+    for (int i = 0; i < timeStamps.size(); i++) {
+      timeSeries.put(timeStamps.get(i), values.get(i));
+    }
+  }
 
   /**
    * Returns data value of the specified timestamp
@@ -48,6 +69,7 @@ public class TimeSeries {
   /**
    * Removes data value of the specified timestamp
    */
+  @Override
   public void remove(long timeStamp) {
     timeSeries.remove(timeStamp);
   }
@@ -59,6 +81,7 @@ public class TimeSeries {
    *
    * @return true if the specified timestamp exists
    */
+  @Override
   public boolean hasTimestamp(long timestamp) {
     return timeSeries.containsKey(timestamp);
   }
@@ -66,6 +89,7 @@ public class TimeSeries {
   /**
    * Returns a sorted timestamp set
    */
+  @Override
   public SortedSet<Long> timestampSet() {
     return timeSeries.navigableKeySet();
   }
@@ -73,6 +97,7 @@ public class TimeSeries {
   /**
    * Returns the number of timestamps in this time series
    */
+  @Override
   public int size() {
     return timeSeries.size();
   }
@@ -80,6 +105,7 @@ public class TimeSeries {
   /**
    * Returns the interval of the time series, which provides the max and min timestamps (inclusive).
    */
+  @Override
   public Interval getTimeSeriesInterval() {
     return timeSeriesInterval;
   }
@@ -96,4 +122,3 @@ public class TimeSeries {
     return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
   }
 }
-
