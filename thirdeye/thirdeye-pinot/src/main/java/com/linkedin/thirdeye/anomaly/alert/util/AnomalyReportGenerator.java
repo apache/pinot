aef4d60637b2af21680583313ac866a8d59976d3
Neha Pawar <npawar@linkedin.com>
2017-04-06 11:15:38 -0700

    Change email subject to Thirdeye Alert

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/alert/util/AnomalyReportGenerator.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/alert/util/AnomalyReportGenerator.java
@@ -91,7 +91,7 @@ public class AnomalyReportGenerator {
 
   public void buildReport(List<MergedAnomalyResultDTO> anomalies,
       ThirdEyeAnomalyConfiguration configuration, AlertConfigDTO alertConfig) {
-    String subject = "Thirdeye anomaly report : " + alertConfig.getName();
+    String subject = "Thirdeye Alert : " + alertConfig.getName();
     long startTime = System.currentTimeMillis();
     long endTime = 0;
     for (MergedAnomalyResultDTO anomaly : anomalies) {
