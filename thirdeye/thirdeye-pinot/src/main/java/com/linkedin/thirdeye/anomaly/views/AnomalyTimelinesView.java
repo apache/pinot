e453295c2425fece91e41b2e33b797e79960301d
Yungyu Chung <ychung@linkedin.com>
2017-04-12 11:37:26 -0700

    Implement parsing function from and to JSON String (#1272)
    * Implement parsing function from and to JSON String

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/views/AnomalyTimelinesView.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/views/AnomalyTimelinesView.java
@@ -1,13 +1,18 @@
 package com.linkedin.thirdeye.anomaly.views;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.linkedin.thirdeye.dashboard.views.TimeBucket;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class AnomalyTimelinesView {
+  private static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
   List<TimeBucket> timeBuckets = new ArrayList<>();
   Map<String, String> summary = new HashMap<>();
   List<Double> currentValues = new ArrayList<>();
@@ -43,5 +48,36 @@ public class AnomalyTimelinesView {
 
   public void addBaselineValues(Double baselineValue) {
     this.baselineValues.add(baselineValue);
+  }
+
+  /**
+   * Convert current instance into JSON String using ObjectMapper
+   *
+   * NOTE, as long as the getter and setter is implemented, the ObjectMapper constructs the JSON String via
+   * the getter and setter
+   * @return
+   *    The JSON String of current instance
+   * @throws JsonProcessingException
+   */
+  public String toJsonString() throws JsonProcessingException {
+    String jsonString = OBJECT_MAPPER.writeValueAsString(this);
+
+    return jsonString;
+  }
+
+  /**
+   * Given the JSON String of an AnomalyTimelinesView, return an instance of AnomalyTimelinesView
+   *
+   * NOTE, as long as the getter and setter is implemented, the ObjectMapper constructs the JSON String via
+   * the getter and setter
+   * @param jsonString
+   *    The JSON String of an instance
+   * @return
+   *    An instance based on the given JSON String
+   * @throws IOException
+   */
+  public static AnomalyTimelinesView fromJsonString(String jsonString) throws IOException {
+    AnomalyTimelinesView view = OBJECT_MAPPER.readValue(jsonString, AnomalyTimelinesView.class);
+    return view;
   }
 }
