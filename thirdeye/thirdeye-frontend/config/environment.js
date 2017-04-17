263de589bb09fdc03980f0d52e5e57078576cbe9
Yves Yuen <yyuen@linkedin.com>
2017-04-17 12:06:58 -0700

    change npm to yarn

--- thirdeye/thirdeye-frontend/config/environment.js
+++ thirdeye/thirdeye-frontend/config/environment.js
@@ -25,6 +25,7 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
+    ENV.rootURL = '/';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
