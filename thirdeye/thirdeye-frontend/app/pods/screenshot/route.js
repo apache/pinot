366589830d7bb3fef81a8ebf9760b2ecb5d90467
Yves Yuen <yyuen@linkedin.com>
2017-04-19 10:47:04 -0700

    import ember-redux and ember-network

--- thirdeye/thirdeye-frontend/app/pods/screenshot/route.js
+++ thirdeye/thirdeye-frontend/app/pods/screenshot/route.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import fetch from 'ember-network/fetch';
 
 export default Ember.Route.extend({
   model(){
-    return Ember.$.getJSON('anomalies/search/time/1492326000000/1492412400000/1');
+    return fetch('anomalies/search/time/1492326000000/1492412400000/1')
+      .then(res => res.json());
   }
 });
