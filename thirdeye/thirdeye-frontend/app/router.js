500ecaa51c122d756a2d1a3d026687b1408657d9
Yves Yuen <yyuen@linkedin.com>
2017-04-26 16:05:04 -0700

    address yuin's feedback

--- thirdeye/thirdeye-frontend/app/router.js
+++ thirdeye/thirdeye-frontend/app/router.js
@@ -7,7 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('example', { path: 'example/:id'});
+  this.route('example', { path: 'example/:id' });
 });
 
 export default Router;
