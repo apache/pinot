9018267877b3986dabef3e441272d50c26d1d5b4
Xiaotian Jiang <xajiang@linkedin.com>
2017-05-02 15:40:34 -0700

    Allow loading offline table schema with offline table name or raw table name (#1382)
    Added APIs in ZKMetadataProvider to load schema using schema name or table name
    Change existing code to load schema using the new APIs

--- pinot-controller/src/main/java/com/linkedin/pinot/controller/api/restlet/resources/PinotTableSchema.java
+++ pinot-controller/src/main/java/com/linkedin/pinot/controller/api/restlet/resources/PinotTableSchema.java
@@ -1,18 +1,5 @@
 package com.linkedin.pinot.controller.api.restlet.resources;
 
-import com.google.common.base.Preconditions;
-import com.linkedin.pinot.common.config.TableNameBuilder;
-import java.io.File;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
-import org.restlet.data.MediaType;
-import org.restlet.data.Status;
-import org.restlet.representation.Representation;
-import org.restlet.representation.StringRepresentation;
-import org.restlet.resource.Get;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.linkedin.pinot.common.config.AbstractTableConfig;
 import com.linkedin.pinot.common.data.Schema;
 import com.linkedin.pinot.common.metrics.ControllerMeter;
 import com.linkedin.pinot.common.restlet.swagger.HttpVerb;
@@ -20,81 +7,71 @@ import com.linkedin.pinot.common.restlet.swagger.Parameter;
 import com.linkedin.pinot.common.restlet.swagger.Paths;
 import com.linkedin.pinot.common.restlet.swagger.Summary;
 import com.linkedin.pinot.common.restlet.swagger.Tags;
-import com.linkedin.pinot.common.utils.CommonConstants.Helix.TableType;
 import com.linkedin.pinot.controller.api.ControllerRestApplication;
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class PinotTableSchema extends BasePinotControllerRestletResource {
-
   private static final Logger LOGGER = LoggerFactory.getLogger(PinotTableSchema.class);
-  private final File baseDataDir;
-  private final File tempDir;
 
-  public PinotTableSchema() throws IOException {
-    baseDataDir = new File(_controllerConf.getDataDir());
+  public PinotTableSchema()
+      throws IOException {
+    File baseDataDir = new File(_controllerConf.getDataDir());
     if (!baseDataDir.exists()) {
       FileUtils.forceMkdir(baseDataDir);
     }
-    tempDir = new File(baseDataDir, "schemasTemp");
-    if (!tempDir.exists()) {
-      FileUtils.forceMkdir(tempDir);
+    File schemaTempDir = new File(baseDataDir, "schemasTemp");
+    if (!schemaTempDir.exists()) {
+      FileUtils.forceMkdir(schemaTempDir);
     }
   }
 
   @Override
   @Get
   public Representation get() {
-    final String tableName = (String) getRequest().getAttributes().get("tableName");
-
+    String tableName = (String) getRequest().getAttributes().get("tableName");
     return getTableSchema(tableName);
   }
 
   @HttpVerb("get")
   @Summary("Gets schema for a table")
   @Tags({"schema", "table"})
-  @Paths({
-      "/tables/{tableName}/schema",
-      "/tables/{tableName}/schema/"
-  })
+  @Paths({"/tables/{tableName}/schema", "/tables/{tableName}/schema/"})
   private Representation getTableSchema(
       @Parameter(name = "tableName", in = "path", description = "Table name for which to get the schema",
           required = true) String tableName) {
+    // TODO: After uniform schema, directly use table name as schema name
+
     if (_pinotHelixResourceManager.hasRealtimeTable(tableName)) {
-      try {
-        AbstractTableConfig config = _pinotHelixResourceManager.getTableConfig(tableName, TableType.REALTIME);
-        String schemaName = config.getValidationConfig().getSchemaName();
-        Schema schema = _pinotHelixResourceManager.getSchema(schemaName);
-        Preconditions.checkNotNull(schema, "Failed to fetch schema: %s for REALTIME table: %s", schemaName, tableName);
-        return new StringRepresentation(schema.getJSONSchema());
-      } catch (Exception e) {
-        LOGGER.error("Caught exception while fetching schema for REALTIME table: {} ", tableName, e);
+      Schema schema = _pinotHelixResourceManager.getRealtimeTableSchema(tableName);
+      if (schema == null) {
+        LOGGER.error("Failed to fetch schema for realtime table: {}", tableName);
         ControllerRestApplication.getControllerMetrics()
             .addMeteredGlobalValue(ControllerMeter.CONTROLLER_TABLE_SCHEMA_GET_ERROR, 1L);
         setStatus(Status.SERVER_ERROR_INTERNAL);
-        return PinotSegmentUploadRestletResource.exceptionToStringRepresentation(e);
+        return new StringRepresentation("Error: failed to fetch schema for realtime table: " + tableName);
       }
+      return new StringRepresentation(schema.getJSONSchema());
     }
 
     if (_pinotHelixResourceManager.hasOfflineTable(tableName)) {
-      // For OFFLINE table, schema name is the same as table name
-      String schemaName = TableNameBuilder.OFFLINE_TABLE_NAME_BUILDER.forTable(tableName);
-      try {
-        Schema schema = _pinotHelixResourceManager.getSchema(schemaName);
-        if (schema == null) {
-          setStatus(Status.CLIENT_ERROR_NOT_FOUND);
-          return new StringRepresentation("Error: schema " + schemaName + " not found");
-        }
-        return new StringRepresentation(schema.getJSONSchema());
-      } catch (Exception e) {
-        LOGGER.error("Caught exception while fetching schema for OFFLINE table :{} ", tableName, e);
-        ControllerRestApplication.getControllerMetrics()
-            .addMeteredGlobalValue(ControllerMeter.CONTROLLER_TABLE_SCHEMA_GET_ERROR, 1L);
-        setStatus(Status.SERVER_ERROR_INTERNAL);
-        return PinotSegmentUploadRestletResource.exceptionToStringRepresentation(e);
+      Schema schema = _pinotHelixResourceManager.getOfflineTableSchema(tableName);
+      if (schema == null) {
+        setStatus(Status.CLIENT_ERROR_NOT_FOUND);
+        return new StringRepresentation("Error: schema not found for offline table: " + tableName);
       }
+      return new StringRepresentation(schema.getJSONSchema());
     }
 
     setStatus(Status.CLIENT_ERROR_NOT_FOUND);
-    return new StringRepresentation("Error: table " + tableName + " not found");
+    return new StringRepresentation("Error: table: " + tableName + " not found");
   }
 }
