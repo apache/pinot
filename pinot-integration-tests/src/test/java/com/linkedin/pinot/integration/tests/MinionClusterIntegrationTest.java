01921012de6abe45f974a946811ff0b0d170c382
Xiaotian Jiang <xajiang@linkedin.com>
2017-05-10 15:08:57 -0700

    Integrate Minion into the cluster (#1420)
    Integrate PinotHelixTaskResourceManager and PinotTaskManager into ControllerStarter
    Let MinionClusterIntegrationTest extends HybridClusterIntegrationTest and run all query tests as well
    Added tests for PinotHelixResourceManager APIs in MinionClusterIntegrationTest

--- pinot-integration-tests/src/test/java/com/linkedin/pinot/integration/tests/MinionClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/com/linkedin/pinot/integration/tests/MinionClusterIntegrationTest.java
@@ -18,16 +18,14 @@ package com.linkedin.pinot.integration.tests;
 import com.google.common.util.concurrent.Uninterruptibles;
 import com.linkedin.pinot.common.config.AbstractTableConfig;
 import com.linkedin.pinot.common.config.PinotTaskConfig;
-import com.linkedin.pinot.common.utils.CommonConstants;
-import com.linkedin.pinot.common.utils.ZkStarter;
 import com.linkedin.pinot.controller.helix.core.PinotHelixResourceManager;
 import com.linkedin.pinot.controller.helix.core.minion.ClusterInfoProvider;
 import com.linkedin.pinot.controller.helix.core.minion.PinotHelixTaskResourceManager;
 import com.linkedin.pinot.controller.helix.core.minion.PinotTaskManager;
 import com.linkedin.pinot.controller.helix.core.minion.generator.PinotTaskGenerator;
-import com.linkedin.pinot.minion.MinionStarter;
 import com.linkedin.pinot.minion.exception.TaskCancelledException;
 import com.linkedin.pinot.minion.executor.BaseTaskExecutor;
+import com.linkedin.pinot.minion.executor.PinotTaskExecutor;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -35,9 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
-import org.apache.commons.configuration.Configuration;
-import org.apache.commons.configuration.PropertiesConfiguration;
-import org.apache.helix.task.TaskDriver;
 import org.apache.helix.task.TaskState;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
@@ -45,49 +40,37 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 
-public class MinionClusterIntegrationTest {
-  private static final int ZK_PORT = 2191;
-  private static final String ZK_ADDRESS = "localhost:" + ZK_PORT;
-  private static final String HELIX_CLUSTER_NAME = "pinot-minion-test";
-  private static final String CONTROLLER_ID = "controller";
+public class MinionClusterIntegrationTest extends HybridClusterIntegrationTest {
+  private static final String RAW_TABLE_NAME = "mytable";
+  private static final String OFFLINE_TABLE_NAME = "mytable_OFFLINE";
+  private static final String REALTIME_TABLE_NAME = "mytable_REALTIME";
   private static final int NUM_WORKERS = 3;
 
-  private ZkStarter.ZookeeperInstance _zookeeperInstance;
   private PinotHelixResourceManager _pinotHelixResourceManager;
   private PinotHelixTaskResourceManager _pinotHelixTaskResourceManager;
   private PinotTaskManager _pinotTaskManager;
-  private MinionStarter[] _minionStarters = new MinionStarter[NUM_WORKERS];
 
   @BeforeClass
   public void setUp()
       throws Exception {
-    // Start a ZooKeeper instance.
-    _zookeeperInstance = ZkStarter.startLocalZkServer(ZK_PORT);
+    // The parent setUp() sets up Zookeeper, Kafka, controller, broker and servers
+    super.setUp();
 
-    // Start a PinotHelixResourceManager
-    _pinotHelixResourceManager = new PinotHelixResourceManager(ZK_ADDRESS, HELIX_CLUSTER_NAME, CONTROLLER_ID, null);
-    _pinotHelixResourceManager.start();
+    _pinotHelixResourceManager = _controllerStarter.getHelixResourceManager();
+    _pinotHelixTaskResourceManager = _controllerStarter.getHelixTaskResourceManager();
+    _pinotTaskManager = _controllerStarter.getTaskManager();
 
-    // Initialize a PinotTaskManager
-    TaskDriver taskDriver = new TaskDriver(_pinotHelixResourceManager.getHelixZkManager());
-    _pinotHelixTaskResourceManager = new PinotHelixTaskResourceManager(taskDriver);
-    _pinotTaskManager = new PinotTaskManager(taskDriver, _pinotHelixResourceManager, _pinotHelixTaskResourceManager);
+    // Register the test task generator into task manager
     _pinotTaskManager.registerTaskGenerator(new TestTaskGenerator(_pinotTaskManager.getClusterInfoProvider()));
     _pinotTaskManager.ensureTaskQueuesExist();
 
-    // Initialize minion starters.
-    for (int i = 0; i < NUM_WORKERS; i++) {
-      Configuration config = new PropertiesConfiguration();
-      config.setProperty(CommonConstants.Helix.Instance.INSTANCE_ID_KEY, "minion" + i);
-      MinionStarter minionStarter = new MinionStarter(ZK_ADDRESS, HELIX_CLUSTER_NAME, config);
-      minionStarter.registerTaskExecutorClass(TestTaskGenerator.TASK_TYPE, TestTaskExecutor.class);
-      minionStarter.start();
-      _minionStarters[i] = minionStarter;
-    }
+    Map<String, Class<? extends PinotTaskExecutor>> taskExecutorsToRegister = new HashMap<>(1);
+    taskExecutorsToRegister.put(TestTaskGenerator.TASK_TYPE, TestTaskExecutor.class);
+    startMinions(NUM_WORKERS, taskExecutorsToRegister);
   }
 
   @Test
-  public void testStopAndResumeJobQueue()
+  public void testStopAndResumeTaskQueue()
       throws Exception {
     // Generate 4 tasks
     _pinotTaskManager.execute();
@@ -95,11 +78,12 @@ public class MinionClusterIntegrationTest {
 
     // Wait at most 60 seconds for all tasks showing up in the cluster
     long endTime = System.currentTimeMillis() + 60_000L;
-    while ((System.currentTimeMillis() < endTime) && (
-        _pinotHelixTaskResourceManager.getTaskStates(TestTaskGenerator.TASK_TYPE).size() != 4)) {
+    while ((System.currentTimeMillis() < endTime)
+        && (_pinotHelixTaskResourceManager.getTaskStates(TestTaskGenerator.TASK_TYPE).size() != 4)) {
       Thread.sleep(100L);
     }
-    Assert.assertEquals(_pinotHelixTaskResourceManager.getTaskStates(TestTaskGenerator.TASK_TYPE).size(), 4);
+    Assert.assertEquals(_pinotHelixTaskResourceManager.getTaskStates(TestTaskGenerator.TASK_TYPE).size(), 4,
+        "Not all tasks showed up within 60 seconds");
 
     // Should not generate more tasks
     _pinotTaskManager.execute();
@@ -108,13 +92,13 @@ public class MinionClusterIntegrationTest {
     Map<String, TaskState> taskStates = _pinotHelixTaskResourceManager.getTaskStates(TestTaskGenerator.TASK_TYPE);
     Assert.assertEquals(taskStates.size(), 4);
     for (TaskState taskState : taskStates.values()) {
-      Assert.assertTrue(taskState.equals(TaskState.IN_PROGRESS));
+      Assert.assertEquals(taskState, TaskState.IN_PROGRESS);
     }
 
-    // Stop the job queue
+    // Stop the task queue
     _pinotHelixTaskResourceManager.stopTaskQueue(TestTaskGenerator.TASK_TYPE);
 
-    // Wait at most 60 seconds for all tasks stopped
+    // Wait at most 60 seconds for all tasks STOPPED
     endTime = System.currentTimeMillis() + 60_000L;
     int stoppedTaskCount = 0;
     while ((System.currentTimeMillis() < endTime) && (stoppedTaskCount != 4)) {
@@ -122,42 +106,63 @@ public class MinionClusterIntegrationTest {
       stoppedTaskCount = 0;
       taskStates = _pinotHelixTaskResourceManager.getTaskStates(TestTaskGenerator.TASK_TYPE);
       for (TaskState taskState : taskStates.values()) {
-        if (taskState.equals(TaskState.STOPPED)) {
+        if (taskState == TaskState.STOPPED) {
           stoppedTaskCount++;
         }
       }
     }
-    Assert.assertEquals(stoppedTaskCount, 4);
+    Assert.assertEquals(stoppedTaskCount, 4, "Not all tasks STOPPED within 60 seconds");
 
-    // Resume the job queue
+    // Resume the task queue
     _pinotHelixTaskResourceManager.resumeTaskQueue(TestTaskGenerator.TASK_TYPE);
 
-    // Wait at most 60 seconds for all tasks completed
+    // Wait at most 60 seconds for all tasks COMPLETED
+    endTime = System.currentTimeMillis() + 60_000L;
     int completedTaskCount = 0;
-    while (completedTaskCount != 4) {
+    while ((System.currentTimeMillis() < endTime) && (completedTaskCount != 4)) {
       Thread.sleep(100L);
       completedTaskCount = 0;
       taskStates = _pinotHelixTaskResourceManager.getTaskStates(TestTaskGenerator.TASK_TYPE);
       for (TaskState taskState : taskStates.values()) {
-        if (taskState.equals(TaskState.COMPLETED)) {
+        if (taskState == TaskState.COMPLETED) {
           completedTaskCount++;
         }
       }
     }
-    Assert.assertEquals(completedTaskCount, 4);
+    Assert.assertEquals(completedTaskCount, 4, "Not all tasks COMPLETED within 60 seconds");
 
-    // Delete the job queue
+    // Delete the task queue
     _pinotHelixTaskResourceManager.deleteTaskQueue(TestTaskGenerator.TASK_TYPE);
+  }
+
+  @Test
+  public void testPinotHelixResourceManagerAPIs() {
+    // Instance APIs
+    Assert.assertEquals(_pinotHelixResourceManager.getAllInstances().size(), 6);
+    Assert.assertEquals(_pinotHelixResourceManager.getOnlineInstanceList().size(), 6);
+    Assert.assertEquals(_pinotHelixResourceManager.getOnlineUnTaggedBrokerInstanceList().size(), 0);
+    Assert.assertEquals(_pinotHelixResourceManager.getOnlineUnTaggedServerInstanceList().size(), 0);
+
+    // Table APIs
+    List<String> tableNames = _pinotHelixResourceManager.getAllTables();
+    Assert.assertEquals(tableNames.size(), 2);
+    Assert.assertTrue(tableNames.contains(OFFLINE_TABLE_NAME));
+    Assert.assertTrue(tableNames.contains(REALTIME_TABLE_NAME));
+    Assert.assertEquals(_pinotHelixResourceManager.getAllRawTables(), Collections.singletonList(RAW_TABLE_NAME));
+    Assert.assertEquals(_pinotHelixResourceManager.getAllRealtimeTables(),
+        Collections.singletonList(REALTIME_TABLE_NAME));
+
+    // Tenant APIs
+    Assert.assertEquals(_pinotHelixResourceManager.getAllBrokerTenantNames(), Collections.singleton("TestTenant"));
+    Assert.assertEquals(_pinotHelixResourceManager.getAllServerTenantNames(), Collections.singleton("TestTenant"));
   }
 
   @AfterClass
   public void tearDown()
       throws Exception {
-    for (MinionStarter minionStarter : _minionStarters) {
-      minionStarter.stop();
-    }
-    _pinotHelixResourceManager.stop();
-    ZkStarter.stopLocalZkServer(_zookeeperInstance);
+    stopMinion();
+
+    super.tearDown();
   }
 
   private static class TestTaskGenerator implements PinotTaskGenerator {
@@ -196,7 +201,7 @@ public class MinionClusterIntegrationTest {
   public static class TestTaskExecutor extends BaseTaskExecutor {
     @Override
     public void executeTask(@Nonnull PinotTaskConfig pinotTaskConfig) {
-      Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
+      Uninterruptibles.sleepUninterruptibly(5, TimeUnit.SECONDS);
 
       if (_cancelled) {
         throw new TaskCancelledException("Task has been cancelled");
