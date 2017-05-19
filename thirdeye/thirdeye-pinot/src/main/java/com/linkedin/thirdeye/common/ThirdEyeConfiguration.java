520c46e6536f091edc7946c6fa4fb102bb7ae877
Yen-Jung Chang <ychang@linkedin.com>
2017-05-18 18:02:19 -0700

    Refine recipient provider factory for external providers

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/common/ThirdEyeConfiguration.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/common/ThirdEyeConfiguration.java
@@ -73,6 +73,10 @@ public abstract class ThirdEyeConfiguration extends Configuration {
     return getRootDir() + "/detector-config/anomaly-functions/alertFilterAutotune.properties";
   }
 
+  public String getAlertGroupRecipientProviderConfigPath() {
+    return getRootDir() + "/detector-config/anomaly-functions/alertGroupRecipientProvider.properties";
+  }
+
   public String getSmtpHost() {
     return smtpHost;
   }
--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomaly/alert/grouping/recipientprovider/AlertGroupRecipientProviderFactoryTest.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomaly/alert/grouping/recipientprovider/AlertGroupRecipientProviderFactoryTest.java
@@ -8,15 +8,17 @@ import org.testng.annotations.Test;
 public class AlertGroupRecipientProviderFactoryTest {
   @Test
   public void testFromSpecNull() throws Exception {
-    AlertGroupRecipientProvider recipientProvider = AlertGroupRecipientProviderFactory.fromSpec(null);
+    AlertGroupRecipientProviderFactory alertGroupRecipientProviderFactory = new AlertGroupRecipientProviderFactory();
+    AlertGroupRecipientProvider recipientProvider = alertGroupRecipientProviderFactory.fromSpec(null);
     Assert.assertEquals(recipientProvider.getClass(), DummyAlertGroupRecipientProvider.class);
   }
 
   @Test
-  public void testAlertGrouperCreation() {
+  public void testDimensionalAlertGroupRecipientProviderCreation() {
+    AlertGroupRecipientProviderFactory alertGroupRecipientProviderFactory = new AlertGroupRecipientProviderFactory();
     Map<String, String> spec = new HashMap<>();
     spec.put(AlertGroupRecipientProviderFactory.GROUP_RECIPIENT_PROVIDER_TYPE_KEY, "diMenSionAL");
-    AlertGroupRecipientProvider alertGrouper = AlertGroupRecipientProviderFactory.fromSpec(spec);
+    AlertGroupRecipientProvider alertGrouper = alertGroupRecipientProviderFactory.fromSpec(spec);
     Assert.assertEquals(alertGrouper.getClass(), DimensionalAlertGroupRecipientProvider.class);
   }
 }
