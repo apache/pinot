37083221ed5708f13fa77dd66487365943f3f1c2
Jennifer Dai <jdai@linkedin.com>
2017-05-22 17:01:03 -0700

    Returning null to fix LLC segment commit when there is no leader

--- pinot-core/src/main/java/com/linkedin/pinot/server/realtime/ControllerLeaderLocator.java
+++ pinot-core/src/main/java/com/linkedin/pinot/server/realtime/ControllerLeaderLocator.java
@@ -80,12 +80,17 @@ public class ControllerLeaderLocator {
 
     BaseDataAccessor<ZNRecord> dataAccessor = _helixManager.getHelixDataAccessor().getBaseDataAccessor();
     Stat stat = new Stat();
-    ZNRecord znRecord = dataAccessor.get("/" + _clusterName + "/CONTROLLER/LEADER", stat, AccessOption.THROW_EXCEPTION_IFNOTEXIST);
-    String leader = znRecord.getId();
-    int index = leader.lastIndexOf('_');
-    _controllerLeaderHostPort = leader.substring(0, index) + ":" + leader.substring(index + 1);
-    _refresh = false;
-    return _controllerLeaderHostPort;
+    try {
+      ZNRecord znRecord = dataAccessor.get("/" + _clusterName + "/CONTROLLER/LEADER", stat, AccessOption.THROW_EXCEPTION_IFNOTEXIST);
+      String leader = znRecord.getId();
+      int index = leader.lastIndexOf('_');
+      _controllerLeaderHostPort = leader.substring(0, index) + ":" + leader.substring(index + 1);
+      _refresh = false;
+      return _controllerLeaderHostPort;
+    } catch (Exception e) {
+      LOGGER.error("Could not locate controller leader, exception " + e);
+      return null;
+    }
   }
 
   public synchronized void refreshControllerLeader() {
