487f149cd1fdda1c104a519ce30ca6f6b8476f89
Kexin Nie <knie@knie-ld1.linkedin.biz>
2017-06-13 11:39:59 -0700

    Change the logic to take field type rather than value type when set parameter

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detector/email/filter/BaseAlertFilter.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detector/email/filter/BaseAlertFilter.java
@@ -2,23 +2,25 @@ package com.linkedin.thirdeye.detector.email.filter;
 
 import java.lang.reflect.Field;
 import java.util.Map;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.swing.StringUIClientPropertyKey;
 
 
 public abstract class BaseAlertFilter implements AlertFilter {
   private final static Logger LOG = LoggerFactory.getLogger(BaseAlertFilter.class);
 
-  // Check if the given string can be parsed as double value
-  public static boolean isNumeric(String str) {
-    try {
-      double d = Double.parseDouble(str);
-    }
-    catch(NumberFormatException nfe) {
-      return false;
-    }
-    return true;
-  }
+//  // Check if the given string can be parsed as double value
+//  public static boolean isNumeric(String str) {
+//    try {
+//      double d = Double.parseDouble(str);
+//    }
+//    catch(NumberFormatException nfe) {
+//      return false;
+//    }
+//    return true;
+//  }
 
 
   /**
@@ -37,14 +39,12 @@ public abstract class BaseAlertFilter implements AlertFilter {
     for (String fieldName : getPropertyNames()) {
       Double value = null;
       String strVal = null;
-      boolean isNumeric = true;
       // Get user's value for the specified field
       if (parameterSetting.containsKey(fieldName)) {
         String fieldVal = parameterSetting.get(fieldName);
-        if (isNumeric(fieldVal)) {
+        if (StringUtils.isNumeric(fieldVal)) {
           value = Double.parseDouble(parameterSetting.get(fieldName));
         } else {
-          isNumeric = false;
           strVal = fieldVal;
         }
       } else {
@@ -53,14 +53,19 @@ public abstract class BaseAlertFilter implements AlertFilter {
           Field field = c.getDeclaredField("DEFAULT_" + fieldName.toUpperCase());
           boolean accessible = field.isAccessible();
           field.setAccessible(true);
-          value = Double.parseDouble((String) field.get(this));
+          Object object = field.get(this);
+          if (object instanceof Double)  {
+            value = (Double) object;
+          } else {
+            strVal = object.toString();
+          }
           field.setAccessible(accessible);
         } catch (NoSuchFieldException | IllegalAccessException e) {
           LOG.error("Failed to get default value for field {} of class {}; exception: {}", "DEFAULT_" + fieldName,
               c.getSimpleName(), e.toString());
         }
         // If failed to get the default value from Class definition, then use value 0d
-        if (value == null) {
+        if (value == null && strVal == null) {
           value = 0d;
         }
         LOG.warn("Unable to read the setting for the field {} of class {}; the value {} is used.", fieldName,
@@ -69,10 +74,11 @@ public abstract class BaseAlertFilter implements AlertFilter {
       // Set the final value to the specified field
       try {
         Field field = c.getDeclaredField(fieldName);
+        Object object = field.get(this);
         boolean accessible = field.isAccessible();
         field.setAccessible(true);
-        if (isNumeric) {
-          field.set(this, Double.valueOf(value));
+        if (object instanceof Double) {
+          field.set(this, value);
         }
         else {
           field.set(this, strVal);
