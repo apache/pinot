6f5915a1b7f7b489a1d207a09b6b981b925b4fcd
Steve McClung <smcclung@linkedin.com>
2017-06-15 12:22:51 -0700

    TE nav and related components

--- thirdeye/thirdeye-frontend/app/pods/self-service/route.js
+++ thirdeye/thirdeye-frontend/app/pods/self-service/route.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel() {
-    this.replaceWith('self-service.manage');
+  redirect: function () {
+    this.transitionTo('self-service.manage');
   }
 });
