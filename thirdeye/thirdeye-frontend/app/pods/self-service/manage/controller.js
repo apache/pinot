58ed50b31b33a38a5fa9dfce2de2f672abfc08b3
Yves Yuen <yyuen@linkedin.com>
2017-06-16 14:51:07 -0700

    functional basic type ahead search

---
+++ thirdeye/thirdeye-frontend/app/pods/self-service/manage/controller.js
@@ -0,0 +1,59 @@
+import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
+import fetch from 'fetch';
+
+export default Ember.Controller.extend({
+  /**
+   * Alerts Search Mode
+   */
+  searchModes: ['function-name', 'alert-group'], 
+
+  /**
+   * Default Search Mode
+   */
+  selectedSearchMode: 'function-name',
+
+  /**
+   * Array of Alerts we're displaying
+   */
+  selectedAlerts: [],
+  
+
+  /**
+   * Handler for serach by function name
+   * Utilizing ember concurrency (task)
+   */
+  searchByFunctionName: task(function* (alert) {
+    yield timeout(600);
+    const url = `/data/autocomplete/functionByName?name=${alert}`;
+    return fetch(url)
+      .then(res => res.json()) 
+  }),
+
+  /**
+   * Handler for serach by alert group name
+   * Utilizing ember concurrency (task)
+   */
+  searchByAlertGroup: task(function* (alert) {
+    yield timeout(600);
+    const url = `/data/autocomplete/functionByAlertName?alertName=${alert}`;
+    return fetch(url)
+      .then(res => res.json()) 
+  }),
+
+  actions: {
+    /**
+     * Handles alert selection from type ahead
+     */
+    onAlertChange(alerts) {
+      this.set('selectedAlerts', [alerts]);
+    },
+    
+    /**
+     * Handles UI mode change
+     */
+    onModeChange(mode) {
+      this.set('selectedSearchMode', mode);
+    },
+  }
+});
