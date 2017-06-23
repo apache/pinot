6fbba8b20e9850dd5fb7a1b3abeb809ab3c33a07
Steve McClung <smcclung@linkedin.com>
2017-06-23 13:53:47 -0700

    TE Create Alert

--- thirdeye/thirdeye-frontend/app/pods/self-serve/create-alert/controller.js
+++ thirdeye/thirdeye-frontend/app/pods/self-serve/create-alert/controller.js
@@ -4,8 +4,38 @@
  * @exports create
  */
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
+import fetch from 'fetch';
 
 export default Ember.Controller.extend({
+  /**
+   * Array of metrics we're displaying
+   */
+  selectedMetrics: [],
+
+  /**
+   * Handler for search by function name
+   * Utilizing ember concurrency (task)
+   */
+  searchMetricsList: task(function* (metric) {
+    yield timeout(600);
+    const url = `/data/autocomplete/metric?name=${metric}`;
+    return fetch(url)
+      .then(res => res.json())
+  }),
+
+  triggerGraphLoad(metricId) {
+    console.log('got metric id here: ', metricId);
+    console.log('fetching data : ', this.fetchAnomalyGraphData(metricId));
+  },
+
+  fetchAnomalyGraphData(metricId) {
+    const url = '/timeseries/compare/${metricId}/1497596399999/1498147199999/1496991599999/1497542399999?dimension=All&granularity=HOURS&filters={}';
+    return fetch(url)
+      .then(res => res.json())
+  },
+
+
   /**
    * Placeholder for patterns of interest options
    */
@@ -25,6 +55,21 @@ export default Ember.Controller.extend({
    * Actions for create alert form view
    */
   actions: {
+    /**
+     * Function called when the dropdown value is updated
+     * @method onChangeDropdown
+     * @param {Object} selectedObj - If has dataset, this is the selected value from dropdown
+     * @return {undefined}
+     */
+    onChangeDropdown(selectedObj) {
+      if (selectedObj) {
+        this.set('selectorVal', selectedObj);
+        this.triggerGraphLoad(selectedObj);
+      } else {
+        this.set('selectorVal', '');
+      }
+    },
+
     submit(data) {
       console.log(data);
     }
