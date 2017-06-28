5d821d1c3e16a88e0545bcc4076bc27edc6b4813
Steve McClung <smcclung@linkedin.com>
2017-06-28 10:59:24 -0700

    TE Create Alert Iteration: initial validation

--- thirdeye/thirdeye-frontend/app/pods/self-serve/create-alert/controller.js
+++ thirdeye/thirdeye-frontend/app/pods/self-serve/create-alert/controller.js
@@ -5,16 +5,21 @@
  */
 import Ember from 'ember';
 import { task, timeout } from 'ember-concurrency';
+import moment from 'moment';
 import fetch from 'fetch';
 
 export default Ember.Controller.extend({
   /**
    * Array of metrics we're displaying
    */
-  selectedMetric: {},
-  selectedMetricDimensions: [],
-
   isMetricSelected: false,
+  isValidated: false,
+  showAlertGroupEdit: true,
+
+  init() {
+    this._super(...arguments);
+    this.set('isSubmitDisabled', true);
+  },
 
   /**
    * Handler for search by function name
@@ -32,47 +37,109 @@ export default Ember.Controller.extend({
    * Utilizing ember concurrency (task)
    */
   fetchMetricDimensions(metricId) {
-    const url = 'data/autocomplete/dimensions/metric/' + metricId;
+    const url = `data/autocomplete/dimensions/metric/${metricId}`;
     return fetch(url)
       .then(res => res.json())
   },
 
-  fetchAnomalyGraphData(metricId, dimension) {
-    const url = '/timeseries/compare/' + metricId + '/1492564800000/1492593000000/1491355200000/1491383400000?dimension=' + dimension + '&granularity=MINUTES&filters={}';
+  fetchMetricMaxTime(metricId) {
+    const url = `/data/maxDataTime/metricId/${metricId}`;
     return fetch(url)
       .then(res => res.json())
   },
 
-  triggerGraphLoad(metric, dimension) {
-    let activeDimension = dimension || 'All';
-    let metricId = metric.id || metric.metricId;
-    console.log('got metric id here: ', metricId);
-    this.fetchAnomalyGraphData(metricId, activeDimension).then(metricData => {
+  fetchAnomalyGraphData(config) {
+    console.log(config);
+    const url = `/timeseries/compare/${config.id}/${config.currentStart}/${config.maxTime}/${config.baselineStart}/${config.baselineEnd}?dimension=${config.dimension}&granularity=MINUTES&filters={}`;
+    return fetch(url)
+      .then(res => res.json())
+  },
+
+  triggerGraphFromMetric(metric) {
+    this.fetchMetricMaxTime(metric.id, 'All').then(maxTime => {
+      console.log('maxTime: ', maxTime);
+      const fixedMaxTime = maxTime || moment().subtract(1,'day').endOf('day').valueOf();
+      const currentStart = moment(fixedMaxTime).subtract(3, 'days');
+      const baselineStart = currentStart.clone().subtract(1, 'week');
+      this.graphConfig = {
+        id: metric.id,
+        dimension: 'All',
+        maxTime: fixedMaxTime,
+        currentStart: currentStart.valueOf(),
+        baselineStart: baselineStart.valueOf(),
+        baselineEnd: moment(fixedMaxTime).subtract(1, 'week').valueOf()
+      };
+      this.fetchAnomalyGraphData(this.graphConfig).then(metricData => {
+        this.set('isMetricSelected', true);
+        this.set('selectedMetric', metricData);
+      });
+    });
+  },
+
+  triggerGraphFromDimensions(dimension) {
+    this.graphConfig.dimension = dimension;
+    this.fetchAnomalyGraphData(this.graphConfig).then(metricData => {
       this.set('isMetricSelected', true);
       this.set('selectedMetric', metricData);
-      if (!dimension) {
-        this.fetchMetricDimensions(metricId).then(dimensionData => {
-          this.set('selectedMetricDimensions', dimensionData);
-        });
-      }
     });
+  },
+
+  loadDimensionOptions(metric) {
+    this.fetchMetricDimensions(metric.id).then(dimensionData => {
+      this.set('selectedMetricDimensions', dimensionData || []);
+    });
+  },
+
+  prepareFunctions(configGroup) {
+    const newFunctionList = [];
+    const existingFunctionList = configGroup.emailConfig ? configGroup.emailConfig.functionIds : [];
+    for (var functionId of existingFunctionList) {
+      console.log(functionId);
+      newFunctionList.push({
+        id: functionId,
+        name: 'some name'
+      });
+    }
+    return newFunctionList;
+  },
+
+  saveThirdEyeEntity(alertData, entityType) {
+    const postProps = {
+      method: 'post',
+      body: JSON.stringify(alertData),
+      headers: { 'content-type': 'Application/Json' }
+    };
+    const url = '/thirdeye/entity?entityType=' + entityType;
+    return fetch(url, postProps);
+  },
+
+  saveThirdEyeFunction(functionData) {
+    const postProps = {
+      method: 'post',
+      headers: { 'content-type': 'Application/Json' }
+    };
+    const url = '/dashboard/anomaly-function/create?' + $.param(functionData);
+    return fetch(url, postProps)
+      .then(res => res.json());
+  },
+
+  checkRequiredValues() {
+    if (this.get('selectedMetricOption') && this.get('selectedPattern') && this.get('alertFunctionName') && this.get('alertGroupNewRecipient')) {
+      return false;
+    } else {
+      return true;
+    }
   },
 
   /**
    * Placeholder for patterns of interest options
    */
-  patternsOfInterest: ['Pattern One', 'Pattern Two'],
-  selectedPattern: 'Pattern One',
-  /**
-   * Placeholder for dimensions options
-   */
-  dimensionExploration: ['First', 'Second', 'Third'],
-  selectedDimension: 'Select One',
+  patternsOfInterest: ['None', 'Up', 'Down', 'Either'],
   /**
    * Placeholder for alert groups options
    */
   allAlertsConfigGroups: Ember.computed.reads('model.allAlertsConfigGroups'),
-  selectedGroup: 'Select an alert group, or create new',
+
   /**
    * Actions for create alert form view
    */
@@ -83,28 +150,103 @@ export default Ember.Controller.extend({
      * @param {Object} selectedObj - If has dataset, this is the selected value from dropdown
      * @return {undefined}
      */
-    onChangeDropdown(selectedObj) {
+    onSelectMetric(selectedObj) {
+      console.log(selectedObj);
+      this.set('selectedMetricOption', selectedObj);
+      this.triggerGraphFromMetric(selectedObj);
+      this.loadDimensionOptions(selectedObj);
+    },
+
+    onSelectDimension(selectedObj) {
+      this.set('dimensionSelectorVal', selectedObj);
+      this.triggerGraphFromDimensions(selectedObj);
+    },
+
+    onSelectPattern(selectedObj) {
+      this.set('selectedPattern', selectedObj);
+    },
+
+    onSelectConfigGroup(selectedObj) {
       if (selectedObj) {
-        this.set('selectorVal', selectedObj);
-        this.triggerGraphLoad(selectedObj, null);
+        this.set('selectedConfigGroup', selectedObj);
+        this.set('selectedGroupRecipients', selectedObj.recipients);
+        this.set('selectedGroupActive', selectedObj.active);
+        this.set('selectedGroupFunctions', this.prepareFunctions(selectedObj));
+        this.set('showAlertGroupEdit', true);
       } else {
-        this.set('selectorVal', '');
+        this.set('configSelectorVal', '');
       }
     },
 
-    onChangeDimension(selectedObj) {
-      let currMetric = this.get('selectedMetric');
-      console.log('currMetric: ', currMetric.metricId);
-      if (selectedObj) {
-        this.set('dimensionSelectorVal', selectedObj);
-        this.triggerGraphLoad(currMetric, selectedObj);
-      } else {
-        this.set('dimensionSelectorVal', '');
-      }
+    onClickChangeGroupEditMode() {
+      this.toggleProperty('showAlertGroupEdit');
     },
 
-    submit(data) {
-      console.log(data);
+    validateRequiredFields() {
+      console.log('validating: ', this.checkRequiredValues());
+      this.set('isSubmitDisabled', this.checkRequiredValues());
+    },
+
+    /**
+     * User hit submit. Buckle up - we're going for a ride! What we have to do here is:
+     * 1. Make sure all fields are validated
+     * 2. Send a new 'alert function' create request, which should return a new function ID
+     * 3. Add this Id to the 'Alert Config Group' for notifications
+     * 4. Send a Edit or Create request for the Alert Config Group based on user's choice
+     * 5. Notify user of result
+     */
+    submit() {
+      // This object contains the data for the new config group
+      const newConfigObj = {
+        name: this.get('createGroupName'),
+        active: this.get('createGroupActive') || false,
+        emailConfig: { "functionIds": [] },
+        recipients: this.get('alertGroupNewRecipient')
+      };
+      // This object contains the data for the new alert function, with default fillers
+      const newFunctionObj = {
+        functionName: this.get('alertFunctionName'),
+        metric: this.get('selectedMetricOption').name,
+        dataset: this.get('selectedMetricOption').dataset,
+        metricFunction: 'SUM',
+        type: 'SIGN_TEST_VANILLA',
+        windowSize: 6,
+        windowUnit: 'HOURS',
+        isActive: false,
+        properties: 'signTestWindowSize=24;anomalyRemovalWeightThreshold=0.6;signTestPattern=' + this.get('selectedPattern') + 'UP;pValueThreshold=0.01;signTestBaselineShift=0.0;signTestBaselineLift=0.90;baseline=w/4wAvg;decayRate=0.5;signTestStepSize=1'
+      };
+
+      // If these two conditions are true, we assume the user wants to edit an existing alert group
+      const isAlertGroupEditModeActive = this.get('showAlertGroupEdit') && this.selectedConfigGroup;
+      // A reference to whichever 'alert config' object will be sent. Let's default to the new one
+      let finalConfigObj = newConfigObj;
+
+      // First, save our new alert function
+      this.saveThirdEyeFunction(newFunctionObj).then(functionResult => {
+        console.log('saving new function', newFunctionObj);
+        // Add our new Alert Function Id to the Alert Config Object
+        if (Ember.typeOf(functionResult) === 'number') {
+          finalConfigObj.emailConfig.functionIds.push(functionResult);
+        }
+
+        // Add new email recipients if we are dealing with an existing Alert Group
+        if (isAlertGroupEditModeActive) {
+          let recipientsArr = this.selectedConfigGroup.recipients.split(',');
+          recipientsArr.push(this.alertGroupNewRecipient);
+          this.selectedConfigGroup.recipients = recipientsArr.join();
+          finalConfigObj = this.selectedConfigGroup;
+        }
+
+        // Finally, save our Alert Config Group
+        this.saveThirdEyeEntity(finalConfigObj, 'ALERT_CONFIG').then(alertResult => {
+          console.log('saving alert config', newFunctionObj);
+          if (alertResult.ok) {
+            this.set('selectedGroupRecipients', finalConfigObj.recipients);
+            this.set('isCreateSuccess', true);
+            this.set('finalFunctionId', functionResult);
+          }
+        });
+      });
     }
   }
 });
