45511ff464994017a02e15ce2297ccd18ada074f
Neha Pawar <npawar@linkedin.com>
2017-08-01 16:50:16 -0700

    Fix code style issues

--- thirdeye/thirdeye-hadoop/src/main/java/com/linkedin/thirdeye/hadoop/push/SegmentPushControllerAPIs.java
+++ thirdeye/thirdeye-hadoop/src/main/java/com/linkedin/thirdeye/hadoop/push/SegmentPushControllerAPIs.java
@@ -222,4 +222,5 @@ public class SegmentPushControllerAPIs {
     return deleteSuccessful;
   }
 
+
 }
--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/auto/onboard/AutoOnboardPinotMetricsUtils.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/auto/onboard/AutoOnboardPinotMetricsUtils.java
@@ -134,5 +134,4 @@ public class AutoOnboardPinotMetricsUtils {
     }
     return isSchemaCorrect;
   }
-
 }
