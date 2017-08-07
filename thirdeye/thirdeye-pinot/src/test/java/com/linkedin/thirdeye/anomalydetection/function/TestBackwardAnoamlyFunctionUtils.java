16ff4d6b7875d2c3feb398a228410d3de95838b4
Yung-Yu Chung <ychung@linkedin.com>
2017-08-07 14:32:22 -0700

    Modify the test case to larger time latency

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomalydetection/function/TestBackwardAnoamlyFunctionUtils.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomalydetection/function/TestBackwardAnoamlyFunctionUtils.java
@@ -7,6 +7,7 @@ import com.linkedin.thirdeye.api.MetricType;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.joda.time.Interval;
 import org.junit.Test;
 
@@ -21,15 +22,15 @@ public class TestBackwardAnoamlyFunctionUtils {
         new MetricSchema(metricNames, Collections.nCopies(metricNames.size(), MetricType.DOUBLE));
     MetricTimeSeries metricTimeSeries = new MetricTimeSeries(metricSchema);
     metricTimeSeries.set(0, TEST_METRIC, 0);
-    metricTimeSeries.set(1, TEST_METRIC, 1);
-    metricTimeSeries.set(2, TEST_METRIC, 2);
-    metricTimeSeries.set(3, TEST_METRIC, 3);
-    metricTimeSeries.set(4, TEST_METRIC, 4);
-    metricTimeSeries.set(5, TEST_METRIC, 5);
+    metricTimeSeries.set(TimeUnit.DAYS.toMillis(28), TEST_METRIC, 1);
+    metricTimeSeries.set(TimeUnit.DAYS.toMillis(56), TEST_METRIC, 2);
+    metricTimeSeries.set(TimeUnit.DAYS.toMillis(84), TEST_METRIC, 3);
+    metricTimeSeries.set(TimeUnit.DAYS.toMillis(112), TEST_METRIC, 4);
+    metricTimeSeries.set(TimeUnit.DAYS.toMillis(140), TEST_METRIC, 5);
 
     List<Interval> intervalList = new ArrayList<>();
-    intervalList.add(new Interval(5, 6));
-    intervalList.add(new Interval(0, 5));
+    intervalList.add(new Interval(TimeUnit.DAYS.toMillis(140), TimeUnit.DAYS.toMillis(168)));
+    intervalList.add(new Interval(0, TimeUnit.DAYS.toMillis(140)));
     List<TimeSeries> timeSeriesList =
         BackwardAnomalyFunctionUtils.splitSetsOfTimeSeries(metricTimeSeries, TEST_METRIC, intervalList);
     assert(timeSeriesList.size() == 2);
@@ -37,10 +38,10 @@ public class TestBackwardAnoamlyFunctionUtils {
     assert(timeSeriesList.get(1).size() == 5);
 
     // assert current time series
-    assert(timeSeriesList.get(0).get(5).equals(5.0));
+    assert(timeSeriesList.get(0).get(TimeUnit.DAYS.toMillis(140)).equals(5.0));
 
     for (int i = 0; i < 5; i++) {
-      assert (timeSeriesList.get(1).get(i).equals(Double.valueOf(i)));
+      assert (timeSeriesList.get(1).get(TimeUnit.DAYS.toMillis(28 * i)).equals(Double.valueOf(i)));
     }
   }
 }
