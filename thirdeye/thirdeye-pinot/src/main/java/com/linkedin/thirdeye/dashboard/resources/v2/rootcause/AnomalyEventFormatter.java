360d80ed0073de5f802dee210f74e52f7bd36467
Alexander Pucher <apucher@linkedin.com>
2017-08-09 17:29:15 -0700

    add related entities and filter dimensions to label

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/rootcause/AnomalyEventFormatter.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/rootcause/AnomalyEventFormatter.java
@@ -3,13 +3,20 @@ package com.linkedin.thirdeye.dashboard.resources.v2.rootcause;
 import com.linkedin.thirdeye.dashboard.resources.v2.RootCauseEventEntityFormatter;
 import com.linkedin.thirdeye.dashboard.resources.v2.pojo.RootCauseEventEntity;
 import com.linkedin.thirdeye.datalayer.bao.MetricConfigManager;
+import com.linkedin.thirdeye.datalayer.dto.AnomalyFunctionDTO;
 import com.linkedin.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
 import com.linkedin.thirdeye.datasource.DAORegistry;
 import com.linkedin.thirdeye.rootcause.impl.AnomalyEventEntity;
+import com.linkedin.thirdeye.rootcause.impl.DimensionEntity;
 import com.linkedin.thirdeye.rootcause.impl.EventEntity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.lang.StringUtils;
 
 
 public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
+  private static final DimensionEntityFormatter DIMENSION_FORMATTER = new DimensionEntityFormatter();
   private final static MetricEntityFormatter METRIC_FORMATTER = new MetricEntityFormatter();
 
   private final MetricConfigManager metricDAO;
@@ -32,10 +39,25 @@ public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
     AnomalyEventEntity e = (AnomalyEventEntity) entity;
 
     MergedAnomalyResultDTO dto = e.getDto();
-    String label = String.format("Anomaly %d (%s)", dto.getId(), dto.getFunction().getFunctionName());
+    AnomalyFunctionDTO func = dto.getFunction();
 
-    // TODO add filters/dimensions as related entities
+    List<String> dimensions = new ArrayList<>();
+    for (Map.Entry<String, String> entry : dto.getDimensions().entrySet()) {
+      dimensions.add(entry.getKey() + ":" + entry.getValue().toUpperCase());
+    }
 
-    return makeRootCauseEventEntity(entity, label, null, dto.getStartTime(), dto.getEndTime(), null);
+    String label = String.format("%s (%s)", func.getFunctionName(), StringUtils.join(dimensions, ", "));
+    String link = String.format("thirdeye#investigate?anomalyId=%d", dto.getId());
+
+    RootCauseEventEntity out = makeRootCauseEventEntity(entity, label, link, dto.getStartTime(), dto.getEndTime(), null);
+
+    for(Map.Entry<String, String> entry : dto.getDimensions().entrySet()) {
+      DimensionEntity de = DimensionEntity.fromDimension(entity.getScore(), entry.getKey(), entry.getValue());
+      out.addRelatedEntity(DIMENSION_FORMATTER.format(de));
+    }
+
+    // TODO add metrics as related entites
+
+    return out;
   }
 }
