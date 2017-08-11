ae92cb94a872d4b26cf179c306b082ecc4d3ce35
Alexander Pucher <apucher@linkedin.com>
2017-08-11 11:38:39 -0700

    improve metric dataset pipeline logic

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/rootcause/impl/MetricDatasetPipeline.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/rootcause/impl/MetricDatasetPipeline.java
@@ -19,6 +19,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections.MapUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,22 +33,30 @@ import org.slf4j.LoggerFactory;
 public class MetricDatasetPipeline extends Pipeline {
   private static final Logger LOG = LoggerFactory.getLogger(MetricDatasetPipeline.class);
 
-  final MetricConfigManager metricDAO;
-  final DatasetConfigManager datasetDAO;
+  private static final String PROP_COEFFICIENT = "coefficient";
+  private static final double PROP_COEFFICIENT_DEFAULT = 0.8;
+  public static final String META_METRIC_COUNT = "__COUNT";
+
+  private final MetricConfigManager metricDAO;
+  private final DatasetConfigManager datasetDAO;
+
+  private final double coefficient;
 
   /**
    * Constructor for dependency injection
    *
    * @param outputName pipeline output name
    * @param inputNames input pipeline names
+   * @param coefficient coefficient for scoring dataset metrics
    * @param metricDAO metric config DAO
    * @param datasetDAO dataset config DAO
    */
-  public MetricDatasetPipeline(String outputName, Set<String> inputNames, MetricConfigManager metricDAO,
+  public MetricDatasetPipeline(String outputName, Set<String> inputNames, double coefficient, MetricConfigManager metricDAO,
       DatasetConfigManager datasetDAO) {
     super(outputName, inputNames);
     this.metricDAO = metricDAO;
     this.datasetDAO = datasetDAO;
+    this.coefficient = coefficient;
   }
 
   /**
@@ -55,12 +64,13 @@ public class MetricDatasetPipeline extends Pipeline {
    *
    * @param outputName pipeline output name
    * @param inputNames input pipeline names
-   * @param ignore configuration properties (none)
+   * @param properties configuration properties ({@code PROP_COEFFICIENT})
    */
-  public MetricDatasetPipeline(String outputName, Set<String> inputNames, Map<String, Object> ignore) {
+  public MetricDatasetPipeline(String outputName, Set<String> inputNames, Map<String, Object> properties) {
     super(outputName, inputNames);
     this.metricDAO = DAORegistry.getInstance().getMetricConfigDAO();
     this.datasetDAO = DAORegistry.getInstance().getDatasetConfigDAO();
+    this.coefficient = MapUtils.getDoubleValue(properties, PROP_COEFFICIENT, PROP_COEFFICIENT_DEFAULT);
   }
 
   @Override
@@ -76,10 +86,10 @@ public class MetricDatasetPipeline extends Pipeline {
       String d = metricDTO.getDataset();
       datasets.add(d);
 
-      double metricScore = me.getScore();
+      double metricScore = me.getScore() * this.coefficient;
       if(!datasetScores.containsKey(d))
         datasetScores.put(d, 0.0d);
-      datasetScores.put(d, datasetScores.get(d) + metricScore);
+      datasetScores.put(d, Math.max(datasetScores.get(d), metricScore));
 
       related.put(d, me);
     }
@@ -96,6 +106,7 @@ public class MetricDatasetPipeline extends Pipeline {
       Collection<MetricConfigDTO> dtos = metricDAO.findByDataset(d);
 
       dtos = removeExisting(dtos, metrics);
+      dtos = removeMeta(dtos);
 
       for(MetricConfigDTO dto : dtos) {
         entities.add(MetricEntity.fromMetric(datasetScore, related.get(d), dto.getId()));
@@ -103,6 +114,16 @@ public class MetricDatasetPipeline extends Pipeline {
     }
 
     return new PipelineResult(context, entities);
+  }
+
+  static Collection<MetricConfigDTO> removeMeta(Iterable<MetricConfigDTO> dtos) {
+    Collection<MetricConfigDTO> out = new ArrayList<>();
+    for(MetricConfigDTO dto : dtos) {
+      if(dto.getName().endsWith(META_METRIC_COUNT))
+        continue;
+      out.add(dto);
+    }
+    return out;
   }
 
   static Collection<MetricConfigDTO> removeExisting(Iterable<MetricConfigDTO> dtos, Iterable<MetricEntity> existing) {
