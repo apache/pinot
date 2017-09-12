7dd3eba0b1ff0122501f2adca50f5f2e0e5dbe9f
Yves Yuen <justYves@users.noreply.github.com>
2017-09-12 10:59:27 -0700

    changed url and script (#1879)

--- thirdeye/thirdeye-pinot/src/main/resources/scripts/getGraphPnj.js
+++ thirdeye/thirdeye-pinot/src/main/resources/scripts/getGraphPnj.js
@@ -1,8 +1,11 @@
+// screenshot script for reference only 
+// actual used script lives in /thirdeye-configs/prod-config
 "use strict";
 
 var page = require('webpage').create();
 var system = require('system');
 var args = require('system').args;
+var isReady = false;
 var options = {
   anomalyRoute: args[1],
   output: args[2],
@@ -11,21 +14,15 @@ var options = {
 };
 
 page.viewportSize = {
-  width: options.width || 800,
+  width: options.width || 900,
   height: options.height || 1600
 };
-page.settings.resourceTimeout = 1000000;
-
+page.settings.resourceTimeout = 180000;
+page.onResourceTimeout = function(error) {
+  phantom.exit(1);
+};
 var requestsArray = [];
 
-// recording navigation request
-page.onNavigationRequested = function(url, type, willNavigate, main) {
-  system.stderr.writeLine('= onNavigationRequested()');
-  system.stderr.writeLine(' Trying to navigate to: ' + url);
-  system.stderr.writeLine(' Caused by: ' + type);
-  system.stderr.writeLine(' Will actually navigate: ' + willNavigate);
-  system.stderr.writeLine(' Sent from the page\'s main frame: ' + main);
-};
 
 // recording resource loading errors; exit when operations are cancelled
 page.onResourceError = function(resourceError) {
@@ -33,6 +30,55 @@ page.onResourceError = function(resourceError) {
   system.stderr.writeLine(' Unable to load resource (#' + resourceError.id + 'URL:' + resourceError.url + ')');
   system.stderr.writeLine(' Error code: ' + resourceError.errorCode + '. Description: ' + resourceError.errorString);
 };
+
+
+page.open(options.anomalyRoute, function(status) {
+  system.stderr.writeLine('= onOpen()');
+  system.stderr.writeLine(' PhantomJS version: ' + JSON.stringify(phantom.version));
+  system.stderr.writeLine(' PhantomJS page settings: ' + JSON.stringify(page.settings));
+  if (status !== 'success') {
+   system.stderr.writeLine('Error opening url');
+   phantom.exit(1);
+  }
+});
+
+page.onCallback = function(data) {
+  console.log(data);
+  if (data.message === 'ready') {
+    system.stderr.writeLine('= onCallBack() && isReady');
+    isReady = true;
+  }
+};
+
+page.onLoadFinished = function(status) {
+  system.stderr.writeLine('= onLoadFinished()');
+  console.log('requestedArray');
+  console.log(requestsArray.length);
+
+  var interval = setInterval(function () {
+
+    if (requestsArray.length === 0 && isReady) {
+      system.stderr.writeLine('taking screenshot');
+      
+      clearInterval(interval);
+        var clipRect = page.evaluate(function () {
+          return document.querySelector('#anomaly-screenshot').getBoundingClientRect();
+        });
+        page.clipRect = {
+          top:    clipRect.top,
+          left:   clipRect.left,
+          width:  clipRect.width + 20,
+          height: clipRect.height
+        };
+        page.render(options.output);
+        phantom.exit();
+    }
+  }, 500);
+
+  interval();
+
+};
+
 
 // on resource requested
 page.onResourceRequested = function(requestData, networkRequest) {
@@ -50,42 +96,4 @@ page.onResourceReceived = function (response) {
   system.stderr.writeLine('= onResourceReceived()');
   system.stderr.writeLine(' Request (#' + response.id + '): ');
   system.stderr.writeLine(requestsArray.length + ' pending requests');
-};
-
-
-page.open(options.anomalyRoute, function(status) {
-  system.stderr.writeLine('= onOpen()');
-  system.stderr.writeLine(' PhantomJS version: ' + JSON.stringify(phantom.version));
-  system.stderr.writeLine(' PhantomJS page settings: ' + JSON.stringify(page.settings));
-  if (status !== 'success') {
-   system.stderr.writeLine('Error opening url');
-   phantom.exit(1);
-  }
-});
-
-page.onLoadFinished = function(status) {
-  system.stderr.writeLine('= onLoadFinished()');
-  console.log('requestedArray');
-  console.log(requestsArray.length);
-
-  var interval = setInterval(function () {
-
-    if (requestsArray.length === 0) {
-      clearInterval(interval);
-      var clipRect = page.evaluate(function () {
-        return document.querySelector('#anomaly-chart-0').getBoundingClientRect();
-      });
-      page.clipRect = {
-        top:    clipRect.top + 10,
-        left:   clipRect.left + 15,
-        width:  clipRect.width,
-        height: clipRect.height
-      };
-      page.render(options.output);
-      phantom.exit();
-    }
-  }, 500);
-
-  interval();
-
 };
