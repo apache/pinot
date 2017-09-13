566f2e89480e012464006fedf88843ae520572d1
Yen-Jung Chang <ychang@linkedin.com>
2017-09-13 10:43:50 -0700

    Separate granulairty of timestamp and data point

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/datalayer/dto/DatasetConfigDTO.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/datalayer/dto/DatasetConfigDTO.java
@@ -9,23 +9,21 @@ public class DatasetConfigDTO extends DatasetConfigBean {
   private TimeGranularity bucketTimeGranularity;
 
   /**
-   * Returns the granularity of a bucket (a data point) of this dataset. The granularity of a bucket, which may be
-   * different from the granularity that is defined in dataset configuration, which actually defines the granularity of
-   * the timestamp of each data point. This variable provides the actual granularity when the granularity defined in
-   * the dataset config is different from the granularity of data point. This information is crucial for non-additive
-   * dataset.
+   * Returns the granularity of a bucket (i.e., a data point) of this dataset if such information is available.
    *
-   * @return the granularity of a bucket (a data point) of this dataset
+   * The granularity that is defined in dataset configuration actually defines the granularity of the timestamp of each
+   * data point. For instance, timestamp's granularity (in database) could be 1-MILLISECONDS but the bucket's
+   * granularity is 1-HOURS. In real applications, the granularity of timestamp is never being used. Therefore, this
+   * method returns the actual granularity of the bucket (data point) if such information is available in the cnofig.
+   * This information is crucial for non-additive dataset.
+   *
+   * @return the granularity of a bucket (a data point) of this dataset.
    */
   public TimeGranularity bucketTimeGranularity() {
     if (bucketTimeGranularity == null) {
-      if (this.isAdditive()) {
-        bucketTimeGranularity = new TimeGranularity(getTimeDuration(), getTimeUnit());
-      } else {
         int size = getNonAdditiveBucketSize() != null ? getNonAdditiveBucketSize() : getTimeDuration();
         TimeUnit timeUnit = getNonAdditiveBucketUnit() != null ? getNonAdditiveBucketUnit() : getTimeUnit();
         bucketTimeGranularity = new TimeGranularity(size, timeUnit);
-      }
     }
     return bucketTimeGranularity;
   }
