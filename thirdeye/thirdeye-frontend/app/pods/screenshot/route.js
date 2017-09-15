e7012fe9c757cd552cbb959abe38d8b9602ee698
Yves Yuen <yyuen@linkedin.com>
2017-09-15 15:48:31 -0700

    fix

--- thirdeye/thirdeye-frontend/app/pods/screenshot/route.js
+++ thirdeye/thirdeye-frontend/app/pods/screenshot/route.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import { checkStatus } from 'thirdeye-frontend/helpers/utils';
 import fetch from 'fetch';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(UnauthenticatedRouteMixin, {
   model(params) {
     const { anomalyId: id} = params;
     const url = `/anomalies/search/anomalyIds/1/1/1?anomalyIds=${id}&functionName=`;
