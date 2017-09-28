93309dceb12e42dccb77198e5d1bdcc16ee0ad2d
Neha Pawar <npawar@linkedin.com>
2017-09-28 08:54:35 -0700

    Add support for star tree segments with udfs (#1946)
    * Add support for star tree segments with udfs
    
    * Move logic to fetch columns from groupBy into separate utility method

--- pinot-common/src/main/java/com/linkedin/pinot/common/utils/request/RequestUtils.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/utils/request/RequestUtils.java
@@ -24,14 +24,18 @@ import com.linkedin.pinot.common.request.FilterQueryMap;
 import com.linkedin.pinot.common.request.GroupBy;
 import com.linkedin.pinot.common.request.HavingFilterQuery;
 import com.linkedin.pinot.common.request.HavingFilterQueryMap;
+import com.linkedin.pinot.common.request.transform.TransformExpressionTree;
 import com.linkedin.pinot.common.segment.SegmentMetadata;
 import com.linkedin.pinot.common.segment.StarTreeMetadata;
+import com.linkedin.pinot.pql.parsers.Pql2Compiler;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import org.apache.commons.lang.mutable.MutableInt;
 
 
@@ -40,6 +44,8 @@ public class RequestUtils {
   }
 
   private static final String USE_STAR_TREE_KEY = "useStarTree";
+  private static final Pql2Compiler PQL2_COMPILER = new Pql2Compiler();
+
 
   /**
    * Generates thrift compliant filterQuery and populate it in the broker request
@@ -201,8 +207,9 @@ public class RequestUtils {
     // Ensure that none of the group-by columns are metric or skipped for materialization.
     GroupBy groupBy = brokerRequest.getGroupBy();
     if (groupBy != null) {
-      List<String> groupByColumns = groupBy.getColumns();
-      for (String groupByColumn : groupByColumns) {
+      Set<String> allGroupByColumns = getAllGroupByColumns(groupBy);
+
+      for (String groupByColumn : allGroupByColumns) {
         if (metricColumnSet.contains(groupByColumn)) {
           return false;
         }
@@ -281,5 +288,32 @@ public class RequestUtils {
     }
     String useStarTreeString = debugOptions.get(USE_STAR_TREE_KEY);
     return (useStarTreeString != null) ? Boolean.valueOf(useStarTreeString) : true;
+  }
+
+  /**
+   * Helper method to extract all column names from group by columns and expressions
+   * @param groupBy
+   * @return
+   */
+  public static Set<String> getAllGroupByColumns(GroupBy groupBy) {
+    Set<String> allGroupByColumns = new HashSet<>();
+
+    if (groupBy != null) {
+      List<String> groupByColumns = groupBy.getColumns();
+      if (groupByColumns != null) {
+        allGroupByColumns.addAll(groupByColumns);
+      }
+
+      List<String> groupByExpressions = groupBy.getExpressions();
+      if (groupByExpressions != null) {
+        for (String expression : groupByExpressions) {
+          TransformExpressionTree expressionTree = PQL2_COMPILER.compileToExpressionTree(expression);
+          List<String> columns = new ArrayList<>();
+          expressionTree.getColumns(columns);
+          allGroupByColumns.addAll(columns);
+        }
+      }
+    }
+    return allGroupByColumns;
   }
 }
