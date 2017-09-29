6e2cb2a90d0ec74a23f18309b1d03874b73017b2
Alexander Pucher <apucher@linkedin.com>
2017-09-29 11:19:33 -0700

    upcoming holidays support

--- thirdeye/thirdeye-frontend/app/actions/events.js
+++ thirdeye/thirdeye-frontend/app/actions/events.js
@@ -100,22 +100,38 @@ const assignEventTimeInfo = (event, anomalyStart, anomalyEnd, baselineStart, bas
   }
 
   let isBaseline = false;
+  let isFuture = false;
+  let isOngoing = false;
   let {
     start: displayStart,
     end: displayEnd,
     label: displayLabel
   } = event;
+
+  // baseline event
   if ((baselineStart < displayEnd && displayStart < baselineEnd)) {
     isBaseline = true;
     displayStart -= baselineOffset;
     displayEnd -= baselineOffset;
     displayLabel += " (baseline)";
   }
-  if (displayEnd <= 0) {
+
+  // upcoming event
+  if (displayStart >= analysisEnd) {
+    isFuture = true;
+    displayStart = analysisEnd - 3600000;
     displayEnd = analysisEnd;
+    displayLabel += " (upcoming)";
   }
 
-  return Object.assign(event, { duration, relStart, relEnd, relDuration, isBaseline, displayStart, displayEnd, displayLabel });
+  // ongoing event
+  if (displayEnd <= 0) {
+    isOngoing = true;
+    displayEnd = analysisEnd;
+    displayLabel += " (ongoing)";
+  }
+
+  return Object.assign(event, { duration, relStart, relEnd, relDuration, isBaseline, isFuture, isOngoing, displayStart, displayEnd, displayLabel });
 };
 
 /**
