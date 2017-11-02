d08c3a14b0fdd3cd8ea9eaae62e477c8d7357686
Alexander Pucher <apucher@linkedin.com>
2017-11-01 19:19:34 -0700

    add basic event handling

--- thirdeye/thirdeye-frontend/app/pods/rootcause/controller.js
+++ thirdeye/thirdeye-frontend/app/pods/rootcause/controller.js
@@ -1,19 +1,44 @@
 import Ember from 'ember';
+import _ from 'lodash';
 
 const extractTimeseries = (json) => {
   const timeseries = {};
-
   Object.keys(json).forEach(range =>
     Object.keys(json[range]).filter(sid => sid != 'timestamp').forEach(sid => {
       const urn = `thirdeye:metric:${sid}`;
+      const jrng = json[range];
+      const jval = jrng[sid];
+
+      const timestamps = [];
+      const values = [];
+      jrng.timestamp.forEach((t, i) => {
+        if (jval[i] != null) {
+          timestamps.push(t);
+          values.push(jval[i]);
+        }
+      });
+
       timeseries[urn] = {
-        timestamps: json[range].timestamp,
-        values: json[range][sid]
+        timestamps: timestamps,
+        values: values
       };
     })
   );
-
   return timeseries;
+};
+
+const isIterable = (obj) => {
+  if (obj == null || _.isString(obj)) {
+    return false;
+  }
+  return typeof obj[Symbol.iterator] === 'function';
+};
+
+const makeIterable = (obj) => {
+  if (obj == null) {
+    return [];
+  }
+  return isIterable(obj) ? [...obj] : [obj];
 };
 
 export default Ember.Controller.extend({
@@ -34,7 +59,7 @@ export default Ember.Controller.extend({
     '_entitiesCache',
     function () {
       console.log('entities()');
-      this.get('_entitiesLoader');
+      this.get('_entitiesLoader'); // trigger loader. hacky
       return this.get('_entitiesCache') || {};
     }
   ),
@@ -44,14 +69,14 @@ export default Ember.Controller.extend({
     '_timeseriesCache',
     function () {
       console.log('timeseries()');
-      this.get('_timeseriesLoader');
+      this.get('_timeseriesLoader'); // trigger loader. hacky
       return this.get('_timeseriesCache') || {};
     }
   ),
 
   chartSelectedUrns: Ember.computed(
-    'selectedUrns',
     'model.primaryMetricUrn',
+    'selectedUrns',
     'invisibleUrns',
     function () {
       console.log('chartSelectedUrns()');
@@ -59,13 +84,13 @@ export default Ember.Controller.extend({
       const invisibleUrns = this.get('invisibleUrns');
       const primaryMetricUrn = this.get('model.primaryMetricUrn');
 
-      console.log('chartSelectedUrns: selectedUrns', selectedUrns);
-      console.log('chartSelectedUrns: invisibleUrns', invisibleUrns);
-      console.log('chartSelectedUrns: primaryMetricUrn', primaryMetricUrn);
-
+      // console.log('chartSelectedUrns: selectedUrns', selectedUrns);
+      // console.log('chartSelectedUrns: invisibleUrns', invisibleUrns);
+      // console.log('chartSelectedUrns: primaryMetricUrn', primaryMetricUrn);
+      //
       const output = new Set(selectedUrns);
       output.add(primaryMetricUrn);
-      invisibleUrns.forEach(output.delete);
+      invisibleUrns.forEach(urn => output.delete(urn));
 
       return output;
     }
@@ -76,6 +101,7 @@ export default Ember.Controller.extend({
     function() {
       console.log('_timeseriesLoader()');
       const entities = this.get('entities');
+      console.log('_timeseriesLoader: entities', entities);
       const metricUrns = Object.keys(entities).filter(urn => entities[urn] && entities[urn].type == 'metric');
 
       console.log('_timeseriesLoader: metricUrns', metricUrns);
@@ -93,10 +119,10 @@ export default Ember.Controller.extend({
       const dimensionEntities = this.get('model.dimensionEntities') || [];
       const metricEntities = this.get('model.metricEntities') || [];
 
-      console.log('_entitiesLoader: eventEntities', eventEntities);
-      console.log('_entitiesLoader: dimensionEntities', dimensionEntities);
-      console.log('_entitiesLoader: metricEntities', metricEntities);
-
+      // console.log('_entitiesLoader: eventEntities', eventEntities);
+      // console.log('_entitiesLoader: dimensionEntities', dimensionEntities);
+      // console.log('_entitiesLoader: metricEntities', metricEntities);
+      //
       const entities = {};
       eventEntities.forEach(e => entities[e.urn] = e);
       dimensionEntities.forEach(e => entities[e.urn] = e);
@@ -110,22 +136,22 @@ export default Ember.Controller.extend({
   _updateEntitiesCache(incoming) {
     console.log('_updateEntitiesCache()');
     const cache = this.get('_entitiesCache') || {};
-    const updatedCache = Object.assign({}, cache, incoming);
-    this.set('_entitiesCache', updatedCache);
-    return updatedCache;
+    Object.keys(incoming).forEach(urn => cache[urn] = incoming[urn]);
+    this.set('_entitiesCache', cache);
+    return cache;
   },
 
   _updateTimeseriesCache(incoming) {
     console.log('_updateTimeseriesCache()');
     const cache = this.get('_timeseriesCache') || {};
-    const updatedCache = Object.assign({}, cache, incoming);
-    this.set('_timeseriesCache', updatedCache);
-    return updatedCache;
+    Object.keys(incoming).forEach(urn => cache[urn] = incoming[urn]);
+    this.set('_timeseriesCache', cache);
+    return cache;
   },
 
   _startRequestMissingTimeseries(urns) {
     console.log('_startRequestMissingTimeseries()');
-    const pending = this.get('_pendingRequests') || {};
+    const pending = this.get('_pendingRequests') || new Set();
     const cache = this.get('_timeseriesCache') || {};
 
     const missing = new Set(urns);
@@ -138,17 +164,15 @@ export default Ember.Controller.extend({
     }
 
     const metricIds = [...missing].map(urn => urn.split(":")[2]);
-    const metricUrns = metricIds.map(mid => `thirdeye:metric:${mid}`);
-
-    const updatedPending = Object.assign({}, updatedPending, metricUrns);
 
     // NOTE: potential race condition?
-    this.set('_pendingRequests', updatedPending);
+    missing.forEach(urn => pending.add(urn));
+    this.set('_pendingRequests', pending);
 
     const idString = metricIds.join(',');
     const analysisRange = this.get('model.analysisRange');
 
-    const url = `/timeseries/query?metricIds=${idString}&ranges=${analysisRange[0]}:${analysisRange[1]}&granularity=5_MINUTES&transformations=timestamp,fillforward`;
+    const url = `/timeseries/query?metricIds=${idString}&ranges=${analysisRange[0]}:${analysisRange[1]}&granularity=15_MINUTES&transformations=timestamp,relative`;
 
     fetch(url)
       .then(res => res.json())
@@ -160,53 +184,63 @@ export default Ember.Controller.extend({
 
   _completeRequestMissingTimeseries(that, incoming) {
     console.log('_completeRequestMissingTimeseries()');
-    const pending = that.get('_pendingRequests') || {};
-
-    const updatedPending = Object.assign({}, pending);
-    Object.keys(incoming).forEach(urn => delete updatedPending[urn]);
+    const pending = that.get('_pendingRequests') || new Set();
 
     // NOTE: potential race condition?
-    that.set('_pendingRequests', updatedPending);
+    Object.keys(incoming).forEach(urn => pending.delete(urn));
+    that.set('_pendingRequests', pending);
 
     console.log('_completeRequestMissingTimeseries: merging cache');
     return that._updateTimeseriesCache(incoming); // return new state, including new metrics
   },
 
   actions: {
-    addSelectUrns(urns) {
+    addSelectedUrns(urns) {
+      console.log('addSelectedUrns()');
       const selectedUrns = this.get('selectedUrns');
-      urns.forEach(selectedUrns.add);
-      this.set('selectedUrns', Object.assign({}, selectedUrns));
+      makeIterable(urns).forEach(urn => selectedUrns.add(urn));
+      this.set('selectedUrns', selectedUrns);
+      this.notifyPropertyChange('selectedUrns');
     },
 
-    removeSelectUrns(urns) {
+    removeSelectedUrns(urns) {
+      console.log('removeSelectedUrns()');
       const selectedUrns = this.get('selectedUrns');
-      urns.forEach(selectedUrns.delete);
-      this.set('selectedUrns', Object.assign({}, selectedUrns));
+      makeIterable(urns).forEach(urn => selectedUrns.delete(urn));
+      this.set('selectedUrns', selectedUrns);
+      this.notifyPropertyChange('selectedUrns');
     },
 
     addFilteredUrns(urns) {
+      console.log('addFilteredUrns()');
       const filteredUrns = this.get('filteredUrns');
-      urns.forEach(filteredUrns.add);
-      this.set('filteredUrns', Object.assign({}, filteredUrns));
+      makeIterable(urns).forEach(urn => filteredUrns.add(urn));
+      this.set('filteredUrns', filteredUrns);
+      this.notifyPropertyChange('filteredUrns');
     },
 
     removeFilteredUrns(urns) {
+      console.log('removeFilteredUrns()');
       const filteredUrns = this.get('filteredUrns');
-      urns.forEach(filteredUrns.delete);
-      this.set('filteredUrns', Object.assign({}, filteredUrns));
+      makeIterable(urns).forEach(urn => filteredUrns.delete(urn));
+      this.set('filteredUrns', filteredUrns);
+      this.notifyPropertyChange('filteredUrns');
     },
 
     addInvisibleUrns(urns) {
+      console.log('addInvisibleUrns()');
       const invisibleUrns = this.get('invisibleUrns');
-      urns.forEach(invisibleUrns.add);
-      this.set('invisibleUrns', Object.assign({}, invisibleUrns));
+      makeIterable(urns).forEach(urn => invisibleUrns.add(urn));
+      this.set('invisibleUrns', invisibleUrns);
+      this.notifyPropertyChange('invisibleUrns');
     },
 
     removeInvisibleUrns(urns) {
+      console.log('removeInvisibleUrns()');
       const invisibleUrns = this.get('invisibleUrns');
-      urns.forEach(invisibleUrns.delete);
-      this.set('filteredUrns', Object.assign({}, invisibleUrns));
+      makeIterable(urns).forEach(urn => invisibleUrns.delete(urn));
+      this.set('invisibleUrns', invisibleUrns);
+      this.notifyPropertyChange('invisibleUrns');
     }
   }
 });
