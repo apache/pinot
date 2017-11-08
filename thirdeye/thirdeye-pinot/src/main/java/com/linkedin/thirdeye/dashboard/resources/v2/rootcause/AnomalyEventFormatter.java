f470029d770ccaa6399cd841965c3d0752f0c3b4
Alexander Pucher <apucher@linkedin.com>
2017-11-08 14:54:41 -0800

    [TE] rootcause entity attributes for holidays, anomalies, metrics (#2078)
    This PR adds entity-specific, filterable "attributes" to rootcause entities returned from RCA endpoints. This allows frontend filtering in addition to backend RCA searches. The attributes are injected by the respective entity formatters.

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/rootcause/AnomalyEventFormatter.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/rootcause/AnomalyEventFormatter.java
@@ -1,5 +1,7 @@
 package com.linkedin.thirdeye.dashboard.resources.v2.rootcause;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import com.linkedin.thirdeye.dashboard.resources.v2.RootCauseEventEntityFormatter;
 import com.linkedin.thirdeye.dashboard.resources.v2.pojo.RootCauseEventEntity;
 import com.linkedin.thirdeye.datalayer.bao.MetricConfigManager;
@@ -16,6 +18,9 @@ import org.apache.commons.lang.StringUtils;
 
 
 public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
+  public static final String ATTR_DATASET = "dataset";
+  public static final String ATTR_METRIC = "dataset";
+
   @Override
   public boolean applies(EventEntity entity) {
     return entity instanceof AnomalyEventEntity;
@@ -28,14 +33,22 @@ public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
     MergedAnomalyResultDTO dto = e.getDto();
     AnomalyFunctionDTO func = dto.getFunction();
 
+    Multimap<String, String> attributes = ArrayListMultimap.create();
+    attributes.put(ATTR_DATASET, dto.getCollection());
+    attributes.put(ATTR_METRIC, dto.getMetric());
+
     List<String> dimensions = new ArrayList<>();
     for (Map.Entry<String, String> entry : dto.getDimensions().entrySet()) {
-      dimensions.add(entry.getKey() + ":" + entry.getValue().toUpperCase());
+      dimensions.add(entry.getKey() + ":" + entry.getValue());
+      attributes.put(entry.getKey(), entry.getValue());
     }
 
     String label = String.format("%s (%s)", func.getFunctionName(), StringUtils.join(dimensions, ", "));
     String link = String.format("thirdeye#investigate?anomalyId=%d", dto.getId());
 
-    return makeRootCauseEventEntity(entity, label, link, dto.getStartTime(), dto.getEndTime(), null);
+    RootCauseEventEntity out = makeRootCauseEventEntity(entity, label, link, dto.getStartTime(), dto.getEndTime(), null);
+    out.setAttributes(attributes);
+
+    return out;
   }
 }
