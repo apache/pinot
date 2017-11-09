ac666112a6e548807e714f99bbf16c42af7a2a90
Alexander Pucher <apucher@linkedin.com>
2017-11-08 19:12:13 -0800

    [TE] rootcause poc - add legend component

---
+++ thirdeye/thirdeye-frontend/app/pods/components/rootcause-legend/component.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  entities: null, // {}
+
+  selectedUrns: null, // Set
+
+  invisibleUrns: null, // Set
+
+  onVisibility: null, // function (Set, bool)
+
+  onSelection: null, // function (Set, bool)
+
+  labels: Ember.computed(
+    'entities',
+    'selectedUrns',
+    function () {
+      const { entities, selectedUrns } = this.getProperties('entities', 'selectedUrns');
+      const labels = {};
+      [...selectedUrns].filter(urn => urn in entities).forEach(urn => labels[urn] = entities[urn].label);
+      return labels;
+    }
+  ),
+
+  actions: {
+    toggleVisibility(urn) {
+      const { onVisibility, invisibleUrns } = this.getProperties('onVisibility', 'invisibleUrns');
+      if (onVisibility) {
+        const state = invisibleUrns.has(urn);
+        onVisibility(urn, state);
+      }
+    },
+    removeUrn(urn) {
+      const { onSelection } = this.getProperties('onSelection');
+      if (onSelection) {
+        onSelection(urn, false);
+      }
+    }
+  }
+});
