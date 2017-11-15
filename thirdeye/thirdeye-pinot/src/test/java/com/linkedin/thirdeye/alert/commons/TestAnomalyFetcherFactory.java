907d85f347dcd98721dab01425f695eac9c9a0d3
Yung-Yu Chung <ychung@linkedin.com>
2017-11-15 10:06:37 -0800

    Use TestNG's assert instead of Java assert

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/alert/commons/TestAnomalyFetcherFactory.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/alert/commons/TestAnomalyFetcherFactory.java
@@ -3,6 +3,7 @@ package com.linkedin.thirdeye.alert.commons;
 import com.linkedin.thirdeye.alert.fetcher.AnomalyFetcher;
 import com.linkedin.thirdeye.alert.fetcher.ContinuumAnomalyFetcher;
 import com.linkedin.thirdeye.alert.fetcher.UnnotifiedAnomalyFetcher;
+import org.junit.Assert;
 import org.testng.annotations.Test;
 
 
@@ -11,11 +12,11 @@ public class TestAnomalyFetcherFactory {
   @Test
   public void testCreateAlertFetcher() throws Exception{
     AnomalyFetcher anomalyFetcher = AnomalyFetcherFactory.fromClassName("ContinuumAnomalyFetcher");
-    assert anomalyFetcher != null;
-    assert anomalyFetcher instanceof ContinuumAnomalyFetcher;
+    Assert.assertNotNull(anomalyFetcher);
+    Assert.assertTrue(anomalyFetcher instanceof ContinuumAnomalyFetcher);
 
     anomalyFetcher = AnomalyFetcherFactory.fromClassName("UnnotifiedAnomalyFetcher");
-    assert anomalyFetcher != null;
-    assert anomalyFetcher instanceof UnnotifiedAnomalyFetcher;
+    Assert.assertNotNull(anomalyFetcher);
+    Assert.assertTrue(anomalyFetcher instanceof UnnotifiedAnomalyFetcher);
   }
 }
