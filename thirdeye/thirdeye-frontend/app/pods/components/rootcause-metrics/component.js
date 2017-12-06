1038634c45c01c3f7dfbfab311b132271ef65c02
Alexander Pucher <apucher@linkedin.com>
2017-12-05 19:59:33 -0800

    fix undefined color issue

--- thirdeye/thirdeye-frontend/app/pods/components/rootcause-metrics/component.js
+++ thirdeye/thirdeye-frontend/app/pods/components/rootcause-metrics/component.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import { toCurrentUrn, toBaselineUrn, hasPrefix, filterPrefix } from '../../../helpers/utils';
+import { toCurrentUrn, toBaselineUrn, hasPrefix, filterPrefix } from 'thirdeye-frontend/helpers/utils';
 
 const ROOTCAUSE_METRICS_SORT_PROPERTY_METRIC = 'metric';
 const ROOTCAUSE_METRICS_SORT_PROPERTY_DATASET = 'dataset';
