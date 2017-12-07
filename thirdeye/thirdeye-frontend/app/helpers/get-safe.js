6d2a5ef6e89add2598fb5ba85d6b7c9b72be5336
Alexander Pucher <apucher@linkedin.com>
2017-12-07 14:11:12 -0800

    review fixes

--- thirdeye/thirdeye-frontend/app/helpers/get-safe.js
+++ thirdeye/thirdeye-frontend/app/helpers/get-safe.js
@@ -1,5 +1,13 @@
 import Ember from 'ember';
 
+/**
+ * Returns the value referred to by key in dict. Avoids resolving dot-syntax as done by the builtin
+ * Ember {{get}} helper to gracefully handle keys with "." (dot).
+ *
+ * @param {Object} dict - the dictionary/object
+ * @param {String} key - the key
+ * @return {Object} dict[key]
+ */
 export function getSafe([dict, key]) {
   if (!dict) { return; }
   return dict[key];
