107584914fb227b68e3a7a1e7d2a7e5a1f97ba58
Subbu Subramaniam <ssubrama@linkedin.com>
2017-12-19 16:32:12 -0800

    Minion does not use a pinot.minion prefix

--- pinot-common/src/main/java/com/linkedin/pinot/common/utils/CommonConstants.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/utils/CommonConstants.java
@@ -271,7 +271,7 @@ public class CommonConstants {
     public static final String DEFAULT_INSTANCE_BASE_DIR =
         System.getProperty("java.io.tmpdir") + File.separator + "PinotMinion";
     public static final String DEFAULT_INSTANCE_DATA_DIR = DEFAULT_INSTANCE_BASE_DIR + File.separator + "data";
-    public static final String PREFIX_OF_CONFIG_OF_SEGMENT_FETCHER_FACTORY = "pinot.minion.segment.fetcher";
+    public static final String PREFIX_OF_CONFIG_OF_SEGMENT_FETCHER_FACTORY = "segment.fetcher";
   }
 
   public static class Metric {
