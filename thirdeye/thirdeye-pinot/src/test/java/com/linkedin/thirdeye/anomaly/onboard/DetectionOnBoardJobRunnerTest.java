72fc0a1be1d6706d62ccc9dafe53755231cf4220
Yung-Yu Chung <ychung@linkedin.com>
2017-12-21 18:09:43 -0800

    Remove non-necessary package

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomaly/onboard/DetectionOnBoardJobRunnerTest.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomaly/onboard/DetectionOnBoardJobRunnerTest.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import sun.text.resources.lt.CollationData_lt;
 
 
 public class DetectionOnBoardJobRunnerTest {
