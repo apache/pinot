102246de8c05ac9029baa4888d6538d781573d90
Thao Bach <tbach@linkedin.com>
2018-01-12 15:20:13 -0800

    Updated import modules

--- thirdeye/thirdeye-frontend/app/pods/components/rootcause-anomaly/component.js
+++ thirdeye/thirdeye-frontend/app/pods/components/rootcause-anomaly/component.js
@@ -1,4 +1,5 @@
-import Ember from 'ember';
+import Component from "@ember/component";
+import { computed, setProperties, getProperties, get } from '@ember/object';
 import moment from 'moment';
 import { humanizeFloat } from 'thirdeye-frontend/helpers/utils';
 
@@ -15,7 +16,7 @@ const ANOMALY_OPTIONS_MAPPING = {
   'NO_FEEDBACK': 'To Be Determined'
 };
 
-export default Ember.Component.extend({
+export default Component.extend({
   classNames: ['rootcause-anomaly'],
   entities: null, // {}
 
@@ -36,11 +37,11 @@ export default Ember.Component.extend({
    */
   optionsMapping: ANOMALY_OPTIONS_MAPPING,
 
-  anomaly: Ember.computed(
+  anomaly: computed(
     'entities',
     'anomalyUrn',
     function () {
-      const { entities, anomalyUrn } = this.getProperties('entities', 'anomalyUrn');
+      const { entities, anomalyUrn } = getProperties('entities', 'anomalyUrn');
 
       if (!anomalyUrn || !entities || !entities[anomalyUrn]) { return false; }
 
@@ -48,46 +49,46 @@ export default Ember.Component.extend({
     }
   ),
 
-  functionName: Ember.computed('anomaly', function () {
-    return this.get('anomaly').attributes.function[0];
+  functionName: computed('anomaly', function () {
+    return get('anomaly').attributes.function[0];
   }),
 
-  anomalyId: Ember.computed('anomaly', function () {
-    return this.get('anomaly').urn.split(':')[3];
+  anomalyId: computed('anomaly', function () {
+    return get('anomaly').urn.split(':')[3];
   }),
 
-  metric: Ember.computed('anomaly', function () {
-    return this.get('anomaly').attributes.metric[0];
+  metric: computed('anomaly', function () {
+    return get('anomaly').attributes.metric[0];
   }),
 
-  dataset: Ember.computed('anomaly', function () {
-    return this.get('anomaly').attributes.dataset[0];
+  dataset: computed('anomaly', function () {
+    return get('anomaly').attributes.dataset[0];
   }),
 
-  current: Ember.computed('anomaly', function () {
-    return parseFloat(this.get('anomaly').attributes.current[0]).toFixed(3);
+  current: computed('anomaly', function () {
+    return parseFloat(get('anomaly').attributes.current[0]).toFixed(3);
   }),
 
-  baseline: Ember.computed('anomaly', function () {
-    return parseFloat(this.get('anomaly').attributes.baseline[0]).toFixed(3);
+  baseline: computed('anomaly', function () {
+    return parseFloat(get('anomaly').attributes.baseline[0]).toFixed(3);
   }),
 
-  change: Ember.computed('anomaly', function () {
-    const attr = this.get('anomaly').attributes;
+  change: computed('anomaly', function () {
+    const attr = get('anomaly').attributes;
     return (parseFloat(attr.current[0]) / parseFloat(attr.baseline[0]) - 1);
   }),
 
-  status: Ember.computed('anomaly', function () {
-    return this.get('anomaly').attributes.status[0];
+  status: computed('anomaly', function () {
+    return get('anomaly').attributes.status[0];
   }),
 
-  duration: Ember.computed('anomaly', function () {
-    const anomaly = this.get('anomaly');
+  duration: computed('anomaly', function () {
+    const anomaly = get('anomaly');
     return moment.duration(anomaly.end - anomaly.start).humanize();
   }),
 
-  dimensions: Ember.computed('anomaly', function () {
-    const attr = this.get('anomaly').attributes;
+  dimensions: computed('anomaly', function () {
+    const attr = get('anomaly').attributes;
     const dimNames = attr.dimensions || [];
     const dimValues = dimNames.reduce((agg, dimName) => { agg[dimName] = attr[dimName][0]; return agg; }, {});
     return dimNames.sort().map(dimName => dimValues[dimName]).join(', ');
@@ -95,40 +96,40 @@ export default Ember.Component.extend({
 
   issueType: null, // TODO
 
-  metricFormatted: Ember.computed(
+  metricFormatted: computed(
     'dataset',
     'metric',
     function () {
       const { dataset, metric } =
-        this.getProperties('dataset', 'metric');
+        getProperties('dataset', 'metric');
       return `${dataset}::${metric}`;
     }
   ),
 
-  dimensionsFormatted: Ember.computed('anomaly', function () {
-    const dimensions = this.get('dimensions');
+  dimensionsFormatted: computed('anomaly', function () {
+    const dimensions = get('dimensions');
     return dimensions ? ` (${dimensions})` : '';
   }),
 
-  changeFormatted: Ember.computed('change', function () {
-    const change = this.get('change');
+  changeFormatted: computed('change', function () {
+    const change = get('change');
     const prefix = change > 0 ? '+' : '';
     return `${prefix}${humanizeFloat(change * 100)}%`;
   }),
 
-  startFormatted: Ember.computed('anomaly', function () {
-    return moment(this.get('anomaly').start).format('MMM D YYYY, hh:mm a');
+  startFormatted: computed('anomaly', function () {
+    return moment(get('anomaly').start).format('MMM D YYYY, hh:mm a');
   }),
 
-  endFormatted: Ember.computed('anomaly', function () {
-    return moment(this.get('anomaly').end).format('MMM D YYYY, hh:mm a');
+  endFormatted: computed('anomaly', function () {
+    return moment(get('anomaly').end).format('MMM D YYYY, hh:mm a');
   }),
 
-  isHidden: Ember.computed(
+  isHidden: computed(
     'requiresFeedback',
     'isHiddenUser',
     function () {
-      const { requiresFeedback, isHiddenUser } = this.getProperties('requiresFeedback', 'isHiddenUser');
+      const { requiresFeedback, isHiddenUser } = getProperties('requiresFeedback', 'isHiddenUser');
       if (isHiddenUser === ROOTCAUSE_HIDDEN_DEFAULT) {
         return !requiresFeedback;
       }
@@ -138,19 +139,19 @@ export default Ember.Component.extend({
 
   actions: {
     onFeedback(status) {
-      const { onFeedback, anomalyUrn } = this.getProperties('onFeedback', 'anomalyUrn');
+      const { onFeedback, anomalyUrn } = getProperties('onFeedback', 'anomalyUrn');
 
       if (onFeedback) {
         onFeedback(anomalyUrn, status, '');
       }
 
       // TODO reload anomaly entity instead
-      this.setProperties({ status });
+      setProperties({ status });
     },
 
     toggleHidden() {
-      const { isHidden } = this.getProperties('isHidden');
-      this.setProperties({ isHiddenUser: !isHidden });
+      const { isHidden } = getProperties('isHidden');
+      setProperties({ isHiddenUser: !isHidden });
     }
   }
 });
