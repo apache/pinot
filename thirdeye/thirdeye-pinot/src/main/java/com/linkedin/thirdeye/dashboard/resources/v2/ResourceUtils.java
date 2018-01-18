db2ee91be2eaaea8ed17f3ef16e996ae2017960d
Alexander Pucher <apucher@linkedin.com>
2018-01-17 16:55:54 -0800

    [TE] rootcause - external links for metric entities (when possible)

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/ResourceUtils.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/ResourceUtils.java
@@ -1,6 +1,7 @@
 package com.linkedin.thirdeye.dashboard.resources.v2;
 
 import com.linkedin.thirdeye.api.TimeGranularity;
+import com.linkedin.thirdeye.dataframe.util.MetricSlice;
 import com.linkedin.thirdeye.datalayer.bao.MetricConfigManager;
 import com.linkedin.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
 import com.linkedin.thirdeye.datalayer.dto.MetricConfigDTO;
@@ -10,20 +11,28 @@ import java.net.URLEncoder;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang3.text.StrSubstitutor;
-import org.json.JSONObject;
+import org.apache.commons.lang.text.StrSubstitutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class ResourceUtils {
   private static final Logger LOG = LoggerFactory.getLogger(ResourceUtils.class);
+  private static final Pattern PATTERN_UNMATCHED = Pattern.compile("\\$\\{");
 
   private ResourceUtils() {
     // left blank
   }
 
+  /**
+   * Generates the external links (if any) for a given anomaly.
+   *
+   * @param mergedAnomaly anomaly dto
+   * @param metricConfigDAO metric config dao
+   * @return map of external urls, keyed by link label
+   */
   public static Map<String, String> getExternalURLs(MergedAnomalyResultDTO mergedAnomaly, MetricConfigManager metricConfigDAO) {
     String metric = mergedAnomaly.getMetric();
     String dataset = mergedAnomaly.getCollection();
@@ -38,32 +47,92 @@ public class ResourceUtils {
     // context for each pair of dimension name and value
     if (MapUtils.isNotEmpty(mergedAnomaly.getDimensions())) {
       for (Map.Entry<String, String> entry : mergedAnomaly.getDimensions().entrySet()) {
-        // TODO: Change to case sensitive?
-        try {
-          String URLEncodedDimensionName = URLEncoder.encode(entry.getKey().toLowerCase(), "UTF-8");
-          String URLEncodedDimensionValue = URLEncoder.encode(entry.getValue().toLowerCase(), "UTF-8");
-          context.put(URLEncodedDimensionName, URLEncodedDimensionValue);
-        } catch (UnsupportedEncodingException e) {
-          LOG.warn("Unable to encode this dimension pair {}:{} for external links.", entry.getKey(), entry.getValue());
-        }
+        putEncoded(context, entry);
       }
     }
 
-    Long startTime = mergedAnomaly.getStartTime();
-    Long endTime = mergedAnomaly.getEndTime();
+    Map<String, String> output = new HashMap<>();
     Map<String, String> externalLinkTimeGranularity = metricConfigDTO.getExtSourceLinkTimeGranularity();
     for (Map.Entry<String, String> externalLinkEntry : urlTemplates.entrySet()) {
       String sourceName = externalLinkEntry.getKey();
       String urlTemplate = externalLinkEntry.getValue();
-      // context for startTime and endTime
-      putExternalLinkTimeContext(startTime, endTime, sourceName, context, externalLinkTimeGranularity);
+
+      putExternalLinkTimeContext(mergedAnomaly.getStartTime(), mergedAnomaly.getEndTime(), sourceName, context, externalLinkTimeGranularity);
 
       StrSubstitutor strSubstitutor = new StrSubstitutor(context);
-      String extSourceUrl = strSubstitutor.replace(urlTemplate);
-      urlTemplates.put(sourceName, extSourceUrl);
+      String result = strSubstitutor.replace(urlTemplate);
+
+      if (PATTERN_UNMATCHED.matcher(result).find()) {
+        LOG.warn("Could not create valid pattern for anomaly '{}'. Skipping.", mergedAnomaly);
+        continue;
+      }
+
+      output.put(sourceName, result);
     }
 
-    return urlTemplates;
+    return output;
+  }
+
+  /**
+   * Generates the external links (if any) for a given metric slice
+   *
+   * @param slice metric slice
+   * @param metricDAO metric config dao
+   * @return map of external urls, keyed by link label
+   */
+  public static Map<String, String> getExternalURLs(MetricSlice slice, MetricConfigManager metricDAO) {
+    MetricConfigDTO metricConfigDTO = metricDAO.findById(slice.getMetricId());
+    if (metricConfigDTO == null) {
+      throw new IllegalArgumentException(String.format("Could not resolve metric id %d", slice.getMetricId()));
+    }
+
+    Map<String, String> urlTemplates = metricConfigDTO.getExtSourceLinkInfo();
+    if (MapUtils.isEmpty(urlTemplates)) {
+      return Collections.emptyMap();
+    }
+
+    // NOTE: this cannot handle multiple values for the same key
+    Map<String, String> context = new HashMap<>();
+    for (Map.Entry<String, String> entry : slice.getFilters().entries()) {
+      putEncoded(context, entry);
+    }
+
+    Map<String, String> output = new HashMap<>();
+    Map<String, String> externalLinkTimeGranularity = metricConfigDTO.getExtSourceLinkTimeGranularity();
+    for (Map.Entry<String, String> externalLinkEntry : urlTemplates.entrySet()) {
+      String sourceName = externalLinkEntry.getKey();
+      String urlTemplate = externalLinkEntry.getValue();
+
+      putExternalLinkTimeContext(slice.getStart(), slice.getEnd(), sourceName, context, externalLinkTimeGranularity);
+
+      StrSubstitutor strSubstitutor = new StrSubstitutor(context);
+      String result = strSubstitutor.replace(urlTemplate);
+
+      if (PATTERN_UNMATCHED.matcher(result).find()) {
+        LOG.warn("Could not create valid pattern for slice '{}'. Skipping.", slice);
+        continue;
+      }
+
+      output.put(sourceName, result);
+    }
+
+    return output;
+  }
+
+  /**
+   * Performs URL encoding for key and value and inserts the result into the given map.
+   *
+   * @param context destination map
+   * @param entry source entry
+   */
+  private static void putEncoded(Map<String, String> context, Map.Entry<String, String> entry) {
+    try {
+      String key = URLEncoder.encode(entry.getKey().toLowerCase(), "UTF-8");
+      String value = URLEncoder.encode(entry.getValue().toLowerCase(), "UTF-8");
+      context.put(key, value);
+    } catch (UnsupportedEncodingException e) {
+      LOG.warn("Unable to encode this dimension pair {}:{} for external links.", entry.getKey(), entry.getValue());
+    }
   }
 
   /**
