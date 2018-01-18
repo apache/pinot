10f3996af45bf3ad78433ce90c503b917e2324eb
Thao Bach <tbach@linkedin.com>
2018-01-17 16:53:13 -0800

    Fixed color

--- thirdeye/thirdeye-frontend/app/helpers/color-delta.js
+++ thirdeye/thirdeye-frontend/app/helpers/color-delta.js
@@ -9,7 +9,7 @@ import Ember from 'ember';
 export function colorDelta([value = 'N/A']) {
   let sign = true;
   if (value ===  'N/A') { return; }
-  
+
   if (Number.isFinite(value)) {
     sign = parseInt(value * 100) >= 0;
   } else {
