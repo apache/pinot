61986303cb76f0a9b98a81164a815816991e9c2d
Alexander Pucher <apucher@linkedin.com>
2018-01-22 17:35:55 -0800

    [TE] rootcause poc - data availability indicator

---
+++ thirdeye/thirdeye-frontend/app/pods/components/rootcause-data-indicator/component.js
@@ -0,0 +1,61 @@
+import Component from '@ember/component';
+import { computed, setProperties, getProperties } from '@ember/object';
+import { reads } from '@ember/object/computed';
+import { checkStatus, toMetricLabel } from 'thirdeye-frontend/helpers/utils';
+
+export default Component.extend({
+  //
+  // external properties
+  //
+  selectedUrn: null,
+
+  anomalyRange: null,
+
+  entities: null,
+
+  //
+  // internal properties
+  //
+
+  /**
+   * Max available data range for the selected metric.
+   * @type {Int}
+   */
+  maxTime: null,
+
+  didReceiveAttrs() {
+    const { selectedUrn, anomalyRange } = getProperties(this, 'selectedUrn', 'anomalyRange');
+
+    if (!selectedUrn || !anomalyRange) { return; }
+
+    if (!selectedUrn.startsWith('thirdeye:metric:')) { return; }
+
+    const id = selectedUrn.split(':')[2];
+
+    fetch(`/data/maxDataTime/metricId/${id}`)
+      .then(checkStatus)
+      .then(res => setProperties(this, { maxTime: res }));
+  },
+
+  /**
+   * True, if the analysis period exceeds the max available data range
+   * @type {boolean}
+   */
+  hasMaxTimeWarning: computed('anomalyRange', 'maxTime', function () {
+    const { anomalyRange, maxTime } = getProperties(this, 'anomalyRange', 'maxTime');
+
+    if (!anomalyRange || !maxTime) { return false; }
+
+    return anomalyRange[1] > maxTime;
+  }),
+
+  /**
+   * Formatted max available data range
+   * @type {string}
+   */
+  maxTimeFormatted: computed('maxTime', function () {
+    const { maxTime } = getProperties(this, 'maxTime');
+    return moment(maxTime).format('MMM D, hh:mm a');
+  })
+
+});
