af8c41f9f42143d859a6d423ccee40f23ee5271f
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2018-01-24 10:19:39 -0800

    Fix the issue where status code 500 is counted as a permanent exception

--- pinot-minion/src/main/java/com/linkedin/pinot/minion/executor/BaseSegmentConversionExecutor.java
+++ pinot-minion/src/main/java/com/linkedin/pinot/minion/executor/BaseSegmentConversionExecutor.java
@@ -157,7 +157,7 @@ public abstract class BaseSegmentConversionExecutor extends BaseTaskExecutor {
               return true;
             } catch (HttpErrorStatusException e) {
               int statusCode = e.getStatusCode();
-              if (statusCode == HttpStatus.SC_CONFLICT || statusCode > 500) {
+              if (statusCode == HttpStatus.SC_CONFLICT || statusCode >= 500) {
                 // Temporary exception
                 LOGGER.warn("Caught temporary exception while uploading segment: {}, will retry", segmentName, e);
                 return false;
