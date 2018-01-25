533d69e74981e341ecf7021e05e99efcf3a96c99
Jack Li(Analytics Engineering) <jlli@jlli-mn1.linkedin.biz>
2018-01-25 15:28:55 -0800

    Add broker meter to track result's group by size

--- pinot-core/src/main/java/com/linkedin/pinot/core/query/reduce/BrokerReduceService.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/query/reduce/BrokerReduceService.java
@@ -204,8 +204,8 @@ public class BrokerReduceService implements ReduceService<BrokerResponseNative> 
               brokerRequest.getGroupBy(), dataTableMap, brokerRequest.getHavingFilterQuery(),
               brokerRequest.getHavingFilterSubQueryMap());
           if (brokerMetrics != null && (!brokerResponseNative.getAggregationResults().isEmpty())) {
-            // We emit the group by size when the result isn't empty. All the sizes among group by results should be the same.
-            // Thus, we can just choose the size of 1st group by result.
+            // We emit the group by size when the result isn't empty. All the sizes among group-by results should be the same.
+            // Thus, we can just emit the one from the 1st result.
             brokerMetrics.addMeteredTableValue(rawTableName, BrokerMeter.GROUP_BY_SIZE,
                 brokerResponseNative.getAggregationResults().get(0).getGroupByResult().size());
           }
