cfdc5ccdb9b7e3e402072919330abb76d9c1d33f
Jihao Zhang <jihzhang@linkedin.com>
2018-02-09 17:26:42 -0800

    Add CSV file source

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/datasource/csv/CSVThirdEyeDataSource.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/datasource/csv/CSVThirdEyeDataSource.java
@@ -1,14 +1,21 @@
 package com.linkedin.thirdeye.datasource.csv;
 
+import com.linkedin.thirdeye.api.TimeGranularity;
+import com.linkedin.thirdeye.api.TimeSpec;
+import com.linkedin.thirdeye.constant.MetricAggFunction;
 import com.linkedin.thirdeye.dataframe.DataFrame;
 import com.linkedin.thirdeye.dataframe.Series;
+import com.linkedin.thirdeye.datasource.MetricFunction;
 import com.linkedin.thirdeye.datasource.ThirdEyeDataSource;
 import com.linkedin.thirdeye.datasource.ThirdEyeRequest;
 import com.linkedin.thirdeye.datasource.ThirdEyeResponse;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static com.linkedin.thirdeye.dataframe.Series.SeriesType.*;
 
@@ -17,7 +24,22 @@ public class CSVThirdEyeDataSource implements ThirdEyeDataSource{
   public static final String COL_TIMESTAMP = "timestamp";
   Map<String, DataFrame> dataSources;
 
-  public CSVThirdEyeDataSource(Map<String, DataFrame> dataSources) {
+  public static CSVThirdEyeDataSource fromDataFrame(Map<String, DataFrame> dataSources) {
+    return new CSVThirdEyeDataSource(new HashMap<String, DataFrame>(dataSources));
+  }
+
+  public static CSVThirdEyeDataSource fromUrl(Map<String, URL> dataSources) throws Exception {
+    Map<String, DataFrame> dataframes = new HashMap<>();
+    for (Map.Entry<String, URL> source : dataSources.entrySet()) {
+      try (InputStreamReader reader = new InputStreamReader(source.getValue().openStream())) {
+        dataframes.put(source.getKey(), DataFrame.fromCsv(reader));
+      }
+    }
+
+    return new CSVThirdEyeDataSource(dataframes);
+  }
+
+  CSVThirdEyeDataSource(Map<String, DataFrame> dataSources) {
     this.dataSources = dataSources;
   }
 
@@ -28,7 +50,20 @@ public class CSVThirdEyeDataSource implements ThirdEyeDataSource{
 
   @Override
   public ThirdEyeResponse execute(ThirdEyeRequest request) throws Exception {
-    return null;
+    DataFrame df = new DataFrame();
+    for(MetricFunction function : request.getMetricFunctions()){
+      MetricAggFunction functionName = function.getFunctionName();
+      if (functionName == MetricAggFunction.SUM){
+        Double sum = dataSources.get(request.getDataSource()).getDoubles(function.getMetricName()).sum().doubleValue();
+        df.addSeries(functionName.toString(), sum);
+      }
+    }
+    CSVThirdEyeResponse response = new CSVThirdEyeResponse(
+        request,
+        new TimeSpec("timestamp", new TimeGranularity(1, TimeUnit.HOURS), TimeSpec.SINCE_EPOCH_FORMAT),
+        df
+        );
+    return response;
   }
 
   @Override
--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/datasource/csv/CSVThirdEyeDataSourceTest.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/datasource/csv/CSVThirdEyeDataSourceTest.java
@@ -1,20 +1,24 @@
 package com.linkedin.thirdeye.datasource.csv;
 
+import com.linkedin.thirdeye.api.TimeGranularity;
+import com.linkedin.thirdeye.api.TimeSpec;
 import com.linkedin.thirdeye.constant.MetricAggFunction;
 import com.linkedin.thirdeye.dataframe.DataFrame;
 import com.linkedin.thirdeye.datasource.MetricFunction;
 import com.linkedin.thirdeye.datasource.ThirdEyeDataSource;
 import com.linkedin.thirdeye.datasource.ThirdEyeRequest;
 import com.linkedin.thirdeye.datasource.ThirdEyeResponse;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.ExpectedExceptions;
 import org.testng.annotations.Test;
 
 
@@ -23,13 +27,46 @@ public class CSVThirdEyeDataSourceTest {
 
   @BeforeMethod
   public void beforeMethod(){
+
     Map<String, DataFrame> sources = new HashMap<>();
     sources.put("source", new DataFrame()
         .addSeries("timestamp", 100)
+        .addSeries("views", 1000)
         .addSeries("country", "us"));
     sources.put("other", new DataFrame());
 
-    dataSource = new CSVThirdEyeDataSource(sources);
+    dataSource = CSVThirdEyeDataSource.fromDataFrame(sources);
+  }
+
+  @Test
+  public void testFromDataFrame() throws Exception {
+    Map<String, DataFrame> sources = new HashMap<>();
+    sources.put("a", new DataFrame());
+    sources.put("b", new DataFrame());
+    sources.put("c", new DataFrame());
+
+    CSVThirdEyeDataSource dataSource = CSVThirdEyeDataSource.fromDataFrame(sources);
+    Assert.assertEquals(new HashSet<>(dataSource.getDatasets()), sources.keySet());
+    Assert.assertNotSame(new HashSet<>(dataSource.getDatasets()), sources.keySet());
+  }
+
+  @Test
+  public void testFromUrl() throws Exception {
+    URL business = this.getClass().getResource("business.csv");
+
+    Map<String, URL> sources = new HashMap<>();
+    sources.put("business", business);
+
+    CSVThirdEyeDataSource dataSource = CSVThirdEyeDataSource.fromUrl(sources);
+
+    Map<String, List<String>> dimensions = new HashMap<>();
+    dimensions.put("country", Arrays.asList("us", "cn"));
+    dimensions.put("browser", Arrays.asList("chrome", "firefox", "safari"));
+
+    Assert.assertEquals(dataSource.getDatasets(), Collections.singletonList("business"));
+    Assert.assertEquals(dataSource.getMaxDataTime("business"), 7200000);
+    Assert.assertEquals(new HashSet<>(dataSource.getDimensionFilters("business").get("country")), new HashSet<>(dimensions.get("country")));
+    Assert.assertEquals(new HashSet<>(dataSource.getDimensionFilters("business").get("browser")), new HashSet<>(dimensions.get("browser")));
   }
 
   @Test
@@ -65,10 +102,18 @@ public class CSVThirdEyeDataSourceTest {
 
   @Test
   public void testExecuteSingleRequest() throws Exception {
-//    ThirdEyeRequest request = ThirdEyeRequest.newBuilder()
-//        .addMetricFunction(new MetricFunction(MetricAggFunction.AVG, "views", 0L, "source", null, null))
-//        .build("ref");
-////    ThirdEyeResponse response = ThirdEyeResponse;
-//    Assert.assertEquals(dataSource.execute(request), reponse);
+    ThirdEyeRequest request = ThirdEyeRequest.newBuilder()
+        .addMetricFunction(new MetricFunction(MetricAggFunction.SUM, "views", 0L, "source", null, null))
+        .setDataSource("source")
+        .build("ref");
+    ThirdEyeResponse expectedResponse = new CSVThirdEyeResponse(
+        request,
+        new TimeSpec("timestamp", new TimeGranularity(1, TimeUnit.HOURS), TimeSpec.SINCE_EPOCH_FORMAT),
+        new DataFrame()
+            .addSeries("SUM_views", 1000)
+        );
+    ThirdEyeResponse response = dataSource.execute(request);
+    Assert.assertEquals(response.getRequest(), expectedResponse.getRequest());
+    Assert.assertEquals(response.getNumRows(), expectedResponse.getNumRows());
   }
 }
