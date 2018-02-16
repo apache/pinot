4d54545113f22d2152f3381d8608d5acb749977c
Subbu Subramaniam <ssubrama@linkedin.com>
2018-02-15 16:45:49 -0800

    Fix default for ssl server certificate validation

--- pinot-common/src/main/java/com/linkedin/pinot/common/utils/ClientSSLContextGenerator.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/utils/ClientSSLContextGenerator.java
@@ -60,7 +60,7 @@ public class ClientSSLContextGenerator {
   }
 
   public ClientSSLContextGenerator(Configuration sslConfig) {
-    if (sslConfig.getBoolean(CONFIG_OF_ENABLE_SERVER_VERIFICATION)) {
+    if (sslConfig.getBoolean(CONFIG_OF_ENABLE_SERVER_VERIFICATION, true)) {
       _serverCACertFile = sslConfig.getString(CONFIG_OF_SERVER_CA_CERT);
     } else {
       _serverCACertFile = null;
