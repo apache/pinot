05cbe5c4311c793781aa54ade157024b098280d0
Thao Bach <tbach@linkedin.com>
2018-02-22 10:44:55 -0800

    Removed .card-container--lg and moved those styles to default .card-container

--- thirdeye/thirdeye-frontend/app/pods/components/dimension-summary/component.js
+++ thirdeye/thirdeye-frontend/app/pods/components/dimension-summary/component.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['dimension-summary card-container card-container--lg card-container--border-grey card-container--padded']
+  classNames: ['dimension-summary card-container card-container--padded']
 });
