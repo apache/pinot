8821e9d3085cea5d77bb2796294df3a496bd0bd6
Yves Yuen <yyuen@linkedin.com>
2018-02-22 15:35:43 -0800

    adding npm install

--- .travis_test.sh
+++ .travis_test.sh
@@ -34,7 +34,7 @@ if [ $? -eq 0 ]; then
 
   cd thirdeye
   # mvn test
-  # failed=$?
+  # failed=$?git 
   # Remove Pinot/ThirdEye files from local Maven repository to avoid a useless cache rebuild
   # rm -rf ~/.m2/repository/com/linkedin/pinot ~/.m2/repository/com/linkedin/thirdeye
   # if [ $failed -eq 0 ]; then
@@ -42,8 +42,9 @@ if [ $? -eq 0 ]; then
   # else
   #   exit 1
   # fi
-
-  ./thirdeye-frontend/node_modules/.bin/ember test --silent --skip-cleanup --launch Chrome
+  cd thirdeye-frontend
+  npm install
+  ./node_modules/.bin/ember test --silent --skip-cleanup --launch Chrome
   failed=$?
   if [ $failed -eq 0 ]; then
     exit 0
