36940af7bffa3c712837b6a78a2af82a312b2e42
Thao Bach <tbach@linkedin.com>
2018-02-26 16:35:59 -0800

    Cleaned up stuff

--- thirdeye/thirdeye-frontend/tests/unit/pods/home/route-test.js
+++ thirdeye/thirdeye-frontend/tests/unit/pods/home/route-test.js
@@ -1,8 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:home', 'Unit | Route | home', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
 });
 
 test('it exists', function(assert) {
