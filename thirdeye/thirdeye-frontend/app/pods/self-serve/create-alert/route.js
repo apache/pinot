77314d05bcc14491a69e6c44d7a99f6081f7e00f
Steve McClung <smcclung@linkedin.com>
2018-02-28 17:16:42 -0800

    [TE] Create Alert - adding post payload to create-job (#2558)
    HOTFIX. Adding POST content (request payload) data to `create-job` request.

--- thirdeye/thirdeye-frontend/app/pods/self-serve/create-alert/route.js
+++ thirdeye/thirdeye-frontend/app/pods/self-serve/create-alert/route.js
@@ -129,10 +129,10 @@ export default Route.extend({
       let onboardStartTime = moment();
       let newFunctionId = null;
 
-      fetch(createAlertUrl, postProps('')).then(checkStatus)
+      fetch(createAlertUrl, postProps({ name: newName })).then(checkStatus)
         .then((result) => {
           newFunctionId = result.id;
-          return fetch(updateAlertUrl, postProps('')).then(checkStatus);
+          return fetch(updateAlertUrl, postProps(data)).then(checkStatus);
         })
         .then((result) => {
           this.get('checkJobCreateStatus').perform(result.jobId, newName, newFunctionId);
