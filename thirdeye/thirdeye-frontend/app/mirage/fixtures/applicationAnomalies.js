215471bbaf27499bfeb123766d8ed0e245646438
Thao Bach <tbach@linkedin.com>
2018-03-02 13:10:16 -0800

    Added more mock anomalies

--- thirdeye/thirdeye-frontend/app/mirage/fixtures/applicationAnomalies.js
+++ thirdeye/thirdeye-frontend/app/mirage/fixtures/applicationAnomalies.js
@@ -29,6 +29,62 @@ export default [
     metric: "Metric Name 1",
     functionName: "Alert Name 1"
   }, {
+    id: 2,
+    start: 12345,
+    end: 23456,
+    dimensions: {
+      country: "US",
+      pageName: "some_page"
+    },
+    severity: 0.86,
+    current: 1444,
+    baseline: 1000,
+    feedback: "Yes, but New Trend",
+    metric: "Metric Name 1",
+    functionName: "Alert Name 1"
+  }, {
+    id: 2,
+    start: 12345,
+    end: 23456,
+    dimensions: {
+      country: "US",
+      pageName: "some_page"
+    },
+    severity: 0.86,
+    current: 1444,
+    baseline: 1000,
+    feedback: "Yes, but New Trend",
+    metric: "Metric Name 1",
+    functionName: "Alert Name 1"
+  }, {
+    id: 2,
+    start: 12345,
+    end: 23456,
+    dimensions: {
+      country: "US",
+      pageName: "some_page"
+    },
+    severity: 0.86,
+    current: 1444,
+    baseline: 1000,
+    feedback: "Yes, but New Trend",
+    metric: "Metric Name 1",
+    functionName: "Alert Name 1"
+  }, {
+    id: 2,
+    start: 12345,
+    end: 23456,
+    dimensions: {
+      country: "US",
+      pageName: "some_page"
+    },
+    severity: 0.86,
+    current: 1444,
+    baseline: 1000,
+    feedback: "Yes, but New Trend",
+    metric: "Metric Name 1",
+    functionName: "Alert Name 1"
+  }, {
     id: 3,
     start: 12345,
     end: 23456,
