2782868de4d18173c2c903cfbb675e37a05da66e
Thao Bach <tbach@linkedin.com>
2018-03-02 10:36:08 -0800

    Fixed test to make it more performant

--- thirdeye/thirdeye-frontend/tests/integration/pods/components/stats-cards/component-test.js
+++ thirdeye/thirdeye-frontend/tests/integration/pods/components/stats-cards/component-test.js
@@ -6,11 +6,11 @@ moduleForComponent('stats-cards', 'Integration | Component | stats cards', {
 });
 
 const CARD = '.te-horizontal-cards__card';
-const TITLE = `${CARD}-title`;
-const DESCRIPTION = `${CARD}-text`;
-const NUMBER = `${CARD}-number`;
 
 test('it renders', function(assert) {
+  const $title = this.$(`${CARD}-title`);
+  const $description = this.$(`${CARD}-text`);
+  const $number = this.$(`${CARD}-number`);
   const stats = [
     ['Number of anomalies', 'total anomalies', 10],
     ['Response Rate', 'description of response', 0.9],
@@ -25,43 +25,43 @@ test('it renders', function(assert) {
 
   // Testing titles of all cards
   assert.equal(
-    this.$(TITLE).get(0).innerText,
+    $title.get(0).innerText,
     stats[0][0],
     'title of 1st card is correct');
   assert.equal(
-    this.$(TITLE).get(1).innerText,
+    $title.get(1).innerText,
     stats[1][0],
     'title of 2nd card is correct');
   assert.equal(
-    this.$(TITLE).get(2).innerText,
+    $title.get(2).innerText,
     stats[2][0],
     'title of 3rd card is correct');
 
   // Testing descriptions of all cards
   assert.equal(
-    this.$(DESCRIPTION).get(0).innerText,
+    $description.get(0).innerText,
     stats[0][1],
     'description of 1st card is correct');
   assert.equal(
-    this.$(DESCRIPTION).get(1).innerText,
+    $description.get(1).innerText,
     stats[1][1],
     'description of 2nd card is correct');
   assert.equal(
-    this.$(DESCRIPTION).get(2).innerText,
+    $description.get(2).innerText,
     stats[2][1],
     'description of 3rd card is correct');
 
   // Testing values of all cards
   assert.equal(
-    this.$(NUMBER).get(0).innerText,
+    $number.get(0).innerText,
     stats[0][2],
     'value of 1st card is correct');
   assert.equal(
-    this.$(NUMBER).get(1).innerText,
+    $number.get(1).innerText,
     stats[1][2],
     'value of 2nd card is correct');
   assert.equal(
-    this.$(NUMBER).get(2).innerText,
+    $number.get(2).innerText,
     stats[2][2],
     'value of 3rd card is correct');
 });
