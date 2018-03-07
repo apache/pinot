99a30501438299c7937e8a5b197ce36eb9b22ace
newsummit <smcclung@linkedin.com>
2018-03-07 14:47:32 -0800

    TE Self-Serve Api import 1 v3

--- thirdeye/thirdeye-frontend/app/utils/api/self-serve.js
+++ thirdeye/thirdeye-frontend/app/utils/api/self-serve.js
@@ -1,19 +1,19 @@
 /**
  * GET request for all alert subscription groups
- * @see {@link namepathOrURL|link text}
+ * @see {@link https://tinyurl.com/ycv55yyx|class EntityManagerResource}
  */
 const allConfigGroups = '/thirdeye/entity/ALERT_CONFIG';
 
 /**
  * GET request for all alert application groups
- * @see {@link namepathOrURL|link text}
+ * @see {@link https://tinyurl.com/ycv55yyx|class EntityManagerResource}
  */
 const allApplications = '/thirdeye/entity/APPLICATION';
 
 /**
  * GET request for job status (a sequence of events including create, replay, autotune)
  * Requires 'jobId' query param
- * @see {@link namepathOrURL|link text}
+ * @see {@link https://tinyurl.com/ya6mwyjt|class DetectionOnboardResource}
  */
 const jobStatus = jobId => `/detection-onboard/get-status?jobId=${jobId}`;
 
@@ -21,7 +21,7 @@ const jobStatus = jobId => `/detection-onboard/get-status?jobId=${jobId}`;
  * POST request to create alert container in DB
  * Requires 'name' query param
  * @param {String} newAlertName: Name of new alert function
- * @see {@link namepathOrURL|link text}
+ * @see {@link https://tinyurl.com/yb4ebnbd|class FunctionOnboardingResource}
  */
 const createAlert = newAlertName => `/function-onboard/create-function?name=${newAlertName}`;
 
@@ -29,7 +29,7 @@ const createAlert = newAlertName => `/function-onboard/create-function?name=${ne
  * POST request to update an existing alert function with properties payload
  * Requires 'payload' object in request body and 'jobName' in query param
  * @param {String} jobName: unique name of new job for alert setup task
- * @see {@link namepathOrURL|link text}
+ * @see {@link https://tinyurl.com/yd84uj2b|class DetectionOnboardResource}
  */
 const updateAlert = jobName => `/detection-onboard/create-job?jobName=${jobName}`;
 
@@ -37,7 +37,7 @@ const updateAlert = jobName => `/detection-onboard/create-job?jobName=${jobName}
  * DELETE request to remove an alert function from the DB
  * Requires query param 'id'
  * @param {String} functionId: id of alert to remove
- * @see {@link namepathOrURL|link text}
+ * @see {@link https://tinyurl.com/ybmgmzhd|class AnomalyResource}
  */
 const deleteAlert = functionId => `/dashboard/anomaly-function?id=${functionId}`;
 
