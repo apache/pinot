95fcbc2156196eacb55e0992c9b98435f3835d9f
Yen-Jung Chang <ychang@linkedin.com>
2018-03-08 17:03:24 -0800

    Update RAW_ANOMALY_RETENTION_DAYS

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/monitor/MonitorConstants.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/monitor/MonitorConstants.java
@@ -13,7 +13,7 @@ public class MonitorConstants {
   public static int DEFAULT_RETENTION_DAYS = 30;
   public static int DEFAULT_COMPLETED_JOB_RETENTION_DAYS = 14;
   public static int DEFAULT_DETECTION_STATUS_RETENTION_DAYS = 7;
-  public static int DEFAULT_RAW_ANOMALY_RETENTION_DAYS = 60;
+  public static int DEFAULT_RAW_ANOMALY_RETENTION_DAYS = 30;
   public static TimeGranularity DEFAULT_MONITOR_FREQUENCY = new TimeGranularity(1, TimeUnit.HOURS);
 
 }
