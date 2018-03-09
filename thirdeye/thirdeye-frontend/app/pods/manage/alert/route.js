0ecbc51716ad3e47bd4e028fedfc3ebc3c42f65f
newsummit <smcclung@linkedin.com>
2018-03-08 22:54:05 -0800

    TE Self-Serve Api import 3: Import Metrics v2

--- thirdeye/thirdeye-frontend/app/pods/manage/alert/route.js
+++ thirdeye/thirdeye-frontend/app/pods/manage/alert/route.js
@@ -107,7 +107,7 @@ export default Route.extend({
     // We do not have a valid alertId. Set error state.
     if (isLoadError) {
       Object.assign(newAlertData, { functionName, isActive: false });
-      errorText = `Creation of alert: ${functionName.toUpperCase()} has failed. Please try again or email ask_thirdeye@linkedin.com`;
+      errorText = `${functionName.toUpperCase()} has failed to create. Please try again or email ask_thirdeye@linkedin.com`;
     }
 
     controller.setProperties({
