cf6929d56e496cf3be5095eb06af910e0fd898f6
Long Huynh <lohuynh@linkedin.com>
2018-03-12 18:06:08 -0700

    [TE] frontend - aaronucsd/added new dimensions custom template for dashboard

--- thirdeye/thirdeye-frontend/app/mirage/fixtures/applicationAnomalies.js
+++ thirdeye/thirdeye-frontend/app/mirage/fixtures/applicationAnomalies.js
@@ -7,150 +7,157 @@ export default [
     start: 1491804013000,
     end: 1491890413000,
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      app: 'company',
+      country: 'US',
+      os: 'Some OS',
+      pageName: 'login_page',
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 1",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 1',
     metricId: 12345,
-    functionName: "Alert Name 1"
+    functionName: 'Alert Name 1'
   }, {
     id: 2,
     start: 1491804013000,
     end: moment().valueOf(),
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: ['US', 'FR'],
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend 2",
-    metricName: "Metric Name 1",
+    feedback: 'Yes, but New Trend 2',
+    metricName: 'Metric Name 1',
     metricId: 12345,
-    functionName: "Alert Name 1"
+    functionName: 'Alert Name 1'
   }, {
     id: 3,
     start: 1491804013000,
     end: moment().valueOf(),
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: ['US', 'FR'],
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 1",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 1',
     metricId: 12345,
-    functionName: "Alert Name 1"
+    functionName: 'Alert Name 1'
   }, {
     id: 4,
     start: 1491804013000,
     end: moment().valueOf(),
-    dimensions: {
-      country: "US",
-      pageName: "some_page"
-    },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 1",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 1',
     metricId: 12345,
-    functionName: "Alert Name 1"
+    functionName: 'Alert Name 1'
   }, {
     id: 5,
     start: 1491804013000,
     end: moment().valueOf(),
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: ['US'],
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 1",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 1',
     metricId: 12345,
-    functionName: "Alert Name 1"
+    functionName: 'Alert Name 1'
   }, {
     id: 6,
     start: 1491804013000,
     end: 1491890413000,
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: ['US'],
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 1",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 1',
     metricId: 12345,
-    functionName: "Alert Name 1"
+    functionName: 'Alert Name 1'
   }, {
     id: 7,
     start: 1491804013000,
     end: 1491890413000,
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: ['US', 'FR'],
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 1",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 1',
     metricId: 12345,
-    functionName: "Alert Name 1"
+    functionName: 'Alert Name 1'
   }, {
     id: 8,
     start: 1491804013000,
     end: 1491890413000,
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: 'US',
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 2",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 2',
     metricId: 12345,
-    functionName: "Alert Name 2"
+    functionName: 'Alert Name 2'
   }, {
     id: 9,
     start: 1491804013000,
     end: moment().valueOf(),
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: ['US', 'FR'],
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 2",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 2',
     metricId: 12345,
-    functionName: "Alert Name 2"
+    functionName: 'Alert Name 2'
   }, {
     id: 10,
     start: 1491804013000,
     end: moment().valueOf(),
     dimensions: {
-      country: "US",
-      pageName: "some_page"
+      country: ['US', 'FR'],
+      pageName: ['some_page'],
+      random1: ['partial']
     },
     severity: 0.86,
     current: 1444,
     baseline: 1000,
-    feedback: "Yes, but New Trend",
-    metricName: "Metric Name 2",
+    feedback: 'Yes, but New Trend',
+    metricName: 'Metric Name 2',
     metricId: 12345,
-    functionName: "Alert Name 2"
+    functionName: 'Alert Name 2'
   }
 ];
