a63c986d6bd2ff6234210bcb5ffd0a1311a58257
Alexander Pucher <apucher@linkedin.com>
2018-03-12 17:25:22 -0700

    [TE] rootcause - anomaly entity with function, metric, weight, and severity attributes

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/rootcause/AnomalyEventFormatter.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/v2/rootcause/AnomalyEventFormatter.java
@@ -10,6 +10,7 @@ import com.linkedin.thirdeye.datalayer.bao.MergedAnomalyResultManager;
 import com.linkedin.thirdeye.datalayer.bao.MetricConfigManager;
 import com.linkedin.thirdeye.datalayer.dto.AnomalyFunctionDTO;
 import com.linkedin.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
+import com.linkedin.thirdeye.datalayer.dto.MetricConfigDTO;
 import com.linkedin.thirdeye.datasource.DAORegistry;
 import com.linkedin.thirdeye.rootcause.impl.AnomalyEventEntity;
 import com.linkedin.thirdeye.rootcause.impl.EventEntity;
@@ -22,7 +23,9 @@ import org.apache.commons.lang.StringUtils;
 public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
   public static final String ATTR_DATASET = "dataset";
   public static final String ATTR_METRIC = "metric";
+  public static final String ATTR_METRIC_ID = "metricId";
   public static final String ATTR_FUNCTION = "function";
+  public static final String ATTR_FUNCTION_ID = "functionId";
   public static final String ATTR_CURRENT = "current";
   public static final String ATTR_BASELINE = "baseline";
   public static final String ATTR_STATUS = "status";
@@ -30,6 +33,8 @@ public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
   public static final String ATTR_DIMENSIONS = "dimensions";
   public static final String ATTR_COMMENT = "comment";
   public static final String ATTR_EXTERNAL_URLS = "externalUrls";
+  public static final String ATTR_WEIGHT = "weight";
+  public static final String ATTR_SCORE = "score";
 
   private final MergedAnomalyResultManager anomalyDAO;
   private final MetricConfigManager metricDAO;
@@ -53,29 +58,34 @@ public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
   public RootCauseEventEntity format(EventEntity entity) {
     AnomalyEventEntity e = (AnomalyEventEntity) entity;
 
-    MergedAnomalyResultDTO dto = this.anomalyDAO.findById(e.getId(), false);
-    AnomalyFunctionDTO func = dto.getFunction();
+    MergedAnomalyResultDTO anomaly = this.anomalyDAO.findById(e.getId(), false);
+    AnomalyFunctionDTO function = anomaly.getFunction();
+    MetricConfigDTO metric = this.getMetricFromFunction(function);
 
     String comment = "";
     AnomalyFeedbackType status = AnomalyFeedbackType.NO_FEEDBACK;
-    if (dto.getFeedback() != null) {
-      comment = dto.getFeedback().getComment();
-      status = dto.getFeedback().getFeedbackType();
+    if (anomaly.getFeedback() != null) {
+      comment = anomaly.getFeedback().getComment();
+      status = anomaly.getFeedback().getFeedbackType();
     }
 
-    Map<String, String> externalUrls = ResourceUtils.getExternalURLs(dto, metricDAO);
+    Map<String, String> externalUrls = ResourceUtils.getExternalURLs(anomaly, this.metricDAO);
 
     Multimap<String, String> attributes = ArrayListMultimap.create();
-    attributes.put(ATTR_DATASET, dto.getCollection());
-    attributes.put(ATTR_METRIC, dto.getMetric());
-    attributes.put(ATTR_FUNCTION, func.getFunctionName());
-    attributes.put(ATTR_CURRENT, String.valueOf(dto.getAvgCurrentVal()));
-    attributes.put(ATTR_BASELINE, String.valueOf(dto.getAvgBaselineVal()));
+    attributes.put(ATTR_DATASET, anomaly.getCollection());
+    attributes.put(ATTR_METRIC, anomaly.getMetric());
+    attributes.put(ATTR_METRIC_ID, String.valueOf(metric.getId()));
+    attributes.put(ATTR_FUNCTION, function.getFunctionName());
+    attributes.put(ATTR_FUNCTION_ID, String.valueOf(function.getId()));
+    attributes.put(ATTR_CURRENT, String.valueOf(anomaly.getAvgCurrentVal()));
+    attributes.put(ATTR_BASELINE, String.valueOf(anomaly.getAvgBaselineVal()));
     attributes.put(ATTR_STATUS, status.toString());
     attributes.put(ATTR_COMMENT, comment);
     // attributes.put(ATTR_ISSUE_TYPE, null); // TODO
-    attributes.putAll(ATTR_DIMENSIONS, dto.getDimensions().keySet());
+    attributes.putAll(ATTR_DIMENSIONS, anomaly.getDimensions().keySet());
     attributes.putAll(ATTR_EXTERNAL_URLS, externalUrls.keySet());
+    attributes.put(ATTR_SCORE, String.valueOf(anomaly.getScore()));
+    attributes.put(ATTR_WEIGHT, String.valueOf(anomaly.getWeight()));
 
     // external urls as attributes
     for (Map.Entry<String, String> entry : externalUrls.entrySet()) {
@@ -84,7 +94,7 @@ public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
 
     // dimensions as attributes
     List<String> dimensionStrings = new ArrayList<>();
-    for (Map.Entry<String, String> entry : dto.getDimensions().entrySet()) {
+    for (Map.Entry<String, String> entry : anomaly.getDimensions().entrySet()) {
       dimensionStrings.add(entry.getValue());
       attributes.put(entry.getKey(), entry.getValue());
     }
@@ -94,12 +104,31 @@ public class AnomalyEventFormatter extends RootCauseEventEntityFormatter {
       dimensionString = String.format(" (%s)", StringUtils.join(dimensionStrings, ", "));
     }
 
-    String label = String.format("%s%s", func.getFunctionName(), dimensionString);
-    String link = String.format("#/rootcause?anomalyId=%d", dto.getId());
+    String label = String.format("%s%s", function.getFunctionName(), dimensionString);
+    String link = String.format("#/rootcause?anomalyId=%d", anomaly.getId());
 
-    RootCauseEventEntity out = makeRootCauseEventEntity(entity, label, link, dto.getStartTime(), dto.getEndTime(), null);
+    RootCauseEventEntity out = makeRootCauseEventEntity(entity, label, link, anomaly.getStartTime(), anomaly.getEndTime(), null);
     out.setAttributes(attributes);
 
     return out;
+  }
+
+  private MetricConfigDTO getMetricFromFunction(AnomalyFunctionDTO function) {
+    // NOTE this should be >= 0 but is a limitation of null field in the DB being converted to 0 by default
+    if (function.getMetricId() > 0) {
+      MetricConfigDTO metric = this.metricDAO.findById(function.getMetricId());
+      if (metric == null) {
+        throw new IllegalArgumentException(String.format("Could not resolve metric id %d", function.getMetricId()));
+      }
+      return metric;
+
+    } else {
+      MetricConfigDTO metric = this.metricDAO.findByMetricAndDataset(function.getMetric(), function.getCollection());
+      if (metric == null) {
+        throw new IllegalArgumentException(String.format("Could not resolve metric '%s' in dataset '%s'", function.getMetric(), function.getCollection()));
+      }
+      return metric;
+
+    }
   }
 }
