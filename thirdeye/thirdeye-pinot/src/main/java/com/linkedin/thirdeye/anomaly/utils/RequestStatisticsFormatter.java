0ae8b358bf502dd84b5b3845701e7b1812c81655
Alexander Pucher <apucher@linkeidn.com>
2018-03-27 13:16:11 -0700

    code cleanup

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/utils/RequestStatisticsFormatter.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/utils/RequestStatisticsFormatter.java
@@ -100,13 +100,13 @@ public class RequestStatisticsFormatter {
     return out;
   }
 
-  private Map<String, Double> durationInMs(Map<String, Long> a, Map<String, Long> b) {
+  private Map<String, Double> durationInMs(Map<String, Long> durations, Map<String, Long> counts) {
     Map<String, Double> out = new HashMap<>();
-    for (String key : a.keySet()) {
-      if (!b.keySet().contains(key)) {
+    for (String key : durations.keySet()) {
+      if (!durations.keySet().contains(key)) {
         continue;
       }
-      out.put(key, a.get(key) / (double) b.get(key) / 1E6);
+      out.put(key, counts.get(key) / (double) counts.get(key) / 1E6);
     }
     return out;
   }
