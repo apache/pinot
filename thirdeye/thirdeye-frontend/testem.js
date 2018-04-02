20a8aff6706a18620910fec17f496a86d3ec1a62
Yves Yuen <yyuen@linkedin.com>
2018-04-02 00:05:43 -0700

    integrate ember test in maven test

--- thirdeye/thirdeye-frontend/testem.js
+++ thirdeye/thirdeye-frontend/testem.js
@@ -2,7 +2,7 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    'Firefox'
   ],
   launch_in_dev: [
     'Chrome'
@@ -13,12 +13,17 @@ module.exports = {
       args: [
         // --no-sandbox is needed when running Chrome inside a container
         process.env.TRAVIS ? '--no-sandbox' : null,
-
         '--disable-gpu',
         '--headless',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
       ].filter(Boolean)
+    },
+    Firefox: {
+      mode: 'ci',
+      args: [
+        '-headless'
+      ]
     }
   }
 };
