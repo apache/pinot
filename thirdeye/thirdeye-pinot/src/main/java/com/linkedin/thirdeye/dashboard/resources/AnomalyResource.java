ccd628289a214a07f86317f550e9f5dcaedb347a
Yungyu Chung <ychung@linkedin.com>
2018-04-18 20:19:26 -0700

    Fix compile error

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/AnomalyResource.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dashboard/resources/AnomalyResource.java
@@ -510,12 +510,12 @@ public class AnomalyResource {
           break;
         case HOURS:
           if (Hours.hoursBetween(startTime, endTime).getHours() < DEFAULT_MINIMUM_REQUIRE_TRAINING_PERIOD.toStandardDuration().getStandardHours()) {
-            startTime = startTime.minus(DEFAULT_MINIMUN_REQUIRE_TRAINING_PERIOD);
+            startTime = startTime.minus(DEFAULT_MINIMUM_REQUIRE_TRAINING_PERIOD);
           }
           break;
         case DAYS:
           if (Days.daysBetween(startTime, endTime).getDays() < DEFAULT_MINIMUM_REQUIRE_TRAINING_PERIOD.toStandardDuration().getStandardDays()) {
-            startTime = startTime.minus(DEFAULT_MINIMUN_REQUIRE_TRAINING_PERIOD);
+            startTime = startTime.minus(DEFAULT_MINIMUM_REQUIRE_TRAINING_PERIOD);
           }
           break;
       }
