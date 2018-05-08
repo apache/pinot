81d7d406035c035475fa9d65eb1457062241ad4e
Alexander Pucher <apucher@linkedin.com>
2018-05-08 15:19:20 -0700

    moving average algo

---
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detection/algorithm/MovingAverageAlgorithm.java
@@ -0,0 +1,320 @@
+package com.linkedin.thirdeye.detection.algorithm;
+
+import com.google.common.base.Preconditions;
+import com.linkedin.thirdeye.dataframe.BooleanSeries;
+import com.linkedin.thirdeye.dataframe.DataFrame;
+import com.linkedin.thirdeye.dataframe.DoubleSeries;
+import com.linkedin.thirdeye.dataframe.LongSeries;
+import com.linkedin.thirdeye.dataframe.Series;
+import com.linkedin.thirdeye.dataframe.util.DataFrameUtils;
+import com.linkedin.thirdeye.dataframe.util.MetricSlice;
+import com.linkedin.thirdeye.datalayer.dto.DetectionConfigDTO;
+import com.linkedin.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
+import com.linkedin.thirdeye.detection.DataProvider;
+import com.linkedin.thirdeye.detection.DetectionPipelineResult;
+import com.linkedin.thirdeye.detection.StaticDetectionPipeline;
+import com.linkedin.thirdeye.detection.StaticDetectionPipelineData;
+import com.linkedin.thirdeye.detection.StaticDetectionPipelineModel;
+import com.linkedin.thirdeye.rootcause.impl.MetricEntity;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+
+
+public class MovingAverageAlgorithm extends StaticDetectionPipeline {
+  private static final String COL_CURR = "current";
+  private static final String COL_BASE = "baseline";
+  private static final String COL_STD = "std";
+  private static final String COL_MEAN = "mean";
+  private static final String COL_QUANTILE = "quantile";
+  private static final String COL_ZSCORE = "zscore";
+  private static final String COL_QUANTILE_VIOLATION = "quantile_violation";
+  private static final String COL_ZSCORE_VIOLATION = "zscore_violation";
+  private static final String COL_ANOMALY = "anomaly";
+  private static final String COL_TIME = DataFrameUtils.COL_TIME;
+  private static final String COL_VALUE = DataFrameUtils.COL_VALUE;
+
+  private static final String PROP_METRIC_URN = "metricUrn";
+
+  private static final String PROP_LOOKBACK = "lookback";
+  private static final Period PROP_LOOKBACK_DEFAULT = new Period(7, PeriodType.days());
+
+  private static final String PROP_QUANTILE = "quantile";
+  private static final double PROP_QUANTILE_DEFAULT = Double.NaN;
+
+  private static final String PROP_ZSCORE = "zscore";
+  private static final double PROP_ZSCORE_DEFAULT = Double.NaN;
+
+  private static final String PROP_WEEK_OVER_WEEK = "weekOverWeek";
+  private static final boolean PROP_WEEK_OVER_WEEK_DEFAULT = false;
+
+  private static final String PROP_TIMEZONE = "timezone";
+  private static final String PROP_TIMEZONE_DEFAULT = "UTC";
+
+  private static final Pattern PATTERN_LOOKBACK = Pattern.compile("([0-9]+)\\s*(\\S*)");
+
+  private final MetricSlice slice;
+  private final Period lookback;
+  private final double zscore;
+  private final double quantile;
+  private final boolean weekOverWeek;
+  private final DateTimeZone timezone;
+
+  public MovingAverageAlgorithm(DataProvider provider, DetectionConfigDTO config, long startTime, long endTime) {
+    super(provider, config, startTime, endTime);
+
+    Preconditions.checkArgument(config.getProperties().containsKey(PROP_METRIC_URN));
+
+    String metricUrn = MapUtils.getString(config.getProperties(), PROP_METRIC_URN);
+    MetricEntity me = MetricEntity.fromURN(metricUrn, 1.0);
+
+    this.weekOverWeek = MapUtils.getBooleanValue(config.getProperties(), PROP_WEEK_OVER_WEEK, PROP_WEEK_OVER_WEEK_DEFAULT);
+    this.quantile = MapUtils.getDoubleValue(config.getProperties(), PROP_QUANTILE, PROP_QUANTILE_DEFAULT);
+    this.zscore = MapUtils.getDoubleValue(config.getProperties(), PROP_ZSCORE, PROP_ZSCORE_DEFAULT);
+    this.lookback = parseLookback(config.getProperties().get(PROP_LOOKBACK).toString());
+    this.timezone = DateTimeZone.forID(MapUtils.getString(config.getProperties(), PROP_TIMEZONE, PROP_TIMEZONE_DEFAULT));
+
+    DateTime trainStart = new DateTime(startTime, this.timezone).minus(this.lookback);
+    if (this.weekOverWeek) {
+      trainStart.minus(new Period(1, PeriodType.weeks()));
+    }
+
+    this.slice = MetricSlice.from(me.getId(), trainStart.getMillis(), endTime, me.getFilters());
+  }
+
+  @Override
+  public StaticDetectionPipelineModel getModel() {
+    return new StaticDetectionPipelineModel()
+        .withTimeseriesSlices(Collections.singleton(this.slice));
+  }
+
+  @Override
+  public DetectionPipelineResult run(StaticDetectionPipelineData data) throws Exception {
+    DataFrame df = data.getTimeseries().get(this.slice);
+
+    if (this.weekOverWeek) {
+      df = this.differentiateWeekOverWeek(df);
+    }
+
+    // potentially variable window size. manual iteration required.
+    df = applyMovingWindow(df);
+
+    df.addSeries(COL_ZSCORE, df.getDoubles(COL_VALUE).subtract(df.get(COL_MEAN)).divide(df.get(COL_STD)));
+
+    // zscore check
+    df.addSeries(COL_ZSCORE_VIOLATION, BooleanSeries.fillValues(df.size(), false));
+    if (!Double.isNaN(this.zscore)) {
+      df.mapInPlace(new Series.DoubleConditional() {
+        @Override
+        public boolean apply(double... values) {
+          return Math.abs(values[0]) > MovingAverageAlgorithm.this.zscore;
+        }
+      }, COL_ZSCORE_VIOLATION, COL_ZSCORE).fillNull(COL_ZSCORE_VIOLATION);
+    }
+
+    // quantile check
+    df.addSeries(COL_QUANTILE_VIOLATION, BooleanSeries.fillValues(df.size(), false));
+    if (!Double.isNaN(this.quantile)) {
+      df.mapInPlace(new Series.DoubleConditional() {
+        @Override
+        public boolean apply(double... values) {
+          return Math.abs(values[0]) > MovingAverageAlgorithm.this.zscore;
+        }
+      }, COL_QUANTILE_VIOLATION, COL_QUANTILE).fillNull(COL_QUANTILE_VIOLATION);
+    }
+
+    // anomalies
+    df.mapInPlace(BooleanSeries.HAS_TRUE, COL_ANOMALY, COL_ZSCORE_VIOLATION, COL_QUANTILE_VIOLATION);
+
+    List<MergedAnomalyResultDTO> anomalies = new ArrayList<>();
+    for (int i = 0; i < df.size(); i++) {
+      if (BooleanSeries.booleanValueOf(df.getBoolean(COL_ANOMALY, i))) {
+        long start = df.getLong(COL_TIME, i);
+        long end = getEndTime(df, i);
+
+        anomalies.add(this.makeAnomaly(this.slice.withStart(start).withEnd(end)));
+      }
+    }
+
+    long maxTime = -1;
+    if (!df.isEmpty()) {
+      maxTime = df.getLongs(COL_TIME).max().longValue();
+    }
+
+    return new DetectionPipelineResult(anomalies, maxTime);
+  }
+
+  private long getEndTime(DataFrame df, int index) {
+    if (index < df.size() - 1) {
+      return df.getLong(COL_TIME, index + 1);
+    }
+    return df.getLongs(COL_TIME).max().longValue() + 1; // TODO use time granularity
+  }
+
+  /**
+   * Returns data frame augmented with moving window statistics (std, mean, quantile)
+   *
+   * @param df data
+   * @return data augmented with moving window statistics
+   */
+  DataFrame applyMovingWindow(DataFrame df) {
+    DataFrame res = new DataFrame(df);
+
+    LongSeries timestamps = res.getLongs(COL_TIME).filter(new Series.LongConditional() {
+      @Override
+      public boolean apply(long... values) {
+        return values[0] >= MovingAverageAlgorithm.this.startTime;
+      }
+    }).dropNull();
+
+    double[] std = new double[timestamps.size()];
+    double[] mean = new double[timestamps.size()];
+    double[] quantile = new double[timestamps.size()];
+
+    Arrays.fill(std, Double.NaN);
+    Arrays.fill(mean, Double.NaN);
+    Arrays.fill(quantile, Double.NaN);
+
+    for (int i = 0; i < timestamps.size(); i++) {
+      DataFrame window = this.makeWindow(res, timestamps.getLong(i));
+
+      if (window.isEmpty()) {
+        continue;
+      }
+
+      mean[i] = window.getDoubles(COL_VALUE).mean().doubleValue();
+
+      if (window.size() >= 2) {
+        std[i] = window.getDoubles(COL_VALUE).std().doubleValue();
+      }
+
+      if (!Double.isNaN(this.quantile)) {
+        quantile[i] = window.getDoubles(COL_VALUE).quantile(this.quantile).doubleValue();
+      }
+    }
+
+    DataFrame output = new DataFrame(COL_TIME, timestamps)
+        .addSeries(COL_STD, DoubleSeries.buildFrom(std))
+        .addSeries(COL_MEAN, DoubleSeries.buildFrom(mean))
+        .addSeries(COL_QUANTILE, DoubleSeries.buildFrom(quantile));
+
+    return res.addSeries(output);
+  }
+
+  /**
+   * Returns a data with values differentiated week-over-week.
+   *
+   * @param df data
+   * @return data with differentiated values
+   */
+  DataFrame differentiateWeekOverWeek(DataFrame df) {
+    DataFrame dfCurr = new DataFrame(df);
+    DataFrame dfBase = new DataFrame(df);
+
+    dfBase.mapInPlace(new Series.LongFunction() {
+      @Override
+      public long apply(long... values) {
+        return new DateTime(values[0], MovingAverageAlgorithm.this.timezone)
+            .plus(MovingAverageAlgorithm.this.lookback).getMillis();
+      }
+    }, COL_TIME);
+    dfBase.renameSeries(COL_VALUE, COL_BASE);
+
+    dfCurr.addSeries(dfBase, COL_BASE);
+    dfCurr.renameSeries(COL_VALUE, COL_CURR);
+
+    dfCurr.addSeries(COL_VALUE, dfCurr.getDoubles(COL_CURR).subtract(dfCurr.get(COL_BASE)));
+
+    return dfCurr.dropNull();
+  }
+
+  /**
+   * Returns variable-size look back window for given timestamp.
+   *
+   * @param df data
+   * @param tCurrent end timestamp (exclusive)
+   * @return window data frame
+   */
+  DataFrame makeWindow(DataFrame df, final long tCurrent) {
+    final long tStart = new DateTime(tCurrent, this.timezone).minus(this.lookback).getMillis();
+
+    return df.filter(new Series.LongConditional() {
+      @Override
+      public boolean apply(long... values) {
+        return values[0] >= tStart && values[0] < tCurrent;
+      }
+    }, COL_TIME).dropNull();
+  }
+
+  /**
+   * Helper for parsing lookback period string from config (e.g. '3 days', '1min', '3600000')
+   *
+   * @param lookback
+   * @return
+   */
+  static Period parseLookback(String lookback) {
+    if (StringUtils.isBlank(lookback)) {
+      return PROP_LOOKBACK_DEFAULT;
+    }
+
+    Matcher m = PATTERN_LOOKBACK.matcher(lookback);
+    if (!m.find()) {
+      throw new IllegalArgumentException(String.format("Could not parse lookback expression '%s'", lookback));
+    }
+
+    int size = Integer.valueOf(m.group(1).trim());
+
+    PeriodType t = PeriodType.millis();
+    if (m.group(2).length() > 0) {
+      t = parsePeriodType(m.group(2).trim());
+    }
+
+    return new Period().withFieldAdded(t.getFieldType(0), size);
+  }
+
+  /**
+   * Helper for heuristically parsing period unit from config (e.g. 'millis', 'hour', 'd')
+   *
+   * @param type period type string
+   * @return PeriodType
+   */
+  static PeriodType parsePeriodType(String type) {
+    type = type.toLowerCase();
+
+    if (type.startsWith("y") || type.startsWith("a")) {
+      return PeriodType.years();
+    }
+    if (type.startsWith("mo")) {
+      return PeriodType.months();
+    }
+    if (type.startsWith("w")) {
+      return PeriodType.weeks();
+    }
+    if (type.startsWith("d")) {
+      return PeriodType.days();
+    }
+    if (type.startsWith("h")) {
+      return PeriodType.hours();
+    }
+    if (type.startsWith("s")) {
+      return PeriodType.seconds();
+    }
+    if (type.startsWith("mill") || type.startsWith("ms")) {
+      return PeriodType.millis();
+    }
+    if (type.startsWith("m")) {
+      return PeriodType.minutes();
+    }
+
+    throw new IllegalArgumentException(String.format("Invalid period type '%s'", type));
+  }
+}
---
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/detection/algorithm/MovingAverageAlgorithmTest.java
@@ -0,0 +1,199 @@
+package com.linkedin.thirdeye.detection.algorithm;
+
+import com.linkedin.thirdeye.dataframe.DataFrame;
+import com.linkedin.thirdeye.dataframe.LongSeries;
+import com.linkedin.thirdeye.dataframe.util.MetricSlice;
+import com.linkedin.thirdeye.datalayer.dto.DetectionConfigDTO;
+import com.linkedin.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
+import com.linkedin.thirdeye.datalayer.dto.MetricConfigDTO;
+import com.linkedin.thirdeye.detection.DataProvider;
+import com.linkedin.thirdeye.detection.DetectionPipelineResult;
+import com.linkedin.thirdeye.detection.MockDataProvider;
+import com.linkedin.thirdeye.detection.StaticDetectionPipeline;
+import com.linkedin.thirdeye.rootcause.timeseries.BaselineAggregateType;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.joda.time.DurationFieldType;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.linkedin.thirdeye.dataframe.util.DataFrameUtils.*;
+
+
+public class MovingAverageAlgorithmTest {
+  private static final String COL_CURR = "current";
+  private static final String COL_BASE = "baseline";
+  private static final String COL_STD = "std";
+  private static final String COL_MEAN = "mean";
+  private static final String COL_QUANTILE = "quantile";
+  private static final String COL_ZSCORE = "zscore";
+
+  private static final String PROP_METRIC_URN = "metricUrn";
+  private static final String PROP_LOOKBACK = "lookback";
+  private static final String PROP_QUANTILE = "quantile";
+  private static final String PROP_ZSCORE = "zscore";
+  private static final String PROP_WEEK_OVER_WEEK = "weekOverWeek";
+  private static final String PROP_TIMEZONE = "timezone";
+
+  private DataProvider provider;
+  private MovingAverageAlgorithm algorithm;
+  private DataFrame data;
+  private Map<String, Object> properties;
+  private DetectionConfigDTO config;
+
+  @BeforeMethod
+  public void beforeMethod() throws Exception {
+    try (Reader dataReader = new InputStreamReader(this.getClass().getResourceAsStream("timeseries-4w.csv"))) {
+      this.data = DataFrame.fromCsv(dataReader);
+      this.data.setIndex(COL_TIME);
+      this.data.addSeries(COL_TIME, this.data.getLongs(COL_TIME).multiply(1000));
+    }
+
+    MetricConfigDTO metricConfigDTO = new MetricConfigDTO();
+    metricConfigDTO.setId(1L);
+    metricConfigDTO.setName("thirdeye-test");
+    metricConfigDTO.setDataset("thirdeye-test-dataset");
+
+    Map<MetricSlice, DataFrame> timeseries = new HashMap<>();
+    timeseries.put(MetricSlice.from(1L, 0L, 2419200000L), this.data);
+    timeseries.put(MetricSlice.from(1L, 604800000L, 2419200000L), this.data);
+
+    this.properties = new HashMap<>();
+    this.properties.put(PROP_METRIC_URN, "thirdeye:metric:1");
+    this.properties.put(PROP_LOOKBACK, "2weeks");
+
+    this.config = new DetectionConfigDTO();
+    this.config.setProperties(properties);
+
+    this.provider = new MockDataProvider()
+        .setTimeseries(timeseries)
+        .setMetrics(Collections.singletonList(metricConfigDTO));
+
+  }
+
+  // TODO complete test with data
+
+  @Test
+  public void testWindow() {
+    this.properties.put(PROP_LOOKBACK, "2hours");
+    this.properties.put(PROP_QUANTILE, 0.75);
+    this.algorithm = new MovingAverageAlgorithm(this.provider, this.config, 3600000L, 9999999L);
+
+    DataFrame input = new DataFrame(COL_TIME, LongSeries.buildFrom(0L, 3600000L, 7200000L, 14400000L))
+        .addSeries(COL_VALUE, 1, 2, 3, 5);
+
+    DataFrame output = new DataFrame(COL_TIME, LongSeries.buildFrom(0L, 3600000L, 7200000L, 14400000L))
+        .addSeries(COL_VALUE, 1, 2, 3, 5)
+        .addSeries(COL_STD, Double.NaN, Double.NaN, 0.7071067811865476, Double.NaN) // TODO avoid exact double compare
+        .addSeries(COL_MEAN, Double.NaN, 1, 1.5, 3)
+        .addSeries(COL_QUANTILE, Double.NaN, 1, 1.75, 3);
+
+    DataFrame window = this.algorithm.applyMovingWindow(input);
+
+    Assert.assertEquals(window, output);
+  }
+
+  @Test
+  public void testLookbackParser() {
+    Assert.assertEquals(MovingAverageAlgorithm.parseLookback("3600"), new Period().withField(DurationFieldType.millis(), 3600));
+    Assert.assertEquals(MovingAverageAlgorithm.parseLookback("1d"), new Period().withField(DurationFieldType.days(), 1));
+    Assert.assertEquals(MovingAverageAlgorithm.parseLookback("2hours"), new Period().withField(DurationFieldType.hours(), 2));
+    Assert.assertEquals(MovingAverageAlgorithm.parseLookback("24 hrs"), new Period().withField(DurationFieldType.hours(), 24));
+    Assert.assertEquals(MovingAverageAlgorithm.parseLookback("1 year"), new Period().withField(DurationFieldType.years(), 1));
+    Assert.assertEquals(MovingAverageAlgorithm.parseLookback("  3   w  "), new Period().withField(DurationFieldType.weeks(), 3));
+  }
+
+  @Test
+  public void testPeriodParser() {
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("ms"), PeriodType.millis());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("millis"), PeriodType.millis());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("s"), PeriodType.seconds());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("sec"), PeriodType.seconds());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("secs"), PeriodType.seconds());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("seconds"), PeriodType.seconds());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("m"), PeriodType.minutes());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("min"), PeriodType.minutes());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("mins"), PeriodType.minutes());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("minutes"), PeriodType.minutes());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("h"), PeriodType.hours());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("hour"), PeriodType.hours());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("hours"), PeriodType.hours());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("d"), PeriodType.days());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("day"), PeriodType.days());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("days"), PeriodType.days());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("w"), PeriodType.weeks());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("week"), PeriodType.weeks());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("weeks"), PeriodType.weeks());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("mon"), PeriodType.months());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("mons"), PeriodType.months());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("month"), PeriodType.months());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("months"), PeriodType.months());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("y"), PeriodType.years());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("year"), PeriodType.years());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("years"), PeriodType.years());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("a"), PeriodType.years());
+    Assert.assertEquals(MovingAverageAlgorithm.parsePeriodType("ans"), PeriodType.years());
+  }
+
+//  @Test
+//  public void testWeekOverWeekDifference() throws Exception {
+//    this.properties.put(PROP_DIFFERENCE, 400);
+//    this.algorithm = new BaselineAlgorithm(this.provider, this.config, 1814400000L, 2419200000L);
+//
+//    DetectionPipelineResult result = this.algorithm.run();
+//
+//    List<MergedAnomalyResultDTO> anomalies = result.getAnomalies();
+//    Assert.assertEquals(result.getLastTimestamp(), 2415600000L);
+//    Assert.assertEquals(anomalies.size(), 1);
+//    Assert.assertEquals(anomalies.get(0).getStartTime(), 2372400000L);
+//    Assert.assertEquals(anomalies.get(0).getEndTime(), 2376000000L);
+//  }
+//
+//  @Test
+//  public void testWeekOverWeekChange() throws Exception {
+//    this.properties.put(PROP_CHANGE, 0.4);
+//    this.algorithm = new BaselineAlgorithm(this.provider, this.config, 1814400000L, 2419200000L);
+//
+//    DetectionPipelineResult result = this.algorithm.run();
+//
+//    List<MergedAnomalyResultDTO> anomalies = result.getAnomalies();
+//    Assert.assertEquals(result.getLastTimestamp(), 2415600000L);
+//    Assert.assertEquals(anomalies.size(), 2);
+//    Assert.assertEquals(anomalies.get(0).getStartTime(), 2372400000L);
+//    Assert.assertEquals(anomalies.get(0).getEndTime(), 2376000000L);
+//    Assert.assertEquals(anomalies.get(1).getStartTime(), 2379600000L);
+//    Assert.assertEquals(anomalies.get(1).getEndTime(), 2383200000L);
+//  }
+//
+//  @Test
+//  public void testThreeWeekMedianChange() throws Exception {
+//    this.properties.put(PROP_WEEKS, 3);
+//    this.properties.put(PROP_AGGREGATION, BaselineAggregateType.MEDIAN.toString());
+//    this.properties.put(PROP_CHANGE, 0.3);
+//    this.algorithm = new BaselineAlgorithm(this.provider, this.config, 1814400000L, 2419200000L);
+//
+//    DetectionPipelineResult result = this.algorithm.run();
+//
+//    List<MergedAnomalyResultDTO> anomalies = result.getAnomalies();
+//    Assert.assertEquals(result.getLastTimestamp(), 2415600000L);
+//    Assert.assertEquals(anomalies.size(), 5);
+//    Assert.assertEquals(anomalies.get(0).getStartTime(), 2005200000L);
+//    Assert.assertEquals(anomalies.get(0).getEndTime(), 2008800000L);
+//    Assert.assertEquals(anomalies.get(1).getStartTime(), 2134800000L);
+//    Assert.assertEquals(anomalies.get(1).getEndTime(), 2138400000L);
+//    Assert.assertEquals(anomalies.get(2).getStartTime(), 2152800000L);
+//    Assert.assertEquals(anomalies.get(2).getEndTime(), 2156400000L);
+//    Assert.assertEquals(anomalies.get(3).getStartTime(), 2181600000L);
+//    Assert.assertEquals(anomalies.get(3).getEndTime(), 2185200000L);
+//    Assert.assertEquals(anomalies.get(4).getStartTime(), 2322000000L);
+//    Assert.assertEquals(anomalies.get(4).getEndTime(), 2325600000L);
+//  }
+
+}
