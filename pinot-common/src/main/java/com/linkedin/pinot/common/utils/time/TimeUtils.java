4642583c677af0aae941fceade5ae03855ab9a43
Sunitha Beeram <sbeeram@linkedin.com>
2018-05-09 13:03:41 -0700

    Avoid catching broad 'Exception' and prefer throwing a more specific runtime exception (#2789)

--- pinot-common/src/main/java/com/linkedin/pinot/common/utils/time/TimeUtils.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/utils/time/TimeUtils.java
@@ -100,19 +100,36 @@ public class TimeUtils {
       .appendSeconds().appendSuffix("s")
       .toFormatter();
 
+  /**
+   * Converts a string representing a period/duration to corresponding milliseconds.
+   * For ex, input "1d" would return 86400000L. Supported units are days (d), hours (h),
+   * minutes (m) and seconds (s).
+   *
+   * @param timeStr string representing the duration
+   * @return the corresponding time coverted to milliseconds
+   * @throws IllegalArgumentException if the string does not conform to the expected format
+   */
   public static Long convertPeriodToMillis(String timeStr) {
     Long millis = 0L;
     if (timeStr != null) {
       try {
         Period p = PERIOD_FORMATTER.parsePeriod(timeStr);
         millis = p.toStandardDuration().getStandardSeconds() * 1000L;
-      } catch (Exception e) {
-        throw new RuntimeException("Invalid time spec '" + timeStr + "' (Valid examples: '3h', '4h30m')", e);
+      } catch (IllegalArgumentException e) {
+        // rethrowing with more contextual information
+        throw new IllegalArgumentException("Invalid time spec '" + timeStr + "' (Valid examples: '3h', '4h30m')", e);
       }
     }
     return millis;
   }
 
+  /**
+   * Converts milliseconds into human readable duration string. For ex, input of 86400000L would
+   * return "1d".
+   *
+   * @param millis the value in milliseconds to be converted
+   * @return the corresponding human readable string or empty string if value cannot be converted
+   */
   public static  String convertMillisToPeriod(Long millis) {
     String periodStr = null;
     if (millis != null) {
