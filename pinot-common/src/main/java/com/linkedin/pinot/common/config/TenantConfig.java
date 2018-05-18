da30818ce7986e69d98aaf79103641f204704988
Jean-François Im <jeanfrancois.im@gmail.com>
2018-05-17 18:01:59 -0700

    Change tenant to tag in tenant configuration (#2819)

--- pinot-common/src/main/java/com/linkedin/pinot/common/config/TenantConfig.java
+++ pinot-common/src/main/java/com/linkedin/pinot/common/config/TenantConfig.java
@@ -26,12 +26,12 @@ import org.slf4j.LoggerFactory;
 public class TenantConfig {
   private static final Logger LOGGER = LoggerFactory.getLogger(TenantConfig.class);
 
-  @ConfigKey("brokerTenant")
-  @ConfigDoc("Broker tenant used by this table")
+  @ConfigKey("brokerTagPrefix")
+  @ConfigDoc("Broker tag prefix used by this table")
   private String broker;
 
-  @ConfigKey("serverTenant")
-  @ConfigDoc("Server tenant used by this table")
+  @ConfigKey("serverTagPrefix")
+  @ConfigDoc("Server tag prefix used by this table")
   private String server;
 
   public String getBroker() {
