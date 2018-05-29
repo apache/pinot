16ce7440a5411e32b2d5ce2ef7ac876dd418525b
Long Huynh <lohuynh@linkedin.com>
2018-05-29 10:43:21 -0700

    [TE] frontend - aaronucsd/Added filter By resolution type (#2853)

--- thirdeye/thirdeye-frontend/app/pods/home/controller.js
+++ thirdeye/thirdeye-frontend/app/pods/home/controller.js
@@ -3,6 +3,8 @@ import floatToPercent from 'thirdeye-frontend/utils/float-to-percent';
 import { computed, set } from '@ember/object';
 import moment from 'moment';
 import { setUpTimeRangeOptions } from 'thirdeye-frontend/utils/manage-alert-utils';
+import * as anomalyUtil from 'thirdeye-frontend/utils/anomaly';
+import _ from 'lodash';
 
 const TIME_PICKER_INCREMENT = 5; // tells date picker hours field how granularly to display time
 const DEFAULT_ACTIVE_DURATION = '1d'; // setting this date range selection as default (Last 24 Hours)
@@ -16,12 +18,58 @@ export default Controller.extend({
   startDate: null,
   endDate: null,
   duration: null,
+  anomalyResponseObj: anomalyUtil.anomalyResponseObj,
+  feedbackType: 'All Resolutions',
+
   /**
    * Overrides ember-models-table's css classes
    */
   classes: {
     table: 'table table-striped table-bordered table-condensed'
   },
+
+  init() {
+    this._super(...arguments);
+    this.get('anomalyResponseObj').push({
+      name: 'All Resolutions',
+      value: 'ALL',
+      status: 'All Resolutions'
+    });
+  },
+
+  anomalyResponseNames: computed(
+    'anomalyResponseObj',
+    function() {
+      return anomalyUtil.anomalyResponseObj.mapBy('name');
+    }
+  ),
+  filteredAnomalyMapping: computed(
+    'model.anomalyMapping', 'feedbackType',
+    function() {
+      let filteredAnomalyMapping = this.get('model.anomalyMapping');
+      const feedbackType = this.get('feedbackType');
+      const feedbackItem = this._checkFeedback(feedbackType);
+
+      if (feedbackItem.value !== 'ALL' && !_.isEmpty(filteredAnomalyMapping)) {
+        let map = {};
+          // Iterate through each anomaly
+        Object.keys(filteredAnomalyMapping).some(function(key) {
+            filteredAnomalyMapping[key].forEach(attr => {
+              if (attr.anomaly.data.feedback === feedbackItem.value) {
+                if (!map[key]) {
+                  map[key] = [];
+                }
+                map[key].push(attr);
+              }
+            });
+        });
+        return map;
+      } else {
+        return filteredAnomalyMapping;
+      }
+
+    }
+  ),
 
   /**
    * Date types to display in the pills
@@ -81,6 +129,17 @@ export default Controller.extend({
     }
   ),
 
+  /**
+   * Helper for getting the matching selected response feedback object
+   * @param {string} selected - selected filter by value
+   * @return {string}
+   */
+  _checkFeedback: function(selected) {
+    return this.get('anomalyResponseObj').find((type) => {
+       return type.name === selected;
+     });
+  },
+
   actions: {
     /**
      * Sets the selected application property based on user selection
@@ -109,6 +168,17 @@ export default Controller.extend({
       //Update the time range option selected
       this.set('timeRangeOptions', setUpTimeRangeOptions(TIME_RANGE_OPTIONS, duration));
       this.transitionToRoute({ queryParams: { appName, duration, startDate, endDate }});
+    },
+
+    /**
+     * Handle dynamically saving anomaly feedback responses
+     * @method onFilterBy
+     * @param {String} feedbackType - the current feedback type
+     * @param {String} selected - the selection item
+     */
+     onFilterBy(feedbackType, selected) {
+       const feedbackItem = this._checkFeedback(selected);
+       this.set('feedbackType', feedbackItem.name);
     }
   }
 });
