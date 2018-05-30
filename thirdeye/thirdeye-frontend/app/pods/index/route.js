0ea06cd442be572209986b06c072d3253312f90f
Long Huynh <lohuynh@linkedin.com>
2018-05-30 15:06:40 -0700

    [TE] frontend - aaronucsd/added auth to dashboard
    1. Made dashboard the home page
    2. Updated fix to login's route willTransition to clean out old `/rca` path
    3. Clean up router.js

--- thirdeye/thirdeye-frontend/app/pods/index/route.js
+++ thirdeye/thirdeye-frontend/app/pods/index/route.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
+/**
+ * @summary A Index Route to serve `home` as the default home page upon login (with no target)
+ */
 export default Route.extend({
+  beforeModel() {
+    this.replaceWith('home');
+  }
 });
