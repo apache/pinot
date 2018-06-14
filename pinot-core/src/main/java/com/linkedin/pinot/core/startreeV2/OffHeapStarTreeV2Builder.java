23cbd2f8fa61aaf1f617f70e2917d203e52e5f5d
Rahul Gupta <rahgupta@rahgupta-mn1.linkedin.biz>
2018-06-14 15:21:56 -0700

    cleaning

--- pinot-core/src/main/java/com/linkedin/pinot/core/startreeV2/OffHeapStarTreeV2Builder.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/startreeV2/OffHeapStarTreeV2Builder.java
@@ -16,53 +16,62 @@
 
 package com.linkedin.pinot.core.startreeV2;
 
+import com.linkedin.pinot.core.startree.StarTreeDataTable;
 import java.io.File;
 import java.util.Map;
 import java.util.Set;
 import java.util.List;
 import org.slf4j.Logger;
-import java.util.HashSet;
 import java.util.ArrayList;
 import java.io.IOException;
 import org.joda.time.DateTime;
 import org.slf4j.LoggerFactory;
 import java.nio.charset.Charset;
+import java.io.DataOutputStream;
 import com.google.common.collect.BiMap;
 import org.apache.commons.io.FileUtils;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ListMultimap;
 import com.linkedin.pinot.core.data.GenericRow;
+import com.linkedin.pinot.core.startree.MetricBuffer;
 import com.linkedin.pinot.common.data.MetricFieldSpec;
+import com.linkedin.pinot.core.startree.DimensionBuffer;
 import com.linkedin.pinot.common.data.DimensionFieldSpec;
 import com.linkedin.pinot.core.segment.creator.impl.V1Constants;
 import com.linkedin.pinot.core.segment.creator.ColumnIndexCreationInfo;
+import xerial.larray.mmap.MMapBuffer;
+import xerial.larray.mmap.MMapMode;
 
 
 public class OffHeapStarTreeV2Builder implements StarTreeV2Builder {
 
-  private static final Charset UTF_8 = Charset.forName("UTF-8");
-  private static final Logger LOGGER = LoggerFactory.getLogger(com.linkedin.pinot.core.startree.OffHeapStarTreeBuilder.class);
-
-  private int _nodeCount;
-  private TreeNode _rootNode;
-  private int _maxNumLeafRecords;
-  private List<Integer> _dimensionsSplitOrder;
-  private Set<Integer> _dimensionsWithoutStarNode;
-
-  private File _tempDir;
-  private File _dataFile;
-
   // Dimensions
-
   private int _dimensionSize;
-  private int _dimensionCount;
-  private final List<String> _dimensionNames = new ArrayList<>();
-  private final List<Object> _dimensionStarValues = new ArrayList<>();
-  private final List<BiMap<Object, Integer>> _dimensionDictionaries = new ArrayList<>();
+  private int _dimensionsCount;
+  private List<Integer> _dimensionsSplitOrder;
+  private List<DimensionFieldSpec> _dimensions;
+  private Set<Integer> _dimensionsWithoutStarNode;
+  private final List<String> _dimensionsName = new ArrayList<>();
+  private final List<BiMap<Object, Integer>> _dimensionsDictionaries = new ArrayList<>();
 
   // Metrics
-  private int _metricCount;
-  private final List<String> _metricNames = new ArrayList<>();
+  private int _metricSize;
+  private int _metricsCount;
+  private final List<String> _metricsName = new ArrayList<>();
+  private ListMultimap<MetricFieldSpec, String> _met2aggfuncPairs;
+
+  // General
+  private File _tempDir;
+  private File _dataFile;
+  private int _nodesCount;
+  private int[] _sortOrder;
+  private int _rawDocsCount;
+  private TreeNode _rootNode;
+  private int _maxNumLeafRecords;
+  private int _aggregatedDocsCount;
+  private DataOutputStream _dataOutputStream;
+  private static final Charset UTF_8 = Charset.forName("UTF-8");
+  private static final Logger LOGGER = LoggerFactory.getLogger(com.linkedin.pinot.core.startree.OffHeapStarTreeBuilder.class);
 
   /**
    * Helper class to represent a tree node.
@@ -90,56 +99,85 @@ public class OffHeapStarTreeV2Builder implements StarTreeV2Builder {
     LOGGER.info("Star tree data file: {}", _dataFile);
 
     // dimension
-    List<DimensionFieldSpec> dimensions = config.getDimensions();
-    _dimensionCount = dimensions.size();
-    for (DimensionFieldSpec dimension : dimensions) {
+    _dimensions = config.getDimensions();
+    _dimensionsCount = _dimensions.size();
+    for (DimensionFieldSpec dimension : _dimensions) {
         String dimensionName = dimension.getName();
-        _dimensionNames.add(dimensionName);
-        _dimensionStarValues.add(dimension.getDefaultNullValue());
-        _dimensionDictionaries.add(HashBiMap.<Object, Integer>create());
+        _dimensionsName.add(dimensionName);
     }
-    _dimensionSize = _dimensionCount * V1Constants.Numbers.INTEGER_SIZE;
+    _dimensionSize = _dimensionsCount * V1Constants.Numbers.INTEGER_SIZE;
 
+    // dimension split order
     List<String> dimensionsSplitOrder = config.getDimensionsSplitOrder();
     if (dimensionsSplitOrder != null) {
       _dimensionsSplitOrder = new ArrayList<>(dimensionsSplitOrder.size());
       for (String dimensionName : dimensionsSplitOrder) {
-        _dimensionsSplitOrder.add(_dimensionNames.indexOf(dimensionName));
+        _dimensionsSplitOrder.add(_dimensionsName.indexOf(dimensionName));
       }
     }
 
-    // dimensions without star nodes.
+    // dimensions without star nodes
     Set<String> dimensionsWithoutStarNode = config.getDimensionsWithoutStarNode();
     if (dimensionsWithoutStarNode != null) {
-      _dimensionsWithoutStarNode = getDimensionIdSet(dimensionsWithoutStarNode);
+      _dimensionsWithoutStarNode =  OffHeapStarTreeV2BuilderHelper.getDimensionIdSet(dimensionsWithoutStarNode,
+          _dimensionsName, _dimensionsCount);
     }
 
-    // Metric
-    ListMultimap<MetricFieldSpec, String> metric2aggFuncPairs = config.getMetric2aggFuncPairs();
-    _metricCount = metric2aggFuncPairs.size();
-    for (MetricFieldSpec metric: metric2aggFuncPairs.keySet()) {
-      _metricNames.add(metric.getName());
+    // metric
+    _met2aggfuncPairs = config.getMetric2aggFuncPairs();
+    _metricsCount = _met2aggfuncPairs.size();
+    for (MetricFieldSpec metric: _met2aggfuncPairs.keys()) {
+      _metricsName.add(metric.getName());
+      _metricSize += metric.getFieldSize();
     }
 
-    LOGGER.info("Dimension Names: {}", _dimensionNames);
-    LOGGER.info("Metric Names: {}", _metricNames);
+    LOGGER.info("Dimension Names: {}", _dimensionsName);
+    LOGGER.info("Metric Names: {}", _metricsName);
 
-    // Initialize the root node
+    // initialize the root node
     _rootNode = new TreeNode();
-    _nodeCount++;
+    _nodesCount++;
 
     // other initialisation
     _maxNumLeafRecords = config.getMaxNumLeafRecords();
+
   }
 
   @Override
   public void append(GenericRow row) throws IOException {
+    DimensionBuffer dimensions = new DimensionBuffer(_dimensionsCount);
+    for (int i = 0; i < _dimensionsCount; i++) {
+      String dimensionName = _dimensionsName.get(i);
+      Object dimensionValue = row.getValue(dimensionName);
+      dimensions.setDimension(i, dimensionValue);
+    }
 
+    // Metrics
+    Object[] metricValues = new Object[_metricsCount];
+    for (int i = 0; i < _metricsCount; i++) {
+      String metricName = _metricsName.get(i);
+      Object metricValue = row.getValue(metricName);
+      metricValues[i] = metricValue;
+    }
+
+    List<MetricFieldSpec> metricFieldSpecList = OffHeapStarTreeV2BuilderHelper.getMetricFieldSpec(_met2aggfuncPairs);
+    MetricBuffer metrics = new MetricBuffer(metricValues, metricFieldSpecList);
+    appendToRawBuffer(dimensions, metrics);
   }
 
   @Override
   public void build() throws IOException {
 
+
+    // compute the dimension split order if it's null.
+    if (_dimensionsSplitOrder == null || _dimensionsSplitOrder.isEmpty()) {
+      _dimensionsSplitOrder = OffHeapStarTreeV2BuilderHelper.computeDefaultSplitOrder(_dimensionsCount, _dimensionsDictionaries);
+    }
+
+    LOGGER.info("Split Order: {}", _dimensionsSplitOrder);
+
+    // Recursively construct the star tree
+    constructStarTree(_rootNode, 0, _rawDocsCount, 0);
   }
 
   @Override
@@ -158,13 +196,18 @@ public class OffHeapStarTreeV2Builder implements StarTreeV2Builder {
 
   }
 
-  private Set<Integer> getDimensionIdSet(Set<String> dimensionNameSet) {
-    Set<Integer> dimensionIdSet = new HashSet<>(dimensionNameSet.size());
-    for (int i = 0; i < _dimensionCount; i++) {
-      if (dimensionNameSet.contains(_dimensionNames.get(i))) {
-        dimensionIdSet.add(i);
-      }
+
+
+  // helper functions
+  private void appendToRawBuffer(DimensionBuffer dimensions, MetricBuffer metrics) throws IOException {
+    appendToBuffer(dimensions, metrics);
+    _rawDocsCount++;
+  }
+
+  private void appendToBuffer(DimensionBuffer dimensions, MetricBuffer metricHolder) throws IOException {
+    for (int i = 0; i < _dimensionsCount; i++) {
+      _dataOutputStream.writeInt(OffHeapStarTreeV2BuilderHelper.flipEndiannessIfNeeded(dimensions.getDimension(i)));
     }
-    return dimensionIdSet;
+    _dataOutputStream.write(metricHolder.toBytes(_metricSize));
   }
 }
