03d6fd293331792211cdbb4d90b3b49617d09bbc
Rahul Gupta <rahgupta@rahgupta-mn1.linkedin.biz>
2018-06-21 00:06:21 -0700

    fixed the travis and test cases for builder helper class

--- pinot-core/src/test/java/com/linkedin/pinot/core/startreeV2/OnHeapStarTreeV2BuilderTest.java
+++ pinot-core/src/test/java/com/linkedin/pinot/core/startreeV2/OnHeapStarTreeV2BuilderTest.java
@@ -30,7 +30,7 @@ public class OnHeapStarTreeV2BuilderTest {
   private static File _indexDir;
   private static StarTreeV2Config _starTreeV2Config;
   private static final String SEGMENT_NAME = "starTreeSegment";
-  private static final String DATA_DIR = System.getProperty("java.io.tmpdir") + File.separator + "SumStarTreeIndexTest";
+  private static final String DATA_DIR = System.getProperty("java.io.tmpdir") + File.separator + "OnHeapStarTreeV2BuilderTest";
 
 
   @BeforeClass
