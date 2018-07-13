71aedc4a8b5d296abcd5a0df2822580066bbf081
Subbu Subramaniam <ssubrama@linkedin.com>
2018-07-13 14:59:28 -0700

    Added header, and also addressed review comment

--- pinot-tools/src/main/java/com/linkedin/pinot/tools/PinotIdealstateChanger.java
+++ pinot-tools/src/main/java/com/linkedin/pinot/tools/PinotIdealstateChanger.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2014-2016 LinkedIn Corp. (pinot-core@linkedin.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.linkedin.pinot.tools;
 
 import com.google.common.base.Function;
@@ -33,6 +49,7 @@ public class PinotIdealstateChanger extends PinotZKChanger {
 
   private IdealState applyChangesToIdealState(IdealState input) {
     // Add code here as needed to change idealstate
+    System.out.println("No change applied to idealstate.");
     return input;
   }
 
