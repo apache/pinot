18b084b764e263e482afc49a60fd6dcac01dd44e
Jihao Zhang <jihzhang@linkedin.com>
2018-07-20 10:43:48 -0700

    Edit dataset detection settings

--- thirdeye/thirdeye-frontend/app/pods/auto-onboard/controller.js
+++ thirdeye/thirdeye-frontend/app/pods/auto-onboard/controller.js
@@ -19,7 +19,6 @@ import {
 import {checkStatus} from 'thirdeye-frontend/utils/utils';
 import fetch from 'fetch';
 
-
 export default Controller.extend({
   detectionConfig: null,
 
@@ -29,7 +28,9 @@ export default Controller.extend({
 
   dimensions: null,
 
-  selectedDimensions: null,
+  selectedDimensions: '{}',
+
+  selectedFilters: '{}',
 
   selectedMetric: null,
 
@@ -53,6 +54,76 @@ export default Controller.extend({
 
   output: null,
 
+  idToNames: {},
+
+  queryParams: ['detectionId'],
+
+  hasDetectionId: observer('detectionId', function () {
+    const detectionId = this.get('detectionId');
+    fetch(`/dataset-auto-onboard/` + detectionId)
+      .then(res => res.json())
+      .then(res => {
+        const nestedProperties = res['properties']['nested'];
+        // fill in values:
+        this.set('detectionConfigName', res['name']);
+        this.set('topk', nestedProperties[0]['k']);
+        this.set('minvalue', nestedProperties[0]['minvalue']);
+        this.set('mincontribution', nestedProperties[0]['minContribution']);
+        this.set('datasetName', res['properties']['datasetName']);
+
+        this._datasetNameChanged().then(res => {
+
+          let dimensionBreakdownUrn = null;
+          const idToNames = this.get('idToNames');
+          const metricsProperties = get(this, 'metricsProperties');
+
+          // fill in dimensions
+          this.set('selectedDimensions', JSON.stringify({
+            'dimensions': nestedProperties[0]['dimensions']
+          }));
+
+          // fill in filters
+          let urnPieces = nestedProperties[0]['metricUrn'].split(':');
+          const filters = {};
+          var i;
+          for(i = 3; i < urnPieces.length; i++) {
+            const filter = urnPieces[i].split('=');
+            if (filter[0] in filters) {
+              filters[filter[0]].push(filter[1]);
+            } else {
+              filters[filter[0]] = [filter[1]];
+            }
+          }
+          this.set('selectedFilters', JSON.stringify(filters));
+
+          // fill in selected metrics
+          const metricIds = nestedProperties.reduce(function (obj, property) {
+            let urn;
+            if ('nestedMetricUrn' in property) {
+              urn = property['nestedMetricUrn'];
+              dimensionBreakdownUrn = property['metricUrn'];
+            } else {
+              urn = property['metricUrn'];
+            }
+            obj.push(urn.split(':')[2]);
+            return obj;
+          }, []);
+
+          Object.keys(metricsProperties).forEach(function (key) {
+            console.log(metricsProperties[key]['id']);
+            console.log(metricIds);
+            if(metricIds.indexOf(metricsProperties[key]['id'].toString()) == -1) {
+              set(metricsProperties[key], 'monitor', false);
+            }
+          });
+
+          // fill in dimension breakdown metric
+          this.set('selectedMetric', idToNames[this._metricUrnToId(dimensionBreakdownUrn)]);
+        });
+      })
+      .catch(error => console.error(`Fetch Error =\n`, error))
+  }),
+
   generalFieldsEnabled: computed.or('dimensions'),
 
   metricsFieldEnabled: computed.or('metrics'),
@@ -66,47 +137,61 @@ export default Controller.extend({
       .catch(err => set(this, 'output', err));
   },
 
+  _metricUrnToId(metricUrn) {
+    // return metricUrn.substring(16, metricUrn.indexOf(':', 16));
+    return metricUrn.split(':')[2];
+  },
+
+  _datasetNameChanged() {
+    const url = `/dataset-auto-onboard/metrics?dataset=` + get(this, 'datasetName');
+    return fetch(url)
+      .then(res => res.json())
+      .then(res => this.set('metrics', res))
+      .then(res => this.set('metricsProperties', res.reduce(function (obj, metric) {
+        obj[metric["name"]] = {
+          "id": metric['id'], "urn": "thirdeye:metric:" + metric['id'], "monitor": true
+        };
+        return obj;
+      }, {})))
+      .then(res => {
+        this.set('metrics', Object.keys(res));
+        const metricUrn = res[Object.keys(res)[0]]['id'];
+        const idToNames = {};
+        Object.keys(res).forEach(function (key) {
+          idToNames[res[key]['id']] = key;
+        });
+        this.set('idToNames', idToNames);
+        this.set('metricUrn', metricUrn);
+        fetch(`/data/autocomplete/filters/metric/${metricUrn}`)
+          .then(checkStatus)
+          .then(res => {
+            this.set('filterOptions', res);
+            this.set('dimensions', {dimensions: Object.keys(res)});
+          })
+      })
+      .catch(error => console.error(`Fetch Error =\n`, error));
+  },
+
   actions: {
     onSave() {
-      const url = `/dataset-auto-onboard/metrics?dataset=` + get(this, 'datasetName');
-      fetch(url)
-        .then(res => res.json())
-        .then(res => this.set('metrics', res))
-        .then(res => this.set('metricsProperties', res.reduce(function (obj, metric) {
-          obj[metric["name"]] = {
-            "id": metric['id'], "urn": "thirdeye:metric:" + metric['id'], "monitor": true
-          };
-          return obj;
-        }, {})))
-        .then(res => {
-          this.set('metrics', Object.keys(res));
-          const metricUrn = res[Object.keys(res)[0]]['id'];
-          this.set('metricUrn', res[Object.keys(res)[0]]['id']);
-          fetch(`/data/autocomplete/filters/metric/${metricUrn}`)
-            .then(checkStatus)
-            .then(res => {
-              this.set('filterOptions', res);
-              this.set('dimensions', {dimensions: Object.keys(res)});
-            })
-        })
-        .catch(error => console.error(`Fetch Error =\n`, error));
+      this._datasetNameChanged();
     },
 
     toggleCheckBox(name) {
       const metricsProperties = get(this, 'metricsProperties');
-      metricsProperties[name]['monitor'] = !metricsProperties[name]['monitor'];
+      set(metricsProperties[name], 'monitor', !metricsProperties[name]['monitor']);
     },
 
     onChangeValue(property, value) {
       const metricsProperties = get(this, 'metricsProperties');
-      Object.keys(metricsProperties).forEach(
-        function (key) {
-          metricsProperties[key][property] = value;
-        }
-      );
+      this.set(property, value);
+      Object.keys(metricsProperties).forEach(function (key) {
+        metricsProperties[key][property] = value;
+      });
     },
 
     onFilters(filters) {
+      this.set('selectedFilters', filters);
       const metricsProperties = get(this, 'metricsProperties');
       const filterMap = JSON.parse(filters);
       Object.keys(metricsProperties).forEach(function (key) {
@@ -132,28 +217,26 @@ export default Controller.extend({
           return;
         }
         const detectionConfig = {
-              className: "com.linkedin.thirdeye.detection.algorithm.DimensionWrapper",
-              nested: [{
-                className: "com.linkedin.thirdeye.detection.algorithm.MovingWindowAlgorithm",
-                baselineWeeks: 4,
-                windowSize: "4 weeks",
-                changeDuration: "7d",
-                outlierDuration: "12h",
-                aucMin: -10,
-                zscoreMin: -4,
-                zscoreMax: 4
-              }]
+          className: "com.linkedin.thirdeye.detection.algorithm.DimensionWrapper", nested: [{
+            className: "com.linkedin.thirdeye.detection.algorithm.MovingWindowAlgorithm",
+            baselineWeeks: 4,
+            windowSize: "4 weeks",
+            changeDuration: "7d",
+            outlierDuration: "12h",
+            aucMin: -10,
+            zscoreMin: -4,
+            zscoreMax: 4
+          }]
         };
-        if(selectedMetric == null) {
+        if (selectedMetric == null) {
           detectionConfig['metricUrn'] = properties['urn'];
         } else {
           detectionConfig['metricUrn'] = metricsProperties[selectedMetric]['urn'];
           detectionConfig['nestedMetricUrn'] = properties['urn'];
         }
 
-        if (selectedDimensions != null){
-          debugger;
-          detectionConfig['dimensions'] = selectedDimensions;
+        if (selectedDimensions != null) {
+          detectionConfig['dimensions'] = selectedDimensions['dimensions'];
         }
         if (properties['topk']) {
           detectionConfig['k'] = parseInt(properties['topk']);
@@ -168,13 +251,11 @@ export default Controller.extend({
       });
 
       const configResult = {
-        "cron": "45 10/15 * * * ? *",
-        "name": get(this, 'detectionConfigName'),
-        "lastTimestamp": 0,
-        "properties": {
+        "cron": "45 10/15 * * * ? *", "name": get(this, 'detectionConfigName'), "lastTimestamp": 0, "properties": {
           "className": "com.linkedin.thirdeye.detection.algorithm.MergeWrapper",
           "maxGap": 7200000,
-          "nested": nestedProperties
+          "nested": nestedProperties,
+          "datasetName": get(this, 'datasetName')
         }
       };
 
@@ -182,8 +263,7 @@ export default Controller.extend({
     },
 
     onSelectDimension(dims) {
-      const dimsMap = JSON.parse(dims);
-      this.set('selectedDimensions', dimsMap['dimensions']);
+      this.set('selectedDimensions', dims);
     },
 
     onChangeName(name) {
--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dataset/DataSetAutoOnboardResource.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/dataset/DataSetAutoOnboardResource.java
@@ -1,11 +1,15 @@
 package com.linkedin.thirdeye.dataset;
 
+import com.linkedin.thirdeye.datalayer.bao.DatasetConfigManager;
+import com.linkedin.thirdeye.datalayer.bao.DetectionConfigManager;
 import com.linkedin.thirdeye.datalayer.bao.MetricConfigManager;
+import com.linkedin.thirdeye.datalayer.dto.DetectionConfigDTO;
 import com.linkedin.thirdeye.datalayer.dto.MetricConfigDTO;
 import com.linkedin.thirdeye.datasource.DAORegistry;
 import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -17,17 +21,28 @@ import javax.ws.rs.core.Response;
 public class DataSetAutoOnboardResource {
   private static final DAORegistry DAO_REGISTRY = DAORegistry.getInstance();
   private final MetricConfigManager metricDAO;
+  private final DetectionConfigManager detectionDAO;
+  private final DatasetConfigManager datasetDAO;
 
   public DataSetAutoOnboardResource() {
     this.metricDAO = DAO_REGISTRY.getMetricConfigDAO();
+    this.detectionDAO = DAO_REGISTRY.getDetectionConfigManager();
+    this.datasetDAO = DAO_REGISTRY.getDatasetConfigDAO();
   }
 
   @GET
   @Path("/metrics")
-  public Response detectionPreview(@QueryParam("dataset") String dataSet) throws Exception {
+  public Response detectionPreview(@QueryParam("dataset") String dataSet) {
     List<MetricConfigDTO> metrics = this.metricDAO.findByDataset(dataSet);
     return Response.ok(metrics).build();
   }
+
+  @GET
+  @Path("/{detectionId}")
+  public DetectionConfigDTO getDetectionConfig(@PathParam("detectionId") long detectionId) {
+    return this.detectionDAO.findById(detectionId);
+  }
+
 
 }
 
