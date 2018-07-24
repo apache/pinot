258b0a6bac19a69b571bf8842b8b3aa93601c912
Jennifer Dai <jdai@linkedin.com>
2018-07-24 16:27:58 -0700

    Updating copy methods

--- pinot-hadoop-filesystem/src/main/java/com/linkedin/pinot/filesystem/AzurePinotFS.java
+++ pinot-hadoop-filesystem/src/main/java/com/linkedin/pinot/filesystem/AzurePinotFS.java
@@ -3,14 +3,13 @@ package com.linkedin.pinot.filesystem;
 import com.microsoft.azure.datalake.store.ADLStoreClient;
 import com.microsoft.azure.datalake.store.DirectoryEntry;
 import com.microsoft.azure.datalake.store.oauth2.AccessTokenProvider;
-import com.microsoft.azure.datalake.store.oauth2.DeviceCodeTokenProvider;
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.blob.CloudBlob;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.datalake.store.oauth2.ClientCredsTokenProvider;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.configuration.Configuration;
@@ -20,78 +19,65 @@ import org.slf4j.LoggerFactory;
 
 public class AzurePinotFS extends PinotFS {
   private static final Logger LOGGER = LoggerFactory.getLogger(AzurePinotFS.class);
-  private static String _accountFQDN;  // full account FQDN, not just the account name
-  private static String _nativeAppId;
-  private static AccessTokenProvider _provider;
-  public static String _storageConnectionString;
-  private static String _container;
+  private ADLStoreClient _adlStoreClient;
+
+  private static final String AZURE = "azure";
+  private static final String ACCOUNT = "account";
+  private static final String AUTHENDPOINT = "authendpoint";
+  private static final String ClIENTID = "clientid";
+  private static final String CLIENTSECRET = "clientsecret";
 
   @Override
   public void init(Configuration config) {
-//    AdlFileSystem adlFileSystem = new AdlFileSystem();
-//    adlFileSystem.initialize(config.getString("uri"), config);
-    _accountFQDN = config.getString("accountFQDN");
-    _nativeAppId = config.getString("nativeAppId");
-    try {
-      _provider = new DeviceCodeTokenProvider(_nativeAppId);
-    } catch (IOException e) {
-      LOGGER.error("Could not instantiate provider");
-      throw new RuntimeException(e);
-    }
-    _storageConnectionString =
-        "DefaultEndpointsProtocol=https;" +
-            "AccountName=" + config.getString("accountName") + ";" +
-            "AccountKey=" + config.getString("accountKey") + ";";
-    _container = config.getString("container");
+    Configuration azureConfigs = config.subset(AZURE);
+    // The ADL account id. Example: {@code mystore.azuredatalakestore.net}.
+    String account = azureConfigs.getString(ACCOUNT);
+    // The endpoint that should be used for authentication.
+    // Usually of the form {@code https://login.microsoftonline.com/<tenant-id>/oauth2/token}.
+    String authEndpoint = azureConfigs.getString(AUTHENDPOINT);
+    // The clientId used to authenticate this application
+    String clientId = azureConfigs.getString(ClIENTID);
+    // The secret key used to authenticate this application
+    String clientSecret = azureConfigs.getString(CLIENTSECRET);
+
+    AccessTokenProvider tokenProvider =
+        new ClientCredsTokenProvider(authEndpoint, clientId, clientSecret);
+    _adlStoreClient = ADLStoreClient.createClient(account, tokenProvider);
   }
 
   @Override
   public boolean delete(URI segmentUri) throws IOException {
-    ADLStoreClient client = ADLStoreClient.createClient(_accountFQDN, _provider);
-    return client.deleteRecursive(segmentUri.getPath());
+    return _adlStoreClient.deleteRecursive(segmentUri.getPath());
   }
 
   @Override
   public boolean move(URI srcUri, URI dstUri) throws IOException {
-    ADLStoreClient client = ADLStoreClient.createClient(_accountFQDN, _provider);
     //rename the file
-    return client.rename(srcUri.getPath(), dstUri.getPath());
+    return _adlStoreClient.rename(srcUri.getPath(), dstUri.getPath());
   }
 
   @Override
   public boolean copy(URI srcUri, URI dstUri) throws IOException {
-    try {
-      CloudStorageAccount storageAccount = CloudStorageAccount.parse(_storageConnectionString);
-      CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-      CloudBlobContainer container = blobClient.getContainerReference(_container);
-      CloudBlob blob = container.getBlockBlobReference(dstUri.getPath());
-      blob.startCopyFromBlob(srcUri);
-    } catch (Exception e) {
-      LOGGER.error("Could not copy from {} to {}", srcUri, dstUri);
-      throw new RuntimeException(e);
-    }
-    return true;
+    // TODO: Add this method. Not needed for now because file will be in final location
+    throw new UnsupportedOperationException("Cannot copy from AzureFS to AzureFS");
   }
 
   @Override
   public boolean exists(URI fileUri) throws IOException {
-    ADLStoreClient client = ADLStoreClient.createClient(_accountFQDN, _provider);
-    return client.checkExists(fileUri.getPath());
+    return _adlStoreClient.checkExists(fileUri.getPath());
   }
 
   @Override
   public long length(URI fileUri) throws IOException {
-    ADLStoreClient client = ADLStoreClient.createClient(_accountFQDN, _provider);
     // get file metadata
-    DirectoryEntry ent = client.getDirectoryEntry(fileUri.getPath());
+    DirectoryEntry ent = _adlStoreClient.getDirectoryEntry(fileUri.getPath());
     return ent.length;
   }
 
   @Override
   public String[] listFiles(URI fileUri) throws IOException {
-    ADLStoreClient client = ADLStoreClient.createClient(_accountFQDN, _provider);
     // list directory contents
-    List<DirectoryEntry> list = client.enumerateDirectory(fileUri.getPath(), Integer.MAX_VALUE);
+    List<DirectoryEntry> list = _adlStoreClient.enumerateDirectory(fileUri.getPath(), Integer.MAX_VALUE);
     List<String> fileNames = new ArrayList<>();
     for (DirectoryEntry directoryEntry : list) {
       fileNames.add(directoryEntry.name);
@@ -101,38 +87,24 @@ public class AzurePinotFS extends PinotFS {
 
   @Override
   public void copyToLocalFile(URI srcUri, URI dstUri) throws IOException {
-    try {
-      CloudStorageAccount storageAccount = CloudStorageAccount.parse(_storageConnectionString);
-      CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-      CloudBlobContainer container = blobClient.getContainerReference(_container);
-      CloudBlob blob = container.getBlockBlobReference(srcUri.getPath());
-      blob.downloadToFile(dstUri.getPath());
-    } catch (Exception e) {
-      LOGGER.error("Could not copy to local from {} to {}", srcUri, dstUri);
-      throw new RuntimeException(e);
+    try (InputStream adlStream = _adlStoreClient.getReadStream(srcUri.getPath())) {
+      Path dstFilePath = Paths.get(dstUri);
+
+      /* Copy the source file to the destination directory as a file with the same name as the source,
+       * replace an existing file with the same name in the destination directory, if any.
+       * Set new file permissions on the copied file.
+       */
+      Files.copy(adlStream, dstFilePath);
+
+      LOGGER.info("Copied file {} from ADL to {}", srcUri, dstUri);
+    } catch (Exception ex) {
+      throw ex;
     }
   }
 
   @Override
   public void copyFromLocalFile(URI srcUri, URI dstUri) throws IOException {
-//    Writer output = new BufferedWriter(new FileWriter(srcUri.getPath()));
-//    output.write("Hello Azure!");
-//    output.close();
-
-    //Getting a blob reference
-    try {
-      CloudStorageAccount storageAccount = CloudStorageAccount.parse(_storageConnectionString);
-      CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
-      CloudBlobContainer container = blobClient.getContainerReference(_container);
-      CloudBlockBlob blob = container.getBlockBlobReference(srcUri.getPath());
-
-      //Creating blob and uploading file to it
-      blob.uploadFromFile(srcUri.getPath());
-
-    } catch (Exception e) {
-      LOGGER.error("Could not copy from local, src {} and dst {}", srcUri, dstUri);
-      throw new RuntimeException(e);
-    }
-
+    // TODO: Add this method. Not needed for now because of metadata upload
+    throw new UnsupportedOperationException("Cannot copy from local to Azure");
   }
 }
