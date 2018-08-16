30ba267405ae70ab188017df8e1fd0bdfd3950c2
Jihao Zhang <jihzhang@linkedin.com>
2018-08-16 09:25:10 -0700

    [TE] Support legacy alert filter in anomaly function translator (#3083)
    This PR enables the support for legacy alert filter translation in the migration endpoint.

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detection/LegacyAnomalyFunctionTranslator.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detection/LegacyAnomalyFunctionTranslator.java
@@ -1,12 +1,15 @@
 package com.linkedin.thirdeye.detection;
 
 import com.google.common.base.Strings;
-import com.linkedin.thirdeye.datalayer.bao.DetectionConfigManager;
 import com.linkedin.thirdeye.datalayer.bao.MetricConfigManager;
 import com.linkedin.thirdeye.datalayer.dto.AnomalyFunctionDTO;
 import com.linkedin.thirdeye.datalayer.dto.DetectionConfigDTO;
 import com.linkedin.thirdeye.datalayer.dto.MetricConfigDTO;
 import com.linkedin.thirdeye.datasource.DAORegistry;
+import com.linkedin.thirdeye.detection.algorithm.LegacyAlertFilterWrapper;
+import com.linkedin.thirdeye.detection.algorithm.LegacyAnomalyFunctionAlgorithm;
+import com.linkedin.thirdeye.detection.algorithm.LegacyMergeWrapper;
+import com.linkedin.thirdeye.detector.email.filter.AlertFilterFactory;
 import com.linkedin.thirdeye.detector.function.AnomalyFunctionFactory;
 import com.linkedin.thirdeye.rootcause.impl.MetricEntity;
 import com.linkedin.thirdeye.util.ThirdEyeUtils;
@@ -23,34 +26,38 @@ import org.slf4j.LoggerFactory;
 public class LegacyAnomalyFunctionTranslator {
   private static final DAORegistry DAO_REGISTRY = DAORegistry.getInstance();
   private static final Logger LOGGER = LoggerFactory.getLogger(LegacyAnomalyFunctionTranslator.class);
-  private DetectionConfigManager detectionConfigDAO;
+
+  private static final String PROP_LEGACY_ALERT_FILTER_CLASS_NAME = "legacyAlertFilterClassName";
+  private static final String PROP_CLASS_NAME = "className";
+  private static final String PROP_METRIC_URN = "metricUrn";
+  private static final String PROP_NESTED = "nested";
+
   private MetricConfigManager metricConfigDAO;
   private final AnomalyFunctionFactory anomalyFunctionFactory;
+  private final AlertFilterFactory alertFilterFactory;
 
   /**
    * Instantiates a new Legacy Anomaly function translator.
    */
-  public LegacyAnomalyFunctionTranslator(AnomalyFunctionFactory anomalyFunctionFactory) {
-    this.detectionConfigDAO = DAO_REGISTRY.getDetectionConfigManager();
+  public LegacyAnomalyFunctionTranslator(AnomalyFunctionFactory anomalyFunctionFactory, AlertFilterFactory alertFilterFactory) {
     this.metricConfigDAO = DAO_REGISTRY.getMetricConfigDAO();
     this.anomalyFunctionFactory = anomalyFunctionFactory;
+    this.alertFilterFactory = alertFilterFactory;
   }
 
   /**
-   * Translate legacy anomaly function to generate a new detection config and persist it into the database.
+   * Translate a legacy anomaly function to a new detection config.
    */
-  public void translate(AnomalyFunctionDTO anomalyFunctionDTO) {
+  public DetectionConfigDTO translate(AnomalyFunctionDTO anomalyFunctionDTO) throws Exception {
     Map<String, Object> properties = new HashMap<>();
-    properties.put("specs", anomalyFunctionDTO);
-    properties.put("className", "com.linkedin.thirdeye.detection.algorithm.LegacyMergeWrapper");
-    properties.put("anomalyFunctionClassName",
-        anomalyFunctionFactory.getClassNameForFunctionType(anomalyFunctionDTO.getType()));
+    properties.put(PROP_CLASS_NAME, LegacyMergeWrapper.class);
+    properties.put("anomalyFunctionClassName", anomalyFunctionFactory.getClassNameForFunctionType(anomalyFunctionDTO.getType()));
 
     String filters = anomalyFunctionDTO.getFilters();
     MetricConfigDTO metricDTO = this.metricConfigDAO.findByMetricAndDataset(anomalyFunctionDTO.getMetric(), anomalyFunctionDTO.getCollection());
     if (metricDTO == null) {
       LOGGER.error("Cannot find metric {} for anomaly function {}", anomalyFunctionDTO.getMetric(), anomalyFunctionDTO.getFunctionName());
-      return;
+      throw new Exception("Cannot find metric");
     }
     anomalyFunctionDTO.setMetricId(metricDTO.getId());
 
@@ -58,26 +65,44 @@ public class LegacyAnomalyFunctionTranslator {
     String metricUrn = me.getUrn();
 
     Map<String, Object> legacyAnomalyFunctionProperties = new HashMap<>();
-    legacyAnomalyFunctionProperties.put("className",
-        "com.linkedin.thirdeye.detection.algorithm.LegacyAnomalyFunctionAlgorithm");
+    legacyAnomalyFunctionProperties.put(PROP_CLASS_NAME, LegacyAnomalyFunctionAlgorithm.class);
     Map<String, Object> nestedProperties = new HashMap<>();
 
     if (!Strings.isNullOrEmpty(anomalyFunctionDTO.getExploreDimensions())) {
       // if anomaly function does dimension exploration, then plug in the dimension wrapper
-      nestedProperties.put("className", "com.linkedin.thirdeye.detection.algorithm.LegacyDimensionWrapper");
-      nestedProperties.put("metricUrn", metricUrn);
-      nestedProperties.put("nested", Collections.singletonList(legacyAnomalyFunctionProperties));
-      properties.put("nested", Collections.singletonList(nestedProperties));
+      nestedProperties.put(PROP_CLASS_NAME, LegacyAnomalyFunctionAlgorithm.class);
+      nestedProperties.put(PROP_METRIC_URN, metricUrn);
+      nestedProperties.put(PROP_NESTED, Collections.singletonList(legacyAnomalyFunctionProperties));
+      properties.put(PROP_NESTED, Collections.singletonList(nestedProperties));
     } else {
-      legacyAnomalyFunctionProperties.put("metricUrn", metricUrn);
-      properties.put("nested", Collections.singletonList(legacyAnomalyFunctionProperties));
+      legacyAnomalyFunctionProperties.put(PROP_METRIC_URN, metricUrn);
+      properties.put(PROP_NESTED, Collections.singletonList(legacyAnomalyFunctionProperties));
     }
 
+    // plug in alert filter if exists
+    Map<String, String> alertFilter = anomalyFunctionDTO.getAlertFilter();
+    if (alertFilter != null) {
+      Map<String, Object> alertFilterProp = new HashMap<>();
+      alertFilterProp.put(PROP_CLASS_NAME, LegacyAlertFilterWrapper.class);
+      String alertFilterType = alertFilter.get("type");
+
+      if (alertFilterType != null) {
+        String alertFilterClassName = this.alertFilterFactory.getClassNameForAlertFilterType(alertFilterType.toUpperCase());
+        if (alertFilterClassName != null) {
+          alertFilterProp.put(PROP_LEGACY_ALERT_FILTER_CLASS_NAME, alertFilterClassName);
+        }
+      }
+
+      alertFilterProp.put(PROP_NESTED, Collections.singletonList(properties));
+      properties = alertFilterProp;
+    }
+
+    properties.put("specs", anomalyFunctionDTO);
     DetectionConfigDTO config = new DetectionConfigDTO();
     config.setName(anomalyFunctionDTO.getFunctionName());
     config.setCron(anomalyFunctionDTO.getCron());
     config.setProperties(properties);
     config.setActive(true);
-    this.detectionConfigDAO.save(config);
+    return config;
   }
 }
