29a5f45301ae6bbe62a926a9c73d38e5f5cd8eb6
Jihao Zhang <jihzhang@linkedin.com>
2018-08-22 11:06:20 -0700

    yaml translator v1

---
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detection/yaml/YamlDetectionConfigTranslator.java
@@ -0,0 +1,4 @@
+package com.linkedin.thirdeye.detection.yaml;
+
+public class YamlDetectionConfigTranslator {
+}
---
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/detection/yaml/YamlTranslatorInfoMap.java
@@ -0,0 +1,20 @@
+package com.linkedin.thirdeye.detection.yaml;
+
+import com.google.common.collect.ImmutableMap;
+import com.linkedin.thirdeye.detection.algorithm.BaselineAlgorithm;
+import com.linkedin.thirdeye.detection.algorithm.BaselineRuleFilterWrapper;
+import com.linkedin.thirdeye.detection.algorithm.MovingWindowAlgorithm;
+import java.util.Map;
+
+
+public class YamlTranslatorInfo {
+  private static final Map<String, String> YAML_INFO_MAP = ImmutableMap.<String, String>builder()
+      .put("BASELINE", String.valueOf(BaselineAlgorithm.class.getName()))
+      .put("BUSINESS_RULE_FILTER", BaselineRuleFilterWrapper.class.getName())
+      .put("MOVING_WINDOW", MovingWindowAlgorithm.class.getName())
+      .build();
+
+  public String get(String key) {
+    return YAML_INFO_MAP.get(key);
+  }
+}
