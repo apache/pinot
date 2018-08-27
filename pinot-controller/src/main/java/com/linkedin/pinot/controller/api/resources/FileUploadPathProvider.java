a2a9b1fe78f89ea149c9aec169129859b86877b7
Jennifer Dai <jenniferdai@users.noreply.github.com>
2018-08-27 14:31:05 -0700

    Changing FileUploadPathProvider to use PinotFS (#3113)
    * In order to support segment move to and from remote filesystems during segment, we need to make FileUploadPathProvider storage agnostic.

--- pinot-controller/src/main/java/com/linkedin/pinot/controller/api/resources/FileUploadPathProvider.java
+++ pinot-controller/src/main/java/com/linkedin/pinot/controller/api/resources/FileUploadPathProvider.java
@@ -15,42 +15,74 @@
  */
 package com.linkedin.pinot.controller.api.resources;
 
-import java.io.File;
-import org.apache.commons.io.FileUtils;
+import com.linkedin.pinot.common.Utils;
 import com.linkedin.pinot.controller.ControllerConf;
+import com.linkedin.pinot.filesystem.PinotFS;
+import com.linkedin.pinot.filesystem.PinotFSFactory;
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 // TODO This is a misc class used by many jersey apis. Need to rename it correctly
 public class FileUploadPathProvider {
   public final static String STATE = "state";
   public final static String TABLE_NAME = "tableName";
+  private final static String DEFAULT_SCHEME = "file";
+  private final static String FILE_UPLOAD_TEMP_PATH = "/fileUploadTemp";
+  private final static String UNTARRED_PATH = "/untarred";
+  private final static String SCHEMAS_TEMP = "/schemasTemp";
+  private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadPathProvider.class);
 
   private final ControllerConf _controllerConf;
 
-  private final File _fileUploadTmpDir;
-  private final File _baseDataDir;
-  private final File _tmpUntarredPath;
-  private final File _schemasTmpDir;
+  private final URI _fileUploadTmpDirURI;
+  private final URI _baseDataDirURI;
+  private final URI _tmpUntarredPathURI;
+  private final URI _schemasTmpDirURI;
   private final String _vip;
 
   public FileUploadPathProvider(ControllerConf controllerConf) throws InvalidControllerConfigException {
     _controllerConf = controllerConf;
+
+    // Pick the correct scheme for PinotFS. For pluggable storage, we will expect that the directory is configured
+    // with a storage prefix. For backwards compatibility, we will assume local/mounted NFS storage if nothing is configured.
+    String scheme = DEFAULT_SCHEME;
+    String dataDir = _controllerConf.getDataDir();
     try {
-      _baseDataDir = new File(_controllerConf.getDataDir());
-      if (!_baseDataDir.exists()) {
-        FileUtils.forceMkdir(_baseDataDir);
+      URI uri = new URI(_controllerConf.getDataDir());
+      String dataDirScheme = uri.getScheme();
+      if (dataDirScheme ==  null) {
+        // Assume local fs
+        dataDir = "file://" + _controllerConf.getDataDir();
+      } else {
+        scheme = dataDirScheme;
       }
-      _fileUploadTmpDir = new File(_baseDataDir, "fileUploadTemp");
-      if (!_fileUploadTmpDir.exists()) {
-        FileUtils.forceMkdir(_fileUploadTmpDir);
+    } catch (URISyntaxException e) {
+      LOGGER.error("Invalid path set as data dir {}", _controllerConf.getDataDir());
+      Utils.rethrowException(e);
+    }
+
+    PinotFS pinotFS = PinotFSFactory.create(scheme);
+
+    try {
+      _baseDataDirURI = new URI(dataDir);
+      if (!pinotFS.exists(_baseDataDirURI)) {
+        pinotFS.mkdir(_baseDataDirURI);
       }
-      _tmpUntarredPath = new File(_fileUploadTmpDir, "untarred");
-      if (!_tmpUntarredPath.exists()) {
-        _tmpUntarredPath.mkdirs();
+      _fileUploadTmpDirURI = new URI(_baseDataDirURI + FILE_UPLOAD_TEMP_PATH);
+      if (!pinotFS.exists(_fileUploadTmpDirURI)) {
+        pinotFS.mkdir(_fileUploadTmpDirURI);
       }
-      _schemasTmpDir = new File(_baseDataDir, "schemasTemp");
-      if (!_schemasTmpDir.exists()) {
-        FileUtils.forceMkdir(_schemasTmpDir);
+      _tmpUntarredPathURI = new URI(_fileUploadTmpDirURI + UNTARRED_PATH);
+      if (!pinotFS.exists(_tmpUntarredPathURI)) {
+        pinotFS.mkdir(_tmpUntarredPathURI);
+      }
+      _schemasTmpDirURI = new URI(_baseDataDirURI + SCHEMAS_TEMP);
+      if (!pinotFS.exists(_schemasTmpDirURI)) {
+        pinotFS.mkdir(_schemasTmpDirURI);
       }
       _vip = _controllerConf.generateVipUrl();
     } catch (Exception e) {
@@ -58,23 +90,41 @@ public class FileUploadPathProvider {
     }
   }
 
-  public File getFileUploadTmpDir() {
-    return _fileUploadTmpDir;
-  }
-
-  public File getBaseDataDir() {
-    return _baseDataDir;
-  }
-
-  public File getTmpUntarredPath() {
-    return _tmpUntarredPath;
-  }
-
   public String getVip() {
     return _vip;
   }
 
+  public URI getFileUploadTmpDirURI() {
+    return _fileUploadTmpDirURI;
+  }
+
+  public URI getBaseDataDirURI() {
+    return _baseDataDirURI;
+  }
+
+  public URI getTmpUntarredPathURI() {
+    return _tmpUntarredPathURI;
+  }
+
+  public URI getSchemasTmpDirURI() {
+    return _schemasTmpDirURI;
+  }
+
+  // TODO: The following getters that return files should eventually be migrated to URIs once pluggable storage support
+  // is complete throughout the system. Leaving this here for backwards compatibility for now.
+  public File getFileUploadTmpDir() {
+    return new File(_fileUploadTmpDirURI);
+  }
+
+  public File getBaseDataDir() {
+    return new File(_baseDataDirURI);
+  }
+
+  public File getTmpUntarredPath() {
+    return new File(_tmpUntarredPathURI);
+  }
+
   public File getSchemasTmpDir() {
-    return _schemasTmpDir;
+    return new File(_schemasTmpDirURI);
   }
 }
