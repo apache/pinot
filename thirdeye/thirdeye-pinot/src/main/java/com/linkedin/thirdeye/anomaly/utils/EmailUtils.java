7de9b73869c435d8cbcdada46dd076412259d809
Akshay Rai <akrai@linkedin.com>
2018-09-04 09:22:33 -0700

    [TE] Bug Fix - Emails not sent out to all the recipients
    Issue:
    Comma separated emails having whitespaces are being discarded
    
    Changes:
    * Trim whitespaces before validating email
    * Replace REGEX with InternetAddress

--- thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/utils/EmailUtils.java
+++ thirdeye/thirdeye-pinot/src/main/java/com/linkedin/thirdeye/anomaly/utils/EmailUtils.java
@@ -5,27 +5,36 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class EmailUtils {
   private static final Logger LOG = LoggerFactory.getLogger(EmailUtils.class);
-  public static Pattern EMAIL_REGEX_PATTERN = Pattern.compile(
-      "\\b[\\w.%+\\-]+@[-.\\w]+\\.[A-Za-z]{2,4}\\b", Pattern.CASE_INSENSITIVE);
+
   /**
    * Check if given email is a valid email
    * @param email
    * @return
    */
   public static boolean isValidEmailAddress(String email) {
-    return EMAIL_REGEX_PATTERN.matcher(email).matches();
+    try {
+      Validate.notEmpty(email);
+      InternetAddress emailAddr = new InternetAddress(email);
+      emailAddr.validate();
+      return true;
+    } catch (AddressException | IllegalArgumentException e) {
+      return false;
+    }
   }
 
   /**
    * Return a list of valid email addresses
-   * @param emails
+   * @param emails comma separated list of emails
    * @return
    */
   public static String getValidEmailAddresses(String emails) {
@@ -37,6 +46,7 @@ public class EmailUtils {
     } else {
       String[] emailArr = emails.split(",");
       for (String email : emailArr) {
+        email = email.trim();
         if (isValidEmailAddress(email) && !addedEmailAddresses.contains(email)) {
           emailAddressList.add(email);
           addedEmailAddresses.add(email);
