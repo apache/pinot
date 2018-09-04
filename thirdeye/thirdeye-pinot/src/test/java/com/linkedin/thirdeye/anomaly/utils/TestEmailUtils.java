028ea549717947a25b5a0678791ecc7603b6fbe8
Akshay Rai <akshayrai09@gmail.com>
2018-09-04 10:43:01 -0700

    [TE] Bug Fix - Emails not sent out to all the recipients (#3136)
    Issue:
    Comma separated emails having whitespaces are being discarded
    
    Changes:
    * Trim whitespaces before validating email
    * Replace REGEX with InternetAddress

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomaly/utils/TestEmailUtils.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/anomaly/utils/TestEmailUtils.java
@@ -7,16 +7,21 @@ import org.testng.annotations.Test;
 public class TestEmailUtils {
   @Test
   public void testIsValidEmailAddress() {
-    Assert.assertTrue(EmailUtils.isValidEmailAddress("abc@cba.abc"));
-    Assert.assertFalse(EmailUtils.isValidEmailAddress("abc"));
-  }
+    Assert.assertTrue(EmailUtils.isValidEmailAddress("user@host.domain"));
 
+    Assert.assertFalse(EmailUtils.isValidEmailAddress(null));
+    Assert.assertFalse(EmailUtils.isValidEmailAddress("    "));
+    Assert.assertFalse(EmailUtils.isValidEmailAddress("user"));
+    Assert.assertFalse(EmailUtils.isValidEmailAddress("@host"));
+    Assert.assertFalse(EmailUtils.isValidEmailAddress("u ser@host.domain"));
+  }
 
   @Test
   public void testGetValidEmailAddresses() {
-    String emailAddresses = "abc@cba.abc,bcd@dcb.bcd";
+    String emailAddresses = "user1@host1.domain1,user2@host1.domain1";
     Assert.assertEquals(EmailUtils.getValidEmailAddresses(emailAddresses), emailAddresses);
-    Assert.assertEquals(EmailUtils.getValidEmailAddresses(emailAddresses + ",abc"), emailAddresses);
-    Assert.assertEquals(EmailUtils.getValidEmailAddresses(emailAddresses + ",abc@cba.abc"), emailAddresses);
+    Assert.assertEquals(EmailUtils.getValidEmailAddresses(emailAddresses + ",user"), emailAddresses);
+    Assert.assertEquals(EmailUtils.getValidEmailAddresses(emailAddresses + ",user1@host1.domain1"), emailAddresses);
+    Assert.assertEquals(EmailUtils.getValidEmailAddresses(emailAddresses + ",, , user1@host1.domain1,  "), emailAddresses);
   }
 }
