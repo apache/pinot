13b9dc45041b4e6105bb99da781d4d75b64335e5
Jennifer Dai <jdai@linkedin.com>
2018-09-20 15:55:14 -0700

    Moving azure to its own module

--- pinot-azure-filesystem/src/test/java/com/linkedin/pinot/filesystem/test/AzurePinotFSTest.java
+++ pinot-azure-filesystem/src/test/java/com/linkedin/pinot/filesystem/test/AzurePinotFSTest.java
@@ -58,9 +58,9 @@ public class AzurePinotFSTest {
 
   @Test
   public void testFS() throws Exception {
-    ADLStoreClient adlStoreClient = mock(ADLStoreClient.class);
-    when(adlStoreClient.checkExists(_adlLocation)).thenReturn(true);
-    when(adlStoreClient.checkExists(_testFile.getPath())).thenReturn(true);
+    ADLStoreClient adlStoreClient = Mockito.mock(ADLStoreClient.class);
+    Mockito.when(adlStoreClient.checkExists(_adlLocation)).thenReturn(true);
+    Mockito.when(adlStoreClient.checkExists(_testFile.getPath())).thenReturn(true);
 
     AzurePinotFS azurePinotFS = new AzurePinotFS(adlStoreClient);
     URI testFileURI = _testFile.toURI();
@@ -70,7 +70,7 @@ public class AzurePinotFSTest {
     File file = new File(_adlLocation, "testfile2");
     MockADLFileInputStream adlFileInputStream = new MockADLFileInputStream(
         new ByteArrayInputStream(Files.readAllBytes(Paths.get(testFileURI))));
-    when(adlStoreClient.getReadStream(anyString())).thenReturn(adlFileInputStream);
+    Mockito.when(adlStoreClient.getReadStream(ArgumentMatchers.anyString())).thenReturn(adlFileInputStream);
     azurePinotFS.copyToLocalFile(testFileURI, file);
     Assert.assertTrue(file.exists());
   }
