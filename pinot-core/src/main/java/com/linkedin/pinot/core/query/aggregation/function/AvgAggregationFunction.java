8d5b7e6e876d025707c030298c9bad22e7ccabbe
Xiaotian (Jackie) Jiang <xajiang@linkedin.com>
2018-10-03 13:40:23 -0700

    [Star-Tree V2] Enhance AVG, MIN_MAX_RANGE aggregatin function to work on serialized bytes
    Enhance AVG, MIN_MAX_RANGE aggregatin function to work on serialized bytes
    Star-Tree can generate pre-aggregated value for these aggregations

--- pinot-core/src/main/java/com/linkedin/pinot/core/query/aggregation/function/AvgAggregationFunction.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/query/aggregation/function/AvgAggregationFunction.java
@@ -15,8 +15,10 @@
  */
 package com.linkedin.pinot.core.query.aggregation.function;
 
+import com.linkedin.pinot.common.data.FieldSpec;
 import com.linkedin.pinot.common.utils.DataSchema;
 import com.linkedin.pinot.core.common.BlockValSet;
+import com.linkedin.pinot.core.common.ObjectSerDeUtils;
 import com.linkedin.pinot.core.query.aggregation.AggregationResultHolder;
 import com.linkedin.pinot.core.query.aggregation.ObjectAggregationResultHolder;
 import com.linkedin.pinot.core.query.aggregation.function.customobject.AvgPair;
@@ -60,12 +62,34 @@ public class AvgAggregationFunction implements AggregationFunction<AvgPair, Doub
   @Override
   public void aggregate(int length, @Nonnull AggregationResultHolder aggregationResultHolder,
       @Nonnull BlockValSet... blockValSets) {
-    double[] valueArray = blockValSets[0].getDoubleValuesSV();
-    double sum = 0.0;
-    for (int i = 0; i < length; i++) {
-      sum += valueArray[i];
+    FieldSpec.DataType valueType = blockValSets[0].getValueType();
+    switch (valueType) {
+      case INT:
+      case LONG:
+      case FLOAT:
+      case DOUBLE:
+        double[] valueArray = blockValSets[0].getDoubleValuesSV();
+        double sum = 0.0;
+        for (int i = 0; i < length; i++) {
+          sum += valueArray[i];
+        }
+        setAggregationResult(aggregationResultHolder, sum, (long) length);
+        break;
+      case BYTES:
+        // Serialized AvgPair
+        byte[][] bytesValues = blockValSets[0].getBytesValuesSV();
+        sum = 0.0;
+        long count = 0L;
+        for (int i = 0; i < length; i++) {
+          AvgPair value = ObjectSerDeUtils.AVG_PAIR_SER_DE.deserialize(bytesValues[i]);
+          sum += value.getSum();
+          count += value.getCount();
+        }
+        setAggregationResult(aggregationResultHolder, sum, count);
+        break;
+      default:
+        throw new IllegalStateException("Illegal data type for AVG aggregation function: " + valueType);
     }
-    setAggregationResult(aggregationResultHolder, sum, (long) length);
   }
 
   protected void setAggregationResult(@Nonnull AggregationResultHolder aggregationResultHolder, double sum,
@@ -81,21 +105,61 @@ public class AvgAggregationFunction implements AggregationFunction<AvgPair, Doub
   @Override
   public void aggregateGroupBySV(int length, @Nonnull int[] groupKeyArray,
       @Nonnull GroupByResultHolder groupByResultHolder, @Nonnull BlockValSet... blockValSets) {
-    double[] valueArray = blockValSets[0].getDoubleValuesSV();
-    for (int i = 0; i < length; i++) {
-      setGroupByResult(groupKeyArray[i], groupByResultHolder, valueArray[i], 1L);
+    FieldSpec.DataType valueType = blockValSets[0].getValueType();
+    switch (valueType) {
+      case INT:
+      case LONG:
+      case FLOAT:
+      case DOUBLE:
+        double[] valueArray = blockValSets[0].getDoubleValuesSV();
+        for (int i = 0; i < length; i++) {
+          setGroupByResult(groupKeyArray[i], groupByResultHolder, valueArray[i], 1L);
+        }
+        break;
+      case BYTES:
+        // Serialized AvgPair
+        byte[][] bytesValues = blockValSets[0].getBytesValuesSV();
+        for (int i = 0; i < length; i++) {
+          AvgPair value = ObjectSerDeUtils.AVG_PAIR_SER_DE.deserialize(bytesValues[i]);
+          setGroupByResult(groupKeyArray[i], groupByResultHolder, value.getSum(), value.getCount());
+        }
+        break;
+      default:
+        throw new IllegalStateException("Illegal data type for AVG aggregation function: " + valueType);
     }
   }
 
   @Override
   public void aggregateGroupByMV(int length, @Nonnull int[][] groupKeysArray,
       @Nonnull GroupByResultHolder groupByResultHolder, @Nonnull BlockValSet... blockValSets) {
-    double[] valueArray = blockValSets[0].getDoubleValuesSV();
-    for (int i = 0; i < length; i++) {
-      double value = valueArray[i];
-      for (int groupKey : groupKeysArray[i]) {
-        setGroupByResult(groupKey, groupByResultHolder, value, 1L);
-      }
+    FieldSpec.DataType valueType = blockValSets[0].getValueType();
+    switch (valueType) {
+      case INT:
+      case LONG:
+      case FLOAT:
+      case DOUBLE:
+        double[] valueArray = blockValSets[0].getDoubleValuesSV();
+        for (int i = 0; i < length; i++) {
+          double value = valueArray[i];
+          for (int groupKey : groupKeysArray[i]) {
+            setGroupByResult(groupKey, groupByResultHolder, value, 1L);
+          }
+        }
+        break;
+      case BYTES:
+        // Serialized AvgPair
+        byte[][] bytesValues = blockValSets[0].getBytesValuesSV();
+        for (int i = 0; i < length; i++) {
+          AvgPair value = ObjectSerDeUtils.AVG_PAIR_SER_DE.deserialize(bytesValues[i]);
+          double sum = value.getSum();
+          long count = value.getCount();
+          for (int groupKey : groupKeysArray[i]) {
+            setGroupByResult(groupKey, groupByResultHolder, sum, count);
+          }
+        }
+        break;
+      default:
+        throw new IllegalStateException("Illegal data type for AVG aggregation function: " + valueType);
     }
   }
 
