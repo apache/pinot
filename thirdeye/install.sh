fb35c387097e702c5f95dfd6d5dd2718583430b6
Alexander Pucher <apucher@linkedin.com>
2018-10-04 14:43:13 -0700

    [TE] demo - build core pinot components

--- thirdeye/install.sh
+++ thirdeye/install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 echo "*******************************************************"
-echo "Attempting to build ThirdEye with Maven"
+echo "Building Pinot components"
+echo "*******************************************************"
+
+cd ..
+mvn install -DskipTests -pl pinot-common,pinot-core,pinot-api -am -Pbuild-shaded-jar || exit 1
+cd thirdeye
+
+echo "*******************************************************"
+echo "Building ThirdEye"
 echo "*******************************************************"
 
 mvn install -DskipTests || exit 1
