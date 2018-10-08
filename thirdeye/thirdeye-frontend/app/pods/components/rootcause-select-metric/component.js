apache/pinot

commit ffc61d6a7668f4536656253468359d4af3e608e6

TE rca metric select editable text (#3277)

thirdeye/thirdeye-frontend/app/pods/components/rootcause-select-metric/component.js
@@ -5,13 +5,15 @@ import { selfServeApiCommon } from 'thirdeye-frontend/utils/api/self-serve';
 import { task, timeout } from 'ember-concurrency';
 import _ from 'lodash';
 import { checkStatus } from 'thirdeye-frontend/utils/utils';
-import { get, computed } from '@ember/object';
+import { get, set, computed, getProperties } from '@ember/object';
 export default Component.extend({
   classNames: ['rootcause-select-metric-dimension'],
 
   selectedUrn: null, // ""
 
   onSelection: null, // function (metricUrn)
+
+  onFocus: null,
 
   placeholder: 'Search for a Metric',
 
@@ -23,6 +25,8 @@ export default Component.extend({
   selectedUrnCache: null, // ""
 
   selectedMetric: null, // {}
+
+  searchInputSelector: '#ember-basic-dropdown-wormhole input',
 
   /**
    * @summary Concurrency task that triggers returning the selected metrics as recommended list
@@ -105,6 +109,22 @@ export default Component.extend({
       const updates = { [metricUrn]: true, [toBaselineUrn(metricUrn)]: true, [toCurrentUrn(metricUrn)]: true };
 
       onSelection(updates);
+    },
+
+    /**
+     * Inserts the selected metric alias into the power-select search field for easy modification
+     * @param {Object} selectObj - metric selected
+     */
+    onFocus(selectObj) {
+      const {
+        onFocus,
+        selectionEditable,
+        searchInputSelector
+      } = getProperties(this, 'onFocus', 'selectionEditable', 'searchInputSelector');
+      if (selectionEditable && selectObj.isActive) {
+        const selectInputEl = document.querySelector(searchInputSelector);
+        selectInputEl.value = selectObj.selected.alias;
+      }
     },
 
     /**

thirdeye/thirdeye-frontend/app/pods/components/rootcause-select-metric-dimension/template.hbs
@@ -14,6 +14,7 @@
       entities=entities
       selectedUrns=selectedUrns
       onSelection=(action "onMetric")
+      selectionEditable=true
     }}
   </div>
   <div class="col-xs-7">

thirdeye/thirdeye-frontend/app/pods/components/rootcause-select-metric/template.hbs
@@ -4,6 +4,7 @@
   triggerId="select-metric"
   onchange=(action "onChange")
   search=(action "onSearch")
+  onfocus=(action "onFocus")
   searchPlaceholder=placeholder
   placeholder=placeholder
   matchTriggerWidth=false

commit 773340de54e6f181d4b07d21556528f3ca6688b1

[TE] rootcause - unbreak rca coldstart page (#3281)
Recent changes in #3273 removed the metric selector from the blank RCA page. This PR re-creates the original state an fixes an error on empty selection introduced in #3277

thirdeye/thirdeye-frontend/app/pods/components/rootcause-select-metric/component.js
@@ -41,6 +41,7 @@ export default Component.extend({
     function() {
       const { selectedUrns, entities } = this.getProperties('selectedUrns', 'entities');
       const result = filterPrefix(selectedUrns, 'thirdeye:metric:')
+        .filter(urn => urn in entities)
         .map((urn) => {
           const entity = entities[urn];
           const agg = entity ? { alias: entity.label, id: entity.urn.split(':')[2] } : {};
@@ -117,11 +118,10 @@ export default Component.extend({
      */
     onFocus(selectObj) {
       const {
-        onFocus,
         selectionEditable,
         searchInputSelector
       } = getProperties(this, 'onFocus', 'selectionEditable', 'searchInputSelector');
-      if (selectionEditable && selectObj.isActive) {
+      if (selectionEditable && selectObj.isActive && selectObj.selected) {
         const selectInputEl = document.querySelector(searchInputSelector);
         selectInputEl.value = selectObj.selected.alias;
       }

thirdeye/thirdeye-frontend/app/pods/rootcause/template.hbs
@@ -133,14 +133,12 @@
     <div class="card-container card-container--md card-container--space-around card-container--flush-top">
       <div class="card-container__row card-container__row--flex row">
         <div class="col-xs-10">
-          {{#if selectedUrns.size}}
-            {{rootcause-select-metric-dimension
-              selectedUrn=metricUrn
-              selectedUrns=selectedUrns
-              entities=entities
-              onSelection=(action "onPrimaryChange")
-            }}
-          {{/if}}
+          {{rootcause-select-metric-dimension
+            selectedUrn=metricUrn
+            selectedUrns=selectedUrns
+            entities=entities
+            onSelection=(action "onPrimaryChange")
+          }}
         </div>
         <div class="col-xs-2">
           {{#if context.urns.size}}