bf9c22f985c76f015d27421d568d52a71753af74
Neha Pawar <npawar@linkedin.com>
2018-10-18 13:42:12 -0700

    Add KafkaStreamConfigProperties for kafka specific properties

---
+++ pinot-core/src/main/java/com/linkedin/pinot/core/realtime/impl/kafka/KafkaStreamConfigProperties.java
@@ -0,0 +1,41 @@
+package com.linkedin.pinot.core.realtime.impl.kafka;
+
+import com.google.common.base.Joiner;
+import com.linkedin.pinot.core.realtime.stream.StreamConfigProperties;
+
+
+/**
+ * Property key definitions for all kafka stream related properties
+ */
+public class KafkaStreamConfigProperties {
+  public static final String DOT_SEPARATOR = ".";
+  public static final String STREAM_TYPE = "kafka";
+
+  public static class HighLevelConsumer {
+    public static final String KAFKA_HLC_ZK_CONNECTION_STRING = "kafka.hlc.zk.connect.string";
+    public static final String KAFKA_HLC_GROUP_ID = "kafka.hlc.group.id";
+    public static final String ZK_SESSION_TIMEOUT_MS = "zookeeper.session.timeout.ms";
+    public static final String ZK_CONNECTION_TIMEOUT_MS = "zookeeper.connection.timeout.ms";
+    public static final String ZK_SYNC_TIME_MS = "zookeeper.sync.time.ms";
+    public static final String REBALANCE_MAX_RETRIES = "rebalance.max.retries";
+    public static final String REBALANCE_BACKOFF_MS = "rebalance.backoff.ms";
+    public static final String AUTO_COMMIT_ENABLE = "auto.commit.enable";
+    public static final String AUTO_OFFSET_RESET = "auto.offset.reset";
+  }
+
+  public static class LowLevelConsumer {
+    public static final String KAFKA_BROKER_LIST = "kafka.broker.list";
+  }
+
+  public static final String KAFKA_CONSUMER_PROP_PREFIX = "kafka.consumer.prop";
+
+  /**
+   * Helper method to create a property string for kafka stream
+   * @param property
+   * @return
+   */
+  public static String constructStreamProperty(String property) {
+    return Joiner.on(DOT_SEPARATOR).join(StreamConfigProperties.STREAM_PREFIX, property);
+  }
+}
+
