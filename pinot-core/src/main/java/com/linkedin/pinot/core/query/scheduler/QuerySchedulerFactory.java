870c5d7d49516f46e07466c6e5c808a5b3b4cb7f
haibow <haibow@users.noreply.github.com>
2018-10-31 22:30:08 -0700

    fix schedulerName (#3421)
    * Fix a typo in QuerySchedulerFactory

--- pinot-core/src/main/java/com/linkedin/pinot/core/query/scheduler/QuerySchedulerFactory.java
+++ pinot-core/src/main/java/com/linkedin/pinot/core/query/scheduler/QuerySchedulerFactory.java
@@ -62,7 +62,7 @@ public class QuerySchedulerFactory {
     } else if (schedulerName.equals(TOKEN_BUCKET_ALGORITHM)) {
       LOGGER.info("Using Priority Token Bucket scheduler");
       return TokenPriorityScheduler.create(schedulerConfig, queryExecutor, serverMetrics, latestQueryTime);
-    } else if (schedulerConfig.equals(BOUNDED_FCFS_ALGORITHM)) {
+    } else if (schedulerName.equals(BOUNDED_FCFS_ALGORITHM)) {
       return BoundedFCFSScheduler.create(schedulerConfig, queryExecutor, serverMetrics, latestQueryTime);
     }
 
