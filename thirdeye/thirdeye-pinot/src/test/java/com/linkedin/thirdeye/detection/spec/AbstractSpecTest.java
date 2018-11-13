48a9471a539dbf1fb8da127175f4941f0c265a21
Jihao Zhang <jihzhang@linkedin.com>
2018-11-12 17:33:28 -0800

    [TE] Percentage change and absolute change ule detector

--- thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/detection/spec/AbstractSpecTest.java
+++ thirdeye/thirdeye-pinot/src/test/java/com/linkedin/thirdeye/detection/spec/AbstractSpecTest.java
@@ -24,7 +24,7 @@ import org.testng.annotations.Test;
 
 public class AbstractSpecTest {
   @Test
-  public void testAbstractSpecMapping() {
+  public void testAbstractSpecMappingDefaultValue() {
     TestSpec spec = AbstractSpec.fromProperties(ImmutableMap.of(), TestSpec.class);
     Assert.assertEquals(spec.getA(), 123);
     Assert.assertEquals(spec.getB(), 456.7);
@@ -32,7 +32,7 @@ public class AbstractSpecTest {
   }
 
   @Test
-  public void testAbstractSpecMapping1() {
+  public void testAbstractSpecMappingIncompleteProperty() {
     TestSpec spec = AbstractSpec.fromProperties(ImmutableMap.of("a", 321), TestSpec.class);
     Assert.assertEquals(spec.getA(), 321);
     Assert.assertEquals(spec.getB(), 456.7);
@@ -40,7 +40,7 @@ public class AbstractSpecTest {
   }
 
   @Test
-  public void testAbstractSpecMapping2() {
+  public void testAbstractSpecMappingNestedObject() {
     RuleBaselineProvider provider = new RuleBaselineProvider();
     TestSpec spec = AbstractSpec.fromProperties(ImmutableMap.of("baselineProvider", provider), TestSpec.class);
     Assert.assertEquals(spec.getA(), 123);
@@ -50,12 +50,20 @@ public class AbstractSpecTest {
   }
 
   @Test
-  public void testAbstractSpecMapping3() {
+  public void testAbstractSpecMappingExtraField() {
     TestSpec spec = AbstractSpec.fromProperties(ImmutableMap.of("a", 321, "className", "org.test.Test"), TestSpec.class);
     Assert.assertEquals(spec.getA(), 321);
     Assert.assertEquals(spec.getB(), 456.7);
     Assert.assertEquals(spec.getC(), "default");
   }
 
+  @Test
+  public void testAbstractSpecMappingNestedMap() {
+    TestSpec spec = AbstractSpec.fromProperties(ImmutableMap.of("a", 321, "className", "org.test.Test", "configuration", ImmutableMap.of("k1", "v1", "k2", "v2")), TestSpec.class);
+    Assert.assertEquals(spec.getA(), 321);
+    Assert.assertEquals(spec.getB(), 456.7);
+    Assert.assertEquals(spec.getC(), "default");
+    Assert.assertEquals(spec.getConfiguration(), ImmutableMap.of("k1", "v1", "k2", "v2"));
+  }
 }
 
