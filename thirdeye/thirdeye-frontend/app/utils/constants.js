20dad1bff30fe7ca71c72c680ed69c006ab9d4a9
Harley Jackson <hjackson@linkedin.com>
2018-12-14 16:57:25 -0800

    [TE] frontend - harleyjj/create-alert - YAML autocomplete for create alert

--- thirdeye/thirdeye-frontend/app/utils/constants.js
+++ thirdeye/thirdeye-frontend/app/utils/constants.js
@@ -11,7 +11,7 @@ export default {
 
 
 export const yamlAlertProps = `# give a name for this detection
-detectionName: name_of_the_detection 
+detectionName: name_of_the_detection
 # the metric to detect the anomalies
 metric: metric_name
 # the data set name for this metric
@@ -24,26 +24,26 @@ dimensionExploration:
   # Create an alert for each dimension value in the dimension
   dimensions:
     - dimensionName
-    
-  # (Optional) only create alert for the dimension value if the contribution to 
-  # overall metric is above the threshold 
+
+  # (Optional) only create alert for the dimension value if the contribution to
+  # overall metric is above the threshold
   minContribution: 0.05
-  
+
   # (Optional) only create alert for the top k dimension values
   k: 10
 
-# (Optional) Filter the metric by 
+# (Optional) Filter the metric by
 filters:
   dimensionName1:
      - dimensionValue1
      - dimensionValue2
   dimensionName2:
      - dimensionValue3
-     
+
 # configure rules of anomaly detection
 rules:
 # configure the first rule
-- # configure the detection rule. ThirdEye will detect anomalies based on the 
+- # configure the detection rule. ThirdEye will detect anomalies based on the
   # detection rules.
   detection:
       # give a name for the detection rule
@@ -51,11 +51,11 @@ rules:
       # ThirdEye rule type
       type: PERCENTAGE_RULE
       # parameters for this rule
-      params: 
+      params:
         offset: wo1w
         change: 0.1
- 
-  # (Optional) configure the exclusion rule. (Exclude the anomalies you don't 
+
+  # (Optional) configure the exclusion rule. (Exclude the anomalies you don't
   # want to see but detected by the detection rule above)
   filter:
     - name: filter_rule_1
@@ -73,3 +73,69 @@ rules:
         pattern: UP
 
 `;
+
+export const yamIt = function(metric, dataset){
+  return `# give a name for this detection
+detectionName: name_of_the_detection
+# the metric to detect the anomalies
+metric: ${metric}
+# the data set name for this metric
+dataset: ${dataset}
+# ThirdEye pipeline type. Just fill in Composite
+pipelineType: Composite
+
+# (Optional) Config dimension exploration
+dimensionExploration:
+  # Create an alert for each dimension value in the dimension
+  dimensions:
+    - dimensionName
+
+  # (Optional) only create alert for the dimension value if the contribution to
+  # overall metric is above the threshold
+  minContribution: 0.05
+
+  # (Optional) only create alert for the top k dimension values
+  k: 10
+
+# (Optional) Filter the metric by
+filters:
+  dimensionName1:
+     - dimensionValue1
+     - dimensionValue2
+  dimensionName2:
+     - dimensionValue3
+
+# configure rules of anomaly detection
+rules:
+# configure the first rule
+- # configure the detection rule. ThirdEye will detect anomalies based on the
+  # detection rules.
+  detection:
+      # give a name for the detection rule
+    - name: detection_rule_1
+      # ThirdEye rule type
+      type: PERCENTAGE_RULE
+      # parameters for this rule
+      params:
+        offset: wo1w
+        change: 0.1
+
+  # (Optional) configure the exclusion rule. (Exclude the anomalies you don't
+  # want to see but detected by the detection rule above)
+  filter:
+    - name: filter_rule_1
+      type: ABSOLUTE_CHANGE_FILTER
+      params:
+        threshold: 10000
+
+# configure more rule if you'd like to
+- detection:
+    - name: detection_rule_2
+      type: ABSOLUTE_CHANGE_RULE
+      params:
+        offset: wo1w
+        change: 1000000
+        pattern: UP
+
+`;
+};
