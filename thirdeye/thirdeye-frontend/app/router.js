e5054e24e8b5915af447d48324a304e7a7432c27
Akshay Rai <akshayrai09@gmail.com>
2019-02-15 09:22:38 -0800

    [TE] Enable Piwik tracking ref: #3839wq

--- thirdeye/thirdeye-frontend/app/router.js
+++ thirdeye/thirdeye-frontend/app/router.js
@@ -1,7 +1,8 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
+import Piwik from 'ember-cli-piwik/mixins/page-view-tracker';
 
-const Router = EmberRouter.extend({
+const Router = EmberRouter.extend(Piwik, {
   location: config.locationType,
   rootURL: config.rootURL
 });
