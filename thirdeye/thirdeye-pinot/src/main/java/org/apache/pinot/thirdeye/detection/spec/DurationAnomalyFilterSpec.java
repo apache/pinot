d57b8675b34174a322a5c313fbaf8ea62be14602
Jihao Zhang <jihzhang@linkedin.com>
2019-03-06 15:10:38 -0800

    [TE] detection - add default values for duration filter

--- thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/detection/spec/DurationAnomalyFilterSpec.java
+++ thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/detection/spec/DurationAnomalyFilterSpec.java
@@ -21,8 +21,8 @@
 package org.apache.pinot.thirdeye.detection.spec;
 
 public class DurationAnomalyFilterSpec extends AbstractSpec {
-  private String minDuration;
-  private String maxDuration;
+  private String minDuration = "PT0S";
+  private String maxDuration = "P365D";
 
   public String getMinDuration() {
     return minDuration;
