b68c8633bffa948968e54de68b77c976e60dd323
Jihao Zhang <jihzhang@linkedin.com>
2019-05-07 16:34:05 -0700

    tests

--- thirdeye/thirdeye-pinot/src/test/java/org/apache/pinot/thirdeye/detection/wrapper/AnomalyDetectorWrapperTest.java
+++ thirdeye/thirdeye-pinot/src/test/java/org/apache/pinot/thirdeye/detection/wrapper/AnomalyDetectorWrapperTest.java
@@ -191,4 +191,21 @@ public class AnomalyDetectorWrapperTest {
     Assert.assertEquals(result.getPredictedUpperBound(), DoubleSeries.buildFrom(1.0, 1.5, 2.5, 3.5, 4.5, 5.0));
     Assert.assertEquals(result.getPredictedLowerBound(), DoubleSeries.buildFrom(1.0, 1.5, 2.5, 3.5, 4.5, 5.0));
   }
+
+  @Test
+  public void testConsolidateTimeSeriesWithNull() {
+    TimeSeries ts1 =
+        new TimeSeries(LongSeries.buildFrom(1L, 2L, 3L, 4L, 5L), DoubleSeries.buildFrom(1.0, 2.0, 3.0, 4.0, 5.0),
+            DoubleSeries.buildFrom(1.0, 2.0, 3.0, 4.0, 5.0), DoubleSeries.buildFrom(1.0, 2.0, 3.0, 4.0, 5.0),
+            DoubleSeries.buildFrom(1.0, 2.0, 3.0, 4.0, 5.0));
+    TimeSeries ts2 =
+        new TimeSeries(LongSeries.buildFrom(2L, 3L, 4L, 5L, 6L), DoubleSeries.buildFrom(1.0, 2.0, 3.0, 4.0, 5.0));
+    TimeSeries result = AnomalyDetectorWrapper.consolidateTimeSeries(ts1, ts2);
+    Assert.assertEquals(result.getTime(), LongSeries.buildFrom(1L, 2L, 3L, 4L, 5L, 6L));
+    Assert.assertEquals(result.getCurrent(), DoubleSeries.buildFrom(1L, 2L, 3L, 4L, 5L, Double.NaN));
+    Assert.assertEquals(result.getPredictedBaseline(), DoubleSeries.buildFrom(1.0, 1.5, 2.5, 3.5, 4.5, 5.0));
+    Assert.assertEquals(result.getPredictedUpperBound(), DoubleSeries.buildFrom(1.0, 2.0, 3.0, 4.0, 5.0, Double.NaN));
+    Assert.assertEquals(result.getPredictedLowerBound(), DoubleSeries.buildFrom(1.0, 2.0, 3.0, 4.0, 5.0, Double.NaN));
+  }
+
 }
