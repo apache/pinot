2bc8ffd33b633e290b0f8f773369ebae19779a6e
Harley Jackson <hjackson@linkedin.com>
2019-05-23 17:01:59 -0700

    [TE] frontend - harleyjj/application - redirect http to https

--- thirdeye/thirdeye-frontend/app/pods/application/route.js
+++ thirdeye/thirdeye-frontend/app/pods/application/route.js
@@ -16,12 +16,16 @@ export default Route.extend(ApplicationRouteMixin, {
   session: service(),
   notifications: service('toast'),
   queryParams: {
-    debug: queryParamsConfig,
+    debug: queryParamsConfig
   },
-  
+
   beforeModel() {
     // calling this._super to trigger ember-simple-auth's hook
     this._super(...arguments);
+
+    if (config.environment === 'production' && location.protocol != 'https:') {
+      location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+    }
 
     // invalidates session if cookie expired
     if (this.get('session.isAuthenticated')) {
