mysteryshub/incubator-pinot

Adding trift support in travis

.travis_install.sh
@@ -41,14 +41,14 @@ fi
 if [ $noThirdEyeChange -ne 0 ]; then
   echo "Full Pinot build"
   echo "No ThirdEye changes"
-  mvn clean install -B -DskipTests=true -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true || exit $?
+  mvn clean install -B -P profile-buildthrift -DskipTests=true -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true || exit $?
 fi
 
 # Build ThirdEye for ThirdEye related changes
 if [ $noThirdEyeChange -eq 0 ]; then
   echo "Partial Pinot build"
   echo "ThirdEye changes only"
-  mvn install -B -DskipTests -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true -pl pinot-common,pinot-core,pinot-api -am
+  mvn install -B -DskipTests -P profile-buildthrift -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true -pl pinot-common,pinot-core,pinot-api -am
   cd thirdeye/thirdeye-hadoop
   mvn clean compile -B -DskipTests
   cd ../..

.travis_test.sh
@@ -58,12 +58,12 @@ fi
 passed=0
 # Only run integration tests if needed
 if [ "$RUN_INTEGRATION_TESTS" != 'false' ]; then
-  mvn test -B -P travis,travis-integration-tests-only
+  mvn test -B -P travis,travis-integration-tests-only,profile-buildthrift
   if [ $? -eq 0 ]; then
     passed=1
   fi
 else
-  mvn test -B -P travis,travis-no-integration-tests
+  mvn test -B -P travis,travis-no-integration-tests,profile-buildthrift
   if [ $? -eq 0 ]; then
     passed=1
   fi

apache/pinot

.travis_install.sh
@@ -41,14 +41,14 @@ fi
 if [ $noThirdEyeChange -ne 0 ]; then
   echo "Full Pinot build"
   echo "No ThirdEye changes"
-  mvn clean install -B -DskipTests=true -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true || exit $?
+  mvn clean install -B -P build-thrift -DskipTests=true -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true || exit $?
 fi
 
 # Build ThirdEye for ThirdEye related changes
 if [ $noThirdEyeChange -eq 0 ]; then
   echo "Partial Pinot build"
   echo "ThirdEye changes only"
-  mvn install -B -DskipTests -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true -pl pinot-common,pinot-core,pinot-api -am
+  mvn install -B -DskipTests -P build-thrift -Dmaven.javadoc.skip=true -Dassembly.skipAssembly=true -pl pinot-common,pinot-core,pinot-api -am
   cd thirdeye/thirdeye-hadoop
   mvn clean compile -B -DskipTests
   cd ../..

.travis_test.sh
@@ -58,12 +58,12 @@ fi
 passed=0
 # Only run integration tests if needed
 if [ "$RUN_INTEGRATION_TESTS" != 'false' ]; then
-  mvn test -B -P travis,travis-integration-tests-only
+  mvn test -B -P travis,travis-integration-tests-only,build-thrift
   if [ $? -eq 0 ]; then
     passed=1
   fi
 else
-  mvn test -B -P travis,travis-no-integration-tests
+  mvn test -B -P travis,travis-no-integration-tests,build-thrift
   if [ $? -eq 0 ]; then
     passed=1
   fi