f5b9038cc819a69b2adde673e3f9429e8c3a9045
Jitendra Kushwaha <jitendrak@uber.com>
2019-05-30 11:44:57 +0530

    Keep PinotClientTransportFactory interface annotation as Private
    Currently this interface is not exposed as public

--- pinot-api/src/main/java/org/apache/pinot/client/PinotClientTransportFactory.java
+++ pinot-api/src/main/java/org/apache/pinot/client/PinotClientTransportFactory.java
@@ -26,7 +26,7 @@ import java.util.Map;
 /**
  * Factory for client transports.
  */
-@InterfaceAudience.Public
+@InterfaceAudience.Private
 @InterfaceStability.Stable
 interface PinotClientTransportFactory {
   /**
