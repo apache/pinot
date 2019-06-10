9a7dddc0895a3b2419792d96558f80bbc8cf45bf
Harley Jackson <hjackson@linkedin.com>
2019-06-10 13:32:47 -0700

    [TE] frontend - harleyjj/login - add AuthenticatedRouteMixin to Anomalies and Alerts Routes (#4297)

--- thirdeye/thirdeye-frontend/app/pods/manage/alerts/index/route.js
+++ thirdeye/thirdeye-frontend/app/pods/manage/alerts/index/route.js
@@ -5,6 +5,7 @@ import { get, getWithDefault } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { checkStatus, formatYamlFilter } from 'thirdeye-frontend/utils/utils';
 import { powerSort } from 'thirdeye-frontend/utils/manage-alert-utils';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 // Maps filter name to alert property for filtering
 const filterToPropertyMap = {
@@ -15,7 +16,7 @@ const filterToPropertyMap = {
   metric: 'metric'
 };
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
 
   // Make duration service accessible
   durationCache: service('services/duration'),
