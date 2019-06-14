7f81c949a5a25944c7b7e01fcbfc283a308c312c
Akshay Rai <akshayrai09@gmail.com>
2019-06-14 09:12:07 -0700

    [TE] Propagate diagnostics and evaluations from AnoamlyFilterWrapper

--- thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/detection/wrapper/AnomalyFilterWrapper.java
+++ thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/detection/wrapper/AnomalyFilterWrapper.java
@@ -21,9 +21,11 @@ package org.apache.pinot.thirdeye.detection.wrapper;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Collections2;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import org.apache.pinot.thirdeye.datalayer.dto.DetectionConfigDTO;
+import org.apache.pinot.thirdeye.datalayer.dto.EvaluationDTO;
 import org.apache.pinot.thirdeye.datalayer.dto.MergedAnomalyResultDTO;
 import org.apache.pinot.thirdeye.detection.ConfigUtils;
 import org.apache.pinot.thirdeye.detection.DataProvider;
@@ -33,7 +35,6 @@ import org.apache.pinot.thirdeye.detection.DetectionUtils;
 import org.apache.pinot.thirdeye.detection.PredictionResult;
 import org.apache.pinot.thirdeye.detection.spi.components.AnomalyFilter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -75,6 +76,8 @@ public class AnomalyFilterWrapper extends DetectionPipeline {
   public final DetectionPipelineResult run() throws Exception {
     List<MergedAnomalyResultDTO> candidates = new ArrayList<>();
     List<PredictionResult> predictionResults = new ArrayList<>();
+    Map<String, Object> diagnostics = new HashMap<>();
+    List<EvaluationDTO> evaluations = new ArrayList<>();
 
     Set<Long> lastTimeStamps = new HashSet<>();
     for (Map<String, Object> properties : this.nestedProperties) {
@@ -94,6 +97,8 @@ public class AnomalyFilterWrapper extends DetectionPipeline {
 
       DetectionPipelineResult intermediate = pipeline.run();
       lastTimeStamps.add(intermediate.getLastTimestamp());
+      diagnostics.putAll(intermediate.getDiagnostics());
+      evaluations.addAll(intermediate.getEvaluations());
       predictionResults.addAll(intermediate.getPredictions());
       candidates.addAll(intermediate.getAnomalies());
     }
@@ -101,6 +106,7 @@ public class AnomalyFilterWrapper extends DetectionPipeline {
     Collection<MergedAnomalyResultDTO> anomalies =
         Collections2.filter(candidates, mergedAnomaly -> mergedAnomaly != null && !mergedAnomaly.isChild() && anomalyFilter.isQualified(mergedAnomaly));
 
-    return new DetectionPipelineResult(new ArrayList<>(anomalies), DetectionUtils.consolidateNestedLastTimeStamps(lastTimeStamps), predictionResults);
+    return new DetectionPipelineResult(new ArrayList<>(anomalies), DetectionUtils.consolidateNestedLastTimeStamps(lastTimeStamps),
+        predictionResults, evaluations).setDiagnostics(diagnostics);
   }
 }
