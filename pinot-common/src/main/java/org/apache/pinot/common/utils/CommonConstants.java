bbf88b31822a6a6b81504a8c4c7fb70e0ec11a88
Neha Pawar <npawar@linkedin.com>
2019-06-27 10:38:52 -0700

    Fix commit

--- pinot-common/src/main/java/org/apache/pinot/common/utils/CommonConstants.java
+++ pinot-common/src/main/java/org/apache/pinot/common/utils/CommonConstants.java
@@ -324,7 +324,7 @@ public class CommonConstants {
        * Decides how the non-winner servers should get the segment
        */
       public enum CompletionMode {
-        DEFAULT, // default behavior - if the in memory segment in the non-winner segmis equivalent to the committed segment, then build and replace, else download
+        DEFAULT, // default behavior - if the in memory segment in the non-winner server is equivalent to the committed segment, then build and replace, else download
         DOWNLOAD // non-winner servers always download the segment, never build it
       }
 
