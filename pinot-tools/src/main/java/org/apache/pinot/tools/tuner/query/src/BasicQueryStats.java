b1eb786e56342fc6d54e4289e77844b4c5864c22
jasperjiaguo <jiaguo@linkedin.biz>
2019-07-14 02:35:03 -0700

    execute accumulator

--- pinot-tools/src/main/java/org/apache/pinot/tools/tuner/query/src/BasicQueryStats.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/tuner/query/src/BasicQueryStats.java
@@ -1,5 +1,9 @@
 package org.apache.pinot.tools.tuner.query.src;
 
+/*
+ * Wrapper for query stats fields, for ex, time, entriesScannedInFilter, etc.
+ */
+
 public abstract class BasicQueryStats {
   public abstract String toString();
 }
