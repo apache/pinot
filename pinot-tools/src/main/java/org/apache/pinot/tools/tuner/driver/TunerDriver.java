80d3d79b1e9253be9207958b30e88949db1b6d26
jasperjiaguo <jiaguo@linkedin.biz>
2019-07-14 11:15:30 -0700

    add interfaces description

--- pinot-tools/src/main/java/org/apache/pinot/tools/tuner/driver/TunerDriver.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/tuner/driver/TunerDriver.java
@@ -6,6 +6,18 @@ import org.apache.pinot.tools.tuner.strategy.BasicStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+TunerDriver is an executable interface, has three pluggable modules:
+    MetaData Manager: a manger for MetaDataProperties, which is an interface to access segment metadata.
+    QuerySrc: an iterator interface over input source, has a pluggable BasicQueryParser, who parses each item in input source, and returns BasicQueryStats, a wrapper of relevant fields in nput.
+    Strategy, which has four user defined functions operating on a map of Map<Long, Map<String, Map<String, ColumnStatsObj>>>:
+                                                                                |		       |				    |				      |
+                                                                            ThreadID	TableName	    ColumnName		Abstract object of stats for a column
+        Filter: A function to filter BasicQueryStats, by table name, number of entries scanned in filters, number of entries scanned post filter, etc. The relevant BasicQueryStats will be feed to Accumulator.
+        Accumulator: A function to process BasicQueryStats and accumulate it to corresponding ColumnStatsObj entry.
+        Merger: A function to merge two ColumnStatsObj entries having the same TableName/ColumnName.
+        Reporter: A function to print(email) out the final results of a table.
+*/
 
 public abstract class TunerDriver {
   protected static final Logger LOGGER = LoggerFactory.getLogger(TunerDriver.class);
