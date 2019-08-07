aea8fca7e2535ce2e8a445754cf5c25bef0cb542
Jihao Zhang <jihzhang@linkedin.com>
2019-08-07 10:46:08 -0700

    [TE] anomalies table endpoint (#4501)
    Fix the issues of anomalies table endpoint to unblock UI development.

--- thirdeye/thirdeye-pinot/src/test/java/org/apache/pinot/thirdeye/datasource/resources/TestAnomalyFlattenResource.java
+++ thirdeye/thirdeye-pinot/src/test/java/org/apache/pinot/thirdeye/datasource/resources/TestAnomalyFlattenResource.java
@@ -19,7 +19,6 @@
 
 package org.apache.pinot.thirdeye.datasource.resources;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -42,7 +41,6 @@ import static org.apache.pinot.thirdeye.dashboard.resources.AnomalyFlattenResour
 
 public class TestAnomalyFlattenResource {
   private DAOTestBase testDAOProvider;
-  public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
   private DetectionConfigManager detectionConfigDAO;
   private MergedAnomalyResultManager mergedAnomalyResultDAO;
@@ -65,36 +63,29 @@ public class TestAnomalyFlattenResource {
   }
 
   @Test
-  public void testFlatAnomalyResults() throws Exception {
-    Map<String, Object> jsonMap = new HashMap<String, Object>() {
-      {
-        put(FUNCTION_ID, detectionConfigId);
-        put(WINDOW_START, new DateTime(2019, 1, 1, 0, 0).getMillis());
-        put(WINDOW_END, new DateTime(2019, 1, 3, 0, 0).getMillis());
-      }
-    };
-    AnomalyFlattenResource resource = new AnomalyFlattenResource();
-    List<Map<String, String>> actualResults = resource.flatAnomalyResults(OBJECT_MAPPER.writeValueAsString(jsonMap));
-    List<Map<String, String>> expectedResults = Arrays.asList(
-        new HashMap<String, String>() {
+  public void testFlatAnomalyResults() {
+    AnomalyFlattenResource resource = new AnomalyFlattenResource(this.mergedAnomalyResultDAO);
+    List<Map<String, Object>> actualResults = resource.flatAnomalyResults(this.detectionConfigId, new DateTime(2019, 1, 1, 0, 0).getMillis(), new DateTime(2019, 1, 3, 0, 0).getMillis(), null);
+    List<Map<String, Object>> expectedResults = Arrays.asList(
+        new HashMap<String, Object>() {
           {
-            put(ANOMALY_ID, Long.toString(2));
+            put(ANOMALY_ID, 2L);
             put("what", "a");
             put("where", "b");
             put("when", "c");
             put("how", "d");
-            put("metric", Double.toString(0d));
+            put("metric", 0d);
             put(ANOMALY_COMMENT, "");
           }
         },
-        new HashMap<String, String>() {
+        new HashMap<String, Object>() {
           {
-            put(ANOMALY_ID, Long.toString(3));
+            put(ANOMALY_ID, 3L);
             put("what", "e");
             put("where", "f");
             put("when", "g");
             put("how", "h");
-            put("metric", Double.toString(0d));
+            put("metric", 0d);
             put(ANOMALY_COMMENT, "");
           }
         }
@@ -106,16 +97,16 @@ public class TestAnomalyFlattenResource {
   public void testFlatAnomalyResult() {
     List<MergedAnomalyResultDTO> anomalies = TestMergedAnomalyResultManager.mockAnomalies(detectionConfigId);
     MergedAnomalyResultDTO anomaly = anomalies.get(0);
-    anomaly.setId(1l);
-    Map<String, String> actualMap = AnomalyFlattenResource.flatAnomalyResult(anomaly, null);
-    Map<String, String> expectMap = new HashMap<String, String>() {
+    anomaly.setId(1L);
+    Map<String, Object> actualMap = AnomalyFlattenResource.flatAnomalyResult(anomaly, null);
+    Map<String, Object> expectMap = new HashMap<String, Object>() {
       {
-        put(ANOMALY_ID, Long.toString(anomaly.getId()));
+        put(ANOMALY_ID, anomaly.getId());
         put("what", "a");
         put("where", "b");
         put("when", "c");
         put("how", "d");
-        put("metric", Double.toString(0d));
+        put("metric", 0d);
         put(ANOMALY_COMMENT, "");
       }
     };
