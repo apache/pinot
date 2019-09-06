ef0301727e5a4a4cde73c9e17313f4af6784cfb9
Xiang Fu <fx19880617@gmail.com>
2019-09-06 16:48:59 -0700

    address comments

--- kubernetes/examples/gke/skaffold/cleanup.sh
+++ kubernetes/examples/gke/skaffold/cleanup.sh
@@ -22,8 +22,8 @@ set -e
 
 if [[ -z "${GCLOUD_PROJECT}" ]]
 then
-      echo "Please set \$GCLOUD_PROJECT variable. E.g. GCLOUD_PROJECT=pinot-demo ./cleanup.sh"
-      exit 1
+  echo "Please set \$GCLOUD_PROJECT variable. E.g. GCLOUD_PROJECT=pinot-demo ./cleanup.sh"
+  exit 1
 fi
 
 GCLOUD_ZONE=us-west1-b
@@ -32,8 +32,8 @@ GCLOUD_CLUSTER=pinot-quickstart
 gcloud container clusters delete ${GCLOUD_CLUSTER} --zone=${GCLOUD_ZONE} --project=${GCLOUD_PROJECT} -q
 
 for diskname in `gcloud compute disks list --zones=${GCLOUD_ZONE} --project ${GCLOUD_PROJECT} |grep  gke-${GCLOUD_CLUSTER}|awk -F ' ' '{print $1}'`;
-do 
-echo $diskname; 
-gcloud compute disks delete $diskname --zone=${GCLOUD_ZONE} --project ${GCLOUD_PROJECT} -q
+do
+  echo $diskname;
+  gcloud compute disks delete $diskname --zone=${GCLOUD_ZONE} --project ${GCLOUD_PROJECT} -q
 done
 
--- kubernetes/examples/gke/skaffold/setup.sh
+++ kubernetes/examples/gke/skaffold/setup.sh
@@ -21,8 +21,8 @@
 set -e
 if [[ -z "${GCLOUD_EMAIL}" ]] ||  [[ -z "${GCLOUD_PROJECT}" ]]
 then
-      echo "Please set both \$GCLOUD_EMAIL and \$GCLOUD_PROJECT variables. E.g. GCLOUD_PROJECT=pinot-demo GCLOUD_EMAIL=pinot-demo@example.com ./setup.sh"
-      exit 1
+  echo "Please set both \$GCLOUD_EMAIL and \$GCLOUD_PROJECT variables. E.g. GCLOUD_PROJECT=pinot-demo GCLOUD_EMAIL=pinot-demo@example.com ./setup.sh"
+  exit 1
 fi
 
 GCLOUD_ZONE=us-west1-b
@@ -30,10 +30,10 @@ GCLOUD_CLUSTER=pinot-quickstart
 GCLOUD_MACHINE_TYPE=n1-standard-2
 GCLOUD_NUM_NODES=1
 gcloud container clusters create ${GCLOUD_CLUSTER} \
-        --num-nodes=${GCLOUD_NUM_NODES} \
-        --machine-type=${GCLOUD_MACHINE_TYPE} \
-        --zone=${GCLOUD_ZONE} \
-        --project=${GCLOUD_PROJECT}
+  --num-nodes=${GCLOUD_NUM_NODES} \
+  --machine-type=${GCLOUD_MACHINE_TYPE} \
+  --zone=${GCLOUD_ZONE} \
+  --project=${GCLOUD_PROJECT}
 gcloud container clusters get-credentials ${GCLOUD_CLUSTER} --zone ${GCLOUD_ZONE} --project ${GCLOUD_PROJECT}
 
  
@@ -44,7 +44,6 @@ gcloud container node-pools create pinot-server-pool \
   --num-nodes=${GCLOUD_NUM_NODES} \
   --zone=${GCLOUD_ZONE} \
   --project=${GCLOUD_PROJECT}
-
 
 kubectl create namespace pinot-quickstart
 
