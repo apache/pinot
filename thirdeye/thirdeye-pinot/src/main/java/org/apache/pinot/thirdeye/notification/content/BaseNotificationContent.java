6ea2d3fca58b1fbd50b7f3915769b39aaed3d7b2
Akshay Rai <akrai@linkedin.com>
2019-09-10 09:14:01 -0700

    resolve conflict error

--- thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/notification/content/BaseNotificationContent.java
+++ thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/notification/content/BaseNotificationContent.java
@@ -65,8 +65,6 @@ import org.joda.time.Weeks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.apache.pinot.thirdeye.alert.content.EntityGroupKeyContentFormatter.*;
-
 
 /**
  * This class (helper) defines the overall alert message content. This will
