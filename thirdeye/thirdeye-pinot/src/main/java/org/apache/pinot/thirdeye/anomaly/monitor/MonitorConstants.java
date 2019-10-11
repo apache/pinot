1df1eb432c2822dced4165a3a42f91c7fa146abf
xiaohui-sun <xhsun@linkedin.com>
2019-10-11 10:59:43 -0700

    [TE] changed retention and Pinot metadata sync frequency

--- thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/anomaly/monitor/MonitorConstants.java
+++ thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/anomaly/monitor/MonitorConstants.java
@@ -29,9 +29,9 @@ public class MonitorConstants {
     EXPIRE
   }
 
-  public static int DEFAULT_RETENTION_DAYS = 30;
-  public static int DEFAULT_COMPLETED_JOB_RETENTION_DAYS = 14;
-  public static int DEFAULT_DETECTION_STATUS_RETENTION_DAYS = 7;
+  public static int DEFAULT_RETENTION_DAYS = 90;
+  public static int DEFAULT_COMPLETED_JOB_RETENTION_DAYS = 90;
+  public static int DEFAULT_DETECTION_STATUS_RETENTION_DAYS = 90;
   public static int DEFAULT_RAW_ANOMALY_RETENTION_DAYS = 30;
   public static TimeGranularity DEFAULT_MONITOR_FREQUENCY = new TimeGranularity(15, TimeUnit.MINUTES);
 
