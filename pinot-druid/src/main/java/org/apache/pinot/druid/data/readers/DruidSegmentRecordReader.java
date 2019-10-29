08af64156f1fa6d00417d4844ca6a3168ba72e0f
Dominique Adapon <dadapon@linkedin.com>
2019-10-28 18:47:40 -0700

    Update Adding DruidSegmentRecordReader
    Added method for validating columns and throwing the appropriate exceptions.
    Also changed Sequence handling from converting to Yielder to simply accumulating.
    Created preliminary test file for new RecordReader which will be renamed and fixed.

---
+++ pinot-druid/src/main/java/org/apache/pinot/druid/data/readers/DruidSegmentRecordReader.java
@@ -0,0 +1,219 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.druid.data.readers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.Nullable;
+import org.apache.druid.jackson.DefaultObjectMapper;
+import org.apache.druid.java.util.common.granularity.Granularities;
+import org.apache.druid.java.util.common.guava.Sequence;
+import org.apache.druid.java.util.common.guava.Sequences;
+import org.apache.druid.segment.BaseObjectColumnValueSelector;
+import org.apache.druid.segment.ColumnSelectorFactory;
+import org.apache.druid.segment.Cursor;
+import org.apache.druid.segment.IndexIO;
+import org.apache.druid.segment.QueryableIndex;
+import org.apache.druid.segment.QueryableIndexStorageAdapter;
+import org.apache.druid.segment.VirtualColumns;
+import org.apache.druid.segment.column.ColumnCapabilities;
+import org.apache.druid.segment.column.ColumnConfig;
+import org.apache.druid.segment.column.ValueType;
+import org.apache.druid.segment.filter.Filters;
+import org.apache.druid.query.DruidProcessingConfig;
+import org.apache.pinot.common.data.FieldSpec;
+import org.apache.pinot.common.data.Schema;
+import org.apache.pinot.core.data.GenericRow;
+import org.apache.pinot.core.data.readers.RecordReader;
+import org.apache.pinot.core.indexsegment.generator.SegmentGeneratorConfig;
+import org.joda.time.chrono.ISOChronology;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * The DruidSegmentRecordReader allows us to convert all of the rows in a Druid segment file
+ * into GenericRows, which are made into Pinot segments.
+ *
+ * Note that Druid uses LONG and not INT, so construct the Pinot schema accordingly.
+ */
+public class DruidSegmentRecordReader implements RecordReader {
+  // TODO: Figure out why logger stuff isn't showing up in console
+  private static final Logger LOGGER = LoggerFactory.getLogger(DruidSegmentRecordReader.class);
+
+  private Schema _pinotSchema;
+  private Cursor _cursor;
+  private ArrayList<String> _columnNames;
+  private List<BaseObjectColumnValueSelector> _selectors;
+  private QueryableIndex _index;
+  private QueryableIndexStorageAdapter _adapter;
+
+  private void validateColumns()
+      throws IllegalArgumentException {
+    for (int i = 0; i < _columnNames.size(); i++) {
+      final String columnName = _columnNames.get(i);
+      ColumnCapabilities capabilities = _adapter.getColumnCapabilities(columnName);
+      if (capabilities == null) {
+        LOGGER.warn("Column {} is not in record", columnName);
+      } else {
+        if (capabilities.getType() == ValueType.COMPLEX) {
+          throw new IllegalArgumentException("Column " + columnName
+              + ": DruidSegmentRecordReader does not support complex metric columns.");
+        }
+        FieldSpec fieldSpec = _pinotSchema.getFieldSpecFor(_columnNames.get(i));
+        if (fieldSpec.isSingleValueField() && capabilities.hasMultipleValues()) {
+          throw new IllegalArgumentException("Column " + columnName
+              + " in Pinot schema is single-valued, but column {} in " + "record is multi-valued.");
+        }
+        if (!fieldSpec.isSingleValueField() && !capabilities.hasMultipleValues()) {
+          throw new IllegalArgumentException("Column " + columnName
+              + " in Pinot schema is multi-valued, but column {} in" + "record is single-valued");
+        }
+        String typeFromSchema = _pinotSchema.getFieldSpecFor(columnName).getDataType().toString();
+        String typeFromRecord = capabilities.getType().toString();
+        if (typeFromRecord != typeFromSchema) {
+          throw new IllegalArgumentException("Type for column " + columnName +
+              " in schema does not match type for column " + columnName + " in record.");
+        }
+      }
+    }
+  }
+
+  private void init(String indexPath, Schema schema)
+      throws IOException {
+    // Only the columns whose names are in the Pinot schema will get processed
+    _pinotSchema = schema;
+    _columnNames = new ArrayList<>();
+    _columnNames.addAll(_pinotSchema.getColumnNames());
+
+    ColumnConfig config = new DruidProcessingConfig() {
+      @Override
+      public String getFormatString() {
+        return "processing-%s";
+      }
+
+      @Override
+      public int intermediateComputeSizeBytes() {
+        return 100 * 1024 * 1024;
+      }
+
+      @Override
+      public int getNumThreads() {
+        return 1;
+      }
+
+      @Override
+      public int columnCacheSizeBytes() {
+        return 25 * 1024 * 1024;
+      }
+    };
+
+    ObjectMapper mapper = new DefaultObjectMapper();
+    final IndexIO indexIO = new IndexIO(mapper, config);
+    _index = indexIO.loadIndex(new File(indexPath));
+    _adapter = new QueryableIndexStorageAdapter(_index);
+
+    // A Sequence "represents an iterable sequence of elements. Unlike normal Iterators however, it doesn't expose
+    // a way for you to extract values from it, instead you provide it with a worker (an Accumulator) and that defines
+    // what happens with the data."
+    final Sequence<Cursor> cursors = _adapter.makeCursors(
+        Filters.toFilter(null),
+        _index.getDataInterval().withChronology(ISOChronology.getInstanceUTC()),
+        VirtualColumns.EMPTY,
+        Granularities.ALL,
+        false,
+        null
+    );
+
+    List<Cursor> cursorList = new ArrayList<>();
+    final Sequence<Object> sequence = Sequences.map(cursors, new Function<Cursor, Object>() {
+      @Override
+      public Object apply(@Nullable Cursor cursor) {
+        cursorList.add(cursor);
+        return null;
+      }
+    });
+    sequence.accumulate(null, (accumulated, in) -> null);
+
+    // There should only be one single Cursor for every segment, so there should only be one Cursor in the cursorList
+    Preconditions.checkArgument(cursorList.size() == 1, "msg");
+    _cursor = cursorList.get(0);
+
+    validateColumns();
+
+    ColumnSelectorFactory columnSelectorFactory = _cursor.getColumnSelectorFactory();
+    _selectors = _columnNames
+        .stream()
+        .map(columnSelectorFactory::makeColumnValueSelector)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public void init(SegmentGeneratorConfig segmentGeneratorConfig)
+      throws IOException {
+    init(segmentGeneratorConfig.getInputFilePath(), segmentGeneratorConfig.getSchema());
+  }
+
+  @Override
+  public boolean hasNext() {
+    return !_cursor.isDone();
+  }
+
+  @Override
+  public GenericRow next() throws IOException {
+    return next(new GenericRow());
+  }
+
+  @Override
+  public GenericRow next(GenericRow reuse) {
+    // Only the columns whose names are in the Pinot schema will get processed
+    for (int i = 0; i < _columnNames.size(); i++) {
+      final String columnName = _columnNames.get(i);
+      BaseObjectColumnValueSelector selector = _selectors.get(i);
+      // If the column does not exist in the segment file, skip it.
+      if (selector != null) {
+        final Object value = selector.getObject();
+        reuse.putField(columnName, value);
+      }
+    }
+    _cursor.advance();
+    return reuse;
+  }
+
+  @Override
+  public void rewind() {
+    _cursor.reset();
+  }
+
+  @Override
+  public Schema getSchema() {
+    return _pinotSchema;
+  }
+
+  @Override
+  public void close() {
+    _index.close();
+  }
+}
