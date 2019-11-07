1f2217a674734fe34423711b8ece08bcea76955a
Dominique Adapon <dadapon@linkedin.com>
2019-11-06 18:14:08 -0800

    Updated DruidSegmentRecordReaderTest
    Improved tests for DruidSegmentRecordReader and made changes according to comments.

--- pinot-druid/src/test/java/org/apache/pinot/druid/data/readers/DruidSegmentRecordReaderTest.java
+++ pinot-druid/src/test/java/org/apache/pinot/druid/data/readers/DruidSegmentRecordReaderTest.java
@@ -9,74 +9,85 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.apache.pinot.common.data.FieldSpec;
 import org.apache.pinot.common.data.Schema;
 import org.apache.pinot.core.data.GenericRow;
+import org.apache.pinot.core.data.readers.JSONRecordReader;
 import org.apache.pinot.core.data.recordtransformer.CompositeTransformer;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 
 public class DruidSegmentRecordReaderTest {
-  private static int wikipedia_size = 39244;
-  private static File wikipedia_test;
-  private static Schema wikipedia_test_schema;
-
-  private static Schema csv_test_schema;
-  private static File csv_test;
-
-  private static File all_types_test;
-  private static Schema all_types_test_schema;
-
-  private File createFileFromFilename(String filename) {
-    return new File(Preconditions
-        .checkNotNull(DruidSegmentRecordReaderTest.class.getClassLoader().getResource(filename))
-        .getFile());
-  }
-
-  private Schema createSchemaFromFile(File schemaFile)
-      throws IOException {
-    return Schema.fromFile(schemaFile);
-  }
-
-  private Schema createSchemaFromFilename(String filename)
-      throws IOException {
-    return createSchemaFromFile(createFileFromFilename(filename));
-  }
+  // test_sample_data is a Druid segment that was created from pinot-core/src/test/resources/data/test_sample_data.json
+  private static final File TEST_SAMPLE_DATA = new File(Preconditions
+      .checkNotNull(DruidSegmentRecordReaderTest.class.getClassLoader().getResource("test_sample_data"))
+      .getFile());
+  private static final File TEST_SAMPLE_DATA_JSON = new File(Preconditions
+      .checkNotNull(DruidSegmentRecordReaderTest.class.getClassLoader().getResource("test_sample_data_dumped.json"))
+      .getFile());
+  // Same schema as in RecordReaderSampleDataTest
+  private static final Schema TEST_SAMPLE_DATA_SCHEMA = new Schema.SchemaBuilder().addSingleValueDimension("column1", FieldSpec.DataType.LONG)
+        .addSingleValueDimension("column2", FieldSpec.DataType.LONG)
+        .addSingleValueDimension("column3", FieldSpec.DataType.STRING)
+        .addSingleValueDimension("column7", FieldSpec.DataType.STRING)
+        .addSingleValueDimension("unknown_dimension", FieldSpec.DataType.STRING)
+        .addMetric("met_impressionCount", FieldSpec.DataType.LONG).addMetric("unknown_metric", FieldSpec.DataType.DOUBLE)
+        .build();
+  // Test based on CSVRecordReaderTest
+  private static final File CSV_TEST = new File(Preconditions
+      .checkNotNull(DruidSegmentRecordReaderTest.class.getClassLoader().getResource("test_druid_from_csv"))
+      .getFile());
+  private static final Schema CSV_TEST_SCHEMA = new Schema.SchemaBuilder().addSingleValueDimension("__time", FieldSpec.DataType.LONG)
+      .addMultiValueDimension("STR_MV", FieldSpec.DataType.STRING)
+      .addMetric("INT_SV", FieldSpec.DataType.LONG)
+      .build();
+  private static final File ALL_TYPES_TEST = new File(Preconditions
+      .checkNotNull(DruidSegmentRecordReaderTest.class.getClassLoader().getResource("test_druid_all_types"))
+      .getFile());
+  private static final Schema ALL_TYPES_TEST_SCHEMA = new Schema.SchemaBuilder().addSingleValueDimension("__time", FieldSpec.DataType.LONG)
+      .addSingleValueDimension("stringval", FieldSpec.DataType.STRING)
+      .addMultiValueDimension("tags", FieldSpec.DataType.STRING)
+      .addMetric("intnum", FieldSpec.DataType.LONG)
+      .addMetric("floatnum", FieldSpec.DataType.FLOAT)
+      .build();
 
   private void testReadRowsWithRecordList(Object[][] records, String[] columns, File dataFile, Schema schema)
       throws IOException {
-    DruidSegmentRecordReader testReader = new DruidSegmentRecordReader(dataFile, schema);
-    CompositeTransformer defaultTransformer = CompositeTransformer.getDefaultTransformer(schema);
+    try (DruidSegmentRecordReader testReader = new DruidSegmentRecordReader(dataFile, schema)) {
+      CompositeTransformer defaultTransformer = CompositeTransformer.getDefaultTransformer(schema);
 
-    List<GenericRow> genericRows = new ArrayList<>();
-    int row = 0;
-    while (testReader.hasNext()) {
-      Assert.assertTrue(row < records.length);
+      List<GenericRow> genericRows = new ArrayList<>();
+      int row = 0;
+      while (testReader.hasNext()) {
+        Assert.assertTrue(row < records.length);
 
-      GenericRow nextRow = testReader.next();
-      GenericRow transformedRow = defaultTransformer.transform(nextRow);
+        GenericRow nextRow = testReader.next();
+        GenericRow transformedRow = defaultTransformer.transform(nextRow);
 
-      for (int col = 0; col < columns.length; col++) {
-        if (records[row][col] != null) {
-          if (transformedRow.getValue(columns[col]) != null && records[row][col] != null) {
-            Assert.assertTrue(transformedRow.getValue(columns[col]).getClass() == records[row][col].getClass(),
-                "In row " + row + " and column " + col + ", type of value in segment (" + transformedRow.getValue(columns[col]).getClass()
-                    + ") does not match type of value in test record (" + records[row][col].getClass() + ").");
-            if (transformedRow.getValue(columns[col]).getClass() == String[].class) {
-              Assert.assertTrue(compareMultiValueColumn(transformedRow.getValue(columns[col]), records[row][col]));
-            } else {
-              Assert.assertEquals(transformedRow.getValue(columns[col]), records[row][col]);
+        for (int col = 0; col < columns.length; col++) {
+          if (records[row][col] != null) {
+            if (transformedRow.getValue(columns[col]) != null && records[row][col] != null) {
+              Assert.assertTrue(transformedRow.getValue(columns[col]).getClass() == records[row][col].getClass(),
+                  String.format("In row {} and column {}, type of value in segment ({}) does not match type of value in test record ({}).",
+                      row, col, transformedRow.getValue(columns[col]).getClass(), records[row][col].getClass()));
+              if (transformedRow.getValue(columns[col]).getClass() == String[].class) {
+                Assert.assertTrue(compareMultiValueColumn(transformedRow.getValue(columns[col]), records[row][col]));
+              } else {
+                Assert.assertEquals(transformedRow.getValue(columns[col]), records[row][col]);
+              }
             }
           }
         }
+        genericRows.add(nextRow);
+        row += 1;
       }
-      genericRows.add(nextRow);
-      row += 1;
+      Assert.assertFalse(testReader.hasNext());
+      Assert.assertEquals(genericRows.size(), records.length, String.format("Size of GenericRows ({}) must be equal to records size ({}).", genericRows.size(), records.length));
     }
-    Assert.assertFalse(testReader.hasNext());
-    Assert.assertEquals(genericRows.size(), records.length, "Size of GenericRows (" + genericRows.size()
-        + ") must be equal to records size (" + records.length + ").");
   }
 
   private boolean compareMultiValueColumn(Object value1, Object value2) {
@@ -87,43 +98,54 @@ public class DruidSegmentRecordReaderTest {
     return value1Set.containsAll(value2Set);
   }
 
-  @BeforeClass
-  public void setUp()
-      throws IOException {
-    // Wikipedia is sample data from Druid's Quickstart tutorial
-    wikipedia_test = createFileFromFilename("test_druid_wikipedia");
-    wikipedia_test_schema = createSchemaFromFilename("wikipedia-schema.json");
-
-    csv_test = createFileFromFilename("test_druid_from_csv");
-    csv_test_schema = createSchemaFromFilename("csv-test-schema.json");
-
-    all_types_test = createFileFromFilename("test-druid-all-types");
-    all_types_test_schema = createSchemaFromFilename("test-druid-all-types-schema.json");
-  }
-
   @Test
-  public void testReadAllRows()
+  public void testRecordReaderWithJson()
       throws IOException {
-    DruidSegmentRecordReader testReader;
-    testReader = new DruidSegmentRecordReader(wikipedia_test, wikipedia_test_schema);
+    CompositeTransformer defaultTransformer = CompositeTransformer.getDefaultTransformer(TEST_SAMPLE_DATA_SCHEMA);
+    try (DruidSegmentRecordReader druidRecordReader = new DruidSegmentRecordReader(TEST_SAMPLE_DATA, TEST_SAMPLE_DATA_SCHEMA);
+        JSONRecordReader jsonRecordReader = new JSONRecordReader(TEST_SAMPLE_DATA_JSON, TEST_SAMPLE_DATA_SCHEMA)) {
+      int numRecords = 0;
+      while (druidRecordReader.hasNext()) {
+        assertTrue(jsonRecordReader.hasNext());
+        numRecords++;
 
-    List<GenericRow> genericRows = new ArrayList<>();
-    while (testReader.hasNext()) {
-      GenericRow nextRow = testReader.next();
-      genericRows.add(nextRow);
+        GenericRow druidRecord = defaultTransformer.transform(druidRecordReader.next());
+        GenericRow jsonRecord = defaultTransformer.transform(jsonRecordReader.next());
+        assertEquals(druidRecord, jsonRecord);
+
+        // Check the values from the first record
+        if (numRecords == 1) {
+          // Dimensions
+          assertEquals(druidRecord.getValue("column1"), 1840748525967736008L);
+          assertEquals(druidRecord.getValue("column2"), 231355578L);
+          assertEquals(druidRecord.getValue("column3"), "CezOib");
+
+          // Dimension empty string
+          assertEquals(druidRecord.getValue("column7").toString(), "null");
+
+          // Dimension default column
+          assertEquals(druidRecord.getValue("unknown_dimension"), "null");
+
+          // Metric
+          assertEquals(druidRecord.getValue("met_impressionCount"), 8637957270245933828L);
+
+          // Metric default column
+          assertEquals(druidRecord.getValue("unknown_metric"), 0.0);
+        }
+      }
+      assertEquals(numRecords, 10001);
     }
-    Assert.assertEquals(genericRows.size(), wikipedia_size, "Generic row size must be " + wikipedia_size + ".");
   }
 
   @Test
   public void testCSVRecord()
       throws IOException {
-    final String[] columms = {"sum_INT_SV", "INT_MV", "__time"};
+    final String[] columms = {"INT_SV", "STR_MV", "__time"};
     final Object[][] records = {
         {new Long(5), new String[]{"10", "15", "20"}, Instant.parse("2010-01-01T00:00:00.000Z").toEpochMilli()},
         {new Long(25), new String[]{"30", "35", "40"}, Instant.parse("2010-01-02T00:00:00.000Z").toEpochMilli()},
         {null, null, Instant.parse("2010-01-03T00:00:00.000Z").toEpochMilli()}};
-    testReadRowsWithRecordList(records, columms, csv_test, csv_test_schema);
+    testReadRowsWithRecordList(records, columms, CSV_TEST, CSV_TEST_SCHEMA);
   }
 
   @Test
@@ -135,7 +157,6 @@ public class DruidSegmentRecordReaderTest {
         {Long.parseLong("1294790400000"), Long.parseLong("1"), new Float(0.1), new String[]{"t1","t2","t3"}, "test1"},
         {Long.parseLong("1294876800000"), Long.parseLong("2"), new Float(10.02), new String[]{"t3","t4","t5"}, "test2"},
         {Long.parseLong("1294963200000"), Long.parseLong("3"), new Float(100.003), new String[]{"t5","t6","t7"}, "test3"}};
-
-    testReadRowsWithRecordList(records, columns, all_types_test, all_types_test_schema);
+    testReadRowsWithRecordList(records, columns, ALL_TYPES_TEST, ALL_TYPES_TEST_SCHEMA);
   }
 }
