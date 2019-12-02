c6e74cdb15c0c3f97a4c9fe27e925ed774e632ee
Siddharth Teotia <steotia@steotia-mn1.linkedin.biz>
2019-12-02 15:13:24 -0800

    review comments

--- pinot-core/src/main/java/org/apache/pinot/core/segment/index/loader/defaultcolumn/V3DefaultColumnHandler.java
+++ pinot-core/src/main/java/org/apache/pinot/core/segment/index/loader/defaultcolumn/V3DefaultColumnHandler.java
@@ -26,6 +26,7 @@ import org.apache.pinot.core.segment.creator.impl.V1Constants;
 import org.apache.pinot.core.segment.index.SegmentMetadataImpl;
 import org.apache.pinot.core.segment.index.loader.LoaderUtils;
 import org.apache.pinot.core.segment.index.loader.V3RemoveIndexException;
+import org.apache.pinot.core.segment.index.loader.V3UpdateIndexException;
 import org.apache.pinot.core.segment.store.ColumnIndexType;
 import org.apache.pinot.core.segment.store.SegmentDirectory;
 import org.slf4j.Logger;
@@ -50,7 +51,12 @@ public class V3DefaultColumnHandler extends BaseDefaultColumnHandler {
 
     // For V3 segment format, only support ADD action
     // For UPDATE and REMOVE action, throw exception to drop and re-download the segment
-    if (!action.isAddAction()) {
+    if (action.isUpdateAction()) {
+      throw new V3UpdateIndexException(
+          "Default value indices for column: " + column + " cannot be updated for V3 format segment.");
+    }
+
+    if (action.isRemoveAction()) {
       throw new V3RemoveIndexException(
           "Default value indices for column: " + column + " cannot be removed for V3 format segment.");
     }
