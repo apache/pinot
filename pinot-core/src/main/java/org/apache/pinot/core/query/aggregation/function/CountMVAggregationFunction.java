da164f49b38306f6aa92be86c776d739ca2944ee
Neha Pawar <npawar@linkedin.com>
2019-12-02 15:25:03 -0800

    Override methods for countMV

--- pinot-core/src/main/java/org/apache/pinot/core/query/aggregation/function/CountMVAggregationFunction.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/aggregation/function/CountMVAggregationFunction.java
@@ -32,6 +32,16 @@ public class CountMVAggregationFunction extends CountAggregationFunction {
   }
 
   @Override
+  public String getColumnName(String column) {
+    return AggregationFunctionType.COUNTMV.getName() + "_" + column;
+  }
+
+  @Override
+  public String getResultColumnName(String column) {
+    return AggregationFunctionType.COUNTMV.getName().toLowerCase() + "(" + column + ")";
+  }
+
+  @Override
   public void accept(AggregationFunctionVisitorBase visitor) {
     visitor.visit(this);
   }
