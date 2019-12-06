0ee28402d38ecc29c5bb3b1ddd217a7eb675daba
Jennifer Dai <jdai@linkedin.com>
2019-12-06 07:42:49 -0800

    Moving crypter classes to pinot-spi

--- pinot-spi/src/main/java/org/apache/pinot/spi/crypt/NoOpPinotCrypter.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/crypt/NoOpPinotCrypter.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pinot.core.crypt;
+package org.apache.pinot.spi.crypt;
 
 import java.io.File;
 import org.apache.commons.configuration.Configuration;
