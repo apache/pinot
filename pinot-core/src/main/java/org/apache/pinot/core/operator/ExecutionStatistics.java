eb5716abc444caf8d6a262cfad26f04cdefccdce
chenboat <chenboat@gmail.com>
2020-01-23 23:13:46 -0800

    Revised based on comments.

--- pinot-core/src/main/java/org/apache/pinot/core/operator/ExecutionStatistics.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/ExecutionStatistics.java
@@ -24,8 +24,8 @@ package org.apache.pinot.core.operator;
 public class ExecutionStatistics {
   // The number of documents scanned post filtering.
   private long _numDocsScanned;
-  // The number of entries (single value entry contains 1 entry, multi-value entry may contain multiple entries) in the
-  // filtering phase of the query execution: could be larger than the total scanned doc num because of multiple
+  // The number of entries (single value entry contains 1 value, multi-value entry may contain multiple values) scanned
+  // in the filtering phase of the query execution: could be larger than the total scanned doc num because of multiple
   // filtering predicates and multi-value entry.
   private long _numEntriesScannedInFilter;
   // Equal to numDocsScanned * numberProjectedColumns.
--- pinot-core/src/main/java/org/apache/pinot/core/operator/docidsets/SizeBasedDocIdSet.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/docidsets/SizeBasedDocIdSet.java
@@ -21,7 +21,7 @@ package org.apache.pinot.core.operator.docidsets;
 import org.apache.pinot.core.common.BlockDocIdIterator;
 import org.apache.pinot.core.operator.dociditerators.SizeBasedDocIdIterator;
 
-// Rename this class to MatchAllDocIdSet.
+// TODO: Rename this class to MatchAllDocIdSet.
 public final class SizeBasedDocIdSet implements FilterBlockDocIdSet {
   private final int _maxDocId;
 
@@ -49,7 +49,7 @@ public final class SizeBasedDocIdSet implements FilterBlockDocIdSet {
 
   @Override
   public long getNumEntriesScannedInFilter() {
-    // No value scanned because all docs are matches and will be returned.
+    // No value scanned because all docs are matched and will be returned.
     return 0L;
   }
 
