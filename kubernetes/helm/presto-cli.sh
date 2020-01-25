4dbe902ea79fb01aa824a5cdc3b28d1168a04e73
xxsacxx <https://gitlab.onionlife.in/>
2020-01-25 21:37:32 +0530

    kubernetes/helm/presto-cli.sh

---
+++ kubernetes/helm/presto-cli.sh
@@ -0,0 +1,9 @@
+if [[ $(nc -z  localhost 8080) != 0 ]]; then
+  kubectl port-forward service/presto-coordinator 8080:8080 -n pinot-quickstart > /dev/null &
+fi
+sleep 2
+open http://localhost:8080/
+
+# Just for blocking
+tail -f /dev/null
+pkill -f "kubectl port-forward sservice/presto-coordinator 8080:8080 -n pinot-quickstart"
