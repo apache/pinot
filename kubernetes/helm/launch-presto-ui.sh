beed26120b6fa2eb83c7a3695f62ee7fd0092547
xxsacxx <https://gitlab.onionlife.in/>
2020-01-28 11:37:22 +0530

    added license and renamed the file

--- kubernetes/helm/launch-presto-ui.sh
+++ kubernetes/helm/launch-presto-ui.sh
@@ -16,12 +16,13 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+
 if [[ $(nc -z  localhost 8080) != 0 ]]; then
   kubectl port-forward service/presto-coordinator 8080:8080 -n pinot-quickstart > /dev/null &
 fi
 sleep 2
 open http://localhost:8080/
 
-# Just for blocking
+# for blocking
 tail -f /dev/null
 pkill -f "kubectl port-forward service/presto-coordinator 8080:8080 -n pinot-quickstart"
