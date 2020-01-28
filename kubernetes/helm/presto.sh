e80851f945f03dc9f2a663cd42047c21c1cea023
SACHIN TRIPATHI <37820547+xxsacxx@users.noreply.github.com>
2020-01-28 11:39:51 +0530

    added license

--- kubernetes/helm/presto.sh
+++
@@ -1,9 +0,0 @@
-if [[ $(nc -z  localhost 8080) != 0 ]]; then
-  kubectl port-forward service/presto-coordinator 8080:8080 -n pinot-quickstart > /dev/null &
-fi
-sleep 2
-open http://localhost:8080/
-
-# Just for blocking
-tail -f /dev/null
-pkill -f "kubectl port-forward service/presto-coordinator 8080:8080 -n pinot-quickstart"
