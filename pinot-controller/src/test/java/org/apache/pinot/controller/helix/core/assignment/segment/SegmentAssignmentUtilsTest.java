95e0f1d14ebaf6d3f6473812ac353cb984b469d4
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2020-03-31 12:11:38 -0700

    Shuffle the segments when rebalancing the table to avoid creating hotspot servers (#5197)
    When new servers are added to an existing replica-group based table
    and rebalance is triggered, current behavior will assign segments
    in alphabetical order, which might move only the new segments to
    the new added servers. Because queries tend to query the most recent
    segments, this behavior might cause new added servers to become the
    hotspot servers.
    To address this issue, we shuffle the segments so that old and new
    segments can be balanced assigned.
    We use the hash of the table name as the random seed to shuffle the
    segments so that the result is deterministic.
    
    It is a little bit tricky to write a test case for this. Since the
    change is straight-forward and the existing tests already have
    pretty good coverage, after manually verified the expected behavior,
    no new test is added.

--- pinot-controller/src/test/java/org/apache/pinot/controller/helix/core/assignment/segment/SegmentAssignmentUtilsTest.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/helix/core/assignment/segment/SegmentAssignmentUtilsTest.java
@@ -21,10 +21,8 @@ package org.apache.pinot.controller.helix.core.assignment.segment;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 import org.apache.pinot.common.assignment.InstancePartitions;
 import org.apache.pinot.common.utils.CommonConstants.Helix.StateModel.SegmentOnlineOfflineStateModel;
@@ -207,7 +205,7 @@ public class SegmentAssignmentUtilsTest {
 
     int numSegments = 90;
     List<String> segments = SegmentAssignmentTestUtils.getNameList(SEGMENT_NAME_PREFIX, numSegments);
-    Map<Integer, Set<String>> partitionIdToSegmentsMap = Collections.singletonMap(0, new HashSet<>(segments));
+    Map<Integer, List<String>> partitionIdToSegmentsMap = Collections.singletonMap(0, segments);
     int numInstances = 9;
     List<String> instances = SegmentAssignmentTestUtils.getNameList(INSTANCE_NAME_PREFIX, numInstances);
 
