32674d0d286faaf269692af09999b5fd19fac83f
Xiang Fu <fx19880617@gmail.com>
2020-04-20 12:37:49 -0700

    move gson to fasterxml for github events quickstart

--- pinot-tools/src/main/java/org/apache/pinot/tools/streams/githubevents/PullRequestMergedEvent.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/streams/githubevents/PullRequestMergedEvent.java
@@ -18,13 +18,12 @@
  */
 package org.apache.pinot.tools.streams.githubevents;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import org.joda.time.format.DateTimeFormatter;
@@ -39,43 +38,43 @@ public class PullRequestMergedEvent {
   private static final DateTimeFormatter DATE_FORMATTER = ISODateTimeFormat.dateTimeNoMillis();
 
   // dimensions
-  private String _title;
-  private List<String> _labels;
-  private String _userId;
-  private String _userType;
-  private String _authorAssociation;
-  private String _mergedBy;
-  private List<String> _assignees;
-  private List<String> _committers;
-  private List<String> _authors;
-  private List<String> _reviewers;
-  private List<String> _commenters;
-  private List<String> _requestedReviewers;
-  private List<String> _requestedTeams;
-  private String _repo;
-  private String _organization;
+  private final String _title;
+  private final List<String> _labels;
+  private final String _userId;
+  private final String _userType;
+  private final String _authorAssociation;
+  private final String _mergedBy;
+  private final List<String> _assignees;
+  private final List<String> _committers;
+  private final List<String> _authors;
+  private final List<String> _reviewers;
+  private final List<String> _commenters;
+  private final List<String> _requestedReviewers;
+  private final List<String> _requestedTeams;
+  private final String _repo;
+  private final String _organization;
 
   // metrics
-  private long _numComments;
-  private long _numReviewComments;
-  private long _numCommits;
-  private long _numLinesAdded;
-  private long _numLinesDeleted;
-  private long _numFilesChanged;
-  private long _numReviewers;
-  private long _numCommenters;
-  private long _numCommitters;
-  private long _numAuthors;
-  private long _createdTimeMillis;
-  private long _elapsedTimeMillis;
+  private final long _numComments;
+  private final long _numReviewComments;
+  private final long _numCommits;
+  private final long _numLinesAdded;
+  private final long _numLinesDeleted;
+  private final long _numFilesChanged;
+  private final long _numReviewers;
+  private final long _numCommenters;
+  private final long _numCommitters;
+  private final long _numAuthors;
+  private final long _createdTimeMillis;
+  private final long _elapsedTimeMillis;
 
   // time
-  private long _mergedTimeMillis;
+  private final long _mergedTimeMillis;
 
-  private JsonObject _pullRequest;
-  private JsonArray _commitsArray;
-  private JsonArray _reviewCommentsArray;
-  private JsonArray _commentsArray;
+  private final JsonNode _pullRequest;
+  private final JsonNode _commitsArray;
+  private final JsonNode _reviewCommentsArray;
+  private final JsonNode _commentsArray;
 
   /**
    * Construct a PullRequestMergedEvent from the github event of type PullRequestEvent which is also merged and closed.
@@ -85,23 +84,23 @@ public class PullRequestMergedEvent {
    * @param reviewComments - review comments data corresponding to the event
    * @param comments - comments data corresponding to the event
    */
-  public PullRequestMergedEvent(JsonObject event, JsonArray commits, JsonArray reviewComments, JsonArray comments) {
+  public PullRequestMergedEvent(JsonNode event, JsonNode commits, JsonNode reviewComments, JsonNode comments) {
 
-    JsonObject payload = event.get("payload").getAsJsonObject();
-    _pullRequest = payload.get("pull_request").getAsJsonObject();
+    JsonNode payload = event.get("payload");
+    _pullRequest = payload.get("pull_request");
     _commitsArray = commits;
     _reviewCommentsArray = reviewComments;
     _commentsArray = comments;
 
     // Dimensions
-    _title = _pullRequest.get("title").getAsString();
+    _title = _pullRequest.get("title").asText();
     _labels = extractLabels();
-    JsonObject user = _pullRequest.get("user").getAsJsonObject();
-    _userId = user.get("login").getAsString();
-    _userType = user.get("type").getAsString();
-    _authorAssociation = _pullRequest.get("author_association").getAsString();
-    JsonObject mergedBy = _pullRequest.get("merged_by").getAsJsonObject();
-    _mergedBy = mergedBy.get("login").getAsString();
+    JsonNode user = _pullRequest.get("user");
+    _userId = user.get("login").asText();
+    _userType = user.get("type").asText();
+    _authorAssociation = _pullRequest.get("author_association").asText();
+    JsonNode mergedBy = _pullRequest.get("merged_by");
+    _mergedBy = mergedBy.get("login").asText();
     _assignees = extractAssignees();
     _committers = Lists.newArrayList(extractCommitters());
     _authors = Lists.newArrayList(extractAuthors());
@@ -109,26 +108,26 @@ public class PullRequestMergedEvent {
     _commenters = Lists.newArrayList(extractCommenters());
     _requestedReviewers = extractRequestedReviewers();
     _requestedTeams = extractRequestedTeams();
-    JsonObject repo = event.get("repo").getAsJsonObject();
-    String[] repoName = repo.get("name").getAsString().split("/");
+    JsonNode repo = event.get("repo");
+    String[] repoName = repo.get("name").asText().split("/");
     _repo = repoName[1];
     _organization = repoName[0];
 
     // Metrics
-    _numComments = _pullRequest.get("comments").getAsInt();
-    _numReviewComments = _pullRequest.get("review_comments").getAsInt();
-    _numCommits = _pullRequest.get("commits").getAsInt();
-    _numLinesAdded = _pullRequest.get("additions").getAsInt();
-    _numLinesDeleted = _pullRequest.get("deletions").getAsInt();
-    _numFilesChanged = _pullRequest.get("changed_files").getAsInt();
+    _numComments = _pullRequest.get("comments").asInt();
+    _numReviewComments = _pullRequest.get("review_comments").asInt();
+    _numCommits = _pullRequest.get("commits").asInt();
+    _numLinesAdded = _pullRequest.get("additions").asInt();
+    _numLinesDeleted = _pullRequest.get("deletions").asInt();
+    _numFilesChanged = _pullRequest.get("changed_files").asInt();
     _numReviewers = _reviewers.size();
     _numCommenters = _commenters.size();
     _numCommitters = _committers.size();
     _numAuthors = _authors.size();
 
     // Time
-    _createdTimeMillis = DATE_FORMATTER.parseMillis(_pullRequest.get("created_at").getAsString());
-    _mergedTimeMillis = DATE_FORMATTER.parseMillis(_pullRequest.get("merged_at").getAsString());
+    _createdTimeMillis = DATE_FORMATTER.parseMillis(_pullRequest.get("created_at").asText());
+    _mergedTimeMillis = DATE_FORMATTER.parseMillis(_pullRequest.get("merged_at").asText());
     _elapsedTimeMillis = _mergedTimeMillis - _createdTimeMillis;
   }
 
@@ -137,10 +136,10 @@ public class PullRequestMergedEvent {
    */
   private Set<String> extractReviewers() {
     Set<String> reviewers;
-    if (_reviewCommentsArray != null && !_reviewCommentsArray.isJsonNull() & _reviewCommentsArray.size() > 0) {
+    if (_reviewCommentsArray != null && _reviewCommentsArray.size() > 0) {
       reviewers = new HashSet<>();
-      for (JsonElement reviewComment : _reviewCommentsArray) {
-        reviewers.add(reviewComment.getAsJsonObject().get("user").getAsJsonObject().get("login").getAsString());
+      for (JsonNode reviewComment : _reviewCommentsArray) {
+        reviewers.add(reviewComment.get("user").get("login").asText());
       }
     } else {
       reviewers = Collections.emptySet();
@@ -153,10 +152,10 @@ public class PullRequestMergedEvent {
    */
   private Set<String> extractCommenters() {
     Set<String> commenters;
-    if (_commentsArray != null && !_commentsArray.isJsonNull() & _commentsArray.size() > 0) {
+    if (_commentsArray != null && _commentsArray.size() > 0) {
       commenters = new HashSet<>();
-      for (JsonElement comment : _commentsArray) {
-        commenters.add(comment.getAsJsonObject().get("user").getAsJsonObject().get("login").getAsString());
+      for (JsonNode comment : _commentsArray) {
+        commenters.add(comment.get("user").get("login").asText());
       }
     } else {
       commenters = Collections.emptySet();
@@ -169,17 +168,15 @@ public class PullRequestMergedEvent {
    */
   private Set<String> extractCommitters() {
     Set<String> committers;
-    if (_commitsArray != null && !_commitsArray.isJsonNull() & _commitsArray.size() > 0) {
+    if (_commitsArray != null && _commitsArray.size() > 0) {
       committers = new HashSet<>();
-      for (JsonElement commit : _commitsArray) {
-        JsonObject commitAsJsonObject = commit.getAsJsonObject();
-        JsonElement committer = commitAsJsonObject.get("committer");
-        if (committer.isJsonNull()) {
-          committers.add(
-              commitAsJsonObject.get("commit").getAsJsonObject().get("committer").getAsJsonObject().get("name")
-                  .getAsString());
+      for (JsonNode commit : _commitsArray) {
+        JsonNode commitAsJsonNode = commit;
+        JsonNode committer = commitAsJsonNode.get("committer");
+        if (committer.size() == 0) {
+          committers.add(commitAsJsonNode.get("commit").get("committer").get("name").asText());
         } else {
-          committers.add(committer.getAsJsonObject().get("login").getAsString());
+          committers.add(committer.get("login").asText());
         }
       }
     } else {
@@ -193,16 +190,15 @@ public class PullRequestMergedEvent {
    */
   private Set<String> extractAuthors() {
     Set<String> authors;
-    if (_commitsArray != null && !_commitsArray.isJsonNull() & _commitsArray.size() > 0) {
+    if (_commitsArray != null && _commitsArray.size() > 0) {
       authors = new HashSet<>();
-      for (JsonElement commit : _commitsArray) {
-        JsonObject commitAsJsonObject = commit.getAsJsonObject();
-        JsonElement author = commitAsJsonObject.get("author");
-        if (author.isJsonNull()) {
-          authors.add(commitAsJsonObject.get("commit").getAsJsonObject().get("author").getAsJsonObject().get("name")
-              .getAsString());
+      for (JsonNode commit : _commitsArray) {
+        JsonNode commitAsJsonNode = commit;
+        JsonNode author = commitAsJsonNode.get("author");
+        if (author.size() == 0) {
+          authors.add(commitAsJsonNode.get("commit").get("author").get("name").asText());
         } else {
-          authors.add(author.getAsJsonObject().get("login").getAsString());
+          authors.add(author.get("login").asText());
         }
       }
     } else {
@@ -215,16 +211,10 @@ public class PullRequestMergedEvent {
    * Extracts labels for the PR
    */
   private List<String> extractLabels() {
-    JsonArray labelsArray = _pullRequest.get("labels").getAsJsonArray();
-    List<String> labels;
-    int size = labelsArray.size();
-    if (size > 0) {
-      labels = new ArrayList<>(size);
-      for (JsonElement label : labelsArray) {
-        labels.add(label.getAsJsonObject().get("name").getAsString());
-      }
-    } else {
-      labels = Collections.emptyList();
+    Iterator<JsonNode> labelsIterator = _pullRequest.get("labels").elements();
+    List<String> labels = new ArrayList<>();
+    while (labelsIterator.hasNext()) {
+      labels.add(labelsIterator.next().get("name").asText());
     }
     return labels;
   }
@@ -233,16 +223,10 @@ public class PullRequestMergedEvent {
    * Extracts assignees for the PR
    */
   private List<String> extractAssignees() {
-    JsonArray assigneesJson = _pullRequest.get("assignees").getAsJsonArray();
-    int numAssignees = assigneesJson.size();
-    List<String> assignees;
-    if (numAssignees > 0) {
-      assignees = new ArrayList<>(numAssignees);
-      for (JsonElement reviewer : assigneesJson) {
-        assignees.add(reviewer.getAsJsonObject().get("login").getAsString());
-      }
-    } else {
-      assignees = Collections.emptyList();
+    Iterator<JsonNode> assigneesIterator = _pullRequest.get("assignees").elements();
+    List<String> assignees = new ArrayList<>();
+    while (assigneesIterator.hasNext()) {
+      assignees.add(assigneesIterator.next().get("login").asText());
     }
     return assignees;
   }
@@ -251,16 +235,10 @@ public class PullRequestMergedEvent {
    * Extracts list of requested reviewers
    */
   private List<String> extractRequestedReviewers() {
-    JsonArray requestedReviewersJson = _pullRequest.get("requested_reviewers").getAsJsonArray();
-    int numRequestedReviewers = requestedReviewersJson.size();
-    List<String> requestedReviewers;
-    if (numRequestedReviewers > 0) {
-      requestedReviewers = new ArrayList<>(numRequestedReviewers);
-      for (JsonElement reviewer : requestedReviewersJson) {
-        requestedReviewers.add(reviewer.getAsJsonObject().get("login").getAsString());
-      }
-    } else {
-      requestedReviewers = Collections.emptyList();
+    Iterator<JsonNode> requestedReviewersIterator = _pullRequest.get("requested_reviewers").elements();
+    List<String> requestedReviewers = new ArrayList<>();
+    while (requestedReviewersIterator.hasNext()) {
+      requestedReviewers.add(requestedReviewersIterator.next().get("login").asText());
     }
     return requestedReviewers;
   }
@@ -269,16 +247,11 @@ public class PullRequestMergedEvent {
    * Extracts list of review requested teams
    */
   private List<String> extractRequestedTeams() {
-    JsonArray requestedTeamsJson = _pullRequest.get("requested_teams").getAsJsonArray();
-    int numRequestedTeams = requestedTeamsJson.size();
-    List<String> requestedTeams;
-    if (numRequestedTeams > 0) {
-      requestedTeams = new ArrayList<>(numRequestedTeams);
-      for (JsonElement team : requestedTeamsJson) {
-        requestedTeams.add(team.getAsJsonObject().get("name").getAsString());
-      }
-    } else {
-      requestedTeams = Collections.emptyList();
+
+    Iterator<JsonNode> requestedTeamsIterator = _pullRequest.get("requested_teams").elements();
+    List<String> requestedTeams = new ArrayList<>();
+    while (requestedTeamsIterator.hasNext()) {
+      requestedTeams.add(requestedTeamsIterator.next().get("name").asText());
     }
     return requestedTeams;
   }
