apache/pinot

commit ac327bb7aa78fc5f3acf5c8bc84c9add2ade0674

Optimize ExpressionFilterOperator (#5132)
1. Add BYTES type and multi-value support
2. Directly consturct DocIdSet to save the overhead of filtering
3. Remove the redundant isMatch() for all scan based iterators

Also changed the numEntriesScannedInFilter for MV column to actual values fetched instead of values evaluated and added some TODOs for future filter optimization

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/AndDocIdIterator.java
@@ -21,13 +21,10 @@ package org.apache.pinot.core.operator.dociditerators;
 import java.util.Arrays;
 import org.apache.pinot.core.common.BlockDocIdIterator;
 import org.apache.pinot.core.common.Constants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
+// TODO: Optimize this
 public final class AndDocIdIterator implements BlockDocIdIterator {
-
-  static final Logger LOGGER = LoggerFactory.getLogger(AndDocIdIterator.class);
   public final BlockDocIdIterator[] docIdIterators;
   public ScanBasedDocIdIterator[] scanBasedDocIdIterators;
   public final int[] docIdPointers;
@@ -103,17 +100,6 @@ public final class AndDocIdIterator implements BlockDocIdIterator {
         if (i > 0) {
           // we need to advance all pointer since we found a new max
           i = -1;
-        }
-      }
-      if (hasScanBasedIterators && i == docIdIterators.length - 1) {
-        // this means we found the docId common to all nonScanBased iterators, now we need to ensure
-        // that its also found in scanBasedIterator, if not matched, we restart the intersection
-        for (ScanBasedDocIdIterator iterator : scanBasedDocIdIterators) {
-          if (!iterator.isMatch(currentMax)) {
-            i = -1;
-            currentMax = currentMax + 1;
-            break;
-          }
         }
       }
     }

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/ExpressionScanDocIdIterator.java
@@ -1,0 +1,342 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.core.operator.dociditerators;
+
+import java.util.Map;
+import org.apache.pinot.core.common.Constants;
+import org.apache.pinot.core.common.DataSource;
+import org.apache.pinot.core.operator.BaseOperator;
+import org.apache.pinot.core.operator.ProjectionOperator;
+import org.apache.pinot.core.operator.blocks.DocIdSetBlock;
+import org.apache.pinot.core.operator.blocks.ProjectionBlock;
+import org.apache.pinot.core.operator.filter.predicate.PredicateEvaluator;
+import org.apache.pinot.core.operator.transform.TransformResultMetadata;
+import org.apache.pinot.core.operator.transform.function.TransformFunction;
+import org.apache.pinot.core.plan.DocIdSetPlanNode;
+import org.roaringbitmap.IntIterator;
+import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
+import org.roaringbitmap.buffer.MutableRoaringBitmap;
+
+
+/**
+ * The {@code ExpressionScanDocIdIterator} is the scan-based doc id iterator that can handle filters on the expressions.
+ * It leverages the projection operator to batch processing the records block by block.
+ */
+public class ExpressionScanDocIdIterator implements ScanBasedDocIdIterator {
+  private final TransformFunction _transformFunction;
+  private final PredicateEvaluator _predicateEvaluator;
+  private final Map<String, DataSource> _dataSourceMap;
+  private final int _endDocId;
+
+  private final int[] _docIdBuffer = new int[DocIdSetPlanNode.MAX_DOC_PER_CALL];
+  private int _numDocIdsFilled;
+
+  private int _currentDocId = -1;
+  private IntIterator _intIterator = null;
+  private int _blockEndDocId = 0;
+
+  // NOTE: Number of entries scanned is not accurate because we might need to scan multiple columns in order to solve
+  // the expression, but we only track the number of entries for the resolved expression
+  private int _numEntriesScanned = 0;
+
+  public ExpressionScanDocIdIterator(TransformFunction transformFunction, PredicateEvaluator predicateEvaluator,
+      Map<String, DataSource> dataSourceMap, int startDocId, int endDocId) {
+    _transformFunction = transformFunction;
+    _predicateEvaluator = predicateEvaluator;
+    _dataSourceMap = dataSourceMap;
+    _blockEndDocId = startDocId;
+    _endDocId = endDocId;
+  }
+
+  @Override
+  public int next() {
+    if (_currentDocId == Constants.EOF) {
+      return Constants.EOF;
+    }
+
+    // If there are remaining records in the current block, return them first
+    if (_intIterator != null && _intIterator.hasNext()) {
+      _currentDocId = _intIterator.next();
+      return _currentDocId;
+    }
+
+    // Evaluate the records in the next block
+    while (_blockEndDocId < _endDocId) {
+      int blockStartDocId = _blockEndDocId;
+      _blockEndDocId = Math.min(blockStartDocId + DocIdSetPlanNode.MAX_DOC_PER_CALL, _endDocId);
+      ProjectionBlock projectionBlock =
+          new ProjectionOperator(_dataSourceMap, new RangeDocIdSetOperator(blockStartDocId, _blockEndDocId))
+              .nextBlock();
+      MutableRoaringBitmap matchingDocIds = new MutableRoaringBitmap();
+      processProjectionBlock(projectionBlock, matchingDocIds);
+      if (!matchingDocIds.isEmpty()) {
+        _intIterator = matchingDocIds.getIntIterator();
+        _currentDocId = _intIterator.next();
+        return _currentDocId;
+      }
+    }
+
+    _currentDocId = Constants.EOF;
+    return Constants.EOF;
+  }
+
+  @Override
+  public int advance(int targetDocId) {
+    if (_currentDocId == Constants.EOF) {
+      return Constants.EOF;
+    }
+    if (targetDocId <= _currentDocId) {
+      return _currentDocId;
+    }
+
+    // Search the current block first
+    if (targetDocId < _blockEndDocId) {
+      while (_intIterator.hasNext()) {
+        _currentDocId = _intIterator.next();
+        if (_currentDocId >= targetDocId) {
+          return _currentDocId;
+        }
+      }
+    }
+
+    // Search the block following the target document id
+    _intIterator = null;
+    _blockEndDocId = targetDocId;
+    return next();
+  }
+
+  @Override
+  public int currentDocId() {
+    return _currentDocId;
+  }
+
+  @Override
+  public MutableRoaringBitmap applyAnd(MutableRoaringBitmap answer) {
+    ProjectionOperator projectionOperator = new ProjectionOperator(_dataSourceMap, new BitmapDocIdSetOperator(answer));
+    MutableRoaringBitmap matchingDocIds = new MutableRoaringBitmap();
+    ProjectionBlock projectionBlock;
+    while ((projectionBlock = projectionOperator.nextBlock()) != null) {
+      processProjectionBlock(projectionBlock, matchingDocIds);
+    }
+    return matchingDocIds;
+  }
+
+  @SuppressWarnings("DuplicatedCode")
+  private void processProjectionBlock(ProjectionBlock projectionBlock, MutableRoaringBitmap matchingDocIds) {
+    TransformResultMetadata resultMetadata = _transformFunction.getResultMetadata();
+    if (resultMetadata.isSingleValue()) {
+      _numEntriesScanned += _numDocIdsFilled;
+      if (resultMetadata.hasDictionary()) {
+        int[] dictIds = _transformFunction.transformToDictIdsSV(projectionBlock);
+        for (int i = 0; i < _numDocIdsFilled; i++) {
+          if (_predicateEvaluator.applySV(dictIds[i])) {
+            matchingDocIds.add(_docIdBuffer[i]);
+          }
+        }
+      } else {
+        switch (resultMetadata.getDataType()) {
+          case INT:
+            int[] intValues = _transformFunction.transformToIntValuesSV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              if (_predicateEvaluator.applySV(intValues[i])) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case LONG:
+            long[] longValues = _transformFunction.transformToLongValuesSV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              if (_predicateEvaluator.applySV(longValues[i])) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case FLOAT:
+            float[] floatValues = _transformFunction.transformToFloatValuesSV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              if (_predicateEvaluator.applySV(floatValues[i])) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case DOUBLE:
+            double[] doubleValues = _transformFunction.transformToDoubleValuesSV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              if (_predicateEvaluator.applySV(doubleValues[i])) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case STRING:
+            String[] stringValues = _transformFunction.transformToStringValuesSV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              if (_predicateEvaluator.applySV(stringValues[i])) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case BYTES:
+            byte[][] bytesValues = _transformFunction.transformToBytesValuesSV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              if (_predicateEvaluator.applySV(bytesValues[i])) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          default:
+            throw new IllegalStateException();
+        }
+      }
+    } else {
+      if (resultMetadata.hasDictionary()) {
+        int[][] dictIdsArray = _transformFunction.transformToDictIdsMV(projectionBlock);
+        for (int i = 0; i < _numDocIdsFilled; i++) {
+          int[] dictIds = dictIdsArray[i];
+          int numDictIds = dictIds.length;
+          _numEntriesScanned += numDictIds;
+          if (_predicateEvaluator.applyMV(dictIds, numDictIds)) {
+            matchingDocIds.add(_docIdBuffer[i]);
+          }
+        }
+      } else {
+        switch (resultMetadata.getDataType()) {
+          case INT:
+            int[][] intValuesArray = _transformFunction.transformToIntValuesMV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              int[] values = intValuesArray[i];
+              int numValues = values.length;
+              _numEntriesScanned += numValues;
+              if (_predicateEvaluator.applyMV(values, numValues)) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case LONG:
+            long[][] longValuesArray = _transformFunction.transformToLongValuesMV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              long[] values = longValuesArray[i];
+              int numValues = values.length;
+              _numEntriesScanned += numValues;
+              if (_predicateEvaluator.applyMV(values, numValues)) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case FLOAT:
+            float[][] floatValuesArray = _transformFunction.transformToFloatValuesMV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              float[] values = floatValuesArray[i];
+              int numValues = values.length;
+              _numEntriesScanned += numValues;
+              if (_predicateEvaluator.applyMV(values, numValues)) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case DOUBLE:
+            double[][] doubleValuesArray = _transformFunction.transformToDoubleValuesMV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              double[] values = doubleValuesArray[i];
+              int numValues = values.length;
+              _numEntriesScanned += numValues;
+              if (_predicateEvaluator.applyMV(values, numValues)) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          case STRING:
+            String[][] valuesArray = _transformFunction.transformToStringValuesMV(projectionBlock);
+            for (int i = 0; i < _numDocIdsFilled; i++) {
+              String[] values = valuesArray[i];
+              int numValues = values.length;
+              _numEntriesScanned += numValues;
+              if (_predicateEvaluator.applyMV(values, numValues)) {
+                matchingDocIds.add(_docIdBuffer[i]);
+              }
+            }
+            break;
+          default:
+            throw new IllegalStateException();
+        }
+      }
+    }
+  }
+
+  @Override
+  public int getNumEntriesScanned() {
+    return _numEntriesScanned;
+  }
+
+  private class BitmapDocIdSetOperator extends BaseOperator<DocIdSetBlock> {
+    static final String OPERATOR_NAME = "BitmapDocIdSetOperator";
+
+    final IntIterator _intIterator;
+
+    BitmapDocIdSetOperator(ImmutableRoaringBitmap bitmap) {
+      _intIterator = bitmap.getIntIterator();
+    }
+
+    @Override
+    protected DocIdSetBlock getNextBlock() {
+      _numDocIdsFilled = 0;
+      while (_numDocIdsFilled < DocIdSetPlanNode.MAX_DOC_PER_CALL && _intIterator.hasNext()) {
+        _docIdBuffer[_numDocIdsFilled++] = _intIterator.next();
+      }
+      if (_numDocIdsFilled > 0) {
+        return new DocIdSetBlock(_docIdBuffer, _numDocIdsFilled);
+      } else {
+        return null;
+      }
+    }
+
+    @Override
+    public String getOperatorName() {
+      return OPERATOR_NAME;
+    }
+  }
+
+  /**
+   * NOTE: This operator contains only one block.
+   */
+  private class RangeDocIdSetOperator extends BaseOperator<DocIdSetBlock> {
+    static final String OPERATOR_NAME = "RangeDocIdSetOperator";
+
+    private DocIdSetBlock _docIdSetBlock;
+
+    RangeDocIdSetOperator(int startDocId, int endDocId) {
+      _numDocIdsFilled = endDocId - startDocId;
+      for (int i = 0; i < _numDocIdsFilled; i++) {
+        _docIdBuffer[i] = startDocId + i;
+      }
+      _docIdSetBlock = new DocIdSetBlock(_docIdBuffer, _numDocIdsFilled);
+    }
+
+    @Override
+    protected DocIdSetBlock getNextBlock() {
+      DocIdSetBlock docIdSetBlock = _docIdSetBlock;
+      _docIdSetBlock = null;
+      return docIdSetBlock;
+    }
+
+    @Override
+    public String getOperatorName() {
+      return OPERATOR_NAME;
+    }
+  }
+}

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/MVScanDocIdIterator.java
@@ -19,7 +19,6 @@
 package org.apache.pinot.core.operator.dociditerators;
 
 import java.util.Arrays;
-import org.apache.commons.lang3.mutable.MutableInt;
 import org.apache.pinot.core.common.BlockMetadata;
 import org.apache.pinot.core.common.BlockMultiValIterator;
 import org.apache.pinot.core.common.BlockValSet;
@@ -38,7 +37,7 @@ public class MVScanDocIdIterator implements ScanBasedDocIdIterator {
   private PredicateEvaluator evaluator;
 
   private String datasourceName;
-  private MutableInt _numEntriesScanned = new MutableInt(0);
+  private int _numEntriesScanned;
 
   public MVScanDocIdIterator(String datasourceName, BlockValSet blockValSet, BlockMetadata blockMetadata,
       PredicateEvaluator evaluator) {
@@ -76,16 +75,6 @@ public class MVScanDocIdIterator implements ScanBasedDocIdIterator {
   }
 
   @Override
-  public boolean isMatch(int docId) {
-    if (currentDocId == Constants.EOF) {
-      return false;
-    }
-    valueIterator.skipTo(docId);
-    int length = valueIterator.nextIntVal(intArray);
-    return evaluator.applyMV(intArray, length, _numEntriesScanned);
-  }
-
-  @Override
   public int advance(int targetDocId) {
     if (currentDocId == Constants.EOF) {
       return currentDocId;
@@ -113,7 +102,8 @@ public class MVScanDocIdIterator implements ScanBasedDocIdIterator {
     while (valueIterator.hasNext() && currentDocId < endDocId) {
       currentDocId = currentDocId + 1;
       int length = valueIterator.nextIntVal(intArray);
-      if (evaluator.applyMV(intArray, length, _numEntriesScanned)) {
+      _numEntriesScanned += length;
+      if (evaluator.applyMV(intArray, length)) {
         return currentDocId;
       }
     }
@@ -145,7 +135,8 @@ public class MVScanDocIdIterator implements ScanBasedDocIdIterator {
       if (docId >= startDocId) {
         valueIterator.skipTo(docId);
         length = valueIterator.nextIntVal(intArray);
-        if (evaluator.applyMV(intArray, length, _numEntriesScanned)) {
+        _numEntriesScanned += length;
+        if (evaluator.applyMV(intArray, length)) {
           result.add(docId);
         }
       }
@@ -155,6 +146,6 @@ public class MVScanDocIdIterator implements ScanBasedDocIdIterator {
 
   @Override
   public int getNumEntriesScanned() {
-    return _numEntriesScanned.intValue();
+    return _numEntriesScanned;
   }
 }

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/SVScanDocIdIterator.java
@@ -83,16 +83,6 @@ public class SVScanDocIdIterator implements ScanBasedDocIdIterator {
   }
 
   @Override
-  public boolean isMatch(int docId) {
-    if (_currentDocId == Constants.EOF) {
-      return false;
-    }
-    _valueIterator.skipTo(docId);
-    _numEntriesScanned++;
-    return _valueMatcher.doesCurrentEntryMatch(_valueIterator);
-  }
-
-  @Override
   public int advance(int targetDocId) {
     if (_currentDocId == Constants.EOF) {
       return _currentDocId;

commit 6cdf280e4a28adc9db762c4196b0cb0fb9f8b8b9

Fix the bug introduced in #5132 where ScanBasedDocIdIterator.isMatch() is still required
ScanBasedDocIdIterator.isMatch() is still required when the query is in the following format:
SELECT ... WHERE (filterA OR filterB) AND filterC
And filterC is working on a scan-based column (column without inverted index)

Enhance the HybridClusterIntegrationTest to catch this issue

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/AndDocIdIterator.java
@@ -102,6 +102,17 @@ public final class AndDocIdIterator implements BlockDocIdIterator {
           i = -1;
         }
       }
+      if (hasScanBasedIterators && i == docIdIterators.length - 1) {
+        // this means we found the docId common to all nonScanBased iterators, now we need to ensure
+        // that its also found in scanBasedIterator, if not matched, we restart the intersection
+        for (ScanBasedDocIdIterator iterator : scanBasedDocIdIterators) {
+          if (!iterator.isMatch(currentMax)) {
+            i = -1;
+            currentMax = currentMax + 1;
+            break;
+          }
+        }
+      }
     }
     currentDocId = currentMax;
     return currentDocId;

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/ExpressionScanDocIdIterator.java
@@ -30,6 +30,7 @@ import org.apache.pinot.core.operator.transform.TransformResultMetadata;
 import org.apache.pinot.core.operator.transform.function.TransformFunction;
 import org.apache.pinot.core.plan.DocIdSetPlanNode;
 import org.roaringbitmap.IntIterator;
+import org.roaringbitmap.PeekableIntIterator;
 import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
 import org.roaringbitmap.buffer.MutableRoaringBitmap;
 
@@ -48,8 +49,9 @@ public class ExpressionScanDocIdIterator implements ScanBasedDocIdIterator {
   private int _numDocIdsFilled;
 
   private int _currentDocId = -1;
-  private IntIterator _intIterator = null;
-  private int _blockEndDocId = 0;
+  private MutableRoaringBitmap _matchingDocIds;
+  private PeekableIntIterator _docIdIterator;
+  private int _blockEndDocId;
 
   // NOTE: Number of entries scanned is not accurate because we might need to scan multiple columns in order to solve
   // the expression, but we only track the number of entries for the resolved expression
@@ -65,14 +67,35 @@ public class ExpressionScanDocIdIterator implements ScanBasedDocIdIterator {
   }
 
   @Override
+  public boolean isMatch(int docId) {
+    if (_currentDocId == Constants.EOF) {
+      return false;
+    }
+    if (docId < _blockEndDocId) {
+      // Document id within the current block
+      return _matchingDocIds.contains(docId);
+    } else {
+      // Search the block following the document id
+      _blockEndDocId = docId;
+      _matchingDocIds = null;
+      _docIdIterator = null;
+      if (next() == Constants.EOF) {
+        return false;
+      } else {
+        return _matchingDocIds.contains(docId);
+      }
+    }
+  }
+
+  @Override
   public int next() {
     if (_currentDocId == Constants.EOF) {
       return Constants.EOF;
     }
 
     // If there are remaining records in the current block, return them first
-    if (_intIterator != null && _intIterator.hasNext()) {
-      _currentDocId = _intIterator.next();
+    if (_docIdIterator != null && _docIdIterator.hasNext()) {
+      _currentDocId = _docIdIterator.next();
       return _currentDocId;
     }
 
@@ -86,8 +109,9 @@ public class ExpressionScanDocIdIterator implements ScanBasedDocIdIterator {
       MutableRoaringBitmap matchingDocIds = new MutableRoaringBitmap();
       processProjectionBlock(projectionBlock, matchingDocIds);
       if (!matchingDocIds.isEmpty()) {
-        _intIterator = matchingDocIds.getIntIterator();
-        _currentDocId = _intIterator.next();
+        _matchingDocIds = matchingDocIds;
+        _docIdIterator = matchingDocIds.getIntIterator();
+        _currentDocId = _docIdIterator.next();
         return _currentDocId;
       }
     }
@@ -105,19 +129,21 @@ public class ExpressionScanDocIdIterator implements ScanBasedDocIdIterator {
       return _currentDocId;
     }
 
-    // Search the current block first
     if (targetDocId < _blockEndDocId) {
-      while (_intIterator.hasNext()) {
-        _currentDocId = _intIterator.next();
-        if (_currentDocId >= targetDocId) {
-          return _currentDocId;
-        }
+      // Search the current block first
+      _docIdIterator.advanceIfNeeded(targetDocId);
+      if (_docIdIterator.hasNext()) {
+        _currentDocId = _docIdIterator.next();
+        return _currentDocId;
       }
+    } else {
+      // Skipping the blocks before the target document id
+      _blockEndDocId = targetDocId;
     }
 
     // Search the block following the target document id
-    _intIterator = null;
-    _blockEndDocId = targetDocId;
+    _matchingDocIds = null;
+    _docIdIterator = null;
     return next();
   }
 
@@ -283,34 +309,6 @@ public class ExpressionScanDocIdIterator implements ScanBasedDocIdIterator {
     return _numEntriesScanned;
   }
 
-  private class BitmapDocIdSetOperator extends BaseOperator<DocIdSetBlock> {
-    static final String OPERATOR_NAME = "BitmapDocIdSetOperator";
-
-    final IntIterator _intIterator;
-
-    BitmapDocIdSetOperator(ImmutableRoaringBitmap bitmap) {
-      _intIterator = bitmap.getIntIterator();
-    }
-
-    @Override
-    protected DocIdSetBlock getNextBlock() {
-      _numDocIdsFilled = 0;
-      while (_numDocIdsFilled < DocIdSetPlanNode.MAX_DOC_PER_CALL && _intIterator.hasNext()) {
-        _docIdBuffer[_numDocIdsFilled++] = _intIterator.next();
-      }
-      if (_numDocIdsFilled > 0) {
-        return new DocIdSetBlock(_docIdBuffer, _numDocIdsFilled);
-      } else {
-        return null;
-      }
-    }
-
-    @Override
-    public String getOperatorName() {
-      return OPERATOR_NAME;
-    }
-  }
-
   /**
    * NOTE: This operator contains only one block.
    */
@@ -332,6 +330,37 @@ public class ExpressionScanDocIdIterator implements ScanBasedDocIdIterator {
       DocIdSetBlock docIdSetBlock = _docIdSetBlock;
       _docIdSetBlock = null;
       return docIdSetBlock;
+    }
+
+    @Override
+    public String getOperatorName() {
+      return OPERATOR_NAME;
+    }
+  }
+
+  /**
+   * NOTE: This operator may contain multiple blocks.
+   */
+  private class BitmapDocIdSetOperator extends BaseOperator<DocIdSetBlock> {
+    static final String OPERATOR_NAME = "BitmapDocIdSetOperator";
+
+    final IntIterator _intIterator;
+
+    BitmapDocIdSetOperator(ImmutableRoaringBitmap bitmap) {
+      _intIterator = bitmap.getIntIterator();
+    }
+
+    @Override
+    protected DocIdSetBlock getNextBlock() {
+      _numDocIdsFilled = 0;
+      while (_numDocIdsFilled < DocIdSetPlanNode.MAX_DOC_PER_CALL && _intIterator.hasNext()) {
+        _docIdBuffer[_numDocIdsFilled++] = _intIterator.next();
+      }
+      if (_numDocIdsFilled > 0) {
+        return new DocIdSetBlock(_docIdBuffer, _numDocIdsFilled);
+      } else {
+        return null;
+      }
     }
 
     @Override

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/MVScanDocIdIterator.java
@@ -75,6 +75,17 @@ public class MVScanDocIdIterator implements ScanBasedDocIdIterator {
   }
 
   @Override
+  public boolean isMatch(int docId) {
+    if (currentDocId == Constants.EOF) {
+      return false;
+    }
+    valueIterator.skipTo(docId);
+    int length = valueIterator.nextIntVal(intArray);
+    _numEntriesScanned += length;
+    return evaluator.applyMV(intArray, length);
+  }
+
+  @Override
   public int advance(int targetDocId) {
     if (currentDocId == Constants.EOF) {
       return currentDocId;
@@ -89,8 +100,7 @@ public class MVScanDocIdIterator implements ScanBasedDocIdIterator {
     } else {
       currentDocId = targetDocId - 1;
       valueIterator.skipTo(targetDocId);
-      int next = next();
-      return next;
+      return next();
     }
   }
 

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/SVScanDocIdIterator.java
@@ -18,12 +18,12 @@
  */
 package org.apache.pinot.core.operator.dociditerators;
 
-import org.apache.pinot.spi.data.FieldSpec;
 import org.apache.pinot.core.common.BlockMetadata;
 import org.apache.pinot.core.common.BlockSingleValIterator;
 import org.apache.pinot.core.common.BlockValSet;
 import org.apache.pinot.core.common.Constants;
 import org.apache.pinot.core.operator.filter.predicate.PredicateEvaluator;
+import org.apache.pinot.spi.data.FieldSpec;
 import org.roaringbitmap.IntIterator;
 import org.roaringbitmap.buffer.MutableRoaringBitmap;
 
@@ -80,6 +80,16 @@ public class SVScanDocIdIterator implements ScanBasedDocIdIterator {
    */
   public void setEndDocId(int endDocId) {
     _endDocId = endDocId;
+  }
+
+  @Override
+  public boolean isMatch(int docId) {
+    if (_currentDocId == Constants.EOF) {
+      return false;
+    }
+    _valueIterator.skipTo(docId);
+    _numEntriesScanned++;
+    return _valueMatcher.doesCurrentEntryMatch(_valueIterator);
   }
 
   @Override

pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/ScanBasedDocIdIterator.java
@@ -31,6 +31,8 @@ import org.roaringbitmap.buffer.MutableRoaringBitmap;
  */
 public interface ScanBasedDocIdIterator extends BlockDocIdIterator {
 
+  boolean isMatch(int docId);
+
   MutableRoaringBitmap applyAnd(MutableRoaringBitmap answer);
 
   /**

pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/BaseClusterIntegrationTestSet.java
@@ -138,6 +138,9 @@ public abstract class BaseClusterIntegrationTestSet extends BaseClusterIntegrati
     testQuery(query, Arrays.asList("SELECT MAX(ArrTime) FROM mytable WHERE DaysSinceEpoch >= 15312",
         "SELECT MIN(ArrTime) FROM mytable WHERE DaysSinceEpoch >= 15312"));
     query =
+        "SELECT SUM(TotalAddGTime) FROM mytable WHERE DivArrDelay NOT IN (67, 260) AND Carrier IN ('F9', 'B6') OR DepTime BETWEEN 2144 AND 1926";
+    testQuery(query, Collections.singletonList(query));
+    query =
         "SELECT ActualElapsedTime, OriginStateFips, MIN(DivReachedDest), SUM(ArrDelay), AVG(CRSDepTime) FROM mytable "
             + "WHERE OriginCityName > 'Beaumont/Port Arthur, TX' OR FlightDate IN ('2014-12-09', '2014-10-05')"
             + " GROUP BY ActualElapsedTime, OriginStateFips "
@@ -320,7 +323,6 @@ public abstract class BaseClusterIntegrationTestSet extends BaseClusterIntegrati
 
   /**
    * Test random SQL queries from the query file.
-   * TODO: fix this test by adding the SQL query file
    */
   public void testSqlQueriesFromQueryFile()
       throws Exception {

pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/HybridClusterIntegrationTest.java
@@ -244,9 +244,30 @@ public class HybridClusterIntegrationTest extends BaseClusterIntegrationTestSet 
 
   @Test
   @Override
+  public void testHardcodedQueries()
+      throws Exception {
+    super.testHardcodedQueries();
+  }
+
+  @Test
+  @Override
+  public void testHardcodedSqlQueries()
+      throws Exception {
+    super.testHardcodedSqlQueries();
+  }
+
+  @Test
+  @Override
   public void testQueriesFromQueryFile()
       throws Exception {
     super.testQueriesFromQueryFile();
+  }
+
+  @Test
+  @Override
+  public void testSqlQueriesFromQueryFile()
+      throws Exception {
+    super.testSqlQueriesFromQueryFile();
   }
 
   @Test