869fff8d0df798f88c26669db3d2451e9d93292c
Akshay Rai <akrai@linkedin.com>
2020-06-01 18:01:20 -0700

    [TE] Fix the run-backend and run-frontend scripts

--- thirdeye/run-backend.sh
+++ thirdeye/run-backend.sh
@@ -4,5 +4,5 @@ echo "Launching ThirdEye backend worker in demo mode"
 echo "*******************************************************"
 
 cd thirdeye-pinot
-java -cp "./target/thirdeye-pinot-1.0-SNAPSHOT.jar" org.apache.pinot.thirdeye.anomaly.ThirdEyeAnomalyApplication "./config"
+java -cp "./target/thirdeye-pinot-1.0.0-SNAPSHOT.jar" org.apache.pinot.thirdeye.anomaly.ThirdEyeAnomalyApplication "./config"
 cd ..
