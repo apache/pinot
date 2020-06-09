5b3837cf5bd1aadfed1e0707dc7c41721ea2265e
Xiang Fu <fx19880617@gmail.com>
2020-06-09 14:19:02 -0700

    Adding github actions for pinot tests (#5512)

---
+++ .github/workflows/scripts/.pinot_quickstart.sh
@@ -0,0 +1,166 @@
+#!/bin/bash -x
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+# Java version
+java -version
+
+# Check ThirdEye related changes
+COMMIT_BEFORE=$(jq -r ".pull_request.base.sha" "${GITHUB_EVENT_PATH}")
+COMMIT_AFTER=$(jq -r ".pull_request.head.sha" "${GITHUB_EVENT_PATH}")
+git fetch
+git diff --name-only "${COMMIT_BEFORE}...${COMMIT_AFTER}" | grep -E
+if [ $? -eq 0 ]; then
+  echo 'Skip ThirdEye tests for Quickstart'
+  exit 0
+fi
+
+# Build
+PASS=1
+for i in $(seq 1 5)
+do
+  if [ "${PASS}" -eq 0 ]; then
+    break;
+  fi
+  mvn clean install -B -DskipTests=true -Pbin-dist -Dmaven.javadoc.skip=true ${DEPLOY_BUILD_OPTS} ${KAFKA_BUILD_OPTS} > /tmp/mvn_build_log
+  if [ $? -eq 0 ]; then
+    PASS=0
+  else
+    tail -1000 /tmp/mvn_build_log
+    PASS=1
+  fi
+done
+if [ "${PASS}" != 0 ]; then
+    exit 1;
+fi
+
+# Quickstart
+DIST_BIN_DIR=$(ls -d pinot-distribution/target/apache-pinot-*/apache-pinot-*)
+cd "${DIST_BIN_DIR}" || exit
+
+# Test quick-start-batch
+bin/quick-start-batch.sh &
+PID=$!
+
+PASS=0
+sleep 30
+for i in $(seq 1 200)
+do
+  curl -X POST --header 'Accept: application/json'  -d '{"sql":"select count(*) from baseballStats limit 1","trace":false}' http://localhost:8000/query/sql
+  COUNT_STAR_RES=$(curl -X POST --header 'Accept: application/json'  -d '{"sql":"select count(*) from baseballStats limit 1","trace":false}' http://localhost:8000/query/sql | jq '.resultTable.rows[0][0]')
+  if [[ "${COUNT_STAR_RES}" =~ ^[0-9]+$ ]]; then
+    if [ "${COUNT_STAR_RES}" -eq 97889 ]; then
+      PASS=1
+      break
+    fi
+  fi
+  sleep 2
+done
+
+if [ "${PASS}" -eq 0 ]; then
+  echo 'Batch Quickstart failed: Cannot get correct result for count star query.'
+  exit 1
+fi
+
+kill -9 $PID
+rm -rf /tmp/PinotAdmin/zkData
+
+# Test quick-start-streaming
+bin/quick-start-streaming.sh &
+PID=$!
+
+PASS=0
+RES_1=0
+sleep 30
+
+for i in $(seq 1 200)
+do
+  curl -X POST --header 'Accept: application/json'  -d '{"sql":"select count(*) from meetupRsvp limit 1","trace":false}' http://localhost:8000/query/sql
+  COUNT_STAR_RES=$(curl -X POST --header 'Accept: application/json'  -d '{"sql":"select count(*) from meetupRsvp limit 1","trace":false}' http://localhost:8000/query/sql | jq '.resultTable.rows[0][0]')
+ if [[ "${COUNT_STAR_RES}" =~ ^[0-9]+$ ]]; then
+    if [ "${COUNT_STAR_RES}" -gt 0 ]; then
+      if [ "${RES_1}" -eq 0 ]; then
+        RES_1=${COUNT_STAR_RES}
+        continue
+      fi
+    fi
+    if [ "${COUNT_STAR_RES}" -gt "${RES_1}" ]; then
+      PASS=1
+      break
+    fi
+  fi
+  sleep 2
+done
+
+if [ "${PASS}" -eq 0 ]; then
+  if [ "${RES_1}" -eq 0 ]; then
+    echo 'Streaming Quickstart test failed: Cannot get correct result for count star query.'
+    exit 1
+  fi
+  echo 'Streaming Quickstart test failed: Cannot get incremental counts for count star query.'
+  exit 1
+fi
+
+kill -9 $PID
+rm -rf /tmp/PinotAdmin/zkData
+
+# Test quick-start-hybrid
+cd bin
+./quick-start-hybrid.sh &
+PID=$!
+
+PASS=0
+RES_1=0
+sleep 30
+for i in $(seq 1 200)
+do
+  curl -X POST --header 'Accept: application/json'  -d '{"sql":"select count(*) from airlineStats limit 1","trace":false}' http://localhost:8000/query/sql
+  COUNT_STAR_RES=$(curl -X POST --header 'Accept: application/json'  -d '{"sql":"select count(*) from airlineStats limit 1","trace":false}' http://localhost:8000/query/sql | jq '.resultTable.rows[0][0]')
+  if [[ "${COUNT_STAR_RES}" =~ ^[0-9]+$ ]]; then
+    if [ "${COUNT_STAR_RES}" -gt 0 ]; then
+      if [ "${RES_1}" -eq 0 ]; then
+        RES_1=${COUNT_STAR_RES}
+        continue
+      fi
+    fi
+    if [ "${COUNT_STAR_RES}" -gt "${RES_1}" ]; then
+      PASS=1
+      break
+    fi
+  fi
+  sleep 2
+done
+
+if [ "${PASS}" -eq 0 ]; then
+  if [ "${RES_1}" -eq 0 ]; then
+    echo 'Hybrid Quickstart test failed: Cannot get correct result for count star query.'
+    exit 1
+  fi
+  echo 'Hybrid Quickstart test failed: Cannot get incremental counts for count star query.'
+  exit 1
+fi
+
+kill -9 $PID
+rm -rf /tmp/PinotAdmin/zkData
+
+cd ../../../../../
+pwd
+mvn clean > /dev/null
+
+exit 0
---
+++ .github/workflows/scripts/.pinot_test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash -x
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+# Java version
+java -version
+
+# Check ThirdEye related changes
+COMMIT_BEFORE=$(jq -r ".pull_request.base.sha" "${GITHUB_EVENT_PATH}")
+COMMIT_AFTER=$(jq -r ".pull_request.head.sha" "${GITHUB_EVENT_PATH}")
+git fetch
+git diff --name-only "${COMMIT_BEFORE}...${COMMIT_AFTER}" | grep -E
+if [ $? -eq 0 ]; then
+  echo 'ThirdEye changes.'
+
+  if [ "${RUN_INTEGRATION_TESTS}" == false ]; then
+    echo 'Skip ThirdEye tests when integration tests off'
+    exit 0
+  fi
+
+  cd thirdeye
+  mvn test
+  failed=$?
+  if [ $failed -eq 0 ]; then
+    exit 0
+  else
+    exit 1
+  fi
+fi
+
+passed=0
+
+# Only run integration tests if needed
+if [ "$RUN_INTEGRATION_TESTS" != false ]; then
+  mvn test -B -P travis,travis-integration-tests-only
+  if [ $? -eq 0 ]; then
+    passed=1
+  fi
+else
+  mvn test -B -P travis,travis-no-integration-tests
+  if [ $? -eq 0 ]; then
+    passed=1
+  fi
+fi
