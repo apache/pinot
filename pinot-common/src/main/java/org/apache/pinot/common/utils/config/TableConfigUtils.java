17c9b4414b92e698bb3a4a5b7405cda0ea7809bf
Sreemanth Pulagam <sreemanth.tan@gmail.com>
2020-06-17 00:47:48 +0530

    Do not allow realtime table creation if timeColumnName is null (#5571)
    Issue #5558: Do not allow realtime table creation if timeColumnName is null

--- pinot-common/src/main/java/org/apache/pinot/common/utils/config/TableConfigUtils.java
+++ pinot-common/src/main/java/org/apache/pinot/common/utils/config/TableConfigUtils.java
@@ -35,6 +35,7 @@ import org.apache.pinot.spi.config.table.SegmentsValidationAndRetentionConfig;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.config.table.TableCustomConfig;
 import org.apache.pinot.spi.config.table.TableTaskConfig;
+import org.apache.pinot.spi.config.table.TableType;
 import org.apache.pinot.spi.config.table.TenantConfig;
 import org.apache.pinot.spi.config.table.UpsertConfig;
 import org.apache.pinot.spi.config.table.assignment.InstanceAssignmentConfig;
@@ -211,6 +212,9 @@ public class TableConfigUtils {
   private static void validateValidationConfig(TableConfig tableConfig) {
     SegmentsValidationAndRetentionConfig validationConfig = tableConfig.getValidationConfig();
     if (validationConfig != null) {
+      if (tableConfig.getTableType() == TableType.REALTIME && validationConfig.getTimeColumnName() == null) {
+        throw new IllegalStateException("Must provide time column in real-time table config");
+      }
       String peerSegmentDownloadScheme = validationConfig.getPeerSegmentDownloadScheme();
       if (peerSegmentDownloadScheme != null) {
         if (!"http".equalsIgnoreCase(peerSegmentDownloadScheme) && !"https".equalsIgnoreCase(peerSegmentDownloadScheme)) {
