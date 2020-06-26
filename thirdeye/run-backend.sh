80f4d62d1ee569cc98a8e472c6b425c3079341f7
vincentchenjl <jianchen@linkedin.com>
2020-06-26 13:55:36 -0700

    [TE] fix run-backend.sh with log4j2

--- thirdeye/run-backend.sh
+++ thirdeye/run-backend.sh
@@ -4,5 +4,5 @@ echo "Launching ThirdEye backend worker in demo mode"
 echo "*******************************************************"
 
 cd thirdeye-pinot
-java -cp "./target/thirdeye-pinot-1.0.0-SNAPSHOT.jar" org.apache.pinot.thirdeye.anomaly.ThirdEyeAnomalyApplication "./config"
+java -Dlog4j.configurationFile=log4j2.xml -cp "./target/thirdeye-pinot-1.0.0-SNAPSHOT.jar" org.apache.pinot.thirdeye.anomaly.ThirdEyeAnomalyApplication "./config"
 cd ..
