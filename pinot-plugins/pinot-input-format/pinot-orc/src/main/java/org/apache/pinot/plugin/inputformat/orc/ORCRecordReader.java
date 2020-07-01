apache/pinot

commit 0622d2c492139738c9078e0a23ce226367ed2b96

Re-implement ORCRecordReader (#5267)
- Support batch read
- Support most value types
- Support read only required fields
- Enhance tests to cover most value types

pinot-plugins/pinot-input-format/pinot-orc/src/main/java/org/apache/pinot/plugin/inputformat/orc/ORCRecordReader.java
@@ -18,12 +18,24 @@
  */
 package org.apache.pinot.plugin.inputformat.orc;
 
+import com.google.common.base.Preconditions;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nullable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.ql.exec.vector.BytesColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.ColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.DoubleColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.ListColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.MapColumnVector;
+import org.apache.hadoop.hive.ql.exec.vector.TimestampColumnVector;
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
 import org.apache.orc.OrcFile;
 import org.apache.orc.Reader;
@@ -33,57 +45,122 @@ import org.apache.pinot.spi.data.readers.GenericRow;
 import org.apache.pinot.spi.data.readers.RecordReader;
 import org.apache.pinot.spi.data.readers.RecordReaderConfig;
 import org.apache.pinot.spi.utils.SchemaFieldExtractorUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.pinot.spi.utils.StringUtils;
 
 
 /**
- * The ORCRecordReader uses a VectorizedRowBatch, which we convert to a Writable. Then, we convert these
- * Writable objects to primitives that we can then store in a GenericRow.
- *
- * When new data types are added to Pinot, we will need to update them here as well.
- * Note that not all ORC types are supported; we only support the ORC types that correspond to either
- * primitives or multivalue columns in Pinot, which is similar to other record readers.
+ * Record Reader for ORC files.
+ * <p>The following ORC types are supported:
+ * <ul>
+ *   <li>BOOLEAN -> String</li> TODO: -> Boolean?
+ *   <li>BYTE, SHORT, INT -> Integer</li>
+ *   <li>LONG, DATE, TIMESTAMP -> Long</li>
+ *   <li>FLOAT -> Float</li>
+ *   <li>DOUBLE -> Double</li>
+ *   <li>STRING, VARCHAR, CHAR -> String</li>
+ *   <li>BINARY -> byte[]</li>
+ *   <li>LIST -> Object[] of the supported types</li> TODO: -> List?
+ *   <li>MAP -> Map of the supported types</li>
+ * </ul>
  */
 public class ORCRecordReader implements RecordReader {
-  private static final Logger LOGGER = LoggerFactory.getLogger(ORCRecordReader.class);
-  private static final String LOCAL_FS_PREFIX = "file://";
-
-  private Schema _pinotSchema;
-  private ORCRecordExtractor _recordExtractor;
-  private TypeDescription _orcSchema;
-  private Reader _reader;
-  private org.apache.orc.RecordReader _recordReader;
-  private VectorizedRowBatch _reusableVectorizedRowBatch;
+  private Schema _schema;
+  private List<String> _orcFields;
+  private List<TypeDescription> _orcFieldTypes;
+  private boolean[] _includeOrcFields;
+  private org.apache.orc.RecordReader _orcRecordReader;
+  private VectorizedRowBatch _rowBatch;
+  private boolean _hasNext;
+  private int _nextRowId;
 
   @Override
   public void init(File dataFile, Schema schema, @Nullable RecordReaderConfig recordReaderConfig)
       throws IOException {
-    _pinotSchema = schema;
-    Path dataFilePath = new Path(LOCAL_FS_PREFIX + dataFile.getAbsolutePath());
-    LOGGER.info("Creating segment from path: {}", dataFilePath);
-    _reader = OrcFile.createReader(dataFilePath, OrcFile.readerOptions(new Configuration()));
-    _orcSchema = _reader.getSchema();
-    LOGGER.info("ORC schema: {}", _orcSchema.toJson());
-    _recordReader = _reader.rows(_reader.options().schema(_orcSchema));
+    _schema = schema;
 
-    // Create a row batch with max size 1
-    _reusableVectorizedRowBatch = _orcSchema.createRowBatch(1);
-    Set<String> sourceFields = SchemaFieldExtractorUtils.extract(schema);
-    ORCRecordExtractorConfig recordExtractorConfig = new ORCRecordExtractorConfig();
-    recordExtractorConfig.setOrcSchema(_orcSchema);
-    _recordExtractor = new ORCRecordExtractor();
-    _recordExtractor.init(sourceFields, recordExtractorConfig);
+    Reader orcReader =
+        OrcFile.createReader(new Path(dataFile.getAbsolutePath()), OrcFile.readerOptions(new Configuration()));
+    TypeDescription orcSchema = orcReader.getSchema();
+    Preconditions
+        .checkState(orcSchema.getCategory() == TypeDescription.Category.STRUCT, "ORC schema must be of type: STRUCT");
+    _orcFields = orcSchema.getFieldNames();
+    _orcFieldTypes = orcSchema.getChildren();
+
+    // Only read the required fields
+    Set<String> schemaFields = SchemaFieldExtractorUtils.extract(schema);
+    int numOrcFields = _orcFields.size();
+    _includeOrcFields = new boolean[numOrcFields];
+    // NOTE: Include for ORC reader uses field id as the index
+    boolean[] orcReaderInclude = new boolean[orcSchema.getMaximumId() + 1];
+    orcReaderInclude[orcSchema.getId()] = true;
+    for (int i = 0; i < numOrcFields; i++) {
+      String field = _orcFields.get(i);
+      if (schemaFields.contains(field)) {
+        TypeDescription fieldType = _orcFieldTypes.get(i);
+        TypeDescription.Category category = fieldType.getCategory();
+        if (category == TypeDescription.Category.LIST) {
+          // Multi-value field
+          TypeDescription.Category childCategory = fieldType.getChildren().get(0).getCategory();
+          Preconditions.checkState(isSupportedSingleValueType(childCategory), "Illegal multi-value field type: %s",
+              childCategory);
+          // NOTE: LIST is stored as 2 vectors
+          int fieldId = fieldType.getId();
+          orcReaderInclude[fieldId] = true;
+          orcReaderInclude[fieldId + 1] = true;
+        } else if (category == TypeDescription.Category.MAP) {
+          // Map field
+          List<TypeDescription> children = fieldType.getChildren();
+          TypeDescription.Category keyCategory = children.get(0).getCategory();
+          Preconditions
+              .checkState(isSupportedSingleValueType(keyCategory), "Illegal map key field type: %s", keyCategory);
+          TypeDescription.Category valueCategory = children.get(1).getCategory();
+          Preconditions
+              .checkState(isSupportedSingleValueType(valueCategory), "Illegal map value field type: %s", valueCategory);
+          // NOTE: MAP is stored as 3 vectors
+          int fieldId = fieldType.getId();
+          orcReaderInclude[fieldId] = true;
+          orcReaderInclude[fieldId + 1] = true;
+          orcReaderInclude[fieldId + 2] = true;
+        } else {
+          // Single-value field
+          Preconditions
+              .checkState(isSupportedSingleValueType(category), "Illegal single-value field type: %s", category);
+          orcReaderInclude[fieldType.getId()] = true;
+        }
+      }
+      _includeOrcFields[i] = true;
+    }
+
+    _orcRecordReader = orcReader.rows(new Reader.Options().include(orcReaderInclude));
+    _rowBatch = orcSchema.createRowBatch();
+    _hasNext = _orcRecordReader.nextBatch(_rowBatch);
+    _nextRowId = 0;
+  }
+
+  private static boolean isSupportedSingleValueType(TypeDescription.Category category) {
+    switch (category) {
+      case BOOLEAN:
+      case BYTE:
+      case SHORT:
+      case INT:
+      case LONG:
+      case FLOAT:
+      case DOUBLE:
+      case STRING:
+      case DATE:
+      case TIMESTAMP:
+      case BINARY:
+      case VARCHAR:
+      case CHAR:
+        return true;
+      default:
+        return false;
+    }
   }
 
   @Override
   public boolean hasNext() {
-    try {
-      return _recordReader.getProgress() != 1;
-    } catch (IOException e) {
-      LOGGER.error("Could not get next record");
-      throw new RuntimeException(e);
-    }
+    return _hasNext;
   }
 
   @Override
@@ -95,25 +172,182 @@ public class ORCRecordReader implements RecordReader {
   @Override
   public GenericRow next(GenericRow reuse)
       throws IOException {
-    _recordReader.nextBatch(_reusableVectorizedRowBatch);
-    return _recordExtractor.extract(_reusableVectorizedRowBatch, reuse);
+    int numFields = _orcFields.size();
+    for (int i = 0; i < numFields; i++) {
+      if (!_includeOrcFields[i]) {
+        continue;
+      }
+      String field = _orcFields.get(i);
+      TypeDescription fieldType = _orcFieldTypes.get(i);
+      TypeDescription.Category category = fieldType.getCategory();
+      if (category == TypeDescription.Category.LIST) {
+        // Multi-value field, extract to Object[]
+        TypeDescription.Category childCategory = fieldType.getChildren().get(0).getCategory();
+        ListColumnVector listColumnVector = (ListColumnVector) _rowBatch.cols[i];
+        int rowId = listColumnVector.isRepeating ? 0 : _nextRowId;
+        if ((listColumnVector.noNulls || !listColumnVector.isNull[rowId])) {
+          int offset = (int) listColumnVector.offsets[rowId];
+          int length = (int) listColumnVector.lengths[rowId];
+          List<Object> values = new ArrayList<>(length);
+          for (int j = 0; j < length; j++) {
+            Object value = extractSingleValue(listColumnVector.child, offset + j, childCategory);
+            // NOTE: Only keep non-null values
+            // TODO: Revisit
+            if (value != null) {
+              values.add(value);
+            }
+          }
+          if (!values.isEmpty()) {
+            reuse.putValue(field, values.toArray());
+          } else {
+            // NOTE: Treat empty list as null
+            // TODO: Revisit
+            reuse.putValue(field, null);
+          }
+        } else {
+          reuse.putValue(field, null);
+        }
+      } else if (category == TypeDescription.Category.MAP) {
+        // Map field
+        List<TypeDescription> children = fieldType.getChildren();
+        TypeDescription.Category keyCategory = children.get(0).getCategory();
+        TypeDescription.Category valueCategory = children.get(1).getCategory();
+        MapColumnVector mapColumnVector = (MapColumnVector) _rowBatch.cols[i];
+        int rowId = mapColumnVector.isRepeating ? 0 : _nextRowId;
+        if ((mapColumnVector.noNulls || !mapColumnVector.isNull[rowId])) {
+          int offset = (int) mapColumnVector.offsets[rowId];
+          int length = (int) mapColumnVector.lengths[rowId];
+          Map<Object, Object> map = new HashMap<>();
+          for (int j = 0; j < length; j++) {
+            int childRowId = offset + j;
+            Object key = extractSingleValue(mapColumnVector.keys, childRowId, keyCategory);
+            Object value = extractSingleValue(mapColumnVector.values, childRowId, valueCategory);
+            map.put(key, value);
+          }
+          reuse.putValue(field, map);
+        } else {
+          reuse.putValue(field, null);
+        }
+      } else {
+        // Single-value field
+        reuse.putValue(field, extractSingleValue(_rowBatch.cols[i], _nextRowId, category));
+      }
+    }
+
+    if (++_nextRowId == _rowBatch.size) {
+      _hasNext = _orcRecordReader.nextBatch(_rowBatch);
+      _nextRowId = 0;
+    }
+    return reuse;
   }
 
+  @Nullable
+  private static Object extractSingleValue(ColumnVector columnVector, int rowId, TypeDescription.Category category) {
+    if (columnVector.isRepeating) {
+      rowId = 0;
+    }
+    switch (category) {
+      case BOOLEAN:
+        // Extract to String
+        LongColumnVector longColumnVector = (LongColumnVector) columnVector;
+        if (longColumnVector.noNulls || !longColumnVector.isNull[rowId]) {
+          return Boolean.toString(longColumnVector.vector[rowId] == 1);
+        } else {
+          return null;
+        }
+      case BYTE:
+      case SHORT:
+      case INT:
+        // Extract to Integer
+        longColumnVector = (LongColumnVector) columnVector;
+        if (longColumnVector.noNulls || !longColumnVector.isNull[rowId]) {
+          return (int) longColumnVector.vector[rowId];
+        } else {
+          return null;
+        }
+      case LONG:
+      case DATE:
+        // Extract to Long
+        longColumnVector = (LongColumnVector) columnVector;
+        if (longColumnVector.noNulls || !longColumnVector.isNull[rowId]) {
+          return longColumnVector.vector[rowId];
+        } else {
+          return null;
+        }
+      case TIMESTAMP:
+        // Extract to Long
+        TimestampColumnVector timestampColumnVector = (TimestampColumnVector) columnVector;
+        if (timestampColumnVector.noNulls || !timestampColumnVector.isNull[rowId]) {
+          return timestampColumnVector.time[rowId];
+        } else {
+          return null;
+        }
+      case FLOAT:
+        // Extract to Float
+        DoubleColumnVector doubleColumnVector = (DoubleColumnVector) columnVector;
+        if (doubleColumnVector.noNulls || !doubleColumnVector.isNull[rowId]) {
+          return (float) doubleColumnVector.vector[rowId];
+        } else {
+          return null;
+        }
+      case DOUBLE:
+        // Extract to Double
+        doubleColumnVector = (DoubleColumnVector) columnVector;
+        if (doubleColumnVector.noNulls || !doubleColumnVector.isNull[rowId]) {
+          return doubleColumnVector.vector[rowId];
+        } else {
+          return null;
+        }
+      case STRING:
+      case VARCHAR:
+      case CHAR:
+        // Extract to String
+        BytesColumnVector bytesColumnVector = (BytesColumnVector) columnVector;
+        if (bytesColumnVector.noNulls || !bytesColumnVector.isNull[rowId]) {
+          int length = bytesColumnVector.length[rowId];
+          if (length != 0) {
+            return StringUtils.decodeUtf8(bytesColumnVector.vector[rowId], bytesColumnVector.start[rowId], length);
+          } else {
+            // NOTE: Treat empty String as null
+            // TODO: Revisit
+            return null;
+          }
+        } else {
+          return null;
+        }
+      case BINARY:
+        // Extract to byte[]
+        bytesColumnVector = (BytesColumnVector) columnVector;
+        if (bytesColumnVector.noNulls || !bytesColumnVector.isNull[rowId]) {
+          int length = bytesColumnVector.length[rowId];
+          byte[] bytes = new byte[length];
+          System.arraycopy(bytesColumnVector.vector[rowId], bytesColumnVector.start[rowId], bytes, 0, length);
+          return bytes;
+        } else {
+          return null;
+        }
+      default:
+        // Unsupported types
+        throw new IllegalStateException("Unsupported field type: " + category);
+    }
+  }
 
   @Override
   public void rewind()
       throws IOException {
-    _recordReader = _reader.rows();
+    _orcRecordReader.seekToRow(0);
+    _hasNext = _orcRecordReader.nextBatch(_rowBatch);
+    _nextRowId = 0;
   }
 
   @Override
   public Schema getSchema() {
-    return _pinotSchema;
+    return _schema;
   }
 
   @Override
   public void close()
       throws IOException {
-    _recordReader.close();
+    _orcRecordReader.close();
   }
 }

commit 2cc2e54a884c43709ee117bf8157a1dad6ea483e

Fix ORC Record reader to ignore extra fields
Fixing an issue introduced in PR #5267
We should not be validating the type of fields that we don't care about.
Cleaned up the messages and exceptions thrown so that we know which
field is the problematic one.

pinot-plugins/pinot-input-format/pinot-orc/src/main/java/org/apache/pinot/plugin/inputformat/orc/ORCRecordReader.java
@@ -97,8 +97,8 @@ public class ORCRecordReader implements RecordReader {
         if (category == TypeDescription.Category.LIST) {
           // Multi-value field
           TypeDescription.Category childCategory = fieldType.getChildren().get(0).getCategory();
-          Preconditions.checkState(isSupportedSingleValueType(childCategory), "Illegal multi-value field type: %s",
-              childCategory);
+          Preconditions.checkState(isSupportedSingleValueType(childCategory), "Illegal multi-value field type: %s (field %s)",
+              childCategory, field);
           // NOTE: LIST is stored as 2 vectors
           int fieldId = fieldType.getId();
           orcReaderInclude[fieldId] = true;
@@ -108,10 +108,10 @@ public class ORCRecordReader implements RecordReader {
           List<TypeDescription> children = fieldType.getChildren();
           TypeDescription.Category keyCategory = children.get(0).getCategory();
           Preconditions
-              .checkState(isSupportedSingleValueType(keyCategory), "Illegal map key field type: %s", keyCategory);
+              .checkState(isSupportedSingleValueType(keyCategory), "Illegal map key field type: %s (field %s)", keyCategory, field);
           TypeDescription.Category valueCategory = children.get(1).getCategory();
           Preconditions
-              .checkState(isSupportedSingleValueType(valueCategory), "Illegal map value field type: %s", valueCategory);
+              .checkState(isSupportedSingleValueType(valueCategory), "Illegal map value field type: %s (field %s)", valueCategory, field);
           // NOTE: MAP is stored as 3 vectors
           int fieldId = fieldType.getId();
           orcReaderInclude[fieldId] = true;
@@ -120,11 +120,11 @@ public class ORCRecordReader implements RecordReader {
         } else {
           // Single-value field
           Preconditions
-              .checkState(isSupportedSingleValueType(category), "Illegal single-value field type: %s", category);
+              .checkState(isSupportedSingleValueType(category), "Illegal single-value field type: %s (field %s)", category, field);
           orcReaderInclude[fieldType.getId()] = true;
         }
+        _includeOrcFields[i] = true;
       }
-      _includeOrcFields[i] = true;
     }
 
     _orcRecordReader = orcReader.rows(new Reader.Options().include(orcReaderInclude));
@@ -186,7 +186,7 @@ public class ORCRecordReader implements RecordReader {
           int length = (int) listColumnVector.lengths[rowId];
           List<Object> values = new ArrayList<>(length);
           for (int j = 0; j < length; j++) {
-            Object value = extractSingleValue(listColumnVector.child, offset + j, childCategory);
+            Object value = extractSingleValue(field, listColumnVector.child, offset + j, childCategory);
             // NOTE: Only keep non-null values
             // TODO: Revisit
             if (value != null) {
@@ -216,8 +216,8 @@ public class ORCRecordReader implements RecordReader {
           Map<Object, Object> map = new HashMap<>();
           for (int j = 0; j < length; j++) {
             int childRowId = offset + j;
-            Object key = extractSingleValue(mapColumnVector.keys, childRowId, keyCategory);
-            Object value = extractSingleValue(mapColumnVector.values, childRowId, valueCategory);
+            Object key = extractSingleValue(field, mapColumnVector.keys, childRowId, keyCategory);
+            Object value = extractSingleValue(field, mapColumnVector.values, childRowId, valueCategory);
             map.put(key, value);
           }
           reuse.putValue(field, map);
@@ -226,7 +226,7 @@ public class ORCRecordReader implements RecordReader {
         }
       } else {
         // Single-value field
-        reuse.putValue(field, extractSingleValue(_rowBatch.cols[i], _nextRowId, category));
+        reuse.putValue(field, extractSingleValue(field, _rowBatch.cols[i], _nextRowId, category));
       }
     }
 
@@ -238,7 +238,7 @@ public class ORCRecordReader implements RecordReader {
   }
 
   @Nullable
-  private static Object extractSingleValue(ColumnVector columnVector, int rowId, TypeDescription.Category category) {
+  private static Object extractSingleValue(String field, ColumnVector columnVector, int rowId, TypeDescription.Category category) {
     if (columnVector.isRepeating) {
       rowId = 0;
     }
@@ -324,7 +324,7 @@ public class ORCRecordReader implements RecordReader {
         }
       default:
         // Unsupported types
-        throw new IllegalStateException("Unsupported field type: " + category);
+        throw new IllegalStateException("Unsupported field type: " + category + " for field " + field);
     }
   }
 

pinot-plugins/pinot-input-format/pinot-orc/src/test/java/org/apache/pinot/plugin/inputformat/orc/ORCRecordReaderTest.java
@@ -52,7 +52,7 @@ public class ORCRecordReaderTest extends AbstractRecordReaderTest {
   protected void writeRecordsToFile(List<Map<String, Object>> recordsToWrite)
       throws Exception {
     TypeDescription schema = TypeDescription.fromString(
-        "struct<dim_sv_int:int,dim_sv_long:bigint,dim_sv_float:float,dim_sv_double:double,dim_sv_string:string,dim_mv_int:array<int>,dim_mv_long:array<bigint>,dim_mv_float:array<float>,dim_mv_double:array<double>,dim_mv_string:array<string>,met_int:int,met_long:bigint,met_float:float,met_double:double>");
+        "struct<dim_sv_int:int,dim_sv_long:bigint,dim_sv_float:float,dim_sv_double:double,dim_sv_string:string,dim_mv_int:array<int>,dim_mv_long:array<bigint>,dim_mv_float:array<float>,dim_mv_double:array<double>,dim_mv_string:array<string>,met_int:int,met_long:bigint,met_float:float,met_double:double,extra_field:struct<f1:int,f2:int>>");
     Writer writer = OrcFile.createWriter(new Path(_dataFile.getAbsolutePath()),
         OrcFile.writerOptions(new Configuration()).setSchema(schema));
 