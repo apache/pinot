c87609c73f7c56416c336455a602c52182304177
Utkarsh Saxena <utkarsh.saxena@razorpay.com>
2020-07-06 19:30:42 +0530

    [TE] Build pinot while building thirdeye

--- thirdeye/install.sh
+++ thirdeye/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # [Optional] Uncomment below lines to build with latest Pinot changes
-# cd ..
-# mvn install -DskipTests -pl pinot-common,pinot-core,pinot-spi,pinot-java-client -am -Pbuild-shaded-jar || exit 1
-# cd thirdeye
+cd ..
+mvn install -DskipTests -pl pinot-common,pinot-core,pinot-spi,pinot-clients -am -Pbuild-shaded-jar || exit 1
+cd thirdeye
 
 echo "*******************************************************"
 echo "Building ThirdEye"
