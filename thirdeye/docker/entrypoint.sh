6d8dbf01d8aa41c59403eaad324fd539da236c18
Utkarsh Saxena <utkarsh.saxena@razorpay.com>
2020-07-08 15:08:50 +0530

    fixup! fixup! [TE] Fix docker image build config

--- thirdeye/docker/entrypoint.sh
+++ thirdeye/docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 
-CONFIG_DIR="./config"
-JAR_PATH="./bin/thirdeye-pinot-1.0.0-SNAPSHOT.jar"
-java -cp "${JAR_PATH}" org.apache.pinot.thirdeye.dashboard.ThirdEyeDashboardApplication ${CONFIG_DIR}
+CONFIG_DIR="/opt/thirdeye/config"
+JAR_PATH="/opt/thirdeye/bin/thirdeye-pinot-1.0.0-SNAPSHOT.jar"
+
+java -cp "${JAR_PATH}" ${CLASSPATH} ${CONFIG_DIR}
 
 #java -cp "${JAR_PATH}" org.apache.pinot.thirdeye.anomaly.ThirdEyeAnomalyApplication ${CONFIG_DIR}
