d411dcdd2902a4fb42c2f88e39f8c4aba4b5bd03
Jihao Zhang <jihzhang@linkedin.com>
2020-08-03 13:26:22 -0700

    address comments

--- thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/dashboard/resources/v2/anomalies/AnomalySearchResource.java
+++ thirdeye/thirdeye-pinot/src/main/java/org/apache/pinot/thirdeye/dashboard/resources/v2/anomalies/AnomalySearchResource.java
@@ -1,3 +1,23 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
 package org.apache.pinot.thirdeye.dashboard.resources.v2.anomalies;
 
 import io.swagger.annotations.Api;
@@ -33,16 +53,16 @@ public class AnomalySearchResource {
   /**
    * Search and paginate the anomalies according to the parameters.
    *
-   * @param limit the limit
-   * @param offset the offset
-   * @param startTime the start time
-   * @param endTime the end time
+   * @param limit the limit of the the number of anomalies returned
+   * @param offset the offset for the start position
+   * @param startTime the start time for the anomaly range
+   * @param endTime the end time for the anomaly range
    * @param feedbacks the feedback types, e.g. ANOMALY, NOT_ANOMALY
    * @param subscriptionGroups the subscription groups
    * @param detectionNames the detection names
-   * @param metrics the metrics
-   * @param datasets the datasets
-   * @param anomalyIds the anomaly ids
+   * @param metrics the metrics for the anomalies
+   * @param datasets the datasets for the anomalies
+   * @param anomalyIds the anomaly ids for the anomalies
    * @return the response
    */
   @GET
