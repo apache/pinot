84674d09f06a8e7c808172d71d092bdef9fb0d74
Jack Li(Analytics Engineering) <jlli@jlli-mn1.linkedin.biz>
2020-08-08 20:47:16 -0700

    Migrate publication from Travis CI to Github action

---
+++ .github/workflows/scripts/.pinot_linkedin_nightly_build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -x
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+export DEV_VERSION="-dev-${GITHUB_RUN_NUMBER}"
+
+echo "Deploying to bintray"
+BUILD_VERSION=$(grep -E "<version>(.*)-SNAPSHOT</version>" pom.xml | cut -d'>' -f2 | cut -d'<' -f1 | cut -d'-' -f1)
+
+echo "Current build version: $BUILD_VERSION${DEV_VERSION}"
+mvn versions:set -DnewVersion="$BUILD_VERSION${DEV_VERSION}" -q -B
+mvn versions:commit -q -B
+
+# Deploy to bintray
+mvn deploy -s .github/workflows/scripts/.ci.settings.xml -DskipTests -q -DretryFailedDeploymentCount=5 -DaltDeploymentRepository=bintray-linkedin-maven::default::'https://api.bintray.com/maven/linkedin/maven/pinot/;publish=1;override=1'
