4c20bb567dfc57805de1cb089ba902468879d34d
Suvodeep Pyne <suvodeep-pyne@users.noreply.github.com>
2020-09-02 14:23:40 -0700

    [TE] Added helm deployment for ThirdEye (#5961)
    ## Description
    Creating a helm application for ThirdEye to work with pinot quickstart
    right off the box. The current change installs MySQL with the initial
    set of tables. This creates thirdeye frontend and backend pods
    
    #### Caveats
     - The initdb.sql needs to be updated regularly which is an overhead. To be refactored later.
     - Using the create-schema.sql is not possible at the moment since the script has errors with MySQL 5.7.
    
    No backward incompatible changes.

---
+++ kubernetes/helm/thirdeye/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+base_dir=$(dirname "$0")
+cd ${base_dir};
+
+# Note:
+# - initdb files must end with .sql
+# - When injecting yaml config via terminal, the period ('.') must be escaped and quoted
+helm install thirdeye . --set-file mysql.initializationFiles."initdb\.sql"="./initdb.sql" $@
