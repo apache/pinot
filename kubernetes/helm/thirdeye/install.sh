2edd2885c3377f6263a483522f8407938bedbed9
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-09-02 12:38:22 -0700

    [TE] Added helm deployment for ThirdEye
    Exposing service type in template. +refactor

--- kubernetes/helm/thirdeye/install.sh
+++ kubernetes/helm/thirdeye/install.sh
@@ -6,4 +6,4 @@ cd ${base_dir};
 # Note:
 # - initdb files must end with .sql
 # - When injecting yaml config via terminal, the period ('.') must be escaped and quoted
-helm install thirdeye . --set-file mysql.initializationFiles."initdb\.sql"="./initdb.sql"
+helm install thirdeye . --set-file mysql.initializationFiles."initdb\.sql"="./initdb.sql" --namespace te
