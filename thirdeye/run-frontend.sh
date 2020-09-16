b394e3673e0489258484fe7488d1483d6ff34ad7
Suvodeep Pyne <pyne.suvodeep@gmail.com>
2020-09-16 16:51:47 -0700

    [TE] Moved Test classes from thirdeye-pinot to thirdeye-dashboard
    Altering bash scripts to load from the dashboard module instead of thirdeye-pinot

--- thirdeye/run-frontend.sh
+++ thirdeye/run-frontend.sh
@@ -3,6 +3,6 @@ echo "*******************************************************"
 echo "Launching ThirdEye Dashboard in demo mode"
 echo "*******************************************************"
 
-cd thirdeye-pinot
-java -Dlog4j.configurationFile=log4j2.xml -cp "./target/thirdeye-pinot-1.0.0-SNAPSHOT.jar" org.apache.pinot.thirdeye.dashboard.ThirdEyeDashboardApplication "./config"
+cd thirdeye-dashboard
+java -Dlog4j.configurationFile=log4j2.xml -cp "./target/thirdeye-dashboard-1.0.0-SNAPSHOT.jar" org.apache.pinot.thirdeye.dashboard.ThirdEyeDashboardApplication "./config"
 cd ..
