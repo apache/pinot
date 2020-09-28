47d15b0a0c1b4dbe70d8a3c0209644e187ac2a35
vincentchenjl <jianchen@linkedin.com>
2020-09-28 15:47:09 -0700

    remove frontend files from tracking

--- thirdeye/thirdeye-dashboard/src/main/resources/app/ember-fetch/fastboot-fetch-805e176077930c98aec5834ae48bd0f2.js
+++
@@ -1,2 +0,0 @@
-define("fetch",["exports"],function(e){var t=FastBoot.require("node-fetch")
-e.default=t,e.Headers=t.Headers,e.Request=t.Request,e.Response=t.Response}),define("fetch/ajax",["exports"],function(){throw new Error("You included `fetch/ajax` but it was renamed to `ember-fetch/ajax`")})
