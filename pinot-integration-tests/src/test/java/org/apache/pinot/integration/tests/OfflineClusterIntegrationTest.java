b658925f82602d9a877d42af10957515fe055146
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2020-10-05 18:25:07 -0700

    Allow modifying/removing existing star-trees during segment reload (#6100)
    For #6092
    Currently we allow generating new star-trees during the segment reload when there is no existing ones.
    This PR adds the support of modifying or removing the existing star-trees during segment reload when it detects changes for the star-tree index config.
    
    Other enhancement:
    
    Deduplicate the star-tree builder config to avoid generating the same star-tree multiple times when user put multiple identical star-tree index configs, or the star-tree index config is the same as the default star-tree.

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
@@ -94,10 +94,14 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
   private static final String TEST_UPDATED_BLOOM_FILTER_QUERY = "SELECT COUNT(*) FROM mytable WHERE Carrier = 'CA'";
 
   // For star-tree triggering test
-  private static final StarTreeIndexConfig STAR_TREE_INDEX_CONFIG =
+  private static final StarTreeIndexConfig STAR_TREE_INDEX_CONFIG_1 =
       new StarTreeIndexConfig(Collections.singletonList("Carrier"), null,
           Collections.singletonList(AggregationFunctionColumnPair.COUNT_STAR.toColumnName()), 100);
-  private static final String TEST_STAR_TREE_QUERY = "SELECT COUNT(*) FROM mytable WHERE Carrier = 'UA'";
+  private static final String TEST_STAR_TREE_QUERY_1 = "SELECT COUNT(*) FROM mytable WHERE Carrier = 'UA'";
+  private static final StarTreeIndexConfig STAR_TREE_INDEX_CONFIG_2 =
+      new StarTreeIndexConfig(Collections.singletonList("DestState"), null,
+          Collections.singletonList(AggregationFunctionColumnPair.COUNT_STAR.toColumnName()), 100);
+  private static final String TEST_STAR_TREE_QUERY_2 = "SELECT COUNT(*) FROM mytable WHERE DestState = 'CA'";
 
   // For default columns test
   private static final String SCHEMA_FILE_NAME_WITH_EXTRA_COLUMNS =
@@ -437,39 +441,116 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
       throws Exception {
     long numTotalDocs = getCountStarResult();
 
-    JsonNode queryResponse = postQuery(TEST_STAR_TREE_QUERY);
-    int result = queryResponse.get("aggregationResults").get(0).get("value").asInt();
+    // Test the first query
+    JsonNode firstQueryResponse = postQuery(TEST_STAR_TREE_QUERY_1);
+    int firstQueryResult = firstQueryResponse.get("aggregationResults").get(0).get("value").asInt();
+    assertEquals(firstQueryResponse.get("totalDocs").asLong(), numTotalDocs);
     // Initially 'numDocsScanned' should be the same as 'COUNT(*)' result
-    assertEquals(queryResponse.get("numDocsScanned").asInt(), result);
+    assertEquals(firstQueryResponse.get("numDocsScanned").asInt(), firstQueryResult);
 
     // Update table config and trigger reload
     TableConfig tableConfig = getOfflineTableConfig();
     IndexingConfig indexingConfig = tableConfig.getIndexingConfig();
-    indexingConfig.setStarTreeIndexConfigs(Collections.singletonList(STAR_TREE_INDEX_CONFIG));
+    indexingConfig.setStarTreeIndexConfigs(Collections.singletonList(STAR_TREE_INDEX_CONFIG_1));
     indexingConfig.setEnableDynamicStarTreeCreation(true);
     updateTableConfig(tableConfig);
     reloadOfflineTable(getTableName());
 
     TestUtils.waitForCondition(aVoid -> {
       try {
-        JsonNode queryResponse1 = postQuery(TEST_STAR_TREE_QUERY);
+        JsonNode queryResponse = postQuery(TEST_STAR_TREE_QUERY_1);
         // Result should not change during reload
-        assertEquals(queryResponse1.get("aggregationResults").get(0).get("value").asInt(), result);
+        assertEquals(queryResponse.get("aggregationResults").get(0).get("value").asInt(), firstQueryResult);
         // Total docs should not change during reload
-        assertEquals(queryResponse1.get("totalDocs").asLong(), numTotalDocs);
+        assertEquals(queryResponse.get("totalDocs").asLong(), numTotalDocs);
         // With star-tree, 'numDocsScanned' should be the same as number of segments (1 per segment)
-        return queryResponse1.get("numDocsScanned").asInt() == NUM_SEGMENTS;
+        return queryResponse.get("numDocsScanned").asInt() == NUM_SEGMENTS;
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
-    }, 600_000L, "Failed to generate bloom filter");
+    }, 600_000L, "Failed to star-tree index");
 
     // Reload again should have no effect
     reloadOfflineTable(getTableName());
-    JsonNode queryResponse1 = postQuery(TEST_STAR_TREE_QUERY);
-    assertEquals(queryResponse1.get("aggregationResults").get(0).get("value").asInt(), result);
-    assertEquals(queryResponse1.get("totalDocs").asLong(), numTotalDocs);
-    assertEquals(queryResponse1.get("numDocsScanned").asInt(), NUM_SEGMENTS);
+    firstQueryResponse = postQuery(TEST_STAR_TREE_QUERY_1);
+    assertEquals(firstQueryResponse.get("aggregationResults").get(0).get("value").asInt(), firstQueryResult);
+    assertEquals(firstQueryResponse.get("totalDocs").asLong(), numTotalDocs);
+    assertEquals(firstQueryResponse.get("numDocsScanned").asInt(), NUM_SEGMENTS);
+
+    // Test the second query
+    JsonNode secondQueryResponse = postQuery(TEST_STAR_TREE_QUERY_2);
+    int secondQueryResult = secondQueryResponse.get("aggregationResults").get(0).get("value").asInt();
+    assertEquals(secondQueryResponse.get("totalDocs").asLong(), numTotalDocs);
+    // Initially 'numDocsScanned' should be the same as 'COUNT(*)' result
+    assertEquals(secondQueryResponse.get("numDocsScanned").asInt(), secondQueryResult);
+
+    // Update table config with a different star-tree index config and trigger reload
+    indexingConfig.setStarTreeIndexConfigs(Collections.singletonList(STAR_TREE_INDEX_CONFIG_2));
+    updateTableConfig(tableConfig);
+    reloadOfflineTable(getTableName());
+
+    TestUtils.waitForCondition(aVoid -> {
+      try {
+        JsonNode queryResponse = postQuery(TEST_STAR_TREE_QUERY_2);
+        // Result should not change during reload
+        assertEquals(queryResponse.get("aggregationResults").get(0).get("value").asInt(), secondQueryResult);
+        // Total docs should not change during reload
+        assertEquals(queryResponse.get("totalDocs").asLong(), numTotalDocs);
+        // With star-tree, 'numDocsScanned' should be the same as number of segments (1 per segment)
+        return queryResponse.get("numDocsScanned").asInt() == NUM_SEGMENTS;
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }, 600_000L, "Failed to star-tree index");
+
+    // First query should not be able to use the star-tree
+    firstQueryResponse = postQuery(TEST_STAR_TREE_QUERY_1);
+    assertEquals(firstQueryResponse.get("numDocsScanned").asInt(), firstQueryResult);
+
+    // Reload again should have no effect
+    reloadOfflineTable(getTableName());
+    firstQueryResponse = postQuery(TEST_STAR_TREE_QUERY_1);
+    assertEquals(firstQueryResponse.get("aggregationResults").get(0).get("value").asInt(), firstQueryResult);
+    assertEquals(firstQueryResponse.get("totalDocs").asLong(), numTotalDocs);
+    assertEquals(firstQueryResponse.get("numDocsScanned").asInt(), firstQueryResult);
+    secondQueryResponse = postQuery(TEST_STAR_TREE_QUERY_2);
+    assertEquals(secondQueryResponse.get("aggregationResults").get(0).get("value").asInt(), secondQueryResult);
+    assertEquals(secondQueryResponse.get("totalDocs").asLong(), numTotalDocs);
+    assertEquals(secondQueryResponse.get("numDocsScanned").asInt(), NUM_SEGMENTS);
+
+    // Remove the star-tree index config and trigger reload
+    indexingConfig.setStarTreeIndexConfigs(null);
+    updateTableConfig(tableConfig);
+    reloadOfflineTable(getTableName());
+
+    TestUtils.waitForCondition(aVoid -> {
+      try {
+        JsonNode queryResponse = postQuery(TEST_STAR_TREE_QUERY_2);
+        // Result should not change during reload
+        assertEquals(queryResponse.get("aggregationResults").get(0).get("value").asInt(), secondQueryResult);
+        // Total docs should not change during reload
+        assertEquals(queryResponse.get("totalDocs").asLong(), numTotalDocs);
+        // Without star-tree, 'numDocsScanned' should be the same as the 'COUNT(*)' result
+        return queryResponse.get("numDocsScanned").asInt() == secondQueryResult;
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }, 600_000L, "Failed to star-tree index");
+
+    // First query should not be able to use the star-tree
+    firstQueryResponse = postQuery(TEST_STAR_TREE_QUERY_1);
+    assertEquals(firstQueryResponse.get("numDocsScanned").asInt(), firstQueryResult);
+
+    // Reload again should have no effect
+    reloadOfflineTable(getTableName());
+    firstQueryResponse = postQuery(TEST_STAR_TREE_QUERY_1);
+    assertEquals(firstQueryResponse.get("aggregationResults").get(0).get("value").asInt(), firstQueryResult);
+    assertEquals(firstQueryResponse.get("totalDocs").asLong(), numTotalDocs);
+    assertEquals(firstQueryResponse.get("numDocsScanned").asInt(), firstQueryResult);
+    secondQueryResponse = postQuery(TEST_STAR_TREE_QUERY_2);
+    assertEquals(secondQueryResponse.get("aggregationResults").get(0).get("value").asInt(), secondQueryResult);
+    assertEquals(secondQueryResponse.get("totalDocs").asLong(), numTotalDocs);
+    assertEquals(secondQueryResponse.get("numDocsScanned").asInt(), secondQueryResult);
   }
 
   /**
@@ -512,11 +593,10 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
 
     if (withExtraColumns) {
       _schemaFileName = SCHEMA_FILE_NAME_WITH_EXTRA_COLUMNS;
-      addSchema(createSchema());
     } else {
       _schemaFileName = SCHEMA_FILE_NAME_WITH_MISSING_COLUMNS;
-      addSchema(createSchema());
     }
+    addSchema(createSchema());
 
     // Trigger reload
     reloadOfflineTable(getTableName());
