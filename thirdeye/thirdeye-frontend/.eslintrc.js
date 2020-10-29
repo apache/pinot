cc5ac551d425495e95d1f39429f9db7be3399721
Alex Bachuk <abachuk@linkedin.com>
2020-10-28 22:56:12 -0400

    cleanup eslintrc

--- thirdeye/thirdeye-frontend/.eslintrc.js
+++ thirdeye/thirdeye-frontend/.eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
   globals: {
-    server: true,
+    server: true
   },
   root: true,
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['ember'],
   extends: ['eslint:recommended', 'plugin:ember/recommended'],
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   rules: {
     'eol-last': ['warn', 'always'],
@@ -38,16 +38,16 @@ module.exports = {
         'testem.js',
         'ember-cli-build.js',
         'config/**/*.js',
-        'lib/*/index.js',
+        'lib/*/index.js'
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015,
+        ecmaVersion: 2015
       },
       env: {
         browser: false,
-        node: true,
-      },
+        node: true
+      }
     },
 
     // test files
@@ -55,8 +55,8 @@ module.exports = {
       files: ['tests/**/*.js'],
       excludedFiles: ['tests/dummy/**/*.js'],
       env: {
-        embertest: true,
-      },
+        embertest: true
+      }
     },
   ],
 };
