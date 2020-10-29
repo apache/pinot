37289edd87c5588294917db5e58df0e43bbe8338
Alex Bachuk <abachuk@linkedin.com>
2020-10-28 22:07:47 -0400

    minor changes, cleanup

--- thirdeye/thirdeye-frontend/.eslintrc.js
+++ thirdeye/thirdeye-frontend/.eslintrc.js
@@ -5,59 +5,57 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ["ember"],
+  extends: ["eslint:recommended", "plugin:ember/recommended"],
   env: {
     browser: true,
-    es6: true
+    es6: true,
   },
   rules: {
     "eol-last": ["warn", "always"],
-    "indent": ["warn", 2, { "SwitchCase": 1 }],
+    indent: ["warn", 2, { SwitchCase: 1 }],
     "space-in-parens": ["warn", "never"],
     "no-trailing-spaces": ["warn"],
     "comma-dangle": ["warn", "never"],
-    "comma-spacing": ["warn", { "before": false, "after": true }],
-    "semi": ["warn", "always"],
+    "comma-spacing": ["warn", { before: false, after: true }],
+    semi: ["warn", "always"],
     "comma-style": ["warn", "last"],
-    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "no-unused-vars": [
+      "warn",
+      { vars: "all", args: "after-used", ignoreRestSiblings: false },
+    ],
     // TODO: fix all warnings and change rules back to "error"
     "ember/avoid-leaking-state-in-ember-objects": ["warn"],
-    "ember/no-attrs-in-components": ["warn"]
+    "ember/no-attrs-in-components": ["warn"],
   },
   overrides: [
     // node files
     {
       files: [
-        'testem.js',
-        'ember-cli-build.js',
-        'config/**/*.js',
-        'lib/*/index.js'
+        "testem.js",
+        "ember-cli-build.js",
+        "config/**/*.js",
+        "lib/*/index.js",
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: "script",
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true
-      }
+        node: true,
+      },
     },
 
     // test files
     {
-      files: ['tests/**/*.js'],
-      excludedFiles: ['tests/dummy/**/*.js'],
+      files: ["tests/**/*.js"],
+      excludedFiles: ["tests/dummy/**/*.js"],
       env: {
-        embertest: true
-      }
-    }
-  ]
+        embertest: true,
+      },
+    },
+  ],
 };
--- thirdeye/thirdeye-frontend/app/pods/components/timeseries-chart/component.js
+++ thirdeye/thirdeye-frontend/app/pods/components/timeseries-chart/component.js
@@ -1,13 +1,13 @@
-import { debounce, later } from '@ember/runloop';
-import Component from '@ember/component';
-import c3 from 'c3';
-import d3 from 'd3';
-import _ from 'lodash';
-import moment from 'moment';
+import { debounce, later } from "@ember/runloop";
+import Component from "@ember/component";
+import c3 from "c3";
+import d3 from "d3";
+import _ from "lodash";
+import moment from "moment";
 
 export default Component.extend({
-  tagName: 'div',
-  classNames: ['timeseries-chart'],
+  tagName: "div",
+  classNames: ["timeseries-chart"],
 
   // internal
   _chart: null,
@@ -18,9 +18,9 @@ export default Component.extend({
     example_series: {
       timestamps: [0, 1, 2, 5, 6],
       values: [10, 10, 5, 27, 28],
-      type: 'line', // 'scatter', 'region'
-      color: 'green'
-    }
+      type: "line", // 'scatter', 'region'
+      color: "green",
+    },
   },
 
   /**
@@ -34,97 +34,110 @@ export default Component.extend({
    * May be overridden
    */
   height: {
-    height: 400
+    height: 400,
   },
 
   tooltip: {
     format: {
-      title: (d) => moment(d).format('MM/DD hh:mm a'),
-      value: (val) => d3.format('.3s')(val),
+      title: (d) => moment(d).format("MM/DD hh:mm a"),
+      value: (val) => d3.format(".3s")(val),
       name: (name) => {
-        if (name === 'Upper and lower bound') {
-          return 'Upper bound';
-        } else if (name === 'lowerBound') {
-          return 'Lower bound';
+        if (name === "Upper and lower bound") {
+          return "Upper bound";
+        } else if (name === "lowerBound") {
+          return "Lower bound";
         }
         return name;
-      }
-    }
+      },
+    },
   },
 
   legend: {
-    hide: ['lowerBound', 'old-anomaly-edges', 'new-anomaly-edges']
+    hide: ["lowerBound", "old-anomaly-edges", "new-anomaly-edges"],
   },
 
   axis: {
     y: {
-      show: true
+      show: true,
     },
     y2: {
-      show: false
+      show: false,
     },
     x: {
-      type: 'timeseries',
+      type: "timeseries",
       show: true,
       tick: {
         count: 5,
-        format: '%Y-%m-%d'
-      }
-    }
+        format: "%Y-%m-%d",
+      },
+    },
   },
 
   /**
    * Converts color strings into their rgba equivalent
    */
   colorMapping: {
-    blue: '#0091CA',
-    green: '#469A1F',
-    red: '#FF2C33',
-    purple: '#827BE9',
-    orange: '#E55800',
-    teal: '#0E95A0',
-    pink: '#FF1B90'
+    blue: "#0091CA",
+    green: "#469A1F",
+    red: "#FF2C33",
+    purple: "#827BE9",
+    orange: "#E55800",
+    teal: "#0E95A0",
+    pink: "#FF1B90",
   },
 
-  subchart: { // on init only
-    show: true
+  subchart: {
+    // on init only
+    show: true,
   },
 
-  zoom: { // on init only
+  zoom: {
+    // on init only
     enabled: true,
-    onzoom: null
+    onzoom: null,
   },
 
-  point: { // on init only
-    show: true
+  point: {
+    // on init only
+    show: true,
   },
 
-  line: { // on init only
-    connectNull: false
+  line: {
+    // on init only
+    connectNull: false,
   },
 
-  id: 'timeseries-chart',
+  id: "timeseries-chart",
 
   _makeDiffConfig() {
-    const cache = this.get('_seriesCache') || {};
-    const series = this.get('series') || {};
-    const colorMapping = this.get('colorMapping');
-    const { axis, legend, tooltip, focusedIds } = this.getProperties('axis', 'legend', 'tooltip', 'focusedIds');
+    const cache = this.get("_seriesCache") || {};
+    const series = this.get("series") || {};
+    const colorMapping = this.get("colorMapping");
+    const { axis, legend, tooltip, focusedIds } = this.getProperties(
+      "axis",
+      "legend",
+      "tooltip",
+      "focusedIds"
+    );
 
     const seriesKeys = Object.keys(series).sort();
 
-    const addedKeys = seriesKeys.filter(sid => !cache[sid]);
-    const changedKeys = seriesKeys.filter(sid => cache[sid] && !_.isEqual(cache[sid], series[sid]));
-    const deletedKeys = Object.keys(cache).filter(sid => !series[sid]);
-    const regionKeys = seriesKeys.filter(sid => series[sid] && series[sid].type == 'region');
+    const addedKeys = seriesKeys.filter((sid) => !cache[sid]);
+    const changedKeys = seriesKeys.filter(
+      (sid) => cache[sid] && !_.isEqual(cache[sid], series[sid])
+    );
+    const deletedKeys = Object.keys(cache).filter((sid) => !series[sid]);
+    const regionKeys = seriesKeys.filter(
+      (sid) => series[sid] && series[sid].type == "region"
+    );
     // keys containing '-region' should not appear in the graph legend.
-    const noLegendKeys = seriesKeys.filter(sid => (sid.includes('-region')));
+    const noLegendKeys = seriesKeys.filter((sid) => sid.includes("-region"));
 
-    const regions = regionKeys.map(sid => {
+    const regions = regionKeys.map((sid) => {
       const t = series[sid].timestamps;
       let region = { start: t[0], end: t[t.length - 1] };
 
-      if ('color' in series[sid]) {
+      if ("color" in series[sid]) {
         region.class = `c3-region--${series[sid].color}`;
       }
 
@@ -132,52 +145,82 @@ export default Component.extend({
     });
 
     const unloadKeys = deletedKeys.concat(noLegendKeys);
-    const unload = unloadKeys.concat(unloadKeys.map(sid => `${sid}-timestamps`));
+    const unload = unloadKeys.concat(
+      unloadKeys.map((sid) => `${sid}-timestamps`)
+    );
 
-    const loadKeys = addedKeys.concat(changedKeys).filter(sid => !noLegendKeys.includes(sid));
+    const loadKeys = addedKeys
+      .concat(changedKeys)
+      .filter((sid) => !noLegendKeys.includes(sid));
     const xs = {};
-    loadKeys.forEach(sid => xs[sid] = `${sid}-timestamps`);
+    loadKeys.forEach((sid) => (xs[sid] = `${sid}-timestamps`));
 
-    const values = loadKeys.map(sid => [sid].concat(series[sid].values));
+    const values = loadKeys.map((sid) => [sid].concat(series[sid].values));
 
-    const timestamps = loadKeys.map(sid => [`${sid}-timestamps`].concat(series[sid].timestamps));
+    const timestamps = loadKeys.map((sid) =>
+      [`${sid}-timestamps`].concat(series[sid].timestamps)
+    );
 
     const columns = values.concat(timestamps);
 
     const colors = {};
-    loadKeys.filter(sid => series[sid].color).forEach(sid => colors[sid] = colorMapping[series[sid].color]);
+    loadKeys
+      .filter((sid) => series[sid].color)
+      .forEach((sid) => (colors[sid] = colorMapping[series[sid].color]));
 
     const types = {};
-    loadKeys.filter(sid => series[sid].type).forEach(sid => types[sid] = series[sid].type);
+    loadKeys
+      .filter((sid) => series[sid].type)
+      .forEach((sid) => (types[sid] = series[sid].type));
 
     const axes = {};
-    loadKeys.filter(sid => 'axis' in series[sid]).forEach(sid => axes[sid] = series[sid].axis);
+    loadKeys
+      .filter((sid) => "axis" in series[sid])
+      .forEach((sid) => (axes[sid] = series[sid].axis));
     // keep the lower bound line in graph but remove in from the legend
-    legend.hide = ['lowerBound', 'old-anomaly-edges', 'new-anomaly-edges'];
-    const config = { unload, xs, columns, types, regions, tooltip, focusedIds, colors, axis, axes, legend };
+    legend.hide = ["lowerBound", "old-anomaly-edges", "new-anomaly-edges"];
+    const config = {
+      unload,
+      xs,
+      columns,
+      types,
+      regions,
+      tooltip,
+      focusedIds,
+      colors,
+      axis,
+      axes,
+      legend,
+    };
     return config;
   },
 
   _makeAxisRange(axis) {
     const range = { min: {}, max: {} };
-    Object.keys(axis).filter(key => 'min' in axis[key]).forEach(key => range['min'][key] = axis[key]['min']);
-    Object.keys(axis).filter(key => 'max' in axis[key]).forEach(key => range['max'][key] = axis[key]['max']);
+    Object.keys(axis)
+      .filter((key) => "min" in axis[key])
+      .forEach((key) => (range["min"][key] = axis[key]["min"]));
+    Object.keys(axis)
+      .filter((key) => "max" in axis[key])
+      .forEach((key) => (range["max"][key] = axis[key]["max"]));
     return range;
   },
 
   _updateCache() {
     // debounce: do not trigger if chart object already destroyed
-    if (this.isDestroyed) { return; }
+    if (this.isDestroyed) {
+      return;
+    }
 
-    const series = this.get('series') || {};
-    this.set('_seriesCache', _.cloneDeep(series));
+    const series = this.get("series") || {};
+    this.set("_seriesCache", _.cloneDeep(series));
   },
 
   /**
    * Updates the focused entity on the chart
    */
-  _updateFocusedIds: function() {
-    const ids = this.get('focusedIds');
+  _updateFocusedIds: function () {
+    const ids = this.get("focusedIds");
 
     if (!_.isEmpty(ids)) {
       this._focus([...ids]);
@@ -192,7 +235,7 @@ export default Component.extend({
    * @param {String} id
    */
   _focus(id) {
-    this.get('_chart').focus(id);
+    this.get("_chart").focus(id);
   },
 
   /**
@@ -200,12 +243,12 @@ export default Component.extend({
    * @private
    */
   _revert() {
-    this.get('_chart').revert();
+    this.get("_chart").revert();
   },
 
   _updateChart() {
     const diffConfig = this._makeDiffConfig();
-    const chart = this.get('_chart');
+    const chart = this.get("_chart");
     chart.regions(diffConfig.regions);
     chart.axis.range(this._makeAxisRange(diffConfig.axis));
     chart.unzoom();
@@ -213,102 +256,128 @@ export default Component.extend({
     this._updateCache();
   },
 
-  _shadeBounds(){
+  _shadeBounds() {
     const parentElement = this.api.internal.config.bindto;
     d3.select(parentElement).select(".confidence-bounds").remove();
     d3.select(parentElement).select(".sub-confidence-bounds").remove();
-    d3.select(parentElement).select('.timeseries-graph__slider-circle').remove();
-    d3.select(parentElement).selectAll('timeseries-graph__slider-line').remove();
+    d3.select(parentElement)
+      .select(".timeseries-graph__slider-circle")
+      .remove();
+    d3.select(parentElement)
+      .selectAll("timeseries-graph__slider-line")
+      .remove();
     const chart = this.api;
-    if (chart && chart.legend && chart.internal && chart.internal.data && chart.internal.data.targets) {
+    if (
+      chart &&
+      chart.legend &&
+      chart.internal &&
+      chart.internal.data &&
+      chart.internal.data.targets
+    ) {
       if (chart.internal.data.targets.length > 24) {
         chart.legend.hide();
       }
     }
     // key is 'Upper and lower bound' because we delete the lowerBound key for the legend.
-    if(chart && chart.internal && chart.internal.data && chart.internal.data.xs && Array.isArray(chart.internal.data.xs['Upper and lower bound'])) {
-      const indices = d3.range(chart.internal.data.xs['Upper and lower bound'].length);
+    if (
+      chart &&
+      chart.internal &&
+      chart.internal.data &&
+      chart.internal.data.xs &&
+      Array.isArray(chart.internal.data.xs["Upper and lower bound"])
+    ) {
+      const indices = d3.range(
+        chart.internal.data.xs["Upper and lower bound"].length
+      );
       const yscale = chart.internal.y;
       const xscale = chart.internal.x;
       const yscaleSub = chart.internal.subY;
       const xscaleSub = chart.internal.subX;
-      const xVals = chart.internal.data.xs['Upper and lower bound'];
-      let upperBoundVals = chart.internal.data.targets.find(target => {
-        return target.id === 'Upper and lower bound';
+      const xVals = chart.internal.data.xs["Upper and lower bound"];
+      let upperBoundVals = chart.internal.data.targets.find((target) => {
+        return target.id === "Upper and lower bound";
       });
-      let lowerBoundVals = chart.internal.data.targets.find(target => {
-        return target.id === 'lowerBound';
+      let lowerBoundVals = chart.internal.data.targets.find((target) => {
+        return target.id === "lowerBound";
       });
 
       if (upperBoundVals && lowerBoundVals) {
-        upperBoundVals = upperBoundVals.values.map(e => e.value);
-        lowerBoundVals = lowerBoundVals.values.map(e => e.value);
+        upperBoundVals = upperBoundVals.values.map((e) => e.value);
+        lowerBoundVals = lowerBoundVals.values.map((e) => e.value);
         // If all upper bound vals are null, we assume that there is only a lower bound
-        if (upperBoundVals.every(val => val === null)) {
-          let currentVals = chart.internal.data.targets.find(target => {
-            return target.id === 'Current';
+        if (upperBoundVals.every((val) => val === null)) {
+          let currentVals = chart.internal.data.targets.find((target) => {
+            return target.id === "Current";
           });
           if (currentVals) {
-            currentVals = currentVals.values.map(e => e.value);
+            currentVals = currentVals.values.map((e) => e.value);
             const currentMax = Math.max(...currentVals);
             upperBoundVals = upperBoundVals.map(() => 2 * currentMax);
           }
         }
-        const area_main = d3.area()
+        const area_main = d3
+          .area()
           .curve(d3.curveLinear)
-          .x(d => xscale(xVals[d]))
-          .y0(d => yscale(lowerBoundVals[d]))
-          .y1(d => yscale(upperBoundVals[d]));
+          .x((d) => xscale(xVals[d]))
+          .y0((d) => yscale(lowerBoundVals[d]))
+          .y1((d) => yscale(upperBoundVals[d]));
 
-        const area_sub = d3.area()
+        const area_sub = d3
+          .area()
           .curve(d3.curveLinear)
-          .x(d => xscaleSub(xVals[d]))
-          .y0(d => yscaleSub(lowerBoundVals[d]))
-          .y1(d => yscaleSub(upperBoundVals[d]));
+          .x((d) => xscaleSub(xVals[d]))
+          .y0((d) => yscaleSub(lowerBoundVals[d]))
+          .y1((d) => yscaleSub(upperBoundVals[d]));
 
         let i = 0;
-        const bothCharts = d3.select(parentElement).selectAll('.c3-chart-bars');
-        bothCharts.each(function() {
+        const bothCharts = d3.select(parentElement).selectAll(".c3-chart-bars");
+        bothCharts.each(function () {
           if (i === 0 && this) {
-            d3.select(this).insert('path')
+            d3.select(this)
+              .insert("path")
               .datum(indices)
-              .attr('class', 'confidence-bounds')
-              .attr('d', area_main);
+              .attr("class", "confidence-bounds")
+              .attr("d", area_main);
           } else if (i === 1 && this) {
-            d3.select(this).insert('path')
+            d3.select(this)
+              .insert("path")
               .datum(indices)
-              .attr('class', 'sub-confidence-bounds')
-              .attr('d', area_sub);
+              .attr("class", "sub-confidence-bounds")
+              .attr("d", area_sub);
           }
           i++;
         });
       }
     }
     // add resize buttons after shading bounds
-    const resizeButtons = d3.selectAll('.resize');
+    const resizeButtons = d3.selectAll(".resize");
 
-    resizeButtons.append('circle')
-      .attr('class', 'timeseries-graph__slider-circle')
-      .attr('cx', 0)
-      .attr('cy', 30)
-      .attr('r', 10)
-      .attr('fill', '#0091CA');
-    resizeButtons.append('line')
-      .attr('class', 'timeseries-graph__slider-line')
+    resizeButtons
+      .append("circle")
+      .attr("class", "timeseries-graph__slider-circle")
+      .attr("cx", 0)
+      .attr("cy", 30)
+      .attr("r", 10)
+      .attr("fill", "#0091CA");
+    resizeButtons
+      .append("line")
+      .attr("class", "timeseries-graph__slider-line")
       .attr("x1", 0)
       .attr("y1", 27)
       .attr("x2", 0)
       .attr("y2", 33);
 
-    resizeButtons.append('line')
-      .attr('class', 'timeseries-graph__slider-line')
+    resizeButtons
+      .append("line")
+      .attr("class", "timeseries-graph__slider-line")
       .attr("x1", -5)
       .attr("y1", 27)
       .attr("x2", -5)
       .attr("y2", 33);
 
-    resizeButtons.append('line')
-      .attr('class', 'timeseries-graph__slider-line')
+    resizeButtons
+      .append("line")
+      .attr("class", "timeseries-graph__slider-line")
       .attr("x1", 5)
       .attr("y1", 27)
       .attr("x2", 5)
@@ -317,8 +386,8 @@ export default Component.extend({
 
   didUpdateAttrs() {
     this._super(...arguments);
-    const series = this.get('series') || {};
-    const cache = this.get('cache') || {};
+    const series = this.get("series") || {};
+    const cache = this.get("cache") || {};
 
     this._updateFocusedIds();
 
@@ -327,7 +396,7 @@ export default Component.extend({
     }
   },
 
-  didRender(){
+  didRender() {
     this._super(...arguments);
 
     later(() => {
@@ -341,8 +410,8 @@ export default Component.extend({
    * for a screenshot
    */
   notifyPhantomJS() {
-    if (typeof window.callPhantom === 'function') {
-      window.callPhantom({message: 'ready'});
+    if (typeof window.callPhantom === "function") {
+      window.callPhantom({ message: "ready" });
     }
   },
 
@@ -351,30 +420,30 @@ export default Component.extend({
 
     const diffConfig = this._makeDiffConfig();
     const config = {};
-    config.bindto = this.get('element');
+    config.bindto = this.get("element");
     config.data = {
       xs: diffConfig.xs,
       columns: diffConfig.columns,
       types: diffConfig.types,
       colors: diffConfig.colors,
-      axes: diffConfig.axes
+      axes: diffConfig.axes,
     };
     config.axis = diffConfig.axis;
     config.regions = diffConfig.regions;
     config.tooltip = diffConfig.tooltip;
     config.focusedIds = diffConfig.focusedIds;
     config.legend = diffConfig.legend;
-    config.subchart = this.get('subchart');
-    config.zoom = this.get('zoom');
-    config.size = this.get('height');
-    config.point = this.get('point');
-    config.line = this.get('line');
-    config.id = this.get('id');
-    config.onrendered = this.get('_shadeBounds');
+    config.subchart = this.get("subchart");
+    config.zoom = this.get("zoom");
+    config.size = this.get("height");
+    config.point = this.get("point");
+    config.line = this.get("line");
+    config.id = this.get("id");
+    config.onrendered = this.get("_shadeBounds");
 
     const chart = c3.generate(config);
-    this.set('_chart', chart);
+    this.set("_chart", chart);
 
     this._updateCache();
-  }
+  },
 });
