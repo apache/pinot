0c5cb6fadb1e501be370d36315b56d2671b572dc
Yupeng Fu <yupeng.fu@gmail.com>
2020-11-16 20:04:04 -0800

    comments

--- pinot-common/src/main/java/org/apache/pinot/common/utils/FileUploadDownloadClient.java
+++ pinot-common/src/main/java/org/apache/pinot/common/utils/FileUploadDownloadClient.java
@@ -243,8 +243,8 @@ public class FileUploadDownloadClient implements Closeable {
     return requestBuilder.build();
   }
 
-  private static HttpUriRequest getAddSchemaRequest(URI uri, String schemaName, File schemaFile, List<Header> headers,
-      List<NameValuePair> parameters) {
+  private static HttpUriRequest getAddSchemaRequest(URI uri, String schemaName, File schemaFile,
+      @Nullable List<Header> headers, @Nullable List<NameValuePair> parameters) {
     return getUploadFileRequest(HttpPost.METHOD_NAME, uri, getContentBody(schemaName, schemaFile), headers, parameters,
         DEFAULT_SOCKET_TIMEOUT_MS);
   }
@@ -435,8 +435,8 @@ public class FileUploadDownloadClient implements Closeable {
    * @throws IOException
    * @throws HttpErrorStatusException
    */
-  public SimpleHttpResponse addSchema(URI uri, String schemaName, File schemaFile, List<Header> headers,
-      List<NameValuePair> parameters)
+  public SimpleHttpResponse addSchema(URI uri, String schemaName, File schemaFile, @Nullable List<Header> headers,
+      @Nullable List<NameValuePair> parameters)
       throws IOException, HttpErrorStatusException {
     return sendRequest(getAddSchemaRequest(uri, schemaName, schemaFile, headers, parameters));
   }
