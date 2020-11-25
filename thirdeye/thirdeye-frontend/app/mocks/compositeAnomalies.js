7d6c9468383e774ff65bd8b1bab0684ef670b365
Tejas Ajmera <tajmera@tajmera-mn1.linkedin.biz>
2020-11-25 02:38:35 -0800

    [TE]frontend - Build the tree parser for composite anomalies

---
+++ thirdeye/thirdeye-frontend/app/mocks/compositeAnomalies.js
@@ -0,0 +1,256 @@
+export const mockData = [
+  {
+    id: 1,
+    startTime: 1599462000000,
+    endTime: 1599721200000,
+    feedback: null,
+    properties: {
+      detectorComponentName: "grouper_one:GROUPER",
+      subEntityName: "entity_one"
+    },
+    children: [
+      {
+        id: 2,
+        startTime: 1599462000000,
+        endTime: 1599721200000,
+        feedback: null,
+        avgCurrentVal: 32,
+        avgBaselineVal: 33,
+        properties: {
+          groupScore: "6.189942819613212",
+          detectorComponentName: "detection_grouper:ANOMALY_SUMMARIZE",
+          subEntityName: "group_entity_one",
+          groupKey: "groupConstituentOne"
+        },
+        children: [
+          {
+            id: 3,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentOne#",
+              feature_section: "groupConstituentOne",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          },
+          {
+            id: 4,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentOne#",
+              feature_section: "groupConstituentOne",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          }
+        ]
+      },
+      {
+        id: 5,
+        startTime: 1599462000000,
+        endTime: 1599721200000,
+        feedback: null,
+        avgCurrentVal: 32,
+        avgBaselineVal: 33,
+        properties: {
+          groupScore: "6.189942819613212",
+          detectorComponentName: "detection_grouper:ANOMALY_SUMMARIZE",
+          subEntityName: "group_entity_one",
+          groupKey: "groupConstituentTwo"
+        },
+        children: [
+          {
+            id: 6,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentTwo#",
+              feature_section: "groupConstituentTwo",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          },
+          {
+            id: 7,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentTwo#",
+              feature_section: "groupConstituentTwo",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          }
+        ]
+      },
+      {
+        id: 8,
+        startTime: 1599462000000,
+        endTime: 1599721200000,
+        avgCurrentVal: 32,
+        avgBaselineVal: 33,
+        feedback: null,
+        properties: {
+          groupScore: "6.189942819613212",
+          detectorComponentName: "detection_grouper:ANOMALY_SUMMARIZE",
+          subEntityName: "group_entity_two",
+          groupKey: "groupConstituentOne"
+        },
+        children: [
+          {
+            id: 9,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentOne#",
+              feature_section: "groupConstituentOne",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          },
+          {
+            id: 10,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentOne#",
+              feature_section: "groupConstituentOne",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          }
+        ]
+      },
+      {
+        id: 11,
+        startTime: 1599462000000,
+        endTime: 1599721200000,
+        avgCurrentVal: 32,
+        avgBaselineVal: 33,
+        feedback: null,
+        properties: {
+          groupScore: "6.189942819613212",
+          detectorComponentName: "detection_grouper:ANOMALY_SUMMARIZE",
+          subEntityName: "group_entity_two",
+          groupKey: "groupConstituentTwo"
+        },
+        children: [
+          {
+            id: 12,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentTwo#",
+              feature_section: "groupConstituentTwo",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          },
+          {
+            id: 13,
+            metric: "metric_four",
+            dimensions: {
+              feature_name: "groupConstituentTwo#",
+              feature_section: "groupConstituentTwo",
+              dimension_three: "True",
+              use_case: "DESKTOP"
+            },
+            startTime: 1599462000000,
+            endTime: 1599548400000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: []
+          }
+        ]
+      },
+      {
+        id: 15,
+        startTime: 1599462000000,
+        endTime: 1599462000000,
+        avgCurrentVal: 32,
+        avgBaselineVal: 33,
+        feedback: null,
+        children: [],
+        metric: "metric_one"
+      },
+      {
+        id: 16,
+        startTime: 1599462000000,
+        endTime: 1599462000000,
+        avgCurrentVal: 32,
+        avgBaselineVal: 33,
+        feedback: null,
+        children: [],
+        metric: "metric_one"
+      },
+      {
+        id: 17,
+        startTime: 1599462000000,
+        endTime: 1599462000000,
+        avgCurrentVal: 32,
+        avgBaselineVal: 33,
+        feedback: null,
+        children: [],
+        metric: "metric_two"
+      },
+      {
+        id: 18,
+        startTime: 1599462000000,
+        endTime: 1599462000000,
+        feedback: null,
+        metric: null,
+        properties: {},
+        children: [
+          {
+            id: 19,
+            startTime: 1599462000000,
+            endTime: 1599462000000,
+            avgCurrentVal: 32,
+            avgBaselineVal: 33,
+            feedback: null,
+            children: [],
+            metric: "metric_three"
+          }
+        ]
+      }
+    ]
+  }
+];
