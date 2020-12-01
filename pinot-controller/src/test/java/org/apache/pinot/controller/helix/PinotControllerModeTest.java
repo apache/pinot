2a86a79b7e33a2b831ff67fcd251a75942d01faa
Amrish Lal <amrish.k.lal@gmail.com>
2020-11-30 22:12:00 -0800

    fix license header.

--- pinot-controller/src/test/java/org/apache/pinot/controller/helix/PinotControllerModeTest.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/helix/PinotControllerModeTest.java
@@ -15,7 +15,9 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
+ */
+
+/*
 package org.apache.pinot.controller.helix;
 
 import java.util.Map;
