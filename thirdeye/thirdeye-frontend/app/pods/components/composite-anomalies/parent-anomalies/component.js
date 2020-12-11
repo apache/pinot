a0a4d65d45f265fef677a9e11a5f665d40ba0e84
Jose Fuentes <jofuente@jofuente-mn1.linkedin.biz>
2020-12-11 11:49:33 -0800

    Fix timezone format (PDT instead of PST) on column heading

--- thirdeye/thirdeye-frontend/app/pods/components/composite-anomalies/parent-anomalies/component.js
+++ thirdeye/thirdeye-frontend/app/pods/components/composite-anomalies/parent-anomalies/component.js
@@ -4,8 +4,8 @@
  * Display a table which contains composite anomalies
  * @module composite-anomalies/parent-anomalies
  * @property {string} title   - Heading to use on the table
- * @property {object[]} data  - [required] array of composite anomalies objects 
- * 
+ * @property {object[]} data  - [required] array of composite anomalies objects
+ *
  * @example
  * {{composite-anomalies/parent-anomalies title=<title> data=<data>}}
  *
@@ -22,89 +22,99 @@ let data = [];
 
 /*
  *  convert animaly 'start' and 'end' into an object to be used by template: 'custom/composite-animalies-table/start-duration'
-*/
+ */
 const getAnomaliesStartDuration = (start, end) => {
-    return {
-        startTime: start, 
-        endTime: end, 
-        duration: moment.duration(end - start).asHours() + ' hours'
-    };
-}
+  return {
+    startTime: start,
+    endTime: end,
+    duration: moment.duration(end - start).asHours() + ' hours'
+  };
+};
 
 /*
- *  convert list of anonalies object in to a objects 
+ *  convert list of anonalies object in to a objects
  */
 const getAnomaliesDetails = (details) => {
-    const detailsObject = [];
-    Object.entries(details).forEach(detail => {
-        detailsObject.push({name: detail[0], count: detail[1]})
-    });
+  const detailsObject = [];
+  Object.entries(details).forEach((detail) => {
+    detailsObject.push({ name: detail[0], count: detail[1] });
+  });
 
-    return detailsObject;
-}
+  return detailsObject;
+};
 
 /*
     map feedback onto a feedbackObject to be use in the feedback dropdown
     This code assumes that `feedback` is either null when it has not be set, or an integer that can then be mapped to feedbackOptions.
 */
 const getFeedback = (feedback = 0) => {
-    const feedbackOptions = ['Not reviewed yet', 'Yes - unexpected', 'Expected temporary change', 'Expected permanent change', 'No change observed'];
-    const selectedFeedback = feedback? feedbackOptions[feedback] : feedbackOptions[0];
+  const feedbackOptions = [
+    'Not reviewed yet',
+    'Yes - unexpected',
+    'Expected temporary change',
+    'Expected permanent change',
+    'No change observed'
+  ];
+  const selectedFeedback = feedback ? feedbackOptions[feedback] : feedbackOptions[0];
 
-    const feedbackObject = {
-        options: feedbackOptions,
-        selected: selectedFeedback
-    };
-    
-    return feedbackObject ;
-}
+  const feedbackObject = {
+    options: feedbackOptions,
+    selected: selectedFeedback
+  };
+
+  return feedbackObject;
+};
 
 export default Component.extend({
-    didReceiveAttrs() {
-        data = this.data;
-    },
-    tagName: 'section', 
-    customClasses: {
-        table: 'parent-anomalies-table'
-    },
-    title: 'Composite Anomalies',   // Default Header if no title is passed in.
-    noRecords: 'No Composite Anomalies found',
-    tableData: computed( 'data', () => {
-        let computedTableData = [];
-        
-        if (data && data.length > 0) {
-            data.map(d => {
-                const row = {
-                    startDuration: getAnomaliesStartDuration(d.startTime, d.endTime), 
-                    anomalies: getAnomaliesDetails(d.details), 
-                    feedback: getFeedback(d.feedback)
-                }
-                computedTableData.push(row);
-            });
-        }
-        
-        return computedTableData;
-    }), 
-    tableColumns: computed(
-        function() {
-            const startDurationColumn = [{
-                template: 'custom/composite-animalies-table/start-duration',
-                propertyName: 'startDuration',
-                title: 'Start / Duration (' + `${moment().tz(config.timeZone).format("z")}` + ')'
-              }];
+  didReceiveAttrs() {
+    data = this.data;
+  },
+  tagName: 'section',
+  customClasses: {
+    table: 'parent-anomalies-table'
+  },
+  title: 'Composite Anomalies', // Default Header if no title is passed in.
+  noRecords: 'No Composite Anomalies found',
+  tableData: computed('data', () => {
+    let computedTableData = [];
 
-            const anomiliesDetailsColumn = [{
-                template: 'custom/composite-animalies-table/anomalies-list',
-                propertyName: 'details',
-                title: 'Anomalies details',
-            }];
+    if (data && data.length > 0) {
+      data.map((d) => {
+        const row = {
+          startDuration: getAnomaliesStartDuration(d.startTime, d.endTime),
+          anomalies: getAnomaliesDetails(d.details),
+          feedback: getFeedback(d.feedback)
+        };
+        computedTableData.push(row);
+      });
+    }
 
-            const feedbackColumn = [{
-                component: 'custom/composite-animalies-table/resolution',
-                propertyName: 'feedback',
-                title: 'Feedback '
-            }];
-            return [...startDurationColumn, ...anomiliesDetailsColumn, ...feedbackColumn];
-        }
-    )
+    return computedTableData;
+  }),
+  tableColumns: computed(function () {
+    const startDurationColumn = [
+      {
+        template: 'custom/composite-animalies-table/start-duration',
+        propertyName: 'startDuration',
+        title: `Start / Duration (${moment.tz([2012, 5], config.timeZone).format('z')})`
+      }
+    ];
+
+    const anomiliesDetailsColumn = [
+      {
+        template: 'custom/composite-animalies-table/anomalies-list',
+        propertyName: 'details',
+        title: 'Anomalies details'
+      }
+    ];
+
+    const feedbackColumn = [
+      {
+        component: 'custom/composite-animalies-table/resolution',
+        propertyName: 'feedback',
+        title: 'Feedback '
+      }
+    ];
+    return [...startDurationColumn, ...anomiliesDetailsColumn, ...feedbackColumn];
+  })
 });
--- thirdeye/thirdeye-frontend/tests/integration/pods/components/composite-anomalies/parent-anomalies/component-test.js
+++ thirdeye/thirdeye-frontend/tests/integration/pods/components/composite-anomalies/parent-anomalies/component-test.js
@@ -2,33 +2,45 @@ import $ from 'jquery';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('composite-anomalies/parent-anomalies', 'Integration | Component | composite anomalies/parent anomalies', {
-  integration: true
-});
+moduleForComponent(
+  'composite-anomalies/parent-anomalies',
+  'Integration | Component | composite anomalies/parent anomalies',
+  {
+    integration: true
+  }
+);
 
-test('it renders', function(assert) {
+test('it renders', function (assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-
 
   this.setProperties({
     tableTitle: 'Composite Anomalies',
     noAnmalies: 'No Composite Anomalies found',
-    tableData: [{
-      id:1,
-      startTime: 1599462000000,
-      endTime: 1599721200000,
-      feedback: null,
-      details: {
-        oe_viral_detection: 2,
-        feed_feature_coverage_health: 2,
-        feed_feature_distribution_health: 2
+    tableData: [
+      {
+        id: 1,
+        startTime: 1599462000000,
+        endTime: 1599721200000,
+        feedback: null,
+        details: {
+          oe_viral_detection: 2,
+          feed_feature_coverage_health: 2,
+          feed_feature_distribution_health: 2
+        }
       }
-    }]
+    ],
+    feedbackOptions: [
+      'Not reviewed yet',
+      'Yes - unexpected',
+      'Expected temporary change',
+      'Expected permanent change',
+      'No change observed'
+    ]
   });
 
   this.render(hbs`{{composite-anomalies/parent-anomalies}}`);
-  
+
   assert.equal($('h4.panel-title').html(), this.tableTitle);
   assert.equal($('p.composite-anomalies-no-records').html(), this.noAnmalies);
 
@@ -39,18 +51,24 @@ test('it renders', function(assert) {
   assert.equal($('.panel-title').html(), this.tableTitle);
   assert.equal($('.composite-anomalies-no-records').html(), this.noAnmalies);
 
-
   this.render(hbs`
     {{composite-anomalies/parent-anomalies data=tableData}}
   `);
 
-assert.equal($('.start-time').html(), 'Sep 7th, 12:00 ');
-assert.equal($('.duration').html(), '72 hours');
-  //.te-label Sep 7th, 12:00 PDT
-  // .duration 72 hours
-  // .details oe_viral_detection (2)
-  // .details feed_feature_coverage_health (2)
-  // .details feed_feature_distribution_health (2)
-  // .ember-power-select-selected-item   Not reviewed yet
+  assert.equal($('.start-time').html(), 'Sep 7th, 12:00 ');
+  assert.equal($('.duration').html(), '72 hours');
+  assert.equal($('.details').html(), 'oe_viral_detection (2)');
+  assert.equal($('.ember-power-select-selected-item').html().trim(), this.feedbackOptions[0]);
 
+  // Check other values based on feedback
+
+  this.feedbackOptions.forEach((option, index) => {
+    this.tableData[0].feedback = index === 0 ? null : index;
+
+    this.render(hbs`
+      {{composite-anomalies/parent-anomalies data=tableData}}
+    `);
+
+    assert.equal($('.ember-power-select-selected-item').html().trim(), option);
+  });
 });
