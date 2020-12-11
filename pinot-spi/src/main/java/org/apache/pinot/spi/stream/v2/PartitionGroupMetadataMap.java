8899614b045caed2e6f159efbaaf7499a345c0d3
KKcorps <kharekartik@gmail.com>
2020-12-11 23:56:08 +0530

    Refactor PartitionGroupMetadataMap interface

--- pinot-spi/src/main/java/org/apache/pinot/spi/stream/v2/PartitionGroupMetadataMap.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/stream/v2/PartitionGroupMetadataMap.java
@@ -1,4 +1,12 @@
 package org.apache.pinot.spi.stream.v2;
 
+import java.util.List;
+
+
 public interface PartitionGroupMetadataMap {
+
+  List<PartitionGroupMetadata> getMetadataList();
+
+  PartitionGroupMetadata getPartitionGroupMetadata(int index);
+
 }
