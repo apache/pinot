ed0362ae081d3ef40a49ec2c23ab32d891c4e596
Jose Fuentes <jofuente@jofuente-mn1.linkedin.biz>
2020-12-16 13:24:21 -0800

    [TE]frontend - THIRDEYE-3772 - address code-review feedback,  refactor anomaly reponses

--- thirdeye/thirdeye-frontend/app/pods/components/composite-anomalies/parent-anomalies/component.js
+++ thirdeye/thirdeye-frontend/app/pods/components/composite-anomalies/parent-anomalies/component.js
@@ -1,7 +1,7 @@
 /*
  * Parent Anomalies Component
  *
- * Display a table which contains composite anomalies
+ * Display a table containing composite anomalies
  * @module composite-anomalies/parent-anomalies
  * @property {string} title   - Heading to use on the table
  * @property {object[]} data  - [required] array of composite anomalies objects
@@ -14,107 +14,104 @@
 
 import Component from '@ember/component';
 import { computed } from '@ember/object';
+import { A as EmberArray } from '@ember/array';
 
 import moment from 'moment';
 import config from 'thirdeye-frontend/config/environment';
+import * as anomalyUtil from 'thirdeye-frontend/utils/anomaly';
 
-let data = [];
-
-/*
- *  convert animaly 'start' and 'end' into an object to be used by template: 'custom/composite-animalies-table/start-duration'
- */
-const getAnomaliesStartDuration = (start, end) => {
-  return {
-    startTime: start,
-    endTime: end,
-    duration: moment.duration(end - start).asHours() + ' hours'
-  };
-};
-
-/*
- *  convert list of anonalies object in to a objects
- */
-const getAnomaliesDetails = (details) => {
-  const detailsObject = [];
-  Object.entries(details).forEach((detail) => {
-    detailsObject.push({ name: detail[0], count: detail[1] });
-  });
-
-  return detailsObject;
-};
-
-/*
-    map feedback onto a feedbackObject to be use in the feedback dropdown
-    This code assumes that `feedback` is either null when it has not be set, or an integer that can then be mapped to feedbackOptions.
-*/
-const getFeedback = (feedback = 0) => {
-  const feedbackOptions = [
-    'Not reviewed yet',
-    'Yes - unexpected',
-    'Expected temporary change',
-    'Expected permanent change',
-    'No change observed'
-  ];
-  const selectedFeedback = feedback ? feedbackOptions[feedback] : feedbackOptions[0];
-
-  const feedbackObject = {
-    options: feedbackOptions,
-    selected: selectedFeedback
-  };
-
-  return feedbackObject;
-};
+const TABLE_COLUMNS = [
+  {
+    template: 'custom/composite-animalies-table/start-duration',
+    propertyName: 'startDuration',
+    title: `Start / Duration (${moment.tz([2012, 5], config.timeZone).format('z')})`
+  },
+  {
+    template: 'custom/composite-animalies-table/anomalies-list',
+    propertyName: 'details',
+    title: 'Anomalies details'
+  },
+  {
+    component: 'custom/composite-animalies-table/resolution',
+    propertyName: 'feedback',
+    title: 'Feedback '
+  }
+];
 
 export default Component.extend({
-  didReceiveAttrs() {
-    data = this.data;
-  },
+  data: EmberArray(),
   tagName: 'section',
   customClasses: {
-    table: 'parent-anomalies-table'
+    table: 'composite-anomalies-table'
   },
   title: 'Composite Anomalies', // Default Header if no title is passed in.
   noRecords: 'No Composite Anomalies found',
-  tableData: computed('data', () => {
+  tableData: computed('data', function () {
     let computedTableData = [];
 
-    if (data && data.length > 0) {
-      data.map((d) => {
+    if (this.data && this.data.length > 0) {
+      this.data.map((d) => {
         const row = {
-          startDuration: getAnomaliesStartDuration(d.startTime, d.endTime),
-          anomalies: getAnomaliesDetails(d.details),
-          feedback: getFeedback(d.feedback)
+          startDuration: this.getAnomaliesStartDuration(d.startTime, d.endTime),
+          anomaliesDetails: this.getAnomaliesDetails(d.details),
+          feedback: this.getFeedback(d.feedback)
         };
         computedTableData.push(row);
       });
     }
-
     return computedTableData;
   }),
-  tableColumns: computed(function () {
-    const startDurationColumn = [
-      {
-        template: 'custom/composite-animalies-table/start-duration',
-        propertyName: 'startDuration',
-        title: `Start / Duration (${moment.tz([2012, 5], config.timeZone).format('z')})`
-      }
-    ];
-
-    const anomiliesDetailsColumn = [
-      {
-        template: 'custom/composite-animalies-table/anomalies-list',
-        propertyName: 'details',
-        title: 'Anomalies details'
-      }
-    ];
-
-    const feedbackColumn = [
-      {
-        component: 'custom/composite-animalies-table/resolution',
-        propertyName: 'feedback',
-        title: 'Feedback '
-      }
-    ];
-    return [...startDurationColumn, ...anomiliesDetailsColumn, ...feedbackColumn];
-  })
+  tableColumns: TABLE_COLUMNS,
+  /*
+   *  convert anomaly 'start' and 'end' into an object to be used by template: 'custom/composite-animalies-table/start-duration'
+   *
+   * @param {Number} start
+   * The start time in milliseconds.
+   * @param {Number} end
+   * The end time in milliseconds
+   *
+   * @returns {Object}
+   * Description of the object.
+   */
+  getAnomaliesStartDuration: (start, end) => {
+    return {
+      startTime: start,
+      endTime: end,
+      duration: moment.duration(end - start).asHours() + ' hours'
+    };
+  },
+  /*
+   *  convert list of anonalies object in to an array of objects to be used by template: 'custom/composite-animalies-table/anomalies-list'
+   *
+   * @param {Object} details
+   * The object containing the list of anomalies and their count.
+   *
+   * @returns {Array}
+   * Description of the object.
+   */
+  getAnomaliesDetails: (anomalies) => {
+    return Object.entries(anomalies).reduce((anomalyList, anomalyDetails) => {
+      anomalyList.push({ name: anomalyDetails[0], count: anomalyDetails[1] });
+      return anomalyList;
+    }, []);
+  },
+  /*
+   * return feedbackObject with pre-selected 'feedback' to be use in the feedback dropdown
+   *
+   * @param {String} feedback
+   * The object containing the list of anomalies and their count.
+   *
+   * @returns {Object} feedbackObject
+   * Description of the object.
+   */
+  getFeedback: (feedback) => {
+    const selectedFeedback = feedback
+      ? anomalyUtil.anomalyResponseObj.find((f) => f.value === feedback)
+      : anomalyUtil.anomalyResponseObj[0];
+    const feedbackObject = {
+      options: anomalyUtil.anomalyResponseObj.mapBy('name'),
+      selected: selectedFeedback ? selectedFeedback.name : anomalyUtil.anomalyResponseObj[0].name
+    };
+    return feedbackObject;
+  }
 });
