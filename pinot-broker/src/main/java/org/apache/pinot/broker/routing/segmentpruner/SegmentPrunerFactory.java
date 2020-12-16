akuna-capital/incubator-pinot

Broker time range pruning(#6189)

pinot-broker/src/main/java/org/apache/pinot/broker/routing/segmentpruner/SegmentPrunerFactory.java
@@ -28,6 +28,7 @@ import org.apache.helix.store.zk.ZkHelixPropertyStore;
 import org.apache.pinot.spi.config.table.ColumnPartitionConfig;
 import org.apache.pinot.spi.config.table.RoutingConfig;
 import org.apache.pinot.spi.config.table.SegmentPartitionConfig;
+import org.apache.pinot.spi.config.table.SegmentsValidationAndRetentionConfig;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.config.table.TableType;
 import org.slf4j.Logger;
@@ -57,8 +58,15 @@ public class SegmentPrunerFactory {
               segmentPruners.add(partitionSegmentPruner);
             }
           }
+
+          if (RoutingConfig.TIME_RANGE_SEGMENT_PRUNER_TYPE.equalsIgnoreCase(segmentPrunerType)) {
+            TimeRangeSegmentPruner timeRangeSegmentPruner = getTimeRangeSegmentPruner(tableConfig, propertyStore);
+            if (timeRangeSegmentPruner != null) {
+              segmentPruners.add(timeRangeSegmentPruner);
+            }
+          }
         }
-        return segmentPruners;
+        return sortSegmentPruners(segmentPruners);
       } else {
         // Handle legacy configs for backward-compatibility
         TableType tableType = tableConfig.getTableType();
@@ -96,5 +104,45 @@ public class SegmentPrunerFactory {
           tableNameWithType);
       return new PartitionSegmentPruner(tableNameWithType, partitionColumn, propertyStore);
     }
+  }
+
+  @Nullable
+  private static TimeRangeSegmentPruner getTimeRangeSegmentPruner(TableConfig tableConfig,
+      ZkHelixPropertyStore<ZNRecord> propertyStore) {
+    String tableNameWithType = tableConfig.getTableName();
+    SegmentsValidationAndRetentionConfig validationConfig = tableConfig.getValidationConfig();
+    if (validationConfig == null) {
+      LOGGER.warn("Cannot enable time range pruning without validation config for table: {}",
+          tableNameWithType);
+      return null;
+    }
+    String timeColumn = validationConfig.getTimeColumnName();
+    if (timeColumn == null) {
+      LOGGER.warn("Cannot enable time range pruning without time column for table: {}",
+          tableNameWithType);
+      return null;
+    }
+
+    LOGGER.info("Using TimeRangePruner on time column: {} for table: {}", timeColumn,
+        tableNameWithType);
+    return new TimeRangeSegmentPruner(tableConfig, propertyStore);
+  }
+
+  private static List<SegmentPruner> sortSegmentPruners(List<SegmentPruner> pruners) {
+    // If there's multiple pruners, move time range pruners to the front。
+    // Partition pruner run time is proportional to input # of segments while time range pruner is not,
+    // Prune based on time range first will have a smaller input size for partition pruners, so have better performance.
+    List<SegmentPruner> sortedPruners = new ArrayList<>();
+    for (SegmentPruner pruner : pruners) {
+      if (pruner instanceof TimeRangeSegmentPruner) {
+        sortedPruners.add(pruner);
+      }
+    }
+    for (SegmentPruner pruner: pruners) {
+      if (!(pruner instanceof TimeRangeSegmentPruner)) {
+        sortedPruners.add(pruner);
+      }
+    }
+    return sortedPruners;
   }
 }

pinot-broker/src/test/java/org/apache/pinot/broker/routing/IntervalST/IntervalTest.java
@@ -1,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.broker.routing.IntervalST;
+
+import org.apache.pinot.broker.routing.segmentpruner.intervalST.Interval;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class IntervalTest {
+
+  @Test
+  public void testInterval() {
+    Interval a = new Interval(1, 5);
+    Interval b = new Interval(6, 8);
+    Interval c = new Interval(1, 4);
+    Interval d = new Interval(1, 6);
+    Interval e = new Interval(1, 5);
+    Interval f = new Interval(5, 5);
+
+    // test compareTo
+    Assert.assertTrue(a.compareTo(b) < 0);
+    Assert.assertTrue(a.compareTo(c) > 0);
+    Assert.assertTrue(a.compareTo(d) < 0);
+    Assert.assertTrue(a.compareTo(e) == 0);
+
+    // test intersects
+    Assert.assertFalse(a.intersects(b));
+    Assert.assertTrue(a.intersects(f));
+
+    // test intersection
+    Assert.assertEquals(Interval.getIntersection(a, b), null);
+    Assert.assertEquals(Interval.getIntersection(a, c), c);
+    Assert.assertEquals(Interval.getIntersection(b, d), new Interval(6, 6));
+
+    // test union
+    Assert.assertEquals(Interval.getUnion(a, b), null);
+    Assert.assertEquals(Interval.getUnion(b, d), new Interval(1, 8));
+    Assert.assertEquals(Interval.getUnion(a, d), d);
+  }
+}

pinot-spi/src/main/java/org/apache/pinot/spi/config/table/RoutingConfig.java
@@ -27,6 +27,7 @@ import org.apache.pinot.spi.config.BaseJsonConfig;
 
 public class RoutingConfig extends BaseJsonConfig {
   public static final String PARTITION_SEGMENT_PRUNER_TYPE = "partition";
+  public static final String TIME_RANGE_SEGMENT_PRUNER_TYPE = "timeRange";
   public static final String REPLICA_GROUP_INSTANCE_SELECTOR_TYPE = "replicaGroup";
   public static final String STRICT_REPLICA_GROUP_INSTANCE_SELECTOR_TYPE = "strictReplicaGroup";
 

apache/pinot

pinot-broker/src/main/java/org/apache/pinot/broker/routing/segmentpruner/SegmentPrunerFactory.java
@@ -28,6 +28,7 @@ import org.apache.helix.store.zk.ZkHelixPropertyStore;
 import org.apache.pinot.spi.config.table.ColumnPartitionConfig;
 import org.apache.pinot.spi.config.table.RoutingConfig;
 import org.apache.pinot.spi.config.table.SegmentPartitionConfig;
+import org.apache.pinot.spi.config.table.SegmentsValidationAndRetentionConfig;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.config.table.TableType;
 import org.slf4j.Logger;
@@ -57,8 +58,15 @@ public class SegmentPrunerFactory {
               segmentPruners.add(partitionSegmentPruner);
             }
           }
+
+          if (RoutingConfig.TIME_SEGMENT_PRUNER_TYPE.equalsIgnoreCase(segmentPrunerType)) {
+            TimeSegmentPruner timeSegmentPruner = getTimeSegmentPruner(tableConfig, propertyStore);
+            if (timeSegmentPruner != null) {
+              segmentPruners.add(timeSegmentPruner);
+            }
+          }
         }
-        return segmentPruners;
+        return sortSegmentPruners(segmentPruners);
       } else {
         // Handle legacy configs for backward-compatibility
         TableType tableType = tableConfig.getTableType();
@@ -96,5 +104,45 @@ public class SegmentPrunerFactory {
           tableNameWithType);
       return new PartitionSegmentPruner(tableNameWithType, partitionColumn, propertyStore);
     }
+  }
+
+  @Nullable
+  private static TimeSegmentPruner getTimeSegmentPruner(TableConfig tableConfig,
+      ZkHelixPropertyStore<ZNRecord> propertyStore) {
+    String tableNameWithType = tableConfig.getTableName();
+    SegmentsValidationAndRetentionConfig validationConfig = tableConfig.getValidationConfig();
+    if (validationConfig == null) {
+      LOGGER.warn("Cannot enable time range pruning without validation config for table: {}",
+          tableNameWithType);
+      return null;
+    }
+    String timeColumn = validationConfig.getTimeColumnName();
+    if (timeColumn == null) {
+      LOGGER.warn("Cannot enable time range pruning without time column for table: {}",
+          tableNameWithType);
+      return null;
+    }
+
+    LOGGER.info("Using TimeRangePruner on time column: {} for table: {}", timeColumn,
+        tableNameWithType);
+    return new TimeSegmentPruner(tableConfig, propertyStore);
+  }
+
+  private static List<SegmentPruner> sortSegmentPruners(List<SegmentPruner> pruners) {
+    // If there's multiple pruners, move time range pruners to the front。
+    // Partition pruner run time is proportional to input # of segments while time range pruner is not,
+    // Prune based on time range first will have a smaller input size for partition pruners, so have better performance.
+    List<SegmentPruner> sortedPruners = new ArrayList<>();
+    for (SegmentPruner pruner : pruners) {
+      if (pruner instanceof TimeSegmentPruner) {
+        sortedPruners.add(pruner);
+      }
+    }
+    for (SegmentPruner pruner: pruners) {
+      if (!(pruner instanceof TimeSegmentPruner)) {
+        sortedPruners.add(pruner);
+      }
+    }
+    return sortedPruners;
   }
 }

pinot-broker/src/test/java/org/apache/pinot/broker/routing/IntervalST/IntervalTest.java
@@ -1,0 +1,57 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.broker.routing.IntervalST;
+
+import org.apache.pinot.broker.routing.segmentpruner.interval.Interval;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class IntervalTest {
+
+  @Test
+  public void testInterval() {
+    Interval a = new Interval(1, 5);
+    Interval b = new Interval(6, 8);
+    Interval c = new Interval(1, 4);
+    Interval d = new Interval(1, 6);
+    Interval e = new Interval(1, 5);
+    Interval f = new Interval(5, 5);
+
+    // test compareTo
+    Assert.assertTrue(a.compareTo(b) < 0);
+    Assert.assertTrue(a.compareTo(c) > 0);
+    Assert.assertTrue(a.compareTo(d) < 0);
+    Assert.assertTrue(a.compareTo(e) == 0);
+
+    // test intersects
+    Assert.assertFalse(a.intersects(b));
+    Assert.assertTrue(a.intersects(f));
+
+    // test intersection
+    Assert.assertEquals(Interval.getIntersection(a, b), null);
+    Assert.assertEquals(Interval.getIntersection(a, c), c);
+    Assert.assertEquals(Interval.getIntersection(b, d), new Interval(6, 6));
+
+    // test union
+    Assert.assertEquals(Interval.getUnion(a, b), null);
+    Assert.assertEquals(Interval.getUnion(b, d), new Interval(1, 8));
+    Assert.assertEquals(Interval.getUnion(a, d), d);
+  }
+}

pinot-spi/src/main/java/org/apache/pinot/spi/config/table/RoutingConfig.java
@@ -27,6 +27,7 @@ import org.apache.pinot.spi.config.BaseJsonConfig;
 
 public class RoutingConfig extends BaseJsonConfig {
   public static final String PARTITION_SEGMENT_PRUNER_TYPE = "partition";
+  public static final String TIME_SEGMENT_PRUNER_TYPE = "time";
   public static final String REPLICA_GROUP_INSTANCE_SELECTOR_TYPE = "replicaGroup";
   public static final String STRICT_REPLICA_GROUP_INSTANCE_SELECTOR_TYPE = "strictReplicaGroup";
 