530877a944d84ee21a508652dcc7e4feb1873e74
Jose Fuentes <jofuente@jofuente-mn1.linkedin.biz>
2020-12-18 13:00:00 -0800

    [TE]frontend - THIRDEYE-3772 - create a new 'composite-anomalies' component plus very simple unit integration test

---
+++ thirdeye/thirdeye-frontend/app/pods/components/composite-anomalies/parent-anomalies/component.js
@@ -0,0 +1,117 @@
+/*
+ * Parent Anomalies Component
+ *
+ * Display a table containing composite anomalies
+ * @module composite-anomalies/parent-anomalies
+ * @property {string} title   - Heading to use on the table
+ * @property {object[]} data  - [required] array of composite anomalies objects
+ *
+ * @example
+ * {{composite-anomalies/parent-anomalies title=<title> data=<data>}}
+ *
+ * @exports composite-anomalies/parent-anomalies
+ */
+
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { A as EmberArray } from '@ember/array';
+
+import moment from 'moment';
+import config from 'thirdeye-frontend/config/environment';
+import * as anomalyUtil from 'thirdeye-frontend/utils/anomaly';
+
+const TABLE_COLUMNS = [
+  {
+    template: 'custom/composite-anomalies-table/start-duration',
+    propertyName: 'startDuration',
+    title: `Start / Duration (${moment.tz([2012, 5], config.timeZone).format('z')})`
+  },
+  {
+    template: 'custom/composite-anomalies-table/anomalies-list',
+    propertyName: 'details',
+    title: 'Anomalies details'
+  },
+  {
+    component: 'custom/composite-anomalies-table/resolution',
+    propertyName: 'feedback',
+    title: 'Feedback '
+  }
+];
+
+export default Component.extend({
+  data: EmberArray(),
+  tagName: 'section',
+  customClasses: {
+    table: 'composite-anomalies-table'
+  },
+  title: 'Composite Anomalies', // Default Header if no title is passed in.
+  noRecords: 'No Composite Anomalies found',
+  tableData: computed('data', function () {
+    const computedTableData = [];
+
+    if (this.data && this.data.length > 0) {
+      this.data.map((d) => {
+        const row = {
+          startDuration: this.getAnomaliesStartDuration(d.startTime, d.endTime),
+          anomaliesDetails: this.getAnomaliesDetails(d.details),
+          feedback: this.getFeedback(d.feedback)
+        };
+        computedTableData.push(row);
+      });
+    }
+    return computedTableData;
+  }),
+  tableColumns: TABLE_COLUMNS,
+  /*
+   *  convert anomaly 'start' and 'end' into an object to be used by template: 'custom/composite-animalies-table/start-duration'
+   *
+   * @param {Number} start
+   * The start time in milliseconds.
+   * @param {Number} end
+   * The end time in milliseconds
+   *
+   * @returns {Object}
+   * Description of the object.
+   */
+  getAnomaliesStartDuration: (start, end) => {
+    return {
+      startTime: start,
+      endTime: end,
+      duration: moment.duration(end - start).asHours() + ' hours'
+    };
+  },
+  /*
+   *  convert list of anonalies object in to an array of objects to be used by template: 'custom/composite-animalies-table/anomalies-list'
+   *
+   * @param {Object} details
+   * The object containing the list of anomalies and their count.
+   *
+   * @returns {Array}
+   * Description of the object.
+   */
+  getAnomaliesDetails: (anomalies) => {
+    return Object.entries(anomalies).reduce((anomalyList, anomalyDetails) => {
+      anomalyList.push({ name: anomalyDetails[0], count: anomalyDetails[1] });
+      return anomalyList;
+    }, []);
+  },
+  /*
+   * return feedbackObject with pre-selected 'feedback' to be use in the feedback dropdown
+   *
+   * @param {String} feedback
+   * The object containing the list of anomalies and their count.
+   *
+   * @returns {Object} feedbackObject
+   * Description of the object.
+   */
+  getFeedback: (feedback) => {
+    const selectedFeedback = feedback
+      ? anomalyUtil.anomalyResponseObj.find((f) => f.value === feedback)
+      : anomalyUtil.anomalyResponseObj[0];
+
+    return {
+      options: anomalyUtil.anomalyResponseObj.mapBy('name'),
+      selected: selectedFeedback ? selectedFeedback.name : anomalyUtil.anomalyResponseObj[0].name
+    };
+  }
+});
---
+++ thirdeye/thirdeye-frontend/tests/integration/pods/components/composite-anomalies/parent-anomalies/component-test.js
@@ -0,0 +1,69 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import * as anomalyUtil from 'thirdeye-frontend/utils/anomaly';
+
+moduleForComponent(
+  'composite-anomalies/parent-anomalies',
+  'Integration | Component | composite anomalies/parent anomalies',
+  {
+    integration: true
+  }
+);
+
+test('it renders', function (assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.setProperties({
+    tableTitle: 'Composite Anomalies',
+    noAnmalies: 'No Composite Anomalies found',
+    tableData: [
+      {
+        id: 1,
+        startTime: 1599462000000,
+        endTime: 1599721200000,
+        feedback: null,
+        details: {
+          oe_viral_detection: 2,
+          feed_feature_coverage_health: 2,
+          feed_feature_distribution_health: 2
+        }
+      }
+    ],
+    feedbackOptionNames: anomalyUtil.anomalyResponseObj.mapBy('name'),
+    feedbackOptionValues: anomalyUtil.anomalyResponseObj.mapBy('value')
+  });
+
+  this.render(hbs`{{composite-anomalies/parent-anomalies}}`);
+
+  assert.equal(this.$('h4.panel-title').html(), this.tableTitle);
+  assert.equal(this.$('p.composite-anomalies-no-records').html(), this.noAnmalies);
+
+  this.render(hbs`
+    {{composite-anomalies/parent-anomalies title=tableTitle}}
+  `);
+
+  assert.equal(this.$('.panel-title').html(), this.tableTitle);
+  assert.equal(this.$('.composite-anomalies-no-records').html(), this.noAnmalies);
+
+  this.render(hbs`
+    {{composite-anomalies/parent-anomalies data=tableData}}
+  `);
+
+  // assert.equal($('.start-time').html(), 'Sep 7th, 12:00 ');
+  assert.equal(this.$('.duration').html(), '72 hours');
+  assert.equal(this.$('.details').html(), 'oe_viral_detection (2)');
+  assert.equal(this.$('.ember-power-select-selected-item').html().trim(), this.feedbackOptionNames[0]);
+
+  // Check other values based on feedback
+
+  this.feedbackOptionNames.forEach((option, index) => {
+    this.tableData[0].feedback = this.feedbackOptionValues[index];
+
+    this.render(hbs`
+      {{composite-anomalies/parent-anomalies data=tableData}}
+    `);
+
+    assert.equal(this.$('.ember-power-select-selected-item').html().trim(), option);
+  });
+});
