781e1676d867816232d5715fe4ccc4e14e0da1fb
Kartik Khare <kharekartik@gmail.com>
2021-01-05 01:19:28 +0530

    Add support for stream partition offsets (#6402)

---
+++ pinot-plugins/pinot-stream-ingestion/pinot-kinesis/src/main/java/org/apache/pinot/plugin/stream/kinesis/KinesisMsgOffsetFactory.java
@@ -0,0 +1,32 @@
+package org.apache.pinot.plugin.stream.kinesis;
+
+import java.io.IOException;
+import org.apache.pinot.spi.stream.StreamConfig;
+import org.apache.pinot.spi.stream.StreamPartitionMsgOffset;
+import org.apache.pinot.spi.stream.StreamPartitionMsgOffsetFactory;
+
+
+public class KinesisMsgOffsetFactory implements StreamPartitionMsgOffsetFactory {
+
+  KinesisConfig _kinesisConfig;
+
+  @Override
+  public void init(StreamConfig streamConfig) {
+    _kinesisConfig = new KinesisConfig(streamConfig);
+  }
+
+  @Override
+  public StreamPartitionMsgOffset create(String offsetStr) {
+    try {
+      return new KinesisCheckpoint(offsetStr);
+    }catch (IOException e){
+      return null;
+    }
+  }
+
+  @Override
+  public StreamPartitionMsgOffset create(StreamPartitionMsgOffset other) {
+    return new KinesisCheckpoint(((KinesisCheckpoint) other).getShardToStartSequenceMap());
+  }
+
+}
