29cf33b280116c4cf7d3305204c3531f2261b1f9
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2021-02-14 15:13:41 -0800

    Enhance StarTreeIndexViewer to support multiple trees (#6569)
    Enhance the StarTreeIndexViewers to support:
    - Visualize multiple star-trees
    - Include dimension name in the node
    - Fix a bug of miscounting the top nodes

--- pinot-tools/src/main/java/org/apache/pinot/tools/StarTreeIndexViewer.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/StarTreeIndexViewer.java
@@ -26,11 +26,9 @@ import java.io.InputStream;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -39,15 +37,13 @@ import javax.ws.rs.core.MediaType;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.pinot.common.segment.ReadMode;
-import org.apache.pinot.core.common.DataSource;
 import org.apache.pinot.core.indexsegment.IndexSegment;
 import org.apache.pinot.core.indexsegment.immutable.ImmutableSegmentLoader;
-import org.apache.pinot.core.segment.index.metadata.SegmentMetadataImpl;
 import org.apache.pinot.core.segment.index.readers.Dictionary;
 import org.apache.pinot.core.startree.StarTree;
 import org.apache.pinot.core.startree.StarTreeNode;
+import org.apache.pinot.core.startree.v2.StarTreeV2;
 import org.apache.pinot.spi.utils.JsonUtils;
-import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
@@ -55,6 +51,57 @@ import org.slf4j.LoggerFactory;
 
 
 public class StarTreeIndexViewer {
+  private static final Logger LOGGER = LoggerFactory.getLogger(StarTreeIndexViewer.class);
+
+  /*
+   * MAX num children to show in the UI
+   */
+  private static final int MAX_CHILDREN = 100;
+
+  public StarTreeIndexViewer(File segmentDir)
+      throws Exception {
+    IndexSegment indexSegment = ImmutableSegmentLoader.load(segmentDir, ReadMode.heap);
+    List<StarTreeV2> starTrees = indexSegment.getStarTrees();
+    if (starTrees == null) {
+      throw new IllegalStateException("Cannot find star-tree in segment directory: " + segmentDir);
+    }
+
+    StarTreeJsonNode rootJsonNode = new StarTreeJsonNode("ROOT");
+    int numStarTrees = starTrees.size();
+    if (numStarTrees == 1) {
+      new SingleTreeViewer(starTrees.get(0)).build(rootJsonNode);
+    } else {
+      for (int i = 0; i < numStarTrees; i++) {
+        StarTreeJsonNode singleTreeRootJsonNode = new StarTreeJsonNode("Star-Tree-" + i);
+        new SingleTreeViewer(starTrees.get(i)).build(singleTreeRootJsonNode);
+        rootJsonNode.addChild(singleTreeRootJsonNode);
+      }
+    }
+
+    indexSegment.destroy();
+    String rootJsonString = JsonUtils.objectToPrettyString(rootJsonNode);
+    LOGGER.info(rootJsonString);
+    startServer(rootJsonString);
+  }
+
+  private void startServer(String jsonString) {
+    GrizzlyHttpServerFactory.createHttpServer(URI.create("http://0.0.0.0:8090/"), new StarTreeResource(jsonString));
+    LOGGER.info("Go to http://localhost:8090/ to view the star-trees");
+  }
+
+  public static void main(String[] args)
+      throws Exception {
+    if (args.length != 1) {
+      LOGGER.error("USAGE: StarIndexViewer <segmentDirectory>");
+      System.exit(1);
+    }
+    String segmentDir = args[0];
+    if (!new File(segmentDir).isDirectory()) {
+      throw new FileNotFoundException("Missing directory: " + segmentDir);
+    }
+    new StarTreeIndexViewer(new File(segmentDir));
+  }
+
   private static class StarTreeJsonNode {
     String _name;
     List<StarTreeJsonNode> _children;
@@ -80,107 +127,74 @@ public class StarTreeIndexViewer {
     }
   }
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(StarTreeIndexViewer.class);
+  private static class SingleTreeViewer {
+    private final StarTreeNode _rootNode;
+    private final List<String> _dimensionNames;
+    private final List<Dictionary> _dimensionDictionaries;
 
-  /*
-   * MAX num children to show in the UI
-   */
-  static int MAX_CHILDREN = 100;
-  private List<String> _dimensionNames;
-  private Map<String, Dictionary> dictionaries;
-
-  public StarTreeIndexViewer(File segmentDir)
-      throws Exception {
-    IndexSegment indexSegment = ImmutableSegmentLoader.load(segmentDir, ReadMode.heap);
-
-    dictionaries = new HashMap<>();
-    SegmentMetadataImpl metadata = new SegmentMetadataImpl(segmentDir);
-
-    for (String columnName : metadata.getAllColumns()) {
-      DataSource dataSource = indexSegment.getDataSource(columnName);
-      dictionaries.put(columnName, dataSource.getDictionary());
-    }
-    StarTree tree = indexSegment.getStarTrees().get(0).getStarTree();
-    _dimensionNames = tree.getDimensionNames();
-    StarTreeJsonNode jsonRoot = new StarTreeJsonNode("ROOT");
-    build(tree.getRoot(), jsonRoot);
-    indexSegment.destroy();
-
-    String writeValueAsString = JsonUtils.objectToPrettyString(jsonRoot);
-    LOGGER.info(writeValueAsString);
-    startServer(segmentDir, writeValueAsString);
-  }
-
-  private int build(StarTreeNode indexNode, StarTreeJsonNode json) {
-    Iterator<? extends StarTreeNode> childrenIterator = indexNode.getChildrenIterator();
-    if (!childrenIterator.hasNext()) {
-      return 0;
-    }
-    int childDimensionId = indexNode.getChildDimensionId();
-    String childDimensionName = _dimensionNames.get(childDimensionId);
-    Dictionary dictionary = dictionaries.get(childDimensionName);
-    int totalChildNodes = indexNode.getNumChildren();
-
-    Comparator<Pair<String, Integer>> comparator = new Comparator<Pair<String, Integer>>() {
-
-      @Override
-      public int compare(Pair<String, Integer> o1, Pair<String, Integer> o2) {
-        return -1 * Integer.compare(o1.getRight(), o2.getRight());
-      }
-    };
-    MinMaxPriorityQueue<Pair<String, Integer>> queue =
-        MinMaxPriorityQueue.orderedBy(comparator).maximumSize(MAX_CHILDREN).create();
-    StarTreeJsonNode allNode = null;
-
-    while (childrenIterator.hasNext()) {
-      StarTreeNode childIndexNode = childrenIterator.next();
-      int childDimensionValueId = childIndexNode.getDimensionValue();
-      String childDimensionValue = "ALL";
-      if (childDimensionValueId != StarTreeNode.ALL) {
-        childDimensionValue = dictionary.get(childDimensionValueId).toString();
-      }
-      StarTreeJsonNode childJson = new StarTreeJsonNode(childDimensionValue);
-      totalChildNodes += build(childIndexNode, childJson);
-      if (childDimensionValueId != StarTreeNode.ALL) {
-        json.addChild(childJson);
-        queue.add(ImmutablePair.of(childDimensionValue, totalChildNodes));
-      } else {
-        allNode = childJson;
+    public SingleTreeViewer(StarTreeV2 starTreeV2) {
+      StarTree starTree = starTreeV2.getStarTree();
+      _rootNode = starTree.getRoot();
+      _dimensionNames = starTree.getDimensionNames();
+      int numDimensions = _dimensionNames.size();
+      _dimensionDictionaries = new ArrayList<>(numDimensions);
+      for (String dimension : _dimensionNames) {
+        _dimensionDictionaries.add(starTreeV2.getDataSource(dimension).getDictionary());
       }
     }
-    //put ALL node at the end
-    if (allNode != null) {
-      json.addChild(allNode);
+
+    public void build(StarTreeJsonNode rootJsonNode) {
+      build(_rootNode, rootJsonNode);
     }
-    if (totalChildNodes > MAX_CHILDREN) {
-      Iterator<Pair<String, Integer>> qIterator = queue.iterator();
-      Set<String> topKDimensions = new HashSet<>();
-      topKDimensions.add("ALL");
-      while (qIterator.hasNext()) {
-        topKDimensions.add(qIterator.next().getKey());
+
+    @SuppressWarnings("UnstableApiUsage")
+    private long build(StarTreeNode treeNode, StarTreeJsonNode jsonNode) {
+      if (treeNode.isLeaf()) {
+        return 0;
       }
-      Iterator<StarTreeJsonNode> iterator = json.getChildren().iterator();
-      while (iterator.hasNext()) {
-        StarTreeJsonNode next = iterator.next();
-        if (!topKDimensions.contains(next.getName())) {
-          iterator.remove();
+
+      int childDimensionId = treeNode.getChildDimensionId();
+      String childDimension = _dimensionNames.get(childDimensionId);
+      Dictionary dictionary = _dimensionDictionaries.get(childDimensionId);
+      int numChildNodes = treeNode.getNumChildren();
+      long totalChildNodes = numChildNodes;
+
+      Comparator<Pair<String, Long>> comparator = (o1, o2) -> Long.compare(o2.getRight(), o1.getRight());
+      MinMaxPriorityQueue<Pair<String, Long>> queue =
+          MinMaxPriorityQueue.orderedBy(comparator).maximumSize(MAX_CHILDREN).create();
+      StarTreeJsonNode starJsonNode = null;
+
+      Iterator<? extends StarTreeNode> childrenIterator = treeNode.getChildrenIterator();
+      while (childrenIterator.hasNext()) {
+        StarTreeNode childTreeNode = childrenIterator.next();
+        int childDimensionValueId = childTreeNode.getDimensionValue();
+        String childDimensionValue =
+            childDimensionValueId != StarTreeNode.ALL ? dictionary.getStringValue(childDimensionValueId) : "ALL";
+        String childJsonNodeName = childDimension + ": " + childDimensionValue;
+        StarTreeJsonNode childJsonNode = new StarTreeJsonNode(childJsonNodeName);
+        long result = build(childTreeNode, childJsonNode);
+        totalChildNodes += result;
+        if (childDimensionValueId != StarTreeNode.ALL) {
+          jsonNode.addChild(childJsonNode);
+          queue.add(ImmutablePair.of(childJsonNodeName, result));
+        } else {
+          starJsonNode = childJsonNode;
         }
       }
+      if (numChildNodes > MAX_CHILDREN) {
+        // Keep the nodes with most child nodes
+        Set<String> nodeNamesToKeep = new HashSet<>();
+        for (Pair<String, Long> pair : queue) {
+          nodeNamesToKeep.add(pair.getKey());
+        }
+        jsonNode.getChildren().removeIf(next -> !nodeNamesToKeep.contains(next.getName()));
+      }
+      // Add star-node at last
+      if (starJsonNode != null) {
+        jsonNode.addChild(starJsonNode);
+      }
+      return totalChildNodes;
     }
-    return totalChildNodes;
-  }
-
-  public static void main(String[] args)
-      throws Exception {
-    if (args.length != 1) {
-      LOGGER.error("USAGE: StarIndexViewer <segmentDirectory>");
-      System.exit(1);
-    }
-    String segmentDir = args[0];
-    if (!new File(segmentDir).exists()) {
-      throw new FileNotFoundException("Missing directory:" + segmentDir);
-    }
-    new StarTreeIndexViewer(new File(segmentDir));
   }
 
   private static class StarTreeResource extends ResourceConfig {
@@ -189,15 +203,6 @@ public class StarTreeIndexViewer {
       StarTreeViewRestResource.json = json;
       register(resource.getClass());
     }
-  }
-
-  private void startServer(final File segmentDirectory, final String json)
-      throws Exception {
-    int httpPort = 8090;
-    URI baseUri = URI.create("http://0.0.0.0:" + Integer.toString(httpPort) + "/");
-    HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(baseUri, new StarTreeResource(json));
-
-    LOGGER.info("Go to http://{}:{}/  to view the star tree", "localhost", httpPort);
   }
 
   @Path("/")
