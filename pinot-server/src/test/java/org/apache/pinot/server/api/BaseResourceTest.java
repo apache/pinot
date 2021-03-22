2e09ef47056ff791bcd2e6e2fcd93b2ea6199776
Chang Liu <changliu@uber.com>
2021-03-21 21:27:43 -0700

    use single class import

--- pinot-server/src/test/java/org/apache/pinot/server/api/BaseResourceTest.java
+++ pinot-server/src/test/java/org/apache/pinot/server/api/BaseResourceTest.java
@@ -58,8 +58,11 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 public abstract class BaseResourceTest {
