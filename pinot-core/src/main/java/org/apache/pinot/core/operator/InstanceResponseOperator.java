94769f623e17eb2409b97823bc696ba9c53b2c44
Mingqiang Liang <miliang@linkedin.com>
2021-05-10 11:53:16 -0700

    extract the adjustment logic and add unit test

--- pinot-core/src/main/java/org/apache/pinot/core/operator/InstanceResponseOperator.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/InstanceResponseOperator.java
@@ -23,7 +23,6 @@ import org.apache.pinot.common.utils.DataTable.MetadataKey;
 import org.apache.pinot.core.common.Operator;
 import org.apache.pinot.core.operator.blocks.InstanceResponseBlock;
 import org.apache.pinot.core.operator.blocks.IntermediateResultsBlock;
-import org.apache.pinot.core.query.request.context.ThreadTimer;
 
 
 public class InstanceResponseOperator extends BaseOperator<InstanceResponseBlock> {
@@ -45,34 +44,41 @@ public class InstanceResponseOperator extends BaseOperator<InstanceResponseBlock
 
     long multipleThreadCpuTimeNs = intermediateResultsBlock.getExecutionThreadCpuTimeNs();
     long totalWallClockTimeNs = endWallClockTimeNs - startWallClockTimeNs;
-    /*
-     * System activities time such as OS paging, GC, context switching are not captured by
-     * totalThreadCpuTimeNs. For example, let's divide query processing into 4 phases.
-     * - phase 1: single thread preparing. Time used: T1
-     * - phase 2: N threads processing segments in parallel, each thread use time T2
-     * - phase 3: GC/OS paging. Time used: T3
-     * - phase 4: single thread merging intermediate results blocks. Time used: T4
-     *
-     * Then we have following equations:
-     * - singleThreadCpuTimeNs = T1 + T4
-     * - multipleThreadCpuTimeNs = T2 * N
-     * - totalWallClockTimeNs = T1 + T2 + T3 + T4 = singleThreadCpuTimeNs + T2 + T3
-     * - totalThreadCpuTimeNsWithoutSystemActivities = T1 + T2 * N + T4 = singleThreadCpuTimeNs + T2 * N
-     * - systemActivitiesTimeNs = T3 = (totalWallClockTimeNs - totalThreadCpuTimeNsWithoutSystemActivities) + T2 * (N - 1)
-     *
-     * Thus:
-     * totalThreadCpuTimeNsWithSystemActivities = totalThreadCpuTimeNsWithoutSystemActivities + systemActivitiesTimeNs
-     * = totalThreadCpuTimeNsWithoutSystemActivities + T3
-     * = totalThreadCpuTimeNsWithoutSystemActivities + (totalWallClockTimeNs - totalThreadCpuTimeNsWithoutSystemActivities) + T2 * (N - 1)
-     * = totalWallClockTimeNs + T2 * (N - 1)
-     * = totalWallClockTimeNs + (multipleThreadCpuTimeNs / N) * (N - 1)
-     */
+
     int numServerThreads = intermediateResultsBlock.getNumServerThreads();
-    long totalThreadCpuTimeNs = totalWallClockTimeNs + multipleThreadCpuTimeNs * (numServerThreads - 1) / numServerThreads;
+    long totalThreadCpuTimeNs =
+        calTotalThreadCpuTimeNs(totalWallClockTimeNs, multipleThreadCpuTimeNs, numServerThreads);
 
     dataTable.getMetadata().put(MetadataKey.THREAD_CPU_TIME_NS.getName(), String.valueOf(totalThreadCpuTimeNs));
 
     return instanceResponseBlock;
+  }
+
+  /*
+   * Calculate totalThreadCpuTimeNs based on totalWallClockTimeNs, multipleThreadCpuTimeNs, and numServerThreads.
+   * System activities time such as OS paging, GC, context switching are not captured by totalThreadCpuTimeNs.
+   * For example, let's divide query processing into 4 phases:
+   * - phase 1: single thread preparing. Time used: T1
+   * - phase 2: N threads processing segments in parallel, each thread use time T2
+   * - phase 3: GC/OS paging. Time used: T3
+   * - phase 4: single thread merging intermediate results blocks. Time used: T4
+   *
+   * Then we have following equations:
+   * - singleThreadCpuTimeNs = T1 + T4
+   * - multipleThreadCpuTimeNs = T2 * N
+   * - totalWallClockTimeNs = T1 + T2 + T3 + T4 = singleThreadCpuTimeNs + T2 + T3
+   * - totalThreadCpuTimeNsWithoutSystemActivities = T1 + T2 * N + T4 = singleThreadCpuTimeNs + T2 * N
+   * - systemActivitiesTimeNs = T3 = (totalWallClockTimeNs - totalThreadCpuTimeNsWithoutSystemActivities) + T2 * (N - 1)
+   *
+   * Thus:
+   * totalThreadCpuTimeNsWithSystemActivities = totalThreadCpuTimeNsWithoutSystemActivities + systemActivitiesTimeNs
+   * = totalThreadCpuTimeNsWithoutSystemActivities + T3
+   * = totalThreadCpuTimeNsWithoutSystemActivities + (totalWallClockTimeNs - totalThreadCpuTimeNsWithoutSystemActivities) + T2 * (N - 1)
+   * = totalWallClockTimeNs + T2 * (N - 1)
+   * = totalWallClockTimeNs + (multipleThreadCpuTimeNs / N) * (N - 1)
+   */
+  public static long calTotalThreadCpuTimeNs(long totalWallClockTimeNs, long multipleThreadCpuTimeNs, int numServerThreads) {
+    return totalWallClockTimeNs + multipleThreadCpuTimeNs * (numServerThreads - 1) / numServerThreads;
   }
 
   @Override
