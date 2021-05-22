8f93d189047c56c68f00205063a6a3785729c705
Qiaochu Liu <qiaochu@uber.com>
2021-05-21 18:17:09 -0700

    fix typo

--- pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/LLRealtimeSegmentDataManager.java
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/LLRealtimeSegmentDataManager.java
@@ -1260,11 +1260,9 @@ public class LLRealtimeSegmentDataManager extends RealtimeSegmentDataManager {
             .setAggregateMetrics(indexingConfig.isAggregateMetrics()).setNullHandlingEnabled(_nullHandlingEnabled)
             .setConsumerDir(consumerDir).setUpsertMode(tableConfig.getUpsertMode())
             .setPartitionUpsertMetadataManager(partitionUpsertMetadataManager)
-            .setPartitionUpsertMetadataManager(partitionUpsertMetadataManager)
             .setGlobalUpsertStrategy(tableConfig.getUpsertConfig().getGlobalUpsertStrategy())
             .setPartialUpsertStrategy(tableConfig.getUpsertConfig().getPartialUpsertStrategy())
             .setCustomUpsertStrategy(tableConfig.getUpsertConfig().getCustomUpsertStrategy());
-    ;
 
     // Create message decoder
     Set<String> fieldsToRead = IngestionUtils.getFieldsForRecordExtractor(_tableConfig.getIngestionConfig(), _schema);
