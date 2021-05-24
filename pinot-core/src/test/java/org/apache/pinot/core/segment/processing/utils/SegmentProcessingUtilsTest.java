3589715ae410f5d5a24cd77b64fa7dacc6a9c780
Laxman Ch <laxman@traceable.ai>
2021-05-24 11:18:06 +0530

    Fix test

--- pinot-core/src/test/java/org/apache/pinot/core/segment/processing/utils/SegmentProcessingUtilsTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/segment/processing/utils/SegmentProcessingUtilsTest.java
@@ -1,12 +1,27 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.core.segment.processing.utils;
 
 import com.google.common.collect.Lists;
-import java.io.IOException;
-import java.util.List;
 import org.apache.avro.Schema;
 import org.apache.avro.Schema.Field;
 import org.apache.avro.Schema.Type;
-import org.apache.avro.SchemaBuilder;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
 import org.apache.pinot.spi.data.FieldSpec.DataType;
@@ -24,8 +39,7 @@ public class SegmentProcessingUtilsTest {
         .addSingleValueDimension("nullableIntField", DataType.INT).build();
     _avroSchema = SegmentProcessorUtils.convertPinotSchemaToAvroSchema(_pinotSchema);
     final Field field = _avroSchema.getField("nullableIntField");
-    Assert.assertEquals(field.schema().getType(), Type.UNION);
-    Assert.assertEquals(field.schema().getTypes(), Lists.newArrayList(Type.NULL, Type.INT));
+    Assert.assertEquals(field.schema().getType(), Type.INT);
     GenericRecord record  = new GenericRecordBuilder(_avroSchema).set("nullableIntField", Integer.valueOf(0)).build();
   }
 
