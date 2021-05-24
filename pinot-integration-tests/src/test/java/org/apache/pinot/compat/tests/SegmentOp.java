958b4d4e0a50685a5a95a149144e9f4ac765e7cb
Subbu Subramaniam <mcvsubbu@users.noreply.github.com>
2021-05-24 11:01:01 -0700

    Enabling compatibility tests in the script (#6959)
    The actual run of compat tests were disahled in the script. This PR
    enables the compat tests.
    
    Fixed the verifier script to work with relative as well as absolute
    paths for working directory.
    
    Other improvements:
    We now preserve the log files of all processes we start.
    
    Changed all pid files to be in one dir so it is easy to stop the cluster
    
    Changed the mechanism to check if some service is up. This part needs more
    improvement.
    
    As yet, the tests fail after server upgrade

--- pinot-integration-tests/src/test/java/org/apache/pinot/compat/tests/SegmentOp.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/compat/tests/SegmentOp.java
@@ -65,8 +65,8 @@ import org.slf4j.LoggerFactory;
 public class SegmentOp extends BaseOp {
   private static final Logger LOGGER = LoggerFactory.getLogger(SegmentOp.class);
   private static final FileFormat DEFAULT_FILE_FORMAT = FileFormat.CSV;
-  private static final int DEFAULT_MAX_SLEEP_TIME_MS = 30000;
-  private static final int DEFAULT_SLEEP_INTERVAL_MS = 200;
+  private static final int DEFAULT_MAX_SLEEP_TIME_MS = 60000;
+  private static final int DEFAULT_SLEEP_INTERVAL_MS = 1000;
 
   public enum Op {
     UPLOAD,
