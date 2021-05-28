6fcccfc39c0179b9ff856d5ebc11098dbaaaccf1
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2021-05-27 23:10:15 -0700

    Add genericRow file reader and writer (#6997)
    Add file reader and writer for genericRow leveraging the ser/de classes
    Replace the usage of ser/de to the file reader/writer

--- pinot-core/src/main/java/org/apache/pinot/core/segment/processing/collector/ConcatCollector.java
+++ pinot-core/src/main/java/org/apache/pinot/core/segment/processing/collector/ConcatCollector.java
@@ -20,10 +20,7 @@ package org.apache.pinot.core.segment.processing.collector;
 
 import com.google.common.base.Preconditions;
 import it.unimi.dsi.fastutil.Arrays;
-import java.io.BufferedOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -31,9 +28,8 @@ import java.util.List;
 import javax.annotation.Nullable;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.FileUtils;
-import org.apache.pinot.core.segment.processing.serde.GenericRowDeserializer;
-import org.apache.pinot.core.segment.processing.serde.GenericRowSerializer;
-import org.apache.pinot.segment.spi.memory.PinotDataBuffer;
+import org.apache.pinot.core.segment.processing.genericrow.GenericRowFileReader;
+import org.apache.pinot.core.segment.processing.genericrow.GenericRowFileWriter;
 import org.apache.pinot.spi.data.FieldSpec;
 import org.apache.pinot.spi.data.FieldSpec.DataType;
 import org.apache.pinot.spi.data.Schema;
@@ -44,25 +40,19 @@ import org.apache.pinot.spi.data.readers.GenericRow;
  * A Collector implementation for collecting and concatenating all incoming rows.
  */
 public class ConcatCollector implements Collector {
-  private static final String RECORDS_FILE_NAME = "collector.records";
+  private static final String RECORD_OFFSET_FILE_NAME = "record.offset";
+  private static final String RECORD_DATA_FILE_NAME = "record.data";
 
   private final List<FieldSpec> _fieldSpecs = new ArrayList<>();
-  private final GenericRowSerializer _genericRowSerializer;
   private final int _numSortColumns;
   private final SortOrderComparator _sortOrderComparator;
   private final File _workingDir;
-  private final File _collectorRecordFile;
+  private final File _recordOffsetFile;
+  private final File _recordDataFile;
 
+  private GenericRowFileWriter _recordFileWriter;
+  private GenericRowFileReader _recordFileReader;
   private int _numDocs;
-
-  // TODO: Avoid using BufferedOutputStream, and use ByteBuffer directly.
-  //  However, ByteBuffer has a limitation that the size cannot exceed 2G.
-  //  There are no limits on the size of data inserted into the {@link Collector}.
-  //  Hence, would need to implement a hybrid approach or a trigger a flush when size exceeds on Collector.
-  private BufferedOutputStream _collectorRecordOutputStream;
-  private List<Long> _collectorRecordOffsets;
-  private PinotDataBuffer _collectorRecordBuffer;
-  private GenericRowDeserializer _genericRowDeserializer;
 
   public ConcatCollector(CollectorConfig collectorConfig, Schema schema) {
     List<String> sortOrder = collectorConfig.getSortOrder();
@@ -92,48 +82,33 @@ public class ConcatCollector implements Collector {
         }
       }
     }
-    // TODO: Pass 'includeNullFields' from the config
-    _genericRowSerializer = new GenericRowSerializer(_fieldSpecs, true);
 
     _workingDir =
         new File(FileUtils.getTempDirectory(), String.format("concat_collector_%d", System.currentTimeMillis()));
     Preconditions.checkState(_workingDir.mkdirs(), "Failed to create dir: %s for %s with config: %s",
         _workingDir.getAbsolutePath(), ConcatCollector.class.getSimpleName(), collectorConfig);
-    _collectorRecordFile = new File(_workingDir, RECORDS_FILE_NAME);
+    _recordOffsetFile = new File(_workingDir, RECORD_OFFSET_FILE_NAME);
+    _recordDataFile = new File(_workingDir, RECORD_DATA_FILE_NAME);
 
-    initializeBuffer();
-  }
-
-  private void initializeBuffer() {
-    Preconditions.checkState(!_collectorRecordFile.exists(),
-        "Collector record file: " + _collectorRecordFile + " already exists");
     try {
-      _collectorRecordOutputStream = new BufferedOutputStream(new FileOutputStream(_collectorRecordFile));
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
+      reset();
+    } catch (IOException e) {
+      throw new RuntimeException("Caught exception while resetting the collector", e);
     }
-    _collectorRecordOffsets = new ArrayList<>();
-    _collectorRecordOffsets.add(0L);
-    _numDocs = 0;
   }
 
   @Override
   public void collect(GenericRow genericRow)
       throws IOException {
-    byte[] genericRowBytes = _genericRowSerializer.serialize(genericRow);
-    _collectorRecordOutputStream.write(genericRowBytes);
-    _collectorRecordOffsets.add(_collectorRecordOffsets.get(_numDocs) + genericRowBytes.length);
+    _recordFileWriter.write(genericRow);
     _numDocs++;
   }
 
   @Override
   public Iterator<GenericRow> iterator()
       throws IOException {
-    _collectorRecordOutputStream.flush();
-    _collectorRecordBuffer = PinotDataBuffer
-        .mapFile(_collectorRecordFile, true, 0, _collectorRecordOffsets.get(_numDocs), PinotDataBuffer.NATIVE_ORDER,
-            "ConcatCollector: generic row buffer");
-    _genericRowDeserializer = new GenericRowDeserializer(_collectorRecordBuffer, _fieldSpecs, true);
+    _recordFileWriter.close();
+    _recordFileReader = new GenericRowFileReader(_recordOffsetFile, _recordDataFile, _fieldSpecs, true);
 
     // TODO: A lot of this code can be made common across Collectors, once {@link RollupCollector} is also converted to off heap implementation
     if (_numSortColumns != 0) {
@@ -142,12 +117,9 @@ public class ConcatCollector implements Collector {
         sortedDocIds[i] = i;
       }
 
-      Arrays.quickSort(0, _numDocs, (i1, i2) -> {
-        long startOffset1 = _collectorRecordOffsets.get(sortedDocIds[i1]);
-        long startOffset2 = _collectorRecordOffsets.get(sortedDocIds[i2]);
-        return _sortOrderComparator.compare(_genericRowDeserializer.partialDeserialize(startOffset1, _numSortColumns),
-            _genericRowDeserializer.partialDeserialize(startOffset2, _numSortColumns));
-      }, (i1, i2) -> {
+      Arrays.quickSort(0, _numDocs, (i1, i2) -> _sortOrderComparator
+          .compare(_recordFileReader.partialRead(sortedDocIds[i1], _numSortColumns),
+              _recordFileReader.partialRead(sortedDocIds[i2], _numSortColumns)), (i1, i2) -> {
         int temp = sortedDocIds[i1];
         sortedDocIds[i1] = sortedDocIds[i2];
         sortedDocIds[i2] = temp;
@@ -170,13 +142,8 @@ public class ConcatCollector implements Collector {
 
       @Override
       public GenericRow next() {
-        long offset;
-        if (sortedDocIds == null) {
-          offset = _collectorRecordOffsets.get(_nextDocId++);
-        } else {
-          offset = _collectorRecordOffsets.get(sortedDocIds[_nextDocId++]);
-        }
-        return _genericRowDeserializer.deserialize(offset, _reuse);
+        int docId = sortedDocIds != null ? sortedDocIds[_nextDocId++] : _nextDocId++;
+        return _recordFileReader.read(docId, _reuse);
       }
     };
   }
@@ -189,28 +156,27 @@ public class ConcatCollector implements Collector {
   @Override
   public void reset()
       throws IOException {
-    try {
-      if (_collectorRecordBuffer != null) {
-        _collectorRecordBuffer.close();
-      }
-      if (_collectorRecordOutputStream != null) {
-        _collectorRecordOutputStream.close();
-      }
-    } finally {
-      FileUtils.deleteQuietly(_collectorRecordFile);
+    if (_recordFileWriter != null) {
+      _recordFileWriter.close();
     }
-    initializeBuffer();
+    if (_recordFileReader != null) {
+      _recordFileReader.close();
+    }
+    FileUtils.cleanDirectory(_workingDir);
+    // TODO: Pass 'includeNullFields' from the config
+    _recordFileWriter = new GenericRowFileWriter(_recordOffsetFile, _recordDataFile, _fieldSpecs, true);
+    _numDocs = 0;
   }
 
   @Override
   public void close()
       throws IOException {
     try {
-      if (_collectorRecordBuffer != null) {
-        _collectorRecordBuffer.close();
+      if (_recordFileWriter != null) {
+        _recordFileWriter.close();
       }
-      if (_collectorRecordOutputStream != null) {
-        _collectorRecordOutputStream.close();
+      if (_recordFileReader != null) {
+        _recordFileReader.close();
       }
     } finally {
       FileUtils.deleteQuietly(_workingDir);
--- pinot-core/src/main/java/org/apache/pinot/core/segment/processing/genericrow/GenericRowDeserializer.java
+++ pinot-core/src/main/java/org/apache/pinot/core/segment/processing/genericrow/GenericRowDeserializer.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pinot.core.segment.processing.serde;
+package org.apache.pinot.core.segment.processing.genericrow;
 
 import com.google.common.base.Preconditions;
 import java.util.List;
