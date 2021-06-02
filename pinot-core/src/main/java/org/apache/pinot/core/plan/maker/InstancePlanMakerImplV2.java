bfb60654c6d3fb9463c19c8817f3bcdd2630ab4a
wuwenw <wuwenw@andrew.cmu.edu>
2021-06-02 12:02:00 -0400

    Revert "rebased"
    This reverts commit 116e74c5f59b08acc20d773fa55491d7c0260cce.

--- pinot-core/src/main/java/org/apache/pinot/core/plan/maker/InstancePlanMakerImplV2.java
+++ pinot-core/src/main/java/org/apache/pinot/core/plan/maker/InstancePlanMakerImplV2.java
@@ -51,6 +51,10 @@ import org.apache.pinot.segment.spi.index.reader.Dictionary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.apache.pinot.core.util.GroupByUtils.getTableCapacity;
+
+
+
 /**
  * The <code>InstancePlanMakerImplV2</code> class is the default implementation of {@link PlanMaker}.
  */
@@ -73,7 +77,7 @@ public class InstancePlanMakerImplV2 implements PlanMaker {
   // Used for SQL GROUP BY (server combine)
   private final int _groupByTrimThreshold;
   private final boolean _enableSegmentGroupTrim;
-  private final int _minSegmentTrimSize;
+  private int _minSegmentTrimSize;
 
   @VisibleForTesting
   public InstancePlanMakerImplV2() {
