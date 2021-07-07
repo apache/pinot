4fdf4f43b683d1e1532c7f3af3ffc28862c986c8
Ken Krugler <ken@transpac.com>
2021-07-07 10:13:46 -0700

    Add test for proper handling of Unicode characters in job yaml

--- pinot-spi/src/test/java/org/apache/pinot/spi/ingestion/batch/IngestionJobLauncherTest.java
+++ pinot-spi/src/test/java/org/apache/pinot/spi/ingestion/batch/IngestionJobLauncherTest.java
@@ -29,17 +29,26 @@ import org.testng.annotations.Test;
 
 public class IngestionJobLauncherTest {
 
-  @Test
-  public void testIngestionJobLauncherWithTemplate()
-      throws IOException, ClassNotFoundException {
-    Map<String, Object> context =
-        GroovyTemplateUtils.getTemplateContext(Arrays.asList("year=2020", "month=05", "day=06"));
-    SegmentGenerationJobSpec spec = IngestionJobLauncher.getSegmentGenerationJobSpec(
-        GroovyTemplateUtils.class.getClassLoader().getResource("ingestion_job_spec_template.yaml").getFile(), null,
-        context);
-    Assert.assertEquals(spec.getInputDirURI(), "file:///path/to/input/2020/05/06");
-    Assert.assertEquals(spec.getOutputDirURI(), "file:///path/to/output/2020/05/06");
-  }
+    @Test
+    public void testIngestionJobLauncherWithTemplate()
+        throws IOException, ClassNotFoundException {
+      Map<String, Object> context =
+          GroovyTemplateUtils.getTemplateContext(Arrays.asList("year=2020", "month=05", "day=06"));
+      SegmentGenerationJobSpec spec = IngestionJobLauncher.getSegmentGenerationJobSpec(
+          GroovyTemplateUtils.class.getClassLoader().getResource("ingestion_job_spec_template.yaml").getFile(), null,
+          context);
+      Assert.assertEquals(spec.getInputDirURI(), "file:///path/to/input/2020/05/06");
+      Assert.assertEquals(spec.getOutputDirURI(), "file:///path/to/output/2020/05/06");
+    }
+
+    @Test
+    public void testIngestionJobLauncherWithUnicodeCharForMultivalueFieldDelimiter()
+        throws IOException, ClassNotFoundException {
+      SegmentGenerationJobSpec spec = IngestionJobLauncher.getSegmentGenerationJobSpec(
+          GroovyTemplateUtils.class.getClassLoader().getResource("ingestion_job_spec_unicode.yaml").getFile(), null,
+          null);
+      Assert.assertEquals("\ufff0", spec.getRecordReaderSpec().getConfigs().get("multiValueDelimiter"));
+    }
 
   @Test
   public void testIngestionJobLauncherWithTemplateAndPropertyFile()
