feb1bc65520ba5f0530c8a3402148e7cb5794ed7
Mingqiang Liang <miliang@linkedin.com>
2021-07-12 13:36:04 -0700

    fix integration test

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
@@ -120,8 +120,8 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
   private static final String DISK_SIZE_IN_BYTES_KEY = "diskSizeInBytes";
   private static final String NUM_SEGMENTS_KEY = "numSegments";
   private static final String NUM_ROWS_KEY = "numRows";
-  private static final String COLUMN_AVG_LENGTH_MAP_KEY = "columnAvgLengthMap";
-  private static final String COLUMN_AVG_CARDINALITY_MAP_KEY = "columnAvgCardinalityMap";
+  private static final String COLUMN_LENGTH_MAP_KEY = "columnLengthMap";
+  private static final String COLUMN_CARDINALITY_MAP_KEY = "columnCardinalityMap";
   private static final int DISK_SIZE_IN_BYTES = 20270480;
   private static final int NUM_ROWS = 115545;
 
@@ -1645,31 +1645,31 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
     assertEquals(oneColumnResponse.get(DISK_SIZE_IN_BYTES_KEY).asInt(), DISK_SIZE_IN_BYTES);
     assertEquals(oneColumnResponse.get(NUM_SEGMENTS_KEY).asInt(), NUM_SEGMENTS);
     assertEquals(oneColumnResponse.get(NUM_ROWS_KEY).asInt(), NUM_ROWS);
-    assertEquals(oneColumnResponse.get(COLUMN_AVG_LENGTH_MAP_KEY).size(), 1);
-    assertEquals(oneColumnResponse.get(COLUMN_AVG_CARDINALITY_MAP_KEY).size(), 1);
+    assertEquals(oneColumnResponse.get(COLUMN_LENGTH_MAP_KEY).size(), 1);
+    assertEquals(oneColumnResponse.get(COLUMN_CARDINALITY_MAP_KEY).size(), 1);
 
     JsonNode threeColumnsResponse = JsonUtils.stringToJsonNode(sendGetRequest(_controllerBaseApiUrl
         + "/tables/mytable/metadata?columns=DivActualElapsedTime&columns=CRSElapsedTime&columns=OriginStateName"));
     assertEquals(threeColumnsResponse.get(DISK_SIZE_IN_BYTES_KEY).asInt(), DISK_SIZE_IN_BYTES);
     assertEquals(threeColumnsResponse.get(NUM_SEGMENTS_KEY).asInt(), NUM_SEGMENTS);
     assertEquals(threeColumnsResponse.get(NUM_ROWS_KEY).asInt(), NUM_ROWS);
-    assertEquals(threeColumnsResponse.get(COLUMN_AVG_LENGTH_MAP_KEY).size(), 3);
-    assertEquals(threeColumnsResponse.get(COLUMN_AVG_CARDINALITY_MAP_KEY).size(), 3);
+    assertEquals(threeColumnsResponse.get(COLUMN_LENGTH_MAP_KEY).size(), 3);
+    assertEquals(threeColumnsResponse.get(COLUMN_CARDINALITY_MAP_KEY).size(), 3);
 
     JsonNode zeroColumnResponse =
         JsonUtils.stringToJsonNode(sendGetRequest(_controllerBaseApiUrl + "/tables/mytable/metadata"));
     assertEquals(zeroColumnResponse.get(DISK_SIZE_IN_BYTES_KEY).asInt(), DISK_SIZE_IN_BYTES);
     assertEquals(zeroColumnResponse.get(NUM_SEGMENTS_KEY).asInt(), NUM_SEGMENTS);
     assertEquals(zeroColumnResponse.get(NUM_ROWS_KEY).asInt(), NUM_ROWS);
-    assertEquals(zeroColumnResponse.get(COLUMN_AVG_LENGTH_MAP_KEY).size(), 0);
-    assertEquals(zeroColumnResponse.get(COLUMN_AVG_CARDINALITY_MAP_KEY).size(), 0);
+    assertEquals(zeroColumnResponse.get(COLUMN_LENGTH_MAP_KEY).size(), 0);
+    assertEquals(zeroColumnResponse.get(COLUMN_CARDINALITY_MAP_KEY).size(), 0);
 
     JsonNode allColumnResponse =
         JsonUtils.stringToJsonNode(sendGetRequest(_controllerBaseApiUrl + "/tables/mytable/metadata?columns=*"));
     assertEquals(allColumnResponse.get(DISK_SIZE_IN_BYTES_KEY).asInt(), DISK_SIZE_IN_BYTES);
     assertEquals(allColumnResponse.get(NUM_SEGMENTS_KEY).asInt(), NUM_SEGMENTS);
     assertEquals(allColumnResponse.get(NUM_ROWS_KEY).asInt(), NUM_ROWS);
-    assertEquals(allColumnResponse.get(COLUMN_AVG_LENGTH_MAP_KEY).size(), 82);
-    assertEquals(allColumnResponse.get(COLUMN_AVG_CARDINALITY_MAP_KEY).size(), 82);
+    assertEquals(allColumnResponse.get(COLUMN_LENGTH_MAP_KEY).size(), 82);
+    assertEquals(allColumnResponse.get(COLUMN_CARDINALITY_MAP_KEY).size(), 82);
   }
 }
