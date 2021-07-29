60c847b063a46202be4a6f75a7a2bf89c383ca38
Xiaobing Li <xiaobing@startree.ai>
2021-07-29 10:55:11 -0700

    remove unused import

--- pinot-common/src/test/java/org/apache/pinot/common/utils/PinotDataTypeTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/utils/PinotDataTypeTest.java
@@ -26,7 +26,6 @@ import org.testng.annotations.Test;
 
 import static org.apache.pinot.common.utils.PinotDataType.*;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
