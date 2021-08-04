59e87efdd4792499bc6850a2bc8b1bee5aa4c3b3
Yupeng Fu <yupeng@uber.com>
2021-08-04 09:27:17 -0700

    fix ut

--- pinot-segment-local/src/test/java/org/apache/pinot/segment/local/utils/HashUtilsTest.java
+++ pinot-segment-local/src/test/java/org/apache/pinot/segment/local/utils/HashUtilsTest.java
@@ -2,6 +2,7 @@ package org.apache.pinot.segment.local.utils;
 
 import org.apache.pinot.spi.config.table.UpsertConfig;
 import org.apache.pinot.spi.data.readers.PrimaryKey;
+import org.apache.pinot.spi.utils.ByteArray;
 import org.apache.pinot.spi.utils.BytesUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -19,17 +20,19 @@ public class HashUtilsTest {
   @Test
   public void testHashPrimaryKey() {
     PrimaryKey pk = new PrimaryKey(new Object[]{"uuid-1", "uuid-2", "uuid-3"});
-    Assert.assertEquals(BytesUtils.toHexString((byte[]) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MD5)),
+    Assert.assertEquals(
+        BytesUtils.toHexString(((ByteArray) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MD5)).getBytes()),
         "58de44997505014e02982846a4d1cbbd");
-    Assert
-        .assertEquals(BytesUtils.toHexString((byte[]) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MURMUR3)),
-            "7e6b4a98296292a4012225fff037fa8c");
+    Assert.assertEquals(BytesUtils
+            .toHexString(((ByteArray) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MURMUR3)).getBytes()),
+        "7e6b4a98296292a4012225fff037fa8c");
     // reorder
     pk = new PrimaryKey(new Object[]{"uuid-3", "uuid-2", "uuid-1"});
-    Assert.assertEquals(BytesUtils.toHexString((byte[]) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MD5)),
+    Assert.assertEquals(
+        BytesUtils.toHexString(((ByteArray) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MD5)).getBytes()),
         "d2df12c6dea7b83f965613614eee58e2");
-    Assert
-        .assertEquals(BytesUtils.toHexString((byte[]) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MURMUR3)),
-            "8d68b314cc0c8de4dbd55f4dad3c3e66");
+    Assert.assertEquals(BytesUtils
+            .toHexString(((ByteArray) HashUtils.hashPrimaryKey(pk, UpsertConfig.HashFunction.MURMUR3)).getBytes()),
+        "8d68b314cc0c8de4dbd55f4dad3c3e66");
   }
 }
