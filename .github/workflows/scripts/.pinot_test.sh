89c6e39856aad9e7c2d9377ab9b94909f0c44b7e
Xiang Fu <xiangfu.1024@gmail.com>
2021-08-07 17:47:32 -0700

    Adding compatible tests

--- .github/workflows/scripts/.pinot_test.sh
+++ .github/workflows/scripts/.pinot_test.sh
@@ -27,21 +27,52 @@ netstat -i
 
 if [ "$RUN_INTEGRATION_TESTS" != false ]; then
   # Integration Tests
-  mvn install -DskipTests -am -B -pl 'pinot-integration-tests' -T 16
+  mvn install -DskipTests -am -B -pl 'pinot-integration-tests' -T 16 || exit 1
   if [ "$RUN_TEST_SET" == "1" ]; then
-    mvn test -am -B -pl 'pinot-integration-tests' -Dtest='C*Test,L*Test,M*Test,R*Test,S*Test' -P github-actions,integration-tests-only && exit 0 || exit 1
+    mvn test -am -B \
+        -pl 'pinot-integration-tests' \
+        -Dtest='C*Test,L*Test,M*Test,R*Test,S*Test' \
+        -P github-actions,integration-tests-only && exit 0 || exit 1
   fi
   if [ "$RUN_TEST_SET" == "2" ]; then
-    mvn test -am -B -pl 'pinot-integration-tests' -Dtest='!C*Test,!L*Test,!M*Test,!R*Test,!S*Test' -P github-actions,integration-tests-only && exit 0 || exit 1
+    mvn test -am -B \
+        -pl 'pinot-integration-tests' \
+        -Dtest='!C*Test,!L*Test,!M*Test,!R*Test,!S*Test' \
+        -P github-actions,integration-tests-only && exit 0 || exit 1
   fi
 else
   # Unit Tests
   if [ "$RUN_TEST_SET" == "1" ]; then
-    mvn install -DskipTests -am -B -pl 'pinot-core,pinot-segment-local' -T 16
-    mvn test -am -B -pl 'pinot-spi,pinot-segment-spi,pinot-common,pinot-segment-local,pinot-core' -P github-actions,no-integration-tests && exit 0 || exit 1
+    mvn install -am -B \
+        -pl 'pinot-spi' \
+        -pl 'pinot-segment-spi' \
+        -pl 'pinot-common' \
+        -pl 'pinot-segment-local' \
+        -pl 'pinot-core' \
+        -pl 'pinot-server' \
+        -pl ':pinot-yammer' \
+        -pl ':pinot-avro-base' \
+        -pl ':pinot-avro' \
+        -pl ':pinot-csv' \
+        -pl ':pinot-json' \
+        -pl ':pinot-segment-uploader-default' \
+        -P github-actions,no-integration-tests && exit 0 || exit 1
   fi
   if [ "$RUN_TEST_SET" == "2" ]; then
-    mvn install -DskipTests -T 16
-    mvn test -am -B -pl '!pinot-spi,!pinot-segment-spi,!pinot-common,!pinot-segment-local,!pinot-core' -P github-actions,no-integration-tests && exit 0 || exit 1
+    mvn install -DskipTests -T 16 || exit 1
+    mvn test -am -B \
+        -pl '!pinot-spi' \
+        -pl '!pinot-segment-spi' \
+        -pl '!pinot-common' \
+        -pl '!pinot-segment-local' \
+        -pl '!pinot-core' \
+        -pl '!pinot-server' \
+        -pl '!:pinot-yammer' \
+        -pl '!:pinot-avro-base' \
+        -pl '!:pinot-avro' \
+        -pl '!:pinot-csv' \
+        -pl '!:pinot-json' \
+        -pl '!:pinot-segment-uploader-default' \
+        -P github-actions,no-integration-tests && exit 0 || exit 1
   fi
 fi
--- compatibility-verifier/checkoutAndBuild.sh
+++ compatibility-verifier/checkoutAndBuild.sh
@@ -74,6 +74,21 @@ function build() {
   local repoOption=""
   local versionOption="-Djdk.version=8"
 
+  mkdir -p ${MVN_CACHE_DIR}
+  echo "<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\""> ../settings.xml
+  echo "      xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"">> ../settings.xml
+  echo "      xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0">> ../settings.xml
+  echo "                          https://maven.apache.org/xsd/settings-1.0.0.xsd\">">> ../settings.xml
+  echo "  <mirrors>">> ../settings.xml
+  echo "    <mirror>">> ../settings.xml
+  echo "      <id>confluent-mirror</id>">> ../settings.xml
+  echo "      <mirrorOf>confluent</mirrorOf>">> ../settings.xml
+  echo "      <url>https://packages.confluent.io/maven/</url>">> ../settings.xml
+  echo "      <blocked>false</blocked>">> ../settings.xml
+  echo "    </mirror>">> ../settings.xml
+  echo "  </mirrors>">> ../settings.xml
+  echo "</settings>">> ../settings.xml
+
   if [ ${buildId} -gt 0 ]; then
     # Build it in a different env under different version so that maven cache does
     # not collide
@@ -83,12 +98,12 @@ function build() {
     repoOption="-Dmaven.repo.local=${mvnCache}/${buildId}"
   fi
 
-  mvn install package -DskipTests -Pbin-dist ${versionOption} ${repoOption} ${PINOT_MAVEN_OPTS} 1>${outFile} 2>&1
+  mvn install package -s ../settings.xml -DskipTests -Pbin-dist ${versionOption} ${repoOption} ${PINOT_MAVEN_OPTS} 1>${outFile} 2>&1
   if [ $? -ne 0 ]; then exit 1; fi
-  mvn -pl pinot-tools package -DskipTests ${versionOption} ${repoOption} ${PINOT_MAVEN_OPTS} 1>>${outFile} 2>&1
+  mvn -pl pinot-tools package -s ../settings.xml -DskipTests ${versionOption} ${repoOption} ${PINOT_MAVEN_OPTS} 1>>${outFile} 2>&1
   if [ $? -ne 0 ]; then exit 1; fi
   if [ $buildTests -eq 1 ]; then
-    mvn -pl pinot-integration-tests package -DskipTests ${versionOption} ${repoOption} ${PINOT_MAVEN_OPTS} 1>>${outFile} 2>&1
+    mvn -pl pinot-integration-tests package -s ../settings.xml -DskipTests ${versionOption} ${repoOption} ${PINOT_MAVEN_OPTS} 1>>${outFile} 2>&1
     if [ $? -ne 0 ]; then exit 1; fi
   fi
 }
@@ -259,6 +274,7 @@ if [ ${oldBuildStatus} -eq 0 ]; then
   echo Old version build completed successfully
 else
   echo Old version build failed. See ${oldBuildOutFile}
+  cat ${oldBuildOutFile}
   exitStatus=1
 fi
 
@@ -266,6 +282,7 @@ if [ ${curBuildStatus} -eq 0 ]; then
   echo Compat checker build completed successfully
 else
   echo Compat checker build failed. See ${curBuildOutFile}
+  cat ${curBuildOutFile}
 fi
 
 if [ ${buildNewTarget} -eq 1 ]; then
@@ -273,6 +290,7 @@ if [ ${buildNewTarget} -eq 1 ]; then
     echo New version build completed successfully
   else
     echo New version build failed. See ${newBuildOutFile}
+    cat ${newBuildOutFile}
     exitStatus=1
   fi
 fi
