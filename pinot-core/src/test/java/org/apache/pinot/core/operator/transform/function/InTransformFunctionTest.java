4942bc7546ba37afee02b73f2fe345bb9d3ac5e3
Xiaotian (Jackie) Jiang <jackie.jxt@gmail.com>
2021-10-09 15:24:20 -0700

    Improve tests

--- pinot-core/src/test/java/org/apache/pinot/core/operator/transform/function/InTransformFunctionTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/operator/transform/function/InTransformFunctionTest.java
@@ -18,9 +18,13 @@
  */
 package org.apache.pinot.core.operator.transform.function;
 
+import com.google.common.collect.Sets;
+import java.util.Set;
 import org.apache.pinot.common.function.TransformFunctionType;
 import org.apache.pinot.common.request.context.ExpressionContext;
 import org.apache.pinot.common.request.context.RequestContextUtils;
+import org.apache.pinot.spi.utils.ByteArray;
+import org.apache.pinot.spi.utils.BytesUtils;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -28,39 +32,44 @@ import static org.testng.Assert.assertTrue;
 
 
 public class InTransformFunctionTest extends BaseTransformFunctionTest {
-  protected static final String STRING_SV_COLUMN = "stringSV";
 
   @Test
   public void testIntInTransformFunction() {
-    String expressionStr = String.format("%s in(1,2,9,5)", INT_SV_COLUMN);
+    String expressionStr =
+        String.format("%s IN (%d, %d, %d)", INT_SV_COLUMN, _intSVValues[2], _intSVValues[5], _intSVValues[9]);
     ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
     TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
     assertTrue(transformFunction instanceof InTransformFunction);
     assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
 
+    Set<Integer> inValues = Sets.newHashSet(_intSVValues[2], _intSVValues[5], _intSVValues[9]);
     int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
     for (int i = 0; i < NUM_ROWS; i++) {
-      int expected = 0;
-      if (_intSVValues[i] == 1 || _intSVValues[i] == 2 || _intSVValues[i] == 9 || _intSVValues[i] == 5) {
-        expected = 1;
+      if (i == 2 || i == 5 || i == 9) {
+        assertEquals(intValues[i], 1);
       }
-      assertEquals(intValues[i], expected);
+      assertEquals(intValues[i], inValues.contains(_intSVValues[i]) ? 1 : 0);
     }
   }
 
   @Test
   public void testIntMVInTransformFunction() {
-    String expressionStr = String.format("%s in(1,2,9,5)", INT_MV_COLUMN);
+    String expressionStr =
+        String.format("%s IN (%d, %d, %d)", INT_MV_COLUMN, _intMVValues[2][0], _intMVValues[5][0], _intMVValues[9][0]);
     ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
     TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
     assertTrue(transformFunction instanceof InTransformFunction);
     assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
 
+    Set<Integer> inValues = Sets.newHashSet(_intMVValues[2][0], _intMVValues[5][0], _intMVValues[9][0]);
     int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
     for (int i = 0; i < NUM_ROWS; i++) {
+      if (i == 2 || i == 5 || i == 9) {
+        assertEquals(intValues[i], 1);
+      }
       int expected = 0;
       for (int intValue : _intMVValues[i]) {
-        if (intValue == 1 || intValue == 2 || intValue == 9 || intValue == 5) {
+        if (inValues.contains(intValue)) {
           expected = 1;
           break;
         }
@@ -71,77 +80,115 @@ public class InTransformFunctionTest extends BaseTransformFunctionTest {
 
   @Test
   public void testIntInTransformFunctionWithTransformedValues() {
-    String expressionStr = String.format("%s in(1,1+1,4+5,2+3)", INT_SV_COLUMN);
+    String expressionStr = String.format("%s IN (%d, 1+1, 4+5)", INT_SV_COLUMN, _intSVValues[2]);
     ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
     TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
     assertTrue(transformFunction instanceof InTransformFunction);
     assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
 
+    Set<Integer> inValues = Sets.newHashSet(_intSVValues[2], 2, 9);
     int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
-
     for (int i = 0; i < NUM_ROWS; i++) {
-      int expected = 0;
-      if (_intSVValues[i] == 1 || _intSVValues[i] == 2 || _intSVValues[i] == 9 || _intSVValues[i] == 5) {
-        expected = 1;
+      if (i == 2) {
+        assertEquals(intValues[i], 1);
       }
-      assertEquals(intValues[i], expected);
+      assertEquals(intValues[i], inValues.contains(_intSVValues[i]) ? 1 : 0);
     }
   }
 
   @Test
   public void testLongInTransformFunction() {
     String expressionStr =
-        String.format("%s in(%d,%d, %d)", LONG_SV_COLUMN, _longSVValues[2], _longSVValues[7], _longSVValues[11]);
+        String.format("%s IN (%d, %d, %d)", LONG_SV_COLUMN, _longSVValues[2], _longSVValues[7], _longSVValues[11]);
     ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
     TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
     assertTrue(transformFunction instanceof InTransformFunction);
     assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
 
+    Set<Long> inValues = Sets.newHashSet(_longSVValues[2], _longSVValues[7], _longSVValues[11]);
     int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
     for (int i = 0; i < NUM_ROWS; i++) {
-      int expected = 0;
       if (i == 2 || i == 7 || i == 11) {
-        expected = 1;
+        assertEquals(intValues[i], 1);
       }
-      assertEquals(intValues[i], expected);
+      assertEquals(intValues[i], inValues.contains(_longSVValues[i]) ? 1 : 0);
     }
   }
 
   @Test
   public void testFloatInTransformFunction() {
     String expressionStr =
-        String.format("%s in(%f,%f, %f)", FLOAT_SV_COLUMN, _floatSVValues[3], _floatSVValues[7], _floatSVValues[9]);
+        String.format("%s IN (%s, %s, %s)", FLOAT_SV_COLUMN, _floatSVValues[3], _floatSVValues[7], _floatSVValues[9]);
     ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
     TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
     assertTrue(transformFunction instanceof InTransformFunction);
     assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
 
+    Set<Float> inValues = Sets.newHashSet(_floatSVValues[3], _floatSVValues[7], _floatSVValues[9]);
     int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
     for (int i = 0; i < NUM_ROWS; i++) {
-      int expected = 0;
       if (i == 3 || i == 7 || i == 9) {
-        expected = 1;
+        assertEquals(intValues[i], 1);
       }
-      assertEquals(intValues[i], expected);
+      assertEquals(intValues[i], inValues.contains(_floatSVValues[i]) ? 1 : 0);
+    }
+  }
+
+  @Test
+  public void testDoubleInTransformFunction() {
+    String expressionStr = String.format("%s IN (%s, %s, %s)", DOUBLE_SV_COLUMN, _doubleSVValues[3], _doubleSVValues[7],
+        _doubleSVValues[9]);
+    ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
+    TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
+    assertTrue(transformFunction instanceof InTransformFunction);
+    assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
+
+    Set<Double> inValues = Sets.newHashSet(_doubleSVValues[3], _doubleSVValues[7], _doubleSVValues[9]);
+    int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
+    for (int i = 0; i < NUM_ROWS; i++) {
+      if (i == 3 || i == 7 || i == 9) {
+        assertEquals(intValues[i], 1);
+      }
+      assertEquals(intValues[i], inValues.contains(_doubleSVValues[i]) ? 1 : 0);
     }
   }
 
   @Test
   public void testStringInTransformFunction() {
     String expressionStr =
-        String.format("%s in('a','b','%s','%s')", STRING_SV_COLUMN, _stringSVValues[2], _stringSVValues[5]);
+        String.format("%s IN ('a','b','%s','%s')", STRING_SV_COLUMN, _stringSVValues[2], _stringSVValues[5]);
     ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
     TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
     assertTrue(transformFunction instanceof InTransformFunction);
     assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
 
+    Set<String> inValues = Sets.newHashSet("a", "b", _stringSVValues[2], _stringSVValues[5]);
     int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
     for (int i = 0; i < NUM_ROWS; i++) {
-      int expected = 0;
       if (i == 2 || i == 5) {
-        expected = 1;
+        assertEquals(intValues[i], 1);
       }
-      assertEquals(intValues[i], expected);
+      assertEquals(intValues[i], inValues.contains(_stringSVValues[i]) ? 1 : 0);
+    }
+  }
+
+  @Test
+  public void testBytesInTransformFunction() {
+    String expressionStr =
+        String.format("%s IN ('%s','%s')", BYTES_SV_COLUMN, BytesUtils.toHexString(_bytesSVValues[2]),
+            BytesUtils.toHexString(_bytesSVValues[5]));
+    ExpressionContext expression = RequestContextUtils.getExpressionFromSQL(expressionStr);
+    TransformFunction transformFunction = TransformFunctionFactory.get(expression, _dataSourceMap);
+    assertTrue(transformFunction instanceof InTransformFunction);
+    assertEquals(transformFunction.getName(), TransformFunctionType.IN.getName());
+
+    Set<ByteArray> inValues = Sets.newHashSet(new ByteArray(_bytesSVValues[2]), new ByteArray(_bytesSVValues[5]));
+    int[] intValues = transformFunction.transformToIntValuesSV(_projectionBlock);
+    for (int i = 0; i < NUM_ROWS; i++) {
+      if (i == 2 || i == 5) {
+        assertEquals(intValues[i], 1);
+      }
+      assertEquals(intValues[i], inValues.contains(new ByteArray(_bytesSVValues[i])) ? 1 : 0);
     }
   }
 }
