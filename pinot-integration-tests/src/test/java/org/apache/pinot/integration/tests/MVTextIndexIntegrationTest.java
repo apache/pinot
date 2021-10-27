2df152283da6e68b0480fc8dac852f3a7bbcc87f
Atri Sharma <atri@apache.org>
2021-10-27 13:34:23 +0530

    Update the column name

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/MVTextIndexIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/MVTextIndexIntegrationTest.java
@@ -4,15 +4,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.util.Collections;
 import java.util.List;
 import org.apache.pinot.spi.config.table.FieldConfig;
-import org.apache.pinot.util.TestUtils;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
-import static org.testng.AssertJUnit.fail;
 
 
 public class MVTextIndexIntegrationTest extends OfflineClusterIntegrationTest {
-  private static final String TEXT_COLUMN_NAME = "skills";
+  private static final String TEXT_COLUMN_NAME = "DivAirports";
 
   private static final String TEST_TEXT_COLUMN_QUERY =
       "SELECT COUNT(*) FROM mytable GROUP BY arrayLength(valueIn(DivAirports,'DFW','ORD'))";
@@ -21,8 +17,6 @@ public class MVTextIndexIntegrationTest extends OfflineClusterIntegrationTest {
   protected List<FieldConfig> getFieldConfigs() {
     return Collections.singletonList(
         new FieldConfig(TEXT_COLUMN_NAME, FieldConfig.EncodingType.RAW, FieldConfig.IndexType.TEXT, null, null));
-
-    //return null;
   }
 
   @Test
