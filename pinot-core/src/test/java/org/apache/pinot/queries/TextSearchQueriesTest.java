a997e14f66a0e98b3e673a418f6465886a84696f
Atri Sharma <atri@apache.org>
2021-10-29 13:45:34 +0530

    Fix checkstyle

--- pinot-core/src/test/java/org/apache/pinot/queries/TextSearchQueriesTest.java
+++ pinot-core/src/test/java/org/apache/pinot/queries/TextSearchQueriesTest.java
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
