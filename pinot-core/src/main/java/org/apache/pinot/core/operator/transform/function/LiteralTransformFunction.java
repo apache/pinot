96266d62c5f9ab9a6ee66b34fe33fb364a960dc0
richardstartin <richard@startree.ai>
2021-11-05 11:31:15 +0000

    lighter weight LiteralTransformFunction, avoid excessive array fills in complex case statements

--- pinot-core/src/main/java/org/apache/pinot/core/operator/transform/function/LiteralTransformFunction.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/transform/function/LiteralTransformFunction.java
@@ -27,7 +27,6 @@ import java.util.Map;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.pinot.core.operator.blocks.ProjectionBlock;
 import org.apache.pinot.core.operator.transform.TransformResultMetadata;
-import org.apache.pinot.core.plan.DocIdSetPlanNode;
 import org.apache.pinot.segment.spi.datasource.DataSource;
 import org.apache.pinot.segment.spi.index.reader.Dictionary;
 import org.apache.pinot.spi.data.FieldSpec.DataType;
@@ -43,6 +42,7 @@ public class LiteralTransformFunction implements TransformFunction {
   private final String _literal;
   private final DataType _dataType;
 
+  // literals may be shared but values are intentionally not volatile as assignment races are benign
   private int[] _intResult;
   private long[] _longResult;
   private float[] _floatResult;
@@ -128,64 +128,82 @@ public class LiteralTransformFunction implements TransformFunction {
 
   @Override
   public int[] transformToIntValuesSV(ProjectionBlock projectionBlock) {
-    if (_intResult == null) {
-      _intResult = new int[DocIdSetPlanNode.MAX_DOC_PER_CALL];
+    int numDocs = projectionBlock.getNumDocs();
+    int[] intResult = _intResult;
+    if (intResult == null || intResult.length < numDocs) {
+      intResult = new int[numDocs];
       if (_dataType != DataType.BOOLEAN) {
-        Arrays.fill(_intResult, new BigDecimal(_literal).intValue());
+        Arrays.fill(intResult, new BigDecimal(_literal).intValue());
       } else {
-        Arrays.fill(_intResult, _literal.equals("true") ? 1 : 0);
+        Arrays.fill(intResult, _literal.equals("true") ? 1 : 0);
       }
+      _intResult = intResult;
     }
-    return _intResult;
+    return intResult;
   }
 
   @Override
   public long[] transformToLongValuesSV(ProjectionBlock projectionBlock) {
-    if (_longResult == null) {
-      _longResult = new long[DocIdSetPlanNode.MAX_DOC_PER_CALL];
+    int numDocs = projectionBlock.getNumDocs();
+    long[] longResult = _longResult;
+    if (longResult == null || longResult.length < numDocs) {
+      longResult = new long[numDocs];
       if (_dataType != DataType.TIMESTAMP) {
-        Arrays.fill(_longResult, new BigDecimal(_literal).longValue());
+        Arrays.fill(longResult, new BigDecimal(_literal).longValue());
       } else {
-        Arrays.fill(_longResult, Timestamp.valueOf(_literal).getTime());
+        Arrays.fill(longResult, Timestamp.valueOf(_literal).getTime());
       }
+      _longResult = longResult;
     }
-    return _longResult;
+    return longResult;
   }
 
   @Override
   public float[] transformToFloatValuesSV(ProjectionBlock projectionBlock) {
-    if (_floatResult == null) {
-      _floatResult = new float[DocIdSetPlanNode.MAX_DOC_PER_CALL];
-      Arrays.fill(_floatResult, new BigDecimal(_literal).floatValue());
+    int numDocs = projectionBlock.getNumDocs();
+    float[] floatResult = _floatResult;
+    if (floatResult == null || floatResult.length < numDocs) {
+      floatResult = new float[numDocs];
+      Arrays.fill(floatResult, new BigDecimal(_literal).floatValue());
+      _floatResult = floatResult;
     }
-    return _floatResult;
+    return floatResult;
   }
 
   @Override
   public double[] transformToDoubleValuesSV(ProjectionBlock projectionBlock) {
-    if (_doubleResult == null) {
-      _doubleResult = new double[DocIdSetPlanNode.MAX_DOC_PER_CALL];
-      Arrays.fill(_doubleResult, new BigDecimal(_literal).doubleValue());
+    int numDocs = projectionBlock.getNumDocs();
+    double[] doubleResult = _doubleResult;
+    if (doubleResult == null || doubleResult.length < numDocs) {
+      doubleResult = new double[numDocs];
+      Arrays.fill(doubleResult, new BigDecimal(_literal).doubleValue());
+      _doubleResult = doubleResult;
     }
     return _doubleResult;
   }
 
   @Override
   public String[] transformToStringValuesSV(ProjectionBlock projectionBlock) {
-    if (_stringResult == null) {
-      _stringResult = new String[DocIdSetPlanNode.MAX_DOC_PER_CALL];
-      Arrays.fill(_stringResult, _literal);
+    int numDocs = projectionBlock.getNumDocs();
+    String[] stringResult = _stringResult;
+    if (stringResult == null || stringResult.length < numDocs) {
+      stringResult = new String[numDocs];
+      Arrays.fill(stringResult, _literal);
+      _stringResult = stringResult;
     }
-    return _stringResult;
+    return stringResult;
   }
 
   @Override
   public byte[][] transformToBytesValuesSV(ProjectionBlock projectionBlock) {
-    if (_bytesResult == null) {
-      _bytesResult = new byte[DocIdSetPlanNode.MAX_DOC_PER_CALL][];
-      Arrays.fill(_bytesResult, BytesUtils.toBytes(_literal));
+    int numDocs = projectionBlock.getNumDocs();
+    byte[][] bytesResult = _bytesResult;
+    if (bytesResult == null || bytesResult.length < numDocs) {
+      bytesResult = new byte[numDocs][];
+      Arrays.fill(bytesResult, BytesUtils.toBytes(_literal));
+      _bytesResult = bytesResult;
     }
-    return _bytesResult;
+    return bytesResult;
   }
 
   @Override
