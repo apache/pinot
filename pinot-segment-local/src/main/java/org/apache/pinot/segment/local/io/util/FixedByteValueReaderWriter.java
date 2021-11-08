bdb612b7f55d34fb8b7f04d0950d0fbb0ec36e32
Richard Startin <richard@startree.ai>
2021-11-08 19:23:49 +0000

    optimise getUnpaddedString with SWAR padding search (#7708)
    This method was the hottest method on the pinot server in a string-heavy load test

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/io/util/FixedByteValueReaderWriter.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/io/util/FixedByteValueReaderWriter.java
@@ -18,6 +18,8 @@
  */
 package org.apache.pinot.segment.local.io.util;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
 import org.apache.pinot.segment.spi.memory.PinotDataBuffer;
 
@@ -54,16 +56,41 @@ public final class FixedByteValueReaderWriter implements ValueReader {
   @Override
   public String getUnpaddedString(int index, int numBytesPerValue, byte paddingByte, byte[] buffer) {
     assert buffer.length >= numBytesPerValue;
-
     long startOffset = (long) index * numBytesPerValue;
-    for (int i = 0; i < numBytesPerValue; i++) {
-      byte currentByte = _dataBuffer.getByte(startOffset + i);
-      if (currentByte == paddingByte) {
-        return new String(buffer, 0, i, UTF_8);
-      }
-      buffer[i] = currentByte;
+    long pattern = (paddingByte & 0xFFL) * 0x101010101010101L;
+    ByteBuffer wrapper = ByteBuffer.wrap(buffer);
+    if (_dataBuffer.order() == ByteOrder.LITTLE_ENDIAN) {
+      wrapper.order(ByteOrder.LITTLE_ENDIAN);
     }
-    return new String(buffer, 0, numBytesPerValue, UTF_8);
+    int position = 0;
+    for (int i = 0; i < ((numBytesPerValue >>> 3) << 3); i += 8) {
+      long word = _dataBuffer.getLong(startOffset + i);
+      wrapper.putLong(i, word);
+      long zeroed = word ^ pattern;
+      long tmp = (zeroed & 0x7F7F7F7F7F7F7F7FL) + 0x7F7F7F7F7F7F7F7FL;
+      tmp = ~(tmp | zeroed | 0x7F7F7F7F7F7F7F7FL);
+      if (tmp == 0) {
+        position += 8;
+      } else {
+        position += _dataBuffer.order() == ByteOrder.LITTLE_ENDIAN
+            ? Long.numberOfTrailingZeros(tmp) >>> 3
+            : Long.numberOfLeadingZeros(tmp) >>> 3;
+        return new String(buffer, 0, position, UTF_8);
+      }
+    }
+    return getUnpaddedStringTail(startOffset, position, numBytesPerValue, paddingByte, buffer);
+  }
+
+  private String getUnpaddedStringTail(long startOffset, int position, int numBytesPerValue, byte paddingByte,
+      byte[] buffer) {
+    for (; position < numBytesPerValue; position++) {
+      byte b = _dataBuffer.getByte(startOffset + position);
+      if (b == paddingByte) {
+        break;
+      }
+      buffer[position] = b;
+    }
+    return new String(buffer, 0, position, UTF_8);
   }
 
   @Override
