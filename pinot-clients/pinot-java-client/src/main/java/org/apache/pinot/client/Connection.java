f0a8af6511f2c8421ee084b1ddc439770f50380d
kbastani <kb@socialmoon.com>
2021-11-17 12:14:50 -0500

    Fixing some style issues

--- pinot-clients/pinot-java-client/src/main/java/org/apache/pinot/client/Connection.java
+++ pinot-clients/pinot-java-client/src/main/java/org/apache/pinot/client/Connection.java
@@ -18,14 +18,13 @@
  */
 package org.apache.pinot.client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
