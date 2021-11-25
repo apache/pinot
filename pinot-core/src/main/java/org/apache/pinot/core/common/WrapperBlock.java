ee3a862fe947062e8ed9e72524f0180d9f69e9fc
Atri Sharma <atri.jiit@gmail.com>
2021-11-25 14:35:13 +0530

    More cleanup

--- pinot-core/src/main/java/org/apache/pinot/core/common/WrapperBlock.java
+++
@@ -1,7 +0,0 @@
-package org.apache.pinot.core.common;
-
-import org.apache.pinot.core.operator.blocks.TransformBlock;
-
-public interface WrapperBlock<T> extends Block {
-    TransformBlock getTransformBlock(T key);
-}
