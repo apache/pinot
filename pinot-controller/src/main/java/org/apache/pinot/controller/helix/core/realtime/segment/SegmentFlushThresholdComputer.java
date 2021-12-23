55116aae706366ccfcc234a3abbe68fc1427006f
Mark Needham <m.h.needham@gmail.com>
2021-12-23 10:57:07 +0000

    add license headers

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/realtime/segment/SegmentFlushThresholdComputer.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/realtime/segment/SegmentFlushThresholdComputer.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.controller.helix.core.realtime.segment;
 
 import java.util.List;
@@ -116,9 +134,8 @@ public class SegmentFlushThresholdComputer {
     // .getSizeThresholdToFlushSegment(),
     // we might end up using a lot more memory than required for the segment Using a minor bump strategy, until
     // we add feature to adjust time We will only slightly bump the threshold based on numRowsConsumed
-    final long timeThresholdMillis = streamConfig.getFlushThresholdTimeMillis();
     if (numRowsConsumed < numRowsThreshold && committingSegmentSizeBytes < desiredSegmentSizeBytes) {
-      long targetSegmentNumRows;
+      final long timeThresholdMillis = streamConfig.getFlushThresholdTimeMillis();
       long currentNumRows = numRowsConsumed;
       StringBuilder logStringBuilder = new StringBuilder().append("Time threshold reached. ");
       if (timeThresholdMillis < timeConsumed) {
@@ -128,12 +145,10 @@ public class SegmentFlushThresholdComputer {
         logStringBuilder.append(" Detected lower time threshold, adjusting numRowsConsumed to ").append(currentNumRows)
             .append(". ");
       }
-      targetSegmentNumRows =
-          (long) (currentNumRows * ROWS_MULTIPLIER_WHEN_TIME_THRESHOLD_HIT);
+      long targetSegmentNumRows = (long) (currentNumRows * ROWS_MULTIPLIER_WHEN_TIME_THRESHOLD_HIT);
       targetSegmentNumRows = capNumRowsIfOverflow(targetSegmentNumRows);
       logStringBuilder.append("Setting segment size for {} as {}");
-      LOGGER.info(logStringBuilder.toString(), newSegmentName,
-          targetSegmentNumRows);
+      LOGGER.info(logStringBuilder.toString(), newSegmentName, targetSegmentNumRows);
       return (int) targetSegmentNumRows;
     }
 
