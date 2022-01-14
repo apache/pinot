6e5bff16201c3cb65244ca4560c5019e0fe3bb84
Mayank Shrivastava <mayanks@apache.org>
2022-01-14 10:19:02 -0800

    Cleanup: Remove remaining occurrences of `incubator`.
    Removed occurrences of `incubator` from the code base.
    - Was mostly used in GH links to Pinot landing page, or issues, etc.
    - This links were still working today as `incubator-pinot` gets redirected to `pinot`.

--- pinot-server/src/main/java/org/apache/pinot/server/starter/helix/AdminApiApplication.java
+++ pinot-server/src/main/java/org/apache/pinot/server/starter/helix/AdminApiApplication.java
@@ -115,7 +115,7 @@ public class AdminApiApplication extends ResourceConfig {
     BeanConfig beanConfig = new BeanConfig();
     beanConfig.setTitle("Pinot Server API");
     beanConfig.setDescription("APIs for accessing Pinot server information");
-    beanConfig.setContact("https://github.com/apache/incubator-pinot");
+    beanConfig.setContact("https://github.com/apache/pinot");
     beanConfig.setVersion("1.0");
     beanConfig.setSchemes(new String[]{CommonConstants.HTTP_PROTOCOL, CommonConstants.HTTPS_PROTOCOL});
     beanConfig.setBasePath("/");
