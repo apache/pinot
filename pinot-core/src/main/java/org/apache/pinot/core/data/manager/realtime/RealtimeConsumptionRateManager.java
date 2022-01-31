513a7b46ed18d84bbb90ac13f30a2ed5adc0719b
Sajjad Moradi <moradi.sajjad@gmail.com>
2022-01-31 10:23:39 -0800

    Add consumption rate limiter for LLConsumer (#6291)
    Having a consumption rate limiter for a table is beneficial in two ways:
    
    1) Currently offline tables can be configured to have storage quota. The storage quota for tables safegaurds the pinot cluster hosting those tables in case of excessive uploads of offline segments so that the cluster won't be under provisioned. Consumption rate limiter can play similar role for hybrid tables and realtime-only tables as it limits the amount of data being ingested into the realtime tables.
    
    2) Bursty pattern of incoming Kafka events introduces long time GC pauses on Pinot server. This behavior has been observed in some tables in LinkedIn. It's also been reported in this issue: Real time server query performance hit when catching up Kafka backlogs #6138.
    
    This PR introduces rate limit on Kafka consumption which can be configured in StreamConfig. The consumption rate limit parameter is defined for the whole topic and the rate limit for each partition will be simply topic rate limit divided by the partition count of that topic.

---
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/RealtimeConsumptionRateManager.java
@@ -0,0 +1,134 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.pinot.core.data.manager.realtime;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.RateLimiter;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import org.apache.pinot.spi.stream.StreamConfig;
+import org.apache.pinot.spi.stream.StreamConsumerFactory;
+import org.apache.pinot.spi.stream.StreamConsumerFactoryProvider;
+import org.apache.pinot.spi.stream.StreamMetadataProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * This class is responsible for creating realtime consumption rate limiters. The rate limit, specified in
+ * StreamConfig of table config, is for the entire topic. The effective rate limit for each partition is simply the
+ * specified rate limit divided by the partition count.
+ * This class leverages a cache for storing partition count for different topics as retrieving partition count from
+ * stream is a bit expensive and also the same count will be used of all partition consumers of the same topic.
+ */
+public class RealtimeConsumptionRateManager {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RealtimeConsumptionRateManager.class);
+  private static final int CACHE_ENTRY_EXPIRATION_TIME_IN_MINUTES = 10;
+
+  // stream config object is required for fetching the partition count from the stream
+  private final LoadingCache<StreamConfig, Integer> _streamConfigToTopicPartitionCountMap;
+  private volatile boolean _isThrottlingAllowed = false;
+
+  @VisibleForTesting
+  RealtimeConsumptionRateManager(LoadingCache<StreamConfig, Integer> streamConfigToTopicPartitionCountMap) {
+    _streamConfigToTopicPartitionCountMap = streamConfigToTopicPartitionCountMap;
+  }
+
+  private static class InstanceHolder {
+    private static final RealtimeConsumptionRateManager INSTANCE = new RealtimeConsumptionRateManager(buildCache());
+  }
+
+  public static RealtimeConsumptionRateManager getInstance() {
+    return InstanceHolder.INSTANCE;
+  }
+
+  public void enableThrottling() {
+    _isThrottlingAllowed = true;
+  }
+
+  public ConsumptionRateLimiter createRateLimiter(StreamConfig streamConfig, String tableName) {
+    if (!streamConfig.getTopicConsumptionRateLimit().isPresent()) {
+      return NOOP_RATE_LIMITER;
+    }
+    int partitionCount;
+    try {
+      partitionCount = _streamConfigToTopicPartitionCountMap.get(streamConfig);
+    } catch (ExecutionException e) {
+      // Exception here means for some reason, partition count cannot be fetched from stream!
+      throw new RuntimeException(e);
+    }
+    double topicRateLimit = streamConfig.getTopicConsumptionRateLimit().get();
+    double partitionRateLimit = topicRateLimit / partitionCount;
+    LOGGER.info("A consumption rate limiter is set up for topic {} in table {} with rate limit: {} "
+            + "(topic rate limit: {}, partition count: {})", streamConfig.getTopicName(), tableName, partitionRateLimit,
+        topicRateLimit, partitionCount);
+    return new RateLimiterImpl(partitionRateLimit);
+  }
+
+  private static LoadingCache<StreamConfig, Integer> buildCache() {
+    return CacheBuilder.newBuilder().expireAfterWrite(CACHE_ENTRY_EXPIRATION_TIME_IN_MINUTES, TimeUnit.MINUTES)
+        .build(new CacheLoader<StreamConfig, Integer>() {
+          @Override
+          public Integer load(StreamConfig streamConfig)
+              throws Exception {
+            String clientId = streamConfig.getTopicName() + "-consumption.rate.manager";
+            StreamConsumerFactory factory = StreamConsumerFactoryProvider.create(streamConfig);
+            try (StreamMetadataProvider streamMetadataProvider = factory.createStreamMetadataProvider(clientId)) {
+              return streamMetadataProvider.fetchPartitionCount(/*maxWaitTimeMs*/10_000);
+            }
+          }
+        });
+  }
+
+  @FunctionalInterface
+  public interface ConsumptionRateLimiter {
+    void throttle(int numMsgs);
+  }
+
+  @VisibleForTesting
+  static final ConsumptionRateLimiter NOOP_RATE_LIMITER = n -> {
+  };
+
+  @VisibleForTesting
+  static class RateLimiterImpl implements ConsumptionRateLimiter {
+    private final double _rate;
+    private final RateLimiter _rateLimiter;
+
+    private RateLimiterImpl(double rate) {
+      _rate = rate;
+      _rateLimiter = RateLimiter.create(rate);
+    }
+
+    @Override
+    public void throttle(int numMsgs) {
+      if (InstanceHolder.INSTANCE._isThrottlingAllowed && numMsgs > 0) {
+        _rateLimiter.acquire(numMsgs);
+      }
+    }
+
+    @VisibleForTesting
+    double getRate() {
+      return _rate;
+    }
+  }
+}
--- pinot-spi/src/main/java/org/apache/pinot/spi/stream/StreamConfig.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/stream/StreamConfig.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import org.apache.pinot.spi.utils.DataSizeUtils;
 import org.apache.pinot.spi.utils.EqualityUtils;
@@ -58,6 +59,8 @@ public class StreamConfig {
 
   private static final String SIMPLE_CONSUMER_TYPE_STRING = "simple";
 
+  private static final double CONSUMPTION_RATE_LIMIT_NOT_SPECIFIED = -1;
+
   private final String _type;
   private final String _topicName;
   private final String _tableNameWithType;
@@ -75,6 +78,8 @@ public class StreamConfig {
   private final int _flushAutotuneInitialRows; // initial num rows to use for SegmentSizeBasedFlushThresholdUpdater
 
   private final String _groupId;
+
+  private final double _topicConsumptionRateLimit;
 
   private final Map<String, String> _streamConfigMap = new HashMap<>();
 
@@ -185,6 +190,9 @@ public class StreamConfig {
 
     String groupIdKey = StreamConfigProperties.constructStreamProperty(_type, StreamConfigProperties.GROUP_ID);
     _groupId = streamConfigMap.get(groupIdKey);
+
+    String rate = streamConfigMap.get(StreamConfigProperties.TOPIC_CONSUMPTION_RATE_LIMIT);
+    _topicConsumptionRateLimit = rate != null ? Double.parseDouble(rate) : CONSUMPTION_RATE_LIMIT_NOT_SPECIFIED;
 
     _streamConfigMap.putAll(streamConfigMap);
   }
@@ -326,6 +334,11 @@ public class StreamConfig {
     return _groupId;
   }
 
+  public Optional<Double> getTopicConsumptionRateLimit() {
+    return _topicConsumptionRateLimit == CONSUMPTION_RATE_LIMIT_NOT_SPECIFIED ? Optional.empty()
+        : Optional.of(_topicConsumptionRateLimit);
+  }
+
   public String getTableNameWithType() {
     return _tableNameWithType;
   }
@@ -342,8 +355,8 @@ public class StreamConfig {
         + _fetchTimeoutMillis + ", _flushThresholdRows=" + _flushThresholdRows + ", _flushThresholdTimeMillis="
         + _flushThresholdTimeMillis + ", _flushSegmentDesiredSizeBytes=" + _flushThresholdSegmentSizeBytes
         + ", _flushAutotuneInitialRows=" + _flushAutotuneInitialRows + ", _decoderClass='" + _decoderClass + '\''
-        + ", _decoderProperties=" + _decoderProperties + ", _groupId='" + _groupId + ", _tableNameWithType='"
-        + _tableNameWithType + '}';
+        + ", _decoderProperties=" + _decoderProperties + ", _groupId='" + _groupId + "', _topicConsumptionRateLimit="
+        + _topicConsumptionRateLimit + ", _tableNameWithType='" + _tableNameWithType + '}';
   }
 
   @Override
@@ -370,6 +383,7 @@ public class StreamConfig {
         .isEqual(_offsetCriteria, that._offsetCriteria) && EqualityUtils.isEqual(_decoderClass, that._decoderClass)
         && EqualityUtils.isEqual(_decoderProperties, that._decoderProperties) && EqualityUtils
         .isEqual(_groupId, that._groupId) && EqualityUtils.isEqual(_tableNameWithType, that._tableNameWithType)
+        && EqualityUtils.isEqual(_topicConsumptionRateLimit, that._topicConsumptionRateLimit)
         && EqualityUtils.isEqual(_streamConfigMap, that._streamConfigMap);
   }
 
@@ -389,6 +403,7 @@ public class StreamConfig {
     result = EqualityUtils.hashCodeOf(result, _decoderClass);
     result = EqualityUtils.hashCodeOf(result, _decoderProperties);
     result = EqualityUtils.hashCodeOf(result, _groupId);
+    result = EqualityUtils.hashCodeOf(result, _topicConsumptionRateLimit);
     result = EqualityUtils.hashCodeOf(result, _streamConfigMap);
     result = EqualityUtils.hashCodeOf(result, _tableNameWithType);
     return result;
