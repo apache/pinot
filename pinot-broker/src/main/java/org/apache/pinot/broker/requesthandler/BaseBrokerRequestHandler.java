ea2f0aa641e17301293662c8e79dfd94d8568438
Prashant Pandey <84911643+suddendust@users.noreply.github.com>
2022-02-02 11:29:49 +0530

    [7867] Handle Select * with Extra Columns (#7959)
    * Expand '*' with other cols
    
    * added license header
    
    * Additional UTs
    
    * Additional UTs for happy case
    
    * linter
    
    * Checkstyle
    
    * Fix failing UT
    
    * Use imperative code instead of stream APIs for performance while expanding '*'
    
    * Expand * in natural ordering of columns
    
    * Linter fixes
    
    * Minor refactoring
    
    * Don't dedup columns, expand '*' request without other columns on broker
    
    * Add test description
    
    * Renamed from `expandStarExpressionToActualColumns` to `expandStarExpressionsToActualColumns`
    
    * Create new selections list instead of modifying the original
    
    * Update pinot-broker/src/main/java/org/apache/pinot/broker/requesthandler/BaseBrokerRequestHandler.java
    
    Co-authored-by: Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
    
    Co-authored-by: Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>

--- pinot-broker/src/main/java/org/apache/pinot/broker/requesthandler/BaseBrokerRequestHandler.java
+++ pinot-broker/src/main/java/org/apache/pinot/broker/requesthandler/BaseBrokerRequestHandler.java
@@ -106,6 +106,7 @@ public abstract class BaseBrokerRequestHandler implements BrokerRequestHandler {
   private static final String IN_SUBQUERY = "inSubquery";
   private static final Expression FALSE = RequestUtils.getLiteralExpression(false);
   private static final Expression TRUE = RequestUtils.getLiteralExpression(true);
+  private static final Expression STAR = RequestUtils.getIdentifierExpression("*");
 
   protected final PinotConfiguration _config;
   protected final RoutingManager _routingManager;
@@ -1459,8 +1460,17 @@ public abstract class BaseBrokerRequestHandler implements BrokerRequestHandler {
       Map<String, String> columnNameMap) {
     Map<String, String> aliasMap = new HashMap<>();
     if (pinotQuery != null) {
+      boolean hasStar = false;
       for (Expression expression : pinotQuery.getSelectList()) {
         fixColumnName(rawTableName, expression, columnNameMap, aliasMap, isCaseInsensitive);
+        //check if the select expression is '*'
+        if (!hasStar && expression.equals(STAR)) {
+          hasStar = true;
+        }
+      }
+      //if query has a '*' selection along with other columns
+      if (hasStar) {
+        expandStarExpressionsToActualColumns(pinotQuery, columnNameMap);
       }
       Expression filterExpression = pinotQuery.getFilterExpression();
       if (filterExpression != null) {
@@ -1485,6 +1495,30 @@ public abstract class BaseBrokerRequestHandler implements BrokerRequestHandler {
         fixColumnName(rawTableName, havingExpression, columnNameMap, aliasMap, isCaseInsensitive);
       }
     }
+  }
+
+  private static void expandStarExpressionsToActualColumns(PinotQuery pinotQuery, Map<String, String> columnNameMap) {
+    List<Expression> originalSelections = pinotQuery.getSelectList();
+    //expand '*'
+    List<Expression> expandedSelections = new ArrayList<>();
+    for (String tableCol : columnNameMap.values()) {
+      Expression newSelection = RequestUtils.createIdentifierExpression(tableCol);
+      //we exclude default virtual columns
+      if (tableCol.charAt(0) != '$') {
+        expandedSelections.add(newSelection);
+      }
+    }
+    //sort naturally
+    expandedSelections.sort(null);
+    List<Expression> newSelections = new ArrayList<>();
+    for (Expression originalSelection : originalSelections) {
+      if (originalSelection.equals(STAR)) {
+        newSelections.addAll(expandedSelections);
+      } else {
+        newSelections.add(originalSelection);
+      }
+    }
+    pinotQuery.setSelectList(newSelections);
   }
 
   /**
