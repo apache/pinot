934e246d91745ee52d2682b03ba6e2073b3cdd42
Atri Sharma <atri.jiit@gmail.com>
2022-02-02 16:20:07 +0530

    Firsr version working

--- pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/NotDocIdIterator.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/dociditerators/NotDocIdIterator.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.core.operator.dociditerators;
 
 import org.apache.pinot.core.common.BlockDocIdIterator;
@@ -6,25 +24,22 @@ import org.apache.pinot.segment.spi.Constants;
 
 public class NotDocIdIterator implements BlockDocIdIterator {
   private BlockDocIdIterator _childDocIdIterator;
-  private int _currentDocIdFromChildIterator;
   private int _lowerLimit;
   private int _upperLimit;
   private int _numDocs;
 
   public NotDocIdIterator(BlockDocIdIterator childDocIdIterator, int numDocs) {
     _childDocIdIterator = childDocIdIterator;
-    _currentDocIdFromChildIterator = childDocIdIterator.next();
     _lowerLimit = 0;
-    _upperLimit = _currentDocIdFromChildIterator == Constants.EOF ? numDocs : _currentDocIdFromChildIterator;
+
+    int currentDocIdFromChildIterator = childDocIdIterator.next();
+    _upperLimit = currentDocIdFromChildIterator == Constants.EOF ? numDocs : currentDocIdFromChildIterator;
     _numDocs = numDocs;
   }
 
   @Override
   public int next() {
-    if (_lowerLimit == _upperLimit) {
-      if (_lowerLimit == _numDocs) {
-        return Constants.EOF;
-      }
+    while (_lowerLimit == _upperLimit) {
       _lowerLimit = _upperLimit + 1;
 
       int nextMatchingDocId = _childDocIdIterator.next();
@@ -36,13 +51,17 @@ public class NotDocIdIterator implements BlockDocIdIterator {
       }
     }
 
+    if (_lowerLimit >= _numDocs) {
+      return Constants.EOF;
+    }
+
     return _lowerLimit++;
   }
 
   @Override
   public int advance(int targetDocId) {
-    if (targetDocId == Constants.EOF) {
-      return targetDocId;
+    if (targetDocId == Constants.EOF || targetDocId > _numDocs) {
+      return Constants.EOF;
     }
 
     if (targetDocId < _lowerLimit) {
@@ -50,14 +69,13 @@ public class NotDocIdIterator implements BlockDocIdIterator {
     }
 
     _lowerLimit = targetDocId + 1;
-    //int upperLimit = _childDocIdIterator.advance(targetDocId);
+
     int upperLimit = findUpperLimitGreaterThanDocId(targetDocId);
 
     if (upperLimit == Constants.EOF) {
       _upperLimit = _numDocs;
     } else {
       _upperLimit = upperLimit;
-      _currentDocIdFromChildIterator = _upperLimit;
     }
 
     return _lowerLimit++;
