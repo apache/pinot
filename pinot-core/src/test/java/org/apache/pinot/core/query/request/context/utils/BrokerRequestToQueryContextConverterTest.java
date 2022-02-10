a899110dff559bf42a5a68fe88fd3d93bf710e4d
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2022-02-10 12:53:34 -0800

    Re-formatting some filtered aggregation classes (#8184)

--- pinot-core/src/test/java/org/apache/pinot/core/query/request/context/utils/BrokerRequestToQueryContextConverterTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/query/request/context/utils/BrokerRequestToQueryContextConverterTest.java
@@ -582,7 +582,7 @@ public class BrokerRequestToQueryContextConverterTest {
     String query = "SELECT COUNT(*) FILTER(WHERE foo > 5), COUNT(*) FILTER(WHERE foo < 6) FROM testTable WHERE bar > 0";
     QueryContext queryContext = QueryContextConverterUtils.getQueryContextFromSQL(query);
     List<Pair<AggregationFunction, FilterContext>> filteredAggregationList =
-        queryContext.getFilteredAggregations();
+        queryContext.getFilteredAggregationFunctions();
     assertNotNull(filteredAggregationList);
     assertEquals(filteredAggregationList.size(), 2);
     assertTrue(filteredAggregationList.get(0).getLeft() instanceof CountAggregationFunction);
