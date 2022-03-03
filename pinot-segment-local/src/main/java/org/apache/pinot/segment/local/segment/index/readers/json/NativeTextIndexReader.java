78f813ab5166e275de04fe4d053437c66632106b
Atri Sharma <atri.jiit@gmail.com>
2022-03-03 13:32:14 +0530

    Intermediate commit

---
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/readers/json/NativeTextIndexReader.java
@@ -0,0 +1,91 @@
+package org.apache.pinot.segment.local.segment.index.readers.json;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Collections;
+import java.util.PrimitiveIterator;
+import org.apache.avro.util.ByteBufferInputStream;
+import org.apache.pinot.segment.local.segment.index.readers.BitmapInvertedIndexReader;
+import org.apache.pinot.segment.local.utils.nativefst.FST;
+import org.apache.pinot.segment.local.utils.nativefst.ImmutableFST;
+import org.apache.pinot.segment.local.utils.nativefst.NativeTextIndexCreator;
+import org.apache.pinot.segment.local.utils.nativefst.utils.RegexpMatcher;
+import org.apache.pinot.segment.spi.index.reader.TextIndexReader;
+import org.apache.pinot.segment.spi.memory.PinotDataBuffer;
+import org.roaringbitmap.RoaringBitmapWriter;
+import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
+import org.roaringbitmap.buffer.MutableRoaringBitmap;
+
+
+public class NativeTextIndexReader implements TextIndexReader {
+  private final FST _fst;
+  private final BitmapInvertedIndexReader _invertedIndex;
+  private int _numDocs;
+
+  public NativeTextIndexReader(PinotDataBuffer dataBuffer, int numDocs) {
+    _numDocs = numDocs;
+    long invertedIndexLength = dataBuffer.getLong(0);
+    long fstDataLength = dataBuffer.getLong(8);
+    int numBitMaps = dataBuffer.getInt(16);
+
+    long fstDataStartOffset = NativeTextIndexCreator.HEADER_LENGTH;
+    long fstDataEndOffset = fstDataStartOffset + fstDataLength;
+    ByteBuffer byteBuffer = dataBuffer.toDirectByteBuffer(fstDataStartOffset, (int) fstDataLength);
+    byte[] arr = new byte[byteBuffer.remaining()];
+   // byteBuffer.get(arr);
+    try {
+      _fst = FST.read(new ByteBufferInputStream(Collections.singletonList(byteBuffer)), ImmutableFST.class, true);
+    } catch (IOException e) {
+      throw new RuntimeException(e.getMessage());
+    }
+
+    long invertedIndexEndOffset = fstDataEndOffset + invertedIndexLength;
+    _invertedIndex = new BitmapInvertedIndexReader(
+        dataBuffer.view(fstDataEndOffset, invertedIndexEndOffset, ByteOrder.BIG_ENDIAN), numBitMaps);
+  }
+
+  @Override
+  public ImmutableRoaringBitmap getDictIds(String searchQuery) {
+    try {
+      RoaringBitmapWriter<MutableRoaringBitmap> writer = RoaringBitmapWriter.bufferWriter().get();
+      RegexpMatcher.regexMatch(searchQuery, _fst, writer::add);
+      return writer.get();
+    } catch (Exception e) {
+      throw new RuntimeException("Caught exception while running query: " + searchQuery, e);
+    }
+  }
+
+  @Override
+  public MutableRoaringBitmap getDocIds(String searchQuery) {
+    try {
+      RoaringBitmapWriter<MutableRoaringBitmap> writer = RoaringBitmapWriter.bufferWriter().get();
+      RegexpMatcher.regexMatch(searchQuery, _fst, writer::add);
+      ImmutableRoaringBitmap matchingDictIds = writer.get();
+      MutableRoaringBitmap matchingDocIds = null;
+
+      for (PrimitiveIterator.OfInt it = matchingDictIds.stream().iterator(); it.hasNext(); ) {
+        int dictId = it.next();
+
+        if (dictId >= 0) {
+          ImmutableRoaringBitmap docIds = _invertedIndex.getDocIds(dictId);
+          if (matchingDocIds == null) {
+            matchingDocIds = docIds.toMutableRoaringBitmap();
+          } else {
+            matchingDocIds.and(docIds);
+          }
+        }
+      }
+
+      return matchingDocIds == null ? new MutableRoaringBitmap() : matchingDocIds;
+    } catch (Exception e) {
+      throw new RuntimeException("Caught exception while running query: " + searchQuery, e);
+    }
+  }
+
+  @Override
+  public void close()
+      throws IOException {
+
+  }
+}
---
+++ pinot-segment-local/src/test/java/org/apache/pinot/segment/local/segment/index/creator/NativeTextIndexCreatorTest.java
@@ -0,0 +1,79 @@
+package org.apache.pinot.segment.local.segment.index.creator;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.apache.pinot.segment.local.segment.index.readers.json.NativeTextIndexReader;
+import org.apache.pinot.segment.local.utils.nativefst.NativeTextIndexCreator;
+import org.apache.pinot.segment.spi.memory.PinotDataBuffer;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.apache.pinot.segment.spi.V1Constants.Indexes.NATIVE_TEXT_INDEX_FILE_EXTENSION;
+
+
+public class NativeTextIndexCreatorTest {
+  private static final File INDEX_DIR = new File(FileUtils.getTempDirectory(), "NativeTextIndexCreatorTest");
+
+  @BeforeClass
+  public void setUp()
+      throws IOException {
+    FileUtils.forceMkdir(INDEX_DIR);
+  }
+
+  @AfterClass
+  public void tearDown()
+      throws IOException {
+    FileUtils.deleteDirectory(INDEX_DIR);
+  }
+
+  @Test
+  public void testIndexWriterReader()
+      throws IOException {
+    String[] uniqueValues = new String[4];
+    uniqueValues[0] = "hello-world";
+    uniqueValues[1] = "hello-world123";
+    uniqueValues[2] = "still";
+    uniqueValues[2] = "zoobar";
+
+    try (NativeTextIndexCreator creator = new NativeTextIndexCreator("testFSTColumn", INDEX_DIR)) {
+      creator.add(uniqueValues, 3);
+      creator.seal();
+    }
+
+    File fstFile = new File(INDEX_DIR, "testFSTColumn" + NATIVE_TEXT_INDEX_FILE_EXTENSION);
+    try (PinotDataBuffer dataBuffer = PinotDataBuffer.mapReadOnlyBigEndianFile(fstFile);
+        NativeTextIndexReader reader = new NativeTextIndexReader(dataBuffer, 3)) {
+
+      int[] matchedDictIds = reader.getDictIds("hello.*").toArray();
+      Assert.assertEquals(1, matchedDictIds.length);
+      Assert.assertEquals(0, matchedDictIds[0]);
+
+      matchedDictIds = reader.getDictIds(".*llo").toArray();
+      Assert.assertEquals(1, matchedDictIds.length);
+      Assert.assertEquals(0, matchedDictIds[0]);
+
+      matchedDictIds = reader.getDictIds("wor.*").toArray();
+      Assert.assertEquals(2, matchedDictIds.length);
+      Assert.assertEquals(2, matchedDictIds[0]);
+      Assert.assertEquals(3, matchedDictIds[1]);
+
+      /*int[] matchedDocIds = reader.getDocIds("hello.*").toArray();
+      Assert.assertEquals(2, matchedDocIds.length);
+      Assert.assertEquals(0, matchedDocIds[0]);
+      Assert.assertEquals(1, matchedDocIds[1]);*/
+
+      /*matchedDocIds = reader.getDocIds(".*llo").toArray();
+      Assert.assertEquals(2, matchedDocIds.length);
+      Assert.assertEquals(0, matchedDocIds[0]);
+      Assert.assertEquals(1, matchedDocIds[1]);*/
+
+      int[] matchedDocIds = reader.getDocIds("wor.*").toArray();
+      Assert.assertEquals(2, matchedDocIds.length);
+      Assert.assertEquals(2, matchedDocIds[0]);
+      Assert.assertEquals(3, matchedDocIds[1]);
+    }
+  }
+}
