25869c5f796ea9f196b00eae6e3acbafb1a5960c
0902horn <0902horn@gmail.com>
2022-03-09 14:31:49 +0800

    Remove unused imports

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/RealtimeRocketMQIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/RealtimeRocketMQIntegrationTest.java
@@ -23,7 +23,6 @@ import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.google.common.base.Function;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
