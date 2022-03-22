f6e0152786c8da6dbaa5f9e19ae47baceec2bfdd
Atri Sharma <securonix@securonixs-MacBook-Pro.local>
2022-03-23 00:39:14 +0530

    More linter cleanup

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/utils/nativefst/NativeTextIndexCreator.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/utils/nativefst/NativeTextIndexCreator.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
 import org.apache.commons.io.FileUtils;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
