150f8a412055363b178119d88bf2bba76cc1eeab
Atri Sharma <securonix@securonixs-MacBook-Pro.local>
2022-05-02 15:31:38 +0530

    Concurrent checks and tests

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/realtime/impl/invertedindex/RealtimeNativeTextIndex.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/realtime/impl/invertedindex/RealtimeNativeTextIndex.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.segment.local.realtime.impl.invertedindex;
 
 import java.io.IOException;
@@ -6,6 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -15,6 +34,7 @@ import org.apache.pinot.segment.local.utils.nativefst.mutablefst.MutableFST;
 import org.apache.pinot.segment.local.utils.nativefst.mutablefst.MutableFSTImpl;
 import org.apache.pinot.segment.local.utils.nativefst.utils.RealTimeRegexpMatcher;
 import org.apache.pinot.segment.spi.index.mutable.MutableTextIndex;
+import org.apache.pinot.segment.spi.index.mutable.ThreadSafeMutableRoaringBitmap;
 import org.roaringbitmap.PeekableIntIterator;
 import org.roaringbitmap.RoaringBitmapWriter;
 import org.roaringbitmap.buffer.ImmutableRoaringBitmap;
@@ -28,12 +48,18 @@ public class RealtimeNativeTextIndex implements MutableTextIndex {
   private final Map<String, Integer> _termToDictIdMapping;
   private int _nextDocId = 0;
   private int _nextDictId = 0;
+  private final ReentrantReadWriteLock.ReadLock _readLock;
+  private final ReentrantReadWriteLock.WriteLock _writeLock;
 
   public RealtimeNativeTextIndex(String column) {
     _column = column;
     _mutableFST = new MutableFSTImpl();
     _termToDictIdMapping = new HashMap<>();
     _invertedIndex = new RealtimeInvertedIndex();
+
+    ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    _readLock = readWriteLock.readLock();
+    _writeLock = readWriteLock.writeLock();
   }
 
   @Override
@@ -45,17 +71,23 @@ public class RealtimeNativeTextIndex implements MutableTextIndex {
       throw new RuntimeException(e.getMessage());
     }
 
-    for (String token : tokens) {
-      Integer currentDictId = _termToDictIdMapping.get(token);
-      if (currentDictId == null) {
-        currentDictId = _nextDictId;
-        _mutableFST.addPath(token, currentDictId);
-        _termToDictIdMapping.put(token, currentDictId);
-        ++_nextDictId;
+    try {
+      _writeLock.lock();
+      for (String token : tokens) {
+        Integer currentDictId = _termToDictIdMapping.get(token);
+        if (currentDictId == null) {
+
+          currentDictId = _nextDictId;
+          _mutableFST.addPath(token, currentDictId);
+          _termToDictIdMapping.put(token, currentDictId);
+          ++_nextDictId;
+        }
+        addToPostingList(currentDictId);
       }
-      addToPostingList(currentDictId);
+      _nextDocId++;
+    } finally {
+      _writeLock.unlock();
     }
-    _nextDocId++;
   }
 
   @Override
@@ -65,21 +97,26 @@ public class RealtimeNativeTextIndex implements MutableTextIndex {
 
   @Override
   public MutableRoaringBitmap getDocIds(String searchQuery) {
-    RoaringBitmapWriter<MutableRoaringBitmap> writer = RoaringBitmapWriter.bufferWriter().get();
-    RealTimeRegexpMatcher.regexMatch(searchQuery, _mutableFST, writer::add);
-    ImmutableRoaringBitmap matchingDictIds = writer.get();
     MutableRoaringBitmap matchingDocIds = null;
+    try {
+      _readLock.lock();
+      RoaringBitmapWriter<MutableRoaringBitmap> writer = RoaringBitmapWriter.bufferWriter().get();
+      RealTimeRegexpMatcher.regexMatch(searchQuery, _mutableFST, writer::add);
+      ImmutableRoaringBitmap matchingDictIds = writer.get();
 
-    for (PeekableIntIterator it = matchingDictIds.getIntIterator(); it.hasNext();) {
-      int dictId = it.next();
-      if (dictId >= 0) {
-        ImmutableRoaringBitmap docIds = _invertedIndex.getDocIds(dictId);
-        if (matchingDocIds == null) {
-          matchingDocIds = docIds.toMutableRoaringBitmap();
-        } else {
-          matchingDocIds.or(docIds);
+      for (PeekableIntIterator it = matchingDictIds.getIntIterator(); it.hasNext(); ) {
+        int dictId = it.next();
+        if (dictId >= 0) {
+          ImmutableRoaringBitmap docIds = _invertedIndex.getDocIds(dictId);
+          if (matchingDocIds == null) {
+            matchingDocIds = docIds.toMutableRoaringBitmap();
+          } else {
+            matchingDocIds.or(docIds);
+          }
         }
       }
+    } finally {
+      _readLock.unlock();
     }
 
     return matchingDocIds == null ? new MutableRoaringBitmap() : matchingDocIds;
