35187ae9e6b601d6c77b78f9989f53972b352582
Xiang Fu <xiangfu.1024@gmail.com>
2022-05-09 16:43:52 -0700

    Move to picocli subcommand

--- pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/FileSystemCommand.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/FileSystemCommand.java
@@ -18,65 +18,40 @@
  */
 package org.apache.pinot.tools.admin.command;
 
-import com.google.common.collect.ImmutableMap;
-import java.io.File;
-import java.io.PrintStream;
-import java.net.URI;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.pinot.spi.env.PinotConfiguration;
-import org.apache.pinot.spi.filesystem.PinotFS;
-import org.apache.pinot.spi.filesystem.PinotFSFactory;
 import org.apache.pinot.spi.plugin.PluginManager;
-import org.apache.pinot.spi.utils.StringUtil;
 import org.apache.pinot.tools.Command;
-import org.apache.pinot.tools.utils.PinotConfigUtils;
+import org.apache.pinot.tools.admin.command.filesystem.CopyFiles;
+import org.apache.pinot.tools.admin.command.filesystem.DeleteFiles;
+import org.apache.pinot.tools.admin.command.filesystem.ListFiles;
+import org.apache.pinot.tools.admin.command.filesystem.MoveFiles;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import picocli.CommandLine;
 
 
-@CommandLine.Command(name = "FileSystem")
+@CommandLine.Command(name = "FileSystem", subcommands = {
+    ListFiles.class,
+    CopyFiles.class,
+    MoveFiles.class,
+    DeleteFiles.class
+})
 public class FileSystemCommand extends AbstractBaseAdminCommand implements Command {
   private static final Logger LOGGER = LoggerFactory.getLogger(FileSystemCommand.class.getName());
 
-  private static final String[] SUPPORTED_FS_COMMANDS = new String[]{
-      "EXISTS", "IS_DIR", "LAST_MODIFIED", "LENGTH",
-      "LIST_FILE", "LIST_FILE_RECURSIVE",
-      "CP", "COPY_DIR",
-      "COPY_FROM_LOCAL_FILE", "COPY_FROM_LOCAL_DIR", "COPY_TO_LOCAL_FILE",
-      "MV", "DELETE"
-  };
-
-  @CommandLine.Option(names = {"-scheme"}, required = true, description = "PinotFS scheme name, e.g. "
-      + "file/s3/adls/gcs/hdfs.")
-  private String _scheme;
-
-  @CommandLine.Option(names = {"-config", "-configFile"}, description =
-      "PinotFS Config file.")
+  @CommandLine.Option(names = {"-conf", "-config", "-configFile", "-config-file"}, scope =
+      CommandLine.ScopeType.INHERIT, description = "PinotFS Config file.")
   private String _configFile;
 
-  @CommandLine.Option(names = {"-command"}, required = true, arity = "1..*", description = "File system command, e.g."
-      + " EXISTS, IS_DIR, LAST_MODIFIED, LENGTH, LIST_FILE, LIST_FILE_RECURSIVE, CP, COPY_DIR, COPY_FROM_LOCAL_FILE, "
-      + "COPY_FROM_LOCAL_DIR, COPY_TO_LOCAL_FILE, MV, DELETE")
-  private String[] _command;
-
-  @CommandLine.Option(names = {"-help", "-h", "--h", "--help"}, usageHelp = true, description = "Print this message.")
+  @CommandLine.Option(names = {"-help", "-h", "--h", "--help"}, usageHelp = true, description = "Print this message.",
+      scope = CommandLine.ScopeType.INHERIT)
   private boolean _help = false;
 
-  public FileSystemCommand setScheme(String scheme) {
-    _scheme = scheme;
-    return this;
-  }
+  @CommandLine.Parameters
+  private String[] _parameters;
 
   public FileSystemCommand setConfigFile(String configFile) {
     _configFile = configFile;
-    return this;
-  }
-
-  public FileSystemCommand setCommand(String[] command) {
-    _command = command;
     return this;
   }
 
@@ -85,16 +60,13 @@ public class FileSystemCommand extends AbstractBaseAdminCommand implements Comma
     return this;
   }
 
-  public String getScheme() {
-    return _scheme;
-  }
-
   public String getConfigFile() {
     return _configFile;
   }
 
-  public String[] getCommand() {
-    return _command;
+  public FileSystemCommand setParameters(String[] parameters) {
+    _parameters = parameters;
+    return this;
   }
 
   @Override
@@ -109,8 +81,7 @@ public class FileSystemCommand extends AbstractBaseAdminCommand implements Comma
 
   @Override
   public String toString() {
-    return ("FileSystem --scheme " + _scheme + " -configFileName " + _configFile + " -command " + Arrays.toString(
-        _command));
+    return "FileSystem -configFile " + _configFile;
   }
 
   @Override
@@ -122,106 +93,19 @@ public class FileSystemCommand extends AbstractBaseAdminCommand implements Comma
     return "Pinot File system operation.";
   }
 
-  public String run()
-      throws Exception {
-    LOGGER.info("Executing command: " + this);
-    Map<String, Object> configs =
-        getConfigFile() == null ? new HashMap<>() : PinotConfigUtils.readConfigFromFile(getConfigFile());
-    PinotFSFactory.init(new PinotConfiguration(configs));
-    registerDefaultPinotFS();
-
-    PinotFS pinotFS = PinotFSFactory.create(getScheme());
-    String output = "DONE";
-    String fsOp = _command[0].replace("_", "").toUpperCase();
-    String[] arguments = Arrays.copyOfRange(_command, 1, _command.length);
-    try {
-      switch (fsOp) {
-        case "LSFILE":
-        case "LISTFILE":
-          output = StringUtil.join("\n", pinotFS.listFiles(URI.create(arguments[0]), false));
-          break;
-        case "LISTFILERECURSIVE":
-          output = StringUtil.join("\n", pinotFS.listFiles(URI.create(arguments[0]), true));
-          break;
-        case "CP":
-        case "COPY":
-          output = String.valueOf(pinotFS.copy(URI.create(arguments[0]), URI.create(arguments[1])));
-          break;
-        case "COPYDIR":
-          pinotFS.copyDir(URI.create(arguments[0]), URI.create(arguments[1]));
-          break;
-        case "COPYFROMLOCALDIR":
-          pinotFS.copyFromLocalDir(new File(arguments[0]), URI.create(arguments[1]));
-          break;
-        case "COPYFROMLOCALFILE":
-          pinotFS.copyFromLocalFile(new File(arguments[0]), URI.create(arguments[1]));
-          break;
-        case "COPYTOLOCALFILE":
-          pinotFS.copyToLocalFile(URI.create(arguments[0]), new File(arguments[1]));
-          break;
-        case "EXISTS":
-          output = String.valueOf(pinotFS.exists(URI.create(arguments[0])));
-          break;
-        case "DELETE":
-          pinotFS.delete(URI.create(arguments[0]), true);
-          break;
-        case "ISDIR":
-        case "ISDIRECTORY":
-          output = String.valueOf(pinotFS.isDirectory(URI.create(arguments[0])));
-          break;
-        case "LASTMODIFIED":
-          output = String.valueOf(pinotFS.lastModified(URI.create(arguments[0])));
-          break;
-        case "LENGTH":
-          output = String.valueOf(pinotFS.length(URI.create(arguments[0])));
-          break;
-        case "MV":
-        case "MOVE":
-          output = String.valueOf(pinotFS.move(URI.create(arguments[0]), URI.create(arguments[1]), true));
-          break;
-        case "MKDIR":
-          output = String.valueOf(pinotFS.mkdir(URI.create(arguments[0])));
-          break;
-        default:
-          output = "Unsupported file system op: " + fsOp + ". Current supported operations are: "
-              + StringUtil.join(", ", SUPPORTED_FS_COMMANDS);
-          break;
-      }
-    } catch (Exception e) {
-      e.printStackTrace(new PrintStream(output));
-    }
-    return output;
-  }
-
-  private void registerDefaultPinotFS() {
-    registerPinotFS("s3", "org.apache.pinot.plugin.filesystem.S3PinotFS",
-        ImmutableMap.of("region", System.getProperty("AWS_REGION", "us-west-2")));
-  }
-
-  public static void registerPinotFS(String scheme, String fsClassName, Map<String, Object> configs) {
-    if (PinotFSFactory.isSchemeSupported(scheme)) {
-      LOGGER.info("PinotFS for scheme: {} is already registered.", scheme);
-      return;
-    }
-    try {
-      PinotFSFactory.register(scheme, fsClassName, new PinotConfiguration(configs));
-      LOGGER.info("Registered PinotFS for scheme: {}", scheme);
-    } catch (Exception e) {
-      LOGGER.error("Unable to init PinotFS for scheme: {}, class name: {}, configs: {}", scheme, fsClassName, configs,
-          e);
-    }
-  }
-
   @Override
   public boolean execute()
       throws Exception {
-    String result = run();
-    LOGGER.info(result);
-    return true;
+    // All FileSystem commands should go to SubCommand class.
+    System.out.println("Unsupported subcommand: " + Arrays.toString(_parameters));
+    return false;
   }
 
   public static void main(String[] args) {
     PluginManager.get().init();
-    new CommandLine(new FileSystemCommand()).execute(args);
+    int exitCode = new CommandLine(new FileSystemCommand()).execute(args);
+    if (exitCode != 0 && Boolean.parseBoolean(System.getProperties().getProperty("pinot.admin.system.exit", "true"))) {
+      System.exit(exitCode);
+    }
   }
 }
---
+++ pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/filesystem/CopyFiles.java
@@ -0,0 +1,113 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.tools.admin.command.filesystem;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import org.apache.pinot.spi.filesystem.LocalPinotFS;
+import org.apache.pinot.spi.filesystem.PinotFS;
+import picocli.CommandLine;
+
+
+@CommandLine.Command(name = "cp", aliases = {"copy"}, description = "Copy file from source to destination")
+public class CopyFiles extends BaseFileOperation {
+  @CommandLine.Parameters(index = "0", description = "Source file")
+  private String _source;
+
+  @CommandLine.Parameters(index = "1", description = "Destination file")
+  private String _destination;
+
+  public CopyFiles setSource(String source) {
+    _source = source;
+    return this;
+  }
+
+  public CopyFiles setDestination(String destination) {
+    _destination = destination;
+    return this;
+  }
+
+  @Override
+  public boolean execute()
+      throws Exception {
+    try {
+      super.initialPinotFS();
+    } catch (Exception e) {
+      System.err.println("Failed to initialize PinotFS, exception: " + e.getMessage());
+      return false;
+    }
+
+    URI sourceURI = URI.create(_source);
+    PinotFS sourcePinotFS = Utils.getPinotFS(sourceURI);
+
+    URI destinationURI = URI.create(_destination);
+    PinotFS destinationPinotFS = Utils.getPinotFS(destinationURI);
+
+    if (sourcePinotFS == destinationPinotFS) {
+      try {
+        sourcePinotFS.copy(sourceURI, destinationURI);
+        return true;
+      } catch (IOException e) {
+        System.err.println(
+            "Unable to copy files from " + _source + " to " + _destination + ", exception: " + e.getMessage());
+      }
+    } else if (sourcePinotFS instanceof LocalPinotFS) {
+      File localFile = new File(_source);
+      if (localFile.isDirectory()) {
+        try {
+          destinationPinotFS.copyFromLocalDir(localFile, destinationURI);
+          return true;
+        } catch (Exception e) {
+          System.err.println(
+              "Failed to copy local directory " + _source + " to " + _destination + ", exception: " + e.getMessage());
+        }
+      } else {
+        try {
+          destinationPinotFS.copyFromLocalFile(localFile, destinationURI);
+          return true;
+        } catch (Exception e) {
+          System.err.println(
+              "Failed to copy local file " + _source + " to " + _destination + ", exception: " + e.getMessage());
+        }
+      }
+    } else if (destinationPinotFS instanceof LocalPinotFS) {
+      try {
+        sourcePinotFS.copyToLocalFile(sourceURI, new File(_destination));
+        return true;
+      } catch (Exception e) {
+        System.err.println(
+            "Failed to copy remote " + _source + " to local file " + _destination + ", exception: " + e.getMessage());
+      }
+    } else {
+      System.err.println("Copying files between two different remote PinotFS is not supported");
+    }
+    return false;
+  }
+
+  @Override
+  public void printUsage() {
+    System.out.println("cp <source-uri> <destination-uri>");
+  }
+
+  @Override
+  public String description() {
+    return "Copy file from source to destination, copying from two different remote PinotFS is not supported.";
+  }
+}
