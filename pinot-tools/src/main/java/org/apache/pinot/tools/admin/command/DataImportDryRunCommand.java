2b1516a9db3a70f070c12862fbc5d9b890ec92d5
Mark Needham <m.h.needham@gmail.com>
2022-05-10 11:18:09 +0100

    clean up

--- pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/DataImportDryRunCommand.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/DataImportDryRunCommand.java
@@ -19,18 +19,16 @@
 package org.apache.pinot.tools.admin.command;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.TreeMap;
 import org.apache.pinot.plugin.inputformat.json.JSONRecordReader;
 import org.apache.pinot.segment.local.segment.creator.RecordReaderSegmentCreationDataSource;
 import org.apache.pinot.segment.local.segment.creator.TransformPipeline;
-import org.apache.pinot.segment.local.segment.creator.impl.stats.SegmentPreIndexStatsCollectorImpl;
-import org.apache.pinot.segment.spi.creator.SegmentPreIndexStatsCollector;
 import org.apache.pinot.segment.spi.creator.StatsCollectorConfig;
 import org.apache.pinot.spi.config.table.TableConfig;
-import org.apache.pinot.spi.config.table.TableType;
 import org.apache.pinot.spi.data.Schema;
 import org.apache.pinot.spi.data.readers.GenericRow;
 import org.apache.pinot.spi.utils.JsonUtils;
-import org.apache.pinot.spi.utils.builder.TableConfigBuilder;
 import org.apache.pinot.tools.Command;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,9 +36,8 @@ import picocli.CommandLine;
 
 
 /**
- * Class for command to infer pinot schema from Json data. Given that it is not always possible to
- * automatically do this, the intention is to get most of the work done by this class, and require any
- * manual editing on top.
+ * Class for command to do a dry run of data ingestion so that we can see how transformation functions and
+ * complex config will be applied.
  */
 @CommandLine.Command(name = "DataImportDryRun")
 public class DataImportDryRunCommand extends AbstractBaseAdminCommand implements Command {
@@ -51,10 +48,6 @@ public class DataImportDryRunCommand extends AbstractBaseAdminCommand implements
 
   @CommandLine.Option(names = {"-tableConfigFile"}, required = true, description = "Path to table config file.")
   String _tableConfigFile;
-
-  @CommandLine.Option(names = {"-schemaFile"}, required = true, description = "Path to schema file.")
-  String _schemaFile;
-
 
   @SuppressWarnings("FieldCanBeLocal")
   @CommandLine.Option(names = {"-help", "-h", "--h", "--help"}, help = true, description = "Print this message.")
@@ -69,9 +62,7 @@ public class DataImportDryRunCommand extends AbstractBaseAdminCommand implements
         new RecordReaderSegmentCreationDataSource(jsonRecordReader);
 
     TableConfig tableConfig = JsonUtils.fileToObject(new File(_tableConfigFile), TableConfig.class);
-    Schema schema = Schema.fromFile(new File(_schemaFile));
-
-    StatsCollectorConfig statsCollectorConfig = new StatsCollectorConfig(tableConfig, schema, null);
+    StatsCollectorConfig statsCollectorConfig = new StatsCollectorConfig(tableConfig, new Schema(), null);
 
     TransformPipeline transformPipeline =
         new TransformPipeline(statsCollectorConfig.getTableConfig(), statsCollectorConfig.getSchema());
@@ -84,10 +75,9 @@ public class DataImportDryRunCommand extends AbstractBaseAdminCommand implements
       reuse.clear();
 
       reuse = jsonRecordReader.next(reuse);
-      System.out.println("reuse = " + reuse);
       transformPipeline.processRow(reuse, reusedResult);
       for (GenericRow row : reusedResult.getTransformedRows()) {
-        System.out.println("row = " + JsonUtils.objectToPrettyString(row.getFieldToValueMap()));
+        System.out.println("Available Fields: " + JsonUtils.objectToPrettyString(new TreeMap<>(row.getFieldToValueMap())));
       }
     }
 
@@ -96,12 +86,11 @@ public class DataImportDryRunCommand extends AbstractBaseAdminCommand implements
 
   @Override
   public String description() {
-    return "Extracting Pinot schema file from JSON data file.";
+    return "Dry run of data import";
   }
 
   @Override
   public boolean getHelp() {
     return _help;
   }
-
 }
