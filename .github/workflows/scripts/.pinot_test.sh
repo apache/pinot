d7a05d80e8a34a3b0f18e9a54cec7584807917dc
Rong Rong <rongr@apache.org>
2022-05-11 22:02:47 -0700

    Revert "[do not merge][CI mod] running OfflineGRPC integration test 100 times"
    This reverts commit 593a531ccc7a74cf33c626656229c56c693b23e1.

--- .github/workflows/scripts/.pinot_test.sh
+++ .github/workflows/scripts/.pinot_test.sh
@@ -28,13 +28,18 @@ netstat -i
 if [ "$RUN_INTEGRATION_TESTS" != false ]; then
   # Integration Tests
   mvn clean install -DskipTests -am -B -pl 'pinot-integration-tests' -T 16 || exit 1
-  for i in {1..100}
-  do
-    echo "Running test the $i-th times!"
-    mvn test -pl 'pinot-integration-tests' \
-        -Dtest='*OfflineGRPCServerIntegrationTest*' \
-        -P github-actions,integration-tests-only || exit 1
-  done
+  if [ "$RUN_TEST_SET" == "1" ]; then
+    mvn test -am -B \
+        -pl 'pinot-integration-tests' \
+        -Dtest='C*Test,L*Test,M*Test,R*Test,S*Test' \
+        -P github-actions,integration-tests-only && exit 0 || exit 1
+  fi
+  if [ "$RUN_TEST_SET" == "2" ]; then
+    mvn test -am -B \
+        -pl 'pinot-integration-tests' \
+        -Dtest='!C*Test,!L*Test,!M*Test,!R*Test,!S*Test' \
+        -P github-actions,integration-tests-only && exit 0 || exit 1
+  fi
 else
   # Unit Tests
   if [ "$RUN_TEST_SET" == "1" ]; then
