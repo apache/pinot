b7767200bbad7ecd53cec832732ca6914d117731
Amrish Lal <amrish.k.lal@gmail.com>
2022-06-28 17:40:22 -0700

    Add licence header.

--- pinot-controller/src/main/java/org/apache/pinot/controller/recommender/rules/impl/JsonIndexRule.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/recommender/rules/impl/JsonIndexRule.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.controller.recommender.rules.impl;
 
 import org.apache.pinot.controller.recommender.exceptions.InvalidInputException;
@@ -8,6 +26,7 @@ import org.apache.pinot.controller.recommender.rules.io.configs.IndexConfig;
 import org.apache.pinot.spi.data.FieldSpec;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 /** JSON columns must be NoDictionary columns with JsonIndex. */
 public class JsonIndexRule extends AbstractRule {
