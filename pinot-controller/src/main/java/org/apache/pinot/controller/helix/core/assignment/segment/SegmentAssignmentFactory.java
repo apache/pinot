c1d7086e608b79650f4dd4924f0bdc6358ea2128
Ankit Sultana <ankitsultana@uber.com>
2022-07-29 10:01:10 -0400

    Remove segment assignment code

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/assignment/segment/SegmentAssignmentFactory.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/assignment/segment/SegmentAssignmentFactory.java
@@ -19,7 +19,6 @@
 package org.apache.pinot.controller.helix.core.assignment.segment;
 
 import org.apache.helix.HelixManager;
-import org.apache.pinot.common.utils.config.TableConfigUtils;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.config.table.TableType;
 
@@ -33,16 +32,11 @@ public class SegmentAssignmentFactory {
 
   public static SegmentAssignment getSegmentAssignment(HelixManager helixManager, TableConfig tableConfig) {
     SegmentAssignment segmentAssignment;
-    boolean isTableInGroup = TableConfigUtils.hasPreConfiguredInstancePartitions(tableConfig);
     if (tableConfig.getTableType() == TableType.OFFLINE) {
-      if (isTableInGroup) {
-        segmentAssignment = new ColocatedOfflineSegmentAssignment();
-      } else {
-        segmentAssignment =
-            tableConfig.isDimTable() ? new OfflineDimTableSegmentAssignment() : new OfflineSegmentAssignment();
-      }
+      segmentAssignment =
+          tableConfig.isDimTable() ? new OfflineDimTableSegmentAssignment() : new OfflineSegmentAssignment();
     } else {
-      segmentAssignment = isTableInGroup ? new ColocatedRealtimeSegmentAssignment() : new RealtimeSegmentAssignment();
+      segmentAssignment = new RealtimeSegmentAssignment();
     }
     segmentAssignment.init(helixManager, tableConfig);
     return segmentAssignment;
