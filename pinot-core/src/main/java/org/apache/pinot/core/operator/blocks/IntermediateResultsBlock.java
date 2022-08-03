38817147f2847ac5f384069d2666750ec7dd7540
Ravishankar <ravishankar.gnanaprakasam@gmail.com>
2022-08-04 03:41:51 +0530

    Add additional metadata to the response (#9122)
    * Add additional metadata to the response
    
    * Correcting the order in the log
    
    Co-authored-by: Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
    
    * Adding todo to add clarity and for future reference
    
    Co-authored-by: Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>

--- pinot-core/src/main/java/org/apache/pinot/core/operator/blocks/IntermediateResultsBlock.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/blocks/IntermediateResultsBlock.java
@@ -69,6 +69,8 @@ public class IntermediateResultsBlock implements Block {
   private long _numTotalDocs;
   private int _numSegmentsProcessed;
   private int _numSegmentsMatched;
+  private int _numConsumingSegmentsProcessed;
+  private int _numConsumingSegmentsMatched;
   private boolean _numGroupsLimitReached;
   private int _numResizes;
   private long _resizeTimeMs;
@@ -268,6 +270,24 @@ public class IntermediateResultsBlock implements Block {
 
   public void setNumSegmentsMatched(int numSegmentsMatched) {
     _numSegmentsMatched = numSegmentsMatched;
+  }
+
+  @VisibleForTesting
+  public int getNumConsumingSegmentsProcessed() {
+    return _numConsumingSegmentsProcessed;
+  }
+
+  public void setNumConsumingSegmentsProcessed(int numConsumingSegmentsProcessed) {
+    _numConsumingSegmentsProcessed = numConsumingSegmentsProcessed;
+  }
+
+  @VisibleForTesting
+  public int getNumConsumingSegmentsMatched() {
+    return _numConsumingSegmentsMatched;
+  }
+
+  public void setNumConsumingSegmentsMatched(int numConsumingSegmentsMatched) {
+    _numConsumingSegmentsMatched = numConsumingSegmentsMatched;
   }
 
   @VisibleForTesting
@@ -477,6 +497,10 @@ public class IntermediateResultsBlock implements Block {
         .put(MetadataKey.NUM_ENTRIES_SCANNED_POST_FILTER.getName(), String.valueOf(_numEntriesScannedPostFilter));
     dataTable.getMetadata().put(MetadataKey.NUM_SEGMENTS_PROCESSED.getName(), String.valueOf(_numSegmentsProcessed));
     dataTable.getMetadata().put(MetadataKey.NUM_SEGMENTS_MATCHED.getName(), String.valueOf(_numSegmentsMatched));
+    dataTable.getMetadata().put(MetadataKey.NUM_CONSUMING_SEGMENTS_PROCESSED.getName(),
+        String.valueOf(_numConsumingSegmentsProcessed));
+    dataTable.getMetadata().put(MetadataKey.NUM_CONSUMING_SEGMENTS_MATCHED.getName(),
+        String.valueOf(_numConsumingSegmentsMatched));
     dataTable.getMetadata().put(MetadataKey.NUM_RESIZES.getName(), String.valueOf(_numResizes));
     dataTable.getMetadata().put(MetadataKey.RESIZE_TIME_MS.getName(), String.valueOf(_resizeTimeMs));
 
