a4e3adb7365ef64b02d3fb72505b97474e3d7244
Xiang Fu <xiangfu.1024@gmail.com>
2022-08-15 16:18:43 -0700

    Refactor offline quickstarts to load all the tables by default (#9206)

--- pinot-tools/src/main/java/org/apache/pinot/tools/QuickStartBase.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/QuickStartBase.java
@@ -18,21 +18,53 @@
  */
 package org.apache.pinot.tools;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.pinot.tools.admin.command.QuickstartRunner;
+import org.apache.pinot.tools.utils.JarUtils;
 import org.apache.pinot.tools.utils.PinotConfigUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
+/**
+ * Assuming that database name is DBNAME, bootstrap path must have the file structure specified below to properly
+ * load the table:
+ *  DBNAME
+ *  ├── ingestionJobSpec.yaml
+ *  ├── rawdata
+ *  │   └── DBNAME_data.csv
+ *  ├── DBNAME_offline_table_config.json
+ *  └── DBNAME_schema.json
+ *
+ */
 public abstract class QuickStartBase {
+  private static final Logger LOGGER = LoggerFactory.getLogger(QuickStartBase.class);
+  private static final String TAB = "\t\t";
+  private static final String NEW_LINE = "\n";
+
+  protected static final String[] DEFAULT_OFFLINE_TABLE_DIRECTORIES = new String[]{
+      "examples/batch/airlineStats",
+      "examples/minions/batch/baseballStats",
+      "examples/batch/dimBaseballTeams",
+      "examples/batch/starbucksStores",
+      "examples/batch/githubEvents",
+      "examples/batch/githubComplexTypeEvents"
+  };
+
   protected File _dataDir = FileUtils.getTempDirectory();
-  protected String _bootstrapDataDir;
+  protected String[] _bootstrapDataDirs;
   protected String _zkExternalAddress;
   protected String _configFilePath;
 
@@ -41,35 +73,19 @@ public abstract class QuickStartBase {
     return this;
   }
 
-  public QuickStartBase setBootstrapDataDir(String bootstrapDataDir) {
-    _bootstrapDataDir = bootstrapDataDir;
+  public QuickStartBase setBootstrapDataDirs(String[] bootstrapDataDirs) {
+    _bootstrapDataDirs = bootstrapDataDirs;
     return this;
-  }
-
-  /**
-   * Assuming that database name is DBNAME, bootstrap path must have the file structure specified below to properly
-   * load the table:
-   *  DBNAME
-   *  ├── ingestionJobSpec.yaml
-   *  ├── rawdata
-   *  │   └── DBNAME_data.csv
-   *  ├── DBNAME_offline_table_config.json
-   *  └── DBNAME_schema.json
-   *
-   * @return bootstrap path if specified by command line argument -bootstrapTableDir; otherwise, default.
-   */
-  public String getBootstrapDataDir(String bootstrapDataDir) {
-    return _bootstrapDataDir != null ? _bootstrapDataDir : bootstrapDataDir;
   }
 
   /** @return Table name if specified by command line argument -bootstrapTableDir; otherwise, default. */
   public String getTableName(String bootstrapDataDir) {
-    return Paths.get(getBootstrapDataDir(bootstrapDataDir)).getFileName().toString();
+    return Paths.get(bootstrapDataDir).getFileName().toString();
   }
 
   /** @return true if bootstrapTableDir is not specified by command line argument -bootstrapTableDir, else false.*/
   public boolean useDefaultBootstrapTableDir() {
-    return _bootstrapDataDir == null;
+    return _bootstrapDataDirs == null;
   }
 
   public QuickStartBase setZkExternalAddress(String zkExternalAddress) {
@@ -83,6 +99,10 @@ public abstract class QuickStartBase {
   }
 
   public abstract List<String> types();
+
+  public void runSampleQueries(QuickstartRunner runner)
+      throws Exception {
+  }
 
   protected void waitForBootstrapToComplete(QuickstartRunner runner)
       throws Exception {
@@ -98,6 +118,99 @@ public abstract class QuickStartBase {
   public abstract void execute()
       throws Exception;
 
+  protected List<QuickstartTableRequest> bootstrapOfflineTableDirectories(File quickstartTmpDir)
+      throws IOException {
+    List<QuickstartTableRequest> quickstartTableRequests = new ArrayList<>();
+    for (String directory : getDefaultBatchTableDirectories()) {
+      String tableName = getTableName(directory);
+      File baseDir = new File(quickstartTmpDir, tableName);
+      File dataDir = new File(baseDir, "rawdata");
+      Preconditions.checkState(dataDir.mkdirs());
+      if (useDefaultBootstrapTableDir()) {
+        copyResourceTableToTmpDirectory(directory, tableName, baseDir, dataDir, false);
+      } else {
+        copyFilesystemTableToTmpDirectory(directory, tableName, baseDir);
+      }
+      quickstartTableRequests.add(new QuickstartTableRequest(baseDir.getAbsolutePath()));
+    }
+    return quickstartTableRequests;
+  }
+
+  private static void copyResourceTableToTmpDirectory(String sourcePath, String tableName, File baseDir, File dataDir,
+      boolean isStreamTable)
+      throws IOException {
+    ClassLoader classLoader = Quickstart.class.getClassLoader();
+    // Copy schema
+    URL resource = classLoader.getResource(sourcePath + File.separator + tableName + "_schema.json");
+    Preconditions.checkNotNull(resource, "Missing schema json file for table - " + tableName);
+    File schemaFile = new File(baseDir, tableName + "_schema.json");
+    FileUtils.copyURLToFile(resource, schemaFile);
+
+    // Copy table config
+    String tableConfigFileSuffix = isStreamTable ? "_realtime_table_config.json" : "_offline_table_config.json";
+    File tableConfigFile = new File(baseDir, tableName + tableConfigFileSuffix);
+    String sourceTableConfig = sourcePath + File.separator + tableName + tableConfigFileSuffix;
+    resource = classLoader.getResource(sourceTableConfig);
+    Preconditions.checkNotNull(resource, "Missing table config file for table - " + tableName);
+    FileUtils.copyURLToFile(resource, tableConfigFile);
+
+    // Copy raw data
+    String sourceRawDataPath = sourcePath + File.separator + "rawdata";
+    resource = classLoader.getResource(sourceRawDataPath);
+    if (resource != null) {
+      File rawDataDir = new File(resource.getFile());
+      if (rawDataDir.isDirectory()) {
+        // Copy the directory from `pinot-tools/src/main/resources/examples` directory. This code path is used for
+        // running Quickstart inside IDE, `ClassLoader.getResource()` should source it at build directory,
+        // e.g. `/pinot-tools/target/classes/examples/batch/airlineStats/rawdata`
+        FileUtils.copyDirectory(rawDataDir, dataDir);
+      } else {
+        // Copy the directory recursively from a jar file. This code path is used for running Quickstart using
+        // pinot-admin script. The `ClassLoader.getResource()` should found the resources in the jar file then
+        // decompress it, e.g. `lib/pinot-all-jar-with-dependencies.jar!/examples/batch/airlineStats/rawdata`
+        String[] jarPathSplits = resource.toString().split("!/", 2);
+        JarUtils.copyResourcesToDirectory(jarPathSplits[0], jarPathSplits[1], dataDir.getAbsolutePath());
+      }
+    } else {
+      LOGGER.warn("Not found rawdata directory for table {} from {}", tableName, sourceRawDataPath);
+    }
+
+    if (!isStreamTable) {
+      // Copy ingestion job spec file
+      resource = classLoader.getResource(sourcePath + File.separator + "ingestionJobSpec.yaml");
+      if (resource != null) {
+        File ingestionJobSpecFile = new File(baseDir, "ingestionJobSpec.yaml");
+        FileUtils.copyURLToFile(resource, ingestionJobSpecFile);
+      }
+    }
+  }
+
+  private static void copyFilesystemTableToTmpDirectory(String sourcePath, String tableName, File baseDir)
+      throws IOException {
+    File fileDb = new File(sourcePath);
+
+    if (!fileDb.exists() || !fileDb.isDirectory()) {
+      throw new RuntimeException("Directory " + fileDb.getAbsolutePath() + " not found.");
+    }
+
+    File schemaFile = new File(fileDb, tableName + "_schema.json");
+    if (!schemaFile.exists()) {
+      throw new RuntimeException("Schema file " + schemaFile.getAbsolutePath() + " not found.");
+    }
+
+    File tableFile = new File(fileDb, tableName + "_offline_table_config.json");
+    if (!tableFile.exists()) {
+      throw new RuntimeException("Table table " + tableFile.getAbsolutePath() + " not found.");
+    }
+
+    File data = new File(fileDb, "rawdata" + File.separator + tableName + "_data.csv");
+    if (!data.exists()) {
+      throw new RuntimeException(("Data file " + data.getAbsolutePath() + " not found. "));
+    }
+
+    FileUtils.copyDirectory(fileDb, baseDir);
+  }
+
   protected Map<String, Object> getConfigOverrides() {
     try {
       return StringUtils.isEmpty(_configFilePath) ? ImmutableMap.of()
@@ -105,5 +218,32 @@ public abstract class QuickStartBase {
     } catch (ConfigurationException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  protected String[] getDefaultBatchTableDirectories() {
+    return DEFAULT_OFFLINE_TABLE_DIRECTORIES;
+  }
+
+  public static String prettyPrintResponse(JsonNode response) {
+    StringBuilder responseBuilder = new StringBuilder();
+
+    // Sql Results
+    if (response.has("resultTable")) {
+      JsonNode columns = response.get("resultTable").get("dataSchema").get("columnNames");
+      int numColumns = columns.size();
+      for (int i = 0; i < numColumns; i++) {
+        responseBuilder.append(columns.get(i).asText()).append(TAB);
+      }
+      responseBuilder.append(NEW_LINE);
+      JsonNode rows = response.get("resultTable").get("rows");
+      for (int i = 0; i < rows.size(); i++) {
+        JsonNode row = rows.get(i);
+        for (int j = 0; j < numColumns; j++) {
+          responseBuilder.append(row.get(j).asText()).append(TAB);
+        }
+        responseBuilder.append(NEW_LINE);
+      }
+    }
+    return responseBuilder.toString();
   }
 }
--- pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/QuickStartCommand.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/QuickStartCommand.java
@@ -39,9 +39,9 @@ public class QuickStartCommand extends AbstractBaseAdminCommand implements Comma
       description = "Type of quickstart, supported: STREAM/BATCH/HYBRID")
   private String _type;
 
-  @CommandLine.Option(names = {"-bootstrapTableDir"}, required = false,
-      description = "Directory containing table schema, config, and data.")
-  private String _bootstrapTableDir;
+  @CommandLine.Option(names = {"-bootstrapTableDir"}, required = false, arity = "1..*",
+      description = "A list of Directories, each directory containing table schema, config, and data.")
+  private String[] _bootstrapTableDirs;
 
   @CommandLine.Option(names = {"-tmpDir", "-quickstartDir", "-dataDir"}, required = false,
       description = "Temp Directory to host quickstart data")
@@ -87,11 +87,19 @@ public class QuickStartCommand extends AbstractBaseAdminCommand implements Comma
   }
 
   public String getBootstrapDataDir() {
-    return _bootstrapTableDir;
+    return (_bootstrapTableDirs != null && _bootstrapTableDirs.length > 0) ? _bootstrapTableDirs[0] : null;
+  }
+
+  public String[] getBootstrapDataDirs() {
+    return _bootstrapTableDirs;
   }
 
   public void setBootstrapTableDir(String bootstrapTableDir) {
-    _bootstrapTableDir = bootstrapTableDir;
+    _bootstrapTableDirs = new String[]{bootstrapTableDir};
+  }
+
+  public void setBootstrapTableDirs(String[] bootstrapTableDirs) {
+    _bootstrapTableDirs = bootstrapTableDirs;
   }
 
   public String getZkExternalAddress() {
@@ -125,7 +133,7 @@ public class QuickStartCommand extends AbstractBaseAdminCommand implements Comma
   }
 
   public QuickStartBase selectQuickStart(String type)
-          throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+      throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
     Set<Class<? extends QuickStartBase>> quickStarts = allQuickStarts();
     for (Class<? extends QuickStartBase> quickStart : quickStarts) {
       QuickStartBase quickStartBase = quickStart.getDeclaredConstructor().newInstance();
@@ -134,18 +142,19 @@ public class QuickStartCommand extends AbstractBaseAdminCommand implements Comma
       }
     }
     throw new UnsupportedOperationException("Unsupported QuickStart type: " + type + ". "
-            + "Valid types are: " + errroMessageFor(quickStarts));
+        + "Valid types are: " + errroMessageFor(quickStarts));
   }
 
   @Override
-  public boolean execute() throws Exception {
+  public boolean execute()
+      throws Exception {
     PluginManager.get().init();
 
     if (_type == null) {
       Set<Class<? extends QuickStartBase>> quickStarts = allQuickStarts();
 
       throw new UnsupportedOperationException("No QuickStart type provided. "
-              + "Valid types are: " + errroMessageFor(quickStarts));
+          + "Valid types are: " + errroMessageFor(quickStarts));
     }
 
     QuickStartBase quickstart = selectQuickStart(_type);
@@ -154,8 +163,8 @@ public class QuickStartCommand extends AbstractBaseAdminCommand implements Comma
       quickstart.setDataDir(_tmpDir);
     }
 
-    if (_bootstrapTableDir != null) {
-      quickstart.setBootstrapDataDir(_bootstrapTableDir);
+    if (_bootstrapTableDirs != null) {
+      quickstart.setBootstrapDataDirs(_bootstrapTableDirs);
     }
 
     if (_zkExternalAddress != null) {
@@ -171,7 +180,7 @@ public class QuickStartCommand extends AbstractBaseAdminCommand implements Comma
   }
 
   private static List<String> errroMessageFor(Set<Class<? extends QuickStartBase>> quickStarts)
-          throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+      throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
     List<String> validTypes = new ArrayList<>();
     for (Class<? extends QuickStartBase> quickStart : quickStarts) {
       validTypes.addAll(quickStart.getDeclaredConstructor().newInstance().types());
