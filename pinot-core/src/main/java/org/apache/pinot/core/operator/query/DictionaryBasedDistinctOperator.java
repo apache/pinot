a1748dc9ce28a7c93b1dcdf57b35e0aacdba3684
Xiaotian (Jackie) Jiang <jackie.jxt@gmail.com>
2022-08-21 16:04:16 -0700

    Do not use aggregation result for distinct query in IntermediateResultsBlock

--- pinot-core/src/main/java/org/apache/pinot/core/operator/query/DictionaryBasedDistinctOperator.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/query/DictionaryBasedDistinctOperator.java
@@ -29,7 +29,6 @@ import org.apache.pinot.core.data.table.Record;
 import org.apache.pinot.core.operator.BaseOperator;
 import org.apache.pinot.core.operator.ExecutionStatistics;
 import org.apache.pinot.core.operator.blocks.IntermediateResultsBlock;
-import org.apache.pinot.core.query.aggregation.function.AggregationFunction;
 import org.apache.pinot.core.query.aggregation.function.DistinctAggregationFunction;
 import org.apache.pinot.core.query.distinct.DistinctTable;
 import org.apache.pinot.segment.spi.AggregationFunctionType;
@@ -56,7 +55,6 @@ public class DictionaryBasedDistinctOperator extends BaseOperator<IntermediateRe
   public DictionaryBasedDistinctOperator(FieldSpec.DataType dataType,
       DistinctAggregationFunction distinctAggregationFunction, Dictionary dictionary, int numTotalDocs,
       boolean nullHandlingEnabled) {
-
     _dataType = dataType;
     _distinctAggregationFunction = distinctAggregationFunction;
     _dictionary = dictionary;
@@ -64,10 +62,8 @@ public class DictionaryBasedDistinctOperator extends BaseOperator<IntermediateRe
     _nullHandlingEnabled = nullHandlingEnabled;
 
     List<OrderByExpressionContext> orderByExpressionContexts = _distinctAggregationFunction.getOrderByExpressions();
-
     if (orderByExpressionContexts != null) {
       OrderByExpressionContext orderByExpressionContext = orderByExpressionContexts.get(0);
-
       _isAscending = orderByExpressionContext.isAsc();
       _hasOrderBy = true;
     }
@@ -75,9 +71,7 @@ public class DictionaryBasedDistinctOperator extends BaseOperator<IntermediateRe
 
   @Override
   protected IntermediateResultsBlock getNextBlock() {
-    DistinctTable distinctTable = buildResult();
-    return new IntermediateResultsBlock(new AggregationFunction[]{_distinctAggregationFunction},
-        Collections.singletonList(distinctTable), _nullHandlingEnabled);
+    return new IntermediateResultsBlock(_distinctAggregationFunction, buildResult());
   }
 
   /**
@@ -124,8 +118,9 @@ public class DictionaryBasedDistinctOperator extends BaseOperator<IntermediateRe
         }
       } else {
         // DictionaryBasedDistinctOperator cannot handle nulls.
-        DistinctTable distinctTable = new DistinctTable(
-            dataSchema, _distinctAggregationFunction.getOrderByExpressions(), limit, _nullHandlingEnabled);
+        DistinctTable distinctTable =
+            new DistinctTable(dataSchema, _distinctAggregationFunction.getOrderByExpressions(), limit,
+                _nullHandlingEnabled);
 
         _numDocsScanned = dictLength;
         for (int i = 0; i < dictLength; i++) {
