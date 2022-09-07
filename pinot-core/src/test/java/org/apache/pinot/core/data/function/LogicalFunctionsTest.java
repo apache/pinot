0067b64bbfee96ec178ea8b5a6eef17732d94f50
Yao Liu <yao@startree.ai>
2022-09-07 02:59:01 -0700

    [Feature] Not scalar function (#9338)
    * test
    
    * Add not scalar function to handle post-agregate not

---
+++ pinot-core/src/test/java/org/apache/pinot/core/data/function/LogicalFunctionsTest.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.core.data.function;
+
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.pinot.segment.local.function.InbuiltFunctionEvaluator;
+import org.apache.pinot.spi.data.readers.GenericRow;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+/**
+ * Tests the scalar transform functions for logical operators
+ * Currently, only not operator is supported.
+ */
+public class LogicalFunctionsTest {
+
+  private void testFunction(String functionExpression, List<String> expectedArguments, GenericRow row,
+      Object expectedResult) {
+    InbuiltFunctionEvaluator evaluator = new InbuiltFunctionEvaluator(functionExpression);
+    Assert.assertEquals(evaluator.getArguments(), expectedArguments);
+    Assert.assertEquals(evaluator.evaluate(row), expectedResult);
+  }
+
+  @Test(dataProvider = "logicalFunctionDataProvider")
+  public void testArithmeticFunctions(String functionExpression, List<String> expectedArguments, GenericRow row,
+      Object expectedResult) {
+    testFunction(functionExpression, expectedArguments, row, expectedResult);
+  }
+
+  @DataProvider(name = "logicalFunctionDataProvider")
+  public Object[][] arithmeticFunctionsDataProvider() {
+    List<Object[]> inputs = new ArrayList<>();
+
+    GenericRow row0 = new GenericRow();
+    row0.putValue("a", true);
+    inputs.add(new Object[]{"not a", Lists.newArrayList("a"), row0, false});
+
+    GenericRow row1 = new GenericRow();
+    row1.putValue("b", false);
+    inputs.add(new Object[]{"not b", Lists.newArrayList("b"), row1, true});
+
+    return inputs.toArray(new Object[0][]);
+  }
+}
