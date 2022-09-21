111d03cb3939f82e23669a6c160e8bdea407e257
Rong Rong <rongr@startree.ai>
2022-09-21 11:25:03 -0700

    adding rules to only convert non-point searches

--- pinot-query-planner/src/main/java/org/apache/calcite/rel/rules/PinotFilterExpandSearchRule.java
+++ pinot-query-planner/src/main/java/org/apache/calcite/rel/rules/PinotFilterExpandSearchRule.java
@@ -1,13 +1,17 @@
 package org.apache.calcite.rel.rules;
 
+import java.util.List;
 import org.apache.calcite.plan.RelOptRule;
 import org.apache.calcite.plan.RelOptRuleCall;
 import org.apache.calcite.rel.core.Filter;
 import org.apache.calcite.rel.logical.LogicalFilter;
+import org.apache.calcite.rex.RexCall;
+import org.apache.calcite.rex.RexLiteral;
 import org.apache.calcite.rex.RexNode;
 import org.apache.calcite.rex.RexUtil;
 import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.tools.RelBuilderFactory;
+import org.apache.calcite.util.Sarg;
 
 
 public class PinotFilterExpandSearchRule extends RelOptRule {
@@ -19,13 +23,20 @@ public class PinotFilterExpandSearchRule extends RelOptRule {
   }
 
   @Override
+  @SuppressWarnings("rawtypes")
   public boolean matches(RelOptRuleCall call) {
     if (call.rels.length < 1) {
       return false;
     }
     if (call.rel(0) instanceof Filter) {
       Filter filter = call.rel(0);
-      return filter.getCondition().getKind() == SqlKind.SEARCH;
+      if (filter.getCondition().getKind() == SqlKind.SEARCH) {
+        RexCall rexCall = (RexCall) filter.getCondition();
+        List<RexNode> operands = rexCall.getOperands();
+        RexLiteral rexLiteral = (RexLiteral) operands.get(1);
+        Sarg sarg = rexLiteral.getValueAs(Sarg.class);
+        return sarg != null && !sarg.isPoints() && !sarg.isComplementedPoints();
+      }
     }
     return false;
   }
