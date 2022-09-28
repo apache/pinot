01160339c3263d0ac25d4a3d9d8870f8e9ffc29d
Jia Guo <jiaguo@linkedin.com>
2022-09-28 02:56:24 -0700

    Add test case

--- pinot-core/src/test/java/org/apache/pinot/queries/ScanBasedANDFilterReorderingTest.java
+++ pinot-core/src/test/java/org/apache/pinot/queries/ScanBasedANDFilterReorderingTest.java
@@ -18,93 +18,188 @@
  */
 package org.apache.pinot.queries;
 
+import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import org.apache.pinot.common.utils.DataSchema;
-import org.apache.pinot.core.operator.BaseOperator;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
 import org.apache.pinot.core.operator.ExecutionStatistics;
-import org.apache.pinot.core.operator.blocks.results.SelectionResultsBlock;
+import org.apache.pinot.core.operator.blocks.results.AggregationResultsBlock;
+import org.apache.pinot.core.operator.query.AggregationOperator;
+import org.apache.pinot.segment.local.indexsegment.immutable.ImmutableSegmentLoader;
+import org.apache.pinot.segment.local.segment.creator.impl.SegmentIndexCreationDriverImpl;
+import org.apache.pinot.segment.local.segment.index.loader.IndexLoadingConfig;
+import org.apache.pinot.segment.spi.ImmutableSegment;
+import org.apache.pinot.segment.spi.IndexSegment;
+import org.apache.pinot.segment.spi.creator.SegmentGeneratorConfig;
+import org.apache.pinot.segment.spi.creator.SegmentIndexCreationDriver;
+import org.apache.pinot.spi.config.table.TableConfig;
+import org.apache.pinot.spi.config.table.TableType;
+import org.apache.pinot.spi.config.table.ingestion.IngestionConfig;
+import org.apache.pinot.spi.data.FieldSpec;
+import org.apache.pinot.spi.data.Schema;
+import org.apache.pinot.spi.data.TimeGranularitySpec;
 import org.apache.pinot.spi.utils.CommonConstants;
+import org.apache.pinot.spi.utils.builder.TableConfigBuilder;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertNotNull;
+
 
 /**
- * <p>There are totally 18 columns, 30000 records inside the original Avro file where 11 columns are selected to build
- * the index segment "test_data-sv.avro". Selected columns information are as following:
+ * The <code>BaseMultiValueQueriesTest</code> class sets up the index segment for the multi-value queries test.
+ * <p>There are totally 14 columns, 100000 records inside the original Avro file where 10 columns are selected to build
+ * the index segment. Selected columns information are as following:
  * <ul>
- *   ColumnName, FieldType, DataType, Cardinality, IsSorted, HasInvertedIndex
- *   <li>column1, METRIC, INT, 6582, F, F</li>
- *   <li>column3, METRIC, INT, 21910, F, F</li>
- *   <li>column5, DIMENSION, STRING, 1, T, F</li>
- *   <li>column6, DIMENSION, INT, 608, F, T</li>
- *   <li>column7, DIMENSION, INT, 146, F, T</li>
- *   <li>column9, DIMENSION, INT, 1737, F, F</li>
- *   <li>column11, DIMENSION, STRING, 5, F, T</li>
- *   <li>column12, DIMENSION, STRING, 5, F, F</li>
- *   <li>column17, METRIC, INT, 24, F, T</li>
- *   <li>column18, METRIC, INT, 1440, F, T</li>
- *   <li>daysSinceEpoch, TIME, INT, 2, T, F</li>
+ *   ColumnName, FieldType, DataType, Cardinality, IsSorted, HasInvertedIndex, IsMultiValueRaw
+ *   <li>column1, METRIC, INT, 51594, F, F, F</li>
+ *   <li>column2, METRIC, INT, 42242, F, F, F</li>
+ *   <li>column3, DIMENSION, STRING, 5, F, T, F</li>
+ *   <li>column4, DIMENSION, STRING, 5, F, F, F</li>
+ *   <li>column5, DIMENSION, STRING, 9, F, F, F</li>
+ *   <li>column6, DIMENSION, INT, 18499, F, F, T</li>
+ *   <li>column7, DIMENSION, INT, 359, F, F, T</li>
+ *   <li>column8, DIMENSION, INT, 850, F, T, F</li>
+ *   <li>column9, METRIC, INT, 146, F, T, F</li>
+ *   <li>column10, METRIC, INT, 3960, F, F, F</li>
+ *   <li>daysSinceEpoch, TIME, INT, 1, T, F, F</li>
  * </ul>
  */
-public class ScanBasedANDFilterReorderingTest extends InnerSegmentSelectionSingleValueQueriesTest {
-  private static final String SELECT_STAR_QUERY = "SELECT * FROM testTable";
-  private static final String SET_AND_OPTIMIZATION = "SET "
-      + CommonConstants.Broker.Request.QueryOptionKey.AND_SCAN_REORDERING + " = 'True';";
-
-  protected static final String FILTER2 =
-      " WHERE column1 in (1236291842, 1123080522) AND column3 in (455144658, 2134396823) "
-          + "AND column17 IN (635942547, 1785555661, 1284373442, 1618904660, 561673250, 1727768835, 423049234, 1799989276)";
+public class ScanBasedANDFilterReorderingTest {
 
   @Test
-  public void testSelectStar() {
-    // Test query with filter
-    BaseOperator<SelectionResultsBlock> selectionOnlyOperator = getOperator(SELECT_STAR_QUERY + FILTER2);
-    SelectionResultsBlock resultsBlock = selectionOnlyOperator.nextBlock();
-    ExecutionStatistics executionStatistics = selectionOnlyOperator.getExecutionStatistics();
-//    assertEquals(executionStatistics.getNumDocsScanned(), 10L);
-//    assertEquals(executionStatistics.getNumEntriesScannedInFilter(), 48241L);
-//    assertEquals(executionStatistics.getNumEntriesScannedPostFilter(), 110L);
-//    assertEquals(executionStatistics.getNumTotalDocs(), 30000L);
-    DataSchema selectionDataSchema = resultsBlock.getDataSchema();
-    Map<String, Integer> columnIndexMap = computeColumnNameToIndexMap(selectionDataSchema);
-//    assertEquals(selectionDataSchema.size(), 11);
-//    assertEquals(getVirtualColumns(selectionDataSchema), 0);
-//    assertTrue(columnIndexMap.containsKey("column1"));
-//    assertTrue(columnIndexMap.containsKey("column11"));
-//    assertEquals(selectionDataSchema.getColumnDataType(columnIndexMap.get("column1")), DataSchema.ColumnDataType.INT);
-//    assertEquals(selectionDataSchema.getColumnDataType(columnIndexMap.get("column11")),
-//        DataSchema.ColumnDataType.STRING);
-    List<Object[]> selectionResult = (List<Object[]>) resultsBlock.getRows();
-//    assertEquals(selectionResult.size(), 10);
-    Object[] firstRow;
-//    assertEquals(firstRow.length, 11);
-//    assertEquals(((Integer) firstRow[columnIndexMap.get("column1")]).intValue(), 351823652);
-//    assertEquals((String) firstRow[columnIndexMap.get("column11")], "t");
+  public static class Test1 extends BaseQueriesTest {
+    private static final String AVRO_DATA = "data" + File.separator + "test_data-mv.avro";
+    private static final String SEGMENT_NAME = "testTable_1756015683_1756015683";
+    private static final File INDEX_DIR = new File(FileUtils.getTempDirectory(), "MultiValueRawQueriesTest");
+    private static final String SET_AND_OPTIMIZATION = "SET "
+        + CommonConstants.Broker.Request.QueryOptionKey.AND_SCAN_REORDERING + " = 'True';";
 
-    // Test query with filter + and optimization; reduce NumEntriesScannedInFilter 48241L->35905L
-    selectionOnlyOperator = getOperator(SET_AND_OPTIMIZATION + SELECT_STAR_QUERY + FILTER2);
-    resultsBlock = selectionOnlyOperator.nextBlock();
-    executionStatistics = selectionOnlyOperator.getExecutionStatistics();
-//    assertEquals(executionStatistics.getNumDocsScanned(), 10L);
-    assertEquals(executionStatistics.getNumEntriesScannedInFilter(), 48241L);
-    assertEquals(executionStatistics.getNumEntriesScannedPostFilter(), 110L);
-    assertEquals(executionStatistics.getNumTotalDocs(), 30000L);
-    selectionDataSchema = resultsBlock.getDataSchema();
-    columnIndexMap = computeColumnNameToIndexMap(selectionDataSchema);
-    assertEquals(selectionDataSchema.size(), 11);
-    assertEquals(getVirtualColumns(selectionDataSchema), 0);
-    assertTrue(columnIndexMap.containsKey("column1"));
-    assertTrue(columnIndexMap.containsKey("column11"));
-    assertEquals(selectionDataSchema.getColumnDataType(columnIndexMap.get("column1")), DataSchema.ColumnDataType.INT);
-    assertEquals(selectionDataSchema.getColumnDataType(columnIndexMap.get("column11")),
-        DataSchema.ColumnDataType.STRING);
-    selectionResult = (List<Object[]>) resultsBlock.getRows();
-    assertEquals(selectionResult.size(), 10);
-    firstRow = selectionResult.get(0);
-    assertEquals(firstRow.length, 11);
-    assertEquals(((Integer) firstRow[columnIndexMap.get("column1")]).intValue(), 351823652);
-    assertEquals((String) firstRow[columnIndexMap.get("column11")], "t");
+    protected static final String COUNT_STAR_QUERY = "SELECT SUM(column1) FROM testTable";
+
+    protected static final String FILTER1 = " WHERE column7 IN (2147483647, 211, 336, 363, 469, 565)"
+        + " AND column6 = 2147483647"
+        + " AND column3 <> 'L'";
+
+    protected static final String FILTER2 = " WHERE column1 > 100000000"
+        + " AND column2 BETWEEN 20000000 AND 1000000000"
+        + " AND column3 <> 'w'"
+        + " AND daysSinceEpoch = 1756015683";
+
+    protected static final String FILTER3 = " WHERE column1 > 100000000"
+        + " AND column2 BETWEEN 20000000 AND 1000000000"
+        + " AND column3 <> 'w'"
+        + " AND daysSinceEpoch = 1756015683";
+
+    private IndexSegment _indexSegment;
+    // Contains 2 identical index segments.
+    private List<IndexSegment> _indexSegments;
+
+    @BeforeTest
+    public void buildSegment()
+        throws Exception {
+      FileUtils.deleteQuietly(INDEX_DIR);
+
+      // Get resource file path.
+      URL resource = getClass().getClassLoader().getResource(AVRO_DATA);
+      assertNotNull(resource);
+      String filePath = resource.getFile();
+
+      // Build the segment schema.
+      Schema schema = new Schema.SchemaBuilder().setSchemaName("testTable").addMetric("column1", FieldSpec.DataType.INT)
+          .addMetric("column2", FieldSpec.DataType.INT).addSingleValueDimension("column3", FieldSpec.DataType.STRING)
+          .addSingleValueDimension("column4", FieldSpec.DataType.STRING)
+          .addSingleValueDimension("column5", FieldSpec.DataType.STRING)
+          .addMultiValueDimension("column6", FieldSpec.DataType.INT)
+          .addMultiValueDimension("column7", FieldSpec.DataType.INT)
+          .addSingleValueDimension("column8", FieldSpec.DataType.INT).addMetric("column9", FieldSpec.DataType.INT)
+          .addMetric("column10", FieldSpec.DataType.INT)
+          .addTime(new TimeGranularitySpec(FieldSpec.DataType.INT, TimeUnit.DAYS, "daysSinceEpoch"), null).build();
+      // The segment generation code in SegmentColumnarIndexCreator will throw
+      // exception if start and end time in time column are not in acceptable
+      // range. For this test, we first need to fix the input avro data
+      // to have the time column values in allowed range. Until then, the check
+      // is explicitly disabled
+      IngestionConfig ingestionConfig = new IngestionConfig();
+      ingestionConfig.setSegmentTimeValueCheck(false);
+      ingestionConfig.setRowTimeValueCheck(false);
+      TableConfig tableConfig = new TableConfigBuilder(TableType.OFFLINE).setTableName("testTable")
+          .setTimeColumnName("daysSinceEpoch")
+          .setIngestionConfig(ingestionConfig).build();
+
+      // Create the segment generator config.
+      SegmentGeneratorConfig segmentGeneratorConfig = new SegmentGeneratorConfig(tableConfig, schema);
+      segmentGeneratorConfig.setInputFilePath(filePath);
+      segmentGeneratorConfig.setTableName("testTable");
+      segmentGeneratorConfig.setOutDir(INDEX_DIR.getAbsolutePath());
+      segmentGeneratorConfig.setInvertedIndexCreationColumns(Arrays.asList("column3", "column8", "column9"));
+
+      // Build the index segment.
+      SegmentIndexCreationDriver driver = new SegmentIndexCreationDriverImpl();
+      driver.init(segmentGeneratorConfig);
+      driver.build();
+    }
+
+    @BeforeClass
+    public void loadSegment()
+        throws Exception {
+      IndexLoadingConfig indexLoadingConfig = new IndexLoadingConfig();
+      indexLoadingConfig.setInvertedIndexColumns(
+          new HashSet<>(Arrays.asList("column3", "column8", "column9")));
+      ImmutableSegment immutableSegment =
+          ImmutableSegmentLoader.load(new File(INDEX_DIR, SEGMENT_NAME), indexLoadingConfig);
+      _indexSegment = immutableSegment;
+      _indexSegments = Arrays.asList(immutableSegment, immutableSegment);
+    }
+
+    @AfterClass
+    public void destroySegment() {
+      _indexSegment.destroy();
+    }
+
+    @AfterTest
+    public void deleteSegment() {
+      FileUtils.deleteQuietly(INDEX_DIR);
+    }
+
+    @Override
+    protected String getFilter() {
+      return FILTER1;
+    }
+
+    @Override
+    protected IndexSegment getIndexSegment() {
+      return _indexSegment;
+    }
+
+    @Override
+    protected List<IndexSegment> getIndexSegments() {
+      return _indexSegments;
+    }
+
+    public void testMultiValueAggregationOnly() {
+      // Test query with optimization
+      AggregationOperator aggregationOperator = getOperator(SET_AND_OPTIMIZATION + COUNT_STAR_QUERY + FILTER1);
+      AggregationResultsBlock resultsBlock = aggregationOperator.nextBlock();
+      ExecutionStatistics executionStatistics = aggregationOperator.getExecutionStatistics();
+      QueriesTestUtils.testInnerSegmentExecutionStatistics(executionStatistics, 46649L, 154999L,
+          46649L, 100000L);
+      Assert.assertEquals(((Number) resultsBlock.getResults().get(0)).longValue(), 44224075056091L);
+
+      // Test query with optimization
+      aggregationOperator = getOperator(COUNT_STAR_QUERY + FILTER1);
+      resultsBlock = aggregationOperator.nextBlock();
+      executionStatistics = aggregationOperator.getExecutionStatistics();
+      QueriesTestUtils.testInnerSegmentExecutionStatistics(executionStatistics, 46649L, 189513L,
+          46649L, 100000L);
+      Assert.assertEquals(((Number) resultsBlock.getResults().get(0)).longValue(), 44224075056091L);
+    }
   }
 }
