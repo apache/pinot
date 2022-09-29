c3908d3af87b809a848627c4e8fdc3cc98c37d63
Yao Liu <yao@startree.ai>
2022-09-28 20:58:49 -0700

    coalesce scalar

--- pinot-common/src/main/java/org/apache/pinot/common/function/scalar/ObjectFunctions.java
+++ pinot-common/src/main/java/org/apache/pinot/common/function/scalar/ObjectFunctions.java
@@ -18,11 +18,22 @@
  */
 package org.apache.pinot.common.function.scalar;
 
+import java.util.Arrays;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.apache.pinot.spi.annotations.ScalarFunction;
 
 public class ObjectFunctions {
   private ObjectFunctions() {
+  }
+
+  private static Object coalesce(List<Object> objects) {
+    for (Object o : objects) {
+      if (o != null) {
+        return o;
+      }
+    }
+    return null;
   }
 
   @ScalarFunction(nullableParameters = true)
@@ -49,5 +60,31 @@ public class ObjectFunctions {
   @ScalarFunction(nullableParameters = true)
   public static boolean isNotDistinctFrom(@Nullable Object obj1, @Nullable Object obj2) {
     return !isDistinctFrom(obj1, obj2);
+  }
+
+  public static Object coalesce(@Nullable Object obj) {
+    return coalesce(Arrays.asList(obj));
+  }
+
+  @ScalarFunction(nullableParameters = true)
+  public static Object coalesce(@Nullable Object obj1, @Nullable Object obj2) {
+    return coalesce(Arrays.asList(obj1, obj2));
+  }
+
+  @ScalarFunction(nullableParameters = true)
+  public static Object coalesce(@Nullable Object obj1, @Nullable Object obj2, @Nullable Object obj3) {
+    return coalesce(Arrays.asList(obj1, obj2, obj3));
+  }
+
+  @ScalarFunction(nullableParameters = true)
+  public static Object coalesce(@Nullable Object obj1, @Nullable Object obj2, @Nullable Object obj3,
+      @Nullable Object obj4) {
+    return coalesce(Arrays.asList(obj1, obj2, obj3, obj4));
+  }
+
+  @ScalarFunction(nullableParameters = true)
+  public static Object coalesce(@Nullable Object obj1, @Nullable Object obj2, @Nullable Object obj3,
+      @Nullable Object obj4, @Nullable Object obj5) {
+    return coalesce(Arrays.asList(obj1, obj2, obj3, obj4, obj5));
   }
 }
