429d5aad307aab508d9448f26e398be15d437bae
Xiaobing Li <kaneleesince2020@gmail.com>
2022-10-04 15:13:07 -0700

    comment the config for clarity

--- pinot-controller/src/main/java/org/apache/pinot/controller/ControllerConf.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/ControllerConf.java
@@ -148,6 +148,8 @@ public class ControllerConf extends PinotConfiguration {
         "controller.minion.task.metrics.emitter.frequencyPeriod";
 
     public static final String PINOT_TASK_MANAGER_SCHEDULER_ENABLED = "controller.task.scheduler.enabled";
+    // This is the expiry for the ended tasks. Helix cleans up the task info from ZK after the expiry time from the
+    // end of the task.
     public static final String PINOT_TASK_EXPIRE_TIME_MS = "controller.task.expire.time.ms";
 
     @Deprecated
