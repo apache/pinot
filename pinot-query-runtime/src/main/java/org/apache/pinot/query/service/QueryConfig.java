fa155befab3b52c35483fe72359ec49a1112ee51
Rong Rong <rongr@apache.org>
2022-10-10 16:13:24 -0700

    SSB integration test  (#9559)
    - clean up the CSVs into AVROs so that it loads much faster
    - create integration test against H2
    - rollback a change no longer necessary after #9485
    
    however, severe thread thrashing is observed when JVM available core count < 4. tracked in #9563
    
    Co-authored-by: Rong Rong <rongr@startree.ai>

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/service/QueryConfig.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/service/QueryConfig.java
@@ -24,8 +24,8 @@ package org.apache.pinot.query.service;
 public class QueryConfig {
   public static final long DEFAULT_TIMEOUT_NANO = 10_000_000_000L;
 
-  public static final String KEY_OF_MAX_INBOUND_QUERY_DATA_BLOCK_BYTES_SIZE = "pinot.query.runner.max.msg.size";
-  public static final int DEFAULT_MAX_INBOUND_QUERY_DATA_BLOCK_BYTES_SIZE = 128 * 1024 * 1024;
+  public static final String KEY_OF_MAX_INBOUND_QUERY_DATA_BLOCK_SIZE_BYTES = "pinot.query.runner.max.msg.size.bytes";
+  public static final int DEFAULT_MAX_INBOUND_QUERY_DATA_BLOCK_SIZE_BYTES = 16 * 1024 * 1024;
 
   public static final String KEY_OF_QUERY_SERVER_PORT = "pinot.query.server.port";
   public static final int DEFAULT_QUERY_SERVER_PORT = 0;
