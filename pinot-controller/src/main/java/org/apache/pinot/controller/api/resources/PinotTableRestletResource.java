0f4b53b525d7b86070aece39557e73165522fb56
Benson Yuan <yuanbenson1772@gmail.com>
2022-10-21 11:40:04 -0700

    Reverts recommender REST API back to PUT (reverts PR #9326)

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotTableRestletResource.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotTableRestletResource.java
@@ -239,7 +239,7 @@ public class PinotTableRestletResource {
     }
   }
 
-  @GET
+  @PUT
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/tables/recommender")
   @ApiOperation(value = "Recommend config", notes = "Recommend a config with input json")
