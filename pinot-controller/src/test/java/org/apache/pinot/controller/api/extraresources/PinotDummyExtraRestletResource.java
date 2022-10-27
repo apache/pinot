cc371e4b5d35745cb19794f3ca370e823202fcc6
Xiaotian (Jackie) Jiang <jackie.jxt@gmail.com>
2022-10-27 15:42:35 -0700

    Fix extra restlet resource test which should be stateless

--- pinot-controller/src/test/java/org/apache/pinot/controller/api/extraresources/PinotDummyExtraRestletResource.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/api/extraresources/PinotDummyExtraRestletResource.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pinot.controller.api.resources.extrapackage;
+package org.apache.pinot.controller.api.extraresources;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
