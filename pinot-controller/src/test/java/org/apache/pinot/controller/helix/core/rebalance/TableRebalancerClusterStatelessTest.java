3d57346ff30a4efcb09c6a23b5564c5fbb1be202
David Y <dmyang@users.noreply.github.com>
2022-10-27 20:27:34 -0700

    Separate noop test

--- pinot-controller/src/test/java/org/apache/pinot/controller/helix/core/rebalance/TableRebalancerClusterStatelessTest.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/helix/core/rebalance/TableRebalancerClusterStatelessTest.java
@@ -78,6 +78,54 @@ public class TableRebalancerClusterStatelessTest extends ControllerTest {
     addFakeBrokerInstancesToAutoJoinHelixCluster(1, true);
   }
 
+  @Test
+  public void testRebalanceNoop() throws Exception {
+    int numServers = 3;
+    for (int i = 0; i < numServers; i++) {
+      addFakeServerInstanceToAutoJoinHelixCluster(SERVER_INSTANCE_ID_PREFIX + i, true);
+    }
+
+    TableRebalancer tableRebalancer = new TableRebalancer(_helixManager);
+    TableConfig tableConfig =
+        new TableConfigBuilder(TableType.OFFLINE).setTableName(RAW_TABLE_NAME).setNumReplicas(NUM_REPLICAS).build();
+
+    // Create the table
+    _helixResourceManager.addTable(tableConfig);
+
+    // Add the segments
+    int numSegments = 10;
+    for (int i = 0; i < numSegments; i++) {
+      _helixResourceManager.addNewSegment(OFFLINE_TABLE_NAME,
+          SegmentMetadataMockUtils.mockSegmentMetadata(RAW_TABLE_NAME, SEGMENT_NAME_PREFIX + i), null);
+    }
+
+    // Table is already balanced so rebalances should all return NO_OP
+
+    // Rebalance in dry-run mode should return NO_OP status
+    Configuration rebalanceConfig = new BaseConfiguration();
+    RebalanceResult rebalanceResult = tableRebalancer.rebalance(tableConfig, new BaseConfiguration());
+    rebalanceConfig.addProperty(RebalanceConfigConstants.DRY_RUN, true);
+    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
+
+    // Rebalance in dry-run mode with reassignInstances should return NO_OP status
+    rebalanceConfig = new BaseConfiguration();
+    rebalanceConfig.addProperty(RebalanceConfigConstants.DRY_RUN, true);
+    rebalanceConfig.addProperty(RebalanceConfigConstants.REASSIGN_INSTANCES, true);
+    rebalanceResult = tableRebalancer.rebalance(tableConfig, rebalanceConfig);
+    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
+
+    // Rebalance should return NO_OP status
+    rebalanceConfig = new BaseConfiguration();
+    rebalanceResult = tableRebalancer.rebalance(tableConfig, rebalanceConfig);
+    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
+
+    // Rebalance with reassignInstances should return NO_OP status
+    rebalanceConfig = new BaseConfiguration();
+    rebalanceConfig.addProperty(RebalanceConfigConstants.REASSIGN_INSTANCES, true);
+    rebalanceResult = tableRebalancer.rebalance(tableConfig, rebalanceConfig);
+    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
+  }
+
   /**
    * Dropping instance from cluster requires waiting for live instance gone and removing instance related ZNodes, which
    * are not the purpose of the test, so combine different rebalance scenarios into one test:
@@ -111,34 +159,10 @@ public class TableRebalancerClusterStatelessTest extends ControllerTest {
     int numSegments = 10;
     for (int i = 0; i < numSegments; i++) {
       _helixResourceManager.addNewSegment(OFFLINE_TABLE_NAME,
-          SegmentMetadataMockUtils.mockSegmentMetadata(RAW_TABLE_NAME, SEGMENT_NAME_PREFIX + i), null);
+      SegmentMetadataMockUtils.mockSegmentMetadata(RAW_TABLE_NAME, SEGMENT_NAME_PREFIX + i), null);
     }
     Map<String, Map<String, String>> oldSegmentAssignment =
         _helixResourceManager.getTableIdealState(OFFLINE_TABLE_NAME).getRecord().getMapFields();
-
-    // Rebalance in dry-run mode should return NO_OP status
-    rebalanceConfig = new BaseConfiguration();
-    rebalanceResult = tableRebalancer.rebalance(tableConfig, new BaseConfiguration());
-    rebalanceConfig.addProperty(RebalanceConfigConstants.DRY_RUN, true);
-    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
-
-    // Rebalance in dry-run mode with reassignInstances should return NO_OP status
-    rebalanceConfig = new BaseConfiguration();
-    rebalanceConfig.addProperty(RebalanceConfigConstants.DRY_RUN, true);
-    rebalanceConfig.addProperty(RebalanceConfigConstants.REASSIGN_INSTANCES, true);
-    rebalanceResult = tableRebalancer.rebalance(tableConfig, rebalanceConfig);
-    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
-
-    // Rebalance should return NO_OP status
-    rebalanceConfig = new BaseConfiguration();
-    rebalanceResult = tableRebalancer.rebalance(tableConfig, rebalanceConfig);
-    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
-
-    // Rebalance with reassignInstances should return NO_OP status
-    rebalanceConfig = new BaseConfiguration();
-    rebalanceConfig.addProperty(RebalanceConfigConstants.REASSIGN_INSTANCES, true);
-    rebalanceResult = tableRebalancer.rebalance(tableConfig, rebalanceConfig);
-    assertEquals(rebalanceResult.getStatus(), RebalanceResult.Status.NO_OP);
 
     // All servers should be assigned to the table
     Map<InstancePartitionsType, InstancePartitions> instanceAssignment = rebalanceResult.getInstanceAssignment();
