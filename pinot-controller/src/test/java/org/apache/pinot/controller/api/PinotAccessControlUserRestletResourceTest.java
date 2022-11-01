ce99d5cf88f1e7f3d6cbd47fcd45b97253635efd
Xiaotian (Jackie) Jiang <jackie.jxt@gmail.com>
2022-11-01 14:56:10 -0700

    [Clean up] Auto reformat controller tests

--- pinot-controller/src/test/java/org/apache/pinot/controller/api/PinotAccessControlUserRestletResourceTest.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/api/PinotAccessControlUserRestletResourceTest.java
@@ -28,111 +28,110 @@ import org.apache.pinot.spi.config.user.UserConfig;
 import org.apache.pinot.spi.utils.JsonUtils;
 import org.apache.pinot.spi.utils.StringUtil;
 import org.apache.pinot.spi.utils.builder.UserConfigBuilder;
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
+
+
 public class PinotAccessControlUserRestletResourceTest {
-    private static final ControllerTest TEST_INSTANCE = ControllerTest.getInstance();
+  private static final ControllerTest TEST_INSTANCE = ControllerTest.getInstance();
 
-    private String _createUserUrl;
-    private final UserConfigBuilder _userConfigBuilder = new UserConfigBuilder();
+  private String _createUserUrl;
+  private final UserConfigBuilder _userConfigBuilder = new UserConfigBuilder();
 
-    @BeforeClass
-    public void setup()
-        throws Exception {
-        TEST_INSTANCE.setupSharedStateAndValidate();
+  @BeforeClass
+  public void setup()
+      throws Exception {
+    TEST_INSTANCE.setupSharedStateAndValidate();
 
-        _createUserUrl = TEST_INSTANCE.getControllerRequestURLBuilder().forUserCreate();
-        _userConfigBuilder.setUsername("testUser").setPassword("123456").setComponentType(ComponentType.CONTROLLER)
-            .setRoleType(RoleType.USER);
+    _createUserUrl = TEST_INSTANCE.getControllerRequestURLBuilder().forUserCreate();
+    _userConfigBuilder.setUsername("testUser").setPassword("123456").setComponentType(ComponentType.CONTROLLER)
+        .setRoleType(RoleType.USER);
+  }
+
+  @Test
+  public void testAddUser()
+      throws Exception {
+    String userConfigString = _userConfigBuilder.setUsername("bad.user.with.dot").build().toJsonString();
+    try {
+      ControllerTest.sendPostRequest(_createUserUrl, userConfigString);
+      fail("Adding a user with dot in username does not fail");
+    } catch (IOException e) {
+      assertTrue(e.getMessage().contains("Username: bad.user.with.dot containing '.' or space is not allowed"));
     }
 
-    @Test
-    public void testAddUser()
-        throws Exception {
-        String userconfigString = _userConfigBuilder.setUsername("bad.user.with.dot").build().toJsonString();
-        try {
-            ControllerTest.sendPostRequest(_createUserUrl, userconfigString);
-            Assert.fail("Adding a user with dot in username does not fail");
-        } catch (IOException e) {
-            Assert.assertTrue(e.getMessage().contains(
-              "Username: bad.user.with.dot containing '.' or space is not allowed"));
-        }
+    // Creating a user with a valid username should succeed
+    userConfigString = _userConfigBuilder.setUsername("valid_table_name").build().toJsonString();
+    ControllerTest.sendPostRequest(_createUserUrl, userConfigString);
 
-        // Creating an user with a valid username should succeed
-        userconfigString = _userConfigBuilder.setUsername("valid_table_name").build().toJsonString();
-        ControllerTest.sendPostRequest(_createUserUrl, userconfigString);
-
-        // Create an user that already exists should fail
-        try {
-            ControllerTest.sendPostRequest(_createUserUrl, userconfigString);
-            Assert.fail("Creation of an existing user does not fail");
-        } catch (IOException e) {
-            Assert.assertTrue(e.getMessage().contains("User valid_table_name_CONTROLLER already exists"));
-        }
+    // Create a user that already exists should fail
+    try {
+      ControllerTest.sendPostRequest(_createUserUrl, userConfigString);
+      fail("Creation of an existing user does not fail");
+    } catch (IOException e) {
+      assertTrue(e.getMessage().contains("User valid_table_name_CONTROLLER already exists"));
     }
+  }
 
-    private UserConfig getUserConfig(String username, String componentType)
-        throws Exception {
-        String userConfigString =
-            ControllerTest.sendGetRequest(TEST_INSTANCE.getControllerRequestURLBuilder()
-                .forUserGet(username, componentType));
-        String usernameWithType = username + "_" + componentType;
-        return JsonUtils.jsonNodeToObject(JsonUtils.stringToJsonNode(userConfigString)
-            .get(usernameWithType), UserConfig.class);
-    }
+  private UserConfig getUserConfig(String username, String componentType)
+      throws Exception {
+    String userConfigString = ControllerTest.sendGetRequest(
+        TEST_INSTANCE.getControllerRequestURLBuilder().forUserGet(username, componentType));
+    String usernameWithType = username + "_" + componentType;
+    return JsonUtils.jsonNodeToObject(JsonUtils.stringToJsonNode(userConfigString).get(usernameWithType),
+        UserConfig.class);
+  }
 
-    @Test
-    public void testUpdateUserConfig()
-        throws Exception {
-        String username = "updateTC";
-        String userConfigString = _userConfigBuilder.setUsername(username).setComponentType(ComponentType.CONTROLLER)
-            .build().toJsonString();
-        ControllerTest.sendPostRequest(_createUserUrl, userConfigString);
-        // user creation should succeed
-        UserConfig userConfig = getUserConfig(username, "CONTROLLER");
-        Assert.assertEquals(userConfig.getRoleType().toString(), RoleType.USER.toString());
-        Assert.assertTrue(BcryptUtils.checkpw("123456", userConfig.getPassword()));
-        userConfig.setRole("ADMIN");
-        userConfig.setPassword("654321");
+  @Test
+  public void testUpdateUserConfig()
+      throws Exception {
+    String username = "updateTC";
+    String userConfigString =
+        _userConfigBuilder.setUsername(username).setComponentType(ComponentType.CONTROLLER).build().toJsonString();
+    ControllerTest.sendPostRequest(_createUserUrl, userConfigString);
+    // user creation should succeed
+    UserConfig userConfig = getUserConfig(username, "CONTROLLER");
+    assertEquals(userConfig.getRoleType().toString(), RoleType.USER.toString());
+    assertTrue(BcryptUtils.checkpw("123456", userConfig.getPassword()));
+    userConfig.setRole("ADMIN");
+    userConfig.setPassword("654321");
 
-        JsonNode jsonResponse = JsonUtils.stringToJsonNode(ControllerTest
-            .sendPutRequest(TEST_INSTANCE.getControllerRequestURLBuilder()
-                    .forUpdateUserConfig(username, "CONTROLLER", true),
-                userConfig.toString()));
-        Assert.assertTrue(jsonResponse.has("status"));
+    JsonNode jsonResponse = JsonUtils.stringToJsonNode(ControllerTest.sendPutRequest(
+        TEST_INSTANCE.getControllerRequestURLBuilder().forUpdateUserConfig(username, "CONTROLLER", true),
+        userConfig.toString()));
+    assertTrue(jsonResponse.has("status"));
 
-        UserConfig modifiedConfig = getUserConfig(username, "CONTROLLER");
-        Assert.assertEquals(modifiedConfig.getRoleType().toString(), "ADMIN");
-        Assert.assertTrue(BcryptUtils.checkpw("654321", modifiedConfig.getPassword()));
-    }
+    UserConfig modifiedConfig = getUserConfig(username, "CONTROLLER");
+    assertEquals(modifiedConfig.getRoleType().toString(), "ADMIN");
+    assertTrue(BcryptUtils.checkpw("654321", modifiedConfig.getPassword()));
+  }
 
-    @Test
-    public void testDeleteUser()
-        throws Exception {
-        // Case 1: Create a CONTORLLER user and delete it directly w/o using query param.
-        UserConfig controllerUserConfig = _userConfigBuilder.setUsername("user1")
-            .setComponentType(ComponentType.CONTROLLER).build();
-        String creationResponse = ControllerTest.sendPostRequest(_createUserUrl, controllerUserConfig.toJsonString());
-        Assert.assertEquals(creationResponse, "{\"status\":\"User user1_CONTROLLER has been successfully added!\"}");
+  @Test
+  public void testDeleteUser()
+      throws Exception {
+    // Case 1: Create a CONTORLLER user and delete it directly w/o using query param.
+    UserConfig controllerUserConfig =
+        _userConfigBuilder.setUsername("user1").setComponentType(ComponentType.CONTROLLER).build();
+    String creationResponse = ControllerTest.sendPostRequest(_createUserUrl, controllerUserConfig.toJsonString());
+    assertEquals(creationResponse, "{\"status\":\"User user1_CONTROLLER has been successfully added!\"}");
 
-        // Delete controller user using CONTROLLER suffix
-        String deleteResponse = ControllerTest.sendDeleteRequest(
-            StringUtil.join("/", TEST_INSTANCE.getControllerBaseApiUrl(),
-                "users", "user1?component=CONTROLLER"));
-        Assert.assertEquals(deleteResponse, "{\"status\":\"User: user1_CONTROLLER has been successfully deleted\"}");
+    // Delete controller user using CONTROLLER suffix
+    String deleteResponse = ControllerTest.sendDeleteRequest(
+        StringUtil.join("/", TEST_INSTANCE.getControllerBaseApiUrl(), "users", "user1?component=CONTROLLER"));
+    assertEquals(deleteResponse, "{\"status\":\"User: user1_CONTROLLER has been successfully deleted\"}");
 
-        // Case 2: Create a BROKER user and delete it directly w/o using query param.
-        UserConfig brokerUserConfig = _userConfigBuilder.setUsername("user1").setComponentType(ComponentType.BROKER)
-            .build();
-        creationResponse = ControllerTest.sendPostRequest(_createUserUrl, brokerUserConfig.toJsonString());
-        Assert.assertEquals(creationResponse, "{\"status\":\"User user1_BROKER has been successfully added!\"}");
+    // Case 2: Create a BROKER user and delete it directly w/o using query param.
+    UserConfig brokerUserConfig =
+        _userConfigBuilder.setUsername("user1").setComponentType(ComponentType.BROKER).build();
+    creationResponse = ControllerTest.sendPostRequest(_createUserUrl, brokerUserConfig.toJsonString());
+    assertEquals(creationResponse, "{\"status\":\"User user1_BROKER has been successfully added!\"}");
 
-        // Delete controller user using BROKER suffix
-        deleteResponse = ControllerTest.sendDeleteRequest(
-            StringUtil.join("/", TEST_INSTANCE.getControllerBaseApiUrl(), "users", "user1?component=BROKER")
-        );
-        Assert.assertEquals(deleteResponse, "{\"status\":\"User: user1_BROKER has been successfully deleted\"}");
-    }
+    // Delete controller user using BROKER suffix
+    deleteResponse = ControllerTest.sendDeleteRequest(
+        StringUtil.join("/", TEST_INSTANCE.getControllerBaseApiUrl(), "users", "user1?component=BROKER"));
+    assertEquals(deleteResponse, "{\"status\":\"User: user1_BROKER has been successfully deleted\"}");
+  }
 }
