1de904f54576540cabd425f02134e4b10597ddf5
Rong Rong <rongr@apache.org>
2022-11-10 11:41:24 -0800

    [multistage] leaf-node return data in non-serialized format (#9755)
    This PR directly uses the InstanceResponseBlock to get the non-serialized rows, this
    - avoid the useless Ser/De
    - splitting data into chunks more directly via the List API instead of via the block API
    
    Co-authored-by: Rong Rong <rongr@startree.ai>

--- pinot-query-runtime/src/test/java/org/apache/pinot/query/runtime/TransferableBlockUtilsTest.java
+++ pinot-query-runtime/src/test/java/org/apache/pinot/query/runtime/TransferableBlockUtilsTest.java
@@ -38,6 +38,7 @@ import org.testng.annotations.Test;
 
 public class TransferableBlockUtilsTest {
   private static final int TOTAL_ROW_COUNT = 50;
+  private static final int TEST_EST_BYTES_PER_COLUMN = 8;
   private static final List<DataSchema.ColumnDataType> EXCLUDE_DATA_TYPES = ImmutableList.of(
       DataSchema.ColumnDataType.OBJECT, DataSchema.ColumnDataType.JSON, DataSchema.ColumnDataType.BYTES,
       DataSchema.ColumnDataType.BYTES_ARRAY);
@@ -67,15 +68,15 @@ public class TransferableBlockUtilsTest {
   public void testSplitBlockUtils(int splitRowCount)
       throws Exception {
     DataSchema dataSchema = getDataSchema();
-    List<Object[]> rows = DataBlockTestUtils.getRandomRows(dataSchema, TOTAL_ROW_COUNT, 1);
     // compare serialized split
+    int estRowSizeInBytes = dataSchema.size() * TEST_EST_BYTES_PER_COLUMN;
+    List<Object[]> rows = DataBlockTestUtils.getRandomRows(dataSchema, TOTAL_ROW_COUNT, 1);
     RowDataBlock rowBlock = DataBlockBuilder.buildFromRows(rows, dataSchema);
-    int rowSizeInBytes = rowBlock.getRowSizeInBytes();
     validateBlocks(TransferableBlockUtils.splitBlock(new TransferableBlock(rowBlock),
-        DataBlock.Type.ROW, rowSizeInBytes * splitRowCount + 1), rows, dataSchema);
+        DataBlock.Type.ROW, estRowSizeInBytes * splitRowCount + 1), rows, dataSchema);
     // compare non-serialized split
     validateBlocks(TransferableBlockUtils.splitBlock(new TransferableBlock(rows, dataSchema, DataBlock.Type.ROW),
-        DataBlock.Type.ROW, rowSizeInBytes * splitRowCount + 1), rows, dataSchema);
+        DataBlock.Type.ROW, estRowSizeInBytes * splitRowCount + 1), rows, dataSchema);
   }
 
   @Test
