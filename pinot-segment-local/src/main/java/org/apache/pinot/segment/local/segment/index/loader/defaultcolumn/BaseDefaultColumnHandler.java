59edf0cab8e1436209fb8a8d952613dac7135490
Kartik Khare <kharekartik@Kartiks-MacBook-Pro.local>
2022-11-10 17:41:11 +0530

    Add check if the index doesn't already exist

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/loader/defaultcolumn/BaseDefaultColumnHandler.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/loader/defaultcolumn/BaseDefaultColumnHandler.java
@@ -554,12 +554,14 @@ public abstract class BaseDefaultColumnHandler implements DefaultColumnHandler {
       }
     }
 
-    try (NullValueVectorCreator nullValueVectorCreator = new NullValueVectorCreator(_indexDir, fieldSpec.getName())) {
-      for (int docId = 0; docId < totalDocs; docId++) {
-        nullValueVectorCreator.setNull(docId);
-      }
+    if (!_segmentWriter.hasIndexFor(column, ColumnIndexType.NULLVALUE_VECTOR)) {
+      try (NullValueVectorCreator nullValueVectorCreator = new NullValueVectorCreator(_indexDir, fieldSpec.getName())) {
+        for (int docId = 0; docId < totalDocs; docId++) {
+          nullValueVectorCreator.setNull(docId);
+        }
 
-      nullValueVectorCreator.seal();
+        nullValueVectorCreator.seal();
+      }
     }
 
     // Add the column metadata information to the metadata properties.
