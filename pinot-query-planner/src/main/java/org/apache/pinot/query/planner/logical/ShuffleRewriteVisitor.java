aeed62c0706a4cf22c94ecbf1e8fa4494b8fcaef
Rong Rong <rongr@startree.ai>
2022-11-29 16:33:22 -0800

    Revert "also fix logical sort"
    This reverts commit 4fd49921cb0f545a9a6d56935893787b36212cb0.

--- pinot-query-planner/src/main/java/org/apache/pinot/query/planner/logical/ShuffleRewriteVisitor.java
+++ pinot-query-planner/src/main/java/org/apache/pinot/query/planner/logical/ShuffleRewriteVisitor.java
@@ -150,9 +150,8 @@ public class ShuffleRewriteVisitor implements StageNodeVisitor<Set<Integer>, Voi
 
   @Override
   public Set<Integer> visitSort(SortNode node, Void context) {
-    // with sort shuffling reorder could change, so we can't directly return the old partition keys.
-    // TODO: fix me.
-    return new HashSet<>();
+    // sort doesn't change the partition keys
+    return node.getInputs().get(0).visit(this, context);
   }
 
   @Override
