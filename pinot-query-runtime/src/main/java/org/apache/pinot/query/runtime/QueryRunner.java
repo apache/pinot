a2fe26b6d9d738be3482e2fcecc80b8608f37dbb
Rong Rong <rongr@apache.org>
2022-12-05 18:58:34 -0800

    Revert "[multistage] Fix leaf stage return (#9892)" (#9916)
    This reverts commit e6d3dcdbdaf9e08f0ffd939e55361bb946b03f93.

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/QueryRunner.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/QueryRunner.java
@@ -20,8 +20,10 @@ package org.apache.pinot.query.runtime;
 
 import com.google.common.base.Preconditions;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import org.apache.helix.HelixManager;
 import org.apache.helix.store.zk.ZkHelixPropertyStore;
@@ -29,10 +31,13 @@ import org.apache.helix.zookeeper.datamodel.ZNRecord;
 import org.apache.pinot.common.exception.QueryException;
 import org.apache.pinot.common.metadata.ZKMetadataProvider;
 import org.apache.pinot.common.metrics.ServerMetrics;
+import org.apache.pinot.common.utils.DataSchema;
 import org.apache.pinot.core.data.manager.InstanceDataManager;
 import org.apache.pinot.core.operator.blocks.InstanceResponseBlock;
+import org.apache.pinot.core.operator.blocks.results.SelectionResultsBlock;
 import org.apache.pinot.core.query.executor.ServerQueryExecutorV1Impl;
 import org.apache.pinot.core.query.request.ServerQueryRequest;
+import org.apache.pinot.core.query.selection.SelectionOperatorUtils;
 import org.apache.pinot.core.transport.ServerInstance;
 import org.apache.pinot.query.mailbox.MailboxService;
 import org.apache.pinot.query.mailbox.MultiplexingMailboxService;
@@ -184,10 +189,47 @@ public class QueryRunner {
   private InstanceResponseBlock processServerQuery(ServerQueryRequest serverQueryRequest,
       ExecutorService executorService) {
     try {
-      return _serverExecutor.execute(serverQueryRequest, executorService);
+      InstanceResponseBlock result = _serverExecutor.execute(serverQueryRequest, executorService);
+
+      if (result.getRows() != null && serverQueryRequest.getQueryContext().getOrderByExpressions() != null) {
+        // we only re-arrange columns to match the projection in the case of order by - this is to ensure
+        // that V1 results match what the expected projection schema in the calcite logical operator; if
+        // we realize that there are other situations where we need to post-process v1 results to adhere to
+        // the expected results we should factor this out and also apply the canonicalization of the data
+        // types during this post-process step (also see LeafStageTransferableBlockOperator#canonicalizeRow)
+        DataSchema dataSchema = result.getDataSchema();
+        List<String> selectionColumns =
+            SelectionOperatorUtils.getSelectionColumns(serverQueryRequest.getQueryContext(), dataSchema);
+
+        int[] columnIndices = SelectionOperatorUtils.getColumnIndices(selectionColumns, dataSchema);
+        int numColumns = columnIndices.length;
+
+        DataSchema resultDataSchema = SelectionOperatorUtils.getSchemaForProjection(dataSchema, columnIndices);
+
+        // Extract the result rows
+        LinkedList<Object[]> rowsInSelectionResults = new LinkedList<>();
+        for (Object[] row : result.getRows()) {
+          assert row != null;
+          Object[] extractedRow = new Object[numColumns];
+          for (int i = 0; i < numColumns; i++) {
+            Object value = row[columnIndices[i]];
+            if (value != null) {
+              extractedRow[i] = value;
+            }
+          }
+
+          rowsInSelectionResults.addFirst(extractedRow);
+        }
+
+        return new InstanceResponseBlock(
+            new SelectionResultsBlock(resultDataSchema, rowsInSelectionResults),
+            serverQueryRequest.getQueryContext());
+      } else {
+        return result;
+      }
     } catch (Exception e) {
       InstanceResponseBlock errorResponse = new InstanceResponseBlock();
-      errorResponse.getExceptions().put(QueryException.QUERY_EXECUTION_ERROR_CODE, e.getMessage());
+      errorResponse.getExceptions().put(QueryException.QUERY_EXECUTION_ERROR_CODE, Objects.toString(e.getMessage()));
       return errorResponse;
     }
   }
