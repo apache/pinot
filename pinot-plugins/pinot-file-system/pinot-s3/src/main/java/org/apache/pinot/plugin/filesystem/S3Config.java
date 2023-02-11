6caa8a038f328feed381511c382c8593e06138a0
Xiaobing <61892277+klsince@users.noreply.github.com>
2023-02-11 10:04:42 -0800

    upload large s3 object by multi parts to overcome the 5GB limit of PutObject (#10263)

--- pinot-plugins/pinot-file-system/pinot-s3/src/main/java/org/apache/pinot/plugin/filesystem/S3Config.java
+++ pinot-plugins/pinot-file-system/pinot-s3/src/main/java/org/apache/pinot/plugin/filesystem/S3Config.java
@@ -22,14 +22,18 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Preconditions;
 import java.util.UUID;
 import org.apache.pinot.spi.env.PinotConfiguration;
+import org.apache.pinot.spi.utils.DataSizeUtils;
 
 
 /**
  * S3 related config
  */
 public class S3Config {
-
   private static final boolean DEFAULT_DISABLE_ACL = true;
+  // From https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html, the part number must be an integer
+  // between 1 and 10000, inclusive; and the min part size allowed is 5MiB, except the last one.
+  private static final long MULTI_PART_UPLOAD_MIN_PART_SIZE = 5 * 1024 * 1024;
+  public static final int MULTI_PART_UPLOAD_MAX_PART_NUM = 10000;
 
   public static final String ACCESS_KEY = "accessKey";
   public static final String SECRET_KEY = "secretKey";
@@ -49,6 +53,9 @@ public class S3Config {
   public static final String EXTERNAL_ID = "externalId";
   public static final String SESSION_DURATION_SECONDS = "sessionDurationSeconds";
   public static final String ASYNC_SESSION_UPDATED_ENABLED = "asyncSessionUpdateEnabled";
+  public static final String MIN_OBJECT_SIZE_FOR_MULTI_PART_UPLOAD = "minObjectSizeForMultiPartUpload";
+  public static final String MULTI_PART_UPLOAD_PART_SIZE = "multiPartUploadPartSize";
+  private static final String DEFAULT_MULTI_PART_UPLOAD_PART_SIZE = "128MB";
   public static final String DEFAULT_IAM_ROLE_BASED_ACCESS_ENABLED = "false";
   public static final String DEFAULT_SESSION_DURATION_SECONDS = "900";
   public static final String DEFAULT_ASYNC_SESSION_UPDATED_ENABLED = "true";
@@ -69,6 +76,8 @@ public class S3Config {
   private String _externalId;
   private int _sessionDurationSeconds;
   private boolean _asyncSessionUpdateEnabled;
+  private final long _minObjectSizeForMultiPartUpload;
+  private final long _multiPartUploadPartSize;
 
   public S3Config(PinotConfiguration pinotConfig) {
     _disableAcl = pinotConfig.getProperty(DISABLE_ACL_CONFIG_KEY, DEFAULT_DISABLE_ACL);
@@ -91,7 +100,13 @@ public class S3Config {
         Integer.parseInt(pinotConfig.getProperty(SESSION_DURATION_SECONDS, DEFAULT_SESSION_DURATION_SECONDS));
     _asyncSessionUpdateEnabled = Boolean.parseBoolean(
         pinotConfig.getProperty(ASYNC_SESSION_UPDATED_ENABLED, DEFAULT_ASYNC_SESSION_UPDATED_ENABLED));
-
+    // non-positive values to disable multipart upload.
+    _minObjectSizeForMultiPartUpload =
+        DataSizeUtils.toBytes(pinotConfig.getProperty(MIN_OBJECT_SIZE_FOR_MULTI_PART_UPLOAD, "-1"));
+    _multiPartUploadPartSize = DataSizeUtils.toBytes(
+        pinotConfig.getProperty(MULTI_PART_UPLOAD_PART_SIZE, DEFAULT_MULTI_PART_UPLOAD_PART_SIZE));
+    Preconditions.checkArgument(_multiPartUploadPartSize > MULTI_PART_UPLOAD_MIN_PART_SIZE,
+        "The part size for multipart upload must be larger than 5MB");
     if (_iamRoleBasedAccess) {
       Preconditions.checkNotNull(_roleArn, "Must provide 'roleArn' if iamRoleBasedAccess is enabled");
     }
@@ -151,5 +166,13 @@ public class S3Config {
 
   public boolean isAsyncSessionUpdateEnabled() {
     return _asyncSessionUpdateEnabled;
+  }
+
+  public long getMinObjectSizeForMultiPartUpload() {
+    return _minObjectSizeForMultiPartUpload;
+  }
+
+  public long getMultiPartUploadPartSize() {
+    return _multiPartUploadPartSize;
   }
 }
