ead72d6ef53bf5e79db4d9b91def641c13bdd413
Rong Rong <rongr@startree.ai>
2023-02-16 13:14:03 -0800

    [multistage] support SEMI/ANTI join

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/operator/HashJoinOperator.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/operator/HashJoinOperator.java
@@ -60,8 +60,8 @@ public class HashJoinOperator extends MultiStageOperator {
   private static final String EXPLAIN_NAME = "HASH_JOIN";
   private static final Logger LOGGER = LoggerFactory.getLogger(AggregateOperator.class);
 
-  private static final Set<JoinRelType> SUPPORTED_JOIN_TYPES =
-      ImmutableSet.of(JoinRelType.INNER, JoinRelType.LEFT, JoinRelType.RIGHT, JoinRelType.FULL);
+  private static final Set<JoinRelType> SUPPORTED_JOIN_TYPES = ImmutableSet.of(
+      JoinRelType.INNER, JoinRelType.LEFT, JoinRelType.RIGHT, JoinRelType.FULL, JoinRelType.SEMI, JoinRelType.ANTI);
 
   private final HashMap<Key, List<Object[]>> _broadcastRightTable;
 
@@ -101,7 +101,7 @@ public class HashJoinOperator extends MultiStageOperator {
     Preconditions.checkState(_leftRowSize > 0, "leftRowSize has to be greater than zero:" + _leftRowSize);
     _resultSchema = node.getDataSchema();
     _resultRowSize = _resultSchema.size();
-    Preconditions.checkState(_resultRowSize > _leftRowSize,
+    Preconditions.checkState(_resultRowSize >= _leftRowSize,
         "Result row size" + _leftRowSize + " has to be greater than left row size:" + _leftRowSize);
     _leftTableOperator = leftTableOperator;
     _rightTableOperator = rightTableOperator;
@@ -215,32 +215,44 @@ public class HashJoinOperator extends MultiStageOperator {
     List<Object[]> container = leftBlock.isEndOfStreamBlock() ? new ArrayList<>() : leftBlock.getContainer();
     for (Object[] leftRow : container) {
       Key key = new Key(_leftKeySelector.getKey(leftRow));
-      // NOTE: Empty key selector will always give same hash code.
-      List<Object[]> matchedRightRows = _broadcastRightTable.getOrDefault(key, null);
-      if (matchedRightRows == null) {
-        if (needUnmatchedLeftRows()) {
-          rows.add(joinRow(leftRow, null));
+      if (_joinType != JoinRelType.SEMI && _joinType != JoinRelType.ANTI) {
+        // NOTE: Empty key selector will always give same hash code.
+        List<Object[]> matchedRightRows = _broadcastRightTable.getOrDefault(key, null);
+        if (matchedRightRows == null) {
+          if (needUnmatchedLeftRows()) {
+            rows.add(joinRow(leftRow, null));
+          }
+          continue;
         }
-        continue;
-      }
-      boolean hasMatchForLeftRow = false;
-      for (int i = 0; i < matchedRightRows.size(); i++) {
-        Object[] rightRow = matchedRightRows.get(i);
-        // TODO: Optimize this to avoid unnecessary object copy.
-        Object[] resultRow = joinRow(leftRow, rightRow);
-        if (_joinClauseEvaluators.isEmpty() || _joinClauseEvaluators.stream().allMatch(
-            evaluator -> (Boolean) FunctionInvokeUtils.convert(evaluator.apply(resultRow),
-                DataSchema.ColumnDataType.BOOLEAN))) {
-          rows.add(resultRow);
-          hasMatchForLeftRow = true;
-          if (_matchedRightRows != null) {
-            HashSet<Integer> matchedRows = _matchedRightRows.computeIfAbsent(key, k -> new HashSet<>());
-            matchedRows.add(i);
+        boolean hasMatchForLeftRow = false;
+        for (int i = 0; i < matchedRightRows.size(); i++) {
+          Object[] rightRow = matchedRightRows.get(i);
+          // TODO: Optimize this to avoid unnecessary object copy.
+          Object[] resultRow = joinRow(leftRow, rightRow);
+          if (_joinClauseEvaluators.isEmpty() || _joinClauseEvaluators.stream().allMatch(
+              evaluator -> (Boolean) FunctionInvokeUtils.convert(evaluator.apply(resultRow),
+                  DataSchema.ColumnDataType.BOOLEAN))) {
+            rows.add(resultRow);
+            hasMatchForLeftRow = true;
+            if (_matchedRightRows != null) {
+              HashSet<Integer> matchedRows = _matchedRightRows.computeIfAbsent(key, k -> new HashSet<>());
+              matchedRows.add(i);
+            }
           }
         }
-      }
-      if (!hasMatchForLeftRow && needUnmatchedLeftRows()) {
-        rows.add(joinRow(leftRow, null));
+        if (!hasMatchForLeftRow && needUnmatchedLeftRows()) {
+          rows.add(joinRow(leftRow, null));
+        }
+      } else if (_joinType == JoinRelType.SEMI) {
+        // SEMI-JOIN only checks existence of the key
+        if (_broadcastRightTable.containsKey(key)) {
+          rows.add(joinRow(leftRow, null));
+        }
+      } else {
+        // ANTI-JOIN only checks non-existence of the key
+        if (!_broadcastRightTable.containsKey(key)) {
+          rows.add(joinRow(leftRow, null));
+        }
       }
     }
     return new TransferableBlock(rows, _resultSchema, DataBlock.Type.ROW);
