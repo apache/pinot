fb9e50d04bf4c78a0cf96c2b40b0e64046905f44
Saurabh Dubey <saurabh.dubey@Saurabhs-MacBook-Pro.local>
2023-02-17 12:00:50 +0530

    Optimize broker reducer when order by is present

--- pinot-core/src/main/java/org/apache/pinot/core/query/selection/SelectionOperatorService.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/selection/SelectionOperatorService.java
@@ -18,15 +18,17 @@
  */
 package org.apache.pinot.core.query.selection;
 
+import java.util.ArrayDeque;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.PriorityQueue;
+import java.util.Queue;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.pinot.common.datatable.DataTable;
 import org.apache.pinot.common.response.broker.ResultTable;
 import org.apache.pinot.common.utils.DataSchema;
 import org.apache.pinot.core.query.request.context.QueryContext;
-import org.apache.pinot.spi.trace.Tracing;
 import org.roaringbitmap.RoaringBitmap;
 
 
@@ -57,12 +59,59 @@ import org.roaringbitmap.RoaringBitmap;
  */
 @SuppressWarnings("rawtypes")
 public class SelectionOperatorService {
-  private final QueryContext _queryContext;
   private final List<String> _selectionColumns;
   private final DataSchema _dataSchema;
-  private final int _offset;
+  private final int _limit;
   private final int _numRowsToKeep;
-  private final PriorityQueue<Object[]> _rows;
+  private final Queue<Object[]> _rowsToKeep;
+  private final Comparator<Object[]> _comparator;
+
+  private static class DataTableAndIndex {
+
+    private final DataTable _dataTable;
+    private final RoaringBitmap[] _nullBitmaps;
+    private int _index;
+    private Pair<Integer, Object[]> _currentMaterializedRow;
+
+    private DataTableAndIndex(DataTable dataTable, boolean nullHandlingEnabled) {
+      _dataTable = dataTable;
+      if (nullHandlingEnabled) {
+        _nullBitmaps = new RoaringBitmap[dataTable.getDataSchema().size()];
+        for (int colId = 0; colId < _nullBitmaps.length; colId++) {
+          _nullBitmaps[colId] = dataTable.getNullRowIds(colId);
+        }
+      } else {
+        _nullBitmaps = null;
+      }
+      _index = 0;
+    }
+
+    public Object[] getRow() {
+      // Because we'll be getting the same index row multiple times;
+      if (_currentMaterializedRow != null && _currentMaterializedRow.getLeft() == _index) {
+        return _currentMaterializedRow.getRight();
+      }
+
+      Object[] row = SelectionOperatorUtils.extractRowFromDataTable(_dataTable, _index);
+      if (_nullBitmaps != null) {
+        for (int colId = 0; colId < _nullBitmaps.length; colId++) {
+          if (_nullBitmaps[colId] != null && _nullBitmaps[colId].contains(_index)) {
+            row[colId] = null;
+          }
+        }
+      }
+      _currentMaterializedRow = Pair.of(_index, row);
+      return row;
+    }
+
+    public void incrementIndex() {
+      _index++;
+    }
+
+    public boolean hasNext() {
+      return _index <= _dataTable.getNumberOfRows();
+    }
+  }
 
   /**
    * Constructor for <code>SelectionOperatorService</code> with {@link DataSchema}. (Inter segment)
@@ -71,16 +120,15 @@ public class SelectionOperatorService {
    * @param dataSchema data schema.
    */
   public SelectionOperatorService(QueryContext queryContext, DataSchema dataSchema) {
-    _queryContext = queryContext;
     _selectionColumns = SelectionOperatorUtils.getSelectionColumns(queryContext, dataSchema);
     _dataSchema = dataSchema;
     // Select rows from offset to offset + limit.
-    _offset = queryContext.getOffset();
-    _numRowsToKeep = _offset + queryContext.getLimit();
+    _limit = queryContext.getLimit();
+    _numRowsToKeep = queryContext.getOffset() + queryContext.getLimit();
     assert queryContext.getOrderByExpressions() != null;
-    _rows = new PriorityQueue<>(Math.min(_numRowsToKeep, SelectionOperatorUtils.MAX_ROW_HOLDER_INITIAL_CAPACITY),
-        SelectionOperatorUtils.getTypeCompatibleComparator(queryContext.getOrderByExpressions(), _dataSchema,
-            _queryContext.isNullHandlingEnabled()));
+    _rowsToKeep = new ArrayDeque<>(_limit);
+    _comparator = SelectionOperatorUtils.getTypeCompatibleComparator(queryContext.getOrderByExpressions(), _dataSchema,
+        queryContext.isNullHandlingEnabled());
   }
 
   /**
@@ -88,8 +136,8 @@ public class SelectionOperatorService {
    *
    * @return selection results.
    */
-  public PriorityQueue<Object[]> getRows() {
-    return _rows;
+  public Queue<Object[]> getRows() {
+    return _rowsToKeep;
   }
 
   /**
@@ -97,29 +145,41 @@ public class SelectionOperatorService {
    * (Broker side)
    */
   public void reduceWithOrdering(Collection<DataTable> dataTables, boolean nullHandlingEnabled) {
-    for (DataTable dataTable : dataTables) {
-      int numRows = dataTable.getNumberOfRows();
-      if (nullHandlingEnabled) {
-        RoaringBitmap[] nullBitmaps = new RoaringBitmap[dataTable.getDataSchema().size()];
-        for (int colId = 0; colId < nullBitmaps.length; colId++) {
-          nullBitmaps[colId] = dataTable.getNullRowIds(colId);
-        }
-        for (int rowId = 0; rowId < numRows; rowId++) {
-          Object[] row = SelectionOperatorUtils.extractRowFromDataTable(dataTable, rowId);
-          for (int colId = 0; colId < nullBitmaps.length; colId++) {
-            if (nullBitmaps[colId] != null && nullBitmaps[colId].contains(rowId)) {
-              row[colId] = null;
-            }
+    List<DataTableAndIndex> dataTableToCurrentIndex = new LinkedList<>();
+    for (DataTable table : dataTables) {
+      dataTableToCurrentIndex.add(new DataTableAndIndex(table, nullHandlingEnabled));
+    }
+
+    Queue<Integer> dataTablesToRemove = new ArrayDeque<>();
+    for (int i = 0; i < _numRowsToKeep; i++) {
+      Object[] rowToKeep = null;
+      int dataTableToIncrement = -1;
+      for (int j = 0; j < dataTableToCurrentIndex.size(); j++) {
+        DataTableAndIndex dataTableAndIndex = dataTableToCurrentIndex.get(j);
+        if (dataTableAndIndex.hasNext()) {
+          Object[] rowHere = dataTableAndIndex.getRow();
+          if (rowToKeep == null || _comparator.compare(rowToKeep, rowHere) >= 0) {
+            rowToKeep = rowHere;
+            dataTableToIncrement = j;
           }
-          SelectionOperatorUtils.addToPriorityQueue(row, _rows, _numRowsToKeep);
-          Tracing.ThreadAccountantOps.sampleAndCheckInterruptionPeriodically(rowId);
+        } else {
+          dataTablesToRemove.add(j);
         }
+      }
+      if (dataTableToIncrement != -1) {
+        if (_rowsToKeep.size() >= _limit) {
+          _rowsToKeep.poll();
+        }
+        dataTableToCurrentIndex.get(dataTableToIncrement).incrementIndex();
+        _rowsToKeep.add(rowToKeep);
       } else {
-        for (int rowId = 0; rowId < numRows; rowId++) {
-          Object[] row = SelectionOperatorUtils.extractRowFromDataTable(dataTable, rowId);
-          SelectionOperatorUtils.addToPriorityQueue(row, _rows, _numRowsToKeep);
-          Tracing.ThreadAccountantOps.sampleAndCheckInterruptionPeriodically(rowId);
-        }
+        // No more rows in dataTables
+        break;
+      }
+
+      while (!dataTablesToRemove.isEmpty()) {
+        // Removing by index is faster with LinkedList
+        dataTableToCurrentIndex.remove(dataTablesToRemove.poll().intValue());
       }
     }
   }
@@ -137,8 +197,8 @@ public class SelectionOperatorService {
 
     // Extract the result rows
     LinkedList<Object[]> rowsInSelectionResults = new LinkedList<>();
-    while (_rows.size() > _offset) {
-      Object[] row = _rows.poll();
+    while (!_rowsToKeep.isEmpty()) {
+      Object[] row = _rowsToKeep.poll();
       assert row != null;
       Object[] extractedRow = new Object[numColumns];
       for (int i = 0; i < numColumns; i++) {
@@ -147,7 +207,6 @@ public class SelectionOperatorService {
           extractedRow[i] = resultDataSchema.getColumnDataType(i).convertAndFormat(value);
         }
       }
-
       rowsInSelectionResults.addFirst(extractedRow);
     }
 
