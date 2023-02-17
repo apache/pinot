33a39a12813c6bfd05a3e4c942bbfcb827fa8b0e
egalpin <egalpin@users.noreply.github.com>
2023-02-17 08:49:30 -0800

    Removes unused import

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/upsert/ComparisonColumns.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/upsert/ComparisonColumns.java
@@ -18,7 +18,6 @@
  */
 package org.apache.pinot.segment.local.upsert;
 
-import com.google.common.base.Preconditions;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nonnull;
