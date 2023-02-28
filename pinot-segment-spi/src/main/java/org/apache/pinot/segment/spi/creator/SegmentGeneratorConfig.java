84372d448ce41d2ad3639596f3edaa9768a65b9b
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2023-02-28 17:13:56 +0100

    Encapsulate changes in IndexLoadingConfig and SegmentGeneratorConfig

--- pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/creator/SegmentGeneratorConfig.java
+++ pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/creator/SegmentGeneratorConfig.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import org.apache.pinot.segment.spi.compression.ChunkCompressionType;
