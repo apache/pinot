ee632252602ce29cdaff677a668ed42e2dc60a05
Omid Andalib <omid.andalib@improving.com>
2023-03-13 12:57:20 -0700

    Fix JDBC driver check for username

--- pinot-clients/pinot-jdbc-client/src/main/java/org/apache/pinot/client/utils/DriverUtils.java
+++ pinot-clients/pinot-jdbc-client/src/main/java/org/apache/pinot/client/utils/DriverUtils.java
@@ -71,7 +71,7 @@ public class DriverUtils {
   public static void handleAuth(Properties info, Map<String, String> headers)
       throws SQLException {
 
-    if (info.contains(USER_PROPERTY) && !headers.containsKey(AUTH_HEADER)) {
+    if (info.containsKey(USER_PROPERTY) && !headers.containsKey(AUTH_HEADER)) {
       String username = info.getProperty(USER_PROPERTY);
       String password = info.getProperty(PASSWORD_PROPERTY, "");
       if (StringUtils.isAnyEmpty(username, password)) {
