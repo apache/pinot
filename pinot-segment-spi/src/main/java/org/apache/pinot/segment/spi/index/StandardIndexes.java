331dfbd98ada07c60becc03ddaea5196c8e5955d
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2023-03-15 13:11:38 +0100

    Add javadoc to StandardIndexes

--- pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/index/StandardIndexes.java
+++ pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/index/StandardIndexes.java
@@ -23,6 +23,26 @@ import org.apache.pinot.segment.spi.index.reader.BloomFilterReader;
 import org.apache.pinot.spi.config.table.BloomFilterConfig;
 
 
+/**
+ * This is a utility class that can be used to get references to the standard index types in a safe way.
+ * <p>
+ *   The ultimate container of valid index types (and therefore the source of truth) is {@link IndexService}.
+ *   In order to get some specific index type, callers have to use {@link IndexService#get(String)}.
+ *   The usability of this method is not great, given that the caller needs to use a literal (which imply that typos
+ *   may produce exceptions at runtime) and it returns the most generic IndexType signature. This signature is so
+ *   general that most of the time caller is forced to do castings.
+ * </p>
+ * <p>
+ *   These usability problems are a cost we have to pay in order to have the ability to get references to index types
+ *   that are not know at compile time, which is a requirement when we have custom index types. But Pinot includes
+ *   a bunch of predefined index types (like forward index, dictionary, null value vector, etc) that should always be
+ *   included in a Pinot distribution. {@link StandardIndexes} contains one get method for each standard index,
+ *   providing a typesafe way to get these references. Instead of having to write something like
+ *   {@code (IndexType<BloomFilterConfig, BloomFilterReader, IndexCreator>)
+ *   IndexService.getInstance().getOrThrow("bloom_filter")},
+ *   a caller can simply use {@link StandardIndexes#bloomFilter()}
+ * </p>
+ */
 @SuppressWarnings("unchecked")
 public class StandardIndexes {
   private StandardIndexes() {
