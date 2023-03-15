c2b279ce576a20aee5e585fbeb0a697eae9b9c2b
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2023-03-15 10:36:34 +0100

    Add some changes included in index-spi-all-types

--- pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/index/IndexService.java
+++ pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/index/IndexService.java
@@ -72,12 +72,12 @@ public class IndexService {
     return _allIndexes;
   }
 
-  public Optional<IndexType<?, ?, ?>> getIndexTypeById(String indexId) {
+  public Optional<IndexType<?, ?, ?>> get(String indexId) {
     return getAllIndexes().stream().filter(indexType -> indexType.getId().equalsIgnoreCase(indexId)).findAny();
   }
 
-  public IndexType<?, ?, ?> getIndexTypeByIdOrThrow(String indexId) {
-    return getIndexTypeById(indexId)
+  public IndexType<?, ?, ?> getOrThrow(String indexId) {
+    return get(indexId)
         .orElseThrow(() -> new IllegalArgumentException("Unknown index id: " + indexId));
   }
 }
