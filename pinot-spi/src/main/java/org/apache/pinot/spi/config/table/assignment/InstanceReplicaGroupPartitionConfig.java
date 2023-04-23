720f21d684b890ea6599b6abe0def38abfca386e
Abhishek Sharma <abhishek.sharma@spothero.com>
2023-04-22 21:23:45 -0400

    10608: fix linting issues.

--- pinot-spi/src/main/java/org/apache/pinot/spi/config/table/assignment/InstanceReplicaGroupPartitionConfig.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/config/table/assignment/InstanceReplicaGroupPartitionConfig.java
@@ -21,8 +21,8 @@ package org.apache.pinot.spi.config.table.assignment;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
-import org.apache.pinot.spi.config.BaseJsonConfig;
 import javax.annotation.Nullable;
+import org.apache.pinot.spi.config.BaseJsonConfig;
 
 
 public class InstanceReplicaGroupPartitionConfig extends BaseJsonConfig {
