d246b81012e734d7175c1cebe1cfb77c49a08f9c
Andi Miller <andi@andimiller.net>
2023-04-27 15:57:22 +0100

    add preconditions to tuple aggregation function

--- pinot-core/src/main/java/org/apache/pinot/core/query/aggregation/function/IntegerTupleSketchAggregationFunction.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/aggregation/function/IntegerTupleSketchAggregationFunction.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pinot.core.query.aggregation.function;
 
+import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Base64;
@@ -56,9 +57,17 @@ public class IntegerTupleSketchAggregationFunction
 
   public IntegerTupleSketchAggregationFunction(List<ExpressionContext> arguments, IntegerSummary.Mode mode) {
     super(arguments.get(0));
+
+    Preconditions.checkArgument(arguments.size() <= 2,
+        "Tuple Sketch Aggregation Function expects at most 2 arguments, got: %s", arguments.size());
     _expressionContext = arguments.get(0);
     _setOps = new IntegerSummarySetOperations(mode, mode);
     if (arguments.size() == 2) {
+      FieldSpec.DataType dataType = arguments.get(1).getLiteral().getType();
+      Preconditions.checkArgument(dataType == FieldSpec.DataType.LONG || dataType == FieldSpec.DataType.INT,
+          "Tuple Sketch Aggregation Function expected the second argument to be a number of entries to keep, but it "
+              + "was of type %s",
+          dataType.toString());
       _entries = ((Long) arguments.get(1).getLiteral().getValue()).intValue();
     } else {
       _entries = (int) Math.pow(2, CommonConstants.Helix.DEFAULT_TUPLE_SKETCH_LGK);
