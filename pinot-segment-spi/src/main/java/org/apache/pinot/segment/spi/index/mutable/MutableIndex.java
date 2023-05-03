fbec4bbd963e2ed449559bd6b7ac908fca5b2fd8
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2023-05-03 10:09:29 +0200

    Add javadoc explaining why MutableIndex implements IndexReader but do not implement IndexCreator

--- pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/index/mutable/MutableIndex.java
+++ pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/index/mutable/MutableIndex.java
@@ -23,26 +23,41 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import org.apache.pinot.segment.spi.index.IndexReader;
 
-
+/**
+ * Implementations of this interface can be used to create indexes in realtime tables and at the same time to read them.
+ *
+ * This interface extends {@link IndexReader} as a marker interface and implementations are encouraged to extend the
+ * index specific reader. For example, MutableForwardIndex should extend ForwardIndexReader. The Java typesystem is not
+ * expressive enough to enforce this constraint.
+ *
+ * This interface explicitly do not extend {@link org.apache.pinot.segment.spi.index.IndexCreator} and usually
+ * implementations should not implement that interface. As explained in their Javadoc, IndexCreators are designed to
+ * add rows in docId order. MutableIndexes, on the other hand, can be modified adding indexes in any order and therefore
+ * their {@link #add(Object, int, int)} methods include an extra parameter that indicates the docId of the value to add.
+ */
 public interface MutableIndex extends IndexReader {
 
   /**
    * Adds the given single value cell to the index.
    *
-   * Rows will be added in no particular order.
+   * Unlike {@link org.apache.pinot.segment.spi.index.IndexCreator#add(Object, int)}, rows can be added in no
+   * particular order, so the docId is required by this method.
    *
    * @param value The nonnull value of the cell. In case the cell was actually null, a default value is received instead
    * @param dictId An optional dictionary value of the cell. If there is no dictionary, -1 is received
+   * @param docId The document id of the given row. A non-negative value.
    */
   void add(@Nonnull Object value, int dictId, int docId);
 
   /**
    * Adds the given multi value cell to the index.
    *
-   * Rows will be added in no particular order.
+   * Unlike {@link org.apache.pinot.segment.spi.index.IndexCreator#add(Object[], int[])}, rows can be added in no
+   * particular order, so the docId is required by this method.
    *
    * @param values The nonnull value of the cell. In case the cell was actually null, an empty array is received instead
    * @param dictIds An optional array of dictionary values. If there is no dictionary, null is received.
+   * @param docId The document id of the given row. A non-negative value.
    */
   void add(@Nonnull Object[] values, @Nullable int[] dictIds, int docId);
 }
