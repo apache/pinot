4875c93dea85e4923619edac1361bbbe3c847acb
Robert Zych <robert.zych@gmail.com>
2023-05-12 15:53:10 -0700

    using Set<Integer> for validIds to reduce lookup time and memory usage

--- pinot-plugins/pinot-minion-tasks/pinot-minion-builtin-tasks/src/main/java/org/apache/pinot/plugin/minion/tasks/upsertcompaction/UpsertCompactionTaskExecutor.java
+++ pinot-plugins/pinot-minion-tasks/pinot-minion-builtin-tasks/src/main/java/org/apache/pinot/plugin/minion/tasks/upsertcompaction/UpsertCompactionTaskExecutor.java
@@ -1,8 +1,8 @@
 package org.apache.pinot.plugin.minion.tasks.upsertcompaction;
 
 import com.google.common.base.Preconditions;
-import com.google.common.primitives.Ints;
 import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -10,8 +10,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import org.apache.helix.HelixAdmin;
 import org.apache.helix.model.IdealState;
@@ -24,7 +24,6 @@ import org.apache.pinot.core.minion.SegmentPurger;
 import org.apache.pinot.plugin.minion.tasks.BaseSingleSegmentConversionExecutor;
 import org.apache.pinot.plugin.minion.tasks.SegmentConversionResult;
 import org.apache.pinot.segment.local.segment.readers.PinotSegmentRecordReader;
-import org.apache.pinot.segment.local.utils.HashUtils;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.data.readers.GenericRow;
 import org.apache.pinot.spi.utils.builder.TableNameBuilder;
@@ -42,49 +41,18 @@ public class UpsertCompactionTaskExecutor extends BaseSingleSegmentConversionExe
     Map<String, String> configs = pinotTaskConfig.getConfigs();
     String tableNameWithType = configs.get(MinionConstants.TABLE_NAME_KEY);
     String rawTableName = TableNameBuilder.extractRawTableName(tableNameWithType);
-    String segmentName = configs.get(MinionConstants.SEGMENT_NAME_KEY);
-    String server = getServer(segmentName, tableNameWithType);
-
-    // get the url for the validDocIds for the server
-    InstanceConfig instanceConfig =
-        MINION_CONTEXT.getClusterManagementTool().getInstanceConfig(MINION_CONTEXT.getClusterName(), server);
-    String endpoint = InstanceUtils.getServerAdminEndpoint(instanceConfig);
-    String url = String.format("%s/segments/%s/%s/validDocIds",
-        endpoint, tableNameWithType, segmentName);
-
-    // get the validDocIds from that server
-    Response response = ClientBuilder.newClient().target(url).request().get(Response.class);
-    Preconditions.checkState(response.getStatus() == Response.Status.OK.getStatusCode(),
-        "Unable to retrieve validDocIds from %s", url);
-    byte[] snapshot = response.readEntity(byte[].class);
-    ImmutableRoaringBitmap bitmap = new ImmutableRoaringBitmap(ByteBuffer.wrap(snapshot));
-
-    // transform validDocIds into validIds using pk columns and time column
     List<String> columns = getSchema(rawTableName).getPrimaryKeyColumns();
     TableConfig tableConfig = getTableConfig(tableNameWithType);
     columns.add(tableConfig.getValidationConfig().getTimeColumnName());
-    PeekableIntIterator iterator = bitmap.getIntIterator();
-    PinotSegmentRecordReader recordReader = new PinotSegmentRecordReader();
-    recordReader.init(indexDir, new HashSet<>(columns), null);
-    GenericRow genericRow = new GenericRow();
-    List<String> validIds = new ArrayList<>();
-    while (iterator.hasNext()) {
-      int validDocId = iterator.next();
-      recordReader.getRecord(validDocId, genericRow);
-      StringBuilder validId = new StringBuilder();
-      for (String column : columns) {
-        validId.append(genericRow.getValue(column).toString());
-      }
-      validIds.add(validId.toString());
-    }
-    recordReader.close();
+    ImmutableRoaringBitmap validDocIds = getValidDocIds(tableNameWithType, configs);
+    Set<Integer> validIds = getValidIds(tableNameWithType, validDocIds, indexDir, columns);
 
     MINION_CONTEXT.setRecordPurgerFactory(x -> row -> {
-      StringBuilder id = new StringBuilder();
+      List<String> values = new ArrayList<>();
       for (String column : columns) {
-        id.append(row.getValue(column).toString());
+        values.add(row.getValue(column).toString());
       }
-      return !validIds.contains(id.toString());
+      return !validIds.contains(values.hashCode());
     });
     SegmentPurger.RecordPurger recordPurger = MINION_CONTEXT
         .getRecordPurgerFactory()
@@ -104,6 +72,46 @@ public class UpsertCompactionTaskExecutor extends BaseSingleSegmentConversionExe
         .build();
   }
 
+  private static ImmutableRoaringBitmap getValidDocIds(String tableNameWithType, Map<String, String> configs) {
+    String segmentName = configs.get(MinionConstants.SEGMENT_NAME_KEY);
+    String server = getServer(segmentName, tableNameWithType);
+
+    // get the url for the validDocIds for the server
+    InstanceConfig instanceConfig =
+        MINION_CONTEXT.getClusterManagementTool().getInstanceConfig(MINION_CONTEXT.getClusterName(), server);
+    String endpoint = InstanceUtils.getServerAdminEndpoint(instanceConfig);
+    String url = String.format("%s/segments/%s/%s/validDocIds",
+        endpoint, tableNameWithType, segmentName);
+
+    // get the validDocIds from that server
+    Response response = ClientBuilder.newClient().target(url).request().get(Response.class);
+    Preconditions.checkState(response.getStatus() == Response.Status.OK.getStatusCode(),
+        "Unable to retrieve validDocIds from %s", url);
+    byte[] snapshot = response.readEntity(byte[].class);
+    ImmutableRoaringBitmap validDocIds = new ImmutableRoaringBitmap(ByteBuffer.wrap(snapshot));
+    return validDocIds;
+  }
+
+  private Set<Integer> getValidIds(String tableNameWithType, ImmutableRoaringBitmap validDocIds,
+      File indexDir, List<String> columns) throws IOException {
+    PeekableIntIterator iterator = validDocIds.getIntIterator();
+    PinotSegmentRecordReader recordReader = new PinotSegmentRecordReader();
+    recordReader.init(indexDir, new HashSet<>(columns), null);
+    GenericRow genericRow = new GenericRow();
+    Set<Integer> validIds = new HashSet<>();
+
+    while (iterator.hasNext()) {
+      int validDocId = iterator.next();
+      recordReader.getRecord(validDocId, genericRow);
+      List<String> values = new ArrayList<>();
+      for (String column : columns) {
+        values.add(genericRow.getValue(column).toString());
+      }
+      validIds.add(values.hashCode());
+    }
+    recordReader.close();
+    return validIds;
+  }
   private static String getServer(String segmentName, String tableNameWithType) {
     String server = null;
     HelixAdmin clusterManagementTool = MINION_CONTEXT.getClusterManagementTool();
