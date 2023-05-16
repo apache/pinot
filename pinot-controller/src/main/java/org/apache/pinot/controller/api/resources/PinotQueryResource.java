3ebede26761e0933c4d42f8f281acd60f9a63103
Soumitra Kumar <soumitra@startree.ai>
2023-05-16 10:44:01 -0700

    Undo change for v2 engine

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotQueryResource.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotQueryResource.java
@@ -265,8 +265,7 @@ public class PinotQueryResource {
   // given a list of tables, returns the list of tableConfigs
   private List<TableConfig> getListTableConfigs(List<String> tableNames) {
     List<TableConfig> allTableConfigList = new ArrayList<>();
-    for (String inputTableName: tableNames) {
-      String tableName = _pinotHelixResourceManager.getActualTableName(inputTableName);
+    for (String tableName: tableNames) {
       List<TableConfig> tableConfigList = new ArrayList<>();
       if (_pinotHelixResourceManager.hasRealtimeTable(tableName)) {
         tableConfigList.add(Objects.requireNonNull(_pinotHelixResourceManager.getRealtimeTableConfig(tableName)));
