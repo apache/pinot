801799016d7f8486a386953d94dc585715c20dc8
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2023-06-02 14:06:11 +0200

    remove comment

--- pinot-plugins/pinot-metrics/pinot-dropwizard/src/main/java/org/apache/pinot/plugin/metrics/dropwizard/DropwizardMetricsFactory.java
+++ pinot-plugins/pinot-metrics/pinot-dropwizard/src/main/java/org/apache/pinot/plugin/metrics/dropwizard/DropwizardMetricsFactory.java
@@ -31,7 +31,6 @@ import org.apache.pinot.spi.metrics.PinotMetricsRegistry;
 @MetricsFactory
 public class DropwizardMetricsFactory implements PinotMetricsFactory {
   public static final String DOMAIN_PROP = "pinot.metrics.dropwizard.domain";
-  // this is the default in Dropwizard, which was used in Pinot <= 0.11
   public static final String DEFAULT_DOMAIN_VALUE = "org.apache.pinot.common.metrics";
   private PinotMetricsRegistry _pinotMetricsRegistry = null;
   private String _domainName;
