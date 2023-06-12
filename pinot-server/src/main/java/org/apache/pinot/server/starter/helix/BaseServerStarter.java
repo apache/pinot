207036950ad5fc13725ee464a2d0a09f26776638
Ankit Sultana <ankitsultana@uber.com>
2023-06-13 02:08:50 +0530

    Close PinotFS after Data Manager Shutdowns (#10888)

--- pinot-server/src/main/java/org/apache/pinot/server/starter/helix/BaseServerStarter.java
+++ pinot-server/src/main/java/org/apache/pinot/server/starter/helix/BaseServerStarter.java
@@ -627,12 +627,6 @@ public abstract class BaseServerStarter implements ServiceStartable {
     LOGGER.info("Shutting down Pinot server");
     long startTimeMs = System.currentTimeMillis();
 
-    try {
-      LOGGER.info("Closing PinotFS classes");
-      PinotFSFactory.shutdown();
-    } catch (IOException e) {
-      LOGGER.warn("Caught exception closing PinotFS classes", e);
-    }
     _adminApiApplication.startShuttingDown();
     _helixAdmin.setConfig(_instanceConfigScope,
         Collections.singletonMap(Helix.IS_SHUTDOWN_IN_PROGRESS, Boolean.toString(true)));
@@ -651,6 +645,14 @@ public abstract class BaseServerStarter implements ServiceStartable {
     }
     _serverQueriesDisabledTracker.stop();
     _realtimeLuceneIndexRefreshState.stop();
+    try {
+      // Close PinotFS after all data managers are shutdown. Otherwise, segments which are being committed will not
+      // be uploaded to the deep-store.
+      LOGGER.info("Closing PinotFS classes");
+      PinotFSFactory.shutdown();
+    } catch (IOException e) {
+      LOGGER.warn("Caught exception closing PinotFS classes", e);
+    }
     LOGGER.info("Deregistering service status handler");
     ServiceStatus.removeServiceStatusCallback(_instanceId);
     _adminApiApplication.stop();
