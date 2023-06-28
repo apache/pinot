91a2242c94cb0772b5c169c3d09a395b01c2b0e1
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2023-06-28 13:00:07 +0200

    Add test on the new way to prioritize filters inside an and

--- pinot-core/src/test/java/org/apache/pinot/core/operator/filter/FilterOperatorUtilsTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/operator/filter/FilterOperatorUtilsTest.java
@@ -18,12 +18,21 @@
  */
 package org.apache.pinot.core.operator.filter;
 
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.OptionalInt;
+import org.apache.pinot.core.common.Operator;
+import org.apache.pinot.core.operator.blocks.FilterBlock;
 import org.apache.pinot.core.query.request.context.QueryContext;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 
@@ -100,5 +109,82 @@ public class FilterOperatorUtilsTest {
     filterOperator = FilterOperatorUtils.getOrFilterOperator(QUERY_CONTEXT,
         Arrays.asList(MATCH_ALL_FILTER_OPERATOR, REGULAR_FILTER_OPERATOR), NUM_DOCS);
     assertTrue(filterOperator instanceof MatchAllFilterOperator);
+  }
+
+  @DataProvider
+  public static Object[][] priorities() {
+    SortedIndexBasedFilterOperator sorted = mock(SortedIndexBasedFilterOperator.class);
+    BitmapBasedFilterOperator bitmap = mock(BitmapBasedFilterOperator.class);
+    RangeIndexBasedFilterOperator range = mock(RangeIndexBasedFilterOperator.class);
+    TextContainsFilterOperator textContains = mock(TextContainsFilterOperator.class);
+    TextMatchFilterOperator textMatch = mock(TextMatchFilterOperator.class);
+    JsonMatchFilterOperator jsonMatch = mock(JsonMatchFilterOperator.class);
+    H3IndexFilterOperator h3 = mock(H3IndexFilterOperator.class);
+    H3InclusionIndexFilterOperator h3Inclusion = mock(H3InclusionIndexFilterOperator.class);
+    AndFilterOperator andFilterOperator = mock(AndFilterOperator.class);
+    OrFilterOperator orFilterOperator = mock(OrFilterOperator.class);
+    NotFilterOperator notWithHighPriority = new NotFilterOperator(sorted, NUM_DOCS);
+    NotFilterOperator notWithLowPriority = new NotFilterOperator(orFilterOperator, NUM_DOCS);
+
+    ExpressionFilterOperator expression = mock(ExpressionFilterOperator.class);
+    BaseFilterOperator unknown = mock(BaseFilterOperator.class);
+
+    MockedPrioritizedFilterOperator prioritizedBetweenSortedAndBitmap = mock(MockedPrioritizedFilterOperator.class);
+    OptionalInt betweenSortedAndBitmapPriority =
+        OptionalInt.of((PrioritizedFilterOperator.HIGH_PRIORITY + PrioritizedFilterOperator.MEDIUM_PRIORITY) / 2);
+    when(prioritizedBetweenSortedAndBitmap.getPriority())
+        .thenReturn(betweenSortedAndBitmapPriority);
+
+    MockedPrioritizedFilterOperator notPrioritized = mock(MockedPrioritizedFilterOperator.class);
+    when(prioritizedBetweenSortedAndBitmap.getPriority())
+        .thenReturn(OptionalInt.empty());
+
+    List<? extends List<? extends BaseFilterOperator>> expectedOrder = Lists.newArrayList(
+        Lists.newArrayList(sorted, notWithHighPriority),
+        Lists.newArrayList(bitmap),
+        Lists.newArrayList(range, textContains, textMatch, jsonMatch, h3, h3Inclusion),
+        Lists.newArrayList(andFilterOperator),
+        Lists.newArrayList(orFilterOperator, notWithLowPriority),
+        Lists.newArrayList(expression),
+        Lists.newArrayList(unknown, notPrioritized)
+    );
+
+    List<Object[]> cases = new ArrayList<>();
+    for (int i = 0; i < expectedOrder.size(); i++) {
+      List<? extends BaseFilterOperator> currentOps = expectedOrder.get(i);
+      for (BaseFilterOperator highPriorityOp : currentOps) {
+        for (int j = i + 1; j < expectedOrder.size(); j++) {
+          List<? extends BaseFilterOperator> lowerPriorityOps = expectedOrder.get(j);
+          for (BaseFilterOperator lowerPriorityOp : lowerPriorityOps) {
+            cases.add(new Object[] {highPriorityOp, lowerPriorityOp});
+          }
+        }
+      }
+    }
+    return cases.toArray(new Object[][]{});
+  }
+
+  @Test(dataProvider = "priorities")
+  public void testPriority(BaseFilterOperator highPriorty, BaseFilterOperator lowerPriorty) {
+    ArrayList<BaseFilterOperator> unsorted = Lists.newArrayList(lowerPriorty, highPriorty);
+    BaseFilterOperator filterOperator =
+        FilterOperatorUtils.getAndFilterOperator(QUERY_CONTEXT, unsorted, NUM_DOCS);
+    assertTrue(filterOperator instanceof AndFilterOperator);
+    List<Operator> actualChildOperators = ((AndFilterOperator) filterOperator).getChildOperators();
+    assertEquals(actualChildOperators, Lists.newArrayList(highPriorty, lowerPriorty), "Filter " + highPriorty
+        + " should have more priority than filter " + lowerPriorty);
+  }
+
+  private void assertOrder(BaseFilterOperator first, BaseFilterOperator second) {
+    BaseFilterOperator filterOperator =
+        FilterOperatorUtils.getAndFilterOperator(QUERY_CONTEXT, Lists.newArrayList(second, first), NUM_DOCS);
+    assertTrue(filterOperator instanceof AndFilterOperator);
+    List<Operator> actualChildOperators = ((AndFilterOperator) filterOperator).getChildOperators();
+    assertEquals(actualChildOperators, Lists.newArrayList(first, second), "Filter " + first + " should have "
+        + "more priority than filter " + second);
+  }
+
+  private static abstract class MockedPrioritizedFilterOperator extends BaseFilterOperator
+      implements PrioritizedFilterOperator<FilterBlock> {
   }
 }
