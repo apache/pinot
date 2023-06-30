9821cee0115be999dea6d18389ad7411f8ad7c47
Abhishek Sharma <abhishek.sharma@spothero.com>
2023-06-30 19:31:19 -0400

    Updated the code for showing the sql parsing exception. (#11009)

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotQueryResource.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotQueryResource.java
@@ -74,6 +74,7 @@ import org.apache.pinot.spi.utils.builder.TableNameBuilder;
 import org.apache.pinot.sql.parsers.CalciteSqlCompiler;
 import org.apache.pinot.sql.parsers.CalciteSqlParser;
 import org.apache.pinot.sql.parsers.PinotSqlType;
+import org.apache.pinot.sql.parsers.SqlCompilationException;
 import org.apache.pinot.sql.parsers.SqlNodeAndOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -144,9 +145,8 @@ public class PinotQueryResource {
     SqlNodeAndOptions sqlNodeAndOptions;
     try {
       sqlNodeAndOptions = CalciteSqlParser.compileToSqlNodeAndOptions(sqlQuery);
-    } catch (Exception ex) {
-      String errorMessage = String.format("Unable to parse the SQL: '%s'", sqlQuery);
-      throw QueryException.getException(QueryException.SQL_PARSING_ERROR, new Exception(errorMessage));
+    } catch (SqlCompilationException ex) {
+      throw QueryException.getException(QueryException.SQL_PARSING_ERROR, ex);
     }
     Map<String, String> options = sqlNodeAndOptions.getOptions();
     if (queryOptions != null) {
