6e246a478a85e4d9d3bf1d8442aae6f249c5565c
Sanskar Modi <sanskarmodi97@gmail.com>
2023-07-04 22:56:05 +0530

    Making this support both colon and pipe format

--- pinot-spi/src/main/java/org/apache/pinot/spi/data/DateTimeGranularitySpec.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/data/DateTimeGranularitySpec.java
@@ -29,10 +29,16 @@ import org.apache.pinot.spi.utils.StringUtil;
  * Class to represent granularity from {@link DateTimeFieldSpec}
  */
 public class DateTimeGranularitySpec {
-  // 'size:timeUnit'
-  private static final char SEPARATOR = '|';
-  private static final int TIME_UNIT_POSITION = 0;
-  private static final int SIZE_POSITION = 1;
+  // Colon format:
+  private static final char COLON_SEPARATOR = ':';
+  private static final int COLON_FORMAT_SIZE_POSITION = 0;
+  private static final int COLON_FORMAT_TIME_UNIT_POSITION = 1;
+
+  // Pipe format:
+  private static final char PIPE_SEPARATOR = '|';
+  private static final int PIPE_FORMAT_TIME_UNIT_POSITION = 0;
+  private static final int PIPE_FORMAT_SIZE_POSITION = 1;
+
   private static final int NUM_TOKENS = 2;
 
   private final int _size;
@@ -43,21 +49,41 @@ public class DateTimeGranularitySpec {
    */
   public DateTimeGranularitySpec(String granularity) {
     Preconditions.checkArgument(StringUtils.isNotEmpty(granularity), "Must provide granularity");
-    String[] granularityTokens = StringUtil.split(granularity, SEPARATOR, 2);
-    Preconditions.checkArgument(granularityTokens.length >= NUM_TOKENS,
-        "Invalid granularity: %s, must be of format 'timeUnit|size", granularity);
+
+    int sizePosition;
+    int timeUnitPosition;
+    String[] granularityTokens;
+
+    if (Character.isDigit(granularity.charAt(0))) {
+      // Colon format
+      granularityTokens = StringUtil.split(granularity, COLON_SEPARATOR, 2);
+      Preconditions.checkArgument(granularityTokens.length >= NUM_TOKENS,
+              "Invalid granularity: %s, must be of format 'size:timeUnit", granularity);
+
+      sizePosition = COLON_FORMAT_SIZE_POSITION;
+      timeUnitPosition = COLON_FORMAT_TIME_UNIT_POSITION;
+    } else {
+      // Pipe format
+      granularityTokens = StringUtil.split(granularity, PIPE_SEPARATOR, 2);
+      Preconditions.checkArgument(granularityTokens.length >= NUM_TOKENS,
+              "Invalid granularity: %s, must be of format 'timeUnit|size", granularity);
+
+      sizePosition = PIPE_FORMAT_SIZE_POSITION;
+      timeUnitPosition = PIPE_FORMAT_TIME_UNIT_POSITION;
+    }
+
     try {
-      _size = Integer.parseInt(granularityTokens[SIZE_POSITION]);
+      _size = Integer.parseInt(granularityTokens[sizePosition]);
     } catch (Exception e) {
       throw new IllegalArgumentException(
-          String.format("Invalid size: %s in granularity: %s", granularityTokens[SIZE_POSITION], granularity));
+          String.format("Invalid size: %s in granularity: %s", granularityTokens[sizePosition], granularity));
     }
     Preconditions.checkArgument(_size > 0, "Invalid size: %s in granularity: %s, must be positive", _size, granularity);
     try {
-      _timeUnit = TimeUnit.valueOf(granularityTokens[TIME_UNIT_POSITION]);
+      _timeUnit = TimeUnit.valueOf(granularityTokens[timeUnitPosition]);
     } catch (Exception e) {
       throw new IllegalArgumentException(
-          String.format("Invalid time unit: %s in granularity: %s", granularityTokens[TIME_UNIT_POSITION],
+          String.format("Invalid time unit: %s in granularity: %s", granularityTokens[timeUnitPosition],
               granularity));
     }
   }
