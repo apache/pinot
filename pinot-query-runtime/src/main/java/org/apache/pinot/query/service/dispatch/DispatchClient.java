9040c2e493d138a157578ab74e451168a4857eb7
Rong Rong <rongr@startree.ai>
2023-07-31 12:54:01 -0700

    remove future, and use blocking stub

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/service/dispatch/DispatchClient.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/service/dispatch/DispatchClient.java
@@ -22,8 +22,6 @@ import io.grpc.Deadline;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.apache.pinot.common.Utils;
 import org.apache.pinot.common.proto.PinotQueryWorkerGrpc;
@@ -44,11 +42,13 @@ class DispatchClient {
   private static final Logger LOGGER = LoggerFactory.getLogger(DispatchClient.class);
   private static final StreamObserver<Worker.CancelResponse> NO_OP_CANCEL_STREAM_OBSERVER = new NoOpObserver();
   private final ManagedChannel _channel;
-  private final PinotQueryWorkerGrpc.PinotQueryWorkerStub _dispatchStub;
+  private final PinotQueryWorkerGrpc.PinotQueryWorkerBlockingStub _dispatchStub;
+  private final PinotQueryWorkerGrpc.PinotQueryWorkerStub _cancelStub;
 
   public DispatchClient(String host, int port) {
     _channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
-    _dispatchStub = PinotQueryWorkerGrpc.newStub(_channel);
+    _dispatchStub = PinotQueryWorkerGrpc.newBlockingStub(_channel);
+    _cancelStub = PinotQueryWorkerGrpc.newStub(_channel);
   }
 
   public ManagedChannel getChannel() {
@@ -57,10 +57,15 @@ class DispatchClient {
 
   public void submit(Worker.QueryRequest request, int stageId, QueryServerInstance virtualServer, Deadline deadline) {
     try {
-      CompletableFuture<Void> future = new CompletableFuture<>();
-      DispatchObserver dispatchObserver = new DispatchObserver(stageId, virtualServer, future);
-      _dispatchStub.withDeadline(deadline).submit(request, dispatchObserver);
-      future.get(deadline.timeRemaining(TimeUnit.MILLISECONDS), TimeUnit.MILLISECONDS);
+      Worker.QueryResponse queryResponse = _dispatchStub.withDeadline(deadline).submit(request);
+      if (queryResponse == null) {
+        throw new TimeoutException(String.format("Unable to dispatch query plan at stage %s on"
+            + " server %s due to timeout.", stageId, virtualServer));
+      } else if (queryResponse.containsMetadata(QueryConfig.KEY_OF_SERVER_RESPONSE_STATUS_ERROR)) {
+        throw new RuntimeException(String.format("Unable to dispatch query plan at stage %s on"
+                + " server %s: ERROR: %s", stageId, virtualServer,
+            queryResponse.getMetadataOrDefault(QueryConfig.KEY_OF_SERVER_RESPONSE_STATUS_ERROR, "null")));
+      }
     } catch (Throwable t) {
       LOGGER.error("Query Dispatch failed!", t);
       Utils.rethrowException(t);
@@ -70,46 +75,9 @@ class DispatchClient {
   public void cancel(long requestId) {
     try {
       Worker.CancelRequest cancelRequest = Worker.CancelRequest.newBuilder().setRequestId(requestId).build();
-      _dispatchStub.cancel(cancelRequest, NO_OP_CANCEL_STREAM_OBSERVER);
+      _cancelStub.cancel(cancelRequest, NO_OP_CANCEL_STREAM_OBSERVER);
     } catch (Exception e) {
       LOGGER.error("Query Cancellation failed!", e);
-    }
-  }
-
-  private static class DispatchObserver implements StreamObserver<Worker.QueryResponse> {
-    private static final Void VOID = null;
-    private final int _stageId;
-    private final QueryServerInstance _virtualServer;
-    private final CompletableFuture<Void> _future;
-
-    DispatchObserver(int stageId, QueryServerInstance virtualServer, CompletableFuture<Void> future) {
-      _stageId = stageId;
-      _virtualServer = virtualServer;
-      _future = future;
-    }
-
-    @Override
-    public void onNext(Worker.QueryResponse queryResponse) {
-      if (queryResponse == null) {
-        _future.completeExceptionally(new TimeoutException(String.format("Unable to dispatch query plan at stage %s on"
-                + " server %s due to timeout.", _stageId, _virtualServer)));
-      } else if (queryResponse.containsMetadata(QueryConfig.KEY_OF_SERVER_RESPONSE_STATUS_ERROR)) {
-        _future.completeExceptionally(new RuntimeException(String.format("Unable to dispatch query plan at stage %s on"
-                + " server %s: ERROR: %s", _stageId, _virtualServer,
-            queryResponse.getMetadataOrDefault(QueryConfig.KEY_OF_SERVER_RESPONSE_STATUS_ERROR, "null"))));
-      } else {
-        _future.complete(VOID);
-      }
-    }
-
-    @Override
-    public void onError(Throwable t) {
-      _future.completeExceptionally(t);
-    }
-
-    @Override
-    public void onCompleted() {
-      // do nothing.
     }
   }
 
