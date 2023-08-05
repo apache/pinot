cb27edaed92b8da2ec1fd159c76557c8c00a1298
Abhishek Sharma <abhishek.sharma@spothero.com>
2023-08-04 20:51:21 -0400

    removed httpclient commons 3 from WebHdfsV1Client

--- pinot-core/src/main/java/org/apache/pinot/core/query/executor/sql/SqlQueryExecutor.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/executor/sql/SqlQueryExecutor.java
@@ -19,7 +19,6 @@
 package org.apache.pinot.core.query.executor.sql;
 
 import com.google.common.collect.ImmutableList;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
