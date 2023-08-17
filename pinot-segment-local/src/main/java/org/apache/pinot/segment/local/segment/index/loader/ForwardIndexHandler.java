e8c78ec22d2ff616f0ec7323168175ad4fcc749e
Saurabh Dubey <saurabh.dubey@Saurabhs-MacBook-Pro.local>
2023-08-17 15:36:48 +0530

    ForwardIndexHandler should only refer to columns in schema

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/loader/ForwardIndexHandler.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/loader/ForwardIndexHandler.java
@@ -219,6 +219,12 @@ public class ForwardIndexHandler extends BaseIndexHandler {
     }
 
     for (String column : existingAllColumns) {
+      if (_schema != null && !_schema.hasColumn(column)) {
+        // _schema will be null only in tests
+        LOGGER.info("Column {} is not in schema, removing forward index", column);
+        columnOperationsMap.put(column, List.of(Operation.DISABLE_FORWARD_INDEX));
+        continue;
+      }
       FieldIndexConfigs newConf = _fieldIndexConfigs.get(column);
       boolean newIsFwd = newConf.getConfig(StandardIndexes.forward()).isEnabled();
       boolean newIsDict = newConf.getConfig(StandardIndexes.dictionary()).isEnabled();
