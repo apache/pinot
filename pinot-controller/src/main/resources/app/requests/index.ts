69e98165920f4c58e9bc426bf13440ff266ab3f0
Jayesh Choudhary <jayeshchoudhary619@gmail.com>
2023-08-20 01:36:10 +0530

    UI: fix message shown after scheduling task

--- pinot-controller/src/main/resources/app/requests/index.ts
+++ pinot-controller/src/main/resources/app/requests/index.ts
@@ -119,7 +119,7 @@ export const cleanupTasks = (taskType: string): Promise<AxiosResponse<OperationR
 export const deleteTasks = (taskType: string): Promise<AxiosResponse<OperationResponse>> =>
   baseApi.delete(`/tasks/${taskType}`, { headers: { ...headers, Accept: 'application/json' } });
 
-export const sheduleTask = (tableName: string, taskType: string): Promise<AxiosResponse<OperationResponse>> =>
+export const scheduleTask = (tableName: string, taskType: string): Promise<AxiosResponse<OperationResponse>> =>
   baseApi.post(`/tasks/schedule?tableName=${tableName}&taskType=${taskType}`, null, { headers: { ...headers, Accept: 'application/json' } });
 
 export const executeTask = (data): Promise<AxiosResponse<OperationResponse>> =>
