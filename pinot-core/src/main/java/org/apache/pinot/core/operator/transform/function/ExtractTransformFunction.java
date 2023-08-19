9583676d71c6bda1ec781476dc123c894ece1bda
Saurabh Dubey <saurabh.dubey@Saurabhs-MacBook-Pro.local>
2023-08-19 19:07:04 +0530

    Add DOY

--- pinot-core/src/main/java/org/apache/pinot/core/operator/transform/function/ExtractTransformFunction.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/transform/function/ExtractTransformFunction.java
@@ -36,7 +36,7 @@ public class ExtractTransformFunction extends BaseTransformFunction {
   protected Chronology _chronology = ISOChronology.getInstanceUTC();
 
   private enum Field {
-    YEAR, MONTH, DAY, DOW, HOUR, MINUTE, SECOND, QUARTER
+    YEAR, MONTH, DAY, DOY, DOW, HOUR, MINUTE, SECOND, QUARTER
   }
 
   @Override
@@ -90,6 +90,10 @@ public class ExtractTransformFunction extends BaseTransformFunction {
           break;
         case DOW:
           accessor = _chronology.dayOfWeek();
+          output[i] = accessor.get(timestamps[i]);
+          break;
+        case DOY:
+          accessor = _chronology.dayOfYear();
           output[i] = accessor.get(timestamps[i]);
           break;
         case HOUR:
