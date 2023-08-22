0c3ddd5d328d9045e8f6b50525e842fad83a632e
Saurabh Dubey <saurabh.dubey@saurabhs-macbook-pro-1.tail8a064.ts.net>
2023-08-22 17:52:12 +0530

    Fix other executors

--- pinot-core/src/main/java/org/apache/pinot/core/query/distinct/raw/BaseRawBytesSingleColumnDistinctExecutor.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/distinct/raw/BaseRawBytesSingleColumnDistinctExecutor.java
@@ -68,7 +68,7 @@ abstract class BaseRawBytesSingleColumnDistinctExecutor implements DistinctExecu
     if (_hasNull) {
       records.add(new Record(new Object[]{null}));
     }
-    assert records.size() <= _limit + 1;
+    assert (records.size() - _limit) <= 1;
     return new DistinctTable(dataSchema, records, _nullHandlingEnabled);
   }
 
