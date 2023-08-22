a51377b7391feaedebc8e2c807afe4779937bb12
Xiaotian (Jackie) Jiang <jackie.jxt@gmail.com>
2023-08-22 11:05:26 -0700

    Minor change to keep all distinct executor consistent

--- pinot-core/src/main/java/org/apache/pinot/core/query/distinct/raw/BaseRawStringSingleColumnDistinctExecutor.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/distinct/raw/BaseRawStringSingleColumnDistinctExecutor.java
@@ -67,7 +67,7 @@ abstract class BaseRawStringSingleColumnDistinctExecutor implements DistinctExec
     if (_hasNull) {
       records.add(new Record(new Object[]{null}));
     }
-    assert (records.size() - _limit) <= 1;
+    assert records.size() - (_hasNull ? 1 : 0) <= _limit;
     return new DistinctTable(dataSchema, records, _nullHandlingEnabled);
   }
 
