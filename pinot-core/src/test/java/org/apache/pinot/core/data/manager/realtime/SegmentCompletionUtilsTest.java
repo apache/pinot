563d3d6b03aec7ae440767b558db586ebf2bea71
xuanyili <xuanyili@uber.com>
2023-08-27 21:33:32 -0700

    fix lint error

--- pinot-core/src/test/java/org/apache/pinot/core/data/manager/realtime/SegmentCompletionUtilsTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/data/manager/realtime/SegmentCompletionUtilsTest.java
@@ -21,9 +21,8 @@ package org.apache.pinot.core.data.manager.realtime;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.assertFalse;
-
+import static org.testng.Assert.assertTrue;
 
 public class SegmentCompletionUtilsTest {
 
