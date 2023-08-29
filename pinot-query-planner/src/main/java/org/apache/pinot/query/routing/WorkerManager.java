63457b8e0f2b8eb0b4a4efb8929d91ba6f343489
Haitao Zhang <haitao@startree.ai>
2023-08-28 17:50:44 -0700

    fix linting issue

--- pinot-query-planner/src/main/java/org/apache/pinot/query/routing/WorkerManager.java
+++ pinot-query-planner/src/main/java/org/apache/pinot/query/routing/WorkerManager.java
@@ -20,7 +20,6 @@ package org.apache.pinot.query.routing;
 
 import com.google.common.base.Preconditions;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
