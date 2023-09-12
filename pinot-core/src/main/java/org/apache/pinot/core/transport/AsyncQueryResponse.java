62c02b5fc831e6c8854a4328db30c4564f5b3ee2
Jia Guo <jiaguo@linkedin.com>
2023-09-12 01:21:24 -0700

    close channel on direct oom, log large response

--- pinot-core/src/main/java/org/apache/pinot/core/transport/AsyncQueryResponse.java
+++ pinot-core/src/main/java/org/apache/pinot/core/transport/AsyncQueryResponse.java
@@ -165,9 +165,6 @@ public class AsyncQueryResponse implements QueryResponse {
     _status.set(Status.FAILED);
     _failedServer = serverRoutingInstance;
     _exception = exception;
-    _serverRoutingStatsManager.recordStatsUponResponseArrival(_requestId, serverRoutingInstance.getInstanceId(),
-        _timeoutMs);
-
     int count = (int) _countDownLatch.getCount();
     for (int i = 0; i < count; i++) {
       _countDownLatch.countDown();
