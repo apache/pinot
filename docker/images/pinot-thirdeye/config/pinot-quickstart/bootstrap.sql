086a93634012d99b3e22cef6525694756586c0f2
Gonzalo Ortiz Jaureguizar <gortiz@users.noreply.github.com>
2023-09-18 22:55:50 +0200

    Remove old thirdeye files. (#11609)

--- docker/images/pinot-thirdeye/config/pinot-quickstart/bootstrap.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO generic_json_entity (id, json_val, beanClass) VALUES (null, '{"id":null,"application":"test","recipients":""}', 'org.apache.pinot.thirdeye.datalayer.pojo.ApplicationBean');
-
-INSERT INTO application_index (base_id, application, recipients) VALUES (1, 'test', '');
-
