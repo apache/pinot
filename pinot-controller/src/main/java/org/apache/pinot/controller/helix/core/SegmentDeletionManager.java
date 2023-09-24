eaaa5bd316c565075cae1bd4833e2ffd4d63cf03
Xiang Fu <xiangfu.1024@gmail.com>
2023-09-24 14:26:10 -0700

    Allow delete local files with special characters

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/SegmentDeletionManager.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/SegmentDeletionManager.java
@@ -291,13 +291,14 @@ public class SegmentDeletionManager {
         for (String tableNameDir : tableNameDirs) {
           String tableName = URIUtils.getLastPart(tableNameDir);
           if (leadControllerManager.isLeaderForTable(tableName)) {
-            URI tableNameURI = URIUtils.getUri(tableNameDir);
+            URI tableNameURI = URIUtils.getUri(deletedDirURI.toString(), URIUtils.encode(tableName));
             // Get files that are aged
             final String[] targetFiles = pinotFS.listFiles(tableNameURI, false);
             int numFilesDeleted = 0;
             for (String targetFile : targetFiles) {
-              URI targetURI = URIUtils.getUri(targetFile);
-              long deletionTimeMs = getDeletionTimeMsFromFile(targetFile, pinotFS.lastModified(targetURI));
+              URI targetURI =
+                  URIUtils.getUri(tableNameURI.toString(), URIUtils.encode(URIUtils.getLastPart(targetFile)));
+              long deletionTimeMs = getDeletionTimeMsFromFile(targetURI.toString(), pinotFS.lastModified(targetURI));
               if (System.currentTimeMillis() >= deletionTimeMs) {
                 if (!pinotFS.delete(targetURI, true)) {
                   LOGGER.warn("Cannot remove file {} from deleted directory.", targetURI);
