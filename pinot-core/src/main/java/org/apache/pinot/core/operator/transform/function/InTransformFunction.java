cb29d3c27087ffe11e3918b3b87fa39c458c5c5f
Abhishek Sharma <abhishek.sharma@spothero.com>
2023-10-02 16:37:44 -0400

    Added calls for BaseTransformation init function for NULL support (#11653)

--- pinot-core/src/main/java/org/apache/pinot/core/operator/transform/function/InTransformFunction.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/transform/function/InTransformFunction.java
@@ -59,6 +59,7 @@ public class InTransformFunction extends BaseTransformFunction {
 
   @Override
   public void init(List<TransformFunction> arguments, Map<String, ColumnContext> columnContextMap) {
+    super.init(arguments, columnContextMap);
     int numArguments = arguments.size();
     Preconditions.checkArgument(numArguments >= 2, "At least 2 arguments are required for [%s] "
         + "transform function: (expression, values)", getName());
