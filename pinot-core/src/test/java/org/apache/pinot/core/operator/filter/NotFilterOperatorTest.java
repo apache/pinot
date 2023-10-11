7124a035e03ea7b10a17caa7ddccec0e80e634c1
dserfe <123439795+dserfe@users.noreply.github.com>
2023-10-11 04:50:57 -0500

    Fix a flaky test (#11771)
    * Update NotFilterOperatorTest.java
    
    * Update patch based on developer's suggestion
    
    * Update NotFilterOperatorTest.java
    
    * Update NotFilterOperatorTest.java
    
    ---------
    
    Co-authored-by: Xiang Fu <xiangfu.1024@gmail.com>

--- pinot-core/src/test/java/org/apache/pinot/core/operator/filter/NotFilterOperatorTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/operator/filter/NotFilterOperatorTest.java
@@ -21,9 +21,8 @@ package org.apache.pinot.core.operator.filter;
 import com.google.common.collect.ImmutableList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
+import java.util.List;
 import org.apache.pinot.core.common.BlockDocIdIterator;
 import org.apache.pinot.segment.spi.Constants;
 import org.testng.Assert;
@@ -35,8 +34,7 @@ public class NotFilterOperatorTest {
   @Test
   public void testNotOperator() {
     int[] docIds1 = new int[]{2, 3, 10, 15, 16, 17, 18, 21, 22, 23, 24, 26, 28};
-    Set<Integer> expectedResult = new HashSet();
-    expectedResult.addAll(Arrays.asList(0, 1, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 19, 20, 25, 27, 29));
+    List<Integer> expectedResult = Arrays.asList(0, 1, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 19, 20, 25, 27, 29);
     Iterator<Integer> expectedIterator = expectedResult.iterator();
     NotFilterOperator notFilterOperator = new NotFilterOperator(new TestFilterOperator(docIds1, 30), 30, false);
     BlockDocIdIterator iterator = notFilterOperator.nextBlock().getBlockDocIdSet().iterator();
