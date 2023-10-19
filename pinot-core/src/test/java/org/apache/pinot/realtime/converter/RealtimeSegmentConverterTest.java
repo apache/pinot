b88eaaf01f9085b688189b0a1637ee6a0a1a737f
Erich Ess <erich@startree.ai>
2023-10-19 10:22:12 -0400

    Added unit tests to check both row major and column major segment builders

--- pinot-core/src/test/java/org/apache/pinot/realtime/converter/RealtimeSegmentConverterTest.java
+++ pinot-core/src/test/java/org/apache/pinot/realtime/converter/RealtimeSegmentConverterTest.java
@@ -84,7 +84,7 @@ public class RealtimeSegmentConverterTest {
   }
 
   @Test
-  public void testNoRecordsIndexed()
+  public void testNoRecordsIndexedRowMajorSegmentBuilder()
       throws Exception {
     File tmpDir = new File(TMP_DIR, "tmp_" + System.currentTimeMillis());
     TableConfig tableConfig =
@@ -97,6 +97,7 @@ public class RealtimeSegmentConverterTest {
                 .setNoDictionaryColumns(Lists.newArrayList(LONG_COLUMN2))
                 .setVarLengthDictionaryColumns(Lists.newArrayList(STRING_COLUMN3))
                 .setOnHeapDictionaryColumns(Lists.newArrayList(LONG_COLUMN3))
+                .setColumnMajorSegmentBuilderEnabled(false)
                 .build();
     Schema schema = new Schema.SchemaBuilder().addSingleValueDimension(STRING_COLUMN1, FieldSpec.DataType.STRING)
         .addSingleValueDimension(STRING_COLUMN2, FieldSpec.DataType.STRING)
@@ -139,6 +140,76 @@ public class RealtimeSegmentConverterTest {
     RealtimeSegmentConverter converter =
         new RealtimeSegmentConverter(mutableSegmentImpl, segmentZKPropsConfig, outputDir.getAbsolutePath(), schema,
             tableNameWithType, tableConfig, segmentName, cdc, false, false);
+    converter.build(SegmentVersion.v3, null);
+
+    File indexDir = new File(outputDir, segmentName);
+    SegmentMetadataImpl segmentMetadata = new SegmentMetadataImpl(indexDir);
+    assertEquals(segmentMetadata.getTotalDocs(), 0);
+    assertEquals(segmentMetadata.getTimeColumn(), DATE_TIME_COLUMN);
+    assertEquals(segmentMetadata.getTimeUnit(), TimeUnit.MILLISECONDS);
+    assertEquals(segmentMetadata.getStartTime(), segmentMetadata.getEndTime());
+    assertTrue(segmentMetadata.getAllColumns().containsAll(schema.getColumnNames()));
+    assertEquals(segmentMetadata.getStartOffset(), "1");
+    assertEquals(segmentMetadata.getEndOffset(), "100");
+  }
+
+  @Test
+  public void testNoRecordsIndexedColumnMajorSegmentBuilder()
+          throws Exception {
+    File tmpDir = new File(TMP_DIR, "tmp_" + System.currentTimeMillis());
+    TableConfig tableConfig =
+            new TableConfigBuilder(TableType.OFFLINE)
+                    .setTableName("testTable")
+                    .setTimeColumnName(DATE_TIME_COLUMN)
+                    .setInvertedIndexColumns(Lists.newArrayList(STRING_COLUMN1))
+                    .setSortedColumn(LONG_COLUMN1)
+                    .setRangeIndexColumns(Lists.newArrayList(STRING_COLUMN2))
+                    .setNoDictionaryColumns(Lists.newArrayList(LONG_COLUMN2))
+                    .setVarLengthDictionaryColumns(Lists.newArrayList(STRING_COLUMN3))
+                    .setOnHeapDictionaryColumns(Lists.newArrayList(LONG_COLUMN3))
+                    .setColumnMajorSegmentBuilderEnabled(true)
+                    .build();
+    Schema schema = new Schema.SchemaBuilder().addSingleValueDimension(STRING_COLUMN1, FieldSpec.DataType.STRING)
+            .addSingleValueDimension(STRING_COLUMN2, FieldSpec.DataType.STRING)
+            .addSingleValueDimension(STRING_COLUMN3, FieldSpec.DataType.STRING)
+            .addSingleValueDimension(STRING_COLUMN4, FieldSpec.DataType.STRING)
+            .addSingleValueDimension(LONG_COLUMN1, FieldSpec.DataType.LONG)
+            .addSingleValueDimension(LONG_COLUMN2, FieldSpec.DataType.LONG)
+            .addSingleValueDimension(LONG_COLUMN3, FieldSpec.DataType.LONG)
+            .addMultiValueDimension(MV_INT_COLUMN, FieldSpec.DataType.INT).addMetric(LONG_COLUMN4, FieldSpec.DataType.LONG)
+            .addDateTime(DATE_TIME_COLUMN, FieldSpec.DataType.LONG, "1:MILLISECONDS:EPOCH", "1:MILLISECONDS").build();
+
+    String tableNameWithType = tableConfig.getTableName();
+    String segmentName = "testTable__0__0__123456";
+    IndexingConfig indexingConfig = tableConfig.getIndexingConfig();
+
+    DictionaryIndexConfig varLengthDictConf = new DictionaryIndexConfig(false, true);
+
+    RealtimeSegmentConfig.Builder realtimeSegmentConfigBuilder =
+            new RealtimeSegmentConfig.Builder().setTableNameWithType(tableNameWithType).setSegmentName(segmentName)
+                    .setStreamName(tableNameWithType).setSchema(schema).setTimeColumnName(DATE_TIME_COLUMN).setCapacity(1000)
+                    .setAvgNumMultiValues(3)
+                    .setIndex(Sets.newHashSet(LONG_COLUMN2), StandardIndexes.dictionary(), DictionaryIndexConfig.DISABLED)
+                    .setIndex(Sets.newHashSet(Sets.newHashSet(STRING_COLUMN3)), StandardIndexes.dictionary(), varLengthDictConf)
+                    .setIndex(Sets.newHashSet(STRING_COLUMN1), StandardIndexes.inverted(), IndexConfig.ENABLED)
+                    .setSegmentZKMetadata(getSegmentZKMetadata(segmentName)).setOffHeap(true)
+                    .setMemoryManager(new DirectMemoryManager(segmentName))
+                    .setStatsHistory(RealtimeSegmentStatsHistory.deserialzeFrom(new File(tmpDir, "stats")))
+                    .setConsumerDir(new File(tmpDir, "consumerDir").getAbsolutePath());
+
+    // create mutable segment impl
+    MutableSegmentImpl mutableSegmentImpl = new MutableSegmentImpl(realtimeSegmentConfigBuilder.build(), null);
+
+    File outputDir = new File(tmpDir, "outputDir");
+    SegmentZKPropsConfig segmentZKPropsConfig = new SegmentZKPropsConfig();
+    segmentZKPropsConfig.setStartOffset("1");
+    segmentZKPropsConfig.setEndOffset("100");
+    ColumnIndicesForRealtimeTable cdc = new ColumnIndicesForRealtimeTable(indexingConfig.getSortedColumn().get(0),
+            indexingConfig.getInvertedIndexColumns(), null, null,
+            indexingConfig.getNoDictionaryColumns(), indexingConfig.getVarLengthDictionaryColumns());
+    RealtimeSegmentConverter converter =
+            new RealtimeSegmentConverter(mutableSegmentImpl, segmentZKPropsConfig, outputDir.getAbsolutePath(), schema,
+                    tableNameWithType, tableConfig, segmentName, cdc, false, false);
     converter.build(SegmentVersion.v3, null);
 
     File indexDir = new File(outputDir, segmentName);
