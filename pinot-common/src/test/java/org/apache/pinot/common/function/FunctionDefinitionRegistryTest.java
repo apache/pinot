9feecdbf40d4e2afe43b502e639b4b551856caa8
Rong Rong <rongr@startree.ai>
2023-10-31 17:27:00 -0700

    fix test

--- pinot-common/src/test/java/org/apache/pinot/common/function/FunctionDefinitionRegistryTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/function/FunctionDefinitionRegistryTest.java
@@ -50,7 +50,8 @@ public class FunctionDefinitionRegistryTest {
       "arraylength", "arrayaverage", "arraymin", "arraymax", "arraysum", "clpdecode", "groovy", "inidset",
       "jsonextractscalar", "jsonextractindex", "jsonextractkey", "lookup", "mapvalue", "timeconvert", "valuein",
       // functions not needed for register b/c they are in std sql table or they will not be composed directly.
-      "in", "not_in", "and", "or", "range", "extract", "is_true", "is_not_true", "is_false", "is_not_false"
+      "in", "not_in", "and", "or", "range", "extract", "is_true", "is_not_true", "is_false", "is_not_false",
+      "constant"
   );
 
   @Test
