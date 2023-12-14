6e0d8ab42b4be8b757eaecb879c89a0c8d4553c2
Rong Rong <rongr@startree.ai>
2023-12-14 06:55:10 -0800

    adding back fallback option to current resolution

--- pinot-common/src/main/java/org/apache/pinot/common/function/FunctionRegistry.java
+++ pinot-common/src/main/java/org/apache/pinot/common/function/FunctionRegistry.java
@@ -19,11 +19,13 @@
 package org.apache.pinot.common.function;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -42,15 +44,16 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Registry for scalar functions.
- * <p>TODO: Merge FunctionRegistry and FunctionDefinitionRegistry to provide one single registry for all functions.
  */
 public class FunctionRegistry {
+  private static final Logger LOGGER = LoggerFactory.getLogger(FunctionRegistry.class);
+  // TODO: remove both when FunctionOperatorTable is in used.
+  private static final Map<String, Map<Integer, FunctionInfo>> FUNCTION_INFO_MAP = new HashMap<>();
+  private static final NameMultimap<PinotScalarFunction> FUNCTION_MAP = new NameMultimap<>();
+
   private FunctionRegistry() {
   }
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(FunctionRegistry.class);
-
-  private static final NameMultimap<PinotScalarFunction> FUNCTION_MAP = new NameMultimap<>();
 
   private static final int VAR_ARG_KEY = -1;
 
@@ -70,7 +73,9 @@ public class FunctionRegistry {
       if (scalarFunction.enabled()) {
         // Parse annotated function names and alias
         Set<String> scalarFunctionNames = Arrays.stream(scalarFunction.names()).collect(Collectors.toSet());
-        scalarFunctionNames.add(method.getName());
+        if (scalarFunctionNames.size() == 0) {
+          scalarFunctionNames.add(method.getName());
+        }
         boolean nullableParameters = scalarFunction.nullableParameters();
         FunctionRegistry.registerFunction(method, scalarFunctionNames, nullableParameters);
       }
@@ -97,11 +102,24 @@ public class FunctionRegistry {
 
   private static void registerFunction(Method method, Set<String> alias, boolean nullableParameters) {
     if (method.getAnnotation(Deprecated.class) == null) {
-//      String name = canonicalize(method.getName());
       for (String name : alias) {
-        FUNCTION_MAP.put(name, new PinotScalarFunction(name, alias, method, nullableParameters));
+        registerFunctionInfoMap(name, method, nullableParameters);
+        registerCalciteNamedFunctionMap(name, method, nullableParameters);
       }
     }
+  }
+
+  private static void registerFunctionInfoMap(String functionName, Method method, boolean nullableParameters) {
+    FunctionInfo functionInfo = new FunctionInfo(method, method.getDeclaringClass(), nullableParameters);
+    String canonicalName = canonicalize(functionName);
+    Map<Integer, FunctionInfo> functionInfoMap = FUNCTION_INFO_MAP.computeIfAbsent(canonicalName, k -> new HashMap<>());
+    FunctionInfo existFunctionInfo = functionInfoMap.put(method.getParameterCount(), functionInfo);
+    Preconditions.checkState(existFunctionInfo == null || existFunctionInfo.getMethod() == functionInfo.getMethod(),
+        "Function: %s with %s parameters is already registered", functionName, method.getParameterCount());
+  }
+
+  private static void registerCalciteNamedFunctionMap(String name, Method method, boolean nullableParameters) {
+    FUNCTION_MAP.put(name, new PinotScalarFunction(name, method, nullableParameters));
   }
 
   public static Map<String, List<PinotScalarFunction>> getRegisteredCalciteFunctionMap() {
@@ -116,7 +134,9 @@ public class FunctionRegistry {
    * Returns {@code true} if the given function name is registered, {@code false} otherwise.
    */
   public static boolean containsFunction(String functionName) {
-    return FUNCTION_MAP.containsKey(canonicalize(functionName), false);
+    // TODO: remove fallback to FUNCTION_INFO_MAP
+    return FUNCTION_MAP.containsKey(canonicalize(functionName), false)
+        || FUNCTION_INFO_MAP.containsKey(canonicalize(functionName));
   }
 
   /**
@@ -126,6 +146,22 @@ public class FunctionRegistry {
    */
   @Nullable
   public static FunctionInfo getFunctionInfo(String functionName, int numParameters) {
+    // TODO: remove fallback to FUNCTION_INFO_MAP
+    try {
+      return getFunctionInfoFromCalciteNamedMap(functionName, numParameters);
+    } catch (IllegalArgumentException iae) {
+      return getFunctionInfoFromFunctionInfoMap(functionName, numParameters);
+    }
+  }
+
+  @Nullable
+  private static FunctionInfo getFunctionInfoFromFunctionInfoMap(String functionName, int numParameters) {
+    Map<Integer, FunctionInfo> functionInfoMap = FUNCTION_INFO_MAP.get(canonicalize(functionName));
+    return functionInfoMap != null ? functionInfoMap.get(numParameters) : null;
+  }
+
+  @Nullable
+  private static FunctionInfo getFunctionInfoFromCalciteNamedMap(String functionName, int numParameters) {
     List<PinotScalarFunction> candidates = findByNumParameters(FUNCTION_MAP.range(functionName, false), numParameters);
     if (candidates.size() <= 1) {
       return candidates.size() == 1 ? candidates.get(0).getFunctionInfo() : null;
@@ -183,15 +219,13 @@ public class FunctionRegistry {
       implements org.apache.calcite.schema.ScalarFunction {
     private final FunctionInfo _functionInfo;
     private final String _name;
-    private final Set<String> _alias;
     private final Method _method;
 
-    public PinotScalarFunction(String name, Set<String> alias, Method method, boolean isNullableParameter) {
+    public PinotScalarFunction(String name, Method method, boolean isNullableParameter) {
       super(method);
       _name = name;
-      _alias = alias;
       _method = method;
-      _functionInfo = new FunctionInfo(method, method.getClass(), isNullableParameter);
+      _functionInfo = new FunctionInfo(method, method.getDeclaringClass(), isNullableParameter);
     }
 
     @Override
@@ -201,10 +235,6 @@ public class FunctionRegistry {
 
     public String getName() {
       return _name;
-    }
-
-    public Set<String> getAlias() {
-      return _alias;
     }
 
     public Method getMethod() {
