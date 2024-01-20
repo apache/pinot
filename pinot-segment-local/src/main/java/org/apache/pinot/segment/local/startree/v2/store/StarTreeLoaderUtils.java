7fcafdbdb0c9683816c231ccc5ae0c8058e6b50f
David Cromberge <davecromberge@gmail.com>
2024-01-20 22:12:16 +0000

    Fix checkstyle violations

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/startree/v2/store/StarTreeLoaderUtils.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/startree/v2/store/StarTreeLoaderUtils.java
@@ -27,7 +27,6 @@ import org.apache.pinot.segment.local.aggregator.ValueAggregatorFactory;
 import org.apache.pinot.segment.local.segment.index.forward.ForwardIndexReaderFactory;
 import org.apache.pinot.segment.local.segment.index.readers.forward.FixedBitSVForwardIndexReaderV2;
 import org.apache.pinot.segment.local.startree.OffHeapStarTree;
-import org.apache.pinot.segment.spi.AggregationFunctionType;
 import org.apache.pinot.segment.spi.ColumnMetadata;
 import org.apache.pinot.segment.spi.datasource.DataSource;
 import org.apache.pinot.segment.spi.index.StandardIndexes;
@@ -83,9 +82,9 @@ public class StarTreeLoaderUtils {
             AggregationFunctionColumnPair.resolveToStoredType(functionColumnPair);
         String metric = storedType.toColumnName();
         // This metric is already "covered" via an existing stored type
-        if (dataSourceMap.containsKey(metric))
+        if (dataSourceMap.containsKey(metric)) {
           continue;
-        else if (!indexReader.hasIndexFor(metric, StandardIndexes.forward())) {
+        } else if (!indexReader.hasIndexFor(metric, StandardIndexes.forward())) {
           metric = functionColumnPair.toColumnName();
         }
 
