3a074847217955da6371d84e98ae7f2923184390
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2024-02-02 10:39:19 +0100

    Qualify AbstractAggregationFunctionTest as an abstract class

--- pinot-core/src/test/java/org/apache/pinot/core/query/aggregation/function/AbstractAggregationFunctionTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/query/aggregation/function/AbstractAggregationFunctionTest.java
@@ -41,7 +41,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 
-public class AbstractAggregationFunctionTest {
+public abstract class AbstractAggregationFunctionTest {
 
   protected File _baseDir;
 
