c9a82c40a2c8bed5e86d8278e0bb57bfc5bee86f
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2024-02-03 11:58:58 -0800

    [Multi-stage] Optimize query dispatch (#12358)

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/service/dispatch/DispatchClient.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/service/dispatch/DispatchClient.java
@@ -26,8 +26,6 @@ import java.util.function.Consumer;
 import org.apache.pinot.common.proto.PinotQueryWorkerGrpc;
 import org.apache.pinot.common.proto.Worker;
 import org.apache.pinot.query.routing.QueryServerInstance;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 /**
@@ -37,8 +35,8 @@ import org.slf4j.LoggerFactory;
  *       let that take care of pooling. (2) Create a DispatchClient interface and implement pooled/non-pooled versions.
  */
 class DispatchClient {
-  private static final Logger LOGGER = LoggerFactory.getLogger(DispatchClient.class);
   private static final StreamObserver<Worker.CancelResponse> NO_OP_CANCEL_STREAM_OBSERVER = new CancelObserver();
+
   private final ManagedChannel _channel;
   private final PinotQueryWorkerGrpc.PinotQueryWorkerStub _dispatchStub;
 
@@ -51,23 +49,13 @@ class DispatchClient {
     return _channel;
   }
 
-  public void submit(Worker.QueryRequest request, int stageId, QueryServerInstance virtualServer, Deadline deadline,
+  public void submit(Worker.QueryRequest request, QueryServerInstance virtualServer, Deadline deadline,
       Consumer<AsyncQueryDispatchResponse> callback) {
-    try {
-      _dispatchStub.withDeadline(deadline).submit(request, new DispatchObserver(stageId, virtualServer, callback));
-    } catch (Exception e) {
-      LOGGER.error("Query Dispatch failed at client-side", e);
-      callback.accept(new AsyncQueryDispatchResponse(
-          virtualServer, stageId, Worker.QueryResponse.getDefaultInstance(), e));
-    }
+    _dispatchStub.withDeadline(deadline).submit(request, new DispatchObserver(virtualServer, callback));
   }
 
   public void cancel(long requestId) {
-    try {
-      Worker.CancelRequest cancelRequest = Worker.CancelRequest.newBuilder().setRequestId(requestId).build();
-      _dispatchStub.cancel(cancelRequest, NO_OP_CANCEL_STREAM_OBSERVER);
-    } catch (Exception e) {
-      LOGGER.error("Query Cancellation failed at client-side", e);
-    }
+    Worker.CancelRequest cancelRequest = Worker.CancelRequest.newBuilder().setRequestId(requestId).build();
+    _dispatchStub.cancel(cancelRequest, NO_OP_CANCEL_STREAM_OBSERVER);
   }
 }
