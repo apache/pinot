7a9381a280b0a0bd1b2a9380b050927b59057a30
Gonzalo Ortiz Jaureguizar <gortiz@users.noreply.github.com>
2024-02-20 14:14:16 +0100

    Remove usages of lombok (#12447)

--- pinot-common/src/main/java/org/apache/pinot/common/config/TlsConfig.java
+++ pinot-common/src/main/java/org/apache/pinot/common/config/TlsConfig.java
@@ -20,18 +20,13 @@ package org.apache.pinot.common.config;
 
 import io.netty.handler.ssl.SslProvider;
 import java.security.KeyStore;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.Objects;
 import org.apache.commons.lang3.StringUtils;
 
 
 /**
  * Container object for TLS/SSL configuration of pinot clients and servers (netty, grizzly, etc.)
  */
-@Getter
-@Setter
-@EqualsAndHashCode
 public class TlsConfig {
   private boolean _clientAuthEnabled;
   private String _keyStoreType = KeyStore.getDefaultType();
@@ -59,7 +54,101 @@ public class TlsConfig {
     _sslProvider = tlsConfig._sslProvider;
   }
 
+  public boolean isClientAuthEnabled() {
+    return _clientAuthEnabled;
+  }
+
+  public void setClientAuthEnabled(boolean clientAuthEnabled) {
+    _clientAuthEnabled = clientAuthEnabled;
+  }
+
+  public String getKeyStoreType() {
+    return _keyStoreType;
+  }
+
+  public void setKeyStoreType(String keyStoreType) {
+    _keyStoreType = keyStoreType;
+  }
+
+  public String getKeyStorePath() {
+    return _keyStorePath;
+  }
+
+  public void setKeyStorePath(String keyStorePath) {
+    _keyStorePath = keyStorePath;
+  }
+
+  public String getKeyStorePassword() {
+    return _keyStorePassword;
+  }
+
+  public void setKeyStorePassword(String keyStorePassword) {
+    _keyStorePassword = keyStorePassword;
+  }
+
+  public String getTrustStoreType() {
+    return _trustStoreType;
+  }
+
+  public void setTrustStoreType(String trustStoreType) {
+    _trustStoreType = trustStoreType;
+  }
+
+  public String getTrustStorePath() {
+    return _trustStorePath;
+  }
+
+  public void setTrustStorePath(String trustStorePath) {
+    _trustStorePath = trustStorePath;
+  }
+
+  public String getTrustStorePassword() {
+    return _trustStorePassword;
+  }
+
+  public void setTrustStorePassword(String trustStorePassword) {
+    _trustStorePassword = trustStorePassword;
+  }
+
+  public String getSslProvider() {
+    return _sslProvider;
+  }
+
+  public void setSslProvider(String sslProvider) {
+    _sslProvider = sslProvider;
+  }
+
   public boolean isCustomized() {
     return StringUtils.isNoneBlank(_keyStorePath) || StringUtils.isNoneBlank(_trustStorePath);
+  }
+
+  public boolean isInsecure() {
+    return _insecure;
+  }
+
+  public void setInsecure(boolean insecure) {
+    _insecure = insecure;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    TlsConfig tlsConfig = (TlsConfig) o;
+    return _clientAuthEnabled == tlsConfig._clientAuthEnabled && _insecure == tlsConfig._insecure && Objects.equals(
+        _keyStoreType, tlsConfig._keyStoreType) && Objects.equals(_keyStorePath, tlsConfig._keyStorePath)
+        && Objects.equals(_keyStorePassword, tlsConfig._keyStorePassword) && Objects.equals(_trustStoreType,
+        tlsConfig._trustStoreType) && Objects.equals(_trustStorePath, tlsConfig._trustStorePath) && Objects.equals(
+        _trustStorePassword, tlsConfig._trustStorePassword) && Objects.equals(_sslProvider, tlsConfig._sslProvider);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(_clientAuthEnabled, _keyStoreType, _keyStorePath, _keyStorePassword, _trustStoreType,
+        _trustStorePath, _trustStorePassword, _sslProvider, _insecure);
   }
 }
