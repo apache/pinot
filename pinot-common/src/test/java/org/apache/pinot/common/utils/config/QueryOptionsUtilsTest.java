811391ad14a2019a7bb09924ebbbd436889ce62a
egalpin <egalpin@users.noreply.github.com>
2024-02-21 11:59:42 -0800

    Adds tests

--- pinot-common/src/test/java/org/apache/pinot/common/utils/config/QueryOptionsUtilsTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/utils/config/QueryOptionsUtilsTest.java
@@ -21,6 +21,8 @@ package org.apache.pinot.common.utils.config;
 
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
+import java.util.Set;
+import org.apache.pinot.spi.config.table.FieldConfig;
 import org.apache.pinot.spi.utils.CommonConstants;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -42,5 +44,17 @@ public class QueryOptionsUtilsTest {
     // Then:
     Assert.assertEquals(resolved.get(CommonConstants.Broker.Request.QueryOptionKey.ENABLE_NULL_HANDLING), "true");
     Assert.assertEquals(resolved.get(CommonConstants.Broker.Request.QueryOptionKey.USE_MULTISTAGE_ENGINE), "false");
+  }
+
+  @Test
+  public void testIndexSkipConfigParsing() {
+    String indexSkipConfigStr = "col1=inverted,range&col2=sorted";
+    Map<String, String> queryOptions =
+        Map.of(CommonConstants.Broker.Request.QueryOptionKey.INDEX_SKIP_CONFIG, indexSkipConfigStr);
+    Map<String, Set<FieldConfig.IndexType>> indexSkipConfig = QueryOptionsUtils.getIndexSkipConfig(queryOptions);
+    Assert.assertEquals(indexSkipConfig.get("col1"),
+        Set.of(FieldConfig.IndexType.RANGE, FieldConfig.IndexType.INVERTED));
+    Assert.assertEquals(indexSkipConfig.get("col2"),
+        Set.of(FieldConfig.IndexType.SORTED));
   }
 }
