f89416f482db93efd9d0383a7c9125eea2de19f9
prashantpandey <pandepra.work@outlook.com>
2024-02-26 23:37:13 +0530

    Add API for zkCreate in ControllerRequestURLBuilder.java

--- pinot-spi/src/main/java/org/apache/pinot/spi/utils/builder/ControllerRequestURLBuilder.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/utils/builder/ControllerRequestURLBuilder.java
@@ -525,6 +525,10 @@ public class ControllerRequestURLBuilder {
     return StringUtil.join("/", _baseUrl, "zk/putChildren", "?path=" + path);
   }
 
+  public String forZKCreate() {
+    return StringUtil.join("/", _baseUrl, "zk/create");
+  }
+
   public String forZkGet(String path) {
     return StringUtil.join("/", _baseUrl, "zk/get", "?path=" + path);
   }
