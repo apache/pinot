2d75f59f17d21e4af41b9afaf9ddef607b46671b
sullis <github@seansullivan.com>
2024-03-06 09:30:49 -0800

    verify Plugin equals method

--- pinot-spi/src/test/java/org/apache/pinot/spi/plugin/PluginTest.java
+++ pinot-spi/src/test/java/org/apache/pinot/spi/plugin/PluginTest.java
@@ -18,36 +18,14 @@
  */
 package org.apache.pinot.spi.plugin;
 
-import java.util.Objects;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import org.testng.annotations.Test;
 
 
-public final class Plugin {
+public class PluginTest {
 
-  final String _name;
-
-  public Plugin(String name) {
-    _name = name;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Plugin plugin = (Plugin) o;
-    return Objects.equals(_name, plugin._name);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(_name);
-  }
-
-  @Override
-  public String toString() {
-    return _name;
+  @Test
+  public void equalsVerifier() {
+    EqualsVerifier.forClass(Plugin.class).verify();
   }
 }
