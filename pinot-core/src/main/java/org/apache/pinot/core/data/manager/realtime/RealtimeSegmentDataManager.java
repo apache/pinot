475e2a1add0581164945e7aa1cb844eebc01c9a3
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2024-03-08 14:09:02 -0800

    Remove unnecessary metric update

--- pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/RealtimeSegmentDataManager.java
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/RealtimeSegmentDataManager.java
@@ -913,7 +913,7 @@ public class RealtimeSegmentDataManager extends SegmentDataManager {
    * @param batchFirstOffset The offset of the first message in the batch.
    */
   private void validateStartOffset(StreamPartitionMsgOffset startOffset, StreamPartitionMsgOffset batchFirstOffset) {
-    if (batchFirstOffset != null && batchFirstOffset.compareTo(startOffset) > 0) {
+    if (batchFirstOffset.compareTo(startOffset) > 0) {
       _serverMetrics.addMeteredTableValue(_tableStreamName, ServerMeter.STREAM_DATA_LOSS, 1L);
       String message = "startOffset(" + startOffset
           + ") is older than topic's beginning offset(" + batchFirstOffset + ")";
@@ -921,9 +921,6 @@ public class RealtimeSegmentDataManager extends SegmentDataManager {
       _realtimeTableDataManager.addSegmentError(_segmentNameStr,
           new SegmentErrorInfo(now(), message, null)
       );
-    } else {
-      // Record that this batch has no data loss.
-      _serverMetrics.addMeteredTableValue(_tableStreamName, ServerMeter.STREAM_DATA_LOSS, 0L);
     }
   }
 
