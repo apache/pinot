9ed37e7dc397b2c83a727db5ebc0ca1b8d2cf337
Abhishek Sharma <abhishek.sharma@spothero.com>
2024-03-11 20:20:10 +0530

    another try for lint failure fix..

--- pinot-spi/src/test/java/org/apache/pinot/spi/env/SegmentMetadataPropertyConfigTest.java
+++ pinot-spi/src/test/java/org/apache/pinot/spi/env/SegmentMetadataPropertyConfigTest.java
@@ -33,7 +33,6 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertNull;
 
 
 public class SegmentMetadataPropertyConfigTest {
