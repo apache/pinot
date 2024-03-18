4cf6075807cc23ac0fd6a643f229948411b1c06d
tibrewalpratik <tibrewalpratik@uber.com>
2024-03-19 03:32:27 +0530

    update to query-param

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotSegmentRestletResource.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotSegmentRestletResource.java
@@ -674,7 +674,9 @@ public class PinotSegmentRestletResource {
       @ApiParam(value = "Retention period for the table segments (e.g. 12h, 3d); If not set, the retention period "
           + "will default to the first config that's not null: the table config, then to cluster setting, then '7d'. "
           + "Using 0d or -1d will instantly delete segments without retention")
-      @QueryParam("retention") String retentionPeriod, List<String> segments, @Context HttpHeaders headers) {
+      @QueryParam("retention") String retentionPeriod,
+      @ApiParam(value = "Segment names to be deleted if not provided deletes all segments by default",
+          allowMultiple = true) @QueryParam("segmentNames") List<String> segments, @Context HttpHeaders headers) {
     tableName = DatabaseUtils.translateTableName(tableName, headers);
     TableType tableType = Constants.validateTableType(tableTypeStr);
     if (tableType == null) {
@@ -682,12 +684,12 @@ public class PinotSegmentRestletResource {
     }
     String tableNameWithType =
         ResourceUtils.getExistingTableNamesWithType(_pinotHelixResourceManager, tableName, tableType, LOGGER).get(0);
-    int numSegments = segments.size();
-    if (numSegments == 0) {
+    if (segments == null || segments.isEmpty()) {
       deleteSegmentsInternal(tableNameWithType,
           _pinotHelixResourceManager.getSegmentsFromPropertyStore(tableNameWithType), retentionPeriod);
       return new SuccessResponse("All segments of table " + tableNameWithType + " deleted");
     } else {
+      int numSegments = segments.size();
       deleteSegmentsInternal(tableNameWithType, segments, retentionPeriod);
       if (numSegments <= 5) {
         return new SuccessResponse("Deleted segments: " + segments + " from table: " + tableNameWithType);
