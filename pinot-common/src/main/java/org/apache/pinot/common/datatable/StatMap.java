1236b1c3f84085880ba009898da4d6fe0a5734d2
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2024-04-04 11:17:51 +0200

    Serialize StatMaps as older stats

--- pinot-common/src/main/java/org/apache/pinot/common/datatable/StatMap.java
+++ pinot-common/src/main/java/org/apache/pinot/common/datatable/StatMap.java
@@ -23,12 +23,10 @@ import com.google.common.base.Preconditions;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.IntPredicate;
 import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import org.apache.pinot.spi.utils.JsonUtils;
@@ -73,19 +71,6 @@ public class StatMap<K extends Enum<K> & StatMap.Key> {
     _longValues = family.createLongValues();
     _booleanValues = family.createBooleanValues();
     _referenceValues = family.createReferenceValues();
-  }
-
-  private StatMap(Family<K> family, @Nullable int[] intValues, @Nullable long[] longValues,
-      @Nullable boolean[] booleanValues, @Nullable Object[] referenceValues) {
-    _family = family;
-    assert intValues == null || intValues.length == family._numIntValues;
-    _intValues = intValues;
-    assert longValues == null || longValues.length == family._numLongValues;
-    _longValues = longValues;
-    assert booleanValues == null || booleanValues.length == family._numBooleanValues;
-    _booleanValues = booleanValues;
-    assert referenceValues == null || referenceValues.length == family._numReferenceValues;
-    _referenceValues = referenceValues;
   }
 
   public int getInt(K key) {
@@ -230,55 +215,30 @@ public class StatMap<K extends Enum<K> & StatMap.Key> {
 
   public StatMap<K> merge(DataInput input)
       throws IOException {
-    int bitsPerId = 32 - Integer.numberOfLeadingZeros(Math.abs(_family._maxIndex));
-    int maxBytesPerId = (bitsPerId + 7) / 8;
+    byte serializedKeys = input.readByte();
 
-    int bytesPerId = input.readInt();
-    Preconditions.checkArgument(bytesPerId <= maxBytesPerId, "Invalid bytes per id: %s. Max expected = %s",
-        bytesPerId, maxBytesPerId);
-
-    int[] keys;
-    keys = readKeys(input, bytesPerId, _family._numIntValues);
-    if (_family._numIntValues != 0) {
-      assert _intValues != null : "Int values should not be null because there are int keys";
-      for (int intKey : keys) {
-        int value = input.readInt();
-        assert value != 0;
-        K key = _family.getKey(intKey, Type.INT);
-        _intValues[intKey] = key.merge(_intValues[intKey], value);
-      }
-    }
-
-    keys = readKeys(input, bytesPerId, _family._numLongValues);
-    if (_family._numLongValues != 0) {
-      assert _longValues != null : "Long values should not be null because there are long keys";
-      for (int longKey : keys) {
-        long value = input.readLong();
-        assert value != 0;
-        K key = _family.getKey(longKey, Type.LONG);
-        _longValues[longKey] = key.merge(_longValues[longKey], value);
-      }
-    }
-
-    keys = readKeys(input, bytesPerId, _family._numBooleanValues);
-    if (_family._numBooleanValues != 0) {
-      assert _booleanValues != null : "Boolean values should not be null because there are boolean keys";
-      for (int booleanKey : keys) {
-        boolean value = input.readBoolean();
-        assert value;
-        K key = _family.getKey(booleanKey, Type.BOOLEAN);
-        _booleanValues[booleanKey] = key.merge(_booleanValues[booleanKey], true);
-      }
-    }
-
-    keys = readKeys(input, bytesPerId, _family._numReferenceValues);
-    if (_family._numReferenceValues != 0) {
-      assert _referenceValues != null : "Reference values should not be null because there are reference keys";
-      for (int refKeys : keys) {
-        // In case we add more reference keys, this should be changed
-        String value = input.readUTF();
-        K key = _family.getKey(refKeys, Type.STRING);
-        _referenceValues[refKeys] = key.merge((String) _referenceValues[refKeys], value);
+    for (byte i = 0; i < serializedKeys; i++) {
+      int ordinal = input.readByte();
+      K key = _family._keys[ordinal];
+      switch (key.getType()) {
+        case BOOLEAN:
+          assert _booleanValues != null : "Boolean values should not be null because there are boolean keys";
+          _booleanValues[ordinal] = true;
+          break;
+        case INT:
+          assert _intValues != null : "Int values should not be null because there are int keys";
+          _intValues[ordinal] = key.merge(_intValues[ordinal], input.readInt());
+          break;
+        case LONG:
+          assert _longValues != null : "Long values should not be null because there are long keys";
+          _longValues[ordinal] = key.merge(_longValues[ordinal], input.readLong());
+          break;
+        case STRING:
+          assert _referenceValues != null : "Reference values should not be null because there are reference keys";
+          _referenceValues[ordinal] = key.merge((String) _referenceValues[ordinal], input.readUTF());
+          break;
+        default:
+          throw new IllegalStateException("Unknown type " + key.getType());
       }
     }
     return this;
@@ -287,226 +247,117 @@ public class StatMap<K extends Enum<K> & StatMap.Key> {
   public ObjectNode asJson() {
     ObjectNode node = JsonUtils.newObjectNode();
 
-    if (_intValues != null) {
-      for (K key : _family.getKeysOfType(Type.INT)) {
-        int index = _family.getIndex(key);
-        int value = _intValues[index];
-        if (value != 0 || key.includeDefaultInJson()) {
-          node.put(key.getStatName(), value);
-        }
-      }
-    }
-
-    if (_longValues != null) {
-      for (K key : _family.getKeysOfType(Type.LONG)) {
-        int index = _family.getIndex(key);
-        long value = _longValues[index];
-        if (value != 0 || key.includeDefaultInJson()) {
-          node.put(key.getStatName(), value);
-        }
-      }
-    }
-
-    if (_booleanValues != null) {
-      for (K key : _family.getKeysOfType(Type.BOOLEAN)) {
-        int index = _family.getIndex(key);
-        boolean value = _booleanValues[index];
-        if (value || key.includeDefaultInJson()) {
-          node.put(key.getStatName(), true);
-        }
-      }
-    }
-
-    if (_referenceValues != null) {
-      for (K key : _family.getKeysOfType(Type.STRING)) {
-        int index = _family.getIndex(key);
-        String value = (String) _referenceValues[index];
-        if (value != null || key.includeDefaultInJson()) {
-          node.put(key.getStatName(), value);
-        }
+    for (int i = 0; i < _family._keys.length; i++) {
+      K key = _family._keys[i];
+      switch (key.getType()) {
+        case BOOLEAN:
+          if (_booleanValues != null && _booleanValues[i]) {
+            node.put(key.getStatName(), true);
+          }
+          break;
+        case INT:
+          if (_intValues != null && _intValues[i] != 0) {
+            node.put(key.getStatName(), _intValues[i]);
+          }
+          break;
+        case LONG:
+          if (_longValues != null && _longValues[i] != 0) {
+            node.put(key.getStatName(), _longValues[i]);
+          }
+          break;
+        case STRING:
+          if (_referenceValues != null && _referenceValues[i] != null) {
+            node.put(key.getStatName(), (String) _referenceValues[i]);
+          }
+          break;
+        default:
+          throw new IllegalStateException("Unknown type " + key.getType());
       }
     }
 
     return node;
   }
 
+  public int size() {
+    int size = 0;
+    if (_intValues != null) {
+      for (int i = 0; i < _intValues.length; i++) {
+        if (_intValues[i] != 0) {
+          size++;
+        }
+      }
+    }
+    if (_longValues != null) {
+      for (int i = 0; i < _longValues.length; i++) {
+        if (_longValues[i] != 0) {
+          size++;
+        }
+      }
+    }
+    if (_booleanValues != null) {
+      for (int i = 0; i < _booleanValues.length; i++) {
+        if (_booleanValues[i]) {
+          size++;
+        }
+      }
+    }
+    if (_referenceValues != null) {
+      for (int i = 0; i < _referenceValues.length; i++) {
+        if (_referenceValues[i] != null) {
+          size++;
+        }
+      }
+    }
+    return size;
+  }
+
   public void serialize(DataOutput output)
       throws IOException {
-    int maxIndex = _family._maxIndex;
-    int bitsPerId = 32 - Integer.numberOfLeadingZeros(Math.abs(maxIndex));
-    int bytesPerId = (bitsPerId + 7) / 8;
 
-    output.writeInt(bytesPerId);
+    output.writeByte(size());
 
-    writeInts(output, bytesPerId);
-    writeLongs(output, bytesPerId);
-    writeBooleans(output, bytesPerId);
-    writeStrings(output, bytesPerId);
-  }
-
-  private void writeInts(DataOutput output, int bytesPerId)
-      throws IOException {
-    if (_intValues == null) {
-      writeInt(output, bytesPerId, 0);
-    } else {
-      writeValuedKeys(output, Type.INT, bytesPerId, _intValues.length, i -> _intValues[i] != 0);
-      for (int intValue : _intValues) {
-        if (intValue != 0) {
-          output.writeInt(intValue);
+    for (int i = 0; i < _family._keys.length; i++) {
+      K key = _family._keys[i];
+      byte index = _family.getIndex(key);
+      switch (key.getType()) {
+        case BOOLEAN: {
+          if (_booleanValues != null && _booleanValues[index]) {
+            output.writeByte(i);
+          }
+          break;
         }
-      }
-    }
-  }
-
-  private void writeLongs(DataOutput output, int bytesPerId)
-      throws IOException {
-    if (_longValues == null) {
-      writeInt(output, bytesPerId, 0);
-    } else {
-      writeValuedKeys(output, Type.LONG, bytesPerId, _longValues.length, i -> _longValues[i] != 0);
-      for (long longValue : _longValues) {
-        if (longValue != 0) {
-          output.writeLong(longValue);
+        case INT: {
+          if (_intValues != null && _intValues[index] != 0) {
+            output.writeByte(i);
+            output.writeInt(_intValues[index]);
+          }
+          break;
         }
-      }
-    }
-  }
-
-  private void writeBooleans(DataOutput output, int bytesPerId)
-      throws IOException {
-    if (_booleanValues == null) {
-      writeInt(output, bytesPerId, 0);
-    } else {
-      writeValuedKeys(output, Type.BOOLEAN, bytesPerId, _booleanValues.length, i -> _booleanValues[i]);
-      for (boolean booleanValue : _booleanValues) {
-        if (booleanValue) {
-          output.writeBoolean(true);
+        case LONG: {
+          if (_longValues != null && _longValues[index] != 0) {
+            output.writeByte(i);
+            output.writeLong(_longValues[index]);
+          }
+          break;
         }
-      }
-    }
-  }
-
-  private void writeStrings(DataOutput output, int bytesPerId)
-      throws IOException {
-    if (_referenceValues == null) {
-      writeInt(output, bytesPerId, 0);
-    } else {
-      writeValuedKeys(output, Type.STRING, bytesPerId, _referenceValues.length, i -> _referenceValues[i] != null);
-      for (Object referenceValue : _referenceValues) {
-        if (referenceValue != null) {
-          output.writeUTF((String) referenceValue);
+        case STRING: {
+          if (_referenceValues != null && _referenceValues[index] != null) {
+            output.writeByte(i);
+            output.writeUTF((String) _referenceValues[index]);
+          }
+          break;
         }
+        default:
+          throw new IllegalStateException("Unknown type " + key.getType());
       }
-    }
-  }
-
-  private void writeValuedKeys(DataOutput output, Type type, int bytesPerId, int length,
-      IntPredicate isValidIndex)
-      throws IOException {
-    List<K> allKeys = _family.getKeysOfType(type);
-    List<K> keys = new ArrayList<>(allKeys.size());
-    for (int i = 0; i < length; i++) {
-      if (isValidIndex.test(i)) {
-        keys.add(allKeys.get(i));
-      }
-    }
-    writeInt(output, bytesPerId, keys.size());
-    for (K key : keys) {
-      writeInt(output, bytesPerId, _family.getIndex(key));
     }
   }
 
   public static <K extends Enum<K> & Key> StatMap<K> deserialize(DataInput input, Class<K> keyClass)
       throws IOException {
-    Family<K> family = new Family<>(keyClass);
-
-    int bytesPerId = input.readInt();
-
-    int[] intKeys = readKeys(input, bytesPerId, family._numIntValues);
-    int[] intValues;
-    if (family._numIntValues == 0) {
-      intValues = null;
-    } else {
-      intValues = new int[family._numIntValues];
-      for (int intKey : intKeys) {
-        int value = input.readInt();
-        assert value != 0;
-        intValues[intKey] = value;
-      }
-    }
-
-    int[] longKeys = readKeys(input, bytesPerId, family._numLongValues);
-    long[] longValues;
-    if (family._numLongValues == 0) {
-      longValues = null;
-    } else {
-      longValues = new long[family._numLongValues];
-      for (int longKey : longKeys) {
-        long value = input.readLong();
-        assert value != 0;
-        longValues[longKey] = value;
-      }
-    }
-
-    int[] booleanKeys = readKeys(input, bytesPerId, family._numBooleanValues);
-    boolean[] booleanValues;
-    if (family._numBooleanValues == 0) {
-      booleanValues = null;
-    } else {
-      booleanValues = new boolean[family._numBooleanValues];
-      for (int booleanKey : booleanKeys) {
-        boolean value = input.readBoolean();
-        assert value;
-        booleanValues[booleanKey] = true;
-      }
-    }
-
-    int[] stringKeys = readKeys(input, bytesPerId, family._numReferenceValues);
-    Object[] referenceValues;
-    if (family._numReferenceValues == 0) {
-      referenceValues = null;
-    } else {
-      referenceValues = new Object[family._numReferenceValues];
-      for (int stringKey : stringKeys) {
-        String value = input.readUTF();
-        referenceValues[stringKey] = value;
-      }
-    }
-
-    return new StatMap<>(family, intValues, longValues, booleanValues, referenceValues);
-  }
-
-  private static int[] readKeys(DataInput input, int bytesPerId, int maxExpectedKeys)
-      throws IOException {
-    // sender may have more or less keys than expected.
-    // In case it has less, receiver extra keys will not be populated
-    // In case it has more, sender extra keys will be ignored
-    int numKeys = Math.min(readInt(input, bytesPerId), maxExpectedKeys);
-    int[] keys;
-    keys = new int[numKeys];
-    for (int i = 0; i < numKeys; i++) {
-      keys[i] = readInt(input, bytesPerId);
-    }
-    return keys;
-  }
-
-  private static void writeInt(DataOutput dos, int bytesPerId, int number) throws IOException {
-    // Write the lowest bytes of the number into the DataOutputStream
-    for (int i = 0; i < bytesPerId; i++) {
-      byte b = (byte) (number & 0xFF); // Get the lowest byte of the number
-      dos.writeByte(b); // Write the byte into the DataOutputStream
-      number >>= 8; // Shift the number 8 bits to the right to get the next byte
-    }
-  }
-
-  private static int readInt(DataInput dis, int bytesPerInt) throws IOException {
-    // Read the lowest bytes from the DataInputStream and reconstruct the integer
-    int number = 0;
-    for (int i = 0; i < bytesPerInt; i++) {
-      byte b = dis.readByte(); // Read the next byte from the DataInputStream
-      number |= (b & 0xFF) << (8 * i); // Combine the byte into the integer
-    }
-    return number;
+    StatMap<K> result = new StatMap<>(keyClass);
+    result.merge(input);
+    return result;
   }
 
   @Override
