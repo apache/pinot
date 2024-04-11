1f17c465f2e31c31d87a1127c6fac08c83b95528
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2024-04-11 14:56:21 -0700

    Cleanup HTTP components dependencies and upgrade Thrift (#12905)

--- pinot-plugins/pinot-input-format/pinot-thrift/src/main/java/org/apache/pinot/plugin/inputformat/thrift/ThriftRecordExtractor.java
+++ pinot-plugins/pinot-input-format/pinot-thrift/src/main/java/org/apache/pinot/plugin/inputformat/thrift/ThriftRecordExtractor.java
@@ -97,7 +97,8 @@ public class ThriftRecordExtractor extends BaseRecordExtractor<TBase> {
   protected Object convertRecord(Object value) {
     TBase record = (TBase) value;
     Map<Object, Object> convertedRecord = new HashMap<>();
-    for (TFieldIdEnum tFieldIdEnum : FieldMetaData.getStructMetaDataMap(record.getClass()).keySet()) {
+    Set<TFieldIdEnum> tFieldIdEnums = FieldMetaData.getStructMetaDataMap(record.getClass()).keySet();
+    for (TFieldIdEnum tFieldIdEnum : tFieldIdEnums) {
       Object fieldValue = record.getFieldValue(tFieldIdEnum);
       if (fieldValue != null) {
         fieldValue = convert(fieldValue);
