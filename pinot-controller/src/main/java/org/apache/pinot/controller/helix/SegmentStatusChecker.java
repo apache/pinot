a710330f8521131761f62a9fa8c7dc10dba79168
Shounak kulkarni <shounakmk219@gmail.com>
2024-04-16 19:43:45 +0530

    remove unused import

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/SegmentStatusChecker.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/SegmentStatusChecker.java
@@ -27,7 +27,6 @@ import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.helix.model.ExternalView;
 import org.apache.helix.model.IdealState;
 import org.apache.helix.store.zk.ZkHelixPropertyStore;
