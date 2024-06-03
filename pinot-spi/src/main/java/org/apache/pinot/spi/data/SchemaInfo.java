4bc0a564544fb9262a47f8d35318869d9288713d
deepthi912 <deepthi.chadalavada@crst.com>
2024-06-02 21:31:17 -0400

    Change details of SchemaInfo checkstyle exception

--- pinot-spi/src/main/java/org/apache/pinot/spi/data/SchemaInfo.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/data/SchemaInfo.java
@@ -1,23 +1,38 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.spi.data;
 
+/**
+ * This class gives the details of a particular schema and the corresponding column metrics
+ *
+ */
 public class SchemaInfo {
-  String schemaName;
-  int dimensionFieldSpecsCount;
-  int dateTimeFieldSpecsCount;
-  int metricFieldSpecsCount;
+  String _schemaName;
+  int _dimensionFieldSpecsCount;
+  int _dateTimeFieldSpecsCount;
+  int _metricFieldSpecsCount;
 
-  /**
-   * This class gives the details of a particular schema and the column metrics
-   * @param schemaName name of the schema
-   * @param dimensionFieldSpecsCount corresponds to dimensions column count
-   * @param dateTimeFieldSpecsCount corresponds to date time fields column count
-   * @param metricFieldSpecsCount corresponds to metric fields column count
-   */
   public SchemaInfo(String schemaName, int dimensionFieldSpecsCount, int dateTimeFieldSpecsCount,
       int metricFieldSpecsCount) {
-    this.schemaName = schemaName;
-    this.dimensionFieldSpecsCount = dimensionFieldSpecsCount;
-    this.dateTimeFieldSpecsCount = dateTimeFieldSpecsCount;
-    this.metricFieldSpecsCount = metricFieldSpecsCount;
+    this._schemaName = schemaName;
+    this._dimensionFieldSpecsCount = dimensionFieldSpecsCount;
+    this._dateTimeFieldSpecsCount = dateTimeFieldSpecsCount;
+    this._metricFieldSpecsCount = metricFieldSpecsCount;
   }
 }
