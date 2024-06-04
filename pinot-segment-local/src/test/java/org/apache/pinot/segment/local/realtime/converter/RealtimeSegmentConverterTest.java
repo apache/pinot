bf9a8242b94ce3af7164b6c1faf9d2ce6e1aca9d
Christopher Peck <peck@uber.com>
2024-06-03 18:51:35 -0700

    lint

--- pinot-segment-local/src/test/java/org/apache/pinot/segment/local/realtime/converter/RealtimeSegmentConverterTest.java
+++ pinot-segment-local/src/test/java/org/apache/pinot/segment/local/realtime/converter/RealtimeSegmentConverterTest.java
@@ -18,7 +18,6 @@
  */
 package org.apache.pinot.segment.local.realtime.converter;
 
-import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import java.io.File;
