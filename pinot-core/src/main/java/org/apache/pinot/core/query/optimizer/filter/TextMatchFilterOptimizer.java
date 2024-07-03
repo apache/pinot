baf4c0bdc703112d4902e22dfd1366a04f075feb
Christopher Peck <peck@uber.com>
2024-07-03 11:53:00 -0700

    spotless

--- pinot-core/src/main/java/org/apache/pinot/core/query/optimizer/filter/TextMatchFilterOptimizer.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/optimizer/filter/TextMatchFilterOptimizer.java
@@ -18,8 +18,8 @@
  */
 package org.apache.pinot.core.query.optimizer.filter;
 
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
