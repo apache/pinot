1764fdff121cfd9dc5209e99674fe7e99fc8f7dc
tibrewalpratik <tibrewalpratik@uber.com>
2024-07-12 21:47:57 +0530

    address comments - add condition

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/upsert/BasePartitionUpsertMetadataManager.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/upsert/BasePartitionUpsertMetadataManager.java
@@ -432,7 +432,9 @@ public abstract class BasePartitionUpsertMetadataManager implements PartitionUps
     String segmentName = segment.getSegmentName();
     _logger.info("Adding segment: {}, current primary key count: {}", segmentName, getNumPrimaryKeys());
     long startTimeMs = System.currentTimeMillis();
-    segment.deleteValidDocIdsSnapshot();
+    if (!_enableSnapshot) {
+      segment.deleteValidDocIdsSnapshot();
+    }
     try (UpsertUtils.RecordInfoReader recordInfoReader = new UpsertUtils.RecordInfoReader(segment, _primaryKeyColumns,
         _comparisonColumns, _deleteRecordColumn)) {
       Iterator<RecordInfo> recordInfoIterator =
