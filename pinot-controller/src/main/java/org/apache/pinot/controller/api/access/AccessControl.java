fbef6d9403d0a55b865fdfeb0b80651f87021b6d
Abhishek Sharma <abhishek.sharma@spothero.com>
2024-07-16 21:04:41 -0400

    Cleanup of the method hasAccess.

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/access/AccessControl.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/access/AccessControl.java
@@ -41,7 +41,7 @@ public interface AccessControl extends FineGrainedAccessControl {
    * @return whether the client has permission
    */
   boolean hasAccess(@Nullable String tableName, AccessType accessType, HttpHeaders httpHeaders,
-      @Nullable String endpointUrl);
+      String endpointUrl);
 
   /**
    * Return whether the client has permission to access the endpoints with are not table level
@@ -51,7 +51,7 @@ public interface AccessControl extends FineGrainedAccessControl {
    * @param endpointUrl the request url for which this access control is called
    * @return whether the client has permission
    */
-  default boolean hasAccess(AccessType accessType, HttpHeaders httpHeaders, @Nullable String endpointUrl) {
+  default boolean hasAccess(AccessType accessType, HttpHeaders httpHeaders, String endpointUrl) {
     return hasAccess(null, accessType, httpHeaders, endpointUrl);
   }
 
