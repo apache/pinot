2586796a6dadef13b2fc7e89923e734d2cdef3da
Shounak kulkarni <shounakmk219@gmail.com>
2024-07-18 15:11:25 +0530

    add nullable annotation

--- pinot-common/src/main/java/org/apache/pinot/common/metrics/AbstractMetrics.java
+++ pinot-common/src/main/java/org/apache/pinot/common/metrics/AbstractMetrics.java
@@ -31,6 +31,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.LongSupplier;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import org.apache.pinot.common.Utils;
 import org.apache.pinot.spi.metrics.PinotGauge;
 import org.apache.pinot.spi.metrics.PinotMeter;
@@ -499,6 +500,7 @@ public abstract class AbstractMetrics<QP extends AbstractMetrics.QueryPhase, M e
    * @param gaugeName gauge name
    * @return gauge value. If gauge is not present return null.
    */
+  @Nullable
   public Long getGaugeValue(final String gaugeName) {
     AtomicLong value = _gaugeValues.get(gaugeName);
     return value != null ? value.get() : null;
