1c3c60534be6d331ccd2f632e7476c5d66b09ef4
Navya <navya.kumar@uber.com>
2024-07-18 10:56:00 -0700

    Address Comments - Reformatting

--- pinot-common/src/main/java/org/apache/pinot/common/function/scalar/MapFunctions.java
+++ pinot-common/src/main/java/org/apache/pinot/common/function/scalar/MapFunctions.java
@@ -1,34 +1,54 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.common.function.scalar;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.pinot.spi.annotations.ScalarFunction;
-
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.pinot.spi.annotations.ScalarFunction;
 
 public class MapFunctions {
-    private MapFunctions() {
-    }
+    private MapFunctions() { }
+
+    public static final ObjectMapper oM = new ObjectMapper();
 
     /**
      * Internal Helper Function to convert Json String Values to Java Maps
+     *
      * @param jsonString
      * @return converted Java Map from JSON String
      */
-    public static Map stringToMap(String jsonString) {
-        //input validation for ObjectMapper.readValue();
+    public static LinkedHashMap stringToMap(String jsonString) {
+        // input validation for ObjectMapper.readValue();
         if (jsonString == null) {
             return null;
         }
 
-        Map<Object, Object> convertedMap = null;
+        LinkedHashMap<Object, Object> convertedMap = null;
         try {
-            //object --> java map with objectMapper
-            convertedMap = new ObjectMapper().readValue(jsonString,
-                    new TypeReference<Map<Object, Object>>() { });
+            // object --> java map with objectMapper
+            convertedMap =
+                oM.readValue(jsonString, new TypeReference<LinkedHashMap<Object, Object>>() { });
         } catch (Exception e) {
             throw new RuntimeException("Unknown issue: " + jsonString);
         }
@@ -37,31 +57,157 @@ public class MapFunctions {
     }
 
     /**
+     * Scalar listMapEntries UDF that lists all key-value entries from a String representation of JSON
+     * Map
+     *
+     * @param jsonString
+     * @return string array of all key-value pairs from JSON map in data
+     */
+    @ScalarFunction
+    public static String[] listMapEntries(String jsonString) {
+        // handle empty string case
+        if (jsonString == null) {
+            return new String[0];
+        }
+
+        // convert string input to equivalent java map
+        LinkedHashMap convertedMap = (LinkedHashMap) stringToMap(jsonString);
+
+        // add map key elements to list
+        List<Map.Entry<Object, Object>> keyList = new ArrayList<>(convertedMap.size());
+        keyList.addAll(convertedMap.keySet());
+
+        // convert raw map entry list to list of string key-value pair representations
+        List<String> stringMapEntriesList = new ArrayList<String>(convertedMap.size());
+        String currentEntryString = "";
+        for (Object keyObj : keyList) {
+            if (convertedMap.get(keyObj) != null) {
+                currentEntryString += keyObj.toString() + " = " + convertedMap.get(keyObj).toString();
+            } else {
+                currentEntryString += keyObj.toString() + " = ";
+            }
+            stringMapEntriesList.add(currentEntryString);
+            currentEntryString = "";
+        }
+
+        // convert string map entries list to string array and return
+        return Arrays.copyOf(
+            stringMapEntriesList.toArray(), stringMapEntriesList.size(), String[].class);
+    }
+
+    /**
      * Scalar listKeys UDF that lists all keys from a String representation of JSON Map
+     *
      * @param jsonString
      * @return string array of all keys from JSON map in data
      */
     @ScalarFunction
     public static String[] listKeys(String jsonString) {
-        //handle empty string case
+        // handle empty string case
         if (jsonString == null) {
-            return new String[]{};
+            return new String[0];
         }
 
-        //convert string input to equivalent java map
-        Map convertedMap = stringToMap(jsonString);
+        // convert string input to equivalent java map
+        LinkedHashMap convertedMap = (LinkedHashMap) stringToMap(jsonString);
 
-        //add map keys to an object list
-        List<Object> mapList = new ArrayList<>(convertedMap.size());
-        mapList.addAll(convertedMap.keySet());
+        // add map key elements to list
+        List<Map.Entry<Object, Object>> keyList = new ArrayList<>(convertedMap.size());
+        keyList.addAll(convertedMap.keySet());
 
-        //convert object list to string list
+        // convert object list to string list
         List<String> stringKeysList = new ArrayList<String>(convertedMap.size());
-        for (Object key: mapList) {
+        for (Object key : keyList) {
             stringKeysList.add(key.toString());
         }
 
-        //convert string keys list to string array and return
+        // convert string keys list to string array and return
         return Arrays.copyOf(stringKeysList.toArray(), stringKeysList.size(), String[].class);
+    }
+
+    /**
+     * Scalar listValues UDF that lists all values from a String representation of JSON Map
+     *
+     * @param jsonString
+     * @return string array of all values from JSON map in data
+     */
+    @ScalarFunction
+    public static String[] listValues(String jsonString) {
+        // handle empty string case
+        if (jsonString == null) {
+            return new String[0];
+        }
+
+        // convert string input to equivalent java map
+        LinkedHashMap convertedMap = (LinkedHashMap) stringToMap(jsonString);
+
+        // add map key elements to list
+        List<Map.Entry<Object, Object>> keyList = new ArrayList<>(convertedMap.size());
+        keyList.addAll(convertedMap.keySet());
+
+        // convert object list to string list
+        List<String> stringValuesList = new ArrayList<String>(convertedMap.size());
+        for (Object key : keyList) {
+            if (convertedMap.get(key) == null) {
+                stringValuesList.add("null");
+                continue;
+            }
+            stringValuesList.add(convertedMap.get(key).toString());
+        }
+
+        // convert string keys list to string array and return
+        return Arrays.copyOf(stringValuesList.toArray(), stringValuesList.size(), String[].class);
+    }
+
+    /**
+     * Scalar mapSize UDF that returns size/number of entries of the Map represented by a JSON Map
+     * String
+     *
+     * @param jsonString
+     * @return size of JSON map in data
+     */
+    @ScalarFunction
+    public static int mapSize(String jsonString) {
+        // handle empty string case
+        if (jsonString == null) {
+            return 0;
+        }
+
+        // convert string input to equivalent java map
+        Map convertedMap = stringToMap(jsonString);
+
+        // handle both cases for where returned map is null or is valid but empty
+        if (convertedMap == null || convertedMap.entrySet().isEmpty()) {
+            return 0;
+        }
+
+        // return number of entries extracted
+        return convertedMap.entrySet().size();
+    }
+
+    /**
+     * Internal Testing Function getValueFromKey that returns given key's corresponding value for
+     * given JSON Map Object
+     *
+     * @param object
+     * @param key
+     * @return corresponding value for given key from JSON map in data
+     */
+    public static Object getValueFromKey(Object object, Object key) {
+        // handle empty string case
+        if (object == null) {
+            return null;
+        }
+
+        // convert json object input to equivalent java map
+        Map convertedMap = stringToMap((String) object);
+
+        // handle both cases for where returned map is null or is valid but empty
+        if (convertedMap == null || convertedMap.entrySet().isEmpty()) {
+            return null;
+        }
+
+        // return key's value in map. returns null if key not found.
+        return convertedMap.get(key);
     }
 }
--- pinot-common/src/test/java/org/apache/pinot/common/function/scalar/MapFunctionsTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/function/scalar/MapFunctionsTest.java
@@ -1,124 +1,253 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.common.function.scalar;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MapFunctionsTest {
 
     @Test
-    public void testRegMapSameTypes()
-            throws JsonProcessingException {
+    public void testRegMapSameTypes() throws JsonProcessingException {
+        // test with all same type keys and values (Strings)
+        String jsonString =
+            "{"
+                + "  \"id\": \"7044885078\","
+                + "  \"type\": \"CreateEvent\","
+                + "  \"public\": \"true\","
+                + "  \"created_at\": \"2018-01-01T11:12:53Z\","
+                + "    \"date\": \"November\","
+                + "    \"occasion\": \"Anniversary\","
+                + "    \"emotion\": \"Happiness\""
+                + "}";
 
-        // CHECKSTYLE:OFF
-        // @formatter:off
+        // check individual elements correctly recorded
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "id"), "7044885078");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "type"), "CreateEvent");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "public"), "true");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "date"), "November");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "created_at"), "2018-01-01T11:12:53Z");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "occasion"), "Anniversary");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "emotion"), "Happiness");
 
-        //test with all same type keys and values (Strings)
-        String jsonString = "{" +
-                "  \"id\": \"7044885078\"," +
-                "  \"type\": \"CreateEvent\"," +
-                "  \"public\": \"true\"," +
-                "  \"created_at\": \"2018-01-01T11:12:53Z\"," +
-                "    \"date\": \"November\"," +
-                "    \"occasion\": \"Anniversary\"," +
-                "    \"emotion\": \"Happiness\"" +
-                "}";
+        // check mapEntries list
+        assertEquals(
+            MapFunctions.listMapEntries(jsonString),
+            new String[] {
+                "id = 7044885078",
+                "type = CreateEvent",
+                "public = true",
+                "created_at = 2018-01-01T11:12:53Z",
+                "date = November",
+                "occasion = Anniversary",
+                "emotion = Happiness"
+            });
 
-        // @formatter:on
-        // CHECKSTYLE:ON
-
-        assertEquals(MapFunctions.listKeys(jsonString), new String[]{"id", "type", "public", "created_at",
-                "date", "occasion", "emotion"});
+        // check key, value lists and nPairs functions
+        assertEquals(
+            MapFunctions.listKeys(jsonString),
+            new String[] {"id", "type", "public", "created_at", "date", "occasion", "emotion"});
+        assertEquals(
+            MapFunctions.listValues(jsonString),
+            new String[] {
+                "7044885078",
+                "CreateEvent",
+                "true",
+                "2018-01-01T11:12:53Z",
+                "November",
+                "Anniversary",
+                "Happiness"
+            });
     }
 
     @Test
-    public void testRegMapDiffTypes1()
-            throws JsonProcessingException {
+    public void testRegMapDiffTypes1() throws JsonProcessingException {
+        // test with different type values (number, bool, array, string, null)
+        String jsonString =
+            "{"
+                + "  \"id\": 704488,"
+                + "  \"type\": false,"
+                + "  \"public\": 4.0,"
+                + "  \"created_at\": \"2018-01-01T11:12:53Z\","
+                + "    \"date\": \"n\","
+                + "    \"occasion\":  [80, 85, 90, 95, 100],"
+                + "    \"extra\": null,"
+                + "    \"emotion\": [\"happy\",\"sad\",\"angry\"]"
+                + "}";
 
-        // CHECKSTYLE:OFF
-        // @formatter:off
+        // check individual elements correctly recorded
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "id"), 704488);
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "type"), false);
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "public"), 4.0);
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "created_at"), "2018-01-01T11:12:53Z");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "date"), "n");
+        assertEquals(
+            MapFunctions.getValueFromKey(jsonString, "occasion"),
+            new ArrayList<>(Arrays.asList(new Integer[] {80, 85, 90, 95, 100})));
+        assertEquals(
+            MapFunctions.getValueFromKey(jsonString, "occasion"),
+            new ArrayList<>(Arrays.asList(new Object[] {80, 85, 90, 95, 100})));
+        assertNull(MapFunctions.getValueFromKey(jsonString, "extra"));
+        assertEquals(
+            MapFunctions.getValueFromKey(jsonString, "emotion"),
+            new ArrayList<>(Arrays.asList(new String[] {"happy", "sad", "angry"})));
+        assertEquals(
+            MapFunctions.getValueFromKey(jsonString, "emotion"),
+            new ArrayList<>(Arrays.asList(new Object[] {"happy", "sad", "angry"})));
 
-        //test with different type values (number, bool, array, string, null)
-        String jsonString = "{" +
-                "  \"id\": 704488," +
-                "  \"type\": false," +
-                "  \"public\": 4.0," +
-                "  \"created_at\": \"2018-01-01T11:12:53Z\"," +
-                "    \"date\": \"n\"," +
-                "    \"occasion\":  [80, 85, 90, 95, 100]," +
-                "    \"extra\": null," +
-                "    \"emotion\": [\"happy\",\"sad\",\"angry\"]" +
-                "}";
+        // check mapEntries list
+        assertEquals(
+            MapFunctions.listMapEntries(jsonString),
+            new String[] {
+                "id = 704488",
+                "type = false",
+                "public = 4.0",
+                "created_at = 2018-01-01T11:12:53Z",
+                "date = n",
+                "occasion = [80, 85, 90, 95, 100]",
+                "extra = ",
+                "emotion = [happy, sad, angry]"
+            });
 
-        // @formatter:on
-        // CHECKSTYLE:ON
-
-        assertEquals(MapFunctions.listKeys(jsonString), new String[]{"id", "type", "public", "created_at", "date",
-                "occasion", "extra", "emotion"});
+        // check key, value lists and nPairs functions
+        assertEquals(
+            MapFunctions.listKeys(jsonString),
+            new String[] {
+                "id", "type", "public", "created_at", "date", "occasion", "extra", "emotion"
+            });
+        assertEquals(
+            MapFunctions.listValues(jsonString),
+            new String[] {
+                "704488",
+                "false",
+                "4.0",
+                "2018-01-01T11:12:53Z",
+                "n",
+                "[80, 85, 90, 95, 100]",
+                "null",
+                "[happy, sad, angry]"
+            });
     }
 
     @Test
-    public void testRegMapDiffTypes2()
-            throws JsonProcessingException {
+    public void testRegMapDiffTypes2() throws JsonProcessingException {
+        // test different value types, emphasis on the case of "subjects" value
+        String jsonString =
+            "{\n"
+                + "    \"name\": \"Pete\",\n"
+                + "    \"age\": 24,\n"
+                + "    \"subjects\": [\n"
+                + "        {\n"
+                + "            \"name\": \"maths\",\n"
+                + "            \"homework_grades\": [80, 85, 90, 95, 100],\n"
+                + "            \"grade\": \"A\"\n"
+                + "        },\n"
+                + "        {\n"
+                + "            \"name\": \"english\",\n"
+                + "            \"homework_grades\": [60, 65, 70, 85, 90],\n"
+                + "            \"grade\": \"B\"\n"
+                + "        }\n"
+                + "    ]\n"
+                + "}";
 
-        // CHECKSTYLE:OFF
-        // @formatter:off
+        // check individual elements correctly recorded
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "name"), "Pete");
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "age"), 24);
+        String correctSubjectContents =
+            "[{\"name\":\"maths\", \"homework_grades\":[80, 85, 90, 95, 100], \"grade\":"
+                + "\"A\"}, {\"name\":\"english\", \"homework_grades\":[60, 65, 70, 85, 90], \"grade\":\"B\"}]";
+        ObjectMapper oM = new ObjectMapper();
+        Object[] correctSubjectValArray = oM.readValue(correctSubjectContents, Object[].class);
+        System.out.println("New: " + new ArrayList<>(Arrays.asList(correctSubjectValArray)));
+        assertEquals(
+            MapFunctions.getValueFromKey(jsonString, "subjects"),
+            new ArrayList<>(Arrays.asList(correctSubjectValArray)));
 
-        //test different value types, emphasis on the case of "subjects" value
-        String jsonString = "{\n" +
-                "    \"name\": \"Pete\",\n" +
-                "    \"age\": 24,\n" +
-                "    \"subjects\": [\n" +
-                "        {\n" +
-                "            \"name\": \"maths\",\n" +
-                "            \"homework_grades\": [80, 85, 90, 95, 100],\n" +
-                "            \"grade\": \"A\"\n" +
-                "        },\n" +
-                "        {\n" +
-                "            \"name\": \"english\",\n" +
-                "            \"homework_grades\": [60, 65, 70, 85, 90],\n" +
-                "            \"grade\": \"B\"\n" +
-                "        }\n" +
-                "    ]\n" +
-                "}";
+        // check mapEntries list
+        assertEquals(
+            MapFunctions.listMapEntries(jsonString),
+            new String[] {
+                "name = Pete",
+                "age = 24",
+                "subjects = [{name=maths, homework_grades=[80, 85, 90, 95, 100], grade=A}, {name=english, "
+                    + "homework_grades=[60, 65, 70, 85, 90], grade=B}]"
+            });
 
-        // @formatter:on
-        // CHECKSTYLE:ON
-
-        assertEquals(MapFunctions.listKeys(jsonString), new String[]{"name", "age", "subjects"});
+        // check key, value lists and nPairs functions
+        assertEquals(MapFunctions.listKeys(jsonString), new String[] {"name", "age", "subjects"});
+        assertEquals(
+            MapFunctions.listValues(jsonString),
+            new String[] {
+                "Pete",
+                "24",
+                "[{name=maths, homework_grades=[80, 85, 90, 95, 100], grade=A}, "
+                    + "{name=english, homework_grades=[60, 65, 70, 85, 90], grade=B}]"
+            });
     }
 
+    @Test
+    public void testSingle() throws JsonProcessingException {
+        // test with different type values (number, bool, array, string, null)
+        String jsonString = "{" + "  \"id\": 704488" + "}";
+
+        // check individual elements correctly recorded
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "id"), 704488);
+
+        // check mapEntries list
+        assertEquals(
+            MapFunctions.listMapEntries(jsonString),
+            new String[] {
+                "id = 704488",
+            });
+
+        // check key, value lists and nPairs functions
+        assertEquals(MapFunctions.listKeys(jsonString), new String[] {"id"});
+        assertEquals(MapFunctions.listValues(jsonString), new String[] {"704488"});
+    }
 
     @Test
-    public void testEmpty()
-            throws JsonProcessingException {
-
-        // CHECKSTYLE:OFF
-        // @formatter:off
-
-        //test with empty JSON object
+    public void testEmpty() throws JsonProcessingException {
+        // test with empty JSON object
         String jsonString = "{" + "}";
 
-        // @formatter:on
-        // CHECKSTYLE:ON
-
-        assertEquals(MapFunctions.listKeys(jsonString), new String[]{});
+        assertEquals(MapFunctions.listMapEntries(jsonString), new String[] {});
+        assertEquals(MapFunctions.listKeys(jsonString), new String[] {});
+        assertEquals(MapFunctions.listValues(jsonString), new String[] {});
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "blah"), null);
     }
 
     @Test
-    public void testNull()
-            throws JsonProcessingException {
+    public void testNull() throws JsonProcessingException {
 
-        // CHECKSTYLE:OFF
-        // @formatter:off
-
-        //test with empty JSON object
+        // test with empty JSON object
         String jsonString = null;
 
-        // @formatter:on
-        // CHECKSTYLE:ON
-
-        assertEquals(MapFunctions.listKeys(jsonString), new String[]{});
+        assertEquals(MapFunctions.listMapEntries(jsonString), new String[] {});
+        assertEquals(MapFunctions.listKeys(jsonString), new String[] {});
+        assertEquals(MapFunctions.listValues(jsonString), new String[] {});
+        assertEquals(MapFunctions.getValueFromKey(jsonString, "blah"), null);
     }
 }
