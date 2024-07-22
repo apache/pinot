8522174d3d24981106237f411d0e36bc6aa020e7
Abhishek Sharma <abhishek.sharma@spothero.com>
2024-07-22 15:35:13 -0400

    Changes as per the PR comments.

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/access/AccessControl.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/access/AccessControl.java
@@ -40,8 +40,7 @@ public interface AccessControl extends FineGrainedAccessControl {
    * @param endpointUrl the request url for which this access control is called
    * @return whether the client has permission
    */
-  boolean hasAccess(@Nullable String tableName, AccessType accessType, HttpHeaders httpHeaders,
-      String endpointUrl);
+  boolean hasAccess(@Nullable String tableName, AccessType accessType, HttpHeaders httpHeaders, String endpointUrl);
 
   /**
    * Return whether the client has permission to access the endpoints with are not table level
