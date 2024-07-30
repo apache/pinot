c2dd3c4bd0ba08eaf01e46779e594a50320f9c6d
Yash Mayya <yash.mayya@gmail.com>
2024-07-30 18:52:57 +0530

    Fix literal type handling in LiteralValueExtractor

--- pinot-core/src/main/java/org/apache/pinot/core/query/reduce/PostAggregationHandler.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/reduce/PostAggregationHandler.java
@@ -108,7 +108,7 @@ public class PostAggregationHandler implements ValueExtractorFactory {
   public ValueExtractor getValueExtractor(ExpressionContext expression) {
     if (expression.getType() == ExpressionContext.Type.LITERAL) {
       // Literal
-      return new LiteralValueExtractor(expression.getLiteral().getStringValue());
+      return new LiteralValueExtractor(expression.getLiteral());
     }
     if (_numGroupByExpressions > 0) {
       Integer groupByExpressionIndex = _groupByExpressionIndexMap.get(expression);
