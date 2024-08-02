1b3e5f8a6d7c3271bda41be38f6427c52a027e44
Caner Balci <canerbalci@gmail.com>
2024-08-01 17:49:19 -0700

    Refactor and add more test coverage

--- pinot-connectors/pinot-spark-3-connector/src/main/scala/org/apache/pinot/connector/spark/v3/datasource/PinotWrite.scala
+++ pinot-connectors/pinot-spark-3-connector/src/main/scala/org/apache/pinot/connector/spark/v3/datasource/PinotWrite.scala
@@ -1,51 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.connector.spark.v3.datasource
 
-import org.apache.pinot.spi.data.{FieldSpec, Schema}
-import org.apache.pinot.spi.data.Schema.SchemaBuilder
+import org.apache.pinot.connector.spark.common.PinotDataSourceWriteOptions
+import org.apache.pinot.spi.data.Schema
 import org.apache.spark.sql.connector.write.{BatchWrite, DataWriterFactory, LogicalWriteInfo, PhysicalWriteInfo, Write, WriterCommitMessage}
 import org.apache.spark.sql.types.StructType
 
-class PinotWrite (
-                 logicalWriteInfo: LogicalWriteInfo
-                 ) extends Write with BatchWrite {
-
-  val _logicalWriteInfo: LogicalWriteInfo = logicalWriteInfo
-  val tableName: String = logicalWriteInfo.options().get("table")
+class PinotWrite(
+                  logicalWriteInfo: LogicalWriteInfo
+                ) extends Write with BatchWrite {
+  val writeOptions: PinotDataSourceWriteOptions = PinotDataSourceWriteOptions.from(logicalWriteInfo.options())
   val writeSchema: StructType = logicalWriteInfo.schema()
-  val pinotSchema: Schema = SparkToPinotTypeTranslator.translate(writeSchema)
-
-  var segmentFormat: String = logicalWriteInfo.options().get("segmentFormat")
-  val savePath: String = logicalWriteInfo.options().get("path")
-
-  if (tableName == null) {
-    throw new IllegalArgumentException("Table name is required")
-  }
-
-  if (savePath == null) {
-    throw new IllegalArgumentException("Save path is required")
-  }
-
-  if (segmentFormat == null) {
-    segmentFormat = s"""$tableName-%d"""
-  }
+  val pinotSchema: Schema = SparkToPinotTypeTranslator.translate(writeSchema, writeOptions.tableName)
 
   override def createBatchWriterFactory(physicalWriteInfo: PhysicalWriteInfo): DataWriterFactory = {
-        val _savePath = savePath
-        val _tableName = tableName
-        val _segmentFormat = segmentFormat
-        val _writeSchema = writeSchema
-        val _pinotSchema = pinotSchema
+    // serialized options
+    val _writeOptions = writeOptions
+    val _writeSchema = writeSchema
+    val _pinotSchema = pinotSchema
+
     (partitionId: Int, taskId: Long) => {
       new PinotDataWriter(
-        physicalWriteInfo,
         partitionId,
         taskId,
-        _tableName,
-        _segmentFormat,
-        _savePath,
+        _writeOptions,
         _writeSchema,
-        _pinotSchema,
-      )
+        _pinotSchema)
     }
   }
 
@@ -58,5 +55,4 @@ class PinotWrite (
   override def abort(messages: Array[WriterCommitMessage]): Unit = {
     messages.foreach(println)
   }
-
 }
