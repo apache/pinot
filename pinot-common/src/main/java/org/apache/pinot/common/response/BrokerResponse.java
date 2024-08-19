10de7c44d757e59a3c0886f4813cfc0845985725
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2024-08-19 16:25:49 +0530

    Checkpoint
    Setup CursorRequestHandlerDelegate
    
    Checkpoint - compiles and sanity test works.
    
    Setup CursorRequestHandlerDelegate
    
    Checkpoint - compiles and sanity test works.
    
    add spi for cursors
    
    Checkpoint: testCursorWorkflow works.
    
    Checkpoint: most tests work.
    
    Fix checkstyle
    
    Register ResultStoreCleaner
    
    Redesign SPI. Make cursor integration test work
    
    Add Test for FsResultStore
    
    Add Test for Auth
    
    Remove unnecessary files.
    
    Checkpoint.
    
    Fix tests to use URLs
    
    Undo unnecessary changes.

--- pinot-common/src/main/java/org/apache/pinot/common/response/BrokerResponse.java
+++ pinot-common/src/main/java/org/apache/pinot/common/response/BrokerResponse.java
@@ -75,7 +75,7 @@ public interface BrokerResponse {
   /**
    * Returns the processing exceptions encountered during the query execution.
    */
-  List<QueryProcessingException> getExceptions();
+   List<QueryProcessingException> getExceptions();
 
   @Deprecated
   @JsonIgnore
