8eb1c1f3c2e1ad21472857f5c9893a50e2fab2a1
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2024-08-19 19:15:33 +0530

    Add resolveTableNamesTest

--- pinot-common/src/test/java/org/apache/pinot/common/utils/request/RequestUtilsTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/utils/request/RequestUtilsTest.java
@@ -18,16 +18,20 @@
  */
 package org.apache.pinot.common.utils.request;
 
+import java.util.Set;
 import org.apache.calcite.sql.SqlDialect;
 import org.apache.calcite.sql.SqlLiteral;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.pinot.common.request.Expression;
 import org.apache.pinot.common.request.ExpressionType;
+import org.apache.pinot.sql.parsers.CalciteSqlParser;
 import org.apache.pinot.sql.parsers.PinotSqlType;
 import org.apache.pinot.sql.parsers.SqlNodeAndOptions;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
 
 
@@ -55,5 +59,26 @@ public class RequestUtilsTest {
     assertEquals(result.getSqlType(), PinotSqlType.DQL);
     assertEquals(result.getSqlNode().toSqlString((SqlDialect) null).toString(),
         "SELECT `foo`\n" + "FROM `countries`\n" + "WHERE `bar` > 1");
+  }
+
+  @DataProvider(name = "queryProvider")
+  public Object[][] queryProvider() {
+    return new Object[][] {
+      {"select foo from countries where bar > 1", Set.of("countries")},
+      {"select 1", null}
+    };
+  }
+
+  @Test(dataProvider = "queryProvider")
+  public void testResolveTableNames(String query, Set<String> expectedSet) {
+    SqlNodeAndOptions sqlNodeAndOptions = CalciteSqlParser.compileToSqlNodeAndOptions(query);
+    Set<String> tableNames =
+        RequestUtils.getTableNames(CalciteSqlParser.compileSqlNodeToPinotQuery(sqlNodeAndOptions.getSqlNode()));
+
+    if (expectedSet == null) {
+      assertNull(tableNames);
+    } else {
+      assertEquals(tableNames, expectedSet);
+    }
   }
 }
