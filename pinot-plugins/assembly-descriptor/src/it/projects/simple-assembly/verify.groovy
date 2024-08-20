6dd22ca16feb2accac144be8959abe798a6ae3b8
Robert Scholte <rfscholte@apache.org>
2024-08-20 12:32:42 +0200

    Introduce pinot-plugin assembly-desciptor as a better alternative than shading

---
+++ pinot-plugins/assembly-descriptor/src/it/projects/simple-assembly/verify.groovy
@@ -0,0 +1,15 @@
+import java.util.zip.ZipFile
+
+def expected = [
+    'PINOT-INF/classes/org/apache/pinot/it/Simple.class',
+    "PINOT-INF/lib/commons-lang3-${commonslang3_version}.jar"
+] as Set
+
+def entries = new File(basedir,'target/simple-assembly-0.0.1-SNAPSHOT-plugin.zip').with {
+  f ->
+    def archive = new ZipFile(f)
+    archive.entries().findAll{ !it.directory }.collect { it.name } as Set
+    archive.close()
+}
+
+assert entries == expected
