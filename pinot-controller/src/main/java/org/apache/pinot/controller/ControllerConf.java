f5df2a785c366744ee730ae72747ef8f3feb718b
prashantpandey <pandepra.work@outlook.com>
2024-08-22 17:35:11 +0530

    Checkstyle

--- pinot-controller/src/main/java/org/apache/pinot/controller/ControllerConf.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/ControllerConf.java
@@ -183,7 +183,8 @@ public class ControllerConf extends PinotConfiguration {
     public static final String SEGMENT_RELOCATOR_BEST_EFFORTS = "controller.segment.relocator.bestEfforts";
     //For no-downtime rebalance, minimum number of replicas to keep alive during rebalance, or maximum number of
     // replicas allowed to be unavailable if value is negative. Default value is -1
-    public static final String SEGMENT_RELOCATOR_MIN_AVAIL_REPLICAS = "controller.segment.relocator.minAvailableReplicas";
+    public static final String SEGMENT_RELOCATOR_MIN_AVAIL_REPLICAS =
+        "controller.segment.relocator.minAvailableReplicas";
     public static final String SEGMENT_RELOCATOR_REASSIGN_INSTANCES = "controller.segment.relocator.reassignInstances";
     public static final String SEGMENT_RELOCATOR_BOOTSTRAP_SERVERS = "controller.segment.relocator.bootstrap";
     public static final String SEGMENT_RELOCATOR_DOWNTIME = "controller.segment.relocator.downtime";
@@ -632,8 +633,8 @@ public class ControllerConf extends PinotConfiguration {
 
   public int getRebalanceCheckerFrequencyInSeconds() {
     return Optional.ofNullable(getProperty(ControllerPeriodicTasksConf.REBALANCE_CHECKER_FREQUENCY_PERIOD))
-        .map(period -> (int) convertPeriodToSeconds(period)).orElse(
-            ControllerPeriodicTasksConf.DEFAULT_REBALANCE_CHECKER_FREQUENCY_IN_SECONDS);
+        .map(period -> (int) convertPeriodToSeconds(period))
+        .orElse(ControllerPeriodicTasksConf.DEFAULT_REBALANCE_CHECKER_FREQUENCY_IN_SECONDS);
   }
 
   public long getRebalanceCheckerInitialDelayInSeconds() {
@@ -643,8 +644,8 @@ public class ControllerConf extends PinotConfiguration {
 
   public int getRealtimeConsumerMonitorRunFrequency() {
     return Optional.ofNullable(getProperty(ControllerPeriodicTasksConf.RT_CONSUMER_MONITOR_FREQUENCY_PERIOD))
-        .map(period -> (int) convertPeriodToSeconds(period)).orElse(
-            ControllerPeriodicTasksConf.DEFAULT_RT_CONSUMER_MONITOR_FREQUENCY_IN_SECONDS);
+        .map(period -> (int) convertPeriodToSeconds(period))
+        .orElse(ControllerPeriodicTasksConf.DEFAULT_RT_CONSUMER_MONITOR_FREQUENCY_IN_SECONDS);
   }
 
   public long getRealtimeConsumerMonitorInitialDelayInSeconds() {
@@ -818,12 +819,10 @@ public class ControllerConf extends PinotConfiguration {
   @Deprecated
   public int getMinionInstancesCleanupTaskMinOfflineTimeBeforeDeletionInSeconds() {
     return Optional.ofNullable(
-        getProperty(ControllerPeriodicTasksConf.MINION_INSTANCES_CLEANUP_TASK_MIN_OFFLINE_TIME_BEFORE_DELETION_PERIOD))
+            getProperty(ControllerPeriodicTasksConf.MINION_INSTANCES_CLEANUP_TASK_MIN_OFFLINE_TIME_BEFORE_DELETION_PERIOD))
         .map(period -> (int) convertPeriodToSeconds(period)).orElseGet(() -> getProperty(
-            ControllerPeriodicTasksConf.
-                DEPRECATED_MINION_INSTANCES_CLEANUP_TASK_MIN_OFFLINE_TIME_BEFORE_DELETION_SECONDS,
-            ControllerPeriodicTasksConf.
-                DEFAULT_MINION_INSTANCES_CLEANUP_TASK_MIN_OFFLINE_TIME_BEFORE_DELETION_IN_SECONDS));
+            ControllerPeriodicTasksConf.DEPRECATED_MINION_INSTANCES_CLEANUP_TASK_MIN_OFFLINE_TIME_BEFORE_DELETION_SECONDS,
+            ControllerPeriodicTasksConf.DEFAULT_MINION_INSTANCES_CLEANUP_TASK_MIN_OFFLINE_TIME_BEFORE_DELETION_IN_SECONDS));
   }
 
   @Deprecated
@@ -1058,8 +1057,8 @@ public class ControllerConf extends PinotConfiguration {
   }
 
   public int getLeadControllerResourceRebalanceDelayMs() {
-    return getProperty(
-        LEAD_CONTROLLER_RESOURCE_REBALANCE_DELAY_MS, DEFAULT_LEAD_CONTROLLER_RESOURCE_REBALANCE_DELAY_MS);
+    return getProperty(LEAD_CONTROLLER_RESOURCE_REBALANCE_DELAY_MS,
+        DEFAULT_LEAD_CONTROLLER_RESOURCE_REBALANCE_DELAY_MS);
   }
 
   public boolean getHLCTablesAllowed() {
