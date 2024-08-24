3a75d4abb2c897b5b368ea55ffd7a4e6ea8ba0e5
Vivek Iyer Vaidyanathan <vvivekiyer@gmail.com>
2024-08-24 09:53:01 -0700

    Binary Workload Scheduler for constrained execution of a set of queries  (#13847)
    * Binary Workload Scheduler
    
    * Address review comments

--- pinot-spi/src/main/java/org/apache/pinot/spi/utils/CommonConstants.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/utils/CommonConstants.java
@@ -428,6 +428,14 @@ public class CommonConstants {
 
         // If query submission causes an exception, still continue to submit the query to other servers
         public static final String SKIP_UNAVAILABLE_SERVERS = "skipUnavailableServers";
+
+        // Indicates that a query belongs to a secondary workload when using the BinaryWorkloadScheduler. The
+        // BinaryWorkloadScheduler divides queries into two workloads, primary and secondary. Primary workloads are
+        // executed in an  Unbounded FCFS fashion. However, secondary workloads are executed in a constrainted FCFS
+        // fashion with limited compute.des queries into two workloads, primary and secondary. Primary workloads are
+        // executed in an  Unbounded FCFS fashion. However, secondary workloads are executed in a constrainted FCFS
+        // fashion with limited compute.
+        public static final String IS_SECONDARY_WORKLOAD = "isSecondaryWorkload";
       }
 
       public static class QueryOptionValue {
