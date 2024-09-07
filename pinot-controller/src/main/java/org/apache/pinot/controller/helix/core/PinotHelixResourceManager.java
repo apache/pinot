f5327a512ab90b6137c88c2fbaf0c517c8ac50b6
Xiang Fu <xiangfu.1024@gmail.com>
2024-09-07 08:48:32 -0700

    Acquire IdealStateUpdaterLock for PinotLLCRealtimeSegmentManager IdealState update (#13947)

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/PinotHelixResourceManager.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/PinotHelixResourceManager.java
@@ -1980,11 +1980,13 @@ public class PinotHelixResourceManager {
     IdealState idealState = _helixAdmin.getResourceIdealState(_helixClusterName, tableNameWithType);
     String replicationConfigured = Integer.toString(tableConfig.getReplication());
     if (!idealState.getReplicas().equals(replicationConfigured)) {
-      HelixHelper.updateIdealState(_helixZkManager, tableNameWithType, is -> {
-        assert is != null;
-        is.setReplicas(replicationConfigured);
-        return is;
-      }, RetryPolicies.exponentialBackoffRetryPolicy(5, 1000L, 1.2f));
+      synchronized (getIdealStateUpdaterLock(tableNameWithType)) {
+        HelixHelper.updateIdealState(_helixZkManager, tableNameWithType, is -> {
+          assert is != null;
+          is.setReplicas(replicationConfigured);
+          return is;
+        }, RetryPolicies.exponentialBackoffRetryPolicy(5, 1000L, 1.2f));
+      }
     }
 
     // Assign instances
@@ -3967,48 +3969,48 @@ public class PinotHelixResourceManager {
       LineageEntry lineageEntryToUpdate, LineageEntry lineageEntryToMatch, ZkHelixPropertyStore<ZNRecord> propertyStore,
       LineageUpdateType lineageUpdateType, Map<String, String> customMap) {
     String tableNameWithType = tableConfig.getTableName();
-      synchronized (getLineageUpdaterLock(tableNameWithType)) {
-        // retry attempts are made to account for the distributed update from other controllers
-        for (int i = 0; i < DEFAULT_SEGMENT_LINEAGE_UPDATE_NUM_RETRY; i++) {
-          // Fetch the segment lineage
-          ZNRecord segmentLineageToUpdateZNRecord =
-              SegmentLineageAccessHelper.getSegmentLineageZNRecord(propertyStore, tableConfig.getTableName());
-          int expectedVersion = segmentLineageToUpdateZNRecord.getVersion();
-          SegmentLineage segmentLineageToUpdate = SegmentLineage.fromZNRecord(segmentLineageToUpdateZNRecord);
-          LineageEntry currentLineageEntry = segmentLineageToUpdate.getLineageEntry(lineageEntryId);
+    synchronized (getLineageUpdaterLock(tableNameWithType)) {
+      // retry attempts are made to account for the distributed update from other controllers
+      for (int i = 0; i < DEFAULT_SEGMENT_LINEAGE_UPDATE_NUM_RETRY; i++) {
+        // Fetch the segment lineage
+        ZNRecord segmentLineageToUpdateZNRecord =
+            SegmentLineageAccessHelper.getSegmentLineageZNRecord(propertyStore, tableConfig.getTableName());
+        int expectedVersion = segmentLineageToUpdateZNRecord.getVersion();
+        SegmentLineage segmentLineageToUpdate = SegmentLineage.fromZNRecord(segmentLineageToUpdateZNRecord);
+        LineageEntry currentLineageEntry = segmentLineageToUpdate.getLineageEntry(lineageEntryId);
 
-          // If the lineage entry doesn't match with the previously fetched lineage, we need to fail the request.
-          if (!currentLineageEntry.equals(lineageEntryToMatch)) {
-            String errorMsg = String.format(
-                "Aborting the to update lineage entry since we find that the entry has been modified for table %s, "
-                    + "entry id: %s", tableConfig.getTableName(), lineageEntryId);
-            LOGGER.error(errorMsg);
-            throw new RuntimeException(errorMsg);
-          }
+        // If the lineage entry doesn't match with the previously fetched lineage, we need to fail the request.
+        if (!currentLineageEntry.equals(lineageEntryToMatch)) {
+          String errorMsg = String.format(
+              "Aborting the to update lineage entry since we find that the entry has been modified for table %s, "
+                  + "entry id: %s", tableConfig.getTableName(), lineageEntryId);
+          LOGGER.error(errorMsg);
+          throw new RuntimeException(errorMsg);
+        }
 
-          // Update lineage entry
-          segmentLineageToUpdate.updateLineageEntry(lineageEntryId, lineageEntryToUpdate);
-          switch (lineageUpdateType) {
-            case END:
-              _lineageManager.updateLineageForEndReplaceSegments(tableConfig, lineageEntryId, customMap,
-                  segmentLineageToUpdate);
-              break;
-            case REVERT:
-              _lineageManager.updateLineageForRevertReplaceSegments(tableConfig, lineageEntryId, customMap,
-                  segmentLineageToUpdate);
-              break;
-            default:
-              String errorMsg = String.format("Aborting the lineage entry update with type: %s, as the allowed update"
-                  + "types in this method are END and REVERT", lineageUpdateType);
-              throw new IllegalStateException(errorMsg);
-          }
+        // Update lineage entry
+        segmentLineageToUpdate.updateLineageEntry(lineageEntryId, lineageEntryToUpdate);
+        switch (lineageUpdateType) {
+          case END:
+            _lineageManager.updateLineageForEndReplaceSegments(tableConfig, lineageEntryId, customMap,
+                segmentLineageToUpdate);
+            break;
+          case REVERT:
+            _lineageManager.updateLineageForRevertReplaceSegments(tableConfig, lineageEntryId, customMap,
+                segmentLineageToUpdate);
+            break;
+          default:
+            String errorMsg = String.format("Aborting the lineage entry update with type: %s, as the allowed update"
+                + "types in this method are END and REVERT", lineageUpdateType);
+            throw new IllegalStateException(errorMsg);
+        }
 
-          // Write back to the lineage entry
-          if (SegmentLineageAccessHelper.writeSegmentLineage(propertyStore, segmentLineageToUpdate, expectedVersion)) {
-            return true;
-          }
+        // Write back to the lineage entry
+        if (SegmentLineageAccessHelper.writeSegmentLineage(propertyStore, segmentLineageToUpdate, expectedVersion)) {
+          return true;
         }
       }
+    }
     return false;
   }
 
