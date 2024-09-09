acf1255b0db4de942a595828ff791a1421d1c587
Chaitanya Deepthi <chdeepthidolie@gmail.com>
2024-09-09 16:19:34 -0700

    checkstyle fixes

--- pinot-controller/src/test/java/org/apache/pinot/controller/util/JsonSerializerTest.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/util/JsonSerializerTest.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.controller.util;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -104,9 +122,8 @@ public class JsonSerializerTest {
     assertEquals(controllerEntry.get("leadControllerId").asText(), "Controller_192.168.1.148_9000");
     assertEquals(controllerEntry.get("tableNames").size(), 2);
     PinotLeadControllerRestletResource.LeadControllerEntry tableReloadResponse =
-        JsonUtils.stringToObject(leadControllerEntryRespStr,
-            new TypeReference<PinotLeadControllerRestletResource.LeadControllerEntry>() {
-            });
+        JsonUtils.stringToObject(leadControllerEntryRespStr, new TypeReference<>() {
+        });
     assertEquals(tableReloadResponse.getLeadControllerId(), "Controller_192.168.1.148_9000");
     assertEquals(tableReloadResponse.getTableNames().size(), 2);
     assertTrue(tableReloadResponse.getTableNames().contains("fineFoodReviews_OFFLINE"));
@@ -157,9 +174,8 @@ public class JsonSerializerTest {
         + "      \"tableNames\": []\n" + "    }\n" + "  }\n" + "}";
     JsonNode jsonNode = JsonUtils.stringToJsonNode(jsonResponse);
     PinotLeadControllerRestletResource.LeadControllerResponse tableReloadResponse =
-        JsonUtils.stringToObject(jsonResponse,
-            new TypeReference<PinotLeadControllerRestletResource.LeadControllerResponse>() {
-            });
+        JsonUtils.stringToObject(jsonResponse, new TypeReference<>() {
+        });
     assertNotNull(jsonNode);
     assertTrue(tableReloadResponse.getLeadControllerResourceEnabled());
     assertNotNull(tableReloadResponse.getLeadControllerEntryMap());
