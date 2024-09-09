878372cea3e1659f717213a61e4e325bc17789c5
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2024-09-09 16:31:37 +0200

    multi-stage-explain: Add javadoc to DatabaseUtils.splitTableName

--- pinot-common/src/main/java/org/apache/pinot/common/utils/DatabaseUtils.java
+++ pinot-common/src/main/java/org/apache/pinot/common/utils/DatabaseUtils.java
@@ -32,6 +32,9 @@ public class DatabaseUtils {
   private DatabaseUtils() {
   }
 
+  /**
+   * Splits a fully qualified table name i.e. {databaseName}.{tableName} into different components.
+   */
   public static String[] splitTableName(String tableName) {
     return StringUtils.split(tableName, '.');
   }
@@ -49,7 +52,7 @@ public class DatabaseUtils {
    */
   public static String translateTableName(String tableName, @Nullable String databaseName, boolean ignoreCase) {
     Preconditions.checkArgument(StringUtils.isNotEmpty(tableName), "'tableName' cannot be null or empty");
-    String[] tableSplit = StringUtils.split(tableName, '.');
+    String[] tableSplit = splitTableName(tableName);
     switch (tableSplit.length) {
       case 1:
         // do not concat the database name prefix if it's a 'default' database
