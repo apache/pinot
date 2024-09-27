c01c1f74d536037435c9aaa8108e7084b1fe4a2e
Ankit Sultana <ankitsultana@uber.com>
2024-09-27 20:13:08 +0530

    complete test + rename engine to language

--- pinot-timeseries/pinot-timeseries-spi/src/test/java/org/apache/pinot/tsdb/spi/plan/LeafTimeSeriesPlanNodeTest.java
+++ pinot-timeseries/pinot-timeseries-spi/src/test/java/org/apache/pinot/tsdb/spi/plan/LeafTimeSeriesPlanNodeTest.java
@@ -1,6 +1,9 @@
 package org.apache.pinot.tsdb.spi.plan;
 
 import java.time.Duration;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+import org.apache.pinot.tsdb.spi.AggInfo;
 import org.apache.pinot.tsdb.spi.TimeBuckets;
 import org.testng.annotations.Test;
 
@@ -8,11 +11,51 @@ import static org.testng.Assert.*;
 
 
 public class LeafTimeSeriesPlanNodeTest {
+  private static final String ID = "plan_id123";
+  private static final String TABLE = "myTable";
+  private static final String TIME_COLUMN = "orderTime";
+  private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+
   @Test
   public void testGetEffectiveFilter() {
-    LeafTimeSeriesPlanNode planNode = new LeafTimeSeriesPlanNode();
     TimeBuckets timeBuckets = TimeBuckets.ofSeconds(
         1000, Duration.ofSeconds(13), 9);
-    planNode.getEffectiveFilter(timeBuckets);
+    final long expectedStartTimeInFilter = 1000;
+    final long expectedEndTimeInFilter = 1000 + 13 * 9;
+    final String nonEmptyFilter = "cityName = 'Chicago'";
+    // Case-1: No offset, and empty filter.
+    {
+      LeafTimeSeriesPlanNode planNode = new LeafTimeSeriesPlanNode(ID, Collections.emptyList(), TABLE, TIME_COLUMN,
+          TIME_UNIT, 0L, "", "value_col", new AggInfo("SUM"),
+          Collections.singletonList("cityName"));
+      assertEquals(planNode.getEffectiveFilter(timeBuckets),
+          "orderTime >= " + expectedStartTimeInFilter + " AND orderTime <= " + expectedEndTimeInFilter);
+    }
+    // Case-2: Offset, but empty filter
+    {
+      LeafTimeSeriesPlanNode planNode = new LeafTimeSeriesPlanNode(ID, Collections.emptyList(), TABLE, TIME_COLUMN,
+          TIME_UNIT, 123L, "", "value_col", new AggInfo("SUM"),
+          Collections.singletonList("cityName"));
+      assertEquals(planNode.getEffectiveFilter(timeBuckets),
+          "orderTime >= " + (expectedStartTimeInFilter - 123) + " AND orderTime <= " + (expectedEndTimeInFilter - 123));
+    }
+    // Case-3: Offset and non-empty filter
+    {
+      LeafTimeSeriesPlanNode planNode = new LeafTimeSeriesPlanNode(ID, Collections.emptyList(), TABLE, TIME_COLUMN,
+          TIME_UNIT, 123L, nonEmptyFilter, "value_col", new AggInfo("SUM"),
+          Collections.singletonList("cityName"));
+      assertEquals(planNode.getEffectiveFilter(timeBuckets),
+          String.format("(%s) AND (orderTime >= %s AND orderTime <= %s)", nonEmptyFilter,
+              (expectedStartTimeInFilter - 123), (expectedEndTimeInFilter - 123)));
+    }
+    // Case-4: Offset, and non-empty filter, and time-unit that is not seconds
+    {
+      LeafTimeSeriesPlanNode planNode = new LeafTimeSeriesPlanNode(ID, Collections.emptyList(), TABLE, TIME_COLUMN,
+          TimeUnit.MILLISECONDS, 123L, nonEmptyFilter, "value_col", new AggInfo("SUM"),
+          Collections.singletonList("cityName"));
+      assertEquals(planNode.getEffectiveFilter(timeBuckets),
+          String.format("(%s) AND (orderTime >= %s AND orderTime <= %s)", nonEmptyFilter,
+              (expectedStartTimeInFilter * 1000 - 123 * 1000), (expectedEndTimeInFilter * 1000 - 123 * 1000)));
+    }
   }
 }
