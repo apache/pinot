a49fcdaf736ec8e9cd80dc594aa40f3a4316753e
Yash Mayya <yash.mayya@gmail.com>
2024-09-30 15:34:38 +0530

    Fix license headers in pinot-kafka-3.0

--- pinot-plugins/pinot-stream-ingestion/pinot-kafka-3.0/src/test/java/org/apache/pinot/plugin/stream/kafka30/KafkaPartitionLevelConsumerTest.java
+++ pinot-plugins/pinot-stream-ingestion/pinot-kafka-3.0/src/test/java/org/apache/pinot/plugin/stream/kafka30/KafkaPartitionLevelConsumerTest.java
@@ -1,4 +1,4 @@
-package org.apache.pinot.plugin.stream.kafka30; /**
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -16,6 +16,7 @@ package org.apache.pinot.plugin.stream.kafka30; /**
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.apache.pinot.plugin.stream.kafka30;
 
 import java.time.Instant;
 import java.util.HashMap;
