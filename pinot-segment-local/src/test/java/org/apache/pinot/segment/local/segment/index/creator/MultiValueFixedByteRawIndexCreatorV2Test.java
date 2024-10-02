bca5edaa76635880154e81b0fd2d041d6ac75aff
Jack Luo <jack.luo@mail.utoronto.ca>
2024-10-02 18:15:33 +0800

    Improved unit test for MultiValueFixedByteRawIndexCreatorTest and MultiValueFixedByteRawIndexCreatorV2Test

--- pinot-segment-local/src/test/java/org/apache/pinot/segment/local/segment/index/creator/MultiValueFixedByteRawIndexCreatorV2Test.java
+++ pinot-segment-local/src/test/java/org/apache/pinot/segment/local/segment/index/creator/MultiValueFixedByteRawIndexCreatorV2Test.java
@@ -20,6 +20,8 @@ package org.apache.pinot.segment.local.segment.index.creator;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.apache.pinot.segment.local.io.writer.impl.VarByteChunkForwardIndexWriterV4;
 import org.apache.pinot.segment.local.segment.creator.impl.fwd.MultiValueFixedByteRawIndexCreator;
@@ -30,11 +32,15 @@ import org.apache.pinot.segment.spi.compression.ChunkCompressionType;
 import org.apache.pinot.segment.spi.index.reader.ForwardIndexReader;
 import org.apache.pinot.segment.spi.memory.PinotDataBuffer;
 import org.apache.pinot.spi.data.FieldSpec;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 
 /**
- Same as MultiValueFixedByteRawIndexCreatorTest, but newer version of forward index creator and reader are used
+ Similar to MultiValueFixedByteRawIndexCreatorTest, but utilizes the newer version of the forward index creator and
+ reader. Additionally, this test class includes a validation test for checking the compression ratio improvement with
+ the  forward index creator version upgrade.
  */
 public class MultiValueFixedByteRawIndexCreatorV2Test extends MultiValueFixedByteRawIndexCreatorTest {
   @BeforeClass
@@ -58,5 +64,60 @@ public class MultiValueFixedByteRawIndexCreatorV2Test extends MultiValueFixedByt
       int writerVersion) {
     return writerVersion == VarByteChunkForwardIndexWriterV4.VERSION ? new VarByteChunkForwardIndexReaderV5(buffer,
         dataType.getStoredType(), false) : new FixedByteChunkMVForwardIndexReaderV2(buffer, dataType.getStoredType());
+  }
+
+  @Test
+  public void validateCompressionRatioIncrease()
+      throws IOException {
+    // Generate input data containing short MV docs with somewhat repetitive data
+    int numDocs = 1000000;
+    int numElements = 0;
+    int maxMVRowSize = 0;
+    List<long[]> inputData = new ArrayList<>(numDocs);
+    for (int i = 0; i < numDocs; i++) {
+      long[] mvRow = new long[Math.abs((int) Math.floor(RANDOM.nextGaussian()))];
+      maxMVRowSize = Math.max(maxMVRowSize, mvRow.length);
+      numElements += mvRow.length;
+      for (int j = 0; j < mvRow.length; j++, numElements++) {
+        mvRow[j] = numElements % 10;
+      }
+      inputData.add(mvRow);
+    }
+
+    for (int writerVersion : List.of(2, 4)) {
+      // Generate MV fixed byte raw fwd index with explicit length
+      File explicitLengthFwdIndexFile = new File(_outputDir, MultiValueFixedByteRawIndexCreator.class.getSimpleName());
+      FileUtils.deleteQuietly(explicitLengthFwdIndexFile);
+      try (MultiValueFixedByteRawIndexCreator creator = new MultiValueFixedByteRawIndexCreator(
+          explicitLengthFwdIndexFile, ChunkCompressionType.ZSTANDARD, numDocs, FieldSpec.DataType.LONG, numElements,
+          true, writerVersion)) {
+        for (long[] mvRow : inputData) {
+          creator.putLongMV(mvRow);
+        }
+      }
+
+      // Generate MV fixed byte raw fwd index with implicit length
+      File implicitLengthFwdIndexFile =
+          new File(_outputDir, MultiValueFixedByteRawIndexCreatorV2.class.getSimpleName());
+      FileUtils.deleteQuietly(implicitLengthFwdIndexFile);
+      try (MultiValueFixedByteRawIndexCreatorV2 creator = new MultiValueFixedByteRawIndexCreatorV2(
+          implicitLengthFwdIndexFile, ChunkCompressionType.ZSTANDARD, numDocs, FieldSpec.DataType.LONG, numElements,
+          true, writerVersion)) {
+        for (long[] mvRow : inputData) {
+          creator.putLongMV(mvRow);
+        }
+      }
+
+      // For the input data, the explicit length compressed MV fixed byte raw forward index is expected to be:
+      // 1. At least 15% larger than the implicit length variant when using Writer Version 2
+      // 2. At least 200% larger than the implicit length variant when using Writer Version 4
+      long expectedImplicitLengthFwdIndexMaxSize;
+      if (writerVersion == 2) {
+        expectedImplicitLengthFwdIndexMaxSize = Math.round(implicitLengthFwdIndexFile.length() * 1.15d);
+      } else {
+        expectedImplicitLengthFwdIndexMaxSize = Math.round(implicitLengthFwdIndexFile.length() * 2.0d);
+      }
+      Assert.assertTrue(expectedImplicitLengthFwdIndexMaxSize < explicitLengthFwdIndexFile.length());
+    }
   }
 }
