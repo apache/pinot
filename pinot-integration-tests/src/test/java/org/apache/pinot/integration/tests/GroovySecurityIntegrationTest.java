8706e16f10d90c49abc701478de26db80972dc9e
Erich Ess <erich@startree.ai>
2024-10-08 14:36:04 -0400

    Adding stub for configuration manager integration test

---
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/GroovySecurityIntegrationTest.java
@@ -0,0 +1,83 @@
+package org.apache.pinot.integration.tests;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.helix.HelixAdmin;
+import org.apache.helix.HelixManager;
+import org.apache.helix.InstanceType;
+import org.apache.helix.manager.zk.ZKHelixAdmin;
+import org.apache.helix.manager.zk.ZKHelixManager;
+import org.apache.helix.store.zk.ZkHelixPropertyStore;
+import org.apache.helix.zookeeper.api.client.RealmAwareZkClient;
+import org.apache.helix.zookeeper.datamodel.ZNRecord;
+import org.apache.helix.zookeeper.datamodel.serializer.ZNRecordSerializer;
+import org.apache.helix.zookeeper.impl.client.ZkClient;
+import org.apache.pinot.broker.queryquota.HelixExternalViewBasedQueryQuotaManager;
+import org.apache.pinot.broker.queryquota.HelixExternalViewBasedQueryQuotaManagerTest;
+import org.apache.pinot.common.metrics.BrokerMetrics;
+import org.apache.pinot.common.utils.ZkStarter;
+import org.apache.pinot.segment.local.function.GroovySecurityConfigManager;
+import org.apache.pinot.segment.local.function.GroovyStaticAnalyzerConfig;
+import org.apache.pinot.spi.utils.StringUtil;
+import org.mockito.Mockito;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+
+public class GroovySecurityIntegrationTest {
+  private ZkHelixPropertyStore<ZNRecord> _testPropertyStore;
+  private HelixManager _helixManager;
+  private HelixExternalViewBasedQueryQuotaManager _queryQuotaManager;
+  private ZkStarter.ZookeeperInstance _zookeeperInstance;
+  private static final String BROKER_INSTANCE_ID = "broker_instance_1";
+
+  @BeforeTest
+  public void beforeTest() {
+    _zookeeperInstance = ZkStarter.startLocalZkServer();
+    String helixClusterName = "TestGroovyScriptSecurityConfigManager";
+
+    _helixManager = initHelixManager(helixClusterName);
+    _testPropertyStore = _helixManager.getHelixPropertyStore();
+
+    _queryQuotaManager =
+        new HelixExternalViewBasedQueryQuotaManager(Mockito.mock(BrokerMetrics.class), BROKER_INSTANCE_ID);
+    _queryQuotaManager.init(_helixManager);
+  }
+
+  @Test
+  public void testWritingConfigToPropertyStore() throws Exception {
+    GroovyStaticAnalyzerConfig config = new GroovyStaticAnalyzerConfig(false,
+        null,
+        GroovyStaticAnalyzerConfig.getDefaultAllowedImports(),
+        null,
+        null);
+    GroovySecurityConfigManager manager = new GroovySecurityConfigManager(_helixManager);
+    manager.setConfig(config);
+    GroovyStaticAnalyzerConfig readConfig = manager.getConfig();
+  }
+
+  private HelixManager initHelixManager(String helixClusterName) {
+    return new GroovySecurityIntegrationTest.FakeHelixManager(helixClusterName, BROKER_INSTANCE_ID, InstanceType.PARTICIPANT,
+        _zookeeperInstance.getZkUrl());
+  }
+
+  public class FakeHelixManager extends ZKHelixManager {
+    FakeHelixManager(String clusterName, String instanceName, InstanceType instanceType, String zkAddress) {
+      super(clusterName, instanceName, instanceType, zkAddress);
+      super._zkclient = new ZkClient(StringUtil.join("/", StringUtils.chomp(_zookeeperInstance.getZkUrl(), "/")),
+          ZkClient.DEFAULT_SESSION_TIMEOUT, ZkClient.DEFAULT_CONNECTION_TIMEOUT, new ZNRecordSerializer());
+      _zkclient.deleteRecursively("/" + clusterName + "/PROPERTYSTORE");
+      _zkclient.createPersistent("/" + clusterName + "/PROPERTYSTORE", true);
+    }
+
+    void closeZkClient() {
+      _zkclient.close();
+    }
+
+    @Override
+    public HelixAdmin getClusterManagmentTool() {
+      return new HelixExternalViewBasedQueryQuotaManagerTest.FakeZKHelixAdmin(_zkclient);
+    }
+  }
+}
--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/function/GroovySecurityConfigManager.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/function/GroovySecurityConfigManager.java
@@ -4,13 +4,14 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import java.util.concurrent.TimeUnit;
+import org.apache.helix.AccessOption;
 import org.apache.helix.HelixManager;
 import org.apache.helix.zookeeper.datamodel.ZNRecord;
 import org.apache.zookeeper.data.Stat;
 
 
 public class GroovySecurityConfigManager {
-  private static LoadingCache<Integer, Long> _configCache;
+  private static LoadingCache<Integer, GroovyStaticAnalyzerConfig> _configCache;
   private static HelixManager _helixManager;
 
   public GroovySecurityConfigManager(HelixManager helixManager) {
@@ -18,14 +19,29 @@ public class GroovySecurityConfigManager {
     _configCache = CacheBuilder.newBuilder()
         .maximumSize(1)
         .expireAfterWrite(5, TimeUnit.MINUTES)
-        .build(new CacheLoader<Integer, Long>() {
+        .build(new CacheLoader<>() {
           @Override
-          public Long load(Integer key)
+          public GroovyStaticAnalyzerConfig load(Integer key)
               throws Exception {
             Stat stat = new Stat();
-            ZNRecord record = _helixManager.getHelixPropertyStore().get("/", stat, 0);
-            return record.getModifiedTime();
+            ZNRecord record = _helixManager.getHelixPropertyStore().get(
+                "/CONFIGS/GROOVY_EXECUTION/StaticAnalyzer",
+                stat, AccessOption.PERSISTENT);
+            return GroovyStaticAnalyzerConfig.fromZNRecord(record);
           }
         });
+  }
+
+  public void setConfig(GroovyStaticAnalyzerConfig config) throws Exception {
+    ZNRecord zr = config.toZNRecord();
+    _helixManager.getHelixPropertyStore().set(
+        "/CONFIGS/GROOVY_EXECUTION/StaticAnalyzer",
+        zr,
+        AccessOption.PERSISTENT
+    );
+  }
+
+  public GroovyStaticAnalyzerConfig getConfig() throws Exception {
+    return _configCache.get(0);
   }
 }
