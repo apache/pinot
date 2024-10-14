4436fe6ee1a83d3205365ba32527bbadad65d9ea
prashantpandey <pandepra.work@outlook.com>
2024-10-14 16:55:12 +0530

    WIP

--- pinot-common/src/test/java/org/apache/pinot/common/metrics/ControllerJMXToPromMetricsTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/metrics/ControllerJMXToPromMetricsTest.java
@@ -22,8 +22,7 @@ import static org.apache.pinot.spi.utils.CommonConstants.CONFIG_OF_METRICS_FACTO
 public class ControllerJMXToPromMetricsTest extends PinotJMXToPromMetricsTest {
   private static final String EXPORTED_METRIC_PREFIX = "pinot_controller_";
   private static final String TASK_TYPE_KEY = "taskType";
-  private static final String TASK_TYPE_VAL = "ClusterHealthCheck";
-
+  private static final String TASK_TYPE = "ClusterHealthCheck";
   private ControllerMetrics _controllerMetrics;
 
   @BeforeClass
@@ -47,27 +46,25 @@ public class ControllerJMXToPromMetricsTest extends PinotJMXToPromMetricsTest {
 
   @Test
   public void controllerTimerTest() {
-
-    Arrays.stream(ControllerTimer.values()).filter(ControllerTimer::isGlobal)
+    Stream.of(ControllerTimer.values()).filter(ControllerTimer::isGlobal)
         .peek(controllerTimer -> _controllerMetrics.addTimedValue(controllerTimer, 30_000, TimeUnit.MILLISECONDS))
         .forEach(
             controllerTimer -> assertTimerExportedCorrectly(controllerTimer.getTimerName(), EXPORTED_METRIC_PREFIX));
 
-    Arrays.stream(ControllerTimer.values()).filter(controllerTimer -> !controllerTimer.isGlobal())
-        .peek(controllerTimer -> {
-          _controllerMetrics.addTimedTableValue(TABLE_NAME_WITH_TYPE, controllerTimer, 30_000L, TimeUnit.MILLISECONDS);
-          _controllerMetrics.addTimedTableValue(RAW_TABLE_NAME, controllerTimer, 30_000L, TimeUnit.MILLISECONDS);
-        }).forEach(controllerTimer -> {
-          assertTimerExportedCorrectly(controllerTimer.getTimerName(), EXPORTED_LABELS_FOR_TABLE_NAME_TABLE_TYPE,
-              EXPORTED_METRIC_PREFIX);
-          assertTimerExportedCorrectly(controllerTimer.getTimerName(), EXPORTED_LABELS_FOR_RAW_TABLE_NAME,
-              EXPORTED_METRIC_PREFIX);
-        });
+    Stream.of(ControllerTimer.values()).filter(controllerTimer -> !controllerTimer.isGlobal()).peek(controllerTimer -> {
+      _controllerMetrics.addTimedTableValue(TABLE_NAME_WITH_TYPE, controllerTimer, 30_000L, TimeUnit.MILLISECONDS);
+      _controllerMetrics.addTimedTableValue(RAW_TABLE_NAME, controllerTimer, 30_000L, TimeUnit.MILLISECONDS);
+    }).forEach(controllerTimer -> {
+      assertTimerExportedCorrectly(controllerTimer.getTimerName(), EXPORTED_LABELS_FOR_TABLE_NAME_TABLE_TYPE,
+          EXPORTED_METRIC_PREFIX);
+      assertTimerExportedCorrectly(controllerTimer.getTimerName(), EXPORTED_LABELS_FOR_RAW_TABLE_NAME,
+          EXPORTED_METRIC_PREFIX);
+    });
   }
 
   @Test
   public void controllerMeterTest() {
-
+    //global meters
     Arrays.stream(ControllerMeter.values()).filter(ControllerMeter::isGlobal)
         .peek(controllerMeter -> _controllerMetrics.addMeteredGlobalValue(controllerMeter, 5L))
         .forEach(controllerMeter -> {
@@ -78,12 +75,11 @@ public class ControllerJMXToPromMetricsTest extends PinotJMXToPromMetricsTest {
 
     Arrays.stream(ControllerMeter.values()).filter(controllerMeter -> !controllerMeter.isGlobal())
         .peek(controllerMeter -> {
-          if (controllerMeter == ControllerMeter.CONTROLLER_PERIODIC_TASK_ERROR) {
-            _controllerMetrics.addMeteredTableValue(TASK_TYPE_VAL, controllerMeter, 1L);
+          if (controllerMeter == ControllerMeter.CONTROLLER_PERIODIC_TASK_ERROR
+              || controllerMeter == ControllerMeter.CONTROLLER_PERIODIC_TASK_RUN) {
+            _controllerMetrics.addMeteredTableValue(TASK_TYPE, controllerMeter, 1L);
           } else if (controllerMeter == ControllerMeter.PERIODIC_TASK_ERROR) {
-            _controllerMetrics.addMeteredTableValue(TABLE_NAME_WITH_TYPE + "." + TASK_TYPE_VAL, controllerMeter, 1L);
-          } else if (controllerMeter == ControllerMeter.CONTROLLER_PERIODIC_TASK_RUN) {
-            _controllerMetrics.addMeteredTableValue(TASK_TYPE_VAL, controllerMeter, 1L);
+            _controllerMetrics.addMeteredTableValue(TABLE_NAME_WITH_TYPE + "." + TASK_TYPE, controllerMeter, 1L);
           } else {
             _controllerMetrics.addMeteredTableValue(TABLE_NAME_WITH_TYPE, controllerMeter, 5L);
             _controllerMetrics.addMeteredTableValue(RAW_TABLE_NAME, controllerMeter, 5L);
@@ -95,7 +91,7 @@ public class ControllerJMXToPromMetricsTest extends PinotJMXToPromMetricsTest {
             assertMeterExportedCorrectly(meterName, List.of("table", "ClusterHealthCheck"), EXPORTED_METRIC_PREFIX);
           } else if (controllerMeter == ControllerMeter.PERIODIC_TASK_ERROR) {
             assertMeterExportedCorrectly(meterName,
-                List.of("periodicTask", "ClusterHealthTask", "table", "myTable", "tableType", "REALTIME"),
+                List.of("periodicTask", "ClusterHealthCheck", "table", "myTable", "tableType", "REALTIME"),
                 EXPORTED_METRIC_PREFIX);
           } else if (controllerMeter == ControllerMeter.CONTROLLER_PERIODIC_TASK_RUN) {
             assertMeterExportedCorrectly(strippedMeterName + "_" + "ClusterHealthCheck", EXPORTED_METRIC_PREFIX);
@@ -124,13 +120,12 @@ public class ControllerJMXToPromMetricsTest extends PinotJMXToPromMetricsTest {
           if (controllerGauge == ControllerGauge.VERSION) {
             _controllerMetrics.setValueOfGlobalGauge(ControllerGauge.VERSION, PinotVersion.VERSION_METRIC_NAME, 1L);
           } else {
-            _controllerMetrics.setValueOfGlobalGauge(controllerGauge, TASK_TYPE_VAL, 1L);
+            _controllerMetrics.setValueOfGlobalGauge(controllerGauge, TASK_TYPE, 1L);
           }
         }).filter(controllerGauge -> controllerGauge != ControllerGauge.VERSION).forEach(controllerGauge -> {
           try {
             String strippedMetricName = getStrippedMetricName(controllerGauge);
-            assertGaugeExportedCorrectly(strippedMetricName, List.of(TASK_TYPE_KEY, TASK_TYPE_VAL),
-                EXPORTED_METRIC_PREFIX);
+            assertGaugeExportedCorrectly(strippedMetricName, List.of(TASK_TYPE_KEY, TASK_TYPE), EXPORTED_METRIC_PREFIX);
           } catch (Exception e) {
             throw new RuntimeException(e);
           }
