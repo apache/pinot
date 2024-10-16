5f065cb5431a8210b66b524ef494cae11bf7dd9c
bziobrowski <26925920+bziobrowski@users.noreply.github.com>
2024-10-16 19:01:37 +0200

    Reverted npm build changes and added up-to-date check of frontend files against /dist/webapp/js/main.js. If source files are older than mina.js npm build is skipped.

--- pinot-controller/src/main/resources/webpack.config.js
+++ pinot-controller/src/main/resources/webpack.config.js
@@ -20,7 +20,6 @@ const path = require("path");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-var HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 
 module.exports = (env, argv) => {
@@ -124,7 +123,6 @@ module.exports = (env, argv) => {
           description: 'Pinot Controller UI',
         },
       }),
-      new HardSourceWebpackPlugin(),
     ],
   };
 };
