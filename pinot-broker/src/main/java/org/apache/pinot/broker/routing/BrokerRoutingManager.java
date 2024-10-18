d4a9bfc2f0145b1f197c5e0f980281e971cd4ed0
Ashish Jayamohan <ashishjayamohan@Ashishs-MBP.attlocal.net>
2024-10-17 17:03:59 -0700

    remove nullity check

--- pinot-broker/src/main/java/org/apache/pinot/broker/routing/BrokerRoutingManager.java
+++ pinot-broker/src/main/java/org/apache/pinot/broker/routing/BrokerRoutingManager.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nullable;
@@ -764,7 +763,7 @@ public class BrokerRoutingManager implements RoutingManager, ClusterChangeHandle
       _partitionMetadataManager = partitionMetadataManager;
       _queryTimeoutMs = queryTimeoutMs;
       _segmentZkMetadataFetcher = segmentZkMetadataFetcher;
-      _disabled = !Objects.requireNonNull(getIdealState(idealStatePath)).isEnabled();
+      _disabled = getIdealState(idealStatePath).isEnabled();
     }
 
     String getTableNameWithType() {
