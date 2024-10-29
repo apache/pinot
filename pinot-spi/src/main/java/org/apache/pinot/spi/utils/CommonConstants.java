325121bcb18ef01162548ccd82ffaf39bf33a70c
praveenc7 <praveenkchaganlal@gmail.com>
2024-10-28 17:58:59 -0700

    review comments

--- pinot-spi/src/main/java/org/apache/pinot/spi/utils/CommonConstants.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/utils/CommonConstants.java
@@ -482,8 +482,8 @@ public class CommonConstants {
         public static final String FILTERED_AGGREGATIONS_SKIP_EMPTY_GROUPS = "filteredAggregationsSkipEmptyGroups";
 
         // When set to true, the max initial result holder capacity will be optimized based on the query. Rather than
-        // using the default value. This is best-effort for now and returns the default value if the optimization is
-        // not possible.
+        // using the default value. This is best-effort for now and returns the default value if the optimization is not
+        // possible.
         public static final String OPTIMIZE_MAX_INITIAL_RESULT_HOLDER_CAPACITY =
             "optimizeMaxInitialResultHolderCapacity";
       }
