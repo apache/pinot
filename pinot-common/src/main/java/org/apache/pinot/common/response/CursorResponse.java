ce1b409a13a19746a199efb124f179027257fbba
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2024-11-05 14:04:33 +0530

    Undo CursorResponse.getTableNames

--- pinot-common/src/main/java/org/apache/pinot/common/response/CursorResponse.java
+++ pinot-common/src/main/java/org/apache/pinot/common/response/CursorResponse.java
@@ -18,9 +18,6 @@
  */
 package org.apache.pinot.common.response;
 
-import java.util.Set;
-
-
 public interface CursorResponse extends BrokerResponse {
 
   void setBrokerHost(String brokerHost);
@@ -131,16 +128,4 @@ public interface CursorResponse extends BrokerResponse {
    * @return number of bytes written
    */
   long getBytesWritten();
-
-  /**
-   * Set the table names queried by the SQL query
-   * @param tableNames List of table names
-   */
-  void setTableNames(Set<String> tableNames);
-
-  /**
-   * Get the list of table names queried by the SQL query
-   * @return Set of table names queried.
-   */
-  Set<String> getTableNames();
 }
