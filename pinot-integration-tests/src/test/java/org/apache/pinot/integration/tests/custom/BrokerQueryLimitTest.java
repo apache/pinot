6a1403adfbb81da98a63bd16288ca913cfa8182b
Bolek Ziobrowski <26925920+bziobrowski@users.noreply.github.com>
2024-11-14 14:56:24 +0100

    Fixed style issues.

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/custom/BrokerQueryLimitTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/custom/BrokerQueryLimitTest.java
@@ -33,82 +33,86 @@ import java.io.File;
 
 import static org.testng.Assert.*;
 
+
 public class BrokerQueryLimitTest extends CustomDataQueryClusterIntegrationTest {
 
-    private static final String LONG_COLUMN = "longCol";
-    public static final int DEFAULT_LIMIT = 5;
+  private static final String LONG_COLUMN = "longCol";
+  public static final int DEFAULT_LIMIT = 5;
 
-    @Test
-    public void testWhenLimitIsNOTSetExplicitlyThenDefaultLimitIsApplied() throws Exception {
-        setUseMultiStageQueryEngine(false);
-        String query = String.format("SELECT %s FROM %s", LONG_COLUMN, getTableName());
+  @Test
+  public void testWhenLimitIsNOTSetExplicitlyThenDefaultLimitIsApplied()
+      throws Exception {
+    setUseMultiStageQueryEngine(false);
+    String query = String.format("SELECT %s FROM %s", LONG_COLUMN, getTableName());
 
-        JsonNode result = postQuery(query).get("resultTable");
-        JsonNode columnDataTypesNode = result.get("dataSchema").get("columnDataTypes");
-        assertEquals(columnDataTypesNode.get(0).textValue(), "LONG");
+    JsonNode result = postQuery(query).get("resultTable");
+    JsonNode columnDataTypesNode = result.get("dataSchema").get("columnDataTypes");
+    assertEquals(columnDataTypesNode.get(0).textValue(), "LONG");
 
-        JsonNode rows = result.get("rows");
-        assertEquals(rows.size(), DEFAULT_LIMIT);
+    JsonNode rows = result.get("rows");
+    assertEquals(rows.size(), DEFAULT_LIMIT);
 
-        for (int rowNum = 0; rowNum < rows.size(); rowNum++) {
-            JsonNode row = rows.get(rowNum);
-            assertEquals(row.size(), 1);
-            assertEquals(row.get(0).asLong(), rowNum );
-        }
+    for (int rowNum = 0; rowNum < rows.size(); rowNum++) {
+      JsonNode row = rows.get(rowNum);
+      assertEquals(row.size(), 1);
+      assertEquals(row.get(0).asLong(), rowNum);
     }
+  }
 
-    @Test
-    public void testWhenLimitISSetExplicitlyThenDefaultLimitIsNotApplied() throws Exception {
-        setUseMultiStageQueryEngine(false);
-        String query = String.format("SELECT %s FROM %s limit 20", LONG_COLUMN, getTableName());
+  @Test
+  public void testWhenLimitISSetExplicitlyThenDefaultLimitIsNotApplied()
+      throws Exception {
+    setUseMultiStageQueryEngine(false);
+    String query = String.format("SELECT %s FROM %s limit 20", LONG_COLUMN, getTableName());
 
-        JsonNode result = postQuery(query).get("resultTable");
-        JsonNode columnDataTypesNode = result.get("dataSchema").get("columnDataTypes");
-        assertEquals(columnDataTypesNode.get(0).textValue(), "LONG");
+    JsonNode result = postQuery(query).get("resultTable");
+    JsonNode columnDataTypesNode = result.get("dataSchema").get("columnDataTypes");
+    assertEquals(columnDataTypesNode.get(0).textValue(), "LONG");
 
-        JsonNode rows = result.get("rows");
-        assertEquals(rows.size(), 20);
+    JsonNode rows = result.get("rows");
+    assertEquals(rows.size(), 20);
 
-        for (int rowNum = 0; rowNum < rows.size(); rowNum++) {
-            JsonNode row = rows.get(rowNum);
-            assertEquals(row.size(), 1);
-            assertEquals(row.get(0).asLong(), rowNum );
-        }
+    for (int rowNum = 0; rowNum < rows.size(); rowNum++) {
+      JsonNode row = rows.get(rowNum);
+      assertEquals(row.size(), 1);
+      assertEquals(row.get(0).asLong(), rowNum);
     }
+  }
 
-    @Override
-    protected void overrideBrokerConf(PinotConfiguration brokerConf) {
-        brokerConf.setProperty(CommonConstants.Broker.CONFIG_OF_BROKER_DEFAULT_QUERY_RESPONSE_LIMIT, DEFAULT_LIMIT);
+  @Override
+  protected void overrideBrokerConf(PinotConfiguration brokerConf) {
+    brokerConf.setProperty(CommonConstants.Broker.CONFIG_OF_BROKER_DEFAULT_QUERY_RESPONSE_LIMIT, DEFAULT_LIMIT);
+  }
+
+  @Override
+  public String getTableName() {
+    return DEFAULT_TABLE_NAME;
+  }
+
+  @Override
+  public Schema createSchema() {
+    return new Schema.SchemaBuilder().setSchemaName(getTableName())
+        .addSingleValueDimension(LONG_COLUMN, FieldSpec.DataType.LONG)
+        .build();
+  }
+
+  @Override
+  public File createAvroFile()
+      throws Exception {
+    org.apache.avro.Schema avroSchema = org.apache.avro.Schema.createRecord("myRecord", null, null, false);
+    avroSchema.setFields(ImmutableList.of(
+        new org.apache.avro.Schema.Field(LONG_COLUMN, org.apache.avro.Schema.create(org.apache.avro.Schema.Type.LONG),
+            null, null)));
+
+    File avroFile = new File(_tempDir, "data.avro");
+    try (DataFileWriter<GenericData.Record> writer = new DataFileWriter<>(new GenericDatumWriter<>(avroSchema))) {
+      writer.create(avroSchema, avroFile);
+      for (int i = 0; i < getCountStarResult(); i++) {
+        GenericData.Record record = new GenericData.Record(avroSchema);
+        record.put(LONG_COLUMN, i);
+        writer.append(record);
+      }
     }
-
-    @Override
-    public String getTableName() {
-        return DEFAULT_TABLE_NAME;
-    }
-
-    @Override
-    public Schema createSchema() {
-        return new Schema.SchemaBuilder().setSchemaName(getTableName())
-                .addSingleValueDimension(LONG_COLUMN, FieldSpec.DataType.LONG)
-                .build();
-    }
-
-    @Override
-    public File createAvroFile() throws Exception {
-        org.apache.avro.Schema avroSchema = org.apache.avro.Schema.createRecord("myRecord", null, null, false);
-        avroSchema.setFields(ImmutableList.of(
-                new org.apache.avro.Schema.Field(LONG_COLUMN, org.apache.avro.Schema.create(org.apache.avro.Schema.Type.LONG),
-                        null, null)));
-
-        File avroFile = new File(_tempDir, "data.avro");
-        try (DataFileWriter<GenericData.Record> writer = new DataFileWriter<>(new GenericDatumWriter<>(avroSchema))) {
-            writer.create(avroSchema, avroFile);
-            for (int i = 0; i < getCountStarResult(); i++) {
-                GenericData.Record record = new GenericData.Record(avroSchema);
-                record.put(LONG_COLUMN, i);
-                writer.append(record);
-            }
-        }
-        return avroFile;
-    }
+    return avroFile;
+  }
 }
