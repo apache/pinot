35f06fb38a60c78b5df518125e14806f4a2cfef7
Bolek Ziobrowski <26925920+bziobrowski@users.noreply.github.com>
2024-11-15 10:41:34 +0100

    Fixed integration test initialization issue.

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/BrokerQueryLimitTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/BrokerQueryLimitTest.java
@@ -16,17 +16,28 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pinot.integration.tests.custom;
+package org.apache.pinot.integration.tests;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.ImmutableList;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.commons.io.FileUtils;
+import org.apache.pinot.integration.tests.custom.CustomDataQueryClusterIntegrationTest;
+import org.apache.pinot.spi.config.table.TableConfig;
+import org.apache.pinot.spi.config.table.TableType;
 import org.apache.pinot.spi.data.FieldSpec;
 import org.apache.pinot.spi.data.Schema;
 import org.apache.pinot.spi.env.PinotConfiguration;
 import org.apache.pinot.spi.utils.CommonConstants;
+import org.apache.pinot.spi.utils.builder.ControllerRequestURLBuilder;
+import org.apache.pinot.spi.utils.builder.TableConfigBuilder;
+import org.apache.pinot.util.TestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -34,8 +45,11 @@ import java.io.File;
 import static org.testng.Assert.*;
 
 
-public class BrokerQueryLimitTest extends CustomDataQueryClusterIntegrationTest {
+// this test uses separate cluster because it needs to change broker configuration
+// which is only done once per suite
+public class BrokerQueryLimitTest extends BaseClusterIntegrationTest {
 
+  protected static final Logger LOGGER = LoggerFactory.getLogger(BrokerQueryLimitTest.class);
   private static final String LONG_COLUMN = "longCol";
   public static final int DEFAULT_LIMIT = 5;
 
@@ -96,7 +110,6 @@ public class BrokerQueryLimitTest extends CustomDataQueryClusterIntegrationTest 
         .build();
   }
 
-  @Override
   public File createAvroFile()
       throws Exception {
     org.apache.avro.Schema avroSchema = org.apache.avro.Schema.createRecord("myRecord", null, null, false);
@@ -114,5 +127,65 @@ public class BrokerQueryLimitTest extends CustomDataQueryClusterIntegrationTest 
       }
     }
     return avroFile;
+  }
+
+  @BeforeClass
+  public void setUp()
+      throws Exception {
+    LOGGER.warn("Setting up integration test class: {}", getClass().getSimpleName());
+    TestUtils.ensureDirectoriesExistAndEmpty(_tempDir, _segmentDir, _tarDir);
+
+    // Start the Pinot cluster
+    startZk();
+    LOGGER.warn("Start Kafka in the integration test class");
+    startKafka();
+    startController();
+    startBroker();
+    startServer();
+
+    if (_controllerRequestURLBuilder == null) {
+      _controllerRequestURLBuilder =
+          ControllerRequestURLBuilder.baseUrl("http://localhost:" + getControllerPort());
+    }
+    TestUtils.ensureDirectoriesExistAndEmpty(_tempDir, _segmentDir, _tarDir);
+    // create & upload schema AND table config
+    Schema schema = createSchema();
+    addSchema(schema);
+
+    File avroFile = createAvroFile();
+    // create offline table
+    TableConfig tableConfig = createOfflineTableConfig();
+    addTableConfig(tableConfig);
+
+    // create & upload segments
+    ClusterIntegrationTestUtils.buildSegmentFromAvro(avroFile, tableConfig, schema, 0, _segmentDir, _tarDir);
+    uploadSegments(getTableName(), _tarDir);
+
+    waitForAllDocsLoaded(60_000);
+    LOGGER.warn("Finished setting up integration test class: {}", getClass().getSimpleName());
+  }
+
+  @AfterClass
+  public void tearDown()
+      throws Exception {
+    LOGGER.warn("Tearing down integration test class: {}", getClass().getSimpleName());
+    dropOfflineTable(getTableName());
+    FileUtils.deleteDirectory(_tempDir);
+
+    // Stop Kafka
+    LOGGER.warn("Stop Kafka in the integration test class");
+    stopKafka();
+    // Shutdown the Pinot cluster
+    stopServer();
+    stopBroker();
+    stopController();
+    stopZk();
+    FileUtils.deleteDirectory(_tempDir);
+    LOGGER.warn("Finished tearing down integration test class: {}", getClass().getSimpleName());
+  }
+
+  @Override
+  public TableConfig createOfflineTableConfig() {
+    return new TableConfigBuilder(TableType.OFFLINE).setTableName(getTableName()).build();
   }
 }
