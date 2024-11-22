73bff3428e878e6fdbe77b67e095124a127dcf6d
Shounak kulkarni <shounakmk219@gmail.com>
2024-11-22 17:41:05 +0530

    Introduce MinionTaskProgressManager

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/plugin/minion/tasks/TestEventObserverFactory.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/plugin/minion/tasks/TestEventObserverFactory.java
@@ -25,6 +25,7 @@ import org.apache.pinot.minion.event.MinionEventObserver;
 import org.apache.pinot.minion.event.MinionEventObserverFactory;
 import org.apache.pinot.minion.executor.MinionTaskZkMetadataManager;
 import org.apache.pinot.spi.annotations.minion.EventObserverFactory;
+import org.apache.pinot.spi.tasks.MinionTaskProgressManager;
 
 import static org.testng.Assert.assertTrue;
 
@@ -37,6 +38,10 @@ public class TestEventObserverFactory implements MinionEventObserverFactory {
 
   @Override
   public void init(MinionTaskZkMetadataManager zkMetadataManager) {
+  }
+
+  @Override
+  public void init(MinionTaskZkMetadataManager zkMetadataManager, MinionTaskProgressManager taskProgressManager) {
   }
 
   @Override
--- pinot-minion/src/main/java/org/apache/pinot/minion/event/BaseMinionProgressObserverFactory.java
+++ pinot-minion/src/main/java/org/apache/pinot/minion/event/BaseMinionProgressObserverFactory.java
@@ -19,6 +19,7 @@
 package org.apache.pinot.minion.event;
 
 import org.apache.pinot.minion.executor.MinionTaskZkMetadataManager;
+import org.apache.pinot.spi.tasks.MinionTaskProgressManager;
 
 
 /**
@@ -30,6 +31,10 @@ public abstract class BaseMinionProgressObserverFactory implements MinionEventOb
    * Initializes the task executor factory.
    */
   public void init(MinionTaskZkMetadataManager zkMetadataManager) {
+  }
+
+  @Override
+  public void init(MinionTaskZkMetadataManager zkMetadataManager, MinionTaskProgressManager taskProgressManager) {
   }
 
   /**
