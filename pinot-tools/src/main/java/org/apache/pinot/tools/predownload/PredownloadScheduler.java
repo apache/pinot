e91a0a37d306c05c12229fa513a357e25118925b
gaoxin <gaoxin@uber.com>
2024-12-19 21:49:28 +0000

    Add license header

--- pinot-tools/src/main/java/org/apache/pinot/tools/predownload/PredownloadScheduler.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/predownload/PredownloadScheduler.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.tools.predownload;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -244,7 +262,6 @@ public class PredownloadScheduler {
       }, _executor);
 
       // TODO: add future.orTimeout() to handle per segment downloading timeout
-      // Right now not able to use due to monorepo incapability with JAVA9+ syntax
       futures.add(future);
     }
 
@@ -311,7 +328,6 @@ public class PredownloadScheduler {
     if (tableInfo == null) {
       throw new PredownloadException("Table info not found for segment: " + segmentInfo.getSegmentName());
     }
-    // TableDataManagerConfig is removed in 1.1: https://github.com/apache/pinot/pull/12189
     String tableDataDir =
         tableInfo.getInstanceDataManagerConfig().getInstanceDataDir() + File.separator + tableInfo.getTableConfig()
             .getTableName();
