d1257f2db112d89d1ff81a852e5af88f8c73fe45
Tony Song <tosong@linkedin.com>
2025-01-24 15:35:37 -0800

    Update query exception type in OfflineClusterIntegrationTest

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/OfflineClusterIntegrationTest.java
@@ -1034,7 +1034,7 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
       testQueryError(sqlQuery, QueryException.QUERY_PLANNING_ERROR_CODE);
     } else {
       response = postQuery(sqlQuery);
-      assertTrue(response.get("exceptions").get(0).get("message").toString().startsWith("\"QueryExecutionError"));
+      assertTrue(response.get("exceptions").get(0).get("message").toString().startsWith("\"QueryValidationError"));
     }
 
     // invalid argument
@@ -1043,7 +1043,7 @@ public class OfflineClusterIntegrationTest extends BaseClusterIntegrationTestSet
       testQueryError(sqlQuery, QueryException.QUERY_PLANNING_ERROR_CODE);
     } else {
       response = postQuery(sqlQuery);
-      assertTrue(response.get("exceptions").get(0).get("message").toString().startsWith("\"QueryExecutionError"));
+      assertTrue(response.get("exceptions").get(0).get("message").toString().startsWith("\"QueryValidationError"));
     }
 
     // invalid argument
