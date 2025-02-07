865c5fada2904686cb0cec39e3580c037081c536
Bolek Ziobrowski <26925920+bziobrowski@users.noreply.github.com>
2025-02-07 14:43:07 +0100

    Push auth before query validation. (#14953)

--- pinot-broker/src/main/java/org/apache/pinot/broker/requesthandler/MultiStageBrokerRequestHandler.java
+++ pinot-broker/src/main/java/org/apache/pinot/broker/requesthandler/MultiStageBrokerRequestHandler.java
@@ -137,6 +137,7 @@ public class MultiStageBrokerRequestHandler extends BaseBrokerRequestHandler {
 
     long compilationStartTimeNs = System.nanoTime();
     long queryTimeoutMs;
+    QueryEnvironment queryEnvironment = null;
     QueryEnvironment.QueryPlannerResult queryPlanResult;
     String database;
     try {
@@ -147,13 +148,15 @@ public class MultiStageBrokerRequestHandler extends BaseBrokerRequestHandler {
           CommonConstants.Broker.DEFAULT_INFER_PARTITION_HINT);
       boolean defaultUseSpool = _config.getProperty(CommonConstants.Broker.CONFIG_OF_SPOOLS,
           CommonConstants.Broker.DEFAULT_OF_SPOOLS);
-      QueryEnvironment queryEnvironment = new QueryEnvironment(QueryEnvironment.configBuilder()
+
+      queryEnvironment = new QueryEnvironment(QueryEnvironment.configBuilder()
           .database(database)
           .tableCache(_tableCache)
           .workerManager(_workerManager)
           .defaultInferPartitionHint(inferPartitionHint)
           .defaultUseSpools(defaultUseSpool)
           .build());
+
       switch (sqlNodeAndOptions.getSqlNode().getKind()) {
         case EXPLAIN:
           boolean askServers = QueryOptionsUtils.isExplainAskingServers(queryOptions)
@@ -189,6 +192,18 @@ public class MultiStageBrokerRequestHandler extends BaseBrokerRequestHandler {
     } catch (WebApplicationException e) {
       throw e;
     } catch (RuntimeException e) {
+      if (queryEnvironment != null) {
+        Set<String> resolvedTables = queryEnvironment.getResolvedTables();
+        if (resolvedTables != null && resolvedTables.size() > 0) {
+          // validate table access to prevent schema leak via error messages
+          TableAuthorizationResult tableAuthorizationResult =
+              hasTableAccess(requesterIdentity, resolvedTables, requestContext, httpHeaders);
+          if (!tableAuthorizationResult.hasAccess()) {
+            throwTableAccessError(tableAuthorizationResult);
+          }
+        }
+      }
+
       String consolidatedMessage = ExceptionUtils.consolidateExceptionMessages(e);
       LOGGER.warn("Caught exception planning request {}: {}, {}", requestId, query, consolidatedMessage);
       _brokerMetrics.addMeteredGlobalValue(BrokerMeter.REQUEST_COMPILATION_EXCEPTIONS, 1);
@@ -226,11 +241,7 @@ public class MultiStageBrokerRequestHandler extends BaseBrokerRequestHandler {
     TableAuthorizationResult tableAuthorizationResult =
         hasTableAccess(requesterIdentity, tableNames, requestContext, httpHeaders);
     if (!tableAuthorizationResult.hasAccess()) {
-      String failureMessage = tableAuthorizationResult.getFailureMessage();
-      if (StringUtils.isNotBlank(failureMessage)) {
-        failureMessage = "Reason: " + failureMessage;
-      }
-      throw new WebApplicationException("Permission denied." + failureMessage, Response.Status.FORBIDDEN);
+      throwTableAccessError(tableAuthorizationResult);
     }
 
     // Validate QPS quota
@@ -335,6 +346,14 @@ public class MultiStageBrokerRequestHandler extends BaseBrokerRequestHandler {
     } finally {
       _queryThrottler.release(estimatedNumQueryThreads);
     }
+  }
+
+  private static void throwTableAccessError(TableAuthorizationResult tableAuthorizationResult) {
+    String failureMessage = tableAuthorizationResult.getFailureMessage();
+    if (StringUtils.isNotBlank(failureMessage)) {
+      failureMessage = "Reason: " + failureMessage;
+    }
+    throw new WebApplicationException("Permission denied." + failureMessage, Response.Status.FORBIDDEN);
   }
 
   private Collection<PlanNode> requestPhysicalPlan(DispatchablePlanFragment fragment,
