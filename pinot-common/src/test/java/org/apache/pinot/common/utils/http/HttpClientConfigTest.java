36595b340f7a8aa9c99cf72de5d091efdbd5f7ee
Christopher Peck <27231838+itschrispeck@users.noreply.github.com>
2025-02-13 16:25:45 -0800

    add configs to customize http client/request timeouts (#15010)

--- pinot-common/src/test/java/org/apache/pinot/common/utils/http/HttpClientConfigTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/utils/http/HttpClientConfigTest.java
@@ -32,15 +32,18 @@ public class HttpClientConfigTest {
     pinotConfiguration.setProperty(HttpClientConfig.MAX_CONNS_CONFIG_NAME, "123");
     pinotConfiguration.setProperty(HttpClientConfig.MAX_CONNS_PER_ROUTE_CONFIG_NAME, "11");
     pinotConfiguration.setProperty(HttpClientConfig.DISABLE_DEFAULT_USER_AGENT_CONFIG_NAME, "true");
+    pinotConfiguration.setProperty(HttpClientConfig.CONNECTION_TIMEOUT, "30000");
     HttpClientConfig httpClientConfig = HttpClientConfig.newBuilder(pinotConfiguration).build();
     Assert.assertEquals(123, httpClientConfig.getMaxConnTotal());
     Assert.assertEquals(11, httpClientConfig.getMaxConnPerRoute());
+    Assert.assertEquals(30000, httpClientConfig.getConnectionTimeoutMs());
     Assert.assertTrue(httpClientConfig.isDisableDefaultUserAgent());
 
     // Ensure default builder uses negative values
     HttpClientConfig defaultConfig = HttpClientConfig.newBuilder(new PinotConfiguration()).build();
     Assert.assertTrue(defaultConfig.getMaxConnTotal() < 0, "default value should be < 0");
     Assert.assertTrue(defaultConfig.getMaxConnPerRoute() < 0, "default value should be < 0");
+    Assert.assertTrue(defaultConfig.getConnectionTimeoutMs() < 0, "default value should be < 0");
     Assert.assertFalse(defaultConfig.isDisableDefaultUserAgent(), "Default user agent should be enabled by default");
   }
 }
