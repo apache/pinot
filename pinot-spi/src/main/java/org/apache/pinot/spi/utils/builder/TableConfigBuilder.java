bc3a4f13b8b0565f5c4035c4016bd195de2ff54e
Harnoor7 <harnoor@startree.ai>
2025-02-19 17:20:46 +0530

    Changes default load mode to MMAP

--- pinot-spi/src/main/java/org/apache/pinot/spi/utils/builder/TableConfigBuilder.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/utils/builder/TableConfigBuilder.java
@@ -56,7 +56,7 @@ public class TableConfigBuilder {
   private static final String REFRESH_SEGMENT_PUSH_TYPE = "REFRESH";
   private static final String DEFAULT_DELETED_SEGMENTS_RETENTION_PERIOD = "7d";
   private static final String DEFAULT_NUM_REPLICAS = "1";
-  private static final String DEFAULT_LOAD_MODE = "HEAP";
+  private static final String DEFAULT_LOAD_MODE = "MMAP";
   private static final String MMAP_LOAD_MODE = "MMAP";
 
   private final TableType _tableType;
