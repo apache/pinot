a68ce0b79edd9b10309585a04ca68ecc0496e8cd
Christopher Peck <peck@uber.com>
2025-02-26 18:52:30 -0800

    closable proj op only

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/readers/forward/VarByteChunkForwardIndexReaderV4.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/segment/index/readers/forward/VarByteChunkForwardIndexReaderV4.java
@@ -334,7 +334,6 @@ public class VarByteChunkForwardIndexReaderV4
   private static final class UncompressedReaderContext extends ReaderContext {
 
     private ByteBuffer _chunk;
-    private List<ByteRange> _ranges;
 
     UncompressedReaderContext(PinotDataBuffer metadata, PinotDataBuffer chunks, long chunkStartOffset) {
       super(chunks, metadata, chunkStartOffset);
@@ -376,7 +375,7 @@ public class VarByteChunkForwardIndexReaderV4
 
   private static final class CompressedReaderContext extends ReaderContext {
 
-    private final ByteBuffer _decompressedBuffer;
+    private ByteBuffer _decompressedBuffer;
     private final ChunkDecompressor _chunkDecompressor;
     private final ChunkCompressionType _chunkCompressionType;
 
@@ -445,6 +444,7 @@ public class VarByteChunkForwardIndexReaderV4
     @Override
     public void close() {
       CleanerUtil.cleanQuietly(_decompressedBuffer);
+      _decompressedBuffer = null;
     }
   }
 }
