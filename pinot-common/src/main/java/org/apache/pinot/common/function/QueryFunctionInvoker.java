149cc89dd0b277266958ee52daa462143f7523eb
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2025-03-10 11:21:00 +0100

    Simplify exception handling in QueryFunctionInvoker by removing logging and directly throwing QueryException

--- pinot-common/src/main/java/org/apache/pinot/common/function/QueryFunctionInvoker.java
+++ pinot-common/src/main/java/org/apache/pinot/common/function/QueryFunctionInvoker.java
@@ -2,20 +2,15 @@ package org.apache.pinot.common.function;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import javax.annotation.Nullable;
 import org.apache.pinot.common.utils.PinotDataType;
 import org.apache.pinot.spi.exception.QueryErrorCode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 
 /// Like [FunctionInvoker], but designed to be called from query engines.
 ///
 /// The main difference is that methods on this class throws
 /// [QueryException][org.apache.pinot.spi.exception.QueryException]s instead of generic [Exception]s.
 public class QueryFunctionInvoker {
-  private static final Logger LOGGER = LoggerFactory.getLogger(QueryFunctionInvoker.class);
 
   private final FunctionInvoker _functionInvoker;
 
@@ -53,15 +48,9 @@ public class QueryFunctionInvoker {
     try {
       return _functionInvoker.invokeDirectly(arguments);
     } catch (InvocationTargetException e) {
-      String msg = "Caught exception while invoking method: " + getMethod()
-          + " with arguments: " + Arrays.toString(arguments);
-      LOGGER.info(msg, e);
-      throw QueryErrorCode.QUERY_EXECUTION.asException(msg + ": " + e.getMessage());
+      throw QueryErrorCode.QUERY_EXECUTION.asException(e);
     } catch (IllegalAccessException e) {
-      String msg = "Caught exception while invoking method: " + getMethod() + " with arguments: "
-          + Arrays.toString(arguments);
-      LOGGER.warn(msg, e);
-      throw QueryErrorCode.INTERNAL.asException(msg);
+      throw QueryErrorCode.INTERNAL.asException(e);
     }
   }
 }
