f0236d55cefae08acb534a4c848f01acca4e0fb7
Glen Matsushita <gmatsushita@hubspot.com>
2025-03-14 08:50:06 -0700

    fix compile error

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/assignment/segment/strategy/custom/ZoneAwareSegmentAssignmentStrategy.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/assignment/segment/strategy/custom/ZoneAwareSegmentAssignmentStrategy.java
@@ -22,14 +22,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.helix.HelixManager;
 import org.apache.helix.model.InstanceConfig;
+import org.apache.pinot.common.assignment.InstancePartitions;
+import org.apache.pinot.controller.helix.core.assignment.segment.SegmentAssignmentUtils;
 import org.apache.pinot.controller.helix.core.assignment.segment.strategy.SegmentAssignmentStrategy;
 import org.apache.pinot.spi.config.table.TableConfig;
+import org.apache.pinot.spi.config.table.assignment.InstancePartitionsType;
 import org.apache.pinot.spi.utils.CommonConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,19 +49,42 @@ public class ZoneAwareSegmentAssignmentStrategy implements SegmentAssignmentStra
   
   private HelixManager _helixManager;
   private TableConfig _tableConfig;
+  private int _replication;
+  private String _tableNameWithType;
 
   @Override
   public void init(HelixManager helixManager, TableConfig tableConfig) {
     _helixManager = helixManager;
     _tableConfig = tableConfig;
-    LOGGER.info("Initialized ZoneAwareSegmentAssignmentStrategy for table: {}", tableConfig.getTableName());
+    _tableNameWithType = tableConfig.getTableName();
+    _replication = tableConfig.getReplication();
+    LOGGER.info("Initialized ZoneAwareSegmentAssignmentStrategy for table: {} with replication: {}", 
+        _tableNameWithType, _replication);
   }
 
   @Override
-  public List<String> assignSegment(String segmentName, List<String> instances, Map<String, Map<String, String>> instanceStates,
-      Map<String, InstanceConfig> instanceConfigMap, int numReplicas) {
+  public List<String> assignSegment(String segmentName, Map<String, Map<String, String>> currentAssignment,
+      InstancePartitions instancePartitions, InstancePartitionsType instancePartitionsType) {
+    
+    LOGGER.info("Assigning segment: {} with instance partitions: {}, type: {}", 
+        segmentName, instancePartitions.getInstancePartitionsName(), instancePartitionsType);
+    
+    // Get all instances from instance partitions
+    List<String> instances = SegmentAssignmentUtils.getInstancesForNonReplicaGroupBasedAssignment(
+        instancePartitions, _replication);
+    
+    // Get instance configs for all instances
+    Map<String, InstanceConfig> instanceConfigMap = new HashMap<>();
+    for (String instance : instances) {
+      InstanceConfig instanceConfig = _helixManager.getHelixDataAccessor().getProperty(
+          _helixManager.getHelixDataAccessor().keyBuilder().instanceConfig(instance));
+      if (instanceConfig != null) {
+        instanceConfigMap.put(instance, instanceConfig);
+      }
+    }
+    
     // Group instances by availability zone
-    Map<String, List<InstanceInfo>> instancesByZone = groupInstancesByZone(instances, instanceConfigMap, instanceStates);
+    Map<String, List<InstanceInfo>> instancesByZone = groupInstancesByZone(instances, instanceConfigMap, currentAssignment);
     
     // Get sorted list of zones based on number of available instances
     List<String> sortedZones = new ArrayList<>(instancesByZone.keySet());
@@ -74,15 +100,16 @@ public class ZoneAwareSegmentAssignmentStrategy implements SegmentAssignmentStra
     
     int availableZones = sortedZones.size();
     
-    LOGGER.info("Assigning segment {} with {} replicas across {} availability zones", segmentName, numReplicas, availableZones);
+    LOGGER.info("Assigning segment {} with {} replicas across {} availability zones", 
+        segmentName, _replication, availableZones);
     
     // Assign replicas to zones, prioritizing distribution across zones
-    List<String> selectedInstances = new ArrayList<>(numReplicas);
+    List<String> selectedInstances = new ArrayList<>(_replication);
     
     // Case: We have at least as many zones as replicas (ideal scenario)
-    if (availableZones >= numReplicas) {
+    if (availableZones >= _replication) {
       // Assign one replica per zone, starting with zones that have fewer instances (for better balancing)
-      for (int i = 0; i < numReplicas; i++) {
+      for (int i = 0; i < _replication; i++) {
         String zone = sortedZones.get(i);
         List<InstanceInfo> zoneInstances = sortedInstancesByZone.get(zone);
         
@@ -116,7 +143,7 @@ public class ZoneAwareSegmentAssignmentStrategy implements SegmentAssignmentStra
       List<String> reverseSortedZones = new ArrayList<>(sortedZones);
       Collections.reverse(reverseSortedZones);
       
-      int remainingReplicas = numReplicas - availableZones;
+      int remainingReplicas = _replication - availableZones;
       int zoneIndex = 0;
       
       while (remainingReplicas > 0) {
@@ -138,6 +165,38 @@ public class ZoneAwareSegmentAssignmentStrategy implements SegmentAssignmentStra
     
     LOGGER.info("Assigned segment {} to instances: {}", segmentName, selectedInstances);
     return selectedInstances;
+  }
+
+  @Override
+  public Map<String, Map<String, String>> reassignSegments(Map<String, Map<String, String>> currentAssignment,
+      InstancePartitions instancePartitions, InstancePartitionsType instancePartitionsType) {
+    
+    LOGGER.info("Rebalancing segments for {} instance partitions: {}", 
+        _tableNameWithType, instancePartitions.getInstancePartitionsName());
+    
+    // Create new assignment map
+    Map<String, Map<String, String>> newAssignment = new HashMap<>();
+    
+    // For each segment in the current assignment
+    for (Map.Entry<String, Map<String, String>> entry : currentAssignment.entrySet()) {
+      String segmentName = entry.getKey();
+      Map<String, String> instanceStateMap = entry.getValue();
+      
+      // Assign segment using our zone-aware strategy
+      List<String> newInstances = assignSegment(segmentName, currentAssignment, instancePartitions, instancePartitionsType);
+      
+      // Create instance state map with same states as current assignment
+      Map<String, String> newInstanceStateMap = new HashMap<>(newInstances.size());
+      for (String instance : newInstances) {
+        // Copy state from the old assignment or use default of ONLINE
+        String state = instanceStateMap.getOrDefault(instance, "ONLINE");
+        newInstanceStateMap.put(instance, state);
+      }
+      
+      newAssignment.put(segmentName, newInstanceStateMap);
+    }
+    
+    return newAssignment;
   }
 
   /**
@@ -165,9 +224,10 @@ public class ZoneAwareSegmentAssignmentStrategy implements SegmentAssignmentStra
       
       // Count current segments assigned to this instance
       int segmentCount = 0;
-      Map<String, String> instanceStateMap = instanceStates.get(instanceName);
-      if (instanceStateMap != null) {
-        segmentCount = instanceStateMap.size();
+      for (Map<String, String> segmentAssignment : instanceStates.values()) {
+        if (segmentAssignment.containsKey(instanceName)) {
+          segmentCount++;
+        }
       }
       
       // Add instance to zone group
--- pinot-controller/src/test/java/org/apache/pinot/controller/helix/core/assignment/segment/strategy/custom/ZoneAwareSegmentAssignmentStrategyTest.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/helix/core/assignment/segment/strategy/custom/ZoneAwareSegmentAssignmentStrategyTest.java
@@ -25,17 +25,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import org.apache.helix.HelixDataAccessor;
 import org.apache.helix.HelixManager;
+import org.apache.helix.PropertyKey;
 import org.apache.helix.model.InstanceConfig;
 import org.apache.helix.zookeeper.datamodel.ZNRecord;
+import org.apache.pinot.common.assignment.InstancePartitions;
 import org.apache.pinot.controller.helix.core.assignment.segment.strategy.SegmentAssignmentStrategy;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.config.table.TableType;
+import org.apache.pinot.spi.config.table.assignment.InstancePartitionsType;
 import org.apache.pinot.spi.utils.CommonConstants;
 import org.apache.pinot.spi.utils.builder.TableConfigBuilder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -43,50 +46,86 @@ import org.testng.annotations.Test;
 public class ZoneAwareSegmentAssignmentStrategyTest {
 
   private static final String RAW_TABLE_NAME = "testTable";
+  private static final String TABLE_NAME_WITH_TYPE = "testTable_OFFLINE";
   private static final String SEGMENT_NAME = "testSegment";
 
   @Mock
   private HelixManager _helixManager;
+  
+  @Mock
+  private HelixDataAccessor _helixDataAccessor;
+  
+  @Mock
+  private PropertyKey.Builder _keyBuilder;
 
   private SegmentAssignmentStrategy _strategy;
   private TableConfig _tableConfig;
+  private InstancePartitions _instancePartitions;
+  private Map<String, InstanceConfig> _instanceConfigMap;
+  private Map<String, PropertyKey> _instanceConfigKeys;
 
   @BeforeMethod
   public void setUp() {
-    MockitoAnnotations.initMocks(this);
-    _tableConfig = new TableConfigBuilder(TableType.OFFLINE).setTableName(RAW_TABLE_NAME).build();
+    // Initialize mocks
+    _helixManager = Mockito.mock(HelixManager.class);
+    _helixDataAccessor = Mockito.mock(HelixDataAccessor.class);
+    _keyBuilder = Mockito.mock(PropertyKey.Builder.class);
+    
+    // Setup table config
+    _tableConfig = new TableConfigBuilder(TableType.OFFLINE).setTableName(RAW_TABLE_NAME).setNumReplicas(3).build();
+    
+    // Setup instance configs and mocks
+    setupInstanceConfigs();
+    
+    // Setup mocks
+    Mockito.when(_helixManager.getHelixDataAccessor()).thenReturn(_helixDataAccessor);
+    Mockito.when(_helixDataAccessor.keyBuilder()).thenReturn(_keyBuilder);
+    
+    // Setup instance config keys and mock getProperty
+    for (Map.Entry<String, InstanceConfig> entry : _instanceConfigMap.entrySet()) {
+      PropertyKey instanceConfigKey = Mockito.mock(PropertyKey.class);
+      _instanceConfigKeys.put(entry.getKey(), instanceConfigKey);
+      Mockito.when(_keyBuilder.instanceConfig(entry.getKey())).thenReturn(instanceConfigKey);
+      Mockito.when(_helixDataAccessor.getProperty(instanceConfigKey)).thenReturn(entry.getValue());
+    }
+    
+    // Setup strategy
     _strategy = new ZoneAwareSegmentAssignmentStrategy();
     _strategy.init(_helixManager, _tableConfig);
+  }
+  
+  private void setupInstanceConfigs() {
+    _instanceConfigMap = new HashMap<>();
+    _instanceConfigKeys = new HashMap<>();
+    
+    // Setup 6 servers across 3 zones (2 servers per zone)
+    setupInstanceWithZone(_instanceConfigMap, "server1", "us-west-2a");
+    setupInstanceWithZone(_instanceConfigMap, "server2", "us-west-2a");
+    setupInstanceWithZone(_instanceConfigMap, "server3", "us-west-2b");
+    setupInstanceWithZone(_instanceConfigMap, "server4", "us-west-2b");
+    setupInstanceWithZone(_instanceConfigMap, "server5", "us-west-2c");
+    setupInstanceWithZone(_instanceConfigMap, "server6", "us-west-2c");
+    
+    // Create instance partitions with all 6 servers
+    _instancePartitions = new InstancePartitions(TABLE_NAME_WITH_TYPE);
+    _instancePartitions.setInstances(0, 0, Arrays.asList(
+        "server1", "server2", "server3", "server4", "server5", "server6"));
   }
 
   @Test
   public void testAssignmentWithMultipleZones() {
-    // Create instances in different zones
-    List<String> instances = Arrays.asList("server1", "server2", "server3", "server4", "server5", "server6");
-    Map<String, InstanceConfig> instanceConfigMap = new HashMap<>();
-    
-    // Setup 6 servers across 3 zones (2 servers per zone)
-    setupInstanceWithZone(instanceConfigMap, "server1", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server2", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server3", "us-west-2b");
-    setupInstanceWithZone(instanceConfigMap, "server4", "us-west-2b");
-    setupInstanceWithZone(instanceConfigMap, "server5", "us-west-2c");
-    setupInstanceWithZone(instanceConfigMap, "server6", "us-west-2c");
-    
     // No existing segments
-    Map<String, Map<String, String>> instanceStates = new HashMap<>();
-    for (String instance : instances) {
-      instanceStates.put(instance, new HashMap<>());
-    }
+    Map<String, Map<String, String>> currentAssignment = new HashMap<>();
     
-    // Test with 3 replicas (should assign one replica to each zone)
-    List<String> selectedInstances = _strategy.assignSegment(SEGMENT_NAME, instances, instanceStates, instanceConfigMap, 3);
+    // Test assignment
+    List<String> selectedInstances = _strategy.assignSegment(
+        SEGMENT_NAME, currentAssignment, _instancePartitions, InstancePartitionsType.OFFLINE);
     
     // Verify we have 3 instances selected
     Assert.assertEquals(selectedInstances.size(), 3);
     
     // Verify instances are from different zones
-    Map<String, Integer> zoneCount = countInstancesByZone(selectedInstances, instanceConfigMap);
+    Map<String, Integer> zoneCount = countInstancesByZone(selectedInstances, _instanceConfigMap);
     Assert.assertEquals(zoneCount.size(), 3);
     for (int count : zoneCount.values()) {
       Assert.assertEquals(count, 1);
@@ -95,36 +134,28 @@ public class ZoneAwareSegmentAssignmentStrategyTest {
   
   @Test
   public void testAssignmentWithFewerZonesThanReplicas() {
-    // Create instances in different zones
-    List<String> instances = Arrays.asList("server1", "server2", "server3", "server4");
-    Map<String, InstanceConfig> instanceConfigMap = new HashMap<>();
-    
-    // Setup 4 servers across 2 zones (2 servers per zone)
-    setupInstanceWithZone(instanceConfigMap, "server1", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server2", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server3", "us-west-2b");
-    setupInstanceWithZone(instanceConfigMap, "server4", "us-west-2b");
+    // Create a new instance partitions with only 4 servers from 2 zones
+    InstancePartitions limitedPartitions = new InstancePartitions(TABLE_NAME_WITH_TYPE);
+    limitedPartitions.setInstances(0, 0, Arrays.asList("server1", "server2", "server3", "server4"));
     
     // No existing segments
-    Map<String, Map<String, String>> instanceStates = new HashMap<>();
-    for (String instance : instances) {
-      instanceStates.put(instance, new HashMap<>());
-    }
+    Map<String, Map<String, String>> currentAssignment = new HashMap<>();
     
-    // Test with 3 replicas (should assign at least one replica to each zone)
-    List<String> selectedInstances = _strategy.assignSegment(SEGMENT_NAME, instances, instanceStates, instanceConfigMap, 3);
+    // Test assignment
+    List<String> selectedInstances = _strategy.assignSegment(
+        SEGMENT_NAME, currentAssignment, limitedPartitions, InstancePartitionsType.OFFLINE);
     
     // Verify we have 3 instances selected
     Assert.assertEquals(selectedInstances.size(), 3);
     
     // Verify instances are distributed across zones (at least one per zone)
-    Map<String, Integer> zoneCount = countInstancesByZone(selectedInstances, instanceConfigMap);
+    Map<String, Integer> zoneCount = countInstancesByZone(selectedInstances, _instanceConfigMap);
     Assert.assertEquals(zoneCount.size(), 2);
     for (int count : zoneCount.values()) {
       Assert.assertTrue(count >= 1);
     }
     
-    // Third replica must be in one of the zones
+    // Total count should still be 3
     int totalCount = 0;
     for (int count : zoneCount.values()) {
       totalCount += count;
@@ -134,39 +165,25 @@ public class ZoneAwareSegmentAssignmentStrategyTest {
   
   @Test
   public void testLoadBalancing() {
-    // Create instances in different zones
-    List<String> instances = Arrays.asList("server1", "server2", "server3", "server4", "server5", "server6");
-    Map<String, InstanceConfig> instanceConfigMap = new HashMap<>();
-    
-    // Setup 6 servers across 3 zones (2 servers per zone)
-    setupInstanceWithZone(instanceConfigMap, "server1", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server2", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server3", "us-west-2b");
-    setupInstanceWithZone(instanceConfigMap, "server4", "us-west-2b");
-    setupInstanceWithZone(instanceConfigMap, "server5", "us-west-2c");
-    setupInstanceWithZone(instanceConfigMap, "server6", "us-west-2c");
-    
     // Create some existing segment assignments to test load balancing
-    Map<String, Map<String, String>> instanceStates = new HashMap<>();
-    for (String instance : instances) {
-      instanceStates.put(instance, new HashMap<>());
-    }
+    Map<String, Map<String, String>> currentAssignment = new HashMap<>();
     
     // Add existing segments to server1, server3, server5
-    Map<String, String> server1Segments = instanceStates.get("server1");
-    server1Segments.put("segment1", "ONLINE");
-    server1Segments.put("segment2", "ONLINE");
+    Map<String, String> segment1Assignment = new HashMap<>();
+    segment1Assignment.put("server1", "ONLINE");
+    segment1Assignment.put("server3", "ONLINE");
+    segment1Assignment.put("server5", "ONLINE");
+    currentAssignment.put("segment1", segment1Assignment);
     
-    Map<String, String> server3Segments = instanceStates.get("server3");
-    server3Segments.put("segment3", "ONLINE");
+    Map<String, String> segment2Assignment = new HashMap<>();
+    segment2Assignment.put("server1", "ONLINE");
+    segment2Assignment.put("server3", "ONLINE");
+    segment2Assignment.put("server5", "ONLINE");
+    currentAssignment.put("segment2", segment2Assignment);
     
-    Map<String, String> server5Segments = instanceStates.get("server5");
-    server5Segments.put("segment4", "ONLINE");
-    server5Segments.put("segment5", "ONLINE");
-    server5Segments.put("segment6", "ONLINE");
-    
-    // Test with 3 replicas (should distribute based on load within zones)
-    List<String> selectedInstances = _strategy.assignSegment(SEGMENT_NAME, instances, instanceStates, instanceConfigMap, 3);
+    // Test assignment
+    List<String> selectedInstances = _strategy.assignSegment(
+        SEGMENT_NAME, currentAssignment, _instancePartitions, InstancePartitionsType.OFFLINE);
     
     // Verify we have 3 instances selected
     Assert.assertEquals(selectedInstances.size(), 3);
@@ -178,32 +195,40 @@ public class ZoneAwareSegmentAssignmentStrategyTest {
   }
   
   @Test
-  public void testSameZoneAllocation() {
-    // Create instances in the same zone
-    List<String> instances = Arrays.asList("server1", "server2", "server3");
-    Map<String, InstanceConfig> instanceConfigMap = new HashMap<>();
+  public void testReassignSegments() {
+    // Create initial assignment with 2 segments
+    Map<String, Map<String, String>> currentAssignment = new HashMap<>();
     
-    // Setup 3 servers all in the same zone
-    setupInstanceWithZone(instanceConfigMap, "server1", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server2", "us-west-2a");
-    setupInstanceWithZone(instanceConfigMap, "server3", "us-west-2a");
+    Map<String, String> segment1Assignment = new HashMap<>();
+    segment1Assignment.put("server1", "ONLINE");
+    segment1Assignment.put("server3", "ONLINE");
+    segment1Assignment.put("server5", "ONLINE");
+    currentAssignment.put("segment1", segment1Assignment);
     
-    // No existing segments
-    Map<String, Map<String, String>> instanceStates = new HashMap<>();
-    for (String instance : instances) {
-      instanceStates.put(instance, new HashMap<>());
+    Map<String, String> segment2Assignment = new HashMap<>();
+    segment2Assignment.put("server1", "ONLINE");
+    segment2Assignment.put("server3", "ONLINE");
+    segment2Assignment.put("server6", "ONLINE");
+    currentAssignment.put("segment2", segment2Assignment);
+    
+    // Test reassignment
+    Map<String, Map<String, String>> newAssignment = _strategy.reassignSegments(
+        currentAssignment, _instancePartitions, InstancePartitionsType.OFFLINE);
+    
+    // Verify we have the same number of segments
+    Assert.assertEquals(newAssignment.size(), currentAssignment.size());
+    
+    // Verify each segment has the correct number of replicas
+    for (Map<String, String> instanceStateMap : newAssignment.values()) {
+      Assert.assertEquals(instanceStateMap.size(), 3);
     }
     
-    // Test with 3 replicas (all must be in the same zone)
-    List<String> selectedInstances = _strategy.assignSegment(SEGMENT_NAME, instances, instanceStates, instanceConfigMap, 3);
-    
-    // Verify we have 3 instances selected
-    Assert.assertEquals(selectedInstances.size(), 3);
-    
-    // All instances should be from the same zone
-    Map<String, Integer> zoneCount = countInstancesByZone(selectedInstances, instanceConfigMap);
-    Assert.assertEquals(zoneCount.size(), 1);
-    Assert.assertEquals(zoneCount.get("us-west-2a").intValue(), 3);
+    // Verify state is preserved
+    for (Map.Entry<String, Map<String, String>> entry : newAssignment.entrySet()) {
+      for (Map.Entry<String, String> instanceState : entry.getValue().entrySet()) {
+        Assert.assertEquals(instanceState.getValue(), "ONLINE");
+      }
+    }
   }
   
   private void setupInstanceWithZone(Map<String, InstanceConfig> instanceConfigMap, String instanceName, String zone) {
