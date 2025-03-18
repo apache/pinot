bc6b760a820f3f41cfbfbabd8707868a931991a5
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2025-03-18 15:27:49 +0100

    Fix imports

--- pinot-integration-test-base/src/test/java/org/apache/pinot/integration/tests/ClusterTest.java
+++ pinot-integration-test-base/src/test/java/org/apache/pinot/integration/tests/ClusterTest.java
@@ -26,7 +26,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
