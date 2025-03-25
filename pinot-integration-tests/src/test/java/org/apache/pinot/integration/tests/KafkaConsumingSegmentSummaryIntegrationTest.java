f98d3ffc714599ccce93257774c9e2993daa496b
J-HowHuang <howardhuang2001@gmail.com>
2025-03-25 11:45:44 -0700

    add integration test

---
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/KafkaConsumingSegmentSummaryIntegrationTest.java
@@ -0,0 +1,127 @@
+package org.apache.pinot.integration.tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import org.apache.commons.io.FileUtils;
+import org.apache.helix.model.HelixConfigScope;
+import org.apache.helix.model.builder.HelixConfigScopeBuilder;
+import org.apache.pinot.controller.helix.core.rebalance.RebalanceResult;
+import org.apache.pinot.controller.helix.core.rebalance.RebalanceSummaryResult;
+import org.apache.pinot.spi.config.table.IndexingConfig;
+import org.apache.pinot.spi.config.table.TableConfig;
+import org.apache.pinot.spi.data.Schema;
+import org.apache.pinot.spi.stream.StreamConfigProperties;
+import org.apache.pinot.spi.utils.CommonConstants;
+import org.apache.pinot.spi.utils.JsonUtils;
+import org.apache.pinot.util.TestUtils;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class KafkaConsumingSegmentSummaryIntegrationTest extends BaseRealtimeClusterIntegrationTest {
+
+  @Override
+  @BeforeClass
+  public void setUp()
+      throws Exception {
+    TestUtils.ensureDirectoriesExistAndEmpty(_tempDir);
+
+    // Start the Pinot cluster
+    startZk();
+    startController();
+
+    HelixConfigScope scope =
+        new HelixConfigScopeBuilder(HelixConfigScope.ConfigScopeProperty.CLUSTER).forCluster(getHelixClusterName())
+            .build();
+    // Set max segment preprocess parallelism to 8
+    _helixManager.getConfigAccessor()
+        .set(scope, CommonConstants.Helix.CONFIG_OF_MAX_SEGMENT_PREPROCESS_PARALLELISM, Integer.toString(8));
+    // Set max segment startree preprocess parallelism to 6
+    _helixManager.getConfigAccessor()
+        .set(scope, CommonConstants.Helix.CONFIG_OF_MAX_SEGMENT_STARTREE_PREPROCESS_PARALLELISM, Integer.toString(6));
+
+    startBroker();
+    startServer();
+
+    // Start Kafka
+    startKafka();
+
+    // Unpack the Avro files
+    List<File> avroFiles = unpackAvroData(_tempDir);
+
+    // Create and upload the schema and table config
+    Schema schema = createSchema();
+    addSchema(schema);
+    TableConfig tableConfig = createRealtimeTableConfig(avroFiles.get(0));
+    IndexingConfig indexingConfig = tableConfig.getIndexingConfig();
+    Map<String, String> streamConfig = getStreamConfigs();
+    streamConfig.put(StreamConfigProperties.SEGMENT_FLUSH_THRESHOLD_SEGMENT_SIZE, "1000000");
+    streamConfig.remove(StreamConfigProperties.SEGMENT_FLUSH_THRESHOLD_ROWS);
+    indexingConfig.setStreamConfigs(streamConfig);
+    tableConfig.setIndexingConfig(indexingConfig);
+    addTableConfig(tableConfig);
+
+    // Push data into Kafka
+    pushAvroIntoKafka(avroFiles);
+
+    // create segments and upload them to controller
+    createSegmentsAndUpload(avroFiles, schema, tableConfig);
+
+    // Set up the H2 connection
+    setUpH2Connection(avroFiles);
+
+    // Initialize the query generator
+    setUpQueryGenerator(avroFiles);
+
+    runValidationJob(600_000);
+
+    // Wait for all documents loaded
+    waitForAllDocsLoaded(600_000L);
+  }
+
+  @Test
+  public void testConsumingSegmentSummary() throws Exception {
+    String response = sendPostRequest(
+        getControllerRequestURLBuilder().forTableRebalance(getTableName(), "REALTIME", true, false, true, false, -1));
+    RebalanceResult result = JsonUtils.stringToObject(response, RebalanceResult.class);
+    Assert.assertNotNull(result);
+    Assert.assertNotNull(result.getRebalanceSummaryResult());
+    Assert.assertNotNull(result.getRebalanceSummaryResult().getSegmentInfo());
+    RebalanceSummaryResult.SegmentInfo segmentInfo = result.getRebalanceSummaryResult().getSegmentInfo();
+    Assert.assertNotNull(segmentInfo.getConsumingSegmentSummary());
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getNumConsumingSegmentsToBeMoved(), 0);
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getMaxBytesConsumingSegmentsToCatchUp(), 0);
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getBytesConsumingSegmentsToCatchUpPerServer().size(), 1);
+
+    startServer();
+    response = sendPostRequest(
+        getControllerRequestURLBuilder().forTableRebalance(getTableName(), "REALTIME", true, false, true, false, -1));
+    result = JsonUtils.stringToObject(response, RebalanceResult.class);
+    Assert.assertNotNull(result);
+    Assert.assertNotNull(result.getRebalanceSummaryResult());
+    Assert.assertNotNull(result.getRebalanceSummaryResult().getSegmentInfo());
+    segmentInfo = result.getRebalanceSummaryResult().getSegmentInfo();
+    Assert.assertNotNull(segmentInfo.getConsumingSegmentSummary());
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getNumConsumingSegmentsToBeMoved(), 1);
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getMaxBytesConsumingSegmentsToCatchUp(), 57801);
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getBytesConsumingSegmentsToCatchUpPerServer().size(), 2);
+    Assert.assertTrue(segmentInfo.getConsumingSegmentSummary().getBytesConsumingSegmentsToCatchUpPerServer().values().stream().allMatch(x -> x == 57801 || x == 0));
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getBytesConsumingSegmentsToCatchUpPerServer().values().stream().reduce(0, Integer::sum), 57801);
+
+    stopServer();
+    response = sendPostRequest(
+        getControllerRequestURLBuilder().forTableRebalance(getTableName(), "REALTIME", true, false, true, false, -1));
+    RebalanceResult result_no_info = JsonUtils.stringToObject(response, RebalanceResult.class);
+    Assert.assertNotNull(result_no_info);
+    Assert.assertNotNull(result_no_info.getRebalanceSummaryResult());
+    Assert.assertNotNull(result_no_info.getRebalanceSummaryResult().getSegmentInfo());
+    segmentInfo = result_no_info.getRebalanceSummaryResult().getSegmentInfo();
+    Assert.assertNotNull(segmentInfo.getConsumingSegmentSummary());
+    Assert.assertEquals(segmentInfo.getConsumingSegmentSummary().getNumConsumingSegmentsToBeMoved(), 1);
+    Assert.assertNull(segmentInfo.getConsumingSegmentSummary().getMaxBytesConsumingSegmentsToCatchUp());
+    Assert.assertNull(segmentInfo.getConsumingSegmentSummary().getBytesConsumingSegmentsToCatchUpPerServer());
+  }
+}
