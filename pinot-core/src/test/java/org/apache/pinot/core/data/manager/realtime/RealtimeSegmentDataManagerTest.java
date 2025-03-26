3bcca12e4c289f6a6a9fd6baf64bd7e7217969a0
lnbest0707 <106711887+lnbest0707-uber@users.noreply.github.com>
2025-03-26 15:21:26 -0700

    Retry on segment cannot build (#15234)
    * Retry on segment cannot build
    
    * Resolve comments
    
    * Minor change on comments
    
    * Change naming to resolve comments

--- pinot-core/src/test/java/org/apache/pinot/core/data/manager/realtime/RealtimeSegmentDataManagerTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/data/manager/realtime/RealtimeSegmentDataManagerTest.java
@@ -55,6 +55,7 @@ import org.apache.pinot.segment.local.segment.index.loader.IndexLoadingConfig;
 import org.apache.pinot.segment.local.utils.SegmentLocks;
 import org.apache.pinot.spi.config.instance.InstanceDataManagerConfig;
 import org.apache.pinot.spi.config.table.TableConfig;
+import org.apache.pinot.spi.config.table.ingestion.IngestionConfig;
 import org.apache.pinot.spi.data.Schema;
 import org.apache.pinot.spi.env.PinotConfiguration;
 import org.apache.pinot.spi.metrics.PinotMetricUtils;
@@ -146,6 +147,10 @@ public class RealtimeSegmentDataManagerTest {
       tableConfig.getIndexingConfig().getStreamConfigs()
           .put(StreamConfigProperties.SEGMENT_FLUSH_THRESHOLD_TIME, maxDuration);
     }
+    if (tableConfig.getIngestionConfig() == null) {
+      tableConfig.setIngestionConfig(new IngestionConfig());
+    }
+    tableConfig.getIngestionConfig().setRetryOnSegmentBuildPrecheckFailure(true);
     RealtimeTableDataManager tableDataManager = createTableDataManager(tableConfig);
     LLCSegmentName llcSegmentName = new LLCSegmentName(SEGMENT_NAME_STR);
     _partitionGroupIdToSemaphoreMap.putIfAbsent(PARTITION_GROUP_ID, new Semaphore(1));
@@ -248,6 +253,7 @@ public class RealtimeSegmentDataManagerTest {
 
     consumer.run();
     Assert.assertTrue(segmentDataManager._buildSegmentCalled);
+    Assert.assertTrue(segmentDataManager._notifySegmentBuildFailedWithDeterministicErrorCalled);
     Assert.assertEquals(segmentDataManager._state.get(segmentDataManager), RealtimeSegmentDataManager.State.ERROR);
     segmentDataManager.close();
   }
@@ -906,6 +912,7 @@ public class RealtimeSegmentDataManagerTest {
     public Field _state;
     public Field _shouldStop;
     public Field _stopReason;
+    public Field _segmentBuildFailedWithDeterministicError;
     private Field _streamMsgOffsetFactory;
     public LinkedList<LongMsgOffset> _consumeOffsets = new LinkedList<>();
     public LinkedList<SegmentCompletionProtocol.Response> _responses = new LinkedList<>();
@@ -915,6 +922,7 @@ public class RealtimeSegmentDataManagerTest {
     public boolean _buildAndReplaceCalled = false;
     public int _stopWaitTimeMs = 100;
     private boolean _downloadAndReplaceCalled = false;
+    private boolean _notifySegmentBuildFailedWithDeterministicErrorCalled = false;
     public boolean _throwExceptionFromConsume = false;
     public boolean _postConsumeStoppedCalled = false;
     public Map<Integer, Semaphore> _semaphoreMap;
@@ -946,6 +954,9 @@ public class RealtimeSegmentDataManagerTest {
       _shouldStop.setAccessible(true);
       _stopReason = RealtimeSegmentDataManager.class.getDeclaredField("_stopReason");
       _stopReason.setAccessible(true);
+      _segmentBuildFailedWithDeterministicError =
+          RealtimeSegmentDataManager.class.getDeclaredField("_segmentBuildFailedWithDeterministicError");
+      _segmentBuildFailedWithDeterministicError.setAccessible(true);
       _semaphoreMap = semaphoreMap;
       _streamMsgOffsetFactory = RealtimeSegmentDataManager.class.getDeclaredField("_streamPartitionMsgOffsetFactory");
       _streamMsgOffsetFactory.setAccessible(true);
@@ -1022,6 +1033,12 @@ public class RealtimeSegmentDataManagerTest {
       _postConsumeStoppedCalled = true;
     }
 
+    @Override
+    protected void notifySegmentBuildFailedWithDeterministicError() {
+      _notifySegmentBuildFailedWithDeterministicErrorCalled = true;
+    }
+
+
     // TODO: Some of the tests rely on specific number of calls to the `now()` method in the SegmentDataManager.
     // This is not a good coding practice and makes the code very fragile. This needs to be fixed.
     // Invoking now() in any part of RealtimeSegmentDataManager code will break the following tests:
@@ -1051,6 +1068,11 @@ public class RealtimeSegmentDataManagerTest {
     protected SegmentBuildDescriptor buildSegmentInternal(boolean forCommit) {
       _buildSegmentCalled = true;
       if (_failSegmentBuild) {
+        try {
+          _segmentBuildFailedWithDeterministicError.set(this, true);
+        } catch (Exception e) {
+          Assert.fail();
+        }
         return null;
       }
       if (!forCommit) {
