443ae6d4f3a30282b5d711dcc9e1f1e99600b511
songqiao su <andysongqiao@gmail.com>
2025-03-28 00:43:27 -0700

    fmt

--- pinot-minion/src/main/java/org/apache/pinot/minion/BaseMinionStarter.java
+++ pinot-minion/src/main/java/org/apache/pinot/minion/BaseMinionStarter.java
@@ -310,7 +310,7 @@ public abstract class BaseMinionStarter implements ServiceStartable {
     _helixManager.connect();
     updateInstanceConfigIfNeeded();
     minionMetrics.setOrUpdateGauge(CommonConstants.Helix.INSTANCE_CONNECTED_METRIC_NAME,
-        () -> _helixManager.isConnected() ? 1L : 0L);
+            () -> _helixManager.isConnected() ? 1L : 0L);
     minionContext.setHelixPropertyStore(_helixManager.getHelixPropertyStore());
     minionContext.setHelixManager(_helixManager);
     LOGGER.info("Starting minion admin application on: {}", ListenerConfigUtil.toString(_listenerConfigs));
