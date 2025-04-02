fa86ccda432b6090724cf0a41859f18eedf0f41d
Sonam Mandal <sonam.mandal@startree.ai>
2025-04-02 13:20:19 -0700

    Address remaining review comments

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/rebalance/TableRebalanceProgressStats.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/rebalance/TableRebalanceProgressStats.java
@@ -20,6 +20,7 @@ package org.apache.pinot.controller.helix.core.rebalance;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import java.util.Objects;
 
 
 /**
@@ -135,23 +136,103 @@ public class TableRebalanceProgressStats {
     return _rebalanceProgressStatsCurrentStep;
   }
 
-  public static boolean progressStatsDiffer(RebalanceProgressStats base, RebalanceProgressStats compare) {
-    // Don't check for changes in the estimated time for completion as this will always be updated since it is
-    // newly calculated for each iteration based on current time and start time
-    return base._totalSegmentsToBeAdded != compare._totalSegmentsToBeAdded
-        || base._totalSegmentsToBeDeleted != compare._totalSegmentsToBeDeleted
-        || base._totalRemainingSegmentsToBeAdded != compare._totalRemainingSegmentsToBeAdded
-        || base._totalRemainingSegmentsToBeDeleted != compare._totalRemainingSegmentsToBeDeleted
-        || base._totalRemainingSegmentsToConverge != compare._totalRemainingSegmentsToConverge
-        || base._totalCarryOverSegmentsToBeAdded != compare._totalCarryOverSegmentsToBeAdded
-        || base._totalCarryOverSegmentsToBeDeleted != compare._totalCarryOverSegmentsToBeDeleted
-        || base._totalUniqueNewUntrackedSegmentsDuringRebalance
-        != compare._totalUniqueNewUntrackedSegmentsDuringRebalance
-        || base._percentageRemainingSegmentsToBeAdded != compare._percentageRemainingSegmentsToBeAdded
-        || base._percentageRemainingSegmentsToBeDeleted != compare._percentageRemainingSegmentsToBeDeleted
-        || base._averageSegmentSizeInBytes != compare._averageSegmentSizeInBytes
-        || base._totalEstimatedDataToBeMovedInBytes != compare._totalEstimatedDataToBeMovedInBytes
-        || base._startTimeMs != compare._startTimeMs;
+  /**
+   * Updates the overall progress stats based on the current step's progress stats. This should be called
+   * during the EV-IS convergence trigger to ensure the overall stats reflect the changes as they are made.
+   * @param latestStepStats latest step level stats calculated in this iteration
+   * @return the newly calculated overall progress stats
+   */
+  public void updateOverallProgressStatsFromStep(TableRebalanceProgressStats.RebalanceProgressStats latestStepStats) {
+    TableRebalanceProgressStats.RebalanceProgressStats lastStepStats = getRebalanceProgressStatsCurrentStep();
+    TableRebalanceProgressStats.RebalanceProgressStats overallProgressStats = getRebalanceProgressStatsOverall();
+    int numAdditionalSegmentsAdded =
+        latestStepStats._totalSegmentsToBeAdded - lastStepStats._totalSegmentsToBeAdded;
+    int numAdditionalSegmentsDeleted =
+        latestStepStats._totalSegmentsToBeDeleted - lastStepStats._totalSegmentsToBeDeleted;
+    int upperBoundOnSegmentsAdded =
+        lastStepStats._totalRemainingSegmentsToBeAdded > lastStepStats._totalSegmentsToBeAdded
+            ? lastStepStats._totalSegmentsToBeAdded : lastStepStats._totalRemainingSegmentsToBeAdded;
+    int numSegmentAddsProcessedInLastStep = Math.abs(upperBoundOnSegmentsAdded
+        - latestStepStats._totalRemainingSegmentsToBeAdded);
+    int upperBoundOnSegmentsDeleted =
+        lastStepStats._totalRemainingSegmentsToBeDeleted > lastStepStats._totalSegmentsToBeDeleted
+            ? lastStepStats._totalSegmentsToBeDeleted : lastStepStats._totalRemainingSegmentsToBeDeleted;
+    int numSegmentDeletesProcessedInLastStep = Math.abs(upperBoundOnSegmentsDeleted
+        - latestStepStats._totalRemainingSegmentsToBeDeleted);
+    int numberNewUntrackedSegmentsAdded = latestStepStats._totalUniqueNewUntrackedSegmentsDuringRebalance
+        - lastStepStats._totalUniqueNewUntrackedSegmentsDuringRebalance;
+
+    TableRebalanceProgressStats.RebalanceProgressStats newOverallProgressStats =
+        new TableRebalanceProgressStats.RebalanceProgressStats();
+
+    newOverallProgressStats._totalSegmentsToBeAdded = overallProgressStats._totalSegmentsToBeAdded
+        + numAdditionalSegmentsAdded;
+    newOverallProgressStats._totalSegmentsToBeDeleted = overallProgressStats._totalSegmentsToBeDeleted
+        + numAdditionalSegmentsDeleted;
+    if (latestStepStats._totalCarryOverSegmentsToBeAdded > 0) {
+      newOverallProgressStats._totalRemainingSegmentsToBeAdded = overallProgressStats._totalRemainingSegmentsToBeAdded;
+    } else {
+      newOverallProgressStats._totalRemainingSegmentsToBeAdded = numAdditionalSegmentsAdded == 0
+          ? overallProgressStats._totalRemainingSegmentsToBeAdded - numSegmentAddsProcessedInLastStep
+          : overallProgressStats._totalRemainingSegmentsToBeAdded + numSegmentAddsProcessedInLastStep;
+    }
+    newOverallProgressStats._totalCarryOverSegmentsToBeAdded =
+        latestStepStats._totalCarryOverSegmentsToBeAdded;
+    if (latestStepStats._totalCarryOverSegmentsToBeDeleted > 0) {
+      newOverallProgressStats._totalRemainingSegmentsToBeDeleted =
+          overallProgressStats._totalRemainingSegmentsToBeDeleted;
+    } else {
+      newOverallProgressStats._totalRemainingSegmentsToBeDeleted = numAdditionalSegmentsDeleted == 0
+          ? overallProgressStats._totalRemainingSegmentsToBeDeleted - numSegmentDeletesProcessedInLastStep
+          : overallProgressStats._totalRemainingSegmentsToBeDeleted + numSegmentDeletesProcessedInLastStep;
+    }
+    newOverallProgressStats._totalCarryOverSegmentsToBeDeleted =
+        latestStepStats._totalCarryOverSegmentsToBeDeleted;
+    newOverallProgressStats._totalRemainingSegmentsToConverge = latestStepStats._totalRemainingSegmentsToConverge;
+    newOverallProgressStats._totalUniqueNewUntrackedSegmentsDuringRebalance =
+        overallProgressStats._totalUniqueNewUntrackedSegmentsDuringRebalance + numberNewUntrackedSegmentsAdded;
+    newOverallProgressStats._percentageRemainingSegmentsToBeAdded =
+        calculatePercentageChange(newOverallProgressStats._totalSegmentsToBeAdded,
+            newOverallProgressStats._totalRemainingSegmentsToBeAdded
+                + newOverallProgressStats._totalCarryOverSegmentsToBeAdded);
+    newOverallProgressStats._percentageRemainingSegmentsToBeDeleted =
+        calculatePercentageChange(newOverallProgressStats._totalSegmentsToBeDeleted,
+            newOverallProgressStats._totalRemainingSegmentsToBeDeleted
+                + newOverallProgressStats._totalCarryOverSegmentsToBeDeleted);
+    // Calculate elapsed time based on start of rebalance (global)
+    newOverallProgressStats._estimatedTimeToCompleteAddsInSeconds =
+        calculateEstimatedTimeToCompleteChange(getStartTimeMs(),
+            newOverallProgressStats._totalSegmentsToBeAdded, newOverallProgressStats._totalRemainingSegmentsToBeAdded);
+    newOverallProgressStats._estimatedTimeToCompleteDeletesInSeconds =
+        calculateEstimatedTimeToCompleteChange(getStartTimeMs(),
+            newOverallProgressStats._totalSegmentsToBeDeleted,
+            newOverallProgressStats._totalRemainingSegmentsToBeDeleted);
+    newOverallProgressStats._averageSegmentSizeInBytes = overallProgressStats._averageSegmentSizeInBytes;
+    newOverallProgressStats._totalEstimatedDataToBeMovedInBytes =
+        overallProgressStats._totalEstimatedDataToBeMovedInBytes
+            + (numAdditionalSegmentsAdded * overallProgressStats._averageSegmentSizeInBytes);
+    newOverallProgressStats._startTimeMs = getStartTimeMs();
+
+    setRebalanceProgressStatsOverall(newOverallProgressStats);
+  }
+
+  public static double calculatePercentageChange(int totalSegmentsToChange, int remainingSegmentsToChange) {
+    return totalSegmentsToChange == 0
+        ? 0.0 : (double) remainingSegmentsToChange / (double) totalSegmentsToChange * 100.0;
+  }
+
+  public static double calculateEstimatedTimeToCompleteChange(long startTime, int totalSegmentsToChange,
+      int remainingSegmentsToChange) {
+    double elapsedTimeInSeconds = (double) (System.currentTimeMillis() - startTime) / 1000.0;
+    int segmentsAlreadyChanged = totalSegmentsToChange - remainingSegmentsToChange;
+    return segmentsAlreadyChanged == 0 ? totalSegmentsToChange == 0 ? 0.0 : -1.0
+        : (double) remainingSegmentsToChange / (double) segmentsAlreadyChanged * elapsedTimeInSeconds;
+  }
+
+  public static long calculateNewEstimatedDataToBeMovedInBytes(long existingDataToBeMovedInBytes,
+      long averageSegmentSizeInBytes, int newSegmentsAdded) {
+    return averageSegmentSizeInBytes < 0
+        ? -1 : existingDataToBeMovedInBytes + ((long) newSegmentsAdded * averageSegmentSizeInBytes);
   }
 
   public static boolean statsDiffer(RebalanceStateStats base, RebalanceStateStats compare) {
@@ -234,6 +315,39 @@ public class TableRebalanceProgressStats {
       _averageSegmentSizeInBytes = 0;
       _totalEstimatedDataToBeMovedInBytes = 0;
       _startTimeMs = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (!(o instanceof RebalanceProgressStats)) {
+        return false;
+      }
+      RebalanceProgressStats that = (RebalanceProgressStats) o;
+      // Don't check for changes in the estimated time for completion as this will always be updated since it is
+      // newly calculated for each iteration based on current time and start time
+      return _totalSegmentsToBeAdded == that._totalSegmentsToBeAdded
+          && _totalSegmentsToBeDeleted == that._totalSegmentsToBeDeleted
+          && _totalRemainingSegmentsToBeAdded == that._totalRemainingSegmentsToBeAdded
+          && _totalRemainingSegmentsToBeDeleted == that._totalRemainingSegmentsToBeDeleted
+          && _totalRemainingSegmentsToConverge == that._totalRemainingSegmentsToConverge
+          && _totalCarryOverSegmentsToBeAdded == that._totalCarryOverSegmentsToBeAdded
+          && _totalCarryOverSegmentsToBeDeleted == that._totalCarryOverSegmentsToBeDeleted
+          && _totalUniqueNewUntrackedSegmentsDuringRebalance == that._totalUniqueNewUntrackedSegmentsDuringRebalance
+          && Double.compare(_percentageRemainingSegmentsToBeAdded, that._percentageRemainingSegmentsToBeAdded)
+          == 0
+          && Double.compare(_percentageRemainingSegmentsToBeDeleted, that._percentageRemainingSegmentsToBeDeleted)
+          == 0 && _averageSegmentSizeInBytes == that._averageSegmentSizeInBytes
+          && _totalEstimatedDataToBeMovedInBytes == that._totalEstimatedDataToBeMovedInBytes
+          && _startTimeMs == that._startTimeMs;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(_totalSegmentsToBeAdded, _totalSegmentsToBeDeleted, _totalRemainingSegmentsToBeAdded,
+          _totalRemainingSegmentsToBeDeleted, _totalRemainingSegmentsToConverge, _totalCarryOverSegmentsToBeAdded,
+          _totalCarryOverSegmentsToBeDeleted, _totalUniqueNewUntrackedSegmentsDuringRebalance,
+          _percentageRemainingSegmentsToBeAdded, _percentageRemainingSegmentsToBeDeleted, _averageSegmentSizeInBytes,
+          _totalEstimatedDataToBeMovedInBytes, _startTimeMs);
     }
   }
 }
