096ad7cdc2c8be828819f0cdd2bb90e35e2fccc2
Xiaotian (Jackie) Jiang <17555551+Jackie-Jiang@users.noreply.github.com>
2025-04-04 15:12:56 -0600

    Make MSE leaf stage use cached TableConfig and Schema from TableDataManager (#15461)

--- pinot-server/src/main/java/org/apache/pinot/server/worker/WorkerQueryServer.java
+++ pinot-server/src/main/java/org/apache/pinot/server/worker/WorkerQueryServer.java
@@ -19,9 +19,7 @@
 package org.apache.pinot.server.worker;
 
 import javax.annotation.Nullable;
-import org.apache.helix.HelixManager;
 import org.apache.pinot.common.config.TlsConfig;
-import org.apache.pinot.common.metrics.ServerMetrics;
 import org.apache.pinot.core.data.manager.InstanceDataManager;
 import org.apache.pinot.query.runtime.QueryRunner;
 import org.apache.pinot.query.service.server.QueryServer;
@@ -38,15 +36,13 @@ public class WorkerQueryServer {
   private final QueryServer _queryWorkerService;
 
   public WorkerQueryServer(PinotConfiguration configuration, InstanceDataManager instanceDataManager,
-      HelixManager helixManager, ServerMetrics serverMetrics, @Nullable TlsConfig tlsConfig,
-      SendStatsPredicate sendStats) {
+      @Nullable TlsConfig tlsConfig, SendStatsPredicate sendStats) {
     _configuration = toWorkerQueryConfig(configuration);
     _queryServicePort = _configuration.getProperty(CommonConstants.MultiStageQueryRunner.KEY_OF_QUERY_SERVER_PORT,
         CommonConstants.MultiStageQueryRunner.DEFAULT_QUERY_SERVER_PORT);
     QueryRunner queryRunner = new QueryRunner();
-    queryRunner.init(_configuration, instanceDataManager, helixManager, serverMetrics, tlsConfig,
-        sendStats::getSendStats);
-    _queryWorkerService = new QueryServer(_queryServicePort, queryRunner, tlsConfig, serverMetrics, configuration);
+    queryRunner.init(_configuration, instanceDataManager, tlsConfig, sendStats::getSendStats);
+    _queryWorkerService = new QueryServer(_queryServicePort, queryRunner, tlsConfig, configuration);
   }
 
   private static PinotConfiguration toWorkerQueryConfig(PinotConfiguration configuration) {
