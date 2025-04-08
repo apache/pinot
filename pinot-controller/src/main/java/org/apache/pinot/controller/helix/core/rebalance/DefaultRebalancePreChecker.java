ab307b305a18d933b970ee56409b8f9f536eb643
J-HowHuang <howardhuang2001@gmail.com>
2025-04-08 14:09:11 -0700

    docs

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/rebalance/DefaultRebalancePreChecker.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/rebalance/DefaultRebalancePreChecker.java
@@ -80,8 +80,8 @@ public class DefaultRebalancePreChecker implements RebalancePreChecker {
 
     tableRebalanceLogger.info("Start pre-checks");
 
-    // If pre-check items are to be done in parallel, we should not use linked hash map but to sort the result in the
-    // end
+    // Right now pre-check items are done sequentially. If pre-check items are to be done in parallel, we should not
+    // use linked hash map but to sort the result in the end
     Map<String, RebalancePreCheckerResult> preCheckResult = new LinkedHashMap<>();
     // Check for reload status
     preCheckResult.put(NEEDS_RELOAD_STATUS, checkReloadNeededOnServers(rebalanceJobId, tableNameWithType,
