b3283d5fea75e9fdcf0d84b617a12cad01ec54e0
Yash Mayya <yash.mayya@gmail.com>
2025-04-11 18:37:31 +0530

    Add null handling support for MV aggregation functions - COUNT, MIN, MAX, SUM, AVG, MINMAXRANGE

--- pinot-core/src/main/java/org/apache/pinot/core/query/aggregation/function/MinMVAggregationFunction.java
+++ pinot-core/src/main/java/org/apache/pinot/core/query/aggregation/function/MinMVAggregationFunction.java
@@ -29,8 +29,8 @@ import org.apache.pinot.segment.spi.AggregationFunctionType;
 
 public class MinMVAggregationFunction extends MinAggregationFunction {
 
-  public MinMVAggregationFunction(List<ExpressionContext> arguments) {
-    super(verifySingleArgument(arguments, "MIN_MV"), false);
+  public MinMVAggregationFunction(List<ExpressionContext> arguments, boolean nullHandlingEnabled) {
+    super(verifySingleArgument(arguments, "MIN_MV"), nullHandlingEnabled);
   }
 
   @Override
@@ -41,48 +41,93 @@ public class MinMVAggregationFunction extends MinAggregationFunction {
   @Override
   public void aggregate(int length, AggregationResultHolder aggregationResultHolder,
       Map<ExpressionContext, BlockValSet> blockValSetMap) {
-    double[][] valuesArray = blockValSetMap.get(_expression).getDoubleValuesMV();
-    double min = aggregationResultHolder.getDoubleResult();
-    for (int i = 0; i < length; i++) {
-      for (double value : valuesArray[i]) {
-        if (value < min) {
-          min = value;
+    BlockValSet blockValSet = blockValSetMap.get(_expression);
+    double[][] valuesArray = blockValSet.getDoubleValuesMV();
+    Double min = foldNotNull(length, blockValSet, null, (acum, from, to) -> {
+      double innerMin = DEFAULT_VALUE;
+      for (int i = from; i < to; i++) {
+        double[] values = valuesArray[i];
+        for (double value : values) {
+          if (value < innerMin) {
+            innerMin = value;
+          }
         }
       }
-    }
-    aggregationResultHolder.setValue(min);
+      return acum == null ? innerMin : Math.min(acum, innerMin);
+    });
+
+    updateAggregationResultHolder(aggregationResultHolder, min);
   }
 
   @Override
   public void aggregateGroupBySV(int length, int[] groupKeyArray, GroupByResultHolder groupByResultHolder,
       Map<ExpressionContext, BlockValSet> blockValSetMap) {
-    double[][] valuesArray = blockValSetMap.get(_expression).getDoubleValuesMV();
-    for (int i = 0; i < length; i++) {
-      int groupKey = groupKeyArray[i];
-      double min = groupByResultHolder.getDoubleResult(groupKey);
-      for (double value : valuesArray[i]) {
-        if (value < min) {
-          min = value;
+    BlockValSet blockValSet = blockValSetMap.get(_expression);
+    double[][] valuesArray = blockValSet.getDoubleValuesMV();
+
+    if (_nullHandlingEnabled) {
+      forEachNotNull(length, blockValSet, (from, to) -> {
+        for (int i = from; i < to; i++) {
+          int groupKey = groupKeyArray[i];
+          Double min = groupByResultHolder.getResult(groupKey);
+          for (double value : valuesArray[i]) {
+            if (min == null || value < min) {
+              min = value;
+            }
+          }
+          groupByResultHolder.setValueForKey(groupKey, min);
         }
+      });
+    } else {
+      for (int i = 0; i < length; i++) {
+        int groupKey = groupKeyArray[i];
+        double min = groupByResultHolder.getDoubleResult(groupKey);
+        for (double value : valuesArray[i]) {
+          if (value < min) {
+            min = value;
+          }
+        }
+        groupByResultHolder.setValueForKey(groupKey, min);
       }
-      groupByResultHolder.setValueForKey(groupKey, min);
     }
   }
 
   @Override
   public void aggregateGroupByMV(int length, int[][] groupKeysArray, GroupByResultHolder groupByResultHolder,
       Map<ExpressionContext, BlockValSet> blockValSetMap) {
-    double[][] valuesArray = blockValSetMap.get(_expression).getDoubleValuesMV();
-    for (int i = 0; i < length; i++) {
-      double[] values = valuesArray[i];
-      for (int groupKey : groupKeysArray[i]) {
-        double min = groupByResultHolder.getDoubleResult(groupKey);
-        for (double value : values) {
-          if (value < min) {
-            min = value;
+    BlockValSet blockValSet = blockValSetMap.get(_expression);
+    double[][] valuesArray = blockValSet.getDoubleValuesMV();
+
+    if (_nullHandlingEnabled) {
+      forEachNotNull(length, blockValSet, (from, to) -> {
+        for (int i = from; i < to; i++) {
+          Double min = null;
+          for (double value : valuesArray[i]) {
+            if (min == null || value < min) {
+              min = value;
+            }
+          }
+
+          for (int groupKey : groupKeysArray[i]) {
+            Double currentMin = groupByResultHolder.getResult(groupKey);
+            if (currentMin == null || (min != null && min < currentMin)) {
+              groupByResultHolder.setValueForKey(groupKey, min);
+            }
           }
         }
-        groupByResultHolder.setValueForKey(groupKey, min);
+      });
+    } else {
+      for (int i = 0; i < length; i++) {
+        double[] values = valuesArray[i];
+        for (int groupKey : groupKeysArray[i]) {
+          double min = groupByResultHolder.getDoubleResult(groupKey);
+          for (double value : values) {
+            if (value < min) {
+              min = value;
+            }
+          }
+          groupByResultHolder.setValueForKey(groupKey, min);
+        }
       }
     }
   }
