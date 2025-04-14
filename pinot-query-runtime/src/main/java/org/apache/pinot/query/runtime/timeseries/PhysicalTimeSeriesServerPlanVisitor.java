9ddec58079f36496e686ad4d525c2720ea60c1d0
Rekha Seethamraju <rekhas@uber.com>
2025-04-14 10:18:41 -0700

    Prevent NPE when query options is null

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/timeseries/PhysicalTimeSeriesServerPlanVisitor.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/timeseries/PhysicalTimeSeriesServerPlanVisitor.java
@@ -24,6 +24,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.stream.Collectors;
 import org.apache.pinot.common.metrics.ServerMetrics;
