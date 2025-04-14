a69227267af572c889946cd116869f6817b10590
J-HowHuang <howardhuang2001@gmail.com>
2025-04-14 11:00:48 -0700

    Add new metric tableRebalanceJobDeletingProgressPercent

--- pinot-common/src/main/java/org/apache/pinot/common/metrics/ControllerGauge.java
+++ pinot-common/src/main/java/org/apache/pinot/common/metrics/ControllerGauge.java
@@ -219,7 +219,8 @@ public enum ControllerGauge implements AbstractMetrics.Gauge {
   DEEP_STORE_WRITE_OPS_IN_PROGRESS("deepStoreWriteOpsInProgress", true),
 
   // The progress of a certain table rebalance job of a table
-  TABLE_REBALANCE_JOB_PROGRESS_PERCENT("percent", false);
+  TABLE_REBALANCE_JOB_ADDING_PROGRESS_PERCENT("percent", false),
+  TABLE_REBALANCE_JOB_DELETING_PROGRESS_PERCENT("percent", false);
 
 
   private final String _gaugeName;
