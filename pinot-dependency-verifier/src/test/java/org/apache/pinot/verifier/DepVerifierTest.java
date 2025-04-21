35abf03efb0d245bee4e02d05dec1915f61d7a55
eujlee <leujean02@gmail.com>
2025-04-21 11:31:03 -0700

    DepVerifier test hardcoded version within POM

---
+++ pinot-dependency-verifier/src/test/java/org/apache/pinot/verifier/DepVerifierTest.java
@@ -0,0 +1,58 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.verifier;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepVerifierTest {
+
+  @Test
+  public void testDetectsHardcodedVersionInRootPom() throws Exception {
+    InputStream inputStream = getClass().getClassLoader()
+        .getResourceAsStream("test-root-pom-hardcoded.xml");
+
+    Assert.assertNotNull(inputStream, "Test file not found");
+
+    boolean foundHardcoded = false;
+    int lineNumber = 0;
+    List<Integer> lineNumberList = new ArrayList<>();
+
+    try (BufferedReader reader = new BufferedReader(
+        new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        lineNumber++;
+        if (line.contains("<version>") && DepVerifier.isHardcoded(line)) {
+          System.out.println("Hardcoded version at line " + lineNumber + ": " + line.trim());
+          foundHardcoded = true;
+        }
+      }
+    }
+
+    Assert.assertFalse(foundHardcoded, "Hardcoded version detected in the root POM file.");
+  }
+}
