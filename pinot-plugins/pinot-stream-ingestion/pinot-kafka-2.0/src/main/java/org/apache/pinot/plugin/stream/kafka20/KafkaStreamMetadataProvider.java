4a9bebfc5fa9244b6753bcbdb4db66c6a74a122a
KKCorps <kartik@startee.ai>
2025-04-25 16:57:58 +0530

    Fix tests

--- pinot-plugins/pinot-stream-ingestion/pinot-kafka-2.0/src/main/java/org/apache/pinot/plugin/stream/kafka20/KafkaStreamMetadataProvider.java
+++ pinot-plugins/pinot-stream-ingestion/pinot-kafka-2.0/src/main/java/org/apache/pinot/plugin/stream/kafka20/KafkaStreamMetadataProvider.java
@@ -120,7 +120,8 @@ public class KafkaStreamMetadataProvider extends KafkaPartitionLevelConnectionHa
       ListOffsetsResult result = _adminClient.listOffsets(request);
       Map<TopicPartition, ListOffsetsResult.ListOffsetsResultInfo> offsets =
           result.all().get(timeoutMillis, TimeUnit.MILLISECONDS);
-      if (offsets == null || offsets.isEmpty()) {
+      if (offsets == null || offsets.isEmpty() || !offsets.containsKey(_topicPartition)
+          || offsets.get(_topicPartition).offset() < 0) {
         // fetch endOffsets as fallback
         request.put(_topicPartition, OffsetSpec.latest());
         result = _adminClient.listOffsets(request);
