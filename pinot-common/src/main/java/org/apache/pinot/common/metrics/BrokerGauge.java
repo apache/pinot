b268bee4756037f77d157fc66984aae0b84ed898
Xiang Fu <xiangfu.1024@gmail.com>
2025-04-27 01:15:07 -0700

    Update BrokerGauge.java

--- pinot-common/src/main/java/org/apache/pinot/common/metrics/BrokerGauge.java
+++ pinot-common/src/main/java/org/apache/pinot/common/metrics/BrokerGauge.java
@@ -87,7 +87,7 @@ public enum BrokerGauge implements AbstractMetrics.Gauge {
   GRPC_NETTY_POOLED_CACHE_SIZE_SMALL("bytes", true),
   GRPC_NETTY_POOLED_CACHE_SIZE_NORMAL("bytes", true),
   GRPC_NETTY_POOLED_THREADLOCALCACHE("bytes", true),
-  GRPC_NETTY_POOLED_CHUNK_SIZE("bytes", true);
+  GRPC_NETTY_POOLED_CHUNK_SIZE("bytes", true),
 
   // GrpcMailboxServer memory metrics
   MAILBOX_SERVER_USED_DIRECT_MEMORY("bytes", true),
