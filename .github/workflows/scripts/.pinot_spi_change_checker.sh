387585365b415cb62163673ae26b311c1be3ef4b
Matthew Jack <mjack@linkedin.com>
2025-04-29 15:22:52 -0700

    Revert "delete gitdiffchecker code and references, update spi pom with japicmp plugin and configuration, include baseline .jar file to be utilized in japicmp comparisons"
    This reverts commit b59f0ca85374f972aa8e0029367d2304e026e8bd.

---
+++ .github/workflows/scripts/.pinot_spi_change_checker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -x
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+FILES_TO_CHECK=("pinot-spi/src/main/java/org/apache/pinot/spi/metrics/PinotMetricsRegistry.java" "pinot-spi/src/main/java/org/apache/pinot/spi/config/table/TableConfig.java")
+len_arr="${#FILES_TO_CHECK[@]}"
+javac -d pinot-spi-change-checker/target/classes pinot-spi-change-checker/src/main/java/org/apache/pinot/changecheck/GitDiffChecker.java
+
+for ((i=0; i < len_arr; i++)); do
+  DIFF=$(git diff origin/master -- "${FILES_TO_CHECK[i]}")
+  echo "$DIFF" > pinot-spi-change-checker/temp_diff_file.txt
+  CONC=$(java -cp pinot-spi-change-checker/target/classes org.apache.pinot.changecheck.GitDiffChecker pinot-spi-change-checker/temp_diff_file.txt)
+  rm pinot-spi-change-checker/temp_diff_file.txt
+  if [[ "$CONC" != "0" ]]; then
+    echo "Incorrect SPI change found in ${FILES_TO_CHECK[i]} at '$CONC'."
+    exit 1
+  fi
+done
+
+rm -rf pinot-spi-change-checker/target/
+echo "No incorrect SPI changes found!"
---
+++ pinot-spi-change-checker/src/main/java/org/apache/pinot/changecheck/GitDiffChecker.java
@@ -0,0 +1,46 @@
+package org.apache.pinot.changecheck;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * method name should not change
+ * method should not be deleted
+ * public API should not become private
+ * method return type should not change
+ * method return type annotation should not change
+ * arguments should remain the same in count and type
+ */
+
+public class GitDiffChecker {
+
+  public static String findDiff(String fileName) throws IOException {
+    LineNumberReader br = new LineNumberReader(new FileReader(fileName));
+    String li;
+    int[] lineNumberCalc = new int[2];
+    Pattern funcDef = Pattern.compile("^\\s*?.+?(.*?)[^{}]*?[{|;]");
+    Pattern annoDef = Pattern.compile("^\\s*?@\\S+?$");
+    while ((li = br.readLine()) != null) {
+      if (li.startsWith("@@")) {
+        // get the line number from code file
+        // and the current line number in the git diff file
+        lineNumberCalc[0] = Integer.parseInt(li.substring(4, li.indexOf(',')));
+        lineNumberCalc[1] = br.getLineNumber();
+      } else if ((!li.isEmpty()) && (li.charAt(0) == '-') && (!li.startsWith("---"))) {
+        Matcher matcher1 = funcDef.matcher(li.substring(1)); //gets rid of the '-' at the beginning
+        Matcher matcher2 = annoDef.matcher(li.substring(1));
+        if (matcher1.matches() || matcher2.matches()) {
+          return " line " + (lineNumberCalc[0] + br.getLineNumber() - lineNumberCalc[1] - 1) + "in the original file: " + li.substring(1).trim();
+        }
+      }
+    }
+    return "0";
+  }
+
+  public static void main(String[] args) throws IOException {
+    System.out.println(findDiff(args[0]));
+  }
+}
