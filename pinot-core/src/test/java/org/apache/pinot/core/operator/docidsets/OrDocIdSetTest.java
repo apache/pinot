apache/pinot

commit 8b88697e739127ab079658b5371b65c666c8d907

Fix the ConcurrentModificationException for And/Or DocIdSet (#12611)

pinot-core/src/main/java/org/apache/pinot/core/operator/docidsets/OrDocIdSet.java
@@ -19,8 +19,8 @@
 package org.apache.pinot.core.operator.docidsets;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import org.apache.pinot.core.common.BlockDocIdIterator;
 import org.apache.pinot.core.common.BlockDocIdSet;
 import org.apache.pinot.core.operator.dociditerators.BitmapBasedDocIdIterator;
@@ -48,12 +48,14 @@ import org.roaringbitmap.buffer.MutableRoaringBitmap;
  * </ul>
  */
 public final class OrDocIdSet implements BlockDocIdSet {
-  private final List<BlockDocIdSet> _docIdSets;
+  // Keep the scan based BlockDocIdSets to be accessed when collecting query execution stats
+  private final AtomicReference<List<BlockDocIdSet>> _scanBasedDocIdSets = new AtomicReference<>();
   private final int _numDocs;
-  private long _numEntriesScannedInFilter = 0L;
+  private List<BlockDocIdSet> _docIdSets;
+  private volatile long _numEntriesScannedInFilter = 0L;
 
   public OrDocIdSet(List<BlockDocIdSet> docIdSets, int numDocs) {
-    _docIdSets = docIdSets instanceof ArrayList ? docIdSets : new ArrayList<>(docIdSets);
+    _docIdSets = docIdSets;
     _numDocs = numDocs;
   }
 
@@ -64,30 +66,29 @@ public final class OrDocIdSet implements BlockDocIdSet {
     List<SortedDocIdIterator> sortedDocIdIterators = new ArrayList<>();
     List<BitmapBasedDocIdIterator> bitmapBasedDocIdIterators = new ArrayList<>();
     List<BlockDocIdIterator> remainingDocIdIterators = new ArrayList<>();
+    long numEntriesScannedForNonScanBasedDocIdSets = 0L;
+    List<BlockDocIdSet> scanBasedDocIdSets = new ArrayList<>();
 
-    Iterator<BlockDocIdSet> iterator = _docIdSets.iterator();
-    for (int i = 0; iterator.hasNext(); i++) {
-      BlockDocIdSet blockDocIdSet = iterator.next();
-      BlockDocIdIterator docIdIterator = blockDocIdSet.iterator();
+    for (int i = 0; i < numDocIdSets; i++) {
+      BlockDocIdSet docIdSet = _docIdSets.get(i);
+      BlockDocIdIterator docIdIterator = docIdSet.iterator();
       allDocIdIterators[i] = docIdIterator;
       if (docIdIterator instanceof SortedDocIdIterator) {
         sortedDocIdIterators.add((SortedDocIdIterator) docIdIterator);
-        // aggregate the number of entries scanned in filter before removing the iterator
-        _numEntriesScannedInFilter += blockDocIdSet.getNumEntriesScannedInFilter();
-        // do not keep holding on to the _docIdRanges since they will occupy heap space during the query execution
-        iterator.remove();
+        numEntriesScannedForNonScanBasedDocIdSets += docIdSet.getNumEntriesScannedInFilter();
       } else if (docIdIterator instanceof BitmapBasedDocIdIterator) {
-        bitmapBasedDocIdIterators.add((BitmapBasedDocIdIterator) docIdIterator);
-        // aggregate the number of entries scanned in filter before removing the iterator
-        // some BitmapBasedDocIdIterator may be generated from underlying index types (e.g. H3Index) that actually
-        // scans documents, so we need to aggregate them here
-        _numEntriesScannedInFilter += blockDocIdSet.getNumEntriesScannedInFilter();
-        // do not keep holding on to the bitmaps since they will occupy heap space during the query execution
-        iterator.remove();
+        numEntriesScannedForNonScanBasedDocIdSets += docIdSet.getNumEntriesScannedInFilter();
       } else {
         remainingDocIdIterators.add(docIdIterator);
+        scanBasedDocIdSets.add(docIdSet);
       }
     }
+
+    // Set _docIdSets to null so that underlying BlockDocIdSets can be garbage collected
+    _docIdSets = null;
+    _numEntriesScannedInFilter = numEntriesScannedForNonScanBasedDocIdSets;
+    _scanBasedDocIdSets.set(scanBasedDocIdSets);
+
     int numSortedDocIdIterators = sortedDocIdIterators.size();
     int numBitmapBasedDocIdIterators = bitmapBasedDocIdIterators.size();
     if (numSortedDocIdIterators + numBitmapBasedDocIdIterators > 1) {
@@ -127,9 +128,13 @@ public final class OrDocIdSet implements BlockDocIdSet {
 
   @Override
   public long getNumEntriesScannedInFilter() {
-    for (BlockDocIdSet docIdSet : _docIdSets) {
-      _numEntriesScannedInFilter += docIdSet.getNumEntriesScannedInFilter();
+    List<BlockDocIdSet> scanBasedDocIdSets = _scanBasedDocIdSets.get();
+    long numEntriesScannedForScanBasedDocIdSets = 0L;
+    if (scanBasedDocIdSets != null) {
+      for (BlockDocIdSet scanBasedDocIdSet : scanBasedDocIdSets) {
+        numEntriesScannedForScanBasedDocIdSets += scanBasedDocIdSet.getNumEntriesScannedInFilter();
+      }
     }
-    return _numEntriesScannedInFilter;
+    return _numEntriesScannedInFilter + numEntriesScannedForScanBasedDocIdSets;
   }
 }

pinot-core/src/main/java/org/apache/pinot/core/operator/docidsets/AndDocIdSet.java
@@ -20,11 +20,10 @@ package org.apache.pinot.core.operator.docidsets;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 import javax.annotation.Nullable;
-import org.apache.commons.collections.MapUtils;
 import org.apache.pinot.common.utils.config.QueryOptionsUtils;
 import org.apache.pinot.core.common.BlockDocIdIterator;
 import org.apache.pinot.core.common.BlockDocIdSet;
@@ -57,14 +56,16 @@ import org.roaringbitmap.buffer.MutableRoaringBitmap;
  * </ul>
  */
 public final class AndDocIdSet implements BlockDocIdSet {
-  private final List<BlockDocIdSet> _docIdSets;
+  // Keep the scan based BlockDocIdSets to be accessed when collecting query execution stats
+  private final AtomicReference<List<BlockDocIdSet>> _scanBasedDocIdSets = new AtomicReference<>();
   private final boolean _cardinalityBasedRankingForScan;
-  private long _numEntriesScannedInFilter = 0L;
+  private List<BlockDocIdSet> _docIdSets;
+  private volatile long _numEntriesScannedInFilter;
 
   public AndDocIdSet(List<BlockDocIdSet> docIdSets, @Nullable Map<String, String> queryOptions) {
-    _docIdSets = docIdSets instanceof ArrayList ? docIdSets : new ArrayList<>(docIdSets);
+    _docIdSets = docIdSets;
     _cardinalityBasedRankingForScan =
-        !MapUtils.isEmpty(queryOptions) && QueryOptionsUtils.isAndScanReorderingEnabled(queryOptions);
+        queryOptions != null && QueryOptionsUtils.isAndScanReorderingEnabled(queryOptions);
   }
 
   @Override
@@ -77,32 +78,32 @@ public final class AndDocIdSet implements BlockDocIdSet {
     List<BitmapBasedDocIdIterator> bitmapBasedDocIdIterators = new ArrayList<>();
     List<ScanBasedDocIdIterator> scanBasedDocIdIterators = new ArrayList<>();
     List<BlockDocIdIterator> remainingDocIdIterators = new ArrayList<>();
+    long numEntriesScannedForNonScanBasedDocIdSets = 0L;
+    List<BlockDocIdSet> scanBasedDocIdSets = new ArrayList<>();
 
-    Iterator<BlockDocIdSet> iterator = _docIdSets.iterator();
-    for (int i = 0; iterator.hasNext(); i++) {
-      BlockDocIdSet blockDocIdSet = iterator.next();
-      BlockDocIdIterator docIdIterator = blockDocIdSet.iterator();
+    for (int i = 0; i < numDocIdSets; i++) {
+      BlockDocIdSet docIdSet = _docIdSets.get(i);
+      BlockDocIdIterator docIdIterator = docIdSet.iterator();
       allDocIdIterators[i] = docIdIterator;
       if (docIdIterator instanceof SortedDocIdIterator) {
         sortedDocIdIterators.add((SortedDocIdIterator) docIdIterator);
-        // aggregate the number of entries scanned in filter before removing the iterator
-        _numEntriesScannedInFilter += blockDocIdSet.getNumEntriesScannedInFilter();
-        // do not keep holding on to the _docIdRanges since they will occupy heap space during the query execution
-        iterator.remove();
+        numEntriesScannedForNonScanBasedDocIdSets += docIdSet.getNumEntriesScannedInFilter();
       } else if (docIdIterator instanceof BitmapBasedDocIdIterator) {
         bitmapBasedDocIdIterators.add((BitmapBasedDocIdIterator) docIdIterator);
-        // aggregate the number of entries scanned in filter before removing the iterator
-        // some BitmapBasedDocIdIterator may be generated from underlying index types (e.g. H3Index) that actually
-        // scans documents, so we need to aggregate them here
-        _numEntriesScannedInFilter += blockDocIdSet.getNumEntriesScannedInFilter();
-        // do not keep holding on to the _docIdRanges since they will occupy heap space during the query execution
-        iterator.remove();
+        numEntriesScannedForNonScanBasedDocIdSets += docIdSet.getNumEntriesScannedInFilter();
       } else if (docIdIterator instanceof ScanBasedDocIdIterator) {
         scanBasedDocIdIterators.add((ScanBasedDocIdIterator) docIdIterator);
+        scanBasedDocIdSets.add(docIdSet);
       } else {
         remainingDocIdIterators.add(docIdIterator);
+        scanBasedDocIdSets.add(docIdSet);
       }
     }
+
+    // Set _docIdSets to null so that underlying BlockDocIdSets can be garbage collected
+    _docIdSets = null;
+    _numEntriesScannedInFilter = numEntriesScannedForNonScanBasedDocIdSets;
+    _scanBasedDocIdSets.set(scanBasedDocIdSets);
 
     // evaluate the bitmaps in the order of the lowest matching num docIds comes first, so that we minimize the number
     // of containers (range) for comparison from the beginning, as will minimize the effort of bitmap AND application
@@ -186,9 +187,13 @@ public final class AndDocIdSet implements BlockDocIdSet {
 
   @Override
   public long getNumEntriesScannedInFilter() {
-    for (BlockDocIdSet child : _docIdSets) {
-      _numEntriesScannedInFilter += child.getNumEntriesScannedInFilter();
+    List<BlockDocIdSet> scanBasedDocIdSets = _scanBasedDocIdSets.get();
+    long numEntriesScannedForScanBasedDocIdSets = 0L;
+    if (scanBasedDocIdSets != null) {
+      for (BlockDocIdSet scanBasedDocIdSet : scanBasedDocIdSets) {
+        numEntriesScannedForScanBasedDocIdSets += scanBasedDocIdSet.getNumEntriesScannedInFilter();
+      }
     }
-    return _numEntriesScannedInFilter;
+    return _numEntriesScannedInFilter + numEntriesScannedForScanBasedDocIdSets;
   }
 }

commit 0f9780a0fa3036253ec5ea4d603a59575bd388c0

Fix Bitmap Filter Execution Order in OrDocIdSet (#15756)
* Add docIdIterator to bitmapBasedDocIdIterators when docIdIterator instanceof BitmapBasedDocIdIterator. Fixes bug introduced in #12611

* Adding UTs

* fix checkstyle

pinot-core/src/main/java/org/apache/pinot/core/operator/docidsets/OrDocIdSet.java
@@ -77,6 +77,7 @@ public final class OrDocIdSet implements BlockDocIdSet {
         sortedDocIdIterators.add((SortedDocIdIterator) docIdIterator);
         numEntriesScannedForNonScanBasedDocIdSets += docIdSet.getNumEntriesScannedInFilter();
       } else if (docIdIterator instanceof BitmapBasedDocIdIterator) {
+        bitmapBasedDocIdIterators.add((BitmapBasedDocIdIterator) docIdIterator);
         numEntriesScannedForNonScanBasedDocIdSets += docIdSet.getNumEntriesScannedInFilter();
       } else {
         remainingDocIdIterators.add(docIdIterator);

pinot-core/src/test/java/org/apache/pinot/core/operator/docidsets/OrDocIdSetTest.java
@@ -1,0 +1,137 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.core.operator.docidsets;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.apache.pinot.core.common.BlockDocIdIterator;
+import org.apache.pinot.core.common.BlockDocIdSet;
+import org.apache.pinot.core.operator.dociditerators.BitmapDocIdIterator;
+import org.apache.pinot.core.operator.dociditerators.OrDocIdIterator;
+import org.apache.pinot.core.operator.dociditerators.SVScanDocIdIterator;
+import org.apache.pinot.core.operator.dociditerators.SortedDocIdIterator;
+import org.apache.pinot.spi.utils.Pairs;
+import org.roaringbitmap.buffer.MutableRoaringBitmap;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+
+public class OrDocIdSetTest {
+    @Test
+    public void iteratorReturnsBitmapDocIdIteratorWhenOnlyIndexBasedIteratorsExist() {
+        // All the idsets are index-base BlockDocIdIterator (SortedDocIdIterator or BitmapBasedDocIdIterator)
+        SortedDocIdSet sortedDocIdSet = mock(SortedDocIdSet.class);
+        SortedDocIdIterator sortedIterator = mock(SortedDocIdIterator.class);
+        BitmapDocIdSet bitmapDocIdSet = mock(BitmapDocIdSet.class);
+        BitmapDocIdIterator bitmapIterator = mock(BitmapDocIdIterator.class);
+
+        when(sortedDocIdSet.iterator()).thenReturn(sortedIterator);
+        when(bitmapDocIdSet.iterator()).thenReturn(bitmapIterator);
+        when(sortedIterator.getDocIdRanges()).thenReturn(Collections.singletonList(new Pairs.IntPair(1, 10)));
+        when(bitmapIterator.getDocIds()).thenReturn(new MutableRoaringBitmap());
+
+        List<BlockDocIdSet> docIdSets = Arrays.asList(sortedDocIdSet, bitmapDocIdSet);
+        OrDocIdSet orDocIdSet = new OrDocIdSet(docIdSets, 100);
+        BlockDocIdIterator iterator = orDocIdSet.iterator();
+        // Make sure the returned iterator is a BitmapDocIdIterator instead of an OrDocIdIterator.
+        assertTrue(iterator instanceof BitmapDocIdIterator);
+        assertFalse(iterator instanceof OrDocIdIterator);
+    }
+
+    @Test
+    public void iteratorReturnsBitmapDocIdIteratorWhenBitmapBasedDocIdIterators() {
+        BitmapDocIdSet bitmapDocIdSet1 = mock(BitmapDocIdSet.class);
+        BitmapDocIdSet bitmapDocIdSet2 = mock(BitmapDocIdSet.class);
+        BitmapDocIdIterator bitmapIterator1 = mock(BitmapDocIdIterator.class);
+        BitmapDocIdIterator bitmapIterator2 = mock(BitmapDocIdIterator.class);
+
+        when(bitmapDocIdSet1.iterator()).thenReturn(bitmapIterator1);
+        when(bitmapDocIdSet2.iterator()).thenReturn(bitmapIterator2);
+        when(bitmapIterator1.getDocIds()).thenReturn(new MutableRoaringBitmap());
+        when(bitmapIterator2.getDocIds()).thenReturn(new MutableRoaringBitmap());
+
+        List<BlockDocIdSet> docIdSets = Arrays.asList(bitmapDocIdSet1, bitmapDocIdSet2);
+        OrDocIdSet orDocIdSet = new OrDocIdSet(docIdSets, 100);
+        BlockDocIdIterator iterator = orDocIdSet.iterator();
+        assertTrue(iterator instanceof BitmapDocIdIterator);
+        assertFalse(iterator instanceof OrDocIdIterator);
+    }
+
+    @Test
+    public void iteratorReturnsBitmapDocIdIteratorWhenBitmapAndSortedDocIdIterators() {
+        SortedDocIdSet sortedDocIdSet = mock(SortedDocIdSet.class);
+        BitmapDocIdSet bitmapDocIdSet = mock(BitmapDocIdSet.class);
+        SortedDocIdIterator sortedIterator = mock(SortedDocIdIterator.class);
+        BitmapDocIdIterator bitmapIterator = mock(BitmapDocIdIterator.class);
+
+        when(sortedDocIdSet.iterator()).thenReturn(sortedIterator);
+        when(bitmapDocIdSet.iterator()).thenReturn(bitmapIterator);
+        when(sortedIterator.getDocIdRanges()).thenReturn(Collections.singletonList(new Pairs.IntPair(1, 5)));
+        when(bitmapIterator.getDocIds()).thenReturn(new MutableRoaringBitmap());
+
+        List<BlockDocIdSet> docIdSets = Arrays.asList(sortedDocIdSet, bitmapDocIdSet);
+        OrDocIdSet orDocIdSet = new OrDocIdSet(docIdSets, 100);
+        BlockDocIdIterator iterator = orDocIdSet.iterator();
+        assertTrue(iterator instanceof BitmapDocIdIterator);
+    }
+
+    @Test
+    public void iteratorReturnsOrDocIdIteratorWhenAtLeastOneScanBasedExist() {
+        BitmapDocIdSet bitmapDocIdSet = mock(BitmapDocIdSet.class);
+        SVScanDocIdSet svScanDocIdSet = mock(SVScanDocIdSet.class);
+        BitmapDocIdIterator bitmapIterator = mock(BitmapDocIdIterator.class);
+        SVScanDocIdIterator svScanDocIdIterator = mock(SVScanDocIdIterator.class);
+
+        when(bitmapDocIdSet.iterator()).thenReturn(bitmapIterator);
+        when(svScanDocIdSet.iterator()).thenReturn(svScanDocIdIterator);
+
+        when(bitmapIterator.getDocIds()).thenReturn(new MutableRoaringBitmap());
+
+        List<BlockDocIdSet> docIdSets = Arrays.asList(bitmapDocIdSet, svScanDocIdSet);
+        OrDocIdSet orDocIdSet = new OrDocIdSet(docIdSets, 100);
+        BlockDocIdIterator iterator = orDocIdSet.iterator();
+        assertTrue(iterator instanceof OrDocIdIterator);
+    }
+
+    @Test
+    public void iteratorReturnsOrDocIdIteratorWhenAtLeastOneScanBasedExist2() {
+        BitmapDocIdSet bitmapDocIdSet = mock(BitmapDocIdSet.class);
+        SortedDocIdSet sortedDocIdIterator = mock(SortedDocIdSet.class);
+        SVScanDocIdSet svScanDocIdSet = mock(SVScanDocIdSet.class);
+        SortedDocIdIterator sortedIterator = mock(SortedDocIdIterator.class);
+        BitmapDocIdIterator bitmapIterator = mock(BitmapDocIdIterator.class);
+        SVScanDocIdIterator svScanDocIdIterator = mock(SVScanDocIdIterator.class);
+
+        when(bitmapDocIdSet.iterator()).thenReturn(bitmapIterator);
+        when(sortedDocIdIterator.iterator()).thenReturn(sortedIterator);
+        when(svScanDocIdSet.iterator()).thenReturn(svScanDocIdIterator);
+        when(sortedIterator.getDocIdRanges()).thenReturn(Collections.singletonList(new Pairs.IntPair(1, 5)));
+        when(bitmapIterator.getDocIds()).thenReturn(new MutableRoaringBitmap());
+
+        List<BlockDocIdSet> docIdSets = Arrays.asList(bitmapDocIdSet, sortedDocIdIterator, svScanDocIdSet);
+        OrDocIdSet orDocIdSet = new OrDocIdSet(docIdSets, 100);
+        BlockDocIdIterator iterator = orDocIdSet.iterator();
+        assertTrue(iterator instanceof OrDocIdIterator);
+    }
+}