28ba274a8b215daa94c582c5bd5284d614646954
RAGHVENDRA KUMAR YADAV <raghavmnnit@gmail.com>
2025-05-22 13:40:33 -0700

    Incorporating the comment.

--- pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/V1Constants.java
+++ pinot-segment-spi/src/main/java/org/apache/pinot/segment/spi/V1Constants.java
@@ -42,7 +42,6 @@ public class V1Constants {
     public static final String RAW_SV_FORWARD_INDEX_FILE_EXTENSION = ".sv.raw.fwd";
     public static final String RAW_MV_FORWARD_INDEX_FILE_EXTENSION = ".mv.raw.fwd";
     public static final String UNSORTED_MV_FORWARD_INDEX_FILE_EXTENSION = ".mv.fwd";
-    public static final String MAP_FORWARD_INDEX_FILE_EXTENSION = ".map.fwd";
     public static final String BITMAP_INVERTED_INDEX_FILE_EXTENSION = ".bitmap.inv";
     public static final String BITMAP_RANGE_INDEX_FILE_EXTENSION = ".bitmap.range";
     public static final String JSON_INDEX_FILE_EXTENSION = ".json.idx";
