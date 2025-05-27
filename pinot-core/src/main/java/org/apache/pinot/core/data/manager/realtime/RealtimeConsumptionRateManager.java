5b2925d5d1cc7b152a650f02b3884f52cbad1310
Harnoor7 <harnoor@startree.ai>
2025-05-27 15:49:35 +0530

    Handles Server Rate Limit config change without restart

--- pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/RealtimeConsumptionRateManager.java
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/RealtimeConsumptionRateManager.java
@@ -59,7 +59,7 @@ public class RealtimeConsumptionRateManager {
 
   private static final String SERVER_CONSUMPTION_RATE_METRIC_KEY_NAME =
       ServerMeter.REALTIME_ROWS_CONSUMED.getMeterName();
-  private ConsumptionRateLimiter _serverRateLimiter = NOOP_RATE_LIMITER;
+  private volatile ConsumptionRateLimiter _serverRateLimiter = NOOP_RATE_LIMITER;
 
   // stream config object is required for fetching the partition count from the stream
   private final LoadingCache<StreamConfig, Integer> _streamConfigToTopicPartitionCountMap;
@@ -87,15 +87,23 @@ public class RealtimeConsumptionRateManager {
     double serverRateLimit =
         serverConfig.getProperty(CommonConstants.Server.CONFIG_OF_SERVER_CONSUMPTION_RATE_LIMIT,
             CommonConstants.Server.DEFAULT_SERVER_CONSUMPTION_RATE_LIMIT);
+    _serverRateLimiter = createServerRateLimiter(serverRateLimit, serverMetrics);
+    return _serverRateLimiter;
+  }
+
+  public void updateServerRateLimiter(double serverRateLimit, ServerMetrics serverMetrics) {
+    _serverRateLimiter = createServerRateLimiter(serverRateLimit, serverMetrics);
+  }
+
+  private ConsumptionRateLimiter createServerRateLimiter(double serverRateLimit, ServerMetrics serverMetrics) {
     if (serverRateLimit > 0) {
       LOGGER.info("Set up ConsumptionRateLimiter with rate limit: {}", serverRateLimit);
       MetricEmitter metricEmitter = new MetricEmitter(serverMetrics, SERVER_CONSUMPTION_RATE_METRIC_KEY_NAME);
-      _serverRateLimiter = new RateLimiterImpl(serverRateLimit, metricEmitter);
+      return new RateLimiterImpl(serverRateLimit, metricEmitter);
     } else {
       LOGGER.info("ConsumptionRateLimiter is disabled");
-      _serverRateLimiter = NOOP_RATE_LIMITER;
+      return NOOP_RATE_LIMITER;
     }
-    return _serverRateLimiter;
   }
 
   public ConsumptionRateLimiter getServerRateLimiter() {
---
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/ServerRateLimitConfigChangeListener.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.core.data.manager.realtime;
+
+import java.util.Map;
+import java.util.Set;
+import org.apache.pinot.common.metrics.ServerMetrics;
+import org.apache.pinot.spi.config.provider.PinotClusterConfigChangeListener;
+import org.apache.pinot.spi.utils.CommonConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ServerRateLimitConfigChangeListener implements PinotClusterConfigChangeListener {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ServerRateLimitConfigChangeListener.class);
+  private final ServerMetrics _serverMetrics;
+
+  public ServerRateLimitConfigChangeListener(ServerMetrics serverMetrics) {
+    _serverMetrics = serverMetrics;
+  }
+
+  @Override
+  public void onChange(Set<String> changedConfigs, Map<String, String> clusterConfigs) {
+    if (!changedConfigs.contains(CommonConstants.Server.CONFIG_OF_SERVER_CONSUMPTION_RATE_LIMIT)) {
+      LOGGER.info("ChangedConfigs: {} does not contain: {}. Skipping updates.", changedConfigs,
+          CommonConstants.Server.CONFIG_OF_SERVER_CONSUMPTION_RATE_LIMIT);
+      return;
+    }
+    // Init serverRateLimit as default in-case serverRateLimit config is deleted from cluster configs.
+    double serverRateLimit = CommonConstants.Server.DEFAULT_SERVER_CONSUMPTION_RATE_LIMIT;
+    if (clusterConfigs.containsKey(CommonConstants.Server.CONFIG_OF_SERVER_CONSUMPTION_RATE_LIMIT)) {
+      serverRateLimit =
+          Double.parseDouble(clusterConfigs.get(CommonConstants.Server.CONFIG_OF_SERVER_CONSUMPTION_RATE_LIMIT));
+    }
+    RealtimeConsumptionRateManager.getInstance().updateServerRateLimiter(serverRateLimit, _serverMetrics);
+  }
+}
