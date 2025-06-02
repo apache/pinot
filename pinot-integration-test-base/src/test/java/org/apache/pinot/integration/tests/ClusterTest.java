c3bc06b11202b1b9e5988b0757b0ee39d6794e42
shauryachats <shauryachats@uber.com>
2025-06-02 05:09:51 +0000

    Introducing integration tests for time series engine

--- pinot-integration-test-base/src/test/java/org/apache/pinot/integration/tests/ClusterTest.java
+++ pinot-integration-test-base/src/test/java/org/apache/pinot/integration/tests/ClusterTest.java
@@ -48,6 +48,7 @@ import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.NameValuePair;
 import org.apache.hc.core5.http.message.BasicHeader;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.pinot.broker.broker.helix.BaseBrokerStarter;
 import org.apache.pinot.broker.broker.helix.HelixBrokerStarter;
 import org.apache.pinot.client.ConnectionFactory;
@@ -87,6 +88,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 
 import static org.apache.pinot.integration.tests.ClusterIntegrationTestUtils.getBrokerQueryApiUrl;
+import static org.apache.pinot.integration.tests.ClusterIntegrationTestUtils.getTimeSeriesQueryApiUrl;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
@@ -547,6 +549,31 @@ public abstract class ClusterTest extends ControllerTest {
       return queryBrokerGrpcEndpoint(query);
     }
     return queryBrokerHttpEndpoint(query);
+  }
+
+  /**
+   * Queries the broker's timeseries query endpoint (/timeseries/api/v1/query_range).
+   * This is used for testing timeseries queries.
+   */
+  public JsonNode getTimeseriesQuery(@Language("sql") String query, long startTime, long endTime) {
+    try {
+      Map<String, String> queryParams = Map.of("language", "m3ql", "query", query, "start",
+        String.valueOf(startTime), "end", String.valueOf(endTime));
+      String url = buildQueryUrl(getTimeSeriesQueryApiUrl(getBrokerBaseApiUrl()), queryParams);
+      JsonNode responseJsonNode = JsonUtils.stringToJsonNode(sendGetRequest(url, Map.of()));
+      return sanitizeResponse(responseJsonNode);
+    } catch (Exception e) {
+      throw new RuntimeException("Failed to get timeseries query: " + query, e);
+    }
+  }
+
+  private static String buildQueryUrl(String baseUrl, Map<String, String> params) throws Exception {
+    URIBuilder builder = new URIBuilder(baseUrl);
+    for (Map.Entry<String, String> entry : params.entrySet()) {
+      builder.addParameter(entry.getKey(), entry.getValue());
+    }
+    URI uri = builder.build();
+    return uri.toString();
   }
 
   /**
