617d3b2fec972fb75b5a04b141e615a861ab98d9
Radhika <radhika.patwari@uber.com>
2025-06-16 13:51:12 +0530

    Create table configuration to decide whether to retain original fields during unnest transformation

--- pinot-common/src/test/java/org/apache/pinot/common/utils/config/TableConfigSerDeUtilsTest.java
+++ pinot-common/src/test/java/org/apache/pinot/common/utils/config/TableConfigSerDeUtilsTest.java
@@ -491,6 +491,13 @@ public class TableConfigSerDeUtilsTest {
     assertEquals(ingestionConfig.getStreamIngestionConfig().getStreamConfigMaps().size(), 1);
     assertEquals(ingestionConfig.getStreamIngestionConfig().getParallelSegmentConsumptionPolicy(),
         ParallelSegmentConsumptionPolicy.ALLOW_DURING_BUILD_ONLY);
+    assertNotNull(ingestionConfig.getComplexTypeConfig());
+    assertEquals(ingestionConfig.getComplexTypeConfig().getFieldsToUnnest(), Arrays.asList("c1", "c2"));
+    assertEquals(ingestionConfig.getComplexTypeConfig().getDelimiter(), ".");
+    assertEquals(ingestionConfig.getComplexTypeConfig().getCollectionNotUnnestedToJson(),
+            ComplexTypeConfig.CollectionNotUnnestedToJson.NON_PRIMITIVE);
+    assertEquals(ingestionConfig.getComplexTypeConfig().getPrefixesToRename(), Collections.emptyMap());
+    assertFalse(ingestionConfig.getComplexTypeConfig().shouldRetainOriginalFieldInUnnest());
   }
 
   private void checkTierConfigList(TableConfig tableConfig) {
