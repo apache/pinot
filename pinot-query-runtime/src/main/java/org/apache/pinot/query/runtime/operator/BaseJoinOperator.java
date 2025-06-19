7203786e062f908a66263bba0cad5bca8480c569
Song Fu <131259315+songwdfu@users.noreply.github.com>
2025-06-19 10:02:25 -0700

    Reduce array copying for non-equi join condition evaluation (#16152)

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/operator/BaseJoinOperator.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/operator/BaseJoinOperator.java
@@ -18,6 +18,7 @@
  */
 package org.apache.pinot.query.runtime.operator;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -271,7 +272,11 @@ public abstract class BaseJoinOperator extends MultiStageOperator {
     return resultRow;
   }
 
-  protected boolean matchNonEquiConditions(Object[] row) {
+  protected List<Object> joinRowView(@Nullable Object[] leftRow, @Nullable Object[] rightRow) {
+    return JoinedRowView.of(leftRow, rightRow, _resultColumnSize, _leftColumnSize);
+  }
+
+  protected boolean matchNonEquiConditions(List<Object> row) {
     if (_nonEquiEvaluators.isEmpty()) {
       return true;
     }
@@ -374,6 +379,108 @@ public abstract class BaseJoinOperator extends MultiStageOperator {
     @Override
     public StatMap.Type getType() {
       return _type;
+    }
+  }
+
+  /**
+   * This util class is a view over the left and right row joined together
+   * currently this is used for filtering and input of projection. So if the joined
+   * tuple doesn't pass the predicate, the join result is not materialized into Object[].
+   *
+   * It is debatable whether we always want to use this instead of copying the tuple
+   */
+  private abstract static class JoinedRowView extends AbstractList<Object> implements List<Object> {
+    protected final int _leftSize;
+    protected final int _size;
+
+    protected JoinedRowView(int resultColumnSize, int leftSize) {
+      _leftSize = leftSize;
+      _size = resultColumnSize;
+    }
+
+    private static final class BothNotNullView extends JoinedRowView {
+      private final Object[] _leftRow;
+      private final Object[] _rightRow;
+
+      private BothNotNullView(Object[] leftRow, Object[] rightRow, int resultColumnSize, int leftSize) {
+        super(resultColumnSize, leftSize);
+        _leftRow = leftRow;
+        _rightRow = rightRow;
+      }
+
+      @Override
+      public Object get(int i) {
+        return i < _leftSize ? _leftRow[i] : _rightRow[i - _leftSize];
+      }
+
+      @Override
+      public Object[] toArray() {
+        Object[] resultRow = new Object[_size];
+        System.arraycopy(_leftRow, 0, resultRow, 0, _leftSize);
+        System.arraycopy(_rightRow, 0, resultRow, _leftSize, _rightRow.length);
+        return resultRow;
+      }
+    }
+
+    private static final class RightNotNullView extends JoinedRowView {
+      private final Object[] _rightRow;
+
+      public RightNotNullView(Object[] rightRow, int resultColumnSize, int leftSize) {
+        super(resultColumnSize, leftSize);
+        _rightRow = rightRow;
+      }
+
+      @Override
+      public Object get(int i) {
+        return i < _leftSize ? null : _rightRow[i - _leftSize];
+      }
+
+      @Override
+      public Object[] toArray() {
+        Object[] resultRow = new Object[_size];
+        System.arraycopy(_rightRow, 0, resultRow, _leftSize, _rightRow.length);
+        return resultRow;
+      }
+    }
+
+    private static final class LeftNotNullView extends JoinedRowView {
+      private final Object[] _leftRow;
+
+      public LeftNotNullView(Object[] leftRow, int resultColumnSize, int leftSize) {
+        super(resultColumnSize, leftSize);
+        _leftRow = leftRow;
+      }
+
+      @Override
+      public Object get(int i) {
+        return i < _leftSize ? _leftRow[i] : null;
+      }
+
+      @Override
+      public Object[] toArray() {
+        Object[] resultRow = new Object[_size];
+        System.arraycopy(_leftRow, 0, resultRow, 0, _leftSize);
+        return resultRow;
+      }
+    }
+
+    public static JoinedRowView of(@Nullable Object[] leftRow, @Nullable Object[] rightRow, int resultColumnSize,
+        int leftSize) {
+      if (leftRow == null && rightRow == null) {
+        throw new IllegalStateException("both left and right side of join are null");
+      }
+      if (leftRow == null) {
+        return new RightNotNullView(rightRow, resultColumnSize, leftSize);
+      }
+      if (rightRow == null) {
+        return new LeftNotNullView(leftRow, resultColumnSize, leftSize);
+      }
+      return new BothNotNullView(leftRow, rightRow, resultColumnSize, leftSize);
+    }
+
+    @Override
+    public int size() {
+      return _size;
     }
   }
 }
