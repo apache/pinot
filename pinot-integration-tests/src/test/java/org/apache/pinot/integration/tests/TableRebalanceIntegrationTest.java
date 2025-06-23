78043c18ee15943a3accf303a651782563b24872
J-HowHuang <howardhuang2001@gmail.com>
2025-06-23 15:24:17 -0700

    do cancel rebalance job while cleaning up

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/TableRebalanceIntegrationTest.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/TableRebalanceIntegrationTest.java
@@ -1485,7 +1485,8 @@ public class TableRebalanceIntegrationTest extends BaseHybridClusterIntegrationT
       rebalanceConfig.setDowntime(false);
       rebalanceConfig.setIncludeConsuming(true);
       // notice that this could get an HTTP 409 CONFLICT, when the test failed due to the timeout waiting on the table
-      // to converge, and try to rebalance again here.
+      // to converge, and try to rebalance again here. So we need to cancel the original job first.
+      sendDeleteRequest(StringUtil.join("/", getControllerRequestURLBuilder().getBaseUrl(), "tables", getTableName(), "rebalance") + "?type=" + tableConfig.getTableType().toString());
       String response = sendPostRequest(getRebalanceUrl(rebalanceConfig, TableType.REALTIME));
       RebalanceResult rebalanceResult = JsonUtils.stringToObject(response, RebalanceResult.class);
       waitForRebalanceToComplete(rebalanceResult.getJobId(), 30000);
