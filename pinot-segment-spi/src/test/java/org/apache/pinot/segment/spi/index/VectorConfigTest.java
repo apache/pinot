d170565c719a8ba5e5ddfd80570373fd348fb9ef
Anshul Singh <anshul98ks123@gmail.com>
2025-06-25 02:49:18 +0530

    Add UTs for VectorConfig

---
+++ pinot-segment-spi/src/test/java/org/apache/pinot/segment/spi/index/VectorConfigTest.java
@@ -0,0 +1,127 @@
+package org.apache.pinot.segment.spi.index;
+
+import static org.apache.pinot.segment.spi.index.creator.VectorIndexConfig.VectorDistanceFunction.COSINE;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.Map;
+import org.apache.pinot.segment.spi.index.creator.VectorIndexConfig;
+import org.apache.pinot.spi.utils.JsonUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class VectorConfigTest {
+
+  @Test
+  public void withEmptyConf()
+      throws JsonProcessingException {
+    String confStr = "{}";
+    VectorIndexConfig config = JsonUtils.stringToObject(confStr, VectorIndexConfig.class);
+
+    assertFalse(config.isDisabled(), "Unexpected disabled");
+    assertNull(config.getVectorIndexType(), "Unexpected vectorIndexType");
+    assertNull(config.getVectorDistanceFunction(), "Unexpected vectorDistanceFunction");
+    assertNull(config.getProperties(), "unexpected properties");
+    assertEquals(config.getVectorDimension(), 0);
+    assertEquals(config.getVersion(), 0);
+  }
+
+  @Test
+  public void withDisabledNull()
+      throws JsonProcessingException {
+    String confStr = "{\"disabled\": null}";
+    VectorIndexConfig config = JsonUtils.stringToObject(confStr, VectorIndexConfig.class);
+
+    assertFalse(config.isDisabled(), "Unexpected disabled");
+    assertNull(config.getVectorIndexType(), "Unexpected vectorIndexType");
+    assertNull(config.getVectorDistanceFunction(), "Unexpected vectorDistanceFunction");
+    assertNull(config.getProperties(), "unexpected properties");
+    assertEquals(config.getVectorDimension(), 0);
+    assertEquals(config.getVersion(), 0);
+  }
+
+  @Test
+  public void withDisabledFalse()
+      throws JsonProcessingException {
+    String confStr = "{\"disabled\": false}";
+    VectorIndexConfig config = JsonUtils.stringToObject(confStr, VectorIndexConfig.class);
+
+    assertFalse(config.isDisabled(), "Unexpected disabled");
+    assertNull(config.getVectorIndexType(), "Unexpected vectorIndexType");
+    assertNull(config.getVectorDistanceFunction(), "Unexpected vectorDistanceFunction");
+    assertNull(config.getProperties(), "unexpected properties");
+    assertEquals(config.getVectorDimension(), 0);
+    assertEquals(config.getVersion(), 0);
+  }
+
+  @Test
+  public void withDisabledTrue()
+      throws JsonProcessingException {
+    String confStr = "{\"disabled\": true}";
+    VectorIndexConfig config = JsonUtils.stringToObject(confStr, VectorIndexConfig.class);
+
+    assertTrue(config.isDisabled(), "Unexpected disabled");
+    assertNull(config.getVectorIndexType(), "Unexpected vectorIndexType");
+    assertNull(config.getVectorDistanceFunction(), "Unexpected vectorDistanceFunction");
+    assertNull(config.getProperties(), "unexpected properties");
+    assertEquals(config.getVectorDimension(), 0);
+    assertEquals(config.getVersion(), 0);
+  }
+
+  @Test
+  public void withSomeData()
+      throws JsonProcessingException {
+    String confStr = "{\n"
+        + "  \"version\": 1,\n"
+        + "  \"properties\": {\n"
+        + "    \"vectorIndexType\": \"HNSW\",\n"
+        + "    \"vectorDimension\": \"1536\",\n"
+        + "    \"vectorDistanceFunction\": \"COSINE\",\n"
+        + "    \"version\": \"1\"\n"
+        + "  },\n"
+        + "  \"vectorDistanceFunction\": \"COSINE\",\n"
+        + "  \"vectorIndexType\": \"HNSW\",\n"
+        + "  \"vectorDimension\": 1536,\n"
+        + "  \"disabled\": false\n"
+        + "}";
+    VectorIndexConfig config = JsonUtils.stringToObject(confStr, VectorIndexConfig.class);
+
+    assertFalse(config.isDisabled(), "Unexpected disabled");
+    assertEquals(config.getVectorDimension(), 1536);
+    assertEquals(config.getVersion(), 1);
+    assertEquals(config.getVectorIndexType(), "HNSW");
+    assertEquals(config.getVectorDistanceFunction(), COSINE);
+
+    Map<String, String> properties = config.getProperties();
+    assertEquals(properties.getOrDefault("vectorDimension", "0"), "1536");
+    assertEquals(properties.getOrDefault("version", "0"), "1");
+    assertEquals(properties.getOrDefault("vectorIndexType", ""), "HNSW");
+    assertEquals(properties.getOrDefault("vectorDistanceFunction", ""), COSINE.toString());
+  }
+
+  @Test
+  public void serde()
+      throws JsonProcessingException {
+    VectorIndexConfig initialConf = new VectorIndexConfig(false);
+
+    String confAsJson = JsonUtils.objectToString(initialConf);
+    Assert.assertEquals(confAsJson, "{\"disabled\":false,\"vectorDimension\":0,\"version\":0}");
+
+    VectorIndexConfig readConf = JsonUtils.stringToObject(confAsJson, VectorIndexConfig.class);
+    Assert.assertEquals(readConf, initialConf, "Unexpected configuration after serialization and deserialization");
+  }
+
+  @Test
+  public void deserializeShort()
+      throws JsonProcessingException {
+    VectorIndexConfig initialConf = new VectorIndexConfig(false);
+
+    String serialized = "{\"disabled\":false,\"vectorDimension\":0,\"version\":0}";
+    VectorIndexConfig vectorIndexConfig = JsonUtils.stringToObject(serialized, VectorIndexConfig.class);
+
+    Assert.assertEquals(vectorIndexConfig, initialConf, "Unexpected configuration after reading " + serialized);
+  }
+}
