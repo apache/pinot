85f233894833226cc789c3474821aa85c09fb930
Wenda (Song) Fu <songwdfu@gmail.com>
2025-06-27 19:52:17 -0400

    add tests covering single table short-circuit case

--- pinot-core/src/test/java/org/apache/pinot/core/query/selection/SelectionOrderByTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/query/selection/SelectionOrderByTest.java
@@ -23,24 +23,164 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.apache.commons.io.FileUtils;
+import org.apache.pinot.common.datatable.DataTable;
+import org.apache.pinot.common.metrics.BrokerMetrics;
+import org.apache.pinot.common.request.BrokerRequest;
+import org.apache.pinot.common.response.broker.BrokerResponseNative;
+import org.apache.pinot.common.response.broker.ResultTable;
+import org.apache.pinot.common.utils.DataSchema;
+import org.apache.pinot.core.common.datatable.DataTableBuilder;
+import org.apache.pinot.core.common.datatable.DataTableBuilderFactory;
+import org.apache.pinot.core.query.reduce.BrokerReduceService;
+import org.apache.pinot.core.transport.ServerRoutingInstance;
 import org.apache.pinot.queries.FluentQueryTest;
 import org.apache.pinot.spi.config.table.TableConfig;
 import org.apache.pinot.spi.config.table.TableType;
 import org.apache.pinot.spi.data.FieldSpec;
 import org.apache.pinot.spi.data.Schema;
+import org.apache.pinot.spi.env.PinotConfiguration;
+import org.apache.pinot.spi.utils.CommonConstants;
 import org.apache.pinot.spi.utils.builder.TableConfigBuilder;
+import org.apache.pinot.sql.parsers.CalciteSqlCompiler;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static org.mockito.Mockito.mock;
+import static org.testng.Assert.assertEquals;
+
 
 /** query-based tests for selection-orderby */
 public class SelectionOrderByTest {
+
+  @Test
+  public void testSingleTable()
+      throws IOException {
+    BrokerReduceService brokerReduceService =
+        new BrokerReduceService(
+            new PinotConfiguration(Map.of(CommonConstants.Broker.CONFIG_OF_MAX_REDUCE_THREADS_PER_QUERY, 2)));
+    BrokerRequest brokerRequest =
+        CalciteSqlCompiler.compileToBrokerRequest("SELECT col1 FROM testTable ORDER BY col1");
+    DataSchema dataSchema =
+        new DataSchema(new String[]{"col1"}, new DataSchema.ColumnDataType[]{DataSchema.ColumnDataType.INT});
+    DataTableBuilder dataTableBuilder = DataTableBuilderFactory.getDataTableBuilder(dataSchema);
+    // sorted block
+    dataTableBuilder.startRow();
+    dataTableBuilder.setColumn(0, 1);
+    dataTableBuilder.finishRow();
+    dataTableBuilder.startRow();
+    dataTableBuilder.setColumn(0, 2);
+    dataTableBuilder.finishRow();
+    dataTableBuilder.startRow();
+    dataTableBuilder.setColumn(0, 3);
+    dataTableBuilder.finishRow();
+
+    DataTable dataTable = dataTableBuilder.build();
+    dataTable.getMetadata().put(DataTable.MetadataKey.SORTED.getName(), "true");
+    Map<ServerRoutingInstance, DataTable> dataTableMap = new HashMap<>();
+    int numSortedInstances = 1;
+    for (int i = 0; i < numSortedInstances; i++) {
+      ServerRoutingInstance instance = new ServerRoutingInstance("localhost", i, TableType.OFFLINE);
+      dataTableMap.put(instance, dataTable);
+    }
+    long reduceTimeoutMs = 100000;
+    BrokerResponseNative brokerResponse =
+        brokerReduceService.reduceOnDataTable(brokerRequest, brokerRequest, dataTableMap, reduceTimeoutMs,
+            mock(BrokerMetrics.class));
+    brokerReduceService.shutDown();
+
+    ResultTable resultTable = brokerResponse.getResultTable();
+    List<Object[]> rows = resultTable.getRows();
+    assertEquals(rows.size(), 3);
+    assertEquals(rows.get(0), new Object[]{1});
+    assertEquals(rows.get(1), new Object[]{2});
+    assertEquals(rows.get(2), new Object[]{3});
+  }
+
+  @Test
+  public void testSingleTableWithNull()
+      throws IOException {
+    BrokerReduceService brokerReduceService =
+        new BrokerReduceService(
+            new PinotConfiguration(Map.of(CommonConstants.Broker.CONFIG_OF_MAX_REDUCE_THREADS_PER_QUERY, 2)));
+    BrokerRequest brokerRequest =
+        CalciteSqlCompiler.compileToBrokerRequest("SELECT col1 FROM testTable ORDER BY col1");
+    DataSchema dataSchema =
+        new DataSchema(new String[]{"col1"}, new DataSchema.ColumnDataType[]{DataSchema.ColumnDataType.INT});
+
+    List<Object[]> unsortedRows = new ArrayList<>();
+    unsortedRows.add(new Object[]{null});
+    unsortedRows.add(new Object[]{2});
+    unsortedRows.add(new Object[]{3});
+
+    DataTable dataTable = SelectionOperatorUtils.getDataTableFromRows(unsortedRows, dataSchema, true);
+    dataTable.getMetadata().put(DataTable.MetadataKey.SORTED.getName(), "true");
+    Map<ServerRoutingInstance, DataTable> dataTableMap = new HashMap<>();
+    int numSortedInstances = 1;
+    for (int i = 0; i < numSortedInstances; i++) {
+      ServerRoutingInstance instance = new ServerRoutingInstance("localhost", i, TableType.OFFLINE);
+      dataTableMap.put(instance, dataTable);
+    }
+    long reduceTimeoutMs = 100000;
+    BrokerResponseNative brokerResponse =
+        brokerReduceService.reduceOnDataTable(brokerRequest, brokerRequest, dataTableMap, reduceTimeoutMs,
+            mock(BrokerMetrics.class));
+    brokerReduceService.shutDown();
+
+    ResultTable resultTable = brokerResponse.getResultTable();
+    List<Object[]> rows = resultTable.getRows();
+    assertEquals(rows.size(), 3);
+    assertEquals(rows.get(0), new Object[]{0});
+    assertEquals(rows.get(1), new Object[]{2});
+    assertEquals(rows.get(2), new Object[]{3});
+  }
+
+  @Test
+  public void testSingleTableWithNullHandlingEnabled()
+      throws IOException {
+    BrokerReduceService brokerReduceService =
+        new BrokerReduceService(
+            new PinotConfiguration(Map.of(CommonConstants.Broker.CONFIG_OF_MAX_REDUCE_THREADS_PER_QUERY, 2)));
+    BrokerRequest brokerRequest =
+        CalciteSqlCompiler.compileToBrokerRequest(
+            "SET enableNullHandling=true; SELECT col1 FROM testTable ORDER BY col1");
+    DataSchema dataSchema =
+        new DataSchema(new String[]{"col1"}, new DataSchema.ColumnDataType[]{DataSchema.ColumnDataType.INT});
+
+    List<Object[]> unsortedRows = new ArrayList<>();
+    unsortedRows.add(new Object[]{1});
+    unsortedRows.add(new Object[]{2});
+    unsortedRows.add(new Object[]{null});
+
+    DataTable dataTable = SelectionOperatorUtils.getDataTableFromRows(unsortedRows, dataSchema, true);
+    dataTable.getMetadata().put(DataTable.MetadataKey.SORTED.getName(), "true");
+    Map<ServerRoutingInstance, DataTable> dataTableMap = new HashMap<>();
+    int numSortedInstances = 1;
+    for (int i = 0; i < numSortedInstances; i++) {
+      ServerRoutingInstance instance = new ServerRoutingInstance("localhost", i, TableType.OFFLINE);
+      dataTableMap.put(instance, dataTable);
+    }
+    long reduceTimeoutMs = 100000;
+    BrokerResponseNative brokerResponse =
+        brokerReduceService.reduceOnDataTable(brokerRequest, brokerRequest, dataTableMap, reduceTimeoutMs,
+            mock(BrokerMetrics.class));
+    brokerReduceService.shutDown();
+
+    ResultTable resultTable = brokerResponse.getResultTable();
+    List<Object[]> rows = resultTable.getRows();
+    assertEquals(rows.size(), 3);
+    assertEquals(rows.get(0), new Object[]{1});
+    assertEquals(rows.get(1), new Object[]{2});
+    assertEquals(rows.get(2), new Object[]{null});
+  }
 
   @Test
   public void list() {
