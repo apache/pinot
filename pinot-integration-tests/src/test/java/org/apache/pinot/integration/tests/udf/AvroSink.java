8c31eeef5ce81c8755ff2bff2c875ef372f0a9fd
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2025-07-22 11:31:01 +0200

    Improve AvroUtils to be able to read big decimals in standard avro format

--- pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/udf/AvroSink.java
+++ pinot-integration-tests/src/test/java/org/apache/pinot/integration/tests/udf/AvroSink.java
@@ -34,6 +34,7 @@ import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.pinot.plugin.inputformat.avro.AvroUtils;
 import org.apache.pinot.spi.data.FieldSpec;
 import org.apache.pinot.spi.data.readers.GenericRow;
 
@@ -109,10 +110,7 @@ public class AvroSink implements AutoCloseable {
       throws IOException {
     _avroSchema = convertPinotSchemaToAvroSchema(schema);
 
-    GenericData genericData = new GenericData();
-    genericData.addLogicalTypeConversion(new Conversions.BigDecimalConversion());
-
-    GenericDatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(_avroSchema, genericData);
+    GenericDatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(_avroSchema, AvroUtils.getGenericData());
     _dataFileWriter = new DataFileWriter<>(datumWriter);
     _dataFileWriter.create(_avroSchema, tempFile);
   }
