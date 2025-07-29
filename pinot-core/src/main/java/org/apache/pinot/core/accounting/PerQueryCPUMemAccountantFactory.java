c488727f49f6d1aa5d21c5f80ebc5317f77d20f7
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2025-07-29 17:12:22 +0530

    Remove logging of self-terminate

--- pinot-core/src/main/java/org/apache/pinot/core/accounting/PerQueryCPUMemAccountantFactory.java
+++ pinot-core/src/main/java/org/apache/pinot/core/accounting/PerQueryCPUMemAccountantFactory.java
@@ -307,7 +307,7 @@ public class PerQueryCPUMemAccountantFactory implements ThreadAccountantFactory 
 
       if (heapUsageBytes > config.getPanicLevel()) {
         if (_cancelSentQueries.add(queryId)) {
-          logTerminatedQuery(maxResourceUsageQuery, heapUsageBytes, false, true);
+          logTerminatedQuery(maxResourceUsageQuery, heapUsageBytes, false);
         }
         return true;
       }
@@ -315,7 +315,7 @@ public class PerQueryCPUMemAccountantFactory implements ThreadAccountantFactory 
       if (heapUsageBytes > config.getCriticalLevel() && maxResourceUsageQuery != null
           && maxResourceUsageQuery.getQueryId().equals(queryId)) {
         if (_cancelSentQueries.add(queryId)) {
-          logTerminatedQuery(maxResourceUsageQuery, heapUsageBytes, false, true);
+          logTerminatedQuery(maxResourceUsageQuery, heapUsageBytes, false);
         }
         return true;
       }
@@ -551,16 +551,15 @@ public class PerQueryCPUMemAccountantFactory implements ThreadAccountantFactory 
         } else {
           queryResourceTracker.getAnchorThread().interrupt();
         }
-        logTerminatedQuery(queryResourceTracker, _watcherTask.getHeapUsageBytes(), callback != null, false);
+        logTerminatedQuery(queryResourceTracker, _watcherTask.getHeapUsageBytes(), callback != null);
       }
     }
 
     protected void logTerminatedQuery(QueryResourceTracker queryResourceTracker, long totalHeapMemoryUsage,
-        boolean hasCallback, boolean isSelfTerminated) {
-      LOGGER.warn("Query {} terminated. Memory Usage: {}. Cpu Usage: {}. Total Heap Usage: {}. Used Callback: {}."
-              + " Self Terminated: {}",
+        boolean hasCallback) {
+      LOGGER.warn("Query {} terminated. Memory Usage: {}. Cpu Usage: {}. Total Heap Usage: {}. Used Callback: {}.",
           queryResourceTracker.getQueryId(), queryResourceTracker.getAllocatedBytes(),
-          queryResourceTracker.getCpuTimeNs(), totalHeapMemoryUsage, hasCallback, isSelfTerminated);
+          queryResourceTracker.getCpuTimeNs(), totalHeapMemoryUsage, hasCallback);
     }
 
     @Override
@@ -909,7 +908,7 @@ public class PerQueryCPUMemAccountantFactory implements ThreadAccountantFactory 
           // Log the killed queries
           for (AggregatedStats stats : aggregatedStatsMap.values()) {
             MseCancelCallback callback = _queryCancelCallbacks.getIfPresent(stats.getQueryId());
-            logTerminatedQuery(stats, _usedBytes, callback != null, false);
+            logTerminatedQuery(stats, _usedBytes, callback != null);
             if (callback != null) {
               _queryCancelCallbacks.invalidate(stats.getQueryId());
             }
@@ -970,7 +969,7 @@ public class PerQueryCPUMemAccountantFactory implements ThreadAccountantFactory 
             if (_queryMonitorConfig.get().isQueryKilledMetricEnabled()) {
               _metrics.addMeteredGlobalValue(_queryKilledMeter, 1);
             }
-            logTerminatedQuery(value, _usedBytes, hasCallBack, false);
+            logTerminatedQuery(value, _usedBytes, hasCallBack);
           }
         }
         logQueryResourceUsage(_aggregatedUsagePerActiveQuery);
