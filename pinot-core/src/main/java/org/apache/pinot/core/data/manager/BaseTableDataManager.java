1a7bc740b4d037863b07f39d6a767051e3aa1da8
Nihal Jain <nihaljain@apache.org>
2025-07-30 18:10:59 +0530

    Replace all usages of commons-collections with commons-collections4

--- pinot-core/src/main/java/org/apache/pinot/core/data/manager/BaseTableDataManager.java
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/BaseTableDataManager.java
@@ -44,7 +44,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Lock;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
--- pinot-core/src/main/java/org/apache/pinot/core/routing/LogicalTableRouteInfo.java
+++ pinot-core/src/main/java/org/apache/pinot/core/routing/LogicalTableRouteInfo.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nullable;
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.pinot.common.request.BrokerRequest;
 import org.apache.pinot.common.request.InstanceRequest;
 import org.apache.pinot.common.request.TableSegmentsInfo;
