102a000a94954917c8a7158b89170c97d64a11dd
Jonathan Baxter <jbaxter@etsy.com>
2025-08-24 18:33:33 -0400

    Bug fix for 1.2.0: getSegments call in ConsistentDataPush mode isn't using auth, so controllers that require auth return 403s during ingestion

--- pinot-segment-local/src/main/java/org/apache/pinot/segment/local/utils/ConsistentDataPushUtils.java
+++ pinot-segment-local/src/main/java/org/apache/pinot/segment/local/utils/ConsistentDataPushUtils.java
@@ -236,7 +236,7 @@ public class ConsistentDataPushUtils {
       try {
         controllerURI = new URI(pinotClusterSpec.getControllerURI());
         Map<String, List<String>> segments =
-            FILE_UPLOAD_DOWNLOAD_CLIENT.getSegments(controllerURI, rawTableName, TableType.OFFLINE, true);
+            FILE_UPLOAD_DOWNLOAD_CLIENT.getSegments(controllerURI, rawTableName, TableType.OFFLINE, true, AuthProviderUtils.makeAuthProvider(spec.getAuthToken()));
         offlineSegments = segments.get(TableType.OFFLINE.toString());
         uriToOfflineSegments.put(controllerURI, offlineSegments);
       } catch (URISyntaxException e) {
