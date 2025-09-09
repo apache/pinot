7adf51147d63b1e83e7a839f2314aa03ea82bf82
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2025-09-09 14:13:33 +0200

    Disable spammy test

--- pinot-core/src/test/java/org/apache/pinot/core/accounting/ResourceManagerAccountingTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/accounting/ResourceManagerAccountingTest.java
@@ -73,7 +73,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
-@Test(enabled = false) // disabled because it spams and affects other tests
+@Test(enabled = false) // this test is very spammy and leaks query threads. Enable when fixed
 public class ResourceManagerAccountingTest {
 
   public static final Logger LOGGER = LoggerFactory.getLogger(ResourceManagerAccountingTest.class);
