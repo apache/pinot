5eb1a5892b049cf794b78effcb99885f145bcba3
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2025-09-19 15:14:54 +0200

    Fix imports

--- pinot-core/src/main/java/org/apache/pinot/core/operator/ReverseDocIdSetOperator.java
+++ pinot-core/src/main/java/org/apache/pinot/core/operator/ReverseDocIdSetOperator.java
@@ -19,7 +19,6 @@
 package org.apache.pinot.core.operator;
 
 import com.google.common.base.Preconditions;
-import java.util.Collections;
 import java.util.List;
 import org.apache.pinot.core.common.BlockDocIdIterator;
 import org.apache.pinot.core.common.BlockDocIdSet;
