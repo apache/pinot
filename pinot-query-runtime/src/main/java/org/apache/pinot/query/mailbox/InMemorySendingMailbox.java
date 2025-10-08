d49bd3c96e09123918c609c0a2f9a2242576744a
Gonzalo Ortiz Jaureguizar <gortiz@users.noreply.github.com>
2025-10-08 17:52:39 +0200

    Remove SendingMailbox.complete method and add that logic when EOS is sent (#16915)

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/mailbox/InMemorySendingMailbox.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/mailbox/InMemorySendingMailbox.java
@@ -69,6 +69,7 @@ public class InMemorySendingMailbox implements SendingMailbox {
   public void send(MseBlock.Eos block, List<DataBuffer> serializedStats)
       throws IOException, TimeoutException {
     sendPrivate(block, serializedStats);
+    _isTerminated = true;
   }
 
   private void sendPrivate(MseBlock block, List<DataBuffer> serializedStats)
@@ -100,11 +101,6 @@ public class InMemorySendingMailbox implements SendingMailbox {
       default:
         throw new IllegalStateException("Unsupported mailbox status: " + status);
     }
-  }
-
-  @Override
-  public void complete() {
-    _isTerminated = true;
   }
 
   @Override
