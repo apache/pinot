481a5aa1e155c571ee6c6466a954fd29d4e0c90d
Gonzalo Ortiz Jaureguizar <gortiz@users.noreply.github.com>
2025-10-16 14:43:32 +0200

    Remove non splitter grpc sender (#17003)

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/mailbox/channel/MailboxContentObserver.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/mailbox/channel/MailboxContentObserver.java
@@ -128,8 +128,8 @@ public class MailboxContentObserver implements StreamObserver<MailboxContent> {
     _mailboxBuffers.clear();
     if (_mailbox != null) {
       String msg = t != null ? t.getMessage() : "Unknown";
-      _mailbox.setErrorBlock(ErrorMseBlock.fromError(
-          QueryErrorCode.QUERY_CANCELLATION, "Cancelled by sender with exception: " + msg), List.of());
+      String errorMessage = "GRPC mailbox cancelled by sender with exception: " + msg;
+      _mailbox.setErrorBlock(ErrorMseBlock.fromError(QueryErrorCode.INTERNAL, errorMessage), List.of());
     } else {
       LOGGER.error("Got error before mailbox is set up", t);
     }
