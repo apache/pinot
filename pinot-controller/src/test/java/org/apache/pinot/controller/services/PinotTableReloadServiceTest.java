d3e9259420a7068352646dd35143fdd107f73a8a
Suvodeep Pyne <suvodeep-pyne@users.noreply.github.com>
2025-10-17 09:42:11 -0700

    [controller] Refactor core reload logic out from the resource class to a dedicated service class (#17023)
    * [controller] Refactor core reload logic out from the resource class to a dedicated service class
    
    * [controller] Add ASF license headers to PinotTableReloadService and its test class

--- pinot-controller/src/test/java/org/apache/pinot/controller/services/PinotTableReloadServiceTest.java
+++ pinot-controller/src/test/java/org/apache/pinot/controller/services/PinotTableReloadServiceTest.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.pinot.controller.api.resources;
+package org.apache.pinot.controller.services;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,12 +35,12 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 
-public class PinotTableReloadResourceTest {
+public class PinotTableReloadServiceTest {
   @Mock
   PinotHelixResourceManager _pinotHelixResourceManager;
 
   @InjectMocks
-  PinotTableReloadResource _resource;
+  PinotTableReloadService _service;
 
   @BeforeMethod
   public void setup() {
@@ -60,26 +60,26 @@ public class PinotTableReloadResourceTest {
     when(_pinotHelixResourceManager.getServers(tableName, "seg01")).thenReturn(Set.of("svr01", "svr03"));
 
     // Get all servers and all their segments.
-    Map<String, List<String>> serverToSegmentsMap = _resource.getServerToSegments(tableName, null, null);
+    Map<String, List<String>> serverToSegmentsMap = _service.getServerToSegments(tableName, null, null);
     assertEquals(serverToSegmentsMap, fullServerToSegmentsMap);
 
     // Get all segments on svr02.
-    serverToSegmentsMap = _resource.getServerToSegments(tableName, null, "svr02");
+    serverToSegmentsMap = _service.getServerToSegments(tableName, null, "svr02");
     assertEquals(serverToSegmentsMap, Map.of("svr02", List.of("seg02", "seg03")));
 
     // Get all servers with seg01.
-    serverToSegmentsMap = _resource.getServerToSegments(tableName, "seg01", null);
+    serverToSegmentsMap = _service.getServerToSegments(tableName, "seg01", null);
     assertEquals(serverToSegmentsMap, Map.of("svr01", List.of("seg01"), "svr03", List.of("seg01")));
 
     // Simply map the provided server to the provided segments.
-    serverToSegmentsMap = _resource.getServerToSegments(tableName, "seg01", "svr01");
+    serverToSegmentsMap = _service.getServerToSegments(tableName, "seg01", "svr01");
     assertEquals(serverToSegmentsMap, Map.of("svr01", List.of("seg01")));
-    serverToSegmentsMap = _resource.getServerToSegments(tableName, "anySegment", "anyServer");
+    serverToSegmentsMap = _service.getServerToSegments(tableName, "anySegment", "anyServer");
     assertEquals(serverToSegmentsMap, Map.of("anyServer", List.of("anySegment")));
-    serverToSegmentsMap = _resource.getServerToSegments(tableName, "seg01|seg02", "svr02");
+    serverToSegmentsMap = _service.getServerToSegments(tableName, "seg01|seg02", "svr02");
     assertEquals(serverToSegmentsMap, Map.of("svr02", List.of("seg01", "seg02")));
     try {
-      _resource.getServerToSegments(tableName, "seg01,seg02", null);
+      _service.getServerToSegments(tableName, "seg01,seg02", null);
     } catch (Exception e) {
       assertTrue(e.getMessage().contains("Only one segment is expected but got: [seg01, seg02]"));
     }
