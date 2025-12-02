1eaaaabef9f903084e8aa7db952d53bc7afc5771
Anurag Rai <anurag.rai@uber.com>
2025-12-02 12:09:36 +0530

    revert port changes

--- pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/QuickstartRunner.java
+++ pinot-tools/src/main/java/org/apache/pinot/tools/admin/command/QuickstartRunner.java
@@ -52,7 +52,7 @@ public class QuickstartRunner {
   private static final int ZK_PORT = 2123;
   private static final String ZK_ADDRESS = "localhost:" + ZK_PORT;
 
-  public static final int DEFAULT_CONTROLLER_PORT = 9001;
+  public static final int DEFAULT_CONTROLLER_PORT = 9000;
   private static final int DEFAULT_BROKER_PORT = 8000;
   private static final int DEFAULT_SERVER_ADMIN_API_PORT = 7500;
   private static final int DEFAULT_SERVER_NETTY_PORT = 7050;
