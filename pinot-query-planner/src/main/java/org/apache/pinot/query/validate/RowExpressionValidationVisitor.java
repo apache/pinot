eab012072ef3a77f6a44e0b58e338be0756f5b75
Anurag Rai <anurag.rai@uber.com>
2025-12-04 10:15:02 +0530

    fix checkstyle and license issue

--- pinot-query-planner/src/main/java/org/apache/pinot/query/validate/RowExpressionValidationVisitor.java
+++ pinot-query-planner/src/main/java/org/apache/pinot/query/validate/RowExpressionValidationVisitor.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.pinot.query.validate;
 
 import java.util.List;
@@ -5,6 +23,7 @@ import org.apache.calcite.runtime.CalciteContextException;
 import org.apache.calcite.sql.SqlCall;
 import org.apache.calcite.sql.SqlKind;
 import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlNodeList;
 import org.apache.calcite.sql.parser.SqlParserPos;
 import org.apache.calcite.sql.util.SqlBasicVisitor;
 import org.apache.calcite.util.Static;
@@ -13,6 +32,7 @@ public class RowExpressionValidationVisitor extends SqlBasicVisitor<Void> {
   public RowExpressionValidationVisitor() {
   }
 
+  @Override
   public Void visit(SqlCall call) {
     SqlKind kind = call.getKind();
 
@@ -22,66 +42,90 @@ public class RowExpressionValidationVisitor extends SqlBasicVisitor<Void> {
       this.validateNoRowInOperands(call);
     }
 
-    return (Void)super.visit(call);
+    return (Void) super.visit(call);
   }
 
   private void validateRowComparison(SqlCall call) {
     List<SqlNode> operands = call.getOperandList();
     if (operands.size() == 2) {
-      SqlNode left = (SqlNode)operands.get(0);
-      SqlNode right = (SqlNode)operands.get(1);
+      SqlNode left = (SqlNode) operands.get(0);
+      SqlNode right = (SqlNode) operands.get(1);
       boolean leftIsRow = this.isRowExpression(left);
       boolean rightIsRow = this.isRowExpression(right);
       if (leftIsRow || rightIsRow) {
         if (leftIsRow && !rightIsRow) {
           SqlParserPos pos = left.getParserPosition();
-          String message = "ROW expressions are only supported when BOTH sides of a comparison are ROW expressions. Left operand is a ROW expression but right operand is not. Example: WHERE (col1, col2) > (val1, val2)";
-          throw (CalciteContextException)Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(), pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
+          String message = "ROW expressions are only supported when BOTH sides of a comparison are ROW expressions. "
+              + "Left operand is a ROW expression but right operand is not. Example: WHERE (col1, col2) > (val1, val2)";
+          throw (CalciteContextException) Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(),
+              pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
         } else if (!leftIsRow && rightIsRow) {
           SqlParserPos pos = right.getParserPosition();
-          String message = "ROW expressions are only supported when BOTH sides of a comparison are ROW expressions. Right operand is a ROW expression but left operand is not. Example: WHERE (col1, col2) > (val1, val2)";
-          throw (CalciteContextException)Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(), pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
+          String message = "ROW expressions are only supported when BOTH sides of a comparison are ROW expressions. "
+              + "Right operand is a ROW expression but left operand is not. Example: WHERE (col1, col2) > (val1, val2)";
+          throw (CalciteContextException) Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(),
+              pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
         } else {
           if (leftIsRow && rightIsRow) {
-            SqlCall leftRow = (SqlCall)left;
-            SqlCall rightRow = (SqlCall)right;
+            SqlCall leftRow = (SqlCall) left;
+            SqlCall rightRow = (SqlCall) right;
             int leftSize = leftRow.getOperandList().size();
             int rightSize = rightRow.getOperandList().size();
             if (leftSize != rightSize) {
               SqlParserPos pos = call.getParserPosition();
-              String message = String.format("ROW expressions in comparison must have the same number of fields. Left has %d field(s), right has %d field(s). Example: WHERE (col1, col2) > (val1, val2)", leftSize, rightSize);
-              throw (CalciteContextException)Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(), pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
+              String message = String.format("ROW expressions in comparison must have the same number of fields. "
+                  + "Left has %d field(s), right has %d field(s). Example: WHERE (col1, col2) > (val1, val2)",
+                  leftSize, rightSize);
+              throw (CalciteContextException) Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(),
+                  pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
             }
 
             if (leftSize == 0) {
               SqlParserPos pos = call.getParserPosition();
               String message = "ROW expressions cannot be empty. Example: WHERE (col1, col2) > (val1, val2)";
-              throw (CalciteContextException)Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(), pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
+              throw (CalciteContextException) Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(),
+                  pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
             }
           }
-
         }
       }
     }
   }
 
   private void validateNoRowInOperands(SqlCall call) {
-    for(SqlNode operand : call.getOperandList()) {
-      if (this.isRowExpression(operand)) {
+    List<SqlNode> operands = call.getOperandList();
+    for (SqlNode operand : operands) {
+      if (operand == null) {
+        continue;
+      }
+      if (isRowExpression(operand)) {
         SqlParserPos pos = operand.getParserPosition();
-        String message = String.format("ROW expressions are only supported in comparison contexts for keyset pagination. Found ROW expression in unsupported context.");
-        throw (CalciteContextException)Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(), pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
+        String message = "ROW expressions are only supported in comparison contexts.";
+        throw (CalciteContextException) Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(),
+            pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
+      }
+
+      if (operand instanceof SqlNodeList) {
+        SqlNodeList nodeList = (SqlNodeList) operand;
+        for (SqlNode node : nodeList) {
+          if (isRowExpression(node)) {
+            SqlParserPos pos = node.getParserPosition();
+            String message = "ROW expressions are only supported in comparison contexts.";
+            throw (CalciteContextException) Static.RESOURCE.validatorContext(pos.getLineNum(), pos.getColumnNum(),
+                pos.getEndLineNum(), pos.getEndColumnNum()).ex(new UnsupportedRowExpressionException(message));
+          }
+        }
       }
     }
-
   }
 
   private boolean isRowExpression(SqlNode node) {
-    return node instanceof SqlCall && ((SqlCall)node).getKind() == SqlKind.ROW;
+    return node instanceof SqlCall && ((SqlCall) node).getKind() == SqlKind.ROW;
   }
 
   private boolean isComparisonOperator(SqlKind kind) {
-    return kind == SqlKind.EQUALS || kind == SqlKind.NOT_EQUALS || kind == SqlKind.GREATER_THAN || kind == SqlKind.GREATER_THAN_OR_EQUAL || kind == SqlKind.LESS_THAN || kind == SqlKind.LESS_THAN_OR_EQUAL;
+    return kind == SqlKind.EQUALS || kind == SqlKind.NOT_EQUALS || kind == SqlKind.GREATER_THAN
+        || kind == SqlKind.GREATER_THAN_OR_EQUAL || kind == SqlKind.LESS_THAN || kind == SqlKind.LESS_THAN_OR_EQUAL;
   }
 
   public static class UnsupportedRowExpressionException extends RuntimeException {
@@ -90,4 +134,3 @@ public class RowExpressionValidationVisitor extends SqlBasicVisitor<Void> {
     }
   }
 }
-
