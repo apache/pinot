4084ebc35ed0b98baabdb42c393680c7c4983ea3
xuanyili <xuanyili@uber.com>
2025-12-11 17:15:53 +0000

    add http:// to the uri

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotTableRestletResource.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/resources/PinotTableRestletResource.java
@@ -305,6 +305,9 @@ public class PinotTableRestletResource {
 
       CopyTablePayload copyTablePayload = JsonUtils.stringToObject(payload, CopyTablePayload.class);
       String sourceControllerUri = copyTablePayload.getSourceClusterUri();
+      if (!sourceControllerUri.startsWith("http://") && !sourceControllerUri.startsWith("https://")) {
+        sourceControllerUri = "http://" + sourceControllerUri;
+      }
       Map<String, String> requestHeaders = copyTablePayload.getHeaders();
 
       LOGGER.info("[copyTable] Start copying table: {} from source: {}", tableName, sourceControllerUri);
