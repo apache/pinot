5ff962820009a773657703d8e96bb23303cfff1f
shauryachats <shauryachats@uber.com>
2025-12-16 04:03:58 +0000

    Removed String.format usage

--- pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/timeseries/TimeSeriesExchangeReceiveOperator.java
+++ pinot-query-runtime/src/main/java/org/apache/pinot/query/runtime/timeseries/TimeSeriesExchangeReceiveOperator.java
@@ -125,14 +125,14 @@ public class TimeSeriesExchangeReceiveOperator extends BaseTimeSeriesOperator {
       long remainingTimeMs = _deadlineMs - System.currentTimeMillis();
       if (remainingTimeMs <= 0) {
         timeoutException = new QueryException(QueryErrorCode.SERVER_NOT_RESPONDING,
-            String.format("Timed out before polling all servers. Successfully Polled: %s of %s", index,
-                _numServersQueried));
+            "Timed out before polling all servers. Successfully Polled: %s of %s" + index + " of " +
+                _numServersQueried);
         break;
       }
       Object result = poll(remainingTimeMs);
       if (result == null) {
         timeoutException = new QueryException(QueryErrorCode.SERVER_NOT_RESPONDING,
-            String.format("Timed out waiting for response. Waited: %s ms", remainingTimeMs));
+            "Timed out waiting for response. Waited: " + remainingTimeMs + " ms");
         break;
       }
       if (result instanceof Throwable) {
@@ -214,14 +214,14 @@ public class TimeSeriesExchangeReceiveOperator extends BaseTimeSeriesOperator {
       long remainingTimeMs = _deadlineMs - System.currentTimeMillis();
       if (remainingTimeMs <= 0) {
         timeoutException = new QueryException(QueryErrorCode.SERVER_NOT_RESPONDING,
-            String.format("Timed out before polling all servers. Successfully Polled: %s of %s", index,
-                _numServersQueried));
+            "Timed out before polling all servers. Successfully Polled: %s of %s" + index + " of " +
+                _numServersQueried);
         break;
       }
       Object result = _receiver.poll(remainingTimeMs, TimeUnit.MILLISECONDS);
       if (result == null) {
         timeoutException = new QueryException(QueryErrorCode.SERVER_NOT_RESPONDING,
-            String.format("Timed out waiting for response. Waited: %s ms", remainingTimeMs));
+            "Timed out waiting for response. Waited: " + remainingTimeMs + " ms");
         break;
       }
       if (result instanceof Throwable) {
