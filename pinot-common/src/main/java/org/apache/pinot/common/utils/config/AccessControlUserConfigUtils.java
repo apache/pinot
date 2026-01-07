7da14a6bf427a79f50d23b28a448efa268d9e3f2
Xiaotian (Jackie) Jiang <jackie.jxt@gmail.com>
2026-01-07 15:34:09 -0800

    [Minor] Fix mal-formated files

--- pinot-common/src/main/java/org/apache/pinot/common/utils/config/AccessControlUserConfigUtils.java
+++ pinot-common/src/main/java/org/apache/pinot/common/utils/config/AccessControlUserConfigUtils.java
@@ -27,67 +27,67 @@ import org.apache.helix.zookeeper.datamodel.ZNRecord;
 import org.apache.pinot.spi.config.user.AccessType;
 import org.apache.pinot.spi.config.user.UserConfig;
 
+
 /**
  * UserConfigUtils is responsible for two things:
  * 1. Used to acquire user config by parsing znRecord that stored in Zookeeper
  * 2. Used to construct znRecord by packaging user config
  */
 public class AccessControlUserConfigUtils {
-    private AccessControlUserConfigUtils() {
+  private AccessControlUserConfigUtils() {
+  }
+
+  public static UserConfig fromZNRecord(ZNRecord znRecord) {
+    Map<String, String> simpleFields = znRecord.getSimpleFields();
+
+    // Mandatory fields
+    String username = simpleFields.get(UserConfig.USERNAME_KEY);
+    String password = simpleFields.get(UserConfig.PASSWORD_KEY);
+    String component = simpleFields.get(UserConfig.COMPONET_KEY);
+    String role = simpleFields.get(UserConfig.ROLE_KEY);
+
+    List<String> tableList = znRecord.getListField(UserConfig.TABLES_KEY);
+    List<String> excludeTableList = znRecord.getListField(UserConfig.EXCLUDE_TABLES_KEY);
+
+    List<String> permissionListFromZNRecord = znRecord.getListField(UserConfig.PERMISSIONS_KEY);
+    List<AccessType> permissionList = null;
+    if (permissionListFromZNRecord != null) {
+      permissionList = permissionListFromZNRecord.stream().map(x -> AccessType.valueOf(x)).collect(Collectors.toList());
+    }
+    return new UserConfig(username, password, component, role, tableList, excludeTableList, permissionList);
+  }
+
+  public static ZNRecord toZNRecord(UserConfig userConfig)
+      throws JsonProcessingException {
+    Map<String, String> simpleFields = new HashMap<>();
+
+    // Mandatory fields
+    simpleFields.put(UserConfig.USERNAME_KEY, userConfig.getUserName());
+    simpleFields.put(UserConfig.PASSWORD_KEY, userConfig.getPassword());
+    simpleFields.put(UserConfig.COMPONET_KEY, userConfig.getComponentType().toString());
+    simpleFields.put(UserConfig.ROLE_KEY, userConfig.getRoleType().toString());
+
+    Map<String, List<String>> listFields = new HashMap<>();
+
+    // Optional fields
+    List<String> tableList = userConfig.getTables();
+    if (tableList != null) {
+      listFields.put(UserConfig.TABLES_KEY, userConfig.getTables());
+    }
+    List<String> excludeTableList = userConfig.getExcludeTables();
+    if (excludeTableList != null) {
+      listFields.put(UserConfig.EXCLUDE_TABLES_KEY, userConfig.getExcludeTables());
     }
 
-    public static UserConfig fromZNRecord(ZNRecord znRecord) {
-        Map<String, String> simpleFields = znRecord.getSimpleFields();
-
-        // Mandatory fields
-        String username = simpleFields.get(UserConfig.USERNAME_KEY);
-        String password = simpleFields.get(UserConfig.PASSWORD_KEY);
-        String component = simpleFields.get(UserConfig.COMPONET_KEY);
-        String role = simpleFields.get(UserConfig.ROLE_KEY);
-
-        List<String> tableList = znRecord.getListField(UserConfig.TABLES_KEY);
-        List<String> excludeTableList = znRecord.getListField(UserConfig.EXCLUDE_TABLES_KEY);
-
-        List<String> permissionListFromZNRecord = znRecord.getListField(UserConfig.PERMISSIONS_KEY);
-        List<AccessType> permissionList = null;
-        if (permissionListFromZNRecord != null) {
-            permissionList = permissionListFromZNRecord.stream()
-                .map(x -> AccessType.valueOf(x)).collect(Collectors.toList());
-        }
-        return new UserConfig(username, password, component, role, tableList, excludeTableList, permissionList);
+    List<AccessType> permissionList = userConfig.getPermissios();
+    if (permissionList != null) {
+      listFields.put(UserConfig.PERMISSIONS_KEY,
+          userConfig.getPermissios().stream().map(e -> e.toString()).collect(Collectors.toList()));
     }
 
-    public static ZNRecord toZNRecord(UserConfig userConfig)
-        throws JsonProcessingException {
-        Map<String, String> simpleFields = new HashMap<>();
-
-        // Mandatory fields
-        simpleFields.put(UserConfig.USERNAME_KEY, userConfig.getUserName());
-        simpleFields.put(UserConfig.PASSWORD_KEY, userConfig.getPassword());
-        simpleFields.put(UserConfig.COMPONET_KEY, userConfig.getComponentType().toString());
-        simpleFields.put(UserConfig.ROLE_KEY, userConfig.getRoleType().toString());
-
-        Map<String, List<String>> listFields = new HashMap<>();
-
-        // Optional fields
-        List<String> tableList = userConfig.getTables();
-        if (tableList != null) {
-            listFields.put(UserConfig.TABLES_KEY, userConfig.getTables());
-        }
-        List<String> excludeTableList = userConfig.getExcludeTables();
-        if (excludeTableList != null) {
-            listFields.put(UserConfig.EXCLUDE_TABLES_KEY, userConfig.getExcludeTables());
-        }
-
-        List<AccessType> permissionList = userConfig.getPermissios();
-        if (permissionList != null) {
-            listFields.put(UserConfig.PERMISSIONS_KEY, userConfig.getPermissios().stream()
-                .map(e -> e.toString()).collect(Collectors.toList()));
-        }
-
-        ZNRecord znRecord = new ZNRecord(userConfig.getUserName());
-        znRecord.setSimpleFields(simpleFields);
-        znRecord.setListFields(listFields);
-        return znRecord;
-    }
+    ZNRecord znRecord = new ZNRecord(userConfig.getUserName());
+    znRecord.setSimpleFields(simpleFields);
+    znRecord.setListFields(listFields);
+    return znRecord;
+  }
 }
--- pinot-spi/src/main/java/org/apache/pinot/spi/config/user/UserConfig.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/config/user/UserConfig.java
@@ -29,123 +29,122 @@ import org.apache.pinot.spi.config.BaseJsonConfig;
 
 
 public class UserConfig extends BaseJsonConfig {
-    public static final String USERNAME_KEY = "username";
-    public static final String PASSWORD_KEY = "password";
-    public static final String COMPONET_KEY = "component";
-    public static final String ROLE_KEY = "role";
-    public static final String AUTH_TOKEN_KEY = "authToken";
-    public static final String TABLES_KEY = "tables";
-    public static final String EXCLUDE_TABLES_KEY = "excludeTables";
-    public static final String PERMISSIONS_KEY = "permissions";
+  public static final String USERNAME_KEY = "username";
+  public static final String PASSWORD_KEY = "password";
+  public static final String COMPONET_KEY = "component";
+  public static final String ROLE_KEY = "role";
+  public static final String AUTH_TOKEN_KEY = "authToken";
+  public static final String TABLES_KEY = "tables";
+  public static final String EXCLUDE_TABLES_KEY = "excludeTables";
+  public static final String PERMISSIONS_KEY = "permissions";
 
-    @JsonPropertyDescription("The name of User")
-    private String _username;
+  @JsonPropertyDescription("The name of User")
+  private String _username;
 
-    @JsonPropertyDescription("The password of User")
-    private String _password;
+  @JsonPropertyDescription("The password of User")
+  private String _password;
 
-    @JsonPropertyDescription("The name of Component")
-    private ComponentType _componentType;
+  @JsonPropertyDescription("The name of Component")
+  private ComponentType _componentType;
 
-    @JsonPropertyDescription("The role of user")
-    private RoleType _roleType;
+  @JsonPropertyDescription("The role of user")
+  private RoleType _roleType;
 
-    @JsonPropertyDescription("The tables owned of User")
-    private List<String> _tables;
+  @JsonPropertyDescription("The tables owned of User")
+  private List<String> _tables;
 
-    @JsonPropertyDescription("The tables excluded for User")
-    private List<String> _excludeTables;
+  @JsonPropertyDescription("The tables excluded for User")
+  private List<String> _excludeTables;
 
-    @JsonPropertyDescription("The table permission of User")
-    private List<AccessType> _permissions;
+  @JsonPropertyDescription("The table permission of User")
+  private List<AccessType> _permissions;
 
-    @JsonCreator
-    public UserConfig(@JsonProperty(value = USERNAME_KEY, required = true) String username,
-        @JsonProperty(value = PASSWORD_KEY, required = true) String password,
-        @JsonProperty(value = COMPONET_KEY, required = true) String component,
-        @JsonProperty(value = ROLE_KEY, required = true) String role,
-        @JsonProperty(value = TABLES_KEY) @Nullable List<String> tableList,
-        @JsonProperty(value = EXCLUDE_TABLES_KEY) @Nullable List<String> excludeTableList,
-        @JsonProperty(value = PERMISSIONS_KEY) @Nullable List<AccessType> permissionList
-    ) {
-        Preconditions.checkArgument(username != null, "'username' must be configured");
-        Preconditions.checkArgument(password != null, "'password' must be configured");
+  @JsonCreator
+  public UserConfig(@JsonProperty(value = USERNAME_KEY, required = true) String username,
+      @JsonProperty(value = PASSWORD_KEY, required = true) String password,
+      @JsonProperty(value = COMPONET_KEY, required = true) String component,
+      @JsonProperty(value = ROLE_KEY, required = true) String role,
+      @JsonProperty(value = TABLES_KEY) @Nullable List<String> tableList,
+      @JsonProperty(value = EXCLUDE_TABLES_KEY) @Nullable List<String> excludeTableList,
+      @JsonProperty(value = PERMISSIONS_KEY) @Nullable List<AccessType> permissionList) {
+    Preconditions.checkArgument(username != null, "'username' must be configured");
+    Preconditions.checkArgument(password != null, "'password' must be configured");
 
-        // NOTE: Handle lower case table type and raw table name for backward-compatibility
-        _username = username;
-        _password = password;
-        _componentType = ComponentType.valueOf(component.toUpperCase());
-        _roleType = RoleType.valueOf(role.toUpperCase());
-        _tables = tableList;
-        _excludeTables = excludeTableList;
-        _permissions = permissionList;
+    // NOTE: Handle lower case table type and raw table name for backward-compatibility
+    _username = username;
+    _password = password;
+    _componentType = ComponentType.valueOf(component.toUpperCase());
+    _roleType = RoleType.valueOf(role.toUpperCase());
+    _tables = tableList;
+    _excludeTables = excludeTableList;
+    _permissions = permissionList;
+  }
+
+  @JsonProperty(USERNAME_KEY)
+  public String getUserName() {
+    return _username;
+  }
+
+  public String getUsernameWithComponent() {
+    return getUserName() + "_" + getComponentType().toString();
+  }
+
+  public boolean isExist(String username, ComponentType component) {
+    return _username.equals(username) && _componentType.equals(component);
+  }
+
+  @JsonProperty(PASSWORD_KEY)
+  public String getPassword() {
+    return _password;
+  }
+
+  @JsonProperty(TABLES_KEY)
+  public List<String> getTables() {
+    return _tables;
+  }
+
+  @JsonProperty(EXCLUDE_TABLES_KEY)
+  public List<String> getExcludeTables() {
+    return _excludeTables;
+  }
+
+  @JsonProperty(PERMISSIONS_KEY)
+  public List<AccessType> getPermissios() {
+    return _permissions;
+  }
+
+  @JsonProperty(COMPONET_KEY)
+  public ComponentType getComponentType() {
+    return _componentType;
+  }
+
+  @JsonProperty(ROLE_KEY)
+  public RoleType getRoleType() {
+    return _roleType;
+  }
+
+  public void setRole(String roleTypeStr) {
+    _roleType = RoleType.valueOf(roleTypeStr);
+  }
+
+  public void setPassword(String password) {
+    _password = password;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    @JsonProperty(USERNAME_KEY)
-    public String getUserName() {
-        return _username;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
+    UserConfig that = (UserConfig) o;
+    return _username.equals(that._username) && _componentType == that._componentType;
+  }
 
-    public String getUsernameWithComponent() {
-        return getUserName() + "_" + getComponentType().toString();
-    }
-
-    public boolean isExist(String username, ComponentType component) {
-        return _username.equals(username) && _componentType.equals(component);
-    }
-
-    @JsonProperty(PASSWORD_KEY)
-    public String getPassword() {
-        return _password;
-    }
-
-    @JsonProperty(TABLES_KEY)
-    public List<String> getTables() {
-        return _tables;
-    }
-
-    @JsonProperty(EXCLUDE_TABLES_KEY)
-    public List<String> getExcludeTables() {
-        return _excludeTables;
-    }
-
-    @JsonProperty(PERMISSIONS_KEY)
-    public List<AccessType> getPermissios() {
-        return _permissions;
-    }
-
-    @JsonProperty(COMPONET_KEY)
-    public ComponentType getComponentType() {
-        return _componentType;
-    }
-
-    @JsonProperty(ROLE_KEY)
-    public RoleType getRoleType() {
-        return _roleType;
-    }
-
-    public void setRole(String roleTypeStr) {
-        _roleType = RoleType.valueOf(roleTypeStr);
-    }
-
-    public void setPassword(String password) {
-        _password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        UserConfig that = (UserConfig) o;
-        return _username.equals(that._username) && _componentType == that._componentType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), _username, _componentType);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), _username, _componentType);
+  }
 }
