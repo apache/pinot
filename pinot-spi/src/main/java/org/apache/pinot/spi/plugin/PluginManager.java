7593019749e438d0103a3b5cb814a5746c556ab9
Arunkumar Saravanan <41850738+arunkumarucet@users.noreply.github.com>
2026-02-05 13:09:53 +0530

    Revert kafka 20 deprecation

--- pinot-spi/src/main/java/org/apache/pinot/spi/plugin/PluginManager.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/plugin/PluginManager.java
@@ -95,12 +95,9 @@ public class PluginManager {
       put("org.apache.pinot.filesystem.LocalPinotFS", "org.apache.pinot.spi.filesystem.LocalPinotFS");
 
       // StreamConsumerFactory
-      // Kafka 2.0 is deprecated - all kafka2/kafka20 class names redirect to kafka30
+      // Old-style class names mapped to current plugin packages
       put("org.apache.pinot.core.realtime.impl.kafka2.KafkaConsumerFactory",
-          "org.apache.pinot.plugin.stream.kafka30.KafkaConsumerFactory");
-      put("org.apache.pinot.plugin.stream.kafka20.KafkaConsumerFactory",
-          "org.apache.pinot.plugin.stream.kafka30.KafkaConsumerFactory");
-      // Old-style kafka3/kafka4 class names mapped to current plugin packages
+          "org.apache.pinot.plugin.stream.kafka20.KafkaConsumerFactory");
       put("org.apache.pinot.core.realtime.impl.kafka3.KafkaConsumerFactory",
           "org.apache.pinot.plugin.stream.kafka30.KafkaConsumerFactory");
       put("org.apache.pinot.core.realtime.impl.kafka4.KafkaConsumerFactory",
