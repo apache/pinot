d9666cc589d9955974879ae3006244e21832c483
Radhika Patwari <45709641+rsrkpatwari1234@users.noreply.github.com>
2026-02-08 14:22:08 +0530

    Update ExceptionUtils.java

--- pinot-common/src/main/java/org/apache/pinot/common/utils/ExceptionUtils.java
+++ pinot-common/src/main/java/org/apache/pinot/common/utils/ExceptionUtils.java
@@ -57,6 +57,35 @@ public class ExceptionUtils {
     return sb.toString();
   }
 
+  public static boolean isCauseInstanceOf(Throwable e, Class<? extends Throwable> type) {
+    while (e != null) {
+      if (type.isInstance(e)) {
+        return true;
+      }
+      if (e.getCause() == e) {
+        break;
+      }
+      e = e.getCause();
+    }
+    return false;
+  }
+
+  /**
+   * Returns the first throwable in the cause chain that is an instance of the given type, or null if none.
+   */
+  @SuppressWarnings("unchecked")
+  public static <T extends Throwable> T getFirstCauseOfType(Throwable e, Class<T> type) {
+    while (e != null) {
+      if (type.isInstance(e)) {
+        return (T) e;
+      }
+      if (e.getCause() == e) {
+        break;
+      }
+      e = e.getCause();
+    }
+    return null;
+  }
 
   public static String getStackTrace(Throwable e) {
     return getStackTrace(e, Integer.MAX_VALUE);
