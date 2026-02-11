4c8e30ca43b1b0b06af09fe0c37120c620f9ef8b
J-HowHuang <howardhuang2001@gmail.com>
2026-02-11 11:18:56 -0800

    reuse DecoratorExecutorService on SegmentOperationsExecutorService

--- pinot-core/src/main/java/org/apache/pinot/core/data/manager/SegmentOperationsExecutorService.java
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/SegmentOperationsExecutorService.java
@@ -18,114 +18,33 @@
  */
 package org.apache.pinot.core.data.manager;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
+import org.apache.pinot.spi.executor.DecoratorExecutorService;
 
 
 /**
  * An ExecutorService wrapper that automatically wraps all submitted tasks with segment operations context.
  */
-public class SegmentOperationsExecutorService implements ExecutorService {
-  private final ExecutorService _delegate;
+public class SegmentOperationsExecutorService extends DecoratorExecutorService {
   private final SegmentOperationsTaskType _taskType;
   private final String _tableNameWithType;
 
   public SegmentOperationsExecutorService(ExecutorService delegate, SegmentOperationsTaskType taskType,
-      @Nullable String tableNameWithType) {
-    _delegate = delegate;
+      String tableNameWithType) {
+    super(delegate);
     _taskType = taskType;
     _tableNameWithType = tableNameWithType;
   }
 
   @Override
-  public void execute(Runnable command) {
-    _delegate.execute(SegmentOperationsTaskWrapper.wrap(command, _taskType, _tableNameWithType));
+  protected <T> Callable<T> decorate(Callable<T> task) {
+    return SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType);
   }
 
   @Override
-  public void shutdown() {
-    _delegate.shutdown();
-  }
-
-  @Override
-  public List<Runnable> shutdownNow() {
-    return _delegate.shutdownNow();
-  }
-
-  @Override
-  public boolean isShutdown() {
-    return _delegate.isShutdown();
-  }
-
-  @Override
-  public boolean isTerminated() {
-    return _delegate.isTerminated();
-  }
-
-  @Override
-  public boolean awaitTermination(long timeout, TimeUnit unit)
-      throws InterruptedException {
-    return _delegate.awaitTermination(timeout, unit);
-  }
-
-  @Override
-  public <T> Future<T> submit(Callable<T> task) {
-    return _delegate.submit(SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType));
-  }
-
-  @Override
-  public <T> Future<T> submit(Runnable task, T result) {
-    return _delegate.submit(SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType), result);
-  }
-
-  @Override
-  public Future<?> submit(Runnable task) {
-    return _delegate.submit(SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType));
-  }
-
-  @Override
-  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks)
-      throws InterruptedException {
-    return _delegate.invokeAll(
-        tasks.stream()
-            .map(task -> SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType))
-            .collect(Collectors.toList()));
-  }
-
-  @Override
-  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-      throws InterruptedException {
-    return _delegate.invokeAll(
-        tasks.stream()
-            .map(task -> SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType))
-            .collect(Collectors.toList()),
-        timeout, unit);
-  }
-
-  @Override
-  public <T> T invokeAny(Collection<? extends Callable<T>> tasks)
-      throws InterruptedException, ExecutionException {
-    return _delegate.invokeAny(
-        tasks.stream()
-            .map(task -> SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType))
-            .collect(Collectors.toList()));
-  }
-
-  @Override
-  public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit)
-      throws InterruptedException, ExecutionException, TimeoutException {
-    return _delegate.invokeAny(
-        tasks.stream()
-            .map(task -> SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType))
-            .collect(Collectors.toList()),
-        timeout, unit);
+  protected Runnable decorate(Runnable task) {
+    return SegmentOperationsTaskWrapper.wrap(task, _taskType, _tableNameWithType);
   }
 }
