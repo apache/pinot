da4313321f8af1f90f5263646b42ee82c629bc68
Krishan Goyal <krishan@startree.ai>
2026-02-19 15:42:04 +0530

    Cleanup code

--- pinot-spi/src/main/java/org/apache/pinot/spi/config/table/TableConfig.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/config/table/TableConfig.java
@@ -24,6 +24,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.google.common.base.Preconditions;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
