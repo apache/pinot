2b95c54db8528fd613e56cc3f574ac7b40649d08
Jhow <44998515+J-HowHuang@users.noreply.github.com>
2026-02-26 15:47:56 -0800

    Provide Segment Operation Task Context (#17623)

---
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/SegmentOperationsExecutorService.java
@@ -0,0 +1,49 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.pinot.core.data.manager;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import org.apache.pinot.spi.executor.DecoratorExecutorService;
+
+
+/**
+ * An ExecutorService wrapper that automatically wraps all submitted tasks with segment operations context.
+ */
+public class SegmentOperationsExecutorService extends DecoratorExecutorService {
+  private final SegmentOperationsTaskType _taskType;
+  private final String _tableNameWithType;
+
+  public SegmentOperationsExecutorService(ExecutorService delegate, SegmentOperationsTaskType taskType,
+      String tableNameWithType) {
+    super(delegate);
+    _taskType = taskType;
+    _tableNameWithType = tableNameWithType;
+  }
+
+  @Override
+  protected <T> Callable<T> decorate(Callable<T> task) {
+    return SegmentOperationsTaskContext.wrap(task, _taskType, _tableNameWithType);
+  }
+
+  @Override
+  protected Runnable decorate(Runnable task) {
+    return SegmentOperationsTaskContext.wrap(task, _taskType, _tableNameWithType);
+  }
+}
--- pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/RealtimeSegmentDataManager.java
+++ pinot-core/src/main/java/org/apache/pinot/core/data/manager/realtime/RealtimeSegmentDataManager.java
@@ -53,6 +53,8 @@ import org.apache.pinot.common.restlet.resources.SegmentErrorInfo;
 import org.apache.pinot.common.utils.LLCSegmentName;
 import org.apache.pinot.common.utils.PauselessConsumptionUtils;
 import org.apache.pinot.common.utils.TarCompressionUtils;
+import org.apache.pinot.core.data.manager.SegmentOperationsTaskContext;
+import org.apache.pinot.core.data.manager.SegmentOperationsTaskType;
 import org.apache.pinot.core.data.manager.realtime.RealtimeConsumptionRateManager.ConsumptionRateLimiter;
 import org.apache.pinot.segment.local.data.manager.SegmentDataManager;
 import org.apache.pinot.segment.local.dedup.DedupContext;
@@ -1633,7 +1635,10 @@ public class RealtimeSegmentDataManager extends SegmentDataManager {
   }
 
   public void startConsumption() {
-    _consumerThread = new Thread(new PartitionConsumer(), _segmentNameStr);
+    Runnable consumer =
+        SegmentOperationsTaskContext.wrap(new PartitionConsumer(), SegmentOperationsTaskType.CONSUMER,
+            _tableNameWithType);
+    _consumerThread = new Thread(consumer, _segmentNameStr);
     _segmentLogger.info("Created new consumer thread {} for {}", _consumerThread, this);
     _consumerThread.start();
   }
