32129a90015aa0159c120cd4f7bfd3ce3cd402e0
RAGHVENDRA KUMAR YADAV <raghavmnnit@gmail.com>
2026-03-04 11:44:20 -0800

    Incorporating review comments.

--- pinot-common/src/main/java/org/apache/pinot/common/tier/FixedTierSegmentSelector.java
+++ pinot-common/src/main/java/org/apache/pinot/common/tier/FixedTierSegmentSelector.java
@@ -27,9 +27,12 @@ import org.apache.pinot.common.metadata.segment.SegmentZKMetadata;
  */
 public class FixedTierSegmentSelector implements TierSegmentSelector {
   private final Set<String> _segmentsToSelect;
+  // ["*"] means select all completed segments
+  private final boolean _selectAllSegments;
 
   public FixedTierSegmentSelector(Set<String> segmentsToSelect) {
     _segmentsToSelect = segmentsToSelect;
+    _selectAllSegments = segmentsToSelect.contains("*");
   }
 
   @Override
@@ -39,8 +42,7 @@ public class FixedTierSegmentSelector implements TierSegmentSelector {
 
   @Override
   public boolean selectSegment(String tableNameWithType, SegmentZKMetadata segmentZKMetadata) {
-    // ["*"] means select all completed segments
-    if (_segmentsToSelect.contains("*")) {
+    if (_selectAllSegments) {
       return segmentZKMetadata.getStatus().isCompleted();
     }
     return _segmentsToSelect.contains(segmentZKMetadata.getSegmentName())
