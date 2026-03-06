23318286afde0bc980cfd082b7cdf8da0fd00a5a
Krishan Goyal <krishan@startree.ai>
2026-03-06 13:28:53 +0530

    cstyle fix

--- pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/periodictask/RealtimeOffsetAutoResetKafkaHandler.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/helix/core/periodictask/RealtimeOffsetAutoResetKafkaHandler.java
@@ -19,7 +19,6 @@
 package org.apache.pinot.controller.helix.core.periodictask;
 
 import com.google.common.base.Preconditions;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
