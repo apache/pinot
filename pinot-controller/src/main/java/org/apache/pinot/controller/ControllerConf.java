31e3d156400efd5af67bb99464c6b7ecfec86e34
Mike Luvin <mluvin@stripe.com>
2026-03-09 16:00:36 -0400

    wait for controller to populate resource utilization info during startup

--- pinot-controller/src/main/java/org/apache/pinot/controller/ControllerConf.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/ControllerConf.java
@@ -386,6 +386,10 @@ public class ControllerConf extends PinotConfiguration {
       "controller.resource.utilization.checker.initial.delay";
   public static final String RESOURCE_UTILIZATION_CHECKER_FREQUENCY =
       "controller.resource.utilization.checker.frequency";
+  // Wait for the resource utilization checker to run once during controller startup, before marking the controller
+  // as ready. When enabled, controller.resource.utilization.checker.initial.delay is set to 0.
+  public static final String RESOURCE_UTILIZATION_CHECKER_WAIT_DURING_STARTUP =
+      "controller.resource.utilization.checker.waitDuringStartup";
   public static final String ENABLE_BATCH_MESSAGE_MODE = "controller.enable.batch.message.mode";
   public static final String ENABLE_HYBRID_TABLE_RETENTION_STRATEGY =
       "controller.enable.hybrid.table.retention.strategy";
@@ -421,6 +425,7 @@ public class ControllerConf extends PinotConfiguration {
   public static final boolean DEFAULT_ENABLE_DISK_UTILIZATION_CHECKER = false;
   public static final long DEFAULT_RESOURCE_UTILIZATION_CHECKER_INITIAL_DELAY = 300L; // 5 minutes
   public static final long DEFAULT_RESOURCE_UTILIZATION_CHECKER_FREQUENCY = 300L; // 5 minutes
+  public static final boolean DEFAULT_RESOURCE_UTILIZATION_CHECKER_WAIT_DURING_STARTUP = false;
   public static final boolean DEFAULT_ENABLE_BATCH_MESSAGE_MODE = false;
   public static final boolean DEFAULT_ENABLE_HYBRID_TABLE_RETENTION_STRATEGY = false;
   public static final String DEFAULT_CONTROLLER_MODE = ControllerMode.DUAL.name();
@@ -1143,11 +1148,17 @@ public class ControllerConf extends PinotConfiguration {
   }
 
   public long getResourceUtilizationCheckerInitialDelay() {
-    return getProperty(RESOURCE_UTILIZATION_CHECKER_INITIAL_DELAY, DEFAULT_RESOURCE_UTILIZATION_CHECKER_INITIAL_DELAY);
+    return isResourceUtilizationCheckerWaitDuringStartupEnabled() ? 0L
+        : getProperty(RESOURCE_UTILIZATION_CHECKER_INITIAL_DELAY, DEFAULT_RESOURCE_UTILIZATION_CHECKER_INITIAL_DELAY);
   }
 
   public long getResourceUtilizationCheckerFrequency() {
     return getProperty(RESOURCE_UTILIZATION_CHECKER_FREQUENCY, DEFAULT_RESOURCE_UTILIZATION_CHECKER_FREQUENCY);
+  }
+
+  public boolean isResourceUtilizationCheckerWaitDuringStartupEnabled() {
+    return getProperty(RESOURCE_UTILIZATION_CHECKER_WAIT_DURING_STARTUP,
+        DEFAULT_RESOURCE_UTILIZATION_CHECKER_WAIT_DURING_STARTUP);
   }
 
   public boolean isResourceUtilizationCheckEnabled() {
