264cbbb03f5090f5af072b810f023ad5b111f1f2
Radhika Patwari <45709641+rsrkpatwari1234@users.noreply.github.com>
2026-04-13 02:27:31 +0530

    Update IngestionDelayTrackerTest.java

--- pinot-core/src/test/java/org/apache/pinot/core/data/manager/realtime/IngestionDelayTrackerTest.java
+++ pinot-core/src/test/java/org/apache/pinot/core/data/manager/realtime/IngestionDelayTrackerTest.java
@@ -53,8 +53,8 @@ import org.testng.annotations.Test;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 
 public class IngestionDelayTrackerTest {
