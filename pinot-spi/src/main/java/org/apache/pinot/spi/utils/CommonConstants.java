362767c55a55cca6e14a92cfd09c48f6bbc61084
Xiang Fu <xiangfu@apache.org>
2026-09-21 21:32:45 -0700

    Correct partition pruning cache cutoff documentation

--- pinot-spi/src/main/java/org/apache/pinot/spi/utils/CommonConstants.java
+++ pinot-spi/src/main/java/org/apache/pinot/spi/utils/CommonConstants.java
@@ -1001,7 +1001,7 @@ public class CommonConstants {
         public static final String IN_PREDICATE_PRUNING_THRESHOLD = "inPredicatePruningThreshold";
 
         /// Set false to use the uncached single-column broker partition pruner. Defaults to true; the cache is only
-        /// used for at least 256 candidate segments. Does not disable partition pruning itself.
+        /// used at the configured minimum segment count (default 32). Does not disable partition pruning itself.
         public static final String ENABLE_PARTITION_PRUNING_CACHE = "enablePartitionPruningCache";
 
         // When evaluating REGEXP_LIKE predicate on a dictionary encoded column:
